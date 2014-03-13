115e307faf8a6be2fff8c64857dcd0a13370a975
Rob Caiger <rob@clocal.co.uk>
2014-03-13 15:40:31 +0000

    Default route added and error controller with standard response. Tidied up old controllers

--- app/api/module/Olcs/Db/src/Controller/UserController.php
+++ app/api/module/Olcs/Db/src/Controller/UserController.php
@@ -1,22 +1,7 @@
 <?php
 namespace Olcs\Db\Controller;
 
-use Zend\View\Model\JsonModel;
-use Olcs\Db\Utility\RestServerInterface as OlcsRestServerInterface;
-
 class UserController extends AbstractBasicRestServerController
 {
-    public function getRolesAction()
-    {
-        $id = (int) $this->plugin('params')->fromRoute('id', false);
-        //die(var_export($id, 1));
 
-        if (!$id) {
-            return new JsonModel(array('result' => 'fail', 'message' => 'ID not found'));
-        }
-
-        $extractedRoles = $this->getService()->getRoles($id);
-
-        return new JsonModel(array('result' => 'ok', 'data' => $extractedRoles));
-    }
 }
