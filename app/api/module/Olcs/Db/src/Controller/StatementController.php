593adc87e31d7862a1dcc3b73bbdfc777510b742
Rob Caiger <rob@clocal.co.uk>
2014-04-09 14:53:36 +0100

    Statement controller, service and unit tests

---
+++ app/api/module/Olcs/Db/src/Controller/StatementController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Statement REST controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+
+namespace Olcs\Db\Controller;
+
+/**
+ * Statement REST controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class StatementController extends AbstractBasicRestServerController
+{
+
+}
