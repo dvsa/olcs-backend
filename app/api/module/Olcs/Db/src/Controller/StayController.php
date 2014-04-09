1309751351dd6c08684b6d27ae0d0a2877da2646
Rob Caiger <rob@clocal.co.uk>
2014-04-09 15:05:32 +0100

    Service and Controller and Unit tests for the Stay Entity

---
+++ app/api/module/Olcs/Db/src/Controller/StayController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Stay REST controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+
+namespace Olcs\Db\Controller;
+
+/**
+ * Stay REST controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class StayController extends AbstractBasicRestServerController
+{
+
+}
