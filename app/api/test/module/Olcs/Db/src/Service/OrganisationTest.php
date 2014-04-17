6a7fadb7a2b15ca0ece00d923c2c5061e0982fc4
vagrant <vagrant@localhost.localdomain>
2014-04-17 14:30:59 +0000

    added missing unit tests for licenceOrganisation

--- app/api/test/module/Olcs/Db/src/Service/OrganisationTest.php
+++ app/api/test/module/Olcs/Db/src/Service/OrganisationTest.php
@@ -3,28 +3,34 @@
 /**
  * Tests Organisation Service
  *
- * @author Rob Caiger <rob@clocal.co.uk>
+ * @author Jakub Igla <jakub.igla@valtech.co.uk>
  */
 
 namespace OlcsTest\Db\Service;
 
 use PHPUnit_Framework_TestCase;
-use Olcs\Db\Service\Organisation;
 
 /**
  * Tests Organisation Service
  *
- * @author Rob Caiger <rob@clocal.co.uk>
+ * @author Jakub Igla <jakub.igla@valtech.co.uk>
  */
 class OrganisationTest extends PHPUnit_Framework_TestCase
 {
+    
     /**
      * Setup the service
      */
     protected function setUp()
     {
-        // We may want to inject the ServiceLocator in the future
-        $this->service = new Organisation();
+        $this->service = $this->getMock('\Olcs\Db\Service\Organisation', array(
+        	'log',
+            'getEntityManager',
+            'extract',
+            'getDoctrineHydrator', 
+            'dbPersist', 
+            'dbFlush',
+        ));
     }
 
     /**
@@ -36,4 +42,286 @@ class OrganisationTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $this->service->getValidSearchFields());
     }
+    
+    /**
+     * Test get
+     *
+     * @group Service
+     */
+    public function testGet()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar'
+        );
+    
+        $orgEntity = $this->getMock('\stdClass');
+        $licenceEntity = $this->getMock('\stdClass', array('getOrganisation'));
+        $licenceEntity->expects($this->once())
+            ->method('getOrganisation')
+            ->will($this->returnValue($orgEntity));
+    
+        $this->service->expects($this->once())
+            ->method('extract')
+            ->with($orgEntity)
+            ->will($this->returnValue($data));
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+            ->method('findOneBy')
+            ->with(array('id' => $id))
+            ->will($this->returnValue($licenceEntity));
+        
+        $entityManagerMock = $this->getMock('\stdClass', array('getRepository'));
+        $entityManagerMock->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+        
+        $this->service->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($entityManagerMock));
+        
+    
+        $this->assertEquals($data, $this->service->getByLicenceId($id));
+    }
+    
+    /**
+     * Test get where is no licence entity
+     *
+     * @group Service
+     */
+    public function testGetNoLicenceEntity()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar'
+        );
+    
+        $this->service->expects($this->once())
+        ->method('log');
+    
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+        ->method('findOneBy')
+        ->with(array('id' => $id))
+        ->will($this->returnValue(null));
+    
+        $entityManagerMock = $this->getMock('\stdClass', array('getRepository'));
+        $entityManagerMock->expects($this->once())
+        ->method('getRepository')
+        ->will($this->returnValue($repoMock));
+    
+        $this->service->expects($this->once())
+        ->method('getEntityManager')
+        ->will($this->returnValue($entityManagerMock));
+    
+    
+        $this->assertEquals(null, $this->service->getByLicenceId($id));
+    }
+    
+    /**
+     * Test get
+     *
+     * @group Service
+     */
+    public function testGetNoOrgEntity()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar'
+        );
+
+        $licenceEntity = $this->getMock('\stdClass', array('getOrganisation'));
+        $licenceEntity->expects($this->once())
+            ->method('getOrganisation')
+            ->will($this->returnValue(null));
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+            ->method('findOneBy')
+            ->with(array('id' => $id))
+            ->will($this->returnValue($licenceEntity));
+    
+        $entityManagerMock = $this->getMock('\stdClass', array('getRepository'));
+        $entityManagerMock->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+    
+        $this->service->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($entityManagerMock));
+    
+    
+        $this->assertEquals(null, $this->service->getByLicenceId($id));
+    }
+    
+    /**
+     * Test Update
+     *  Without version
+     *
+     * @expectedException \Olcs\Db\Exceptions\NoVersionException
+     *
+     * @group Service
+     * @group ServiceAbstract
+     */
+    public function testUpdateWithoutVersion()
+    {
+        $id = 7;
+        $data = array(
+        );
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        $this->service->updateByLicenceId($id, $data);
+    }
+    
+    /**
+     * Test get where is no licence entity
+     *
+     * @group Service
+     */
+    public function testUpdateNoLicenceEntity()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar',
+                'version' => 1,
+        );
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+            ->method('findOneBy')
+            ->with(array('id' => $id))
+            ->will($this->returnValue(null));
+    
+        $entityManagerMock = $this->getMock('\stdClass', array('getRepository'));
+        $entityManagerMock->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+    
+        $this->service->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($entityManagerMock));
+    
+    
+        $this->assertEquals(null, $this->service->updateByLicenceId($id, $data));
+    }
+    
+    /**
+     * Test get
+     *
+     * @group Service
+     */
+    public function testUpdateNoOrgEntity()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar',
+                'version' => 1,
+        );
+    
+        $licenceEntity = $this->getMock('\stdClass', array('getOrganisation'));
+        $licenceEntity->expects($this->once())
+            ->method('getOrganisation')
+            ->will($this->returnValue(null));
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+            ->method('findOneBy')
+            ->with(array('id' => $id))
+            ->will($this->returnValue($licenceEntity));
+    
+        $entityManagerMock = $this->getMock('\stdClass', array('getRepository'));
+        $entityManagerMock->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+    
+        $this->service->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($entityManagerMock));
+    
+    
+        $this->assertEquals(null, $this->service->updateByLicenceId($id, $data));
+    }
+    
+    /**
+     * Test get
+     *
+     * @group Service
+     */
+    public function testUpdate()
+    {
+        $id = 7;
+        $data = array(
+                'foo' => 'bar',
+                'version' => 1,
+        );
+        
+        $orgEntity = $this->getMock('\stdClass');
+        
+        $licenceEntity = $this->getMock('\stdClass', array('getOrganisation'));
+        $licenceEntity->expects($this->once())
+            ->method('getOrganisation')
+            ->will($this->returnValue($orgEntity));
+        
+        $mockHydrator = $this->getMock('\stdClass', array('hydrate'));
+        $mockHydrator->expects($this->once())
+            ->method('hydrate')
+            ->with($data, $orgEntity)
+            ->will($this->returnValue($orgEntity));
+        
+        $repoMock = $this->getMock('\stdClass', array('findOneBy'));
+        $repoMock->expects($this->once())
+            ->method('findOneBy')
+            ->with(array('id' => $id))
+            ->will($this->returnValue($licenceEntity));
+        
+        $mockEntityManager = $this->getMock('\stdClass', array('lock', 'getRepository'));
+        $mockEntityManager->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($repoMock));
+        
+        $mockEntityManager->expects($this->once())
+            ->method('lock')
+            ->will($this->returnValue($orgEntity));
+    
+        
+    
+        $this->service->expects($this->once())
+            ->method('log');
+    
+        
+    
+        
+        $this->service->expects($this->once())
+            ->method('getDoctrineHydrator')
+            ->will($this->returnValue($mockHydrator));
+        
+        $this->service->expects($this->exactly(2))
+            ->method('getEntityManager')
+            ->will($this->returnValue($mockEntityManager));
+        
+        $this->service->expects($this->once())
+            ->method('dbPersist')
+            ->will($this->returnValue($orgEntity));
+        
+        $this->service->expects($this->once())
+            ->method('dbFlush');
+    
+        $this->assertEquals(true, $this->service->updateByLicenceId($id, $data));
+    }
+    
 }
