760458eac299df5b60e9b915535892955e60ca7e
Alex P <alexander@toscontrol.com>
2014-05-02 16:49:18 +0100

    coding standards - some spaces removed

--- module/Olcs/Db/config/module.config.php
+++ module/Olcs/Db/config/module.config.php
@@ -113,7 +113,7 @@ return array(
             'ViewJsonStrategy'
         ),
         'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',    
+        'exception_template' => 'error/index',
         'template_map' => array(
              'error/404' => __DIR__ . '/../view/error/404.phtml',
              'error/index' => __DIR__ . '/../view/error/index.phtml',
