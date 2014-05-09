ac022bd3e2aa28f5194bac83f296d3506b42a873
Alex P <alexander@toscontrol.com>
2014-05-09 11:14:55 +0100

    config sections restored

--- module/Olcs/Db/config/module.config.php
+++ module/Olcs/Db/config/module.config.php
@@ -70,8 +70,55 @@ return array(
                         'controller' => 'person-licence-search'
                     )
                 )
-            )
+            ),
+            'licence-vehicle' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/licence-vehicle[/:id]',
+                    'constraints' => array(
+                        'id' => '[0-9]+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'licence-vehicle'
+                    )
+                )
+            ),
         )
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory'
+        ),
+        'factories' => array(
+            'serviceFactory' => '\Olcs\Db\Service\Factory'
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Generic' => 'Olcs\Db\Controller\GenericController',
+            'licence-organisation' => 'Olcs\Db\Controller\LicenceOrganisationController',
+            'licencevehicleusage' => 'Olcs\Db\Controller\LicenceVehicleUsageController',
+            'licence-vehicle' => 'Olcs\Db\Controller\LicenceVehicleController',
+            'note' => 'Olcs\Db\Controller\NoteController',
+            'operator-search' => 'Olcs\Db\Controller\OperatorSearchController',
+            'person-search' => 'Olcs\Db\Controller\PersonSearchController',
+            'person-licence-search' => 'Olcs\Db\Controller\PersonLicenceSearchController'
+        )
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => false,
+        'strategies' => array(
+            'ViewJsonStrategy'
+        ),
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+             'error/404' => __DIR__ . '/../view/error/404.phtml',
+             'error/index' => __DIR__ . '/../view/error/index.phtml',
+             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+         ),
     ),
     'companies_house_credentials' => array(
         'password' => 'XMLGatewayTestPassword',
