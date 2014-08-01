1ce03cbd6a775e963abf41b2f55947de80aa887a
Shaun Lizzio <shaun@lizzio.co.uk>
2014-08-01 16:16:14 +0100

    Added new back end controller and service to search for Defendants

---
+++ app/api/module/Olcs/Db/src/Service/Person.php
@@ -0,0 +1,239 @@
+<?php
+
+/**
+ * Person Service
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+
+namespace Olcs\Db\Service;
+
+/**
+ * Person Service
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+class Person extends ServiceAbstract
+{
+
+    /**
+     * Returns an indexed array of valid search terms for this service / entity.
+     *
+     * @return array
+     */
+    public function getValidSearchFields()
+    {
+        return array('firstName', 'lastName', 'dateOfBirth');
+    }
+
+    /**
+     * Find licences from a given search
+     *
+     * @param array $options
+     *
+     * @return array
+     */
+    public function findPersons($options = array())
+    {
+        $optionConditions = array(
+            'name' => array(
+                'condition' => '(p.first_name LIKE ? OR p.surname LIKE ?)',
+                'type' => 'LIKE'
+            ),
+            'firstName' => array(
+                'condition' => 'p.first_name LIKE ?',
+                'type' => 'LIKE'
+            ),
+            'lastName' => array(
+                'condition' => 'p.surname LIKE ?',
+                'type' => 'LIKE'
+            ),
+            'dateOfBirth' => array(
+                'condition' => 'p.date_of_birth = ?',
+                'type' => 'EQUALS'
+            )
+        );
+
+        $lookupColumn = array(
+            'name' => array('p.first_name', 'p.surname'),
+            'firstName' => 'p.first_name',
+            'lastName' => 'p.surname',
+            'dob' => 'p.date_of_birth',
+        );
+
+        $sortOrder = $this->getSortOrder($options);
+        $orderByClause = $this->formatOrderByClause(
+            $this->getOrderBy($options, $lookupColumn, $lookupColumn['name']), $sortOrder
+        );
+
+        list($conditions, $params) = $this->formatConditionsFromOptions($optionConditions, $options);
+
+        $where = $this->formatWhereClause($conditions);
+
+        $sql = 'SELECT p.* FROM person p ' . $where . $orderByClause;
+
+        $dataQuery = $this->em->getConnection()->prepare($sql);
+        $dataQuery->execute($params);
+        $results = $dataQuery->fetchAll();
+
+        return $results;
+    }
+
+    /**
+     * Get the sort order from the options
+     *
+     * @param array $options
+     * @return string
+     */
+    private function getSortOrder($options, $default = 'ASC')
+    {
+        return (
+            isset($options['order']) && $options['order'] === 'DESC'
+            ) ? 'DESC' : $default;
+    }
+
+    /**
+     * Get the order by column name
+     *
+     * @param array $options
+     * @param array $lookupColumn
+     * @param string $default
+     * @return string
+     */
+    private function getOrderBy($options, $lookupColumn, $default = '')
+    {
+        return (
+            isset($options['sort']) && isset($lookupColumn[$options['sort']])
+            ) ? $lookupColumn[$options['sort']] : $default;
+    }
+
+    /**
+     * Get the limit
+     *
+     * @param array $options
+     * @param int $default
+     * @return int
+     */
+    private function getLimit($options, $default = 10)
+    {
+        return (
+            isset($options['limit']) && is_numeric($options['limit']) && $options['limit'] > 0
+            ) ? (int) $options['limit'] : $default;
+    }
+
+    /**
+     * Get the page number
+     *
+     * @param array $options
+     * @param int $default
+     * @return int
+     */
+    private function getPageNumber($options, $default = 1)
+    {
+        return (
+            isset($options['page']) && is_numeric($options['page']) && $options['page'] > 0
+            ) ? (int) $options['page'] : $default;
+    }
+
+    /**
+     * Format the order by statement
+     *
+     * @param string|array $orderBy
+     * @param string $sortOrder
+     */
+    private function formatOrderByClause($orderBy, $sortOrder)
+    {
+        if (is_array($orderBy)) {
+            return 'ORDER BY ' . implode(' ' . $sortOrder . ', ', $orderBy) . ' ' . $sortOrder . ' ';
+        }
+
+        return 'ORDER BY ' . $orderBy . ' ' . $sortOrder . ' ';
+    }
+
+    /**
+     * Format limit clause
+     *
+     * @param int $limit
+     * @param int $offset
+     */
+    private function formatLimitClause($limit, $offset = null)
+    {
+        if (empty($offset)) {
+            return 'LIMIT ' . $limit;
+        } else {
+            return 'LIMIT ' . $offset . ',' . $limit;
+        }
+    }
+
+    /**
+     * Format where clause
+     *
+     * @param array $conditions
+     * @return string
+     */
+    private function formatWhereClause($conditions, $prependWhere = true)
+    {
+        if (empty($conditions)) {
+            return '';
+        }
+
+        return ($prependWhere ? 'WHERE ' : '') . '( ' . implode(') AND (', $conditions) . ') ';
+    }
+
+    /**
+     * Format a value for binding
+     *
+     * @param string $value
+     * @param string $type
+     * @return string
+     */
+    private function formatValue($value, $type)
+    {
+        switch (strtoupper($type)) {
+            case 'LIKE':
+                return '%' . $value . '%';
+            /**
+             * These could be useful in the future
+             */
+            //case 'STARTS WITH':
+            //    return $value . '%';
+            //case 'ENDS WITH':
+            //    return '%' . $value;
+            case 'DATE':
+                $parts = explode('-', $value);
+                $newParts = array_map(
+                    function ($value) {
+                        return empty($value) ? '%' : $value;
+                    }, $parts
+                );
+                return implode('-', $newParts);
+            default:
+                return $value;
+        }
+    }
+
+    /**
+     * Generate individual where clauses from the data passed in
+     *
+     * @param array $optionsToFields
+     * @param array $options
+     * @return array
+     */
+    private function formatConditionsFromOptions($optionConditions, $options)
+    {
+        $conditions = $params = array();
+
+        foreach ($optionConditions as $optionKey => $details) {
+
+            if (isset($options[$optionKey])) {
+
+                $value = $this->formatValue($options[$optionKey], $details['type']);
+
+                $conditions[] = str_replace('?', ':' . $optionKey, $details['condition']);
+                $params[':' . $optionKey] = $value;
+            }
+        }
+
+        return array($conditions, $params);
+    }
+}
