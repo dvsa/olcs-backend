80b02eb39e586a60e788370c4ef6ed9d53ba462f
Shaun Lizzio <shaun@lizzio.co.uk>
2014-08-08 16:02:58 +0100

    Changed route name, to fix home search page

--- app/api/module/Olcs/Db/config/module.config.php
+++ app/api/module/Olcs/Db/config/module.config.php
@@ -71,7 +71,7 @@ return [
                     ]
                 ]
             ],
-            'operator-search' => [
+            'organisation-search' => [
                 'type' => 'segment',
                 'options' => [
                     'route' => '/organisation-search[/:id]',
