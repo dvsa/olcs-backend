fe629cb4bb27dbf96984a402116052251053cb7d
Rob Caiger <rob@clocal.co.uk>
2014-08-12 10:43:44 +0100

    Fixed the Yes No type unit tests

--- app/api/test/module/Olcs/Db/src/Entity/Types/YesNoNullTypeTest.php
+++ app/api/test/module/Olcs/Db/src/Entity/Types/YesNoNullTypeTest.php
@@ -33,7 +33,7 @@ class YesNoNullTypeTest extends PHPUnit_Framework_TestCase
     public function testGetSqlDeclaration()
     {
         $mockPlatform = $this->getMock('\Doctrine\DBAL\Platforms\MySqlPlatform');
-        $this->assertEquals('tinyint(1) NULL', $this->type->getSqlDeclaration(array(), $mockPlatform));
+        $this->assertEquals('tinyint(1) NULL COMMENT \'(DC2Type:yesnonull)\'', $this->type->getSqlDeclaration(array(), $mockPlatform));
     }
 
     /**
