1cb6d1f972195571c5b9e771dcd603d707090220
Chris Riley <chris.riley@valtech.co.uk>
2014-08-14 16:06:58 +0100

    olcs-2657: Added refdata controller and repository for fetching dynamic select data

---
+++ module/Olcs/Db/src/Entity/Repository/RefData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Olcs\Db\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class RefData extends EntityRepository
+{
+    public function findAllByCategoryAndLanguage($category, $language)
+    {
+        $qb = $this->createQueryBuilder('r');
+        $qb->where('r.refDataCategoryId = ?0');
+
+        $qb->setParameters([$category]);
+
+        $q = $qb->getQuery();
+        $q->setHint(
+            \Doctrine\ORM\Query::HINT_CUSTOM_OUTPUT_WALKER,
+            'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker'
+        );
+        $q->setHint(\Gedmo\Translatable\TranslatableListener::HINT_FALLBACK, 1);
+        $q->setHint(\Gedmo\Translatable\TranslatableListener::HINT_TRANSLATABLE_LOCALE, $language);
+
+        return $q->getArrayResult();
+    }
+}
