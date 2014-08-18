6a2fe629fadb5916ee857aa520aab90f1def9fe0
Henry Hayes <henryhayesuk@gmail.com>
2014-08-18 11:37:05 +0100

    Impounding legislation types

--- app/api/module/Olcs/Db/src/Entity/RefData.php
+++ app/api/module/Olcs/Db/src/Entity/RefData.php
@@ -3,6 +3,7 @@
 namespace Olcs\Db\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 use Olcs\Db\Entity\Traits;
 
 /**
@@ -35,6 +36,15 @@ class RefData implements Interfaces\EntityInterface
     protected $refDataCategory;
 
     /**
+     * Impounding
+     *
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Olcs\Db\Entity\Impounding", mappedBy="impoundingLegislationTypes", fetch="LAZY")
+     */
+    protected $impoundings;
+
+    /**
      * Olbs key
      *
      * @var string
@@ -42,6 +52,14 @@ class RefData implements Interfaces\EntityInterface
      * @ORM\Column(type="string", name="olbs_key", length=20, nullable=true)
      */
     protected $olbsKey;
+
+    /**
+     * Initialise the collections
+     */
+    public function __construct()
+    {
+        $this->impoundings = new ArrayCollection();
+    }
 
 
     /**
@@ -65,6 +83,66 @@ class RefData implements Interfaces\EntityInterface
     public function getRefDataCategory()
     {
         return $this->refDataCategory;
+    }
+
+    /**
+     * Set the impounding
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $impoundings
+     * @return RefData
+     */
+    public function setImpoundings($impoundings)
+    {
+        $this->impoundings = $impoundings;
+
+        return $this;
+    }
+
+    /**
+     * Get the impoundings
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getImpoundings()
+    {
+        return $this->impoundings;
+    }
+
+    /**
+     * Add a impoundings
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $impoundings
+     * @return RefData
+     */
+    public function addImpoundings($impoundings)
+    {
+        if ($impoundings instanceof ArrayCollection) {
+            $this->impoundings = new ArrayCollection(
+                array_merge(
+                    $this->impoundings->toArray(),
+                    $impoundings->toArray()
+                )
+            );
+        } elseif (!$this->impoundings->contains($impoundings)) {
+            $this->impoundings->add($impoundings);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove a impoundings
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $impoundings
+     * @return RefData
+     */
+    public function removeImpoundings($impoundings)
+    {
+        if ($this->impoundings->contains($impoundings)) {
+            $this->impoundings->remove($impoundings);
+        }
+
+        return $this;
     }
 
     /**
