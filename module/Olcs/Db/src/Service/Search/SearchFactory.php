bfc464d3436a05b7d117a7f4315665f8faa18c74
Chris Riley <chris.riley@valtech.co.uk>
2014-10-24 15:38:12 +0100

    Cs fixes

--- module/Olcs/Db/src/Service/Search/SearchFactory.php
+++ module/Olcs/Db/src/Service/Search/SearchFactory.php
@@ -5,6 +5,10 @@ namespace Olcs\Db\Service\Search;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+/**
+ * Class SearchFactory
+ * @package Olcs\Db\Service\Search
+ */
 class SearchFactory implements FactoryInterface
 {
     /**
