a82f6ab08c5e69ec7ef20ae439292635a6d31e2c
Rob Caiger <rob@clocal.co.uk>
2014-11-13 11:44:15 +0000

    Gitignore

---
+++ app/api/data/Entity/Partials/CustomLastModifiedOnField_Properties.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Olcs\Db\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Last modified on field trait
+ *
+ * @Note: Auto-generated (Shared between 134 entities)
+ */
+trait CustomLastModifiedOnField
+{
+
+    /**
+     * Last modified on
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", name="last_modified_on", nullable=true)
+     */
+    protected $lastModifiedOn;
+
+    /**
+     * Set the last modified on
+     *
+     * @param \DateTime $lastModifiedOn
+     * @return \Olcs\Db\Entity\Interfaces\EntityInterface
+     */
+    public function setLastModifiedOn($lastModifiedOn)
+    {
+        $this->lastModifiedOn = $lastModifiedOn;
+
+        return $this;
+    }
+
+    /**
+     * Get the last modified on
+     *
+     * @return \DateTime
+     */
+    public function getLastModifiedOn()
+    {
+        return $this->lastModifiedOn;
+    }
+
+    /**
+     * Set the lastModifiedOn field on persist
+     *
+     * @ORM\PreUpdate
+     */
+    public function setLastModifiedOnBeforeUpdate()
+    {
+        $this->setLastModifiedOn(new \DateTime('NOW'));
+    }
+}
