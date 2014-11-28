e5d9aec171aef5f73a4ab2d4a621b7a2537f14dc
Rob Caiger <rob@clocal.co.uk>
2014-11-28 09:12:28 +0000

    Schema updates

--- data/db/standardise-schema.sh
+++ data/db/standardise-schema.sh
@@ -5,11 +5,11 @@ read -s -p "Please enter mysql root password: " mypassword
 echo "Refreshing tmp database";
 mysql -uroot --password=$mypassword -e "DROP DATABASE IF EXISTS olcs_tmp; CREATE DATABASE olcs_tmp";
 
-echo "Importing olcs_schema";
-mysql -uroot --password=$mypassword olcs_tmp < olcs_schema.sql;
+echo "Importing schema";
+mysql -uroot --password=$mypassword olcs_tmp < schema.sql;
 
-echo "Dumping olcs_schema";
-mysqldump -uroot --password=$mypassword olcs_tmp > olcs_dump.sql;
+echo "Dumping schema_dump";
+mysqldump -uroot --password=$mypassword olcs_tmp > schema_dump.sql;
 
 echo "Refreshing tmp database";
 mysql -uroot --password=$mypassword -e "DROP DATABASE IF EXISTS olcs_tmp; CREATE DATABASE olcs_tmp";
