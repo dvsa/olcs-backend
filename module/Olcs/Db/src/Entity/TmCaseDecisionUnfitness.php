5cbd3eb5872607be9e5d5a6b35f85462b629f9bd
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-01-16 17:53:44 +0000

    Reinstate tm_case_decision_rehab and tm_case_decision_unfitness

---
+++ module/Olcs/Db/src/Entity/TmCaseDecisionUnfitness.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Olcs\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Olcs\Db\Entity\Traits;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * TmCaseDecisionUnfitness Entity
+ *
+ * Auto-Generated
+ *
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @Gedmo\SoftDeleteable(fieldName="deletedDate", timeAware=true)
+ * @ORM\Table(name="tm_case_decision_unfitness",
+ *    indexes={
+ *        @ORM\Index(name="fk_tm_case_decision_unfitness_tm_case_decision1_idx", columns={"tm_case_decision_id"}),
+ *        @ORM\Index(name="fk_tm_case_decision_unfitness_ref_data1_idx", columns={"unfitness_reason_id"}),
+ *        @ORM\Index(name="fk_tm_case_decision_unfitness_user1_idx", columns={"created_by"}),
+ *        @ORM\Index(name="fk_tm_case_decision_unfitness_user2_idx", columns={"last_modified_by"})
+ *    }
+ * )
+ */
+class TmCaseDecisionUnfitness implements Interfaces\EntityInterface
+{
+    use Traits\CustomBaseEntity,
+        Traits\CreatedByManyToOne,
+        Traits\CustomCreatedOnField,
+        Traits\CustomDeletedDateField,
+        Traits\IdIdentity,
+        Traits\LastModifiedByManyToOne,
+        Traits\CustomLastModifiedOnField,
+        Traits\TmCaseDecisionManyToOne,
+        Traits\CustomVersionField;
+
+    /**
+     * Unfitness reason
+     *
+     * @var \Olcs\Db\Entity\RefData
+     *
+     * @ORM\ManyToOne(targetEntity="Olcs\Db\Entity\RefData")
+     * @ORM\JoinColumn(name="unfitness_reason_id", referencedColumnName="id", nullable=false)
+     */
+    protected $unfitnessReason;
+
+    /**
+     * Set the unfitness reason
+     *
+     * @param \Olcs\Db\Entity\RefData $unfitnessReason
+     * @return TmCaseDecisionUnfitness
+     */
+    public function setUnfitnessReason($unfitnessReason)
+    {
+        $this->unfitnessReason = $unfitnessReason;
+
+        return $this;
+    }
+
+    /**
+     * Get the unfitness reason
+     *
+     * @return \Olcs\Db\Entity\RefData
+     */
+    public function getUnfitnessReason()
+    {
+        return $this->unfitnessReason;
+    }
+}
