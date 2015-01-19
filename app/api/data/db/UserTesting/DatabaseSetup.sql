d118ff530afda398db2fa02f63ee9fe5f9060bd7
Rob Caiger <rob@clocal.co.uk>
2015-01-19 15:40:22 +0000

    Updated db name in user data scripts

--- app/api/data/db/UserTesting/DatabaseSetup.sql
+++ app/api/data/db/UserTesting/DatabaseSetup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS olcs_be;
+DROP DATABASE IF EXISTS olcs_user_testing;
 
-CREATE DATABASE olcs_be;
+CREATE DATABASE olcs_user_testing;
 
-GRANT ALL ON olcs_be.* TO olcs_be@localhost IDENTIFIED BY 'password';
+GRANT ALL ON olcs_user_testing.* TO olcs_user_testing@localhost IDENTIFIED BY 'password';
