7008d8559299c943d013564c8d281752292c2586
Rob Caiger <rob@clocal.co.uk>
2015-01-19 15:40:22 +0000

    Updated db name in user data scripts

--- data/db/UserTesting/db_setup.sql
+++ data/db/UserTesting/db_setup.sql
@@ -4,8 +4,8 @@ mysql -uroot -ppassword < DatabaseSetup.sql
 
 ../../../vendor/bin/doctrine-module orm:schema:update --force
 
-mysql -uroot -ppassword olcs_be < ../Rollout.sql
+mysql -uroot -ppassword olcs_user_testing < ../Rollout.sql
 
-mysql -uroot -ppassword olcs_be < minimum_data.sql
+mysql -uroot -ppassword olcs_user_testing < minimum_data.sql
 
 echo "All done!"
