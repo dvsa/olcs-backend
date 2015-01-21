e511d44ec668775665e184860778d736c9e01609
Rob Caiger <rob@clocal.co.uk>
2015-01-21 16:03:25 +0000

    Updated testdata

--- app/api/data/db/testdata.sql
+++ app/api/data/db/testdata.sql
@@ -1377,13 +1377,13 @@ INSERT INTO `community_lic` (
     `status`, `licence_id`, `expired_date`, `issue_no`, `serial_no`,
     `serial_no_prefix`, `specified_date`, `licence_expired_date`
 ) VALUES
-    ('cl_sts_pending', 7, NULL, 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_valid', 7, NULL, 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_valid', 7, NULL, 1, 12345, 'A', NULL, NULL),
-    ('cl_sts_expired', 7, '2014-01-01', 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_withdrawn', 7, NULL, 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_suspended', 7, NULL, 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_void', 7, NULL, 0, 12345, 'A', NULL, NULL),
-    ('cl_sts_returned', 7, NULL, 0, 12345, 'A', NULL, NULL);
+    ('cl_sts_valid', 7, NULL, 0, NULL, 'UKGB', '2015-01-01', NULL),
+    ('cl_sts_valid', 7, NULL, 1, NULL, 'UKGB', '2015-01-01', NULL),
+    ('cl_sts_expired', 7, '2014-01-01', 2, NULL, 'UKGB', '2015-01-01', NULL),
+    ('cl_sts_withdrawn', 7, '2014-03-12', 3, NULL, 'UKGB', '2015-01-01', NULL),
+    ('cl_sts_suspended', 7, '2014-06-07', 4, NULL, 'UKGB', '2015-01-01', NULL),
+    ('cl_sts_void', 7, '2014-09-20', 5, NULL, 'UKNI', '2015-01-01', NULL),
+    ('cl_sts_returned', 7, '2014-01-18', 6, NULL, 'UKNI', '2015-01-01', NULL)
+    ('cl_sts_pending', 7, NULL, 7, NULL, 'UKNI', NULL, NULL);
 
 SET foreign_key_checks = 1;
