bf69f120215b658950841050a0e9936630e708f5
Rob Caiger <rob@clocal.co.uk>
2015-02-04 15:13:43 +0000

    Reverted back due to sql failures

--- data/db/db_testdata_setup.sh
+++ data/db/db_testdata_setup.sh
@@ -2,9 +2,9 @@
 
 mysql -uroot -ppassword < DatabaseSetup.sql
 
-#../../vendor/bin/doctrine-module orm:schema:update --force
+../../vendor/bin/doctrine-module orm:schema:update --force
 
-mysql -uroot -ppassword olcs_be < schema.sql
+#mysql -uroot -ppassword olcs_be < schema.sql
 
 mysql -uroot -ppassword olcs_be < Rollout.sql
 
