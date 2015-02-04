4831616bdb795ded0f4cd6b8b452e4d8b7fbdefd
Henry Hayes <henryhayesuk@gmail.com>
2015-02-04 12:27:17 +0000

    Bus reg search view

---
+++ app/api/test/module/Olcs/Db/src/Entity/View/BusRegSearchViewTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace OlcsTest\Db\Entity\View;
+
+use Olcs\Db\Entity\View\BusRegSearchView;
+
+/**
+ * BusRegSearchView entity unit tests
+ *
+ * N.B. NOT Auto-Generated!!
+ *
+ * @author Craig Reasbeck <craig.reasbeck@valtech.co.uk>
+ */
+class BusRegSearchViewTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DocumentSearchView
+     */
+    protected $entity;
+
+    /**
+     * @var array
+     */
+    protected $testData;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->testData = [
+            'id' => 15,
+            'serviceNo' => '46474',
+            'regNo' => 'PD2737280/15711',
+            'licId' => '110',
+            'licNo' => 'PD2737280',
+            'licStatus' => 'Not Yet Submitted',
+            'organisationName' => 'Leeds city council',
+            'startPoint' => 'Leeds',
+            'finishPoint' => 'Doncaster',
+            'busRegStatus' => 'Registered',
+            'routeNo' => '15711',
+            'variationNo' => '6'
+        ];
+        $this->entity = new BusRegSearchView();
+
+        // no public methods to set data exist so we must use reflection api
+        // (which, apparently, is what Doctrine does)
+        $ref = new \ReflectionObject($this->entity);
+        foreach (array_keys($this->testData) as $property) {
+            $refProperty = $ref->getProperty($property);
+            $refProperty->setAccessible(true);
+            $refProperty->setValue($this->entity, $this->testData[$property]);
+        }
+    }
+
+    public function testGetters()
+    {
+        // test all teh getters
+        foreach ($this->testData as $property => $value) {
+            $getter = 'get'.ucfirst($property);
+            $this->assertEquals($value, $this->entity->$getter());
+        }
+    }
+}
