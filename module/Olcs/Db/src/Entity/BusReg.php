d31be731b2f2a5123b23414cd73860f74b1ffa1c
Chris Riley <chris.riley@valtech.co.uk>
2015-02-17 17:01:40 +0000

    Adding wip before holiday

--- module/Olcs/Db/src/Entity/BusReg.php
+++ module/Olcs/Db/src/Entity/BusReg.php
@@ -535,6 +535,15 @@ class BusReg implements Interfaces\EntityInterface
     protected $otherServices;
 
     /**
+     * Short notice
+     *
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="Olcs\Db\Entity\BusShortNotice", mappedBy="busReg")
+     */
+    protected $shortNotices;
+
+    /**
      * Document
      *
      * @var \Doctrine\Common\Collections\ArrayCollection
@@ -553,6 +562,7 @@ class BusReg implements Interfaces\EntityInterface
         $this->localAuthoritys = new ArrayCollection();
         $this->busServiceTypes = new ArrayCollection();
         $this->otherServices = new ArrayCollection();
+        $this->shortNotices = new ArrayCollection();
         $this->documents = new ArrayCollection();
     }
 
@@ -1886,6 +1896,66 @@ class BusReg implements Interfaces\EntityInterface
     {
         if ($this->otherServices->contains($otherServices)) {
             $this->otherServices->removeElement($otherServices);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set the short notice
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $shortNotices
+     * @return BusReg
+     */
+    public function setShortNotices($shortNotices)
+    {
+        $this->shortNotices = $shortNotices;
+
+        return $this;
+    }
+
+    /**
+     * Get the short notices
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getShortNotices()
+    {
+        return $this->shortNotices;
+    }
+
+    /**
+     * Add a short notices
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $shortNotices
+     * @return BusReg
+     */
+    public function addShortNotices($shortNotices)
+    {
+        if ($shortNotices instanceof ArrayCollection) {
+            $this->shortNotices = new ArrayCollection(
+                array_merge(
+                    $this->shortNotices->toArray(),
+                    $shortNotices->toArray()
+                )
+            );
+        } elseif (!$this->shortNotices->contains($shortNotices)) {
+            $this->shortNotices->add($shortNotices);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove a short notices
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $shortNotices
+     * @return BusReg
+     */
+    public function removeShortNotices($shortNotices)
+    {
+        if ($this->shortNotices->contains($shortNotices)) {
+            $this->shortNotices->removeElement($shortNotices);
         }
 
         return $this;
