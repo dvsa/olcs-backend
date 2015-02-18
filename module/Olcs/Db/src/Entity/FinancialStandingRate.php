313d4db967b6368df24364b948796fa621d95cff
Dan Eggleston <dan@stolenegg.com>
2015-02-18 14:02:03 +0000

    OLCS-6988 update schema with new financial_standing_rate table

---
+++ module/Olcs/Db/src/Entity/FinancialStandingRate.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Olcs\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Olcs\Db\Entity\Traits;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * FinancialStandingRate Entity
+ *
+ * Auto-Generated
+ *
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @Gedmo\SoftDeleteable(fieldName="deletedDate", timeAware=true)
+ * @ORM\Table(name="financial_standing_rate",
+ *    indexes={
+ *        @ORM\Index(name="fk_financial_standing_rate_ref_data1_idx", columns={"licence_type"}),
+ *        @ORM\Index(name="fk_financial_standing_rate_ref_data2_idx", columns={"goods_or_psv"}),
+ *        @ORM\Index(name="fk_financial_standing_rate_user1_idx", columns={"created_by"}),
+ *        @ORM\Index(name="fk_financial_standing_rate_user2_idx", columns={"last_modified_by"})
+ *    }
+ * )
+ */
+class FinancialStandingRate implements Interfaces\EntityInterface
+{
+    use Traits\CustomBaseEntity,
+        Traits\CreatedByManyToOne,
+        Traits\CustomCreatedOnField,
+        Traits\CustomDeletedDateField,
+        Traits\GoodsOrPsvManyToOneAlt1,
+        Traits\LastModifiedByManyToOne,
+        Traits\CustomLastModifiedOnField,
+        Traits\LicenceTypeManyToOneAlt1,
+        Traits\CustomVersionField;
+
+    /**
+     * Additional vehicle rate
+     *
+     * @var float
+     *
+     * @ORM\Column(type="decimal", name="additional_vehicle_rate", precision=10, scale=2, nullable=true)
+     */
+    protected $additionalVehicleRate;
+
+    /**
+     * Effective from
+     *
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date", name="effective_from", nullable=false)
+     */
+    protected $effectiveFrom;
+
+    /**
+     * First vehicle rate
+     *
+     * @var float
+     *
+     * @ORM\Column(type="decimal", name="first_vehicle_rate", precision=10, scale=2, nullable=true)
+     */
+    protected $firstVehicleRate;
+
+    /**
+     * Identifier - Id
+     *
+     * @var boolean
+     *
+     * @ORM\Id
+     * @ORM\Column(type="boolean", name="id")
+     */
+    protected $id;
+
+    /**
+     * Set the additional vehicle rate
+     *
+     * @param float $additionalVehicleRate
+     * @return FinancialStandingRate
+     */
+    public function setAdditionalVehicleRate($additionalVehicleRate)
+    {
+        $this->additionalVehicleRate = $additionalVehicleRate;
+
+        return $this;
+    }
+
+    /**
+     * Get the additional vehicle rate
+     *
+     * @return float
+     */
+    public function getAdditionalVehicleRate()
+    {
+        return $this->additionalVehicleRate;
+    }
+
+    /**
+     * Set the effective from
+     *
+     * @param \DateTime $effectiveFrom
+     * @return FinancialStandingRate
+     */
+    public function setEffectiveFrom($effectiveFrom)
+    {
+        $this->effectiveFrom = $effectiveFrom;
+
+        return $this;
+    }
+
+    /**
+     * Get the effective from
+     *
+     * @return \DateTime
+     */
+    public function getEffectiveFrom()
+    {
+        return $this->effectiveFrom;
+    }
+
+    /**
+     * Set the first vehicle rate
+     *
+     * @param float $firstVehicleRate
+     * @return FinancialStandingRate
+     */
+    public function setFirstVehicleRate($firstVehicleRate)
+    {
+        $this->firstVehicleRate = $firstVehicleRate;
+
+        return $this;
+    }
+
+    /**
+     * Get the first vehicle rate
+     *
+     * @return float
+     */
+    public function getFirstVehicleRate()
+    {
+        return $this->firstVehicleRate;
+    }
+
+    /**
+     * Set the id
+     *
+     * @param boolean $id
+     * @return FinancialStandingRate
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the id
+     *
+     * @return boolean
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
