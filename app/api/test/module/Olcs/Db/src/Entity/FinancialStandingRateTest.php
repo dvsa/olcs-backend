370c1d70e243b964f5ec9a0975f3811398b8bd6d
Dan Eggleston <dan@stolenegg.com>
2015-02-18 14:02:03 +0000

    OLCS-6988 update schema with new financial_standing_rate table

---
+++ app/api/test/module/Olcs/Db/src/Entity/FinancialStandingRateTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OlcsTest\Db\Entity;
+
+/**
+ * FinancialStandingRate Entity Unit Tests
+ *
+ * Auto-Generated
+ */
+class FinancialStandingRateTest extends Abstracts\EntityTester
+{
+    /**
+     * Define the entity to test
+     *
+     * @var string
+     */
+    protected $entityClass = 'Olcs\Db\Entity\FinancialStandingRate';
+}
