39d7cd5f47190764c315efa699193446eeecb3d7
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-03-04 10:12:37 +0000

    Linked document and doc_template

--- data/db/EntityConfig.php
+++ data/db/EntityConfig.php
@@ -460,7 +460,13 @@ return array(
         ),
         'suppress_from_op' => array(
             'type' => 'yesno'
-        )
+        ),
+        'document_id' => [
+            'inversedBy' => array(
+                'entity' => 'Document',
+                'property' => 'template'
+            )
+        ]
     ),
     'doc_template_bookmark' => array(
         'doc_template_id' => array(
