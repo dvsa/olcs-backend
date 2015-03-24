5c6bd390ddc2cfb19d3b3397c77a63c92df97fd0
Nick Payne <nick@kurai.co.uk>
2015-03-24 14:17:49 +0000

    ensure created_by and last_modified_by are nullable in application_organisation_person

--- app/api/module/Olcs/Db/src/Entity/ApplicationOrganisationPerson.php
+++ app/api/module/Olcs/Db/src/Entity/ApplicationOrganisationPerson.php
@@ -26,8 +26,10 @@ use Olcs\Db\Entity\Traits;
 class ApplicationOrganisationPerson implements Interfaces\EntityInterface
 {
     use Traits\CustomBaseEntity,
+        Traits\CreatedByManyToOne,
         Traits\CustomCreatedOnField,
         Traits\IdIdentity,
+        Traits\LastModifiedByManyToOne,
         Traits\CustomLastModifiedOnField,
         Traits\OrganisationManyToOne,
         Traits\PersonManyToOne,
@@ -52,26 +54,6 @@ class ApplicationOrganisationPerson implements Interfaces\EntityInterface
      * @ORM\JoinColumn(name="application_id", referencedColumnName="id", nullable=false)
      */
     protected $application;
-
-    /**
-     * Created by
-     *
-     * @var \Olcs\Db\Entity\User
-     *
-     * @ORM\ManyToOne(targetEntity="Olcs\Db\Entity\User")
-     * @ORM\JoinColumn(name="created_by", referencedColumnName="id", nullable=false)
-     */
-    protected $createdBy;
-
-    /**
-     * Last modified by
-     *
-     * @var \Olcs\Db\Entity\User
-     *
-     * @ORM\ManyToOne(targetEntity="Olcs\Db\Entity\User")
-     * @ORM\JoinColumn(name="last_modified_by", referencedColumnName="id", nullable=false)
-     */
-    protected $lastModifiedBy;
 
     /**
      * Original person
@@ -127,52 +109,6 @@ class ApplicationOrganisationPerson implements Interfaces\EntityInterface
     public function getApplication()
     {
         return $this->application;
-    }
-
-    /**
-     * Set the created by
-     *
-     * @param \Olcs\Db\Entity\User $createdBy
-     * @return ApplicationOrganisationPerson
-     */
-    public function setCreatedBy($createdBy)
-    {
-        $this->createdBy = $createdBy;
-
-        return $this;
-    }
-
-    /**
-     * Get the created by
-     *
-     * @return \Olcs\Db\Entity\User
-     */
-    public function getCreatedBy()
-    {
-        return $this->createdBy;
-    }
-
-    /**
-     * Set the last modified by
-     *
-     * @param \Olcs\Db\Entity\User $lastModifiedBy
-     * @return ApplicationOrganisationPerson
-     */
-    public function setLastModifiedBy($lastModifiedBy)
-    {
-        $this->lastModifiedBy = $lastModifiedBy;
-
-        return $this;
-    }
-
-    /**
-     * Get the last modified by
-     *
-     * @return \Olcs\Db\Entity\User
-     */
-    public function getLastModifiedBy()
-    {
-        return $this->lastModifiedBy;
     }
 
     /**
