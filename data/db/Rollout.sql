821c2daa10a6543aef71f2cdee1fcad4e1833311
Shaun Lizzio <shaun@lizzio.co.uk>
2015-03-24 16:25:15 +0000

    Modified Roles, added descriptions

--- data/db/Rollout.sql
+++ data/db/Rollout.sql
@@ -6802,19 +6802,19 @@ VALUES
     (701, NULL, NULL, NULL, 'doc_rtf', NULL, NULL, NULL, 31, NULL, NULL, 9, 1, NULL, 1, NULL, '/templates/GB/PUB_APPS_SUPP_DOCS_FINAL.rtf', 0, NULL, NULL, '2012-09-14 00:00:00', NULL, 'GV - New/Var Incomplete - final request for supporting docs', 0, NULL, '2012-09-14 00:00:00', 1);
 
 
-INSERT INTO `role` (`id`, `role`, `code`) VALUES
-    (1, 'internal-limited-read-only', ''), -- internal only
-    (2, 'internal-read-only', ''), -- internal only
-    (3, 'internal-case-worker', ''), -- internal only
-    (4, 'internal-admin', ''), -- internal only
-    (5, 'operator-admin', ''), -- selfserve
-    (6, 'operator-user', ''), -- selfserve
-    (7, 'operator-tm', ''), -- selfserve
-    (8, 'operator-ebsr', ''), -- selfserve
-    (9, 'partner-admin', ''), -- selfserve
-    (10, 'partner-user', ''), -- selfserve
-    (11, 'local-authority-admin', ''), -- selfserve
-    (12, 'local-authority-user', ''); -- selfserve
+INSERT INTO `role` (`id`, `role`, `code`, `description`) VALUES
+    (1, 'internal-limited-read-only', '', 'Internal - Limited read only'), -- internal only
+    (2, 'internal-read-only', '', 'Internal - Read only'), -- internal only
+    (3, 'internal-case-worker', '', 'Internal - Case worker'), -- internal only
+    (4, 'internal-admin', '', 'Internal - Admin'), -- internal only
+    (5, 'operator-admin', '', 'Operator - Admin'), -- selfserve
+    (6, 'operator-user', '', 'Operator - User'), -- selfserve
+    (7, 'operator-tm', '', 'Operator - Transport Manager'), -- selfserve
+    (8, 'operator-ebsr', '', 'Operator - EBSR'), -- selfserve
+    (9, 'partner-admin', '', 'Partner - Admin'), -- selfserve
+    (10, 'partner-user', '', 'Partner - User'), -- selfserve
+    (11, 'local-authority-admin', '', 'Local Authority administrator'), -- selfserve
+    (12, 'local-authority-user', '', 'Local Authority user'); -- selfserve
 
 -- @TODO Added some some code values to temporarily fix strict mode errors
 INSERT INTO `permission` (`id`, `name`, `code`) VALUES
