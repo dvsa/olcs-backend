ffff41ea96a3a14433917f2d3b093d33de451ef5
Dan Eggleston <dan@stolenegg.com>
2015-04-15 11:21:13 +0100

    OLCS-6202 add isMiscellaneous to FeeType entity, make goodsOrPsv nullable

--- module/Olcs/Db/src/Entity/Traits/GoodsOrPsvManyToOneAlt1.php
+++ module/Olcs/Db/src/Entity/Traits/GoodsOrPsvManyToOneAlt1.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Goods or psv many to one alt1 trait
  *
- * Auto-Generated (Shared between 4 entities)
+ * Auto-Generated (Shared between 3 entities)
  */
 trait GoodsOrPsvManyToOneAlt1
 {
