ca8f76beebffbd3ec6a7fe3639406526169d9579
Henry Hayes <henryhayesuk@gmail.com>
2015-05-11 11:46:29 +0100

    Removed unneeded isset

--- module/Olcs/Db/src/Controller/SearchController.php
+++ module/Olcs/Db/src/Controller/SearchController.php
@@ -24,7 +24,7 @@ class SearchController extends AbstractController
             $elastic->setFilters($params['filters']);
         }
 
-        if (isset($params['dateRanges']) && !empty($params['dateRanges']) && is_array($params['dateRanges'])) {
+        if (!empty($params['dateRanges']) && is_array($params['dateRanges'])) {
 
             $elastic->setDateRanges($params['dateRanges']);
         }
