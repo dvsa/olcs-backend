d49097e51f04c923023cc586037ef56352f2a7a5
Dan Eggleston <dan@stolenegg.com>
2015-05-14 16:33:59 +0100

    OLCS-9114 entities for companies house alerts

---
+++ app/api/test/module/Olcs/Db/src/Entity/CompaniesHouseAlertReasonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OlcsTest\Db\Entity;
+
+/**
+ * CompaniesHouseAlertReason Entity Unit Tests
+ *
+ * Auto-Generated
+ */
+class CompaniesHouseAlertReasonTest extends Abstracts\EntityTester
+{
+    /**
+     * Define the entity to test
+     *
+     * @var string
+     */
+    protected $entityClass = 'Olcs\Db\Entity\CompaniesHouseAlertReason';
+}
