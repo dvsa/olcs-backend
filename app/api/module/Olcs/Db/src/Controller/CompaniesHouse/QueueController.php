22c868a229c02c21cc7bef6e6535c26844d4e38f
Dan Eggleston <dan@stolenegg.com>
2015-05-18 17:33:51 +0100

    OLCS-9114 custom backend endpoint to populate queue via INSERT...SELECT query

---
+++ app/api/module/Olcs/Db/src/Controller/CompaniesHouse/QueueController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Companies House Queue Controller
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Olcs\Db\Controller\CompaniesHouse;
+
+use Olcs\Db\Controller\AbstractController;
+use Zend\Http\Response;
+
+/**
+ * Companies House Queue Controller
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class QueueController extends AbstractController
+{
+    public function create($data)
+    {
+        $formattedData = $this->formatDataFromJson($data);
+
+        if ($formattedData instanceof Response) {
+            return $formattedData;
+        }
+
+        $response = $this->getServiceLocator()->get('CompaniesHouse/Queue')
+            ->enqueueActiveOrganisations($formattedData['type']);
+
+        return $this->respond(Response::STATUS_CODE_201, 'Queue Populated', $response);
+
+    }
+}
