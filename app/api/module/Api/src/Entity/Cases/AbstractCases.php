e8dbc6334c3bf196d287f221fcdfb6003f7cb603
Shaun Lizzio <shaun@lizzio.co.uk>
2015-05-29 10:45:11 +0100

    Added QueryHandler object and amended entities to index by id and lazy load

--- app/api/module/Api/src/Entity/Cases/AbstractCases.php
+++ app/api/module/Api/src/Entity/Cases/AbstractCases.php
@@ -374,7 +374,7 @@ abstract class AbstractCases implements \JsonSerializable
      *
      * @var \Doctrine\Common\Collections\ArrayCollection
      *
-     * @ORM\OneToMany(targetEntity="Dvsa\Olcs\Api\Entity\Legacy\LegacyOffence", mappedBy="case")
+     * @ORM\OneToMany(targetEntity="Dvsa\Olcs\Api\Entity\Legacy\LegacyOffence", mappedBy="case", indexBy="id", fetch="LAZY")
      */
     protected $legacyOffences;
 
