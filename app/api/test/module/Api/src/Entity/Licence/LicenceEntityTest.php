e40edaa9e9920babaa88e6538e228b54502ed5c0
Rob Caiger <rob@clocal.co.uk>
2015-05-29 15:08:56 +0100

    Unit tests

--- app/api/test/module/Api/src/Entity/Licence/LicenceEntityTest.php
+++ app/api/test/module/Api/src/Entity/Licence/LicenceEntityTest.php
@@ -2,6 +2,7 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Licence;
 
+use Mockery as m;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Licence\Licence as Entity;
 
@@ -18,4 +19,47 @@ class LicenceEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /**
+     * @dataProvider licenceTypeProvider
+     */
+    public function testCanBecomeSpecialRestricted($goodsOrPsv, $licenceType, $expected)
+    {
+        /** @var Entity $licence */
+        $licence = m::mock(Entity::class)->makePartial();
+
+        $licence->shouldReceive('getGoodsOrPsv->getId')
+            ->andReturn($goodsOrPsv);
+
+        $licence->shouldReceive('getLicenceType->getId')
+            ->andReturn($licenceType);
+
+        $this->assertEquals($expected, $licence->canBecomeSpecialRestricted());
+    }
+
+    public function licenceTypeProvider()
+    {
+        return [
+            [
+                Entity::LICENCE_CATEGORY_GOODS_VEHICLE,
+                Entity::LICENCE_TYPE_STANDARD_NATIONAL,
+                false
+            ],
+            [
+                Entity::LICENCE_CATEGORY_PSV,
+                Entity::LICENCE_TYPE_STANDARD_NATIONAL,
+                false
+            ],
+            [
+                Entity::LICENCE_CATEGORY_PSV,
+                Entity::LICENCE_TYPE_RESTRICTED,
+                false
+            ],
+            [
+                Entity::LICENCE_CATEGORY_PSV,
+                Entity::LICENCE_TYPE_SPECIAL_RESTRICTED,
+                true
+            ]
+        ];
+    }
 }
