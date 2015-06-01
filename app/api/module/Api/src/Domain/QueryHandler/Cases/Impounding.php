d1dafd216c104f10d36a5d2ff8227d6b90edc527
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-01 13:46:33 +0100

    Add QueryHandler object

---
+++ app/api/module/Api/src/Domain/QueryHandler/Cases/Impounding.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Cases;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Impounding
+ */
+final class Impounding extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Impounding';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        $repo = $this->getRepo();
+
+        return [
+            'result' => $repo->fetchList($query),
+            'count' => $repo->fetchCount($query)
+        ];
+    }
+}
