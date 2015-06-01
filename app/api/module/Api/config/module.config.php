0cb7c1de1e5f4be2a4c1fb8c85d3f045ef79d4f7
Rob Caiger <rob@clocal.co.uk>
2015-06-01 13:49:04 +0100

    Started working on Business details

--- app/api/module/Api/config/module.config.php
+++ app/api/module/Api/config/module.config.php
@@ -114,6 +114,7 @@ return [
     ],
     \Dvsa\Olcs\Api\Domain\QueryPartialServiceManagerFactory::CONFIG_KEY => [
         'factories' => [
+            'withContactDetails' => \Dvsa\Olcs\Api\Domain\QueryPartial\WithContactDetailsFactory::class,
             'withRefdata' => \Dvsa\Olcs\Api\Domain\QueryPartial\WithRefdataFactory::class,
             'withUser' => \Dvsa\Olcs\Api\Domain\QueryPartial\WithUserFactory::class,
         ],
