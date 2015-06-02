c51284a3c24bcdfd97f4799a8f71bf36cc4bae19
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-06-02 17:08:44 +0100

    Update Bus Reg Quality Schemes

---
+++ app/api/module/Api/src/Domain/Command/Bus/UpdateQualitySchemes.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Update Quality Schemes
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Bus;
+
+use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
+
+/**
+ * Update Quality Schemes
+ */
+final class UpdateQualitySchemes extends AbstractIdOnlyCommand
+{
+
+}
