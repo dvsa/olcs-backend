1396262239f92aa1d4a838fd044975d00cba9932
Nick Payne <nick@kurai.co.uk>
2015-06-02 13:41:41 +0100

    fix completion status logic

--- module/Api/src/Domain/CommandHandler/PreviousConviction/CreatePreviousConviction.php
+++ module/Api/src/Domain/CommandHandler/PreviousConviction/CreatePreviousConviction.php
@@ -56,7 +56,7 @@ final class CreatePreviousConviction extends AbstractCommandHandler
     private function updateApplicationCompletion($applicationId)
     {
         return $this->getCommandHandler()->handleCommand(
-            UpdateApplicationCompletion::create(['id' => $applicationId, 'section' => 'typeOfLicence'])
+            UpdateApplicationCompletion::create(['id' => $applicationId, 'section' => 'convictionsPenalties'])
         );
     }
 
