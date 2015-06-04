8e6a4b7ffecc99133ddb8766e3b17c421c49ba84
Alex P <alex.peshkov@valtech.co.uk>
2015-06-04 13:48:51 +0100

    OLCS-9572 duplicated method removed

--- app/api/module/Api/src/Controller/GenericController.php
+++ app/api/module/Api/src/Controller/GenericController.php
@@ -119,20 +119,4 @@ class GenericController extends AbstractRestfulController
         $result = $this->getServiceLocator()->get('CommandHandlerManager')->handleCommand($dto);
         return $result;
     }
-
-    public function deleteList()
-    {
-        $dto = $this->params('dto');
-
-        try {
-            $result = $this->handleCommand($dto);
-            return $this->response()->successfulUpdate($result);
-        } catch (Exception\NotFoundException $ex) {
-            return $this->response()->notFound();
-        } catch (Exception\Exception $ex) {
-            return $this->response()->error(400, $ex->getMessages());
-        } catch (\Exception $ex) {
-            return $this->response()->error(500, [$ex->getMessage(), explode('#', $ex->getTraceAsString())]);
-        }
-    }
 }
