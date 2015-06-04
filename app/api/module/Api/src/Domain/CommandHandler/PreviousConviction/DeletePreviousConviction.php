ecc247f5669ff24e4f3b16bd06d7f00a2be3d8ae
Nick Payne <nick@kurai.co.uk>
2015-06-04 08:26:09 +0100

    delete previous conviction tests

--- app/api/module/Api/src/Domain/CommandHandler/PreviousConviction/DeletePreviousConviction.php
+++ app/api/module/Api/src/Domain/CommandHandler/PreviousConviction/DeletePreviousConviction.php
@@ -7,16 +7,12 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\PreviousConviction;
 
-use Dvsa\Olcs\Api\Domain\Command\Application\UpdateApplicationCompletion;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Transfer\Command\PreviousConviction\CreatePreviousConviction as Cmd;
-use Dvsa\Olcs\Api\Entity\Application\PreviousConviction;
-use Dvsa\Olcs\Api\Entity\Application\Application;
 
 /**
- * Create Previous Conviction
+ * Delete Previous Conviction
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
  */
@@ -35,11 +31,12 @@ final class DeletePreviousConviction extends AbstractCommandHandler
                 $this->getRepo()->delete(
                     $this->getRepo()->fetchById($id)
                 );
+
+                $result->addId('previousConviction' . $id, $id);
+                $result->addMessage('Previous conviction removed');
             }
 
             $this->getRepo()->commit();
-
-            $result->addMessage('Previous conviction(s) deleted');
         } catch (\Exception $ex) {
             $this->getRepo()->rollback();
 
