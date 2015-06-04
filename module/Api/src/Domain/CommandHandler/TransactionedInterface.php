a66dd271e7a01bb810e6ec1bc7749dc72a764011
Chris Riley <chris.riley@valtech.co.uk>
2015-06-04 14:17:27 +0100

    cs fixes

--- module/Api/src/Domain/CommandHandler/TransactionedInterface.php
+++ module/Api/src/Domain/CommandHandler/TransactionedInterface.php
@@ -1,8 +1,15 @@
 <?php
 
+/**
+ * Interface TransactionedInterface
+ * @package Dvsa\Olcs\Api\Domain\CommandHandler
+ */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler;
 
-
+/**
+ * Interface TransactionedInterface
+ * @package Dvsa\Olcs\Api\Domain\CommandHandler
+ */
 interface TransactionedInterface
 {
 
