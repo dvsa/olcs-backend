3617c1670808feeb9044715322bba9110df02f25
Rob Caiger <rob@clocal.co.uk>
2015-06-05 16:01:38 +0100

    Fixed mapping

--- app/api/module/Api/config/module.config.php
+++ app/api/module/Api/config/module.config.php
@@ -73,7 +73,7 @@ return [
             TransferQuery\Trailer\Trailers::class => QueryHandler\Trailer\Trailers::class,
             TransferQuery\Irfo\IrfoGvPermit::class => QueryHandler\Irfo\IrfoGvPermit::class,
             TransferQuery\Irfo\IrfoGvPermitList::class => QueryHandler\Irfo\IrfoGvPermitList::class,
-            TransferQuery\Cases\ImpoundingList::class => QueryHandler\Processing\ImpoundingList::class,
+            TransferQuery\Cases\ImpoundingList::class => QueryHandler\Cases\ImpoundingList::class,
             TransferQuery\Cases\Impounding::class => QueryHandler\Cases\Impounding::class,
         ]
     ],
