64af9ec3cd8bf1d6613fb57b4611cfe4cfd242ab
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-05 08:17:32 +0100

    Added code to create complaint command handler, added draft query objects for single/list and added complaint repository

---
+++ app/api/module/Api/src/Domain/QueryHandler/Cases/Complaint/Complaint.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Cases\Complaint;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * PI
+ */
+final class Complaint extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Complaint';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        $complaint = $this->getRepo()->fetchCaseComplaintUsingId($query);
+
+        return $complaint;
+    }
+}
