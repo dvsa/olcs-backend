28e3d7e6fe26afedf995709c5343eb4e9bf3c15d
Dan Eggleston <dan@stolenegg.com>
2015-06-10 16:03:37 +0100

    OLCS-9603 remove getCalculated values nullifying, fix author tags

--- app/api/module/Api/src/Entity/Fee/FeePayment.php
+++ app/api/module/Api/src/Entity/Fee/FeePayment.php
@@ -23,14 +23,4 @@ use Doctrine\ORM\Mapping as ORM;
 class FeePayment extends AbstractFeePayment
 {
 
-    protected function getCalculatedValues()
-    {
-        // Fee payments are usually returned as a child of fee or payment, so
-        // we need to avoid recursion. Query handlers must explicitly include
-        // the required children.
-        return [
-            'fee' => null,
-            'payment' => null,
-        ];
-    }
 }
