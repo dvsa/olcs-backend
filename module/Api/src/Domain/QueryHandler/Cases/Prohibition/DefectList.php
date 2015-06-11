cb8c5a076fb562a8ed990dae210445835a433bb2
Henry Hayes <henryhayesuk@gmail.com>
2015-06-11 14:18:11 +0100

    Prohibition

---
+++ module/Api/src/Domain/QueryHandler/Cases/Prohibition/DefectList.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * DefectList
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Cases\Prohibition;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Dvsa\Olcs\Api\Domain\Repository\Defect as DefectRepository;
+
+use Dvsa\Olcs\Transfer\Query\Cases\Prohibition\DefectList as Query;
+
+/**
+ * DefectList
+ */
+class DefectList extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Defect';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        /* @var Query $query */
+
+        /** @var DefectRepository $repo */
+        $repo = $this->getRepo();
+
+        return [
+            'result' => $this->resultList($repo->fetchList($query)),
+            'count' => $repo->fetchCount($query)
+        ];
+    }
+}
---
+++ module/Api/src/Domain/QueryHandler/Cases/Prohibition/ProhibitionList.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * ProhibitionList
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Cases\Prohibition;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Dvsa\Olcs\Api\Domain\Repository\Prohibition as ProhibitionRepository;
+
+use Dvsa\Olcs\Transfer\Query\Cases\Prohibition\ProhibitionList as Query;
+
+/**
+ * ProhibitionList
+ */
+class ProhibitionList extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Prohibition';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        /* @var Query $query */
+
+        /** @var ProhibitionRepository $repo */
+        $repo = $this->getRepo();
+
+        return [
+            'result' => $this->resultList($repo->fetchList($query)),
+            'count' => $repo->fetchCount($query)
+        ];
+    }
+}
