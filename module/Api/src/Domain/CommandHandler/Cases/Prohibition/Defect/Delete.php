750f664ad87c80c7aeabcd3333db2b94d4b02448
Henry Hayes <henryhayesuk@gmail.com>
2015-06-11 14:57:37 +0100

    Corrected namespacing in Prohibition

--- module/Api/src/Domain/CommandHandler/Cases/Prohibition/Defect/Delete.php
+++ module/Api/src/Domain/CommandHandler/Cases/Prohibition/Defect/Delete.php
@@ -1,22 +1,22 @@
 <?php
 
 /**
- * Delete Conviction
+ * Delete Prohibition
  */
-namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Conviction;
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Prohibition\Defect;
 
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Prohibition\Prohibition as Entity;
-use Dvsa\Olcs\Api\Domain\Repository\Prohibition as Repository;
+use Dvsa\Olcs\Api\Entity\Prohibition\ProhibitionDefect as Entity;
+use Dvsa\Olcs\Api\Domain\Repository\ProhibitionDefect as Repository;
 use Dvsa\Olcs\Api\Entity as Entities;
-use Dvsa\Olcs\Transfer\Command\Cases\Prohibition\Delete as DeleteCommand;
+use Dvsa\Olcs\Transfer\Command\Cases\Prohibition\Defect\Delete as DeleteCommand;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Doctrine\ORM\Query;
 
 /**
- * Delete Conviction
+ * Delete Prohibition
  */
 final class Delete extends AbstractCommandHandler implements TransactionedInterface
 {
