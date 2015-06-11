6507cec25a38b08bede2a17da897e19701c09eb9
Dan Eggleston <dan@stolenegg.com>
2015-06-11 11:19:12 +0100

    OLCS-9603 update unit test for payment entity

--- test/module/Api/src/Entity/Fee/PaymentEntityTest.php
+++ test/module/Api/src/Entity/Fee/PaymentEntityTest.php
@@ -4,6 +4,8 @@ namespace Dvsa\OlcsTest\Api\Entity\Fee;
 
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Fee\Payment as Entity;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Mockery as m;
 
 /**
  * Payment Entity Unit Tests
@@ -26,5 +28,78 @@ class PaymentEntityTest extends EntityTester
         $feePayments = $sut->getFeePayments();
 
         $this->assertInstanceOf('Doctrine\Common\Collections\ArrayCollection', $feePayments);
+    }
+
+    /**
+     * @param string $status
+     * @param boolean $expected
+     *
+     * @dataProvider isOutstandingProvider
+     */
+    public function testIsOutstanding($status, $expected)
+    {
+        $sut = new $this->entityClass;
+
+        $status = m::mock(RefData::class)
+            ->shouldReceive('getId')
+            ->once()
+            ->andReturn($status)
+            ->getMock();
+
+        $sut->setStatus($status);
+
+        $this->assertEquals($expected, $sut->isOutstanding());
+    }
+
+    /**
+     * @return array
+     */
+    public function isOutstandingProvider()
+    {
+        return [
+            [Entity::STATUS_OUTSTANDING, true],
+            [Entity::STATUS_CANCELLED, false],
+            [Entity::STATUS_LEGACY, false],
+            [Entity::STATUS_FAILED, false],
+            [Entity::STATUS_PAID, false],
+            ['invalid', false],
+        ];
+    }
+
+
+    /**
+     * @param string $status
+     * @param boolean $expected
+     *
+     * @dataProvider isPaidProvider
+     */
+    public function testIsPaid($status, $expected)
+    {
+        $sut = new $this->entityClass;
+
+        $status = m::mock(RefData::class)
+            ->shouldReceive('getId')
+            ->once()
+            ->andReturn($status)
+            ->getMock();
+
+        $sut->setStatus($status);
+
+        $this->assertEquals($expected, $sut->isPaid());
+    }
+
+    /**
+     * @return array
+     */
+    public function isPaidProvider()
+    {
+        return [
+            [Entity::STATUS_OUTSTANDING, false],
+            [Entity::STATUS_CANCELLED, false],
+            [Entity::STATUS_LEGACY, false],
+            [Entity::STATUS_FAILED, false],
+            [Entity::STATUS_PAID, true],
+            ['invalid', false],
+        ];
     }
 }
