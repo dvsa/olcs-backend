b665aa2eef2128db2349c5fc15743c1acf7ef650
Henry Hayes <henryhayesuk@gmail.com>
2015-06-12 09:25:05 +0100

    Abstract delete

--- module/Api/src/Domain/CommandHandler/Cases/Conviction/Delete.php
+++ module/Api/src/Domain/CommandHandler/Cases/Conviction/Delete.php
@@ -5,44 +5,14 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Conviction;
 
-use Dvsa\Olcs\Api\Domain\Command\Result;
-use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
-use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Cases\Conviction as Entity;
-use Dvsa\Olcs\Api\Domain\Repository\Conviction as Repository;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
 use Dvsa\Olcs\Api\Entity as Entities;
-use Dvsa\Olcs\Transfer\Command\Cases\Conviction\Delete as DeleteCommand;
-use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Doctrine\ORM\Query;
 
 /**
  * Delete Conviction
  */
-final class Delete extends AbstractCommandHandler implements TransactionedInterface
+final class Delete extends AbstractDeleteCommandHandler
 {
     protected $repoServiceName = 'Conviction';
-
-    /**
-     * Delete
-     *
-     * @param CommandInterface $command
-     * @return Result
-     */
-    public function handleCommand(CommandInterface $command)
-    {
-        /* @var $command DeleteCommand For traceability */
-
-        $result = new Result();
-
-        /** @var Repository $repo */
-        $repo = $this->getRepo();
-
-        /* @var Entity $entity */
-        $entity = $repo->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
-        $repo->delete($entity);
-
-        $result->addMessage('Deleted');
-
-        return $result;
-    }
 }
