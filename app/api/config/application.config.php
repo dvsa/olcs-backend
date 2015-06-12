61f318de72fb834cf263f00e39cb2a83fad6b529
Mat Evans <mat.evans@valtech.co.uk>
2015-06-12 17:46:31 +0100

    OLCS-9684 Added Cli module and licence-rule-status cli command

--- app/api/config/application.config.php
+++ app/api/config/application.config.php
@@ -10,6 +10,7 @@ return array(
         'Olcs\Db',
         'Dvsa\Olcs\Api',
         'CpmsClient',
+        'Dvsa\Olcs\Cli',
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
---
+++ app/api/module/Api/src/Domain/Repository/LicenceStatusRule.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * LicenceStatusRule Repo
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\Repository;
+
+use Dvsa\Olcs\Api\Entity\Licence\LicenceStatusRule as Entity;
+
+/**
+ * LicenceStatusRule Repo
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+class LicenceStatusRule extends AbstractRepository
+{
+    protected $entity = Entity::class;
+
+    protected $alias = 'lsr';
+
+    /**
+     * Fetch rules to set a licence to revoke, curtail or suspend
+     *
+     * @param string $date Date to check for rules, normally this will be now
+     *
+     * @return array
+     */
+    public function fetchRevokeCurtailSuspend($date)
+    {
+        $doctrineQb = $this->createQueryBuilder();
+        $this->getQueryBuilder()->modifyQuery($doctrineQb)->withRefdata()
+            ->with('licenceStatus')->with('licence', 'l')->with('l.status');
+
+        $doctrineQb->andWhere($doctrineQb->expr()->isNull($this->alias .'.startProcessedDate'))
+            ->andWhere($doctrineQb->expr()->isNull($this->alias .'.deletedDate'))
+            ->andWhere($doctrineQb->expr()->lte($this->alias .'.startDate', ':startDate'));
+
+        $doctrineQb->setParameter('startDate', $date);
+
+        return $doctrineQb->getQuery()->getResult();
+    }
+
+    /**
+     * Fetch rules to set a licence to valid
+     *
+     * @param string $date Date to check for rules, normally this will be now
+     *
+     * @return array
+     */
+    public function fetchToValid($date)
+    {
+        $doctrineQb = $this->createQueryBuilder();
+        $this->getQueryBuilder()->modifyQuery($doctrineQb)->withRefdata()
+            ->with('licenceStatus')
+            ->with('licence', 'l')
+            ->with('l.status')
+            ->with('l.licenceVehicles', 'lv')
+            ->with('lv.vehicle');
+
+        $doctrineQb->andWhere($doctrineQb->expr()->isNull($this->alias .'.endProcessedDate'))
+            ->andWhere($doctrineQb->expr()->isNotNull($this->alias .'.endDate'))
+            ->andWhere($doctrineQb->expr()->isNull($this->alias .'.deletedDate'))
+            ->andWhere($doctrineQb->expr()->lte($this->alias .'.endDate', ':endDate'));
+
+        $doctrineQb->setParameter('endDate', $date);
+
+        return $doctrineQb->getQuery()->getResult();
+    }
+}
