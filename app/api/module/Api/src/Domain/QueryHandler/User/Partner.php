d9a1a9bc88aef76f3b9fe9f4fb2ed6598b84b51f
Piotr Filak <piotr.filak@valtech.co.uk>
2015-06-15 16:11:57 +0100

    OLCS-9870 - Internal - Admin - Partner BE
    Conflicts:
    	module/Api/config/module.config.php

---
+++ app/api/module/Api/src/Domain/QueryHandler/User/Partner.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Partner
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\User;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * Partner
+ */
+class Partner extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Partner';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        return $this->getRepo()->fetchUsingId($query);
+    }
+}
