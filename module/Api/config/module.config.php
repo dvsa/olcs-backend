ccdd4126abf67efe57283facdf860eb694f272c4
Rob Caiger <rob@clocal.co.uk>
2015-06-17 09:43:25 +0100

    Migration of Document Generation and Bookmarks

--- module/Api/config/module.config.php
+++ module/Api/config/module.config.php
@@ -3,6 +3,8 @@
 use Dvsa\Olcs\Api\Domain\Repository\RepositoryFactory;
 use Dvsa\Olcs\Api\Domain\QueryPartial;
 use Dvsa\Olcs\Api\Domain\Util;
+use Dvsa\Olcs\Api\Domain\Query\Bookmark as BookmarkQuery;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Bookmark as BookmarkQueryHandler;
 
 return [
     'router' => [
