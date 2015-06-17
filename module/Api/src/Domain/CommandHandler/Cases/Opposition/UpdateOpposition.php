bf6d802520cf7e15cc2efd9326bc9a68cbe89982
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-17 14:08:50 +0100

    fixed create and Update to work with single contact details data structure. Applied business rule to both create and update

--- module/Api/src/Domain/CommandHandler/Cases/Opposition/UpdateOpposition.php
+++ module/Api/src/Domain/CommandHandler/Cases/Opposition/UpdateOpposition.php
@@ -44,57 +44,45 @@ final class UpdateOpposition extends AbstractCommandHandler implements Transacti
     {
         $result = new Result();
 
-        $opposition = $this->getRepo()->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
+        /** @var $opposition Opposition */
+        $opposition = $this->updateOppositionObject($command);
+        $result->addMessage('Opposition updated');
 
-        $contactDetails = $this->updateContactDetailsObject($command, $opposition->getOpposer()->getContactDetails());
-        $result->addMessage('Contact details updated');
-
-        $opposition->getOpposer()->setContactDetails($contactDetails);
+        $opposition->getOpposer()->update(
+            [
+                'opposerType' => $this->getRepo()->getRefdataReference($command->getOpposerType()),
+                'oppositionType' => $this->getRepo()->getRefdataReference($command->getOppositionType()),
+            ]
+        );
         $result->addMessage('Opposer updated');
 
-        $opposition = $this->updateOppositionObject($command, $opposition);
-        $result->addMessage('Opposition updated');
+        $opposition->getOpposer()->getContactDetails()->update(
+            $this->getRepo('ContactDetails')->populateRefDataReference(
+                $command->getOpposerContactDetails()
+            )
+        );
+
+        $result->addMessage('Contact details updated');
 
         $this->getRepo()->save($opposition);
 
         $result->addId('opposition ', $opposition->getId());
-        $result->addId('opposer ', $opposer->getId());
-        $result->addId('contactDetails', $contactDetails->getId());
+        $result->addId('opposer', $opposition->getOpposer()->getId());
+        $result->addId('contactDetails', $opposition->getOpposer()->getContactDetails()->getId());
 
         return $result;
-    }
-
-    private function updateContactDetailsObject($command, ContactDetails $contactDetails)
-    {
-        return $contactDetails->update(
-            $this->getRepo('ContactDetails')->populateRefDataReference(
-                $this->getObjectorContactDetails($command)
-            )
-        );
-    }
-
-    private function getObjectorContactDetails(Cmd $command)
-    {
-        $objectorContactDetails['emailAddress'] = $command->getEmailAddress();
-
-        $objectorContactDetails['address'] = $this->updateAddressArray($command);
-
-        $objectorContactDetails['person'] = $this->updatePersonArray($command);
-
-        $objectorContactDetails['phoneContacts'] = $this->updatePhoneContactsArray($command);
-
-        return $objectorContactDetails;
     }
 
     /**
      * Update the opposition  object
      *
      * @param Cmd $command
-     * @param Opposer $opposer
      * @return Opposition
      */
-    private function updateOppositionObject(Cmd $command, Opposition $opposition)
+    private function updateOppositionObject(Cmd $command)
     {
+        $opposition = $this->getRepo()->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
+
         if ($command->getRaisedDate() !== null) {
             $opposition->setRaisedDate(new \DateTime($command->getRaisedDate()));
         }
@@ -126,24 +114,6 @@ final class UpdateOpposition extends AbstractCommandHandler implements Transacti
     }
 
     /**
-     * Update the opposer  object
-     *
-     * @param Cmd $command
-     * @param ContactDetails $contactDetails
-     * @return Opposer
-     */
-    private function updateOpposerObject(Cmd $command, ContactDetails $contactDetails)
-    {
-        $opposer = new Opposer(
-            $contactDetails,
-            $this->getRepo()->getRefdataReference($command->getOpposerType()),
-            $this->getRepo()->getRefdataReference($command->getOppositionType())
-        );
-
-        return $opposer;
-    }
-
-    /**
      * Generate list of operatingCentres based on type of opposition. At present it allows both types to specify OCs
      * This may need to be either one or the other.
      *
@@ -171,63 +141,5 @@ final class UpdateOpposition extends AbstractCommandHandler implements Transacti
         }
 
         return $collection;
-    }
-
-    /**
-     * Update person array
-     * @param Cmd $command
-     * @return Person|null
-     */
-    private function updatePersonArray(Cmd $command)
-    {
-        if (!empty($command->getForename()) || !empty($command->getForename())) {
-            $person = [];
-            $person['forename'] = $command->getForename();
-            $person['familyName'] = $command->getFamilyName();
-            return $person;
-        }
-        return null;
-    }
-
-    /**
-     * Update address array
-     * @param Cmd $command
-     * @return Address|null
-     */
-    private function updateAddressArray(Cmd $command)
-    {
-        if (!empty($command->getOpposerAddress()['addressLine1']) ||
-            !empty($command->getOpposerAddress()['addressLine2']) ||
-            !empty($command->getOpposerAddress()['addressLine3']) ||
-            !empty($command->getOpposerAddress()['addressLine4']) ||
-            !empty($command->getOpposerAddress()['town']) ||
-            !empty($command->getOpposerAddress()['postcode']) ||
-            !empty($command->getOpposerAddress()['countryCode'])
-        ) {
-            return $command->getOpposerAddress();
-        }
-        return null;
-    }
-
-    /**
-     * Generates an array of phone contact details
-     *
-     * @param Cmd $command
-     * @return ArrayCollection|null
-     */
-    private function updatePhoneContactsArray(Cmd $command)
-    {
-        $phoneContacts = [];
-        if (!is_null($command->getPhone()))
-        {
-            $phoneContact = [];
-            $phoneContact['phoneContactType'] = PhoneContact::PHONE_CONTACT_TYPE_TEL;
-            $phoneContact['phoneNumber'] = $command->getPhone();
-
-            $phoneContacts[] = $phoneContact;
-            return $phoneContacts;
-        }
-
-        return null;
     }
 }
