d002c104cd8c702c727c5d0aafba46e22607ebeb
Chris Riley <chris.riley@valtech.co.uk>
2015-06-22 14:45:37 +0100

    Fixed issues with updated

--- app/api/module/Api/src/Domain/CommandHandler/Cases/Conviction/Update.php
+++ app/api/module/Api/src/Domain/CommandHandler/Cases/Conviction/Update.php
@@ -44,4 +44,68 @@ final class Update extends CreateUpdateAbstract implements TransactionedInterfac
 
         return $result;
     }
+
+    protected function setData($entity, CommandInterface $command)
+    {
+        /* @var $entity Entity For traceability */
+        /* @var $command CreateCommand For traceability */
+
+        /** @var Repository $repo For traceability */
+        $repo = $this->getRepo();
+
+        // Let's do the foreign keys first
+
+        // Defendant Type - required - no if needed.
+
+        $entity->setDefendantType($repo->getRefdataReference($command->getDefendantType()));
+
+        // Conviction Category - required - no if needed.
+        if ($command->getConvictionCategory() !== '') {
+            $entity->setConvictionCategory($repo->getRefdataReference($command->getConvictionCategory()));
+        }
+
+        // Transport Manager is optional if Case is present.
+        if ($command->getTransportManager() !== null) {
+            $tm = $this->getRepo()->getReference(Entities\Tm\TransportManager::class, $command->getTransportManager());
+            $entity->setTransportManager($tm);
+        }
+
+        // Person...
+        if ($command->getPersonFirstName() !== null) {
+            $entity->setPersonFirstname($command->getPersonFirstName());
+        }
+        if ($command->getPersonLastName() !== null) {
+            $entity->setPersonLastname($command->getPersonLastName());
+        }
+        if ($command->getBirthDate() !== null) {
+            $entity->setBirthDate(new \DateTime($command->getBirthDate()));
+        }
+
+        // Dates - Required
+        $entity->setOffenceDate(new \DateTime($command->getOffenceDate()));
+        $entity->setConvictionDate(new \DateTime($command->getConvictionDate()));
+
+        // MSI - Required
+        $entity->setMsi($command->getMsi());
+
+        if ($command->getCourt() !== null) {
+            $entity->setCourt($command->getCourt());
+        }
+        if ($command->getPenalty() !== null) {
+            $entity->setPenalty($command->getPenalty());
+        }
+        if ($command->getCosts() !== null) {
+            $entity->setCosts($command->getCosts());
+        }
+        if ($command->getNotes() !== null) {
+            $entity->setNotes($command->getNotes());
+        }
+        if ($command->getTakenIntoConsideration() !== null) {
+            $entity->setTakenIntoConsideration($command->getTakenIntoConsideration());
+        }
+
+        // Is Declared & Is Dealt With - Required
+        $entity->setIsDeclared($command->getIsDeclared());
+        $entity->setIsDealtWith($command->getIsDealtWith());
+    }
 }
