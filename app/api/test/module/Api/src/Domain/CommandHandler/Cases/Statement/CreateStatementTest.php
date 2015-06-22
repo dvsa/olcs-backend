7e813b36ac84a252a490e4753dc390cd78549ef0
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-22 09:02:14 +0100

    Added Licence type to improve line coverage

--- app/api/test/module/Api/src/Domain/CommandHandler/Cases/Statement/CreateStatementTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Cases/Statement/CreateStatementTest.php
@@ -134,6 +134,7 @@ class CreateStatementTest extends CommandHandlerTestCase
                 $referencedPayload['requestorsContactDetails']
             );
 
+        $this->references[LicenceEntity::class][7]->setLicenceType(LicenceEntity::LICENCE_STATUS_VALID);
         $this->references[LicenceEntity::class][7]->shouldReceive('getLicNo')->andReturn('12345');
         $this->references[CasesEntity::class][24]->setLicence($this->references[LicenceEntity::class][7]);
 
