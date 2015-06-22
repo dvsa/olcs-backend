53d4d153cca41d5ab6ace4f0967a2d3c152b495b
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-06-22 15:24:39 +0100

    Added updateCase and updateAnnualTestHistory

--- app/api/test/module/Api/src/Entity/Cases/CasesEntityTest.php
+++ app/api/test/module/Api/src/Entity/Cases/CasesEntityTest.php
@@ -2,6 +2,7 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Cases;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Cases\Cases as Entity;
 
@@ -12,10 +13,69 @@ use Dvsa\Olcs\Api\Entity\Cases\Cases as Entity;
  */
 class CasesEntityTest extends EntityTester
 {
+    public function setUp()
+    {
+        /** @var \Dvsa\Olcs\Api\Entity\Cases\Cases entity */
+        $this->entity = $this->instantiate($this->entityClass);
+    }
+
     /**
      * Define the entity to test
      *
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /**
+     * Tests update
+     */
+    public function testUpdate()
+    {
+        $caseType = '';
+        $categorys = new ArrayCollection();
+        $outcomes = new ArrayCollection();
+        $application = null;
+        $licence = null;
+        $transportManager = null;
+        $ecmsNo = 'abcd123456';
+        $description = 'description';
+
+        $this->entity->update(
+            $caseType,
+            $categorys,
+            $outcomes,
+            $application,
+            $licence,
+            $transportManager,
+            $ecmsNo,
+            $description
+        );
+
+        $this->assertEquals($caseType, $this->entity->getCaseType());
+        $this->assertEquals($categorys, $this->entity->getCategorys());
+        $this->assertEquals($outcomes, $this->entity->getOutcomes());
+        $this->assertEquals($application, $this->entity->getApplication());
+        $this->assertEquals($licence, $this->entity->getLicence());
+        $this->assertEquals($transportManager, $this->entity->getTransportManager());
+        $this->assertEquals($ecmsNo, $this->entity->getEcmsNo());
+        $this->assertEquals($description, $this->entity->getDescription());
+
+        return true;
+    }
+
+    /**
+     * Tests updateAnnualTestHistory
+     */
+    public function testUpdateAnnualTestHistory()
+    {
+        $annualTestHistory = 'annual test history';
+
+        $this->entity->updateAnnualTestHistory(
+            $annualTestHistory
+        );
+
+        $this->assertEquals($annualTestHistory, $this->entity->getAnnualTestHistory());
+
+        return true;
+    }
 }
