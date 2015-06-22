9dc894208a51f118abff616616894a38fc2e6782
Rob Caiger <rob@clocal.co.uk>
2015-06-22 10:04:15 +0100

    Fixed psv discs

--- config/application.config.php
+++ config/application.config.php
@@ -8,9 +8,9 @@ return array(
         'Olcs\Logging',
         'DoctrineModule',
         'DoctrineORMModule',
+        'CpmsClient',
         'Olcs\Db',
         'Dvsa\Olcs\Api',
-        'CpmsClient',
         'Dvsa\Olcs\Cli',
     ],
     // These are various options for the listeners attached to the ModuleManager
