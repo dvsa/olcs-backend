7bd5fb6559a7664681c5e088965972879a3d604c
Mat Evans <mat.evans@valtech.co.uk>
2015-06-24 09:36:57 +0100

    OLCS-9684 Fix issue with cli not having permission to proxy directoy

--- app/api/config/autoload/global.php
+++ app/api/config/autoload/global.php
@@ -15,8 +15,11 @@ return array(
     'doctrine' => array(
         'configuration' => array(
             'orm_default' => array(
-                'proxy_dir'         => sys_get_temp_dir() . '/OlcsBe/Proxy',
-                'proxy_namespace'   => 'OlcsBe\Proxy',
+                // If running as CLI then use different directory to avoid permissions problems
+                'proxy_dir'         => (PHP_SAPI === 'cli') ?
+                    sys_get_temp_dir() .'/EntityCli/Proxy' :
+                    sys_get_temp_dir() .'/Entity/Proxy',
+                'proxy_namespace'   => 'Dvsa\Olcs\Api\Entity\Proxy',
             ),
         )
     ),
