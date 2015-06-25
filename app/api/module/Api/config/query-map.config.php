c13f5571531c15033ed7223af00fe214e57cb32d
Rob Caiger <rob@clocal.co.uk>
2015-06-25 15:51:52 +0100

    Re-added case mapping

--- app/api/module/Api/config/query-map.config.php
+++ app/api/module/Api/config/query-map.config.php
@@ -63,6 +63,7 @@ return [
     TransferQuery\Variation\TypeOfLicence::class => QueryHandler\Variation\TypeOfLicence::class,
 
     // Cases
+    TransferQuery\Cases\Cases::class => QueryHandler\Cases\Cases::class,
     TransferQuery\Cases\Pi::class => QueryHandler\Cases\Pi::class,
     TransferQuery\Cases\LegacyOffence::class => QueryHandler\Cases\LegacyOffence::class,
     TransferQuery\Cases\LegacyOffenceList::class => QueryHandler\Cases\LegacyOffenceList::class,
