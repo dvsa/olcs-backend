d9bf540b28e1d61abd72380355722cef2d2ac46b
Alex P <alex.peshkov@valtech.co.uk>
2015-06-25 10:56:38 +0100

    OLCS-9691 query handler for operator business details added

---
+++ app/api/module/Api/src/Domain/QueryHandler/Operator/BusinessDetails.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Business Details
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Operator;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * Business Details
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class BusinessDetails extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Organisation';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        return $this->getRepo()->fetchBusinessDetailsUsingId($query);
+    }
+}
