0a57cae53ebc5eb9018e3bb84c7a6bb7d401d705
Shaun Lizzio <shaun@lizzio.co.uk>
2015-06-26 09:23:13 +0100

    Changed env complaints to return additional info

--- app/api/module/Api/src/Domain/QueryHandler/Cases/EnvironmentalComplaint/EnvironmentalComplaintList.php
+++ app/api/module/Api/src/Domain/QueryHandler/Cases/EnvironmentalComplaint/EnvironmentalComplaintList.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Query;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Doctrine\ORM\QueryBuilder;
-use Dvsa\Olcs\Api\Domain\Repository\Complaint as Repo;
+use Dvsa\Olcs\Api\Domain\Repository\Complaint as ComplaintRepo;
 
 /**
  * EnvironmentalComplaint QueryHandler
@@ -17,11 +17,17 @@ final class EnvironmentalComplaintList extends AbstractQueryHandler
 
     public function handleQuery(QueryInterface $query)
     {
-        /** @var Repo $repo */
+        /** @var ComplaintRepo $repo */
         $repo = $this->getRepo();
-
         return [
-            'result' => $repo->fetchList($query),
+            'result' => $this->resultList(
+                $repo->fetchList($query, Query::HYDRATE_OBJECT),
+                [
+                    'complainantContactDetails' => [
+                        'person'
+                    ]
+                ]
+            ),
             'count' => $repo->fetchCount($query)
         ];
     }
