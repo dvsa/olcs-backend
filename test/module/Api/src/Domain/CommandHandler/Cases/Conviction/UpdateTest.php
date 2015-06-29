9306fd6536fc75cba13eede82eb791abbe9126ee
Henry Hayes <henryhayesuk@gmail.com>
2015-06-29 10:47:26 +0100

    Unit test updates and conviction update

--- test/module/Api/src/Domain/CommandHandler/Cases/Conviction/UpdateTest.php
+++ test/module/Api/src/Domain/CommandHandler/Cases/Conviction/UpdateTest.php
@@ -122,9 +122,6 @@ class UpdateTest extends CommandHandlerTestCase
                     ->shouldreceive('setBirthDate')
                     ->with(m::type(\DateTime::class))
 
-                    ->shouldReceive('setCase')
-                    ->with(m::type(Entity\Cases\Cases::class))
-
                     ->shouldreceive('setOffenceDate')
                     ->with(m::type(\DateTime::class))
 
