7a09aee9f6bd9205df935e430ede63f0ba5d3196
Dan Eggleston <dan@stolenegg.com>
2015-06-29 11:54:16 +0100

    OLCS-9596 implement application overview query handler

---
+++ app/api/module/Api/src/Domain/QueryHandler/Application/Overview.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Application Overview
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Application;
+
+use Doctrine\Common\Collections\Criteria;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Dvsa\Olcs\Api\Entity\Application\ApplicationTracking as ApplicationTrackingEntity;
+use Dvsa\Olcs\Transfer\Query\Licence\Overview as LicenceOverviewQry;
+/**
+ * Application Overview
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class Overview extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Application';
+
+    protected $extraRepos = ['Fee'];
+
+    public function handleQuery(QueryInterface $query)
+    {
+        $application = $this->getRepo()->fetchUsingId($query);
+
+        $licenceQuery = LicenceOverviewQry::create(['id' => $application->getLicence()->getId()]);
+        $licence = $this->getQueryHandler()->handleQuery($licenceQuery);
+
+        return $this->result(
+            $application,
+            [
+                'applicationCompletion',
+                'applicationTracking',
+                'status',
+                'interimStatus',
+                'licenceType',
+                'goodsOrPsv',
+            ],
+            [
+                'licence' => $licence->serialize(),
+                'feeCount' => $this->getFeeCount($application),
+                'oppositionCount' => $this->getOppositionCount($application),
+                'valueOptions' => [
+                    'tracking' => ApplicationTrackingEntity::getValueOptions(),
+                ],
+            ]
+        );
+    }
+
+    protected function getFeeCount($application)
+    {
+        $fees = $this->getRepo('Fee')->fetchOutstandingFeesByApplicationId($application->getId());
+        return count($fees);
+    }
+
+    /**
+     * @todo
+     */
+    protected function getOppositionCount($application)
+    {
+        // $query = [
+        //     'sort' => 'createdOn',
+        //     'order' => 'DESC',
+        // ];
+
+        // $bundle = $this->tableBundle;
+        // $bundle['children']['case'] = [
+        //     'criteria' => [
+        //         'application' => $applicationId,
+        //     ],
+        //     'required' => true
+        // ];
+        return 0;
+    }
+}
