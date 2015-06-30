a2d7a7f07f9ba6d1b7af5eec830deb11c057e213
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-06-30 18:04:54 +0100

    Annual Test History query handler

--- test/module/Api/src/Domain/QueryHandler/Cases/AnnualTestHistoryTest.php
+++ test/module/Api/src/Domain/QueryHandler/Cases/AnnualTestHistoryTest.php
@@ -1,7 +1,36 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: ian
- * Date: 30/06/15
- * Time: 17:03
+ * Annual Test History test
  */
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Cases;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\Cases\AnnualTestHistory;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\Repository\Cases as CasesRepo;
+use Dvsa\Olcs\Transfer\Query\Bus\BusReg as Qry;
+
+/**
+ * Annual Test History Test
+ */
+class AnnualTestHistoryTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new AnnualTestHistory();
+        $this->mockRepo('Cases', CasesRepo::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = Qry::create(['id' => 111]);
+
+        $this->repoMap['Cases']->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn(['foo']);
+
+        $this->assertEquals(['foo'], $this->sut->handleQuery($query));
+    }
+}
