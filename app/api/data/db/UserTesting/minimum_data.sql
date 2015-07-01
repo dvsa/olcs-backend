474fb71982f830c9d3398cf499fcb0fe65ec1f59
Dan Eggleston <dan@stolenegg.com>
2015-07-01 11:10:50 +0100

    update minimal testdata file

--- app/api/data/db/UserTesting/minimum_data.sql
+++ app/api/data/db/UserTesting/minimum_data.sql
@@ -4,7 +4,25 @@ TRUNCATE TABLE `organisation`;
 TRUNCATE TABLE `user`;
 TRUNCATE TABLE `organisation_user`;
 
-INSERT INTO `organisation` (`id`) VALUES (1);
+INSERT INTO `organisation`
+(
+    `id`,
+    `lead_tc_area_id`,
+    `company_or_llp_no`,
+    `name`,
+    `type`,
+    `allow_email`
+)
+VALUES
+(
+    1,
+    'B',
+    '12345678',
+    'Operator Licensing Ltd.',
+    'org_t_rc',
+    1
+);
+
 INSERT INTO `user` (`id`) VALUES (1);
 INSERT INTO `organisation_user` (`organisation_id`, `user_id`) VALUES (1, 1);
 
