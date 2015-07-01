f5bd1d0eb73834717c7d892b80de12298e763385
Alex P <alex.peshkov@valtech.co.uk>
2015-07-01 10:57:52 +0100

    OLCS-9628 grant application process - inspection request

---
+++ app/api/module/Api/src/Domain/Command/InspectionRequest/CreateFromGrant.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Inspection Request / Create From Grant
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\InspectionRequest;
+
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * Inspection Request / Create From Grant
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+final class CreateFromGrant extends AbstractCommand
+{
+    public $application;
+
+    public $duePeriod;
+
+    protected $caseworkerNotes;
+
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    public function getDuePeriod()
+    {
+        return $this->duePeriod;
+    }
+
+    public function getCaseworkerNotes()
+    {
+        return $this->caseworkerNotes;
+    }
+}
--- app/api/module/Api/src/Entity/Inspection/InspectionRequest.php
+++ app/api/module/Api/src/Entity/Inspection/InspectionRequest.php
@@ -3,6 +3,8 @@
 namespace Dvsa\Olcs\Api\Entity\Inspection;
 
 use Doctrine\ORM\Mapping as ORM;
+use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 
 /**
  * InspectionRequest Entity
@@ -29,5 +31,108 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class InspectionRequest extends AbstractInspectionRequest
 {
+    const ERROR_FIELD_IS_REQUIRED = 'IR-FR-1';
+    const ERROR_DUE_DATE = 'IR-DD-2';
+    const ERROR_DUE_DATE_NOT_IN_RANGE = 'IR-DD-3';
 
+    const REPORT_TYPE_MAINTENANCE_REQUEST = 'insp_rep_t_maint';
+    const RESULT_TYPE_NEW = 'insp_res_t_new';
+    const RESULT_TYPE_SATISFACTORY = 'insp_res_t_new_sat';
+    const RESULT_TYPE_UNSATISFACTORY = 'insp_res_t_new_unsat';
+    const REQUEST_TYPE_NEW_OP = 'insp_req_t_new_op';
+
+    private $duePeriods = [3, 6, 9, 12];
+
+    public function updateInspectionRequest(
+        $requestType,
+        $requestDate,
+        $dueDate,
+        $duePeriod,
+        $resultType,
+        $requestorNotes,
+        $reportType,
+        $application,
+        $licence,
+        $requestorUser,
+        $operatingCentre = null,
+        $inspectorName = null,
+        $returnDate = null,
+        $fromDate = null,
+        $toDate = null,
+        $vehiclesExaminedNo = null,
+        $trailersExaminedNo = null,
+        $inspectorNotes = null
+    ) {
+        $this->validateInspectionRequest($reportType, $requestDate, $dueDate, $duePeriod, $resultType, $requestorUser);
+
+        $this->setRequestType($requestType);
+
+        $today = new DateTime('now');
+        if (!$requestDate) {
+            $this->setRequestDate($today);
+        }
+        if (!$dueDate) {
+            $this->setDueDate(
+                $today->add(new \DateInterval('P' . $duePeriod . 'M'))
+            );
+        }
+        $this->setResultType($resultType);
+        $this->setRequestorNotes($requestorNotes);
+        $this->setReportType($reportType);
+        $this->setApplication($application);
+        $this->setLicence($licence);
+        $this->setRequestorUser($requestorUser);
+        $this->setOperatingCentre($operatingCentre);
+        $this->setInspectorName($inspectorName);
+        $this->setReturnDate($returnDate);
+        $this->setFromDate($fromDate);
+        $this->setToDate($toDate);
+        $this->setVehiclesExaminedNo($vehiclesExaminedNo);
+        $this->setTrailersExaminedNo($trailersExaminedNo);
+        $this->setInspectorNotes($inspectorNotes);
+    }
+
+    protected function validateInspectionRequest(
+        $reportType,
+        $requestDate,
+        $dueDate,
+        $duePeriod,
+        $resultType,
+        $requestorUser
+    ) {
+        $errors = [];
+        if (!$reportType) {
+            $errors[] = [
+                'reportType' => [
+                    self::ERROR_FIELD_IS_REQUIRED => 'Field is required'
+                ]
+            ];
+        }
+        if (!$resultType) {
+            $errors[] = [
+                'resultType' => [
+                    self::ERROR_FIELD_IS_REQUIRED => 'Field is required'
+                ]
+            ];
+        }
+        if (!$requestorUser) {
+            $errors[] = [
+                'requestorUser' => [
+                    self::ERROR_FIELD_IS_REQUIRED => 'Field is required'
+                ]
+            ];
+        }
+        if (!$dueDate && !$duePeriod) {
+            $errors[]['dueDate'][self::ERROR_FIELD_IS_REQUIRED] = 'Field is required';
+        }
+        if (!$duePeriod && !($dueDate > $requestDate)) {
+            $errors[]['dueDate'][self::ERROR_DUE_DATE] = 'Due date should be the same or after date requested';
+        }
+        if ($duePeriod && !in_array($duePeriod, $this->duePeriods)) {
+            $errors[]['dueDate'][self::ERROR_DUE_DATE_NOT_IN_RANGE] = 'Due date not in range';
+        }
+        if (count($errors)) {
+            throw new ValidationException($errors);
+        }
+    }
 }
