cb28815de296a05cc10edc6606169a07452bfe54
Dan Eggleston <dan@stolenegg.com>
2015-07-03 11:37:56 +0100

    sync schemas - mainly index reordering

--- module/Api/src/Entity/Licence/AbstractContinuation.php
+++ module/Api/src/Entity/Licence/AbstractContinuation.php
@@ -19,7 +19,8 @@ use Doctrine\ORM\Mapping as ORM;
  *        @ORM\Index(name="ix_continuation_month", columns={"month"}),
  *        @ORM\Index(name="ix_continuation_year", columns={"year"}),
  *        @ORM\Index(name="fk_continuation_traffic_area1_idx", columns={"traffic_area_id"}),
- *        @ORM\Index(name="fk_continuation_user1_idx", columns={"created_by"})
+ *        @ORM\Index(name="fk_continuation_user1_idx", columns={"created_by"}),
+ *        @ORM\Index(name="ix_continuation_ta_year_month", columns={"traffic_area_id","year","month"})
  *    }
  * )
  */
