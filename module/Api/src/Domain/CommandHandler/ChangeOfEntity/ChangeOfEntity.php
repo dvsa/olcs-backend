a0c1ff6300ebb675bf37057f142b3b4d775ebf66
Dan Eggleston <dan@stolenegg.com>
2015-07-06 10:31:48 +0100

    OLCS-10109 new command for create/update change of entity

---
+++ module/Api/src/Domain/CommandHandler/ChangeOfEntity/ChangeOfEntity.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Change Of Entity
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\ChangeOfEntity;
+
+use DateTime;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Entity\Organisation\ChangeOfEntity as ChangeOfEntityEntity;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+
+/**
+ * Change Of Entity
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+final class ChangeOfEntity extends AbstractCommandHandler
+{
+    protected $repoServiceName = 'ChangeOfEntity';
+
+    protected $extraRepos = ['Application'];
+
+    public function handleCommand(CommandInterface $command)
+    {
+        if ($command->getId()) {
+            /* @var $changeOfEntity ChangeOfEntityEntity */
+            $changeOfEntity = $this->getRepo()->fetchUsingId($command);
+            $messageAction = 'Updated';
+        } else {
+            $changeOfEntity = new ChangeOfEntityEntity();
+            $application = $this->getRepo('Application')->fetchById($command->getApplicationId());
+            $changeOfEntity->setLicence($application->getLicence());
+            $messageAction = 'Created';
+        }
+
+        if ($command->getApplicationId()) {
+            $changeOfEntity
+                ->setOldLicenceNo($command->getOldLicenceNo())
+                ->setOldOrganisationName($command->getOldOrganisationName());
+        }
+
+        $this->getRepo()->save($changeOfEntity);
+
+        $result = new Result();
+        $result->addId("changeOfEntity", $changeOfEntity->getId());
+        $result->addMessage("ChangeOfEntity ID {$changeOfEntity->getId()} " . $messageAction);
+
+        return $result;
+    }
+}
