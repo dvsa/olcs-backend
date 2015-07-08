41d7aeed0bf648c85dede92de0dfee048bc4f195
Dan Eggleston <dan@stolenegg.com>
2015-07-08 10:19:16 +0100

    OLCS-9873 add cli action to enqueue companies house lookups for organisations

---
+++ app/api/module/Api/src/Domain/CommandHandler/CompaniesHouse/EnqueueOrganisations.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Companies House Enqueue Organisations
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\CompaniesHouse;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Entity\Queue\Queue;
+
+/**
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+final class EnqueueOrganisations extends AbstractCommandHandler implements TransactionedInterface
+{
+    protected $repoServiceName = 'Queue';
+
+    public function handleCommand(CommandInterface $command)
+    {
+        $rows = $this->getRepo()->enqueueAllOrganisations(Queue::TYPE_COMPANIES_HOUSE_COMPARE);
+
+        $result = new Result();
+        $result->addMessage('Enqueued ' . $rows . ' messages');
+
+        return $result;
+    }
+}
