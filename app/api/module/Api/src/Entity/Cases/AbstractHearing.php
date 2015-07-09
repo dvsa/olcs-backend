bd666239bc1e29b16ba806159357eec8964b7687
Shaun Lizzio <shaun@lizzio.co.uk>
2015-07-09 16:29:33 +0100

    Added presidingStaffName and outcome to hearing table

--- app/api/module/Api/src/Entity/Cases/AbstractHearing.php
+++ app/api/module/Api/src/Entity/Cases/AbstractHearing.php
@@ -23,7 +23,8 @@ use Gedmo\Mapping\Annotation as Gedmo;
  *        @ORM\Index(name="ix_hearing_created_by", columns={"created_by"}),
  *        @ORM\Index(name="ix_hearing_last_modified_by", columns={"last_modified_by"}),
  *        @ORM\Index(name="ix_hearing_presiding_tc_id", columns={"presiding_tc_id"}),
- *        @ORM\Index(name="ix_hearing_hearing_type", columns={"hearing_type"})
+ *        @ORM\Index(name="ix_hearing_hearing_type", columns={"hearing_type"}),
+ *        @ORM\Index(name="ix_hearing_outcome", columns={"outcome"})
  *    },
  *    uniqueConstraints={
  *        @ORM\UniqueConstraint(name="uk_hearing_olbs_key_olbs_type", columns={"olbs_key","olbs_type"})
@@ -147,6 +148,25 @@ abstract class AbstractHearing implements BundleSerializableInterface, JsonSeria
      * @ORM\Column(type="string", name="olbs_type", length=32, nullable=true)
      */
     protected $olbsType;
+
+    /**
+     * Outcome
+     *
+     * @var \Dvsa\Olcs\Api\Entity\System\RefData
+     *
+     * @ORM\ManyToOne(targetEntity="Dvsa\Olcs\Api\Entity\System\RefData", fetch="LAZY")
+     * @ORM\JoinColumn(name="outcome", referencedColumnName="id", nullable=true)
+     */
+    protected $outcome;
+
+    /**
+     * Presiding staff name
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string", name="presiding_staff_name", length=255, nullable=true)
+     */
+    protected $presidingStaffName;
 
     /**
      * Presiding tc
@@ -470,6 +490,52 @@ abstract class AbstractHearing implements BundleSerializableInterface, JsonSeria
     public function getOlbsType()
     {
         return $this->olbsType;
+    }
+
+    /**
+     * Set the outcome
+     *
+     * @param \Dvsa\Olcs\Api\Entity\System\RefData $outcome
+     * @return Hearing
+     */
+    public function setOutcome($outcome)
+    {
+        $this->outcome = $outcome;
+
+        return $this;
+    }
+
+    /**
+     * Get the outcome
+     *
+     * @return \Dvsa\Olcs\Api\Entity\System\RefData
+     */
+    public function getOutcome()
+    {
+        return $this->outcome;
+    }
+
+    /**
+     * Set the presiding staff name
+     *
+     * @param string $presidingStaffName
+     * @return Hearing
+     */
+    public function setPresidingStaffName($presidingStaffName)
+    {
+        $this->presidingStaffName = $presidingStaffName;
+
+        return $this;
+    }
+
+    /**
+     * Get the presiding staff name
+     *
+     * @return string
+     */
+    public function getPresidingStaffName()
+    {
+        return $this->presidingStaffName;
     }
 
     /**
