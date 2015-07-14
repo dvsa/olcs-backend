0fda65e3fb16beb62c2fc65b4fee644f8df6499e
Dan Eggleston <dan@stolenegg.com>
2015-07-14 15:18:09 +0100

    OLCS-9873 more unit tests for commands, queries and controllers

---
+++ test/module/Api/src/Domain/Command/CompaniesHouse/CompareTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Companies House Compare command test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Command\CompaniesHouse;
+
+use Dvsa\Olcs\Api\Domain\Command\CompaniesHouse\Compare;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Companies House Compare command test
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class CompareTest extends PHPUnit_Framework_TestCase
+{
+    public function testStructure()
+    {
+        $command = Compare::create(['companyNumber' => '01234567']);
+
+        $this->assertEquals('01234567', $command->getCompanyNumber());
+        $this->assertEquals(['companyNumber' => '01234567'], $command->getArrayCopy());
+    }
+}
