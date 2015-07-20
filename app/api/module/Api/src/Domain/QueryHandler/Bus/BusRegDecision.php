f4c8510de66eb3a651f59d33fdcccbe4dd04fa11
Piotr Filak <piotr.filak@valtech.co.uk>
2015-07-20 16:06:21 +0100

    OLCS-9840 - Internal - Bus reg - Processing Decision BE

---
+++ app/api/module/Api/src/Domain/QueryHandler/Bus/BusRegDecision.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Bus Reg Decision
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Bus;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * Bus Reg Decision
+ */
+class BusRegDecision extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'Bus';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        $busReg = $this->getRepo()->fetchUsingId($query);
+
+        return $this->result(
+            $busReg,
+            [],
+            ['decision' => $busReg->getDecision()]
+        );
+    }
+}
