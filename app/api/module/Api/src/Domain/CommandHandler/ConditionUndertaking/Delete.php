708de873fbb54c53fc55a3840b3cf70ae0c8011f
Shaun Lizzio <shaun@lizzio.co.uk>
2015-07-20 09:05:15 +0100

    Removed old cases c and u, replaced for generic c and u, added delete

---
+++ app/api/module/Api/src/Domain/CommandHandler/ConditionUndertaking/Delete.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Delete ConditionUndertaking
+ */
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\ConditionUndertaking;
+
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
+use Dvsa\Olcs\Api\Entity as Entities;
+use Doctrine\ORM\Query;
+
+/**
+ * Delete ConditionUndertaking
+ */
+final class Delete extends AbstractDeleteCommandHandler
+{
+    protected $repoServiceName = 'ConditionUndertaking';
+}
