edfac8da8a1cf351c5d403cc9b7d74bddc19f95a
Shaun Lizzio <shaun@lizzio.co.uk>
2015-07-20 10:24:29 +0100

    Added UT for CaseWithLicence, removed unnecessary code

--- module/Api/src/Domain/Repository/Cases.php
+++ module/Api/src/Domain/Repository/Cases.php
@@ -20,18 +20,6 @@ class Cases extends AbstractRepository
     protected $entity = Entity::class;
 
     /**
-     * @param QueryBuilder $qb
-     * @param int          $id
-     */
-    protected function buildDefaultQuery(QueryBuilder $qb, $id)
-    {
-        parent::buildDefaultQuery($qb, $id);
-
-        $this->getQueryBuilder()
-            ->with('licence');
-    }
-
-    /**
      * Fetch the default record by it's id
      *
      * @param Query|QryCmd $query
@@ -93,7 +81,6 @@ class Cases extends AbstractRepository
         parent::buildDefaultQuery($qb, $query->getId());
 
         $this->getQueryBuilder()
-            ->withRefdata()
             ->with('licence', 'l')
             ->with('l.operatingCentres', 'loc')
             ->with('loc.operatingCentre', 'oc')
@@ -110,26 +97,6 @@ class Cases extends AbstractRepository
         }
 
         return $results[0];
-
-
-
-        $qb = $this->createQueryBuilder();
-
-        $this->getQueryBuilder()->modifyQuery($qb)
-            ->withRefdata()
-            ->with('licence');
-
-        $qb->andWhere($qb->expr()->eq('p.case', ':byId'))
-            ->setParameter('byId', $query->getId())
-            ->setMaxResults(1);
-
-        $pi = $qb->getQuery()->getResult();
-
-        if (!empty($pi) && $hydrateMode === Query::HYDRATE_ARRAY) {
-            $case['pi'] = $pi[0];
-        }
-
-        return $case;
     }
 
     /**
