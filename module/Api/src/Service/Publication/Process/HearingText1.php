d2058d817cb006791c76692f6206bf2826f67542
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-21 12:03:20 +0100

    Text1 filter with info specific to Pi Hearings

---
+++ module/Api/src/Service/Publication/Process/HearingText1.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Hearing text 1
+ */
+namespace Dvsa\Olcs\Api\Service\Publication\Process;
+
+/**
+ * Hearing text 1
+ */
+class HearingText1 extends Text1
+{
+    protected $pi = 'Public Inquiry (%s) to be held at %s, on %s commencing at %s';
+}
