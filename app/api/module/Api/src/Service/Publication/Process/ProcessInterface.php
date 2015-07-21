409549d2d79b617cb6bab5216ca4a52fd9b279fe
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-21 11:57:30 +0100

    Changed interface to specify ImmutableArrayObject

--- app/api/module/Api/src/Service/Publication/Process/ProcessInterface.php
+++ app/api/module/Api/src/Service/Publication/Process/ProcessInterface.php
@@ -5,8 +5,9 @@ namespace Dvsa\Olcs\Api\Service\Publication\Process;
 
 
 use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Service\Publication\ImmutableArrayObject;
 
 interface ProcessInterface
 {
-    public function process(PublicationLink $publication, \ArrayObject $context);
+    public function process(PublicationLink $publication, ImmutableArrayObject $context);
 }
