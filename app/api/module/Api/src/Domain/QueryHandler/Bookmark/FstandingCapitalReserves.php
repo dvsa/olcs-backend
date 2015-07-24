960b1f3f8d2629117cf1e47d4a4c95671c605d0e
Dan Eggleston <dan@stolenegg.com>
2015-07-24 15:06:56 +0100

    OLCS-9276 add bookmark service and query handler, plus unit tests

--- app/api/module/Api/src/Domain/QueryHandler/Bookmark/FstandingCapitalReserves.php
+++ app/api/module/Api/src/Domain/QueryHandler/Bookmark/FstandingCapitalReserves.php
@@ -9,6 +9,7 @@ namespace Dvsa\Olcs\Api\Domain\QueryHandler\Bookmark;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Fstanding Capital Reserves Bookmark
