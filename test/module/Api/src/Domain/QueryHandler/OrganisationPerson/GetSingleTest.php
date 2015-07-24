08c52206ec4077d77d186b030295e3c5bc949e75
Mat Evans <mat.evans@valtech.co.uk>
2015-07-24 15:43:10 +0100

    OLCS-9266 Operator People

---
+++ test/module/Api/src/Domain/QueryHandler/OrganisationPerson/GetSingleTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\OrganisationPerson;
+
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\QueryHandler\OrganisationPerson\GetSingle as QueryHandler;
+use Dvsa\Olcs\Transfer\Query\OrganisationPerson\GetSingle as Query;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+
+/**
+ * GetSingleTest
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+class GetSingleTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new QueryHandler();
+        $this->mockRepo('OrganisationPerson', \Dvsa\Olcs\Api\Domain\Repository\OrganisationPerson::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = Query::create(['QUERY']);
+
+        $mockOrganisationPerson = m::mock(\Dvsa\Olcs\Api\Entity\Organisation\OrganisationPerson::class);
+        $mockOrganisationPerson->shouldReceive('serialize')->with(
+            ['person' => ['title']]
+        )->once()->andReturn(['SERIALIZED']);
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchUsingId')->with($query)
+            ->andReturn($mockOrganisationPerson);
+
+        $result = $this->sut->handleQuery($query);
+
+        $this->assertSame(['SERIALIZED'], $result->serialize());
+    }
+}
