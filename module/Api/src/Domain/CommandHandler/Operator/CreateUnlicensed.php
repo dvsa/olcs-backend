fdca4738fb3134038bf0b8cd0742a7772f6ea5e0
Dan Eggleston <dan@stolenegg.com>
2015-07-27 16:20:17 +0100

    OLCS-3675 implement command handler for create unlicensed operator

--- module/Api/src/Domain/CommandHandler/Operator/CreateUnlicensed.php
+++ module/Api/src/Domain/CommandHandler/Operator/CreateUnlicensed.php
@@ -11,6 +11,10 @@ use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Entity\ContactDetails\ContactDetails as ContactDetailsEntity;
+use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
+use Dvsa\Olcs\Api\Entity\Organisation\Organisation as OrganisationEntity;
+use Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea as TrafficAreaEntity;
 
 /**
  * Create Unlicensed Operator
@@ -19,11 +23,44 @@ use Dvsa\Olcs\Transfer\Command\CommandInterface;
  */
 final class CreateUnlicensed extends AbstractCommandHandler implements TransactionedInterface
 {
-    protected $repoServiceName = 'Organisation';
+    protected $repoServiceName = 'Licence';
 
     public function handleCommand(CommandInterface $command)
     {
         $result = new Result();
+
+        $organisation = new OrganisationEntity();
+        $organisation
+            ->setName($command->getName())
+            ->setIsUnlicensed(true)
+            ->setType($this->getRepo()->getRefdataReference(OrganisationEntity::ORG_TYPE_OTHER));
+
+        $contactDetails = new ContactDetailsEntity(
+            $this->getRepo()->getRefdataReference(ContactDetailsEntity::CONTACT_TYPE_CORRESPONDENCE_ADDRESS)
+        );
+        // @TODO add address and phone data
+
+        $licence = new LicenceEntity(
+            $organisation,
+            $this->getRepo()->getRefdataReference(LicenceEntity::LICENCE_STATUS_UNLICENSED)
+        );
+        $licence
+            ->setOrganisation($organisation)
+            ->setTrafficArea($this->getRepo()->getReference(TrafficAreaEntity::class, $command->getTrafficArea()))
+            ->setGoodsOrPsv($this->getRepo()->getRefdataReference($command->getOperatorType()))
+            ->setLicenceType($this->getRepo()->getRefdataReference(LicenceEntity::LICENCE_TYPE_RESTRICTED))
+            ->setNiFlag($command->getTrafficArea() == TrafficAreaEntity::NORTHERN_IRELAND_TRAFFIC_AREA_CODE)
+            ->setCorrespondenceCd($contactDetails);
+
+        $this->getRepo()->save($licence);
+
+        $result
+            ->addId('licence', $licence->getId())
+            ->addMessage('Licence added')
+            ->addId('organisation', $licence->getOrganisation()->getId())
+            ->addMessage('Organisation added')
+            ->addId('contactDetails', $licence->getCorrespondenceCd()->getId())
+            ->addMessage('ContactDetails added');
 
         return $result;
     }
