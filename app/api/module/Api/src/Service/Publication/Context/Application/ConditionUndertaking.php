c005f8fe46fbef46253eb6fe8499686a1ad0b4b3
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-28 15:09:40 +0100

    Application condition undertaking filter

---
+++ app/api/module/Api/src/Service/Publication/Context/Application/ConditionUndertaking.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Publication\Context\Application;
+
+use Dvsa\Olcs\Api\Service\Publication\Context\AbstractContext;
+use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Service\Helper\FormatAddress;
+
+class ConditionUndertaking extends AbstractContext
+{
+    const ATTACHED_LIC = 'Attached to Licence.';
+    const ATTACHED_OC = 'Attached to Operating Centre: %s';
+
+    const COND_NEW = 'New %s: %s';
+    const COND_REMOVE = '%s to be removed: %s';
+    const COND_UPDATE = 'Current %s: %s';
+    const COND_AMENDED = 'Amended to: %s';
+
+    /**
+     * @param PublicationLink $publication
+     * @param \ArrayObject $context
+     * @return \ArrayObject
+     */
+    public function provide(PublicationLink $publication, \ArrayObject $context)
+    {
+        $data = [];
+        $conditionUndertakings = $publication->getApplication()->getConditionUndertakings();
+
+        if ($conditionUndertakings->count()) {
+            $addressFilter = new FormatAddress();
+
+            foreach ($conditionUndertakings as $conditionUndertaking) {
+                $action = $conditionUndertaking->getAction();
+
+                //work out the action
+                switch ($action) {
+                    case 'A':
+                        $actionString = self::COND_NEW;
+                        break;
+                    case 'D':
+                        $actionString = self::COND_REMOVE;
+                        break;
+                    case 'U':
+                        $actionString = self::COND_UPDATE;
+                        break;
+                    default:
+                        $actionString = self::COND_NEW;
+                }
+
+                $string = sprintf(
+                    $actionString,
+                    $conditionUndertaking->getConditionType()->getDescription(),
+                    $conditionUndertaking->getNotes()
+                );
+
+                $operatingCentre = $conditionUndertaking->getOperatingCentre();
+
+                //work out if it's a licence or an oc
+                if (!empty($operatingCentre)) {
+                    $string = ' Attached to Operating Centre: ' . $addressFilter->format($operatingCentre->getAddress());
+                } else {
+                    $string .= ' Attached to Licence.';
+                }
+
+                if ($action === 'U') {
+                    $string .= " " . sprintf(self::COND_AMENDED, $conditionUndertaking->getNotes());
+                }
+
+                $data[] = $string;
+            }
+        }
+
+        $context->offsetSet('conditionUndertaking', $data);
+
+        return $context;
+    }
+}
