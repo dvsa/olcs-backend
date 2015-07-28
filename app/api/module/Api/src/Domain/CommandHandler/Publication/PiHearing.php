27e90828fb1e245117c120f1e9ba8614bb9fde78
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-28 15:12:56 +0100

    Completed PiHearing command handler, moved some methods into trait

--- app/api/module/Api/src/Domain/CommandHandler/Publication/PiHearing.php
+++ app/api/module/Api/src/Domain/CommandHandler/Publication/PiHearing.php
@@ -10,20 +10,27 @@ use Dvsa\Olcs\Api\Domain\PublicationGeneratorAwareTrait;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Doctrine\ORM\Query;
 
+use Dvsa\Olcs\Transfer\Command\Publication\PiHearing as PiHearingCmd;
+use Dvsa\Olcs\Transfer\Command\Publication\PiDecision as PiDecisionCmd;
 use Dvsa\Olcs\Api\Entity\Pi\PiHearing as PiHearingEntity;
 use Dvsa\Olcs\Api\Entity\Cases\Cases as CasesEntity;
 use Dvsa\Olcs\Api\Entity\Pi\Pi as PiEntity;
+use Dvsa\Olcs\Api\Entity\Tm\TransportManager as TransportManagerEntity;
 use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
+use Dvsa\Olcs\Api\Entity\Publication\Publication as PublicationEntity;
 use Dvsa\Olcs\Api\Entity\Publication\PublicationSection as PublicationSectionEntity;
 use Dvsa\Olcs\Api\Entity\Publication\PublicationLink as PublicationLinkEntity;
+use Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea as TrafficAreaEntity;
+use Dvsa\Olcs\Api\Domain\Query\Bookmark\UnpublishedPi as UnpublishedPiQry;
 
 class PiHearing extends AbstractCommandHandler implements TransactionedInterface, PublicationGeneratorAwareInterface
 {
     use PublicationGeneratorAwareTrait;
+    use CreatePublicationTrait;
 
     protected $repoServiceName = 'PublicationLink';
 
-    protected $extraRepos = ['PiHearing', 'Publication'];
+    protected $extraRepos = ['PiHearing', 'Publication', 'TrafficArea'];
 
     /**
      * @param CommandInterface $command
@@ -36,95 +43,168 @@ class PiHearing extends AbstractCommandHandler implements TransactionedInterface
          * @var PiEntity $pi
          * @var CasesEntity $case
          * @var LicenceEntity $licence
+         * @var $command PiHearingCmd|PiDecisionCmd
          */
         $hearing = $this->getRepo('PiHearing')->fetchUsingId($command, Query::HYDRATE_OBJECT);
         $pi = $hearing->getPi();
         $case = $pi->getCase();
 
         if ($case->isTm()) {
-            return $this->handlePiHearing($hearing, $pi, $case, $command->getText2());
+            return $this->handleTmHearing($command, $hearing, $pi, $case);
         }
 
-        return $this->handleHearing($hearing, $pi, $case, $command->getText2());
+        return $this->handleHearing($hearing, $pi, $case, $command);
     }
 
-    public function handleHearing(PiHearingEntity $hearing, PiEntity $pi, CasesEntity $case, $text2)
-    {
-        /** @var LicenceEntity $licence */
+    public function handleTmHearing(
+        CommandInterface $command,
+        PiHearingEntity $hearing,
+        PiEntity $pi,
+        CasesEntity $case
+    ) {
+        $result = new Result();
 
-        $pubSection = 13;
+        /**
+         * @var TransportManagerEntity $transportManager
+         * @var PublicationSectionEntity $publicationSection
+         * @var TrafficAreaEntity $trafficArea
+         * @var PiHearingCmd|PiDecisionCmd $command
+         */
+        if ($command instanceof PiHearingCmd) {
+            $pubSection = PublicationSectionEntity::TM_HEARING_SECTION;
+            $handler = 'TmHearingPublication';
+        } else {
+            $pubSection = PublicationSectionEntity::TM_DECISION_SECTION;
+            $handler = 'TmHearingDecision';
+        }
+
+        $transportManager = $case->getTransportManager();
+        $publicationSection = $this->getPublicationSection($pubSection);
+        $trafficAreas = $command->getTrafficAreas();
+        $pubTypes = $command->getPubTypes();
+
+        if (in_array('all', $trafficAreas)) {
+            $trafficAreas = $this->getRepo('TrafficArea')->fetchAll();
+        }
+
+        if (in_array('All', $pubTypes)) {
+            $pubTypes = ['A&D', 'N&P'];
+        }
+
+        foreach ($trafficAreas as $ta) {
+            foreach ($pubTypes as $pubType) {
+                if ($ta instanceof TrafficAreaEntity) {
+                    $trafficArea = $this->getRepo()->getReference(TrafficAreaEntity::class, $ta->getId());
+                } else {
+                    $trafficArea = $this->getRepo()->getReference(TrafficAreaEntity::class, $ta);
+                }
+
+                //no N&P for Northern Ireland
+                if ($trafficArea->getId() === TrafficAreaEntity::NORTHERN_IRELAND_TRAFFIC_AREA_CODE
+                    && $pubType == 'N&P') {
+                    continue;
+                }
+
+                /**
+                 * @var UnpublishedPiQry $unpublishedQuery
+                 * @var PublicationEntity $publication
+                 * @var PublicationLinkEntity $publicationLink
+                 */
+                $publication = $this->getPublication($trafficArea->getId(), $pubType);
+                $unpublishedQuery = $this->getUnpublishedPiQuery($publication->getId(), $pi->getId(), $pubSection);
+                $publicationLink = $this->getPublicationLink($unpublishedQuery);
+                $publicationLink->updateTmPiHearing(
+                    $transportManager,
+                    $pi,
+                    $publication,
+                    $publicationSection,
+                    $trafficArea,
+                    $command->getText2()
+                );
+
+                $result->merge(
+                    $this->createPublication(
+                        $handler,
+                        $publicationLink,
+                        $this->extractHearingData($hearing)
+                    )
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param PiHearingEntity $hearing
+     * @param PiEntity $pi
+     * @param CasesEntity $case
+     * @param CommandInterface $command
+     * @return Result
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
+    public function handleHearing(PiHearingEntity $hearing, PiEntity $pi, CasesEntity $case, CommandInterface $command)
+    {
+        /**
+         * @var LicenceEntity $licence
+         * @var PiHearingCmd|PiDecisionCmd $command
+         */
+        if ($command instanceof PiHearingCmd) {
+            $pubSection = PublicationSectionEntity::HEARING_SECTION;
+            $handler = 'HearingPublication';
+        } else {
+            $pubSection = PublicationSectionEntity::DECISION_SECTION;
+            $handler = 'HearingDecision';
+        }
 
         $licence = $case->getLicence();
         $licType = $licence->getGoodsOrPsv()->getId();
         $pubType = ($licType == LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE ? 'A&D' : 'N&P');
         $trafficArea = $licence->getTrafficArea();
-        $publicationSection = $this->getRepo()->getReference(PublicationSectionEntity::class, $pubSection);
+        $publicationSection = $this->getPublicationSection($pubSection);
+        $text2 = $command->getText2();
 
-        $hearingData = [
+        /**
+         * @var UnpublishedPiQry $unpublishedQuery
+         * @var PublicationEntity $publication
+         * @var PublicationLinkEntity $publicationLink
+         */
+        $publication = $this->getPublication($trafficArea->getId(), $pubType);
+        $unpublishedQuery = $this->getUnpublishedPiQuery($publication->getId(), $pi->getId(), $pubSection);
+        $publicationLink = $this->getPublicationLink($unpublishedQuery);
+        $publicationLink->updatePiHearing($licence, $pi, $publication, $publicationSection, $trafficArea, $text2);
+
+        return $this->createPublication($handler, $publicationLink, $this->extractHearingData($hearing));
+    }
+
+    /**
+     * @param int $publication
+     * @param int $pi
+     * @param int $pubSection
+     * @return UnpublishedPiQry
+     */
+    public function getUnpublishedPiQuery($publication, $pi, $pubSection)
+    {
+        $data =  [
+            'publication' => $publication,
+            'pi' => $pi,
+            'publicationSection' => $pubSection
+        ];
+
+        return UnpublishedPiQry::create($data);
+    }
+
+    /**
+     * @param PiHearingEntity $hearing
+     * @return array
+     */
+    public function extractHearingData($hearing)
+    {
+        return [
             'piVenue' => $hearing->getPiVenue()->getId(),
             'piVenueOther' => $hearing->getPiVenueOther(),
             'hearingDate' => $hearing->getHearingDate(),
             'id' => $hearing->getId()
         ];
-
-        $publication = $this->getRepo('Publication')->fetchLatestForTrafficAreaAndType(
-            $trafficArea->getId(),
-            $pubType
-        );
-
-        //check if we have an existing publication link
-        $previousPublication = $this->getRepo()->fetchSingleUnpublished(
-            $publication->getId(),
-            $pi->getId(),
-            $pubSection
-        );
-
-        if (!empty($previousPublication)) {
-            $publicationLink = $previousPublication;
-            $message = 'Publication link updated';
-        } else {
-            $publicationLink = new PublicationLinkEntity();
-            $message = 'Publication link created';
-        }
-
-        $publicationLink->updatePiHearing($licence, $pi, $publication, $publicationSection, $trafficArea, $text2);
-        $result = $this->createPublication('HearingPublication', $publicationLink, $hearingData);
-        $result->addMessage($message);
-
-        return $result;
-    }
-
-    public function handleTmHearing(PiHearingEntity $hearing, PiEntity $pi, CasesEntity $case, $text2)
-    {
-        //$trafficAreas = $hearing->ge
-
-        //return \Doctrine\Common\Util\Debug::dump($publicationLink);
-    }
-
-    /**
-     * @param $publicationConfig
-     * @param PublicationLinkEntity $publicationLink
-     * @param $existingContext
-     * @return Result
-     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
-     * @throws \Exception
-     */
-    private function createPublication(
-        $publicationConfig,
-        PublicationLinkEntity $publicationLink,
-        $existingContext
-    ) {
-        $publicationLink = $this->publicationGenerator->createPublication(
-            $publicationConfig,
-            $publicationLink,
-            $existingContext
-        );
-
-        $this->getRepo()->save($publicationLink);
-
-        $result = new Result();
-        $result->addId('publicationLink', $publicationLink->getId());
-
-        return $result;
     }
 }
