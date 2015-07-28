180e3a6579e030196f8ed56d55b59f5df1751ca5
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-28 15:10:43 +0100

    Application operating centres publication filter

---
+++ app/api/module/Api/src/Service/Publication/Context/Application/OperatingCentres.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Publication\Context\Application;
+
+use Dvsa\Olcs\Api\Service\Publication\Context\AbstractContext;
+use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
+use Dvsa\Olcs\Api\Entity\Application\ApplicationOperatingCentre as ApplicationOperatingCentreEntity;
+use Dvsa\Olcs\Api\Service\Helper\FormatAddress;
+
+class OperatingCentres extends AbstractContext
+{
+    /**
+     * @param PublicationLink $publication
+     * @param \ArrayObject $context
+     * @return \ArrayObject
+     */
+    public function provide(PublicationLink $publication, \ArrayObject $context)
+    {
+        $newOcData = [];
+        $application = $publication->getApplication();
+        $licType = $application->getGoodsOrPsv()->getId();
+        $operatingCentres = $application->getOperatingCentres();
+        $authorisationText = '';
+        $addressFilter = new FormatAddress();
+
+        foreach ($operatingCentres as $oc) {
+            /**
+             * @var ApplicationOperatingCentreEntity $oc
+             */
+            $totAuthTrailers = $oc->getNoOfTrailersRequired();
+            $totAuthVehicles = $oc->getNoOfVehiclesRequired();
+
+            $gvWithTrailers = (bool)($licType == LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE && $totAuthTrailers);
+
+            //check we have authorised vehicles
+            if ($totAuthVehicles || $gvWithTrailers) {
+                $authorisationText = 'Authorisation: ';
+
+                if ($totAuthVehicles && $gvWithTrailers) {
+                    $authorisationText .= $totAuthVehicles . ' Vehicle(s) and ' . $totAuthTrailers . ' Trailer(s)';
+                } elseif ($totAuthVehicles) {
+                    $authorisationText .= $totAuthVehicles . ' Vehicle(s)';
+                } else {
+                    $authorisationText .= $totAuthTrailers . ' Trailer(s)';
+                }
+            }
+
+            $ocAction = $oc->getAction();
+
+            switch ($ocAction) {
+                case 'U':
+                    $prefix = 'Update ';
+                    break;
+                case 'D':
+                    $prefix = 'Remove ';
+                    break;
+                default:
+                    $prefix = 'New ';
+            }
+
+            $operatingCentreText = $addressFilter->format($oc->getOperatingCentre()->getAddress());
+
+            $newOcData[] = trim($prefix . 'Operating Centre: ' . $operatingCentreText . ' ' . $authorisationText);
+        }
+
+        return $context;
+    }
+}
