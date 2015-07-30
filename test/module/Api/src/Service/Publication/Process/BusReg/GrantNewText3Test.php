f8690d737913b1e6e2b3ae453e959504bdfd1057
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-07-30 17:38:24 +0100

    Test for bus reg publication processors

---
+++ test/module/Api/src/Service/Publication/Process/BusReg/GrantNewText3Test.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Publication\Process\BusReg;
+
+use Dvsa\Olcs\Api\Service\Publication\Process\BusReg\GrantNewText3;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Service\Publication\ImmutableArrayObject;
+use Dvsa\Olcs\Api\Entity\Bus\BusReg as BusRegEntity;
+
+/**
+ * Class GrantNewText3
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class GrantNewText3Test extends MockeryTestCase
+{
+    /**
+     * @group publicationFilter
+     *
+     * Test the Bus Reg GrantNewText3 filter
+     */
+    public function testProcess()
+    {
+        $sut = new GrantNewText3();
+
+        $startPoint = 'start point';
+        $finishPoint = 'finish point';
+        $via = 'via point';
+        $busServices = 'bus services';
+        $busServiceTypes = 'bus service types';
+        $otherDetails = 'Other details';
+        $effectiveDate = '2014-05-14';
+        $endDate = '2014-06-30';
+        $formattedEffectiveDate = '14 May 2014';
+        $formattedEndDate = '30 June 2014';
+
+        $text = "From: " . $startPoint . "\n"
+            . "To: " . $finishPoint . "\n"
+            . "Via: " . $via . "\n"
+            . "Name or No.: " . $busServices . "\n"
+            . "Service type: " . $busServiceTypes . "\n"
+            . "Effective date: " . $formattedEffectiveDate . "\n"
+            . "End date: " . $formattedEndDate . "\n"
+            . "Other details: " . $otherDetails;
+
+        $expectedString = sprintf(
+            $text,
+            $startPoint,
+            $finishPoint,
+            $via,
+            $busServices,
+            $busServiceTypes,
+            $formattedEffectiveDate,
+            $formattedEndDate,
+            $otherDetails
+        );
+
+        $input = [
+            'busServices' => $busServices,
+            'busServiceTypes' => $busServiceTypes
+        ];
+
+        $busRegMock = m::mock(BusRegEntity::class);
+        $busRegMock->shouldReceive('getStartPoint')->andReturn($startPoint);
+        $busRegMock->shouldReceive('getFinishPoint')->andReturn($finishPoint);
+        $busRegMock->shouldReceive('getEffectiveDate')->andReturn($effectiveDate);
+        $busRegMock->shouldReceive('getEndDate')->andReturn($endDate);
+        $busRegMock->shouldReceive('getVia')->andReturn($via);
+        $busRegMock->shouldReceive('getOtherDetails')->andReturn($otherDetails);
+
+        $publicationLink = m::mock(PublicationLink::class)->makePartial();
+        $publicationLink->shouldReceive('getBusReg')->andReturn($busRegMock);
+
+        $output = $sut->process($publicationLink, new ImmutableArrayObject($input));
+        $this->assertEquals($expectedString, $output->getText3());
+    }
+}
---
+++ test/module/Api/src/Service/Publication/Process/BusReg/Text2Test.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Publication\Process\BusReg;
+
+use Dvsa\Olcs\Api\Service\Publication\Process\BusReg\Text2 as BusRegText2;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Service\Publication\ImmutableArrayObject;
+use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
+use Dvsa\Olcs\Api\Entity\Organisation\Organisation as OrganisationEntity;
+use Dvsa\Olcs\Api\Entity\Organisation\TradingName as TradingNameEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class Text2Test
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class Text2Test extends MockeryTestCase
+{
+    /**
+     * @group publicationFilter
+     *
+     * Test the bus reg text 2 filter
+     */
+    public function testProcess()
+    {
+        $sut = new BusRegText2();
+
+        $organisationName = 'organisation name';
+        $organisationTradingName1 = 'trading name 1';
+        $organisationTradingName2 = 'trading name 2';
+        $licenceAddress = 'licence address';
+
+        $input = [
+            'licenceAddress' => $licenceAddress
+        ];
+
+        $tradingName = m::mock(TradingNameEntity::class);
+        $tradingName->shouldReceive('getName')->andReturn($organisationTradingName1);
+        $tradingName2 = m::mock(TradingNameEntity::class);
+        $tradingName2->shouldReceive('getName')->andReturn($organisationTradingName2);
+        $tradingNames = new ArrayCollection([$tradingName, $tradingName2]);
+
+        $organisationMock = m::mock(OrganisationEntity::class);
+        $organisationMock->shouldReceive('getName')->andReturn($organisationName);
+        $organisationMock->shouldReceive('getTradingNames')->andReturn($tradingNames);
+
+        $licenceMock = m::mock(LicenceEntity::class);
+        $licenceMock->shouldReceive('getOrganisation')->andReturn($organisationMock);
+
+        $publicationLink = m::mock(PublicationLink::class)->makePartial();
+        $publicationLink->shouldReceive('getLicence->getOrganisation')->andReturn($organisationMock);
+
+        $expectedString = strtoupper($organisationName . ' T/A ' . $organisationTradingName2 . ', ' . $licenceAddress);
+
+        $output = $sut->process($publicationLink, new ImmutableArrayObject($input));
+        $this->assertEquals($expectedString, $output->getText2());
+    }
+}
