9bd4f6475c683fb221a6ce28f2b838424f5bf5ea
Dan Eggleston <dan@stolenegg.com>
2015-07-30 13:34:06 +0100

    OLCS-3675 unit test for update command handler

--- test/module/Api/src/Domain/CommandHandler/Operator/CreateUnlicensedTest.php
+++ test/module/Api/src/Domain/CommandHandler/Operator/CreateUnlicensedTest.php
@@ -57,7 +57,7 @@ class CreateUnlicensedTest extends CommandHandlerTestCase
             ],
             ContactDetailsEntity::class => [
                 1 => m::mock(ContactDetailsEntity::class)->makePartial(),
-            ]
+            ],
         ];
 
         parent::initReferences();
