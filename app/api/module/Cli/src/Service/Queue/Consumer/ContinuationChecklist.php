ab5663455d8c782fdbdc9dafe8d72f96e35a66d2
Dan Eggleston <dan@stolenegg.com>
2015-07-31 18:01:27 +0100

    OLCS-10245 WIP on queue consumer

--- app/api/module/Cli/src/Service/Queue/Consumer/ContinuationChecklist.php
+++ app/api/module/Cli/src/Service/Queue/Consumer/ContinuationChecklist.php
@@ -28,4 +28,27 @@ class ContinuationChecklist extends AbstractConsumer
     {
         return ['id' => $item->getEntityId()];
     }
+
+    /**
+     * @todo probably don't need this unless we override processMessage()
+     */
+    protected function skip(QueueEntity $item)
+    {
+        return $this->success($item, 'Continuation detail no longer pending');
+    }
+
+    /**
+     * Mark the message as failed and continuation detail record as errored
+     *
+     * @param QueueEntity $item
+     * @param string $reason
+     * @return string
+     * @todo port additional logic from olcs-internal consumer on failure
+     * @see Cli\Service\Queue\Consumer\ContinuationChecklist
+     */
+    protected function failed(QueueEntity $item, $reason = null)
+    {
+        // $this->getServiceLocator()->get('Entity\ContinuationDetail')->checklistFailed($item['entityId']);
+        return parent::failed($item, $reason);
+    }
 }
