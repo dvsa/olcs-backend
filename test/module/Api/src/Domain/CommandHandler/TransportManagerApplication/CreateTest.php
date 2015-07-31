c43d10100b40269ff5a071ca019a92e38256262b
Alex P <alex.peshkov@valtech.co.uk>
2015-07-31 14:36:08 +0100

    OLCS-9256 Transport managers: It's possible to add the same TM repeatedly

--- test/module/Api/src/Domain/CommandHandler/TransportManagerApplication/CreateTest.php
+++ test/module/Api/src/Domain/CommandHandler/TransportManagerApplication/CreateTest.php
@@ -14,6 +14,8 @@ use Dvsa\Olcs\Api\Domain\Repository\User;
 use Dvsa\Olcs\Transfer\Command\TransportManagerApplication\Create as Command;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
 use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
+use Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication as TransportManagerApplicationEntity;
 
 /**
  * CreateTest
@@ -23,7 +25,6 @@ use Mockery as m;
 class CreateTest extends CommandHandlerTestCase
 {
     protected $loggedInUser;
-
 
     public function setUp()
     {
@@ -61,9 +62,14 @@ class CreateTest extends CommandHandlerTestCase
         $command = Command::create(['application' => 863, 'user' => 234, 'action' => 'D']);
 
         $user = $this->loggedInUser;
-        $user->setTransportManager('TM');
+        $mockTm = m::mock()
+            ->shouldReceive('getId')
+            ->andReturn(21)
+            ->getMock();
 
-        $this->repoMap['User']->shouldReceive('fetchById')->with(234)->once()->andReturn($user);
+        $user->setTransportManager($mockTm);
+
+        $this->repoMap['User']->shouldReceive('fetchForTma')->with(234)->once()->andReturn($user);
         $this->repoMap['TransportManagerApplication']->shouldReceive('save')->once()->andReturnUsing(
             function (\Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication $tma) {
                 $tma->setId(534);
@@ -74,9 +80,13 @@ class CreateTest extends CommandHandlerTestCase
                     $tma->getApplication()
                 );
                 $this->assertSame($this->refData['tmap_st_incomplete'], $tma->getTmApplicationStatus());
-                $this->assertSame('TM', $tma->getTransportManager());
+                $this->assertSame(21, $tma->getTransportManager()->getId());
             }
-        );
+        )
+        ->shouldReceive('fetchByTmAndApplication')
+        ->with(21, 863)
+        ->once()
+        ->andReturn(null);
 
         $this->expectedSideEffect(
             \Dvsa\Olcs\Api\Domain\Command\Application\UpdateApplicationCompletion::class,
@@ -97,7 +107,7 @@ class CreateTest extends CommandHandlerTestCase
         $user = new \Dvsa\Olcs\Api\Entity\User\User();
         $user->setContactDetails('CD');
 
-        $this->repoMap['User']->shouldReceive('fetchById')->with(234)->once()->andReturn($user);
+        $this->repoMap['User']->shouldReceive('fetchForTma')->with(234)->once()->andReturn($user);
 
         $savedTm = null;
         $this->repoMap['TransportManager']->shouldReceive('save')->once()->andReturnUsing(
@@ -147,6 +157,53 @@ class CreateTest extends CommandHandlerTestCase
             ],
             new \Dvsa\Olcs\Api\Domain\Command\Result()
         );
+
+        $this->sut->handleCommand($command);
+    }
+
+    public function testHandleCommandWithException()
+    {
+        $this->setExpectedException(
+            ValidationException::class,
+            [
+                'registeredUser' => [
+                    TransportManagerApplicationEntity::ERROR_TM_EXIST =>
+                        'forename familyName has already been added to this application'
+                ]
+            ]
+        );
+        $command = Command::create(['application' => 863, 'user' => 234, 'action' => 'D']);
+
+        $mockContactDetails = m::mock()
+            ->shouldReceive('getPerson')
+            ->andReturn(
+                m::mock()
+                ->shouldReceive('getForename')
+                ->andReturn('forename')
+                ->once()
+                ->shouldReceive('getFamilyname')
+                ->andReturn('familyName')
+                ->once()
+                ->getMock()
+            )
+            ->getMock();
+
+        $user = new \Dvsa\Olcs\Api\Entity\User\User();
+        $user->setContactDetails($mockContactDetails);
+        $mockTm = m::mock()
+            ->shouldReceive('getId')
+            ->andReturn(21)
+            ->getMock();
+
+        $user->setTransportManager($mockTm);
+
+        $this->repoMap['User']->shouldReceive('fetchForTma')->with(234)->once()->andReturn($user);
+
+        $this->repoMap['TransportManagerApplication']
+            ->shouldReceive('fetchByTmAndApplication')
+            ->with(21, 863)
+            ->once()
+            ->andReturn(['foo']);
 
         $this->sut->handleCommand($command);
     }
