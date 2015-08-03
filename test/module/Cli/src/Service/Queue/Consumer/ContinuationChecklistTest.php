72bc018234bfc8581220f034eacec97eac4fb23e
Dan Eggleston <dan@stolenegg.com>
2015-08-03 14:09:42 +0100

    OLCS-10245 add unit test for queue consumer failure

--- test/module/Cli/src/Service/Queue/Consumer/ContinuationChecklistTest.php
+++ test/module/Cli/src/Service/Queue/Consumer/ContinuationChecklistTest.php
@@ -8,6 +8,7 @@
 namespace Dvsa\OlcsTest\Cli\Service\Queue\Consumer;
 
 use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail as ContinuationDetailEntity;
 use Dvsa\Olcs\Api\Entity\Queue\Queue as QueueEntity;
 use Dvsa\Olcs\Cli\Service\Queue\Consumer\ContinuationChecklist as Sut;
 use Dvsa\OlcsTest\Cli\Service\Queue\Consumer\AbstractConsumerTestCase;
@@ -51,6 +52,47 @@ class ContinuationChecklistTest extends AbstractConsumerTestCase
 
         $this->assertEquals(
             'Successfully processed message: 99  ContinuationDetail updated, Document created',
+            $result
+        );
+    }
+
+    public function testProcessMessageFailure()
+    {
+        $item = new QueueEntity();
+        $item->setId(99);
+        $item->setEntityId(69);
+
+        $this->expectCommandException(
+            \Dvsa\Olcs\Api\Domain\Command\ContinuationDetail\Process::class,
+            ['id' => 69],
+            \Dvsa\Olcs\Api\Domain\Exception\Exception::class,
+            'epic fail'
+        );
+
+        $this->expectCommand(
+            \Dvsa\Olcs\Transfer\Command\ContinuationDetail\Update::class,
+            [
+              'id' => 69,
+              'status' => ContinuationDetailEntity::STATUS_ERROR,
+              'version' => null,
+              'received' => null,
+              'totAuthVehicles' => null,
+              'totPsvDiscs' => null,
+              'totCommunityLicences' => null,
+            ],
+            new Result()
+        );
+
+        $this->expectCommand(
+            \Dvsa\Olcs\Api\Domain\Command\Queue\Failed::class,
+            ['item' => $item],
+            new Result()
+        );
+
+        $result = $this->sut->processMessage($item);
+
+        $this->assertEquals(
+            'Failed to process message: 99  epic fail',
             $result
         );
     }
