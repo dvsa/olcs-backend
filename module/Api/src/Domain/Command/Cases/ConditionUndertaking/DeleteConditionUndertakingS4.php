8b637e1e4d1cdb3775cbbb34a4c34f8093be4fed
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-08-04 14:51:21 +0100

    Added in refusing an application.

---
+++ module/Api/src/Domain/Command/Cases/ConditionUndertaking/DeleteConditionUndertakingS4.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * DeleteConditionUndertaking.php
+ *
+ * @author Josh Curtis <josh.curtis@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Cases\ConditionUndertaking;
+
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * Class DeleteConditionUndertaking
+ *
+ * Delete condition undertaking.
+ *
+ * @package Dvsa\Olcs\Api\Domain\Command\Discs
+ * @author Josh Curtis <josh.curtis@valtech.co.uk>
+ */
+final class DeleteConditionUndertakingS4 extends AbstractCommand
+{
+    protected $s4 = null;
+
+    /**
+     * @return null
+     */
+    public function getS4()
+    {
+        return $this->s4;
+    }
+}
