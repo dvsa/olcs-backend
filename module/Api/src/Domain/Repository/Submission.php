8c3419acb1b303d3ace22672016e356bd0829fbf
Shaun Lizzio <shaun@lizzio.co.uk>
2015-08-04 15:07:24 +0100

    Added more query data for index list

--- module/Api/src/Domain/Repository/Submission.php
+++ module/Api/src/Domain/Repository/Submission.php
@@ -3,6 +3,7 @@
 namespace Dvsa\Olcs\Api\Domain\Repository;
 
 use Dvsa\Olcs\Api\Entity\Submission\Submission as Entity;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Submission
@@ -12,4 +13,32 @@ use Dvsa\Olcs\Api\Entity\Submission\Submission as Entity;
 class Submission extends AbstractRepository
 {
     protected $entity = Entity::class;
+
+    /**
+     * Overridden default query to return appropriate table joins
+     * @param QueryBuilder $qb
+     * @param int $id
+     * @return \Dvsa\Olcs\Api\Domain\QueryBuilder
+     */
+    protected function buildDefaultQuery(QueryBuilder $qb, $id)
+    {
+        return $this->getQueryBuilder()->modifyQuery($qb)
+            ->withRefdata()
+            ->with('recipientUser', 'r')
+            ->with('r.contactDetails', 'rcd')
+            ->with('rcd.person')
+            ->byId($id);
+    }
+
+    /**
+     * Override to add additional data to the default fetchList() method
+     * @param QueryBuilder $qb
+     */
+    protected function applyListJoins(QueryBuilder $qb)
+    {
+        $this->getQueryBuilder()
+            ->with('recipientUser', 'r')
+            ->with('r.contactDetails', 'rcd')
+            ->with('rcd.person');
+    }
 }
