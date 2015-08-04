711573ac369e6fc99ca9a1c5d4c61a8d8498bef8
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-08-04 14:53:31 +0100

    Consolidated queries for finding unpublished publication entries

--- app/api/module/Api/src/Domain/Query/Bookmark/UnpublishedBusReg.php
+++ app/api/module/Api/src/Domain/Query/Bookmark/UnpublishedBusReg.php
@@ -2,34 +2,12 @@
 
 namespace Dvsa\Olcs\Api\Domain\Query\Bookmark;
 
-use Dvsa\Olcs\Transfer\Query\AbstractQuery;
-
 /**
  * Unpublished Bus Reg
  */
-class UnpublishedBusReg extends AbstractQuery
+class UnpublishedBusReg extends Unpublished
 {
     protected $busReg;
-    protected $publicationSection;
-    protected $publication;
-
-    protected $bundle = [];
-
-    /**
-     * @return int
-     */
-    public function getPublicationSection()
-    {
-        return $this->publicationSection;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPublication()
-    {
-        return $this->publication;
-    }
 
     /**
      * @return int
@@ -37,13 +15,5 @@ class UnpublishedBusReg extends AbstractQuery
     public function getBusReg()
     {
         return $this->busReg;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBundle()
-    {
-        return $this->bundle;
     }
 }
--- app/api/module/Api/src/Domain/Query/Bookmark/UnpublishedPi.php
+++ app/api/module/Api/src/Domain/Query/Bookmark/UnpublishedPi.php
@@ -2,34 +2,12 @@
 
 namespace Dvsa\Olcs\Api\Domain\Query\Bookmark;
 
-use Dvsa\Olcs\Transfer\Query\AbstractQuery;
-
 /**
  * Unpublished Pi
  */
-class UnpublishedPi extends AbstractQuery
+class UnpublishedPi extends Unpublished
 {
     protected $pi;
-    protected $publicationSection;
-    protected $publication;
-
-    protected $bundle = [];
-
-    /**
-     * @return mixed
-     */
-    public function getPublicationSection()
-    {
-        return $this->publicationSection;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPublication()
-    {
-        return $this->publication;
-    }
 
     /**
      * @return mixed
@@ -37,13 +15,5 @@ class UnpublishedPi extends AbstractQuery
     public function getPi()
     {
         return $this->pi;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBundle()
-    {
-        return $this->bundle;
     }
 }
