da11504b41b901ca65c1a33aa5902141081e957e
Alex P <alex.peshkov@valtech.co.uk>
2015-08-11 11:49:20 +0100

    OLCS-5139 mistype fixed

--- app/api/module/Api/src/Domain/Util/DateTime/AddMonthsRoundingDown.php
+++ app/api/module/Api/src/Domain/Util/DateTime/AddMonthsRoundingDown.php
@@ -9,7 +9,7 @@ class AddMonthsRoundingDown
 {
     /**
      * Calculates a date that is $months before/after $date with rounding down.
-     *  2015-12-31 + 2 month => 2015-12-31
+     *  2015-12-31 + 2 month => 2016-02-29
      *  2010-01-12 + 2 month => 2010-03-12
      *
      * @param \DateTime $date
