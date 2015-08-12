cd26dab9f0256c7a58f29f5c055773dd17af793b
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-08-12 15:50:24 +0100

    Added configuration of export.

--- module/Api/config/query-map.config.php
+++ module/Api/config/query-map.config.php
@@ -100,7 +100,8 @@ return [
     TransferQuery\Organisation\OutstandingFees::class => QueryHandler\Organisation\OutstandingFees::class,
     TransferQuery\Organisation\Dashboard::class => QueryHandler\Organisation\Dashboard::class,
     TransferQuery\Organisation\People::class => QueryHandler\Organisation\People::class,
-    TransferQuery\Organisation\CpidOrganisation::class => QueryHandler\Organisation\CpidOrganisation::class,
+    TransferQuery\Organisation\CpidOrganisation::class
+        => QueryHandler\Organisation\CpidOrganisation::class,
 
     // Variation
     TransferQuery\Variation\Variation::class => QueryHandler\Variation\Variation::class,
