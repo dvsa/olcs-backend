ab9fff77239570fbfb57479d3faba698ed940f68
Rob Caiger <rob@clocal.co.uk>
2015-08-13 13:58:54 +0100

    Fixed psv vehicles

--- app/api/module/Api/src/Domain/QueryHandler/Licence/PsvVehicles.php
+++ app/api/module/Api/src/Domain/QueryHandler/Licence/PsvVehicles.php
@@ -11,6 +11,7 @@ use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Api\Entity;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Domain\Service\PsvVehicles\PsvVehiclesQueryHelper;
 
 /**
  * Psv Vehicles
@@ -21,112 +22,28 @@ class PsvVehicles extends AbstractQueryHandler
 {
     protected $repoServiceName = 'Licence';
 
-    protected $extraRepos = ['LicenceVehicle'];
+    /**
+     * @var PsvVehiclesQueryHelper
+     */
+    protected $helper;
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->helper = $serviceLocator->getServiceLocator()->get('PsvVehiclesQueryHelper');
+
+        return parent::createService($serviceLocator);
+    }
 
     public function handleQuery(QueryInterface $query)
     {
         /** @var Entity\Licence\Licence $licence */
         $licence = $this->getRepo()->fetchUsingId($query);
 
-        $small = $this->getSmallVehicles($licence, $query->getIncludeRemoved());
-        $medium = $this->getMediumVehicles($licence, $query->getIncludeRemoved());
-        $large = $this->getLargeVehicles($licence, $query->getIncludeRemoved());
+        $flags = $this->helper->getCommonQueryFlags($licence, $query);
 
-        $smallCount = count($small);
-        $mediumCount = count($medium);
-        $largeCount = count($large);
+        $flags['canTransfer'] = !$licence->getOtherActiveLicences()->isEmpty();
+        $flags['hasBreakdown'] = $licence->hasPsvBreakdown();
 
-        $activeSmallCount = $this->countActiveSmallVehicles($licence);
-        $activeMediumCount = $this->countActiveMediumVehicles($licence);
-        $activeLargeCount = $this->countActiveLargeVehicles($licence);
-
-        return $this->result(
-            $licence,
-            [],
-            [
-                'showSmallTable' => $licence->shouldShowSmallTable($smallCount),
-                'showMediumTable' => $licence->shouldShowMediumTable($mediumCount),
-                'showLargeTable' => $licence->shouldShowLargeTable($largeCount),
-                'smallAuthExceeded' => $licence->isSmallAuthExceeded($activeSmallCount),
-                'mediumAuthExceeded' => $licence->isMediumAuthExceeded($activeMediumCount),
-                'largeAuthExceeded' => $licence->isLargeAuthExceeded($activeLargeCount),
-                'availableSmallSpaces' => $licence->getAvailableSmallSpaces($activeSmallCount),
-                'availableMediumSpaces' => $licence->getAvailableMediumSpaces($activeMediumCount),
-                'availableLargeSpaces' => $licence->getAvailableLargeSpaces($activeLargeCount),
-                'small' => $small,
-                'medium' => $medium,
-                'large' => $large,
-                'total' => $smallCount + $mediumCount + $largeCount,
-                'canTransfer' => !$licence->getOtherActiveLicences()->isEmpty(),
-                'hasBreakdown' => $licence->hasPsvBreakdown()
-            ]
-        );
-    }
-
-    private function countActiveSmallVehicles(Entity\Licence\Licence $licence)
-    {
-        return $this->getActiveVehiclesCount($licence, Entity\Vehicle\Vehicle::PSV_TYPE_SMALL);
-    }
-
-    private function countActiveMediumVehicles(Entity\Licence\Licence $licence)
-    {
-        return $this->getActiveVehiclesCount($licence, Entity\Vehicle\Vehicle::PSV_TYPE_MEDIUM);
-    }
-
-    private function countActiveLargeVehicles(Entity\Licence\Licence $licence)
-    {
-        if (!$licence->canHaveLargeVehicles()) {
-            return 0;
-        }
-
-        return $this->getActiveVehiclesCount($licence, Entity\Vehicle\Vehicle::PSV_TYPE_LARGE);
-    }
-
-    private function getSmallVehicles(Entity\Licence\Licence $licence, $includeRemoved)
-    {
-        return $this->getVehicles(
-            $licence,
-            Entity\Vehicle\Vehicle::PSV_TYPE_SMALL,
-            $includeRemoved
-        );
-    }
-
-    private function getMediumVehicles(Entity\Licence\Licence $licence, $includeRemoved)
-    {
-        return $this->getVehicles(
-            $licence,
-            Entity\Vehicle\Vehicle::PSV_TYPE_MEDIUM,
-            $includeRemoved
-        );
-    }
-
-    private function getLargeVehicles(Entity\Licence\Licence $licence, $includeRemoved)
-    {
-        if (!$licence->canHaveLargeVehicles()) {
-            return [];
-        }
-
-        return $this->getVehicles(
-            $licence,
-            Entity\Vehicle\Vehicle::PSV_TYPE_LARGE,
-            $includeRemoved
-        );
-    }
-
-    private function getVehicles(Entity\Licence\Licence $licence, $type, $includeRemoved)
-    {
-        return $this->resultList(
-            $this->getRepo('LicenceVehicle')->getPsvVehiclesByType(
-                $licence,
-                $type,
-                $includeRemoved
-            ),
-            ['vehicle']
-        );
-    }
-
-    private function getActiveVehiclesCount(Entity\Licence\Licence $licence, $type)
-    {
-        return $this->getRepo('LicenceVehicle')->getPsvVehiclesByType($licence, $type)->count();
+        return $this->result($licence, [], $flags);
     }
 }
