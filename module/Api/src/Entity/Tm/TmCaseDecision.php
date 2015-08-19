47ba4f0357e6bc49639d5b9704f2cafd8dc09378
Piotr Filak <piotr.filak@valtech.co.uk>
2015-08-19 15:45:07 +0100

    OLCS-9421 - Internal - Cases - Processing BE - Create/Update ReputeNotLost

--- module/Api/src/Entity/Tm/TmCaseDecision.php
+++ module/Api/src/Entity/Tm/TmCaseDecision.php
@@ -2,6 +2,8 @@
 
 namespace Dvsa\Olcs\Api\Entity\Tm;
 
+use Dvsa\Olcs\Api\Entity\Cases\Cases as CasesEntity;
+use Dvsa\Olcs\Api\Entity\System\RefData;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,5 +24,61 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class TmCaseDecision extends AbstractTmCaseDecision
 {
+    const DECISION_DECLARE_UNFIT = 'tm_decision_rl';
+    const DECISION_NO_FURTHER_ACTION = 'tm_decision_noa';
+    const DECISION_REPUTE_NOT_LOST = 'tm_decision_rnl';
 
+    public function __construct(CasesEntity $case, RefData $decision)
+    {
+        parent::__construct();
+        $this->setCase($case);
+        $this->setDecision($decision);
+    }
+
+    /**
+     * @param CasesEntity $case
+     * @param RefData $decision
+     * @param array $data Array of data
+     * @return TmCaseDecision
+     */
+    public static function create(CasesEntity $case, RefData $decision, array $data)
+    {
+        $tmCaseDecision = new static($case, $decision);
+        $tmCaseDecision->update($data);
+
+        return $tmCaseDecision;
+    }
+
+    /**
+     * @param array $data Array of data
+     */
+    public function update(array $data)
+    {
+        // update common properties
+        $this->setIsMsi($data['isMsi']);
+        $this->setDecisionDate(new \DateTime($data['decisionDate']));
+
+        // each decision may have different update
+        switch($this->getDecision()->getId()) {
+            case self::DECISION_REPUTE_NOT_LOST:
+                $this->updateReputeNotLost($data);
+                break;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array $data Array of data
+     */
+    private function updateReputeNotLost(array $data)
+    {
+        if ($data['notifiedDate'] !== null) {
+            $this->setNotifiedDate(new \DateTime($data['notifiedDate']));
+        }
+
+        if ($data['reputeNotLostReason'] !== null) {
+            $this->setReputeNotLostReason($data['reputeNotLostReason']);
+        }
+    }
 }
--- test/module/Api/src/Entity/Tm/TmCaseDecisionEntityTest.php
+++ test/module/Api/src/Entity/Tm/TmCaseDecisionEntityTest.php
@@ -2,8 +2,11 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Tm;
 
-use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Dvsa\Olcs\Api\Entity\Cases\Cases as CasesEntity;
 use Dvsa\Olcs\Api\Entity\Tm\TmCaseDecision as Entity;
+use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
+use Mockery as m;
 
 /**
  * TmCaseDecision Entity Unit Tests
@@ -18,4 +21,74 @@ class TmCaseDecisionEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    public function testConstruct()
+    {
+        $case = m::mock(CasesEntity::class);
+        $decision = m::mock(RefData::class);
+
+        $entity = new Entity($case, $decision);
+
+        $this->assertSame($case, $entity->getCase());
+        $this->assertSame($decision, $entity->getDecision());
+    }
+
+    public function testCreateForReputeNotLost()
+    {
+        $data = [
+            'case' => 11,
+            'isMsi' => 'Y',
+            'decisionDate' => '2016-01-01',
+            'notifiedDate' => '2016-01-01',
+            'reputeNotLostReason' => 'testing',
+        ];
+
+        $case = m::mock(CasesEntity::class);
+
+        $decision = m::mock(RefData::class)->makePartial();
+        $decision->setId(Entity::DECISION_REPUTE_NOT_LOST);
+
+        $entity = Entity::create($case, $decision, $data);
+
+        $this->assertSame($case, $entity->getCase());
+        $this->assertSame($decision, $entity->getDecision());
+        $this->assertEquals($data['isMsi'], $entity->getIsMsi());
+        $this->assertInstanceOf(\DateTime::class, $entity->getDecisionDate());
+        $this->assertEquals($data['decisionDate'], $entity->getDecisionDate()->format('Y-m-d'));
+        $this->assertInstanceOf(\DateTime::class, $entity->getNotifiedDate());
+        $this->assertEquals($data['notifiedDate'], $entity->getNotifiedDate()->format('Y-m-d'));
+        $this->assertEquals($data['reputeNotLostReason'], $entity->getReputeNotLostReason());
+    }
+
+    public function testUpdateForReputeNotLost()
+    {
+        $data = [
+            'isMsi' => 'Y',
+            'decisionDate' => '2016-01-01',
+            'notifiedDate' => '2016-01-01',
+            'reputeNotLostReason' => 'testing',
+        ];
+
+        $case = m::mock(CasesEntity::class);
+
+        $decision = m::mock(RefData::class)->makePartial();
+        $decision->setId(Entity::DECISION_REPUTE_NOT_LOST);
+
+        $entity = new Entity($case, $decision);
+
+        // set existing data on the entity before update
+        $entity->setIsMsi('N');
+        $entity->setDecisionDate(new \DateTime('2015-10-12'));
+
+        $entity->update($data);
+
+        $this->assertSame($case, $entity->getCase());
+        $this->assertSame($decision, $entity->getDecision());
+        $this->assertEquals($data['isMsi'], $entity->getIsMsi());
+        $this->assertInstanceOf(\DateTime::class, $entity->getDecisionDate());
+        $this->assertEquals($data['decisionDate'], $entity->getDecisionDate()->format('Y-m-d'));
+        $this->assertInstanceOf(\DateTime::class, $entity->getNotifiedDate());
+        $this->assertEquals($data['notifiedDate'], $entity->getNotifiedDate()->format('Y-m-d'));
+        $this->assertEquals($data['reputeNotLostReason'], $entity->getReputeNotLostReason());
+    }
 }
