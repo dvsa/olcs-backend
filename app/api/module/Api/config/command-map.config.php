96fa7d52df22179a0aa2ec73c29c7eb8b62242b6
Piotr Filak <piotr.filak@valtech.co.uk>
2015-08-25 13:43:34 +0100

    OLCS-10344 - stock control buttons

--- app/api/module/Api/config/command-map.config.php
+++ app/api/module/Api/config/command-map.config.php
@@ -248,6 +248,7 @@ return [
     TransferCommand\Irfo\CreateIrfoGvPermit::class => CommandHandler\Irfo\CreateIrfoGvPermit::class,
     TransferCommand\Irfo\UpdateIrfoGvPermit::class => CommandHandler\Irfo\UpdateIrfoGvPermit::class,
     TransferCommand\Irfo\CreateIrfoPermitStock::class => CommandHandler\Irfo\CreateIrfoPermitStock::class,
+    TransferCommand\Irfo\UpdateIrfoPermitStock::class => CommandHandler\Irfo\UpdateIrfoPermitStock::class,
     TransferCommand\Irfo\CreateIrfoPsvAuth::class => CommandHandler\Irfo\CreateIrfoPsvAuth::class,
     TransferCommand\Irfo\UpdateIrfoPsvAuth::class => CommandHandler\Irfo\UpdateIrfoPsvAuth::class,
 
