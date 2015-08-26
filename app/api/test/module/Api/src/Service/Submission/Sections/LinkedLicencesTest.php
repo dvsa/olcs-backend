64200b46d69ed369b37721faf79da16e7a4e18df
Shaun Lizzio <shaun@lizzio.co.uk>
2015-08-26 11:41:15 +0100

    Added UT for TransportManagers

---
+++ app/api/test/module/Api/src/Service/Submission/Sections/LinkedLicencesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Submission\Sections;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class LinkedLicencesTest
+ * @to-do this currently doesnt really test anything because the criteria object on licence::getLinkedLicences
+ * seems to work for the application but not for unit testing.
+ *
+ * @author Shaun Lizzio <shaun@valtech.co.uk>
+ */
+class LinkedLicencesTest extends SubmissionSectionTest
+{
+    protected $submissionSection = '\Dvsa\Olcs\Api\Service\Submission\Sections\LinkedLicences';
+
+    protected $expectedResult = [
+        'data' => [
+            'tables' => [
+                'linked-licences' => [
+
+                ]
+            ]
+        ]
+    ];
+
+    /**
+     * Filter provider
+     *
+     * @return array
+     */
+    public function sectionTestProvider()
+    {
+        $case = $this->getCase();
+
+        return [
+            [$case, $this->expectedResult],
+        ];
+    }
+}
