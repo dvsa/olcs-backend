8b632090793c693c19d1872470741b68fa33acf6
Shaun Lizzio <shaun@lizzio.co.uk>
2015-08-26 10:56:04 +0100

    Added UT for LeadTcArea, CurrentSubmissions, AuthRequested

---
+++ test/module/Api/src/Service/Submission/Sections/LeadTcAreaTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Submission\Sections;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class LeadTcAreaTest
+ * @author Shaun Lizzio <shaun@valtech.co.uk>
+ */
+class LeadTcAreaTest extends SubmissionSectionTest
+{
+    protected $submissionSection = '\Dvsa\Olcs\Api\Service\Submission\Sections\LeadTcArea';
+
+    /**
+     * Filter provider
+     *
+     * @return array
+     */
+    public function sectionTestProvider()
+    {
+        $case = $this->getCase();
+
+        $expectedResult = [
+            'data' => [
+                'text' => 'FOO'
+            ]
+        ];
+
+        return [
+            [$case, $expectedResult],
+        ];
+    }
+}
