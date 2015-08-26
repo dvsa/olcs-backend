93fd90f37a21515effb47dd196584dce5e4ffc34
Shaun Lizzio <shaun@lizzio.co.uk>
2015-08-26 16:24:33 +0100

    Added config and skeleton classes for sections 17-28

---
+++ module/Api/src/Service/Submission/Sections/RegistrationDetails.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Submission\Sections;
+
+use Dvsa\Olcs\Api\Entity\Cases\Cases as CasesEntity;
+
+/**
+ * Class RegistrationDetails
+ * @package Dvsa\Olcs\Api\Service\Submission\Sections
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+final class RegistrationDetails extends AbstractSection
+{
+    public function generateSection(CasesEntity $case)
+    {
+        return ['data' => []];
+    }
+}
