b66168876bec6d6626b37659fe62fc4151916be8
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-09-01 11:21:00 +0100

    Methos to create and update pi hearings

--- module/Api/src/Entity/Pi/PiHearing.php
+++ module/Api/src/Entity/Pi/PiHearing.php
@@ -3,6 +3,10 @@
 namespace Dvsa\Olcs\Api\Entity\Pi;
 
 use Doctrine\ORM\Mapping as ORM;
+use Dvsa\Olcs\Api\Entity\Pi\PresidingTc as PresidingTcEntity;
+use Dvsa\Olcs\Api\Entity\Pi\Pi as PiEntity;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Dvsa\Olcs\Api\Entity\Pi\PiVenue as PiVenueEntity;
 
 /**
  * PiHearing Entity
@@ -24,5 +28,202 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class PiHearing extends AbstractPiHearing
 {
+    /**
+     * @param PiEntity $pi
+     * @param PresidingTcEntity $presidingTc
+     * @param RefData $presidedByRole
+     * @param \DateTime $hearingDate
+     * @param PiVenueEntity|null $piVenue
+     * @param string $piVenueOther
+     * @param int $witnesses
+     * @param string $isCancelled
+     * @param string $cancelledDate
+     * @param string $cancelledReason
+     * @param string $isAdjourned
+     * @param string $adjournedDate
+     * @param string $adjournedReason
+     * @param string $details
+     */
+    public function __construct(
+        PiEntity $pi,
+        PresidingTcEntity $presidingTc,
+        RefData $presidedByRole,
+        \DateTime $hearingDate,
+        $piVenue,
+        $piVenueOther,
+        $witnesses,
+        $isCancelled,
+        $cancelledDate,
+        $cancelledReason,
+        $isAdjourned,
+        $adjournedDate,
+        $adjournedReason,
+        $details
+    ) {
+        $this->create(
+            $pi,
+            $presidingTc,
+            $presidedByRole,
+            $hearingDate,
+            $piVenue,
+            $piVenueOther,
+            $witnesses,
+            $isCancelled,
+            $cancelledDate,
+            $cancelledReason,
+            $isAdjourned,
+            $adjournedDate,
+            $adjournedReason,
+            $details
+        );
+    }
 
+    /**
+     * @param Pi $pi
+     * @param PresidingTc $presidingTc
+     * @param RefData $presidedByRole
+     * @param \DateTime $hearingDate
+     * @param PiVenueEntity|null $piVenue
+     * @param string $piVenueOther
+     * @param int $witnesses
+     * @param string $isCancelled
+     * @param string $cancelledDate
+     * @param string $cancelledReason
+     * @param string $isAdjourned
+     * @param string $adjournedDate
+     * @param string $adjournedReason
+     * @param string $details
+     */
+    private function create(
+        PiEntity $pi,
+        PresidingTcEntity $presidingTc,
+        RefData $presidedByRole,
+        \DateTime $hearingDate,
+        $piVenue,
+        $piVenueOther,
+        $witnesses,
+        $isCancelled,
+        $cancelledDate,
+        $cancelledReason,
+        $isAdjourned,
+        $adjournedDate,
+        $adjournedReason,
+        $details
+    ) {
+        $this->pi = $pi;
+        $this->presidingTc = $presidingTc;
+        $this->presidedByRole = $presidedByRole;
+        $this->piVenue = $piVenue;
+        $this->piVenueOther = $piVenueOther;
+        $this->hearingDate = $hearingDate;
+        $this->witnesses = $witnesses;
+        $this->details = $details;
+        $this->isCancelled = $isCancelled;
+        $this->isAdjourned = $isAdjourned;
+
+        $this->processAdjournedAndCancelled(
+            $isCancelled,
+            $cancelledReason,
+            $cancelledDate,
+            $isAdjourned,
+            $adjournedReason,
+            $adjournedDate
+        );
+    }
+
+    /**
+     * @param PresidingTc $presidingTc
+     * @param RefData $presidedByRole
+     * @param \DateTime $hearingDate
+     * @param PiVenueEntity|null $piVenue
+     * @param string $piVenueOther
+     * @param int $witnesses
+     * @param string $isCancelled
+     * @param string $cancelledDate
+     * @param string $cancelledReason
+     * @param string $isAdjourned
+     * @param string $adjournedDate
+     * @param string $adjournedReason
+     * @param string $details
+     */
+    public function update(
+        PresidingTcEntity $presidingTc,
+        RefData $presidedByRole,
+        \DateTime $hearingDate,
+        $piVenue,
+        $piVenueOther,
+        $witnesses,
+        $isCancelled,
+        $cancelledDate,
+        $cancelledReason,
+        $isAdjourned,
+        $adjournedDate,
+        $adjournedReason,
+        $details
+    ) {
+        $this->presidingTc = $presidingTc;
+        $this->presidedByRole = $presidedByRole;
+        $this->piVenue = $piVenue;
+        $this->piVenueOther = $piVenueOther;
+        $this->hearingDate = $hearingDate;
+        $this->witnesses = $witnesses;
+        $this->details = $details;
+        $this->isCancelled = $isCancelled;
+        $this->isAdjourned = $isAdjourned;
+
+        $this->processAdjournedAndCancelled(
+            $isCancelled,
+            $cancelledReason,
+            $cancelledDate,
+            $isAdjourned,
+            $adjournedReason,
+            $adjournedDate
+        );
+    }
+
+    private function processAdjournedAndCancelled(
+        $isCancelled,
+        $cancelledReason,
+        $cancelledDate,
+        $isAdjourned,
+        $adjournedReason,
+        $adjournedDate
+    ) {
+        if ($isCancelled == 'Y') {
+            $this->cancelledDate = $this->processDate($cancelledDate);
+            $this->cancelledReason = $cancelledReason;
+        } else {
+            $this->cancelledReason = null;
+            $this->cancelledDate = null;
+        }
+
+        if ($isAdjourned == 'Y') {
+            $this->adjournedDate = $this->processDate($adjournedDate, 'Y-m-d H:i:s', false);
+            $this->adjournedReason = $adjournedReason;
+        } else {
+            $this->adjournedReason = null;
+            $this->adjournedDate = null;
+        }
+    }
+
+    /**
+     * @param string $date
+     * @param string $format
+     * @param bool $zeroTime
+     * @return \DateTime|null
+     */
+    private function processDate($date, $format = 'Y-m-d', $zeroTime = true)
+    {
+        $dateTime = \DateTime::createFromFormat($format, $date);
+
+        if (!$dateTime instanceof \DateTime) {
+            return null;
+        }
+
+        if ($zeroTime) {
+            $dateTime->setTime(0, 0, 0);
+        }
+
+        return $dateTime;
+    }
 }
