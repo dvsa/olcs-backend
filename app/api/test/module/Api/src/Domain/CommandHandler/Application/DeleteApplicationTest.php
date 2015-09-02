7f7a13bca02c369f6cfb84729bdc2627d0a1e675
Mat Evans <mat.evans@valtech.co.uk>
2015-09-02 11:34:50 +0100

    OLCS-9042 Refuse, delete applications when licence is revoked or suspended

---
+++ app/api/test/module/Api/src/Domain/CommandHandler/Application/DeleteApplicationTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Application;
+
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\Command\Application\DeleteApplication as Command;
+use Dvsa\Olcs\Api\Domain\CommandHandler\Application\DeleteApplication as CommandHandler;
+use Dvsa\Olcs\Api\Entity\Application\Application;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Mockery as m;
+
+/**
+ * DeleteApplicationTest
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+class DeleteApplicationTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new CommandHandler();
+        $this->mockRepo('Application', \Dvsa\Olcs\Api\Domain\Repository\Application::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        parent::initReferences();
+    }
+
+    public function testHandleCommandNotVariation()
+    {
+        $data = [
+            'id' => 52,
+        ];
+        $command = Command::create($data);
+
+        $application = m::mock(Application::class)->makePartial();
+        $application->setIsVariation(false);
+
+        $this->repoMap['Application']->shouldReceive('fetchUsingId')->with($command)->once()->andReturn($application);
+
+        $this->setExpectedException(\Dvsa\Olcs\Api\Domain\Exception\BadRequestException::class);
+
+        $this->sut->handleCommand($command);
+    }
+
+    /**
+     * @dataProvider dpTestHandleCommandWrongStatus
+     */
+    public function testHandleCommandWrongStatus($status)
+    {
+        $data = [
+            'id' => 52,
+        ];
+        $command = Command::create($data);
+
+        $application = m::mock(Application::class)->makePartial();
+        $application->setIsVariation(true);
+        $application->setStatus((new RefData())->setId($status));
+
+        $this->repoMap['Application']->shouldReceive('fetchUsingId')->with($command)->once()->andReturn($application);
+
+        $this->setExpectedException(\Dvsa\Olcs\Api\Domain\Exception\BadRequestException::class);
+
+        $this->sut->handleCommand($command);
+    }
+
+    public function dpTestHandleCommandWrongStatus()
+    {
+        return [
+            [Application::APPLICATION_STATUS_CURTAILED],
+            [Application::APPLICATION_STATUS_GRANTED],
+            [Application::APPLICATION_STATUS_NOT_TAKEN_UP],
+            [Application::APPLICATION_STATUS_REFUSED],
+            [Application::APPLICATION_STATUS_UNDER_CONSIDERATION],
+            [Application::APPLICATION_STATUS_VALID],
+            [Application::APPLICATION_STATUS_WITHDRAWN],
+        ];
+    }
+    public function testHandleCommand()
+    {
+        $data = [
+            'id' => 52,
+        ];
+        $command = Command::create($data);
+
+        $application = m::mock(Application::class)->makePartial();
+        $application->setId($data['id']);
+        $application->setIsVariation(true);
+        $application->setStatus((new RefData())->setId(Application::APPLICATION_STATUS_NOT_SUBMITTED));
+
+        $this->repoMap['Application']->shouldReceive('fetchUsingId')->with($command)->once()->andReturn($application);
+
+        $this->repoMap['Application']->shouldReceive('delete')->with($application)->once();
+
+        $response = $this->sut->handleCommand($command);
+
+        $this->assertSame(
+            [
+                'id' => [],
+                'messages' => [
+                    'Application 52 deleted.'
+                ]
+            ],
+            $response->toArray()
+        );
+    }
+}
