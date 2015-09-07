113801057c829e11738959810f7b4687d344d272
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-09-07 10:38:08 +0100

    Changed impounding delete to work the same as other deletes

--- module/Api/src/Domain/CommandHandler/Cases/Impounding/DeleteImpounding.php
+++ module/Api/src/Domain/CommandHandler/Cases/Impounding/DeleteImpounding.php
@@ -7,34 +7,14 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Impounding;
 
-use Doctrine\ORM\Query;
-use Doctrine\Common\Collections\ArrayCollection;
-use Dvsa\Olcs\Api\Domain\Command\Result;
-use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
-use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Cases\Impounding;
-use Dvsa\Olcs\Transfer\Command\Cases\Impounding\DeleteImpounding as Cmd;
-use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
 
 /**
  * Delete Impounding
  *
  * @author Shaun Lizzio <shaun@lizzio.co.uk>
  */
-final class DeleteImpounding extends AbstractCommandHandler implements TransactionedInterface
+final class DeleteImpounding extends AbstractDeleteCommandHandler
 {
     protected $repoServiceName = 'Impounding';
-
-    public function handleCommand(CommandInterface $command)
-    {
-        $result = new Result();
-
-        $impounding = $this->getRepo()->fetchUsingId($command);
-
-        $this->getRepo()->delete($impounding);
-
-        $result->addMessage('Impounding deleted');
-
-        return $result;
-    }
 }
