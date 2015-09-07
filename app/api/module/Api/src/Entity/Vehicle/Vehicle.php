0192c441a079cc312fbeb552c278822799defc75
Mat Evans <mat.evans@valtech.co.uk>
2015-09-07 11:56:17 +0100

    OLCS-6196 Add index to vehicle.vrm

--- app/api/module/Api/src/Entity/Vehicle/Vehicle.php
+++ app/api/module/Api/src/Entity/Vehicle/Vehicle.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="vehicle",
  *    indexes={
+ *        @ORM\Index(name="ix_vehicle_vrm", columns={"vrm"}),
  *        @ORM\Index(name="ix_vehicle_created_by", columns={"created_by"}),
  *        @ORM\Index(name="ix_vehicle_last_modified_by", columns={"last_modified_by"}),
  *        @ORM\Index(name="ix_vehicle_psv_type", columns={"psv_type"})
