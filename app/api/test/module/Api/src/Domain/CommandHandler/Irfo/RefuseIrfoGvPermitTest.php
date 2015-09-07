10efe2714cd733968849df7c7ebc306868c257f7
Piotr Filak <piotr.filak@valtech.co.uk>
2015-09-07 11:49:47 +0100

    OLCS-10297 - Add Permit (GV) - Withdrawn Action OLCS-10298 - Add Permit (GV) - Refuse Action

---
+++ app/api/test/module/Api/src/Domain/CommandHandler/Irfo/RefuseIrfoGvPermitTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Refuse Irfo Gv Permit Test
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Irfo;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelIrfoGvPermitFees as CancelFeesDto;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\Irfo\RefuseIrfoGvPermit as Sut;
+use Dvsa\Olcs\Api\Domain\Repository\IrfoGvPermit as IrfoGvPermitRepo;
+use Dvsa\Olcs\Api\Entity\Irfo\IrfoGvPermit as IrfoGvPermitEntity;
+use Dvsa\Olcs\Transfer\Command\Irfo\RefuseIrfoGvPermit as Cmd;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Mockery as m;
+
+/**
+ * Refuse Irfo Gv Permit Test
+ */
+class RefuseIrfoGvPermitTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new Sut();
+        $this->mockRepo('IrfoGvPermit', IrfoGvPermitRepo::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        $this->refData = [
+            IrfoGvPermitEntity::STATUS_REFUSED,
+        ];
+
+        parent::initReferences();
+    }
+
+    /**
+     * testHandleCommand
+     */
+    public function testHandleCommand()
+    {
+        $id = 99;
+
+        $command = Cmd::Create(
+            [
+                'id' => $id,
+            ]
+        );
+
+        /** @var IrfoGvPermitEntity $irfoGvPermit */
+        $irfoGvPermit = m::mock(IrfoGvPermitEntity::class);
+        $irfoGvPermit->shouldReceive('refuse')
+            ->once()
+            ->with($this->refData[IrfoGvPermitEntity::STATUS_REFUSED])
+            ->shouldReceive('getId')
+            ->andReturn($id);
+
+        $this->repoMap['IrfoGvPermit']->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($irfoGvPermit)
+            ->shouldReceive('save')
+            ->with(m::type(IrfoGvPermitEntity::class))
+            ->once();
+
+        $this->expectedSideEffect(CancelFeesDto::class, ['id' => $id], new Result());
+
+        $result = $this->sut->handleCommand($command);
+
+        $this->assertInstanceOf(Result::class, $result);
+    }
+}
