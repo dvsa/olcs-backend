262a1205f400e7a03da45921d706a05e2548e92f
Alex P <alex.peshkov@valtech.co.uk>
2015-09-17 10:42:10 +0100

    OLCS-8384 Internal Overview Pages - Registered for selfserve marker

--- app/api/module/Api/src/Domain/QueryHandler/Licence/Overview.php
+++ app/api/module/Api/src/Domain/QueryHandler/Licence/Overview.php
@@ -63,6 +63,7 @@ class Overview extends AbstractQueryHandler
                         'status',
                     ],
                     'leadTcArea',
+                    'organisationUsers'
                 ],
                 'psvDiscs' => [
                     'criteria' => $discCriteria,
