ea949f9723572eb96f945bc20dbcd586edbd77bb
Dan Eggleston <dan@stolenegg.com>
2015-09-25 09:41:49 +0100

    OLCS-10199 align entities - only index renames

--- module/Api/src/Entity/Organisation/AbstractOrganisation.php
+++ module/Api/src/Entity/Organisation/AbstractOrganisation.php
@@ -23,14 +23,14 @@ use Gedmo\Mapping\Annotation as Gedmo;
  *        @ORM\Index(name="ix_organisation_created_by", columns={"created_by"}),
  *        @ORM\Index(name="ix_organisation_last_modified_by", columns={"last_modified_by"}),
  *        @ORM\Index(name="ix_organisation_type", columns={"type"}),
- *        @ORM\Index(name="ix_organisation_cpid", columns={"cpid"}),
  *        @ORM\Index(name="ix_organisation_lead_tc_area_id", columns={"lead_tc_area_id"}),
  *        @ORM\Index(name="ix_organisation_name", columns={"name"}),
  *        @ORM\Index(name="ix_organisation_contact_details_id", columns={"contact_details_id"}),
  *        @ORM\Index(name="ix_organisation_irfo_contact_details_id",
      *     columns={"irfo_contact_details_id"}),
  *        @ORM\Index(name="ix_organisation_irfo_nationality", columns={"irfo_nationality"}),
- *        @ORM\Index(name="ix_organisation_cpid_name", columns={"cpid","name"})
+ *        @ORM\Index(name="ix_organisation_cpid_name", columns={"cpid","name"}),
+ *        @ORM\Index(name="IDX_E6E132B4A2C69A0E", columns={"cpid"})
  *    }
  * )
  */
