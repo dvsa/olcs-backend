ce8ba871cf4486f0a13133f487494e114e8db312
Rob Caiger <rob@clocal.co.uk>
2015-09-25 13:12:52 +0100

    Altered align script

--- app/api/module/Api/src/Entity/Fee/FeeTransaction.php
+++ app/api/module/Api/src/Entity/Fee/FeeTransaction.php
@@ -13,13 +13,13 @@ use Doctrine\ORM\Mapping as ORM;
  *        @ORM\Index(name="ix_fee_transaction_transaction_id", columns={"transaction_id"}),
  *        @ORM\Index(name="ix_fee_transaction_fee_id", columns={"fee_id"}),
  *        @ORM\Index(name="ix_fee_transaction_reversed_fee_transaction_id",
-     *     columns={"reversed_fee_transaction_id"}),
+ *     columns={"reversed_fee_transaction_id"}),
  *        @ORM\Index(name="ix_fee_transaction_created_by", columns={"created_by"}),
  *        @ORM\Index(name="ix_fee_transaction_last_modified_by", columns={"last_modified_by"})
  *    },
  *    uniqueConstraints={
  *        @ORM\UniqueConstraint(name="uk_fee_transaction_fee_id_transaction_id",
-     *     columns={"fee_id","transaction_id"})
+ *     columns={"fee_id","transaction_id"})
  *    }
  * )
  */
