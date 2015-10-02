2c102430d5e844d637b025e2fb2391d6c55f4b5b
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-10-02 14:11:37 +0100

    renamed publication "create" to CreateNextPublication and simplified so that only an id is needed

---
+++ app/api/module/Api/src/Domain/Command/Publication/CreateNextPublication.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Create a publication based on the id of the previous publication
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Publication;
+
+use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
+
+/**
+ * Create a publication based on the id of the previous publication
+ */
+final class CreateNextPublication extends AbstractIdOnlyCommand
+{
+    //
+}
