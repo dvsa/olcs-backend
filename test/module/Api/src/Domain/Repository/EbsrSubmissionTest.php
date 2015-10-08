928ba509819db77a89df42088c6d0ea7217394f2
Shaun Lizzio <shaun@lizzio.co.uk>
2015-10-08 12:17:22 +0100

    Removed filters as no longer needed

--- test/module/Api/src/Domain/Repository/EbsrSubmissionTest.php
+++ test/module/Api/src/Domain/Repository/EbsrSubmissionTest.php
@@ -36,38 +36,4 @@ class EbsrSubmissionTest extends RepositoryTestCase
         $expectedQuery = 'BLAH AND m.organisation = [[ORG1]]';
         $this->assertEquals($expectedQuery, $this->query);
     }
-
-    /**
-     * Tests appliying list filters
-     */
-    public function testApplyListFilters()
-    {
-        $sut = m::mock(Repo::class)->makePartial()->shouldAllowMockingProtectedMethods();
-
-        $submissionType = 'submission type';
-        $submissionStatus = 'submission status';
-
-        $query = SubmissionListQry::create(
-            ['ebsrSubmissionType' => $submissionType, 'ebsrSubmissionStatus' => $submissionStatus]
-        );
-
-        $mockQb = m::mock(QueryBuilder::class);
-        $mockQb->shouldReceive('expr->eq')
-            ->with('m.ebsrSubmissionType', ':ebsrSubmissionType')
-            ->once()
-            ->andReturnSelf();
-        $mockQb->shouldReceive('andWhere')->once()->andReturnSelf();
-        $mockQb->shouldReceive('setParameter')->with('ebsrSubmissionType', $submissionType)->once()->andReturnSelf();
-        $mockQb->shouldReceive('expr->eq')
-            ->with('m.ebsrSubmissionStatus', ':ebsrSubmissionStatus')
-            ->once()
-            ->andReturnSelf();
-        $mockQb->shouldReceive('andWhere')->once()->andReturnSelf();
-        $mockQb->shouldReceive('setParameter')
-            ->with('ebsrSubmissionStatus', $submissionStatus)
-            ->once()
-            ->andReturnSelf();
-
-        $sut->applyListFilters($mockQb, $query);
-    }
 }
