15dc20f9cba9dba11dcd16ec9b7475ecbcf2ab58
Henry Hayes <henryhayesuk@gmail.com>
2015-10-08 14:32:42 +0100

    Added multiple EBSR templates

--- test/module/Api/src/Domain/CommandHandler/Email/SendEbsrCancelledTest.php
+++ test/module/Api/src/Domain/CommandHandler/Email/SendEbsrCancelledTest.php
@@ -7,17 +7,8 @@
  */
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Email;
 
-use Dvsa\Olcs\Api\Domain\CommandHandler\Email\SendEbsrCancelled as CommandHandler;
-use Dvsa\Olcs\Transfer\Command\Ebsr\SubmissionCreate as SubmissionCreateCommand;
-use Dvsa\Olcs\Api\Domain\Repository\EbsrSubmission as EbsrSubmissionRepo;
-use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
-use Dvsa\Olcs\Api\Entity\System\RefData;
-use Dvsa\Olcs\Email\Service\TemplateRenderer;
-use Dvsa\Olcs\Email\Service\Client as EmailClient;
 use Mockery as m;
-use Dvsa\Olcs\Api\Entity\Ebsr\EbsrSubmission as EbsrSubmissionEntity;
 use Doctrine\ORM\Query;
-use Dvsa\Olcs\Transfer\Query\Ebsr\EbsrSubmission as EbsrSubmissionQuery;
 
 /**
  * Send Ebsr Cancelled Email Test
