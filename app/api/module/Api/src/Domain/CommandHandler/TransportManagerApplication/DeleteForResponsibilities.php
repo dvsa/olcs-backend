891fb11ae8633b3bcb86b27a4a1f1460d95528c9
Rob Caiger <rob@clocal.co.uk>
2015-10-09 16:33:55 +0100

    Update completion when add/delete TM internally

--- app/api/module/Api/src/Domain/CommandHandler/TransportManagerApplication/DeleteForResponsibilities.php
+++ app/api/module/Api/src/Domain/CommandHandler/TransportManagerApplication/DeleteForResponsibilities.php
@@ -7,8 +7,12 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\TransportManagerApplication;
 
+use Dvsa\Olcs\Api\Domain\Command\Application\UpdateApplicationCompletion;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Entity\Application\Application;
 
 /**
  * Delete a Transport Manager Application for TM Responsibilities
@@ -18,4 +22,27 @@ use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 final class DeleteForResponsibilities extends AbstractDeleteCommandHandler implements TransactionedInterface
 {
     protected $repoServiceName = 'TransportManagerApplication';
+
+    protected function doDelete(array $ids)
+    {
+        $applications = [];
+        foreach ($ids as $id) {
+            /** @var TransportManagerApplication $record */
+            $record = $this->getRepo()->fetchById($id);
+            $application = $record->getApplication();
+
+            $applications[$application->getId()] = $application;
+        }
+
+        $result = parent::doDelete($ids);
+
+        /** @var Application $application */
+        foreach ($applications as $application) {
+
+            $completionData = ['id' => $application->getId(),'section' => 'transportManagers'];
+            $result->merge($this->handleSideEffect(UpdateApplicationCompletion::create($completionData)));
+        }
+
+        return $result;
+    }
 }
