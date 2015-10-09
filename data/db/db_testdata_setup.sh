dc71c3095045b86de21dfcba69200c4f95c34f07
Dan Eggleston <dan@stolenegg.com>
2015-10-09 15:50:03 +0100

    OLCS-10722 tidy up rollout data

--- data/db/db_testdata_setup.sh
+++ data/db/db_testdata_setup.sh
@@ -7,6 +7,8 @@ mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs_schema.sql
 mysql -uroot -ppassword olcs_be < Rollout.sql
 
 mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs-refdata.sql
+#grep -v "^CALL\|USE" ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql > test-olcs-refdata.sql
+#mysql -uroot -ppassword olcs_be < test-olcs-refdata.sql
 
 mysql -uroot -ppassword olcs_be < testdata.sql
 
