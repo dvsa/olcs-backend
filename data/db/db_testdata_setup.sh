23104de34f691288aa3252024762b9384abad1d5
Dan Eggleston <dan@stolenegg.com>
2015-10-09 17:07:35 +0100

    OLCS-10722 tweak testdata script

--- data/db/db_testdata_setup.sh
+++ data/db/db_testdata_setup.sh
@@ -10,7 +10,8 @@ mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs_schema.sql
 mysql -uroot -ppassword olcs_be < Rollout.sql
 
 # refdata
-grep -v "^CALL\|^USE" ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql > test-olcs-refdata.sql
+echo "-- this file is auto-generated - DO NOT EDIT!" > test-olcs-refdata.sql
+grep -v "^CALL\|^USE" ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql >> test-olcs-refdata.sql
 mysql -uroot -ppassword olcs_be < test-olcs-refdata.sql
 mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs_conv_category_refdata.sql
 
