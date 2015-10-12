2cebcf3232d8c5f1fef581df0fad12d552cd6d14
Rob Caiger <rob@clocal.co.uk>
2015-10-12 16:24:29 +0100

    unit tests

---
+++ app/api/test/module/Api/src/Domain/QueryHandler/Application/DeclarationUndertakingsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Declaration Undertakings Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Application;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\Application\DeclarationUndertakings;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\Repository\Application as ApplicationRepo;
+use Dvsa\Olcs\Transfer\Query\Application\DeclarationUndertakings as Qry;
+use Mockery as m;
+
+/**
+ * Declaration Undertakings Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class DeclarationUndertakingsTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new DeclarationUndertakings();
+        $this->mockRepo('Application', ApplicationRepo::class);
+
+        $this->mockedSmServices['Review\ApplicationUndertakings'] = m::mock();
+
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = Qry::create(['id' => 111]);
+
+        $mockApplication = m::mock(\Dvsa\Olcs\Api\Entity\Application\Application::class);
+        $mockApplication->shouldReceive('isGoods')->andReturn(true);
+
+        $data = [
+            'foo' => 'bar',
+            'isGoods' => true,
+            'isInternal' => false
+        ];
+
+        $this->mockedSmServices['Review\ApplicationUndertakings']->shouldReceive('getMarkup')
+            ->once()->with($data)->andReturn('markup');
+
+        $this->repoMap['Application']->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn($mockApplication);
+
+        $mockApplication->shouldReceive('serialize')->andReturn(['foo' => 'bar']);
+
+        $expected = [
+            'foo' => 'bar',
+            'undertakings' => 'markup'
+        ];
+
+        $this->assertEquals($expected, $this->sut->handleQuery($query)->serialize());
+    }
+}
