d455a652a9886eaeb80b2ef365c8b5068b3f4342
Dan Eggleston <dan@stolenegg.com>
2015-10-13 13:38:03 +0100

    OLCS-10832 add counts to fee and correspondence query responses

--- app/api/module/Api/src/Domain/QueryHandler/Organisation/OutstandingFees.php
+++ app/api/module/Api/src/Domain/QueryHandler/Organisation/OutstandingFees.php
@@ -20,7 +20,7 @@ class OutstandingFees extends AbstractQueryHandler
 {
     protected $repoServiceName = 'Organisation';
 
-    protected $extraRepos = ['Fee'];
+    protected $extraRepos = ['Fee', 'Correspondence'];
 
     public function handleQuery(QueryInterface $query)
     {
@@ -43,8 +43,18 @@ class OutstandingFees extends AbstractQueryHandler
                             'transaction'
                         ]
                     ]
-                )
+                ),
+                'correspondenceCount' => $this->getCorrespondenceCount($organisation->getId()),
             ]
         );
+    }
+
+    /**
+     * @param int $organisationId
+     * @return int
+     */
+    protected function getCorrespondenceCount($organisationId)
+    {
+        return $this->getRepo('Correspondence')->getUnreadCountForOrganisation($organisationId);
     }
 }
