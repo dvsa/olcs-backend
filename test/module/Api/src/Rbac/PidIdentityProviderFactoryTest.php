a608628f2d87138a6e1474b81c366aefe016540b
Chris Riley <chris.riley@valtech.co.uk>
2015-10-14 09:52:08 +0100

    Added tests for pid identity provider

---
+++ test/module/Api/src/Rbac/PidIdentityProviderFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Rbac;
+
+use Dvsa\Olcs\Api\Domain\Repository\RepositoryInterface;
+use Dvsa\Olcs\Api\Rbac\PidIdentityProvider;
+use Dvsa\Olcs\Api\Rbac\PidIdentityProviderFactory;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+
+class PidIdentityProviderFactoryTest extends MockeryTestCase
+{
+    public function testCreateService()
+    {
+        $mockRequest = m::mock(Request::class);
+        $mockUserRepo = m::mock(RepositoryInterface::class);
+
+        $mockSl = m::mock(ServiceLocatorInterface::class);
+        $mockSl->shouldReceive('get')->with('RepositoryServiceManager')->andReturnSelf();
+        $mockSl->shouldReceive('get')->with('Request')->andReturn($mockRequest);
+        $mockSl->shouldReceive('get')->with('User')->andReturn($mockUserRepo);
+        $mockSl->shouldReceive('get')->with('Config')->andReturn(['openam' => ['pid_header' => 'X-Pid']]);
+
+        $sut = new PidIdentityProviderFactory();
+
+        $service = $sut->createService($mockSl);
+
+        $this->assertInstanceOf(PidIdentityProvider::class, $service);
+    }
+}
