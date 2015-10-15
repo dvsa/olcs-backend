a91e37558e74a461e2768b799bd0aed98f690b32
Dan Eggleston <dan@stolenegg.com>
2015-10-15 15:08:01 +0100

    fix syntax error in Rollout.sql

--- data/db/Rollout.sql
+++ data/db/Rollout.sql
@@ -4972,7 +4972,7 @@ INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES
     (31, 34), -- local-authority-admin = can-manage-user-selfserve
 
     (23, 35), -- internal-case-worker = can-manage-user-internal
-    (24, 35); -- internal-admin = can-manage-user-internal
+    (24, 35), -- internal-admin = can-manage-user-internal
 
     (5, 36), -- operator-admin = can-read-user-selfserve
     (6, 36); -- operator-user = can-read-user-selfserve
