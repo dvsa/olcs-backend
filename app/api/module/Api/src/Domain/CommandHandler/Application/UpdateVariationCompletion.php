78aaafc0b1dc8aa5085d56f5c5b5a7c013fa3c3b
Alex P <alex.peshkov@valtech.co.uk>
2015-10-15 11:49:54 +0100

    OLCS-10737 unable to grant variation

--- app/api/module/Api/src/Domain/CommandHandler/Application/UpdateVariationCompletion.php
+++ app/api/module/Api/src/Domain/CommandHandler/Application/UpdateVariationCompletion.php
@@ -16,6 +16,8 @@ use Dvsa\Olcs\Api\Entity\Application\Application;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
 use Zend\Filter\Word\CamelCaseToUnderscore;
 use Zend\Filter\Word\UnderscoreToCamelCase;
+use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
+use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 
 /**
  * Update Variation Completion
@@ -25,8 +27,12 @@ use Zend\Filter\Word\UnderscoreToCamelCase;
  *
  * @author Rob Caiger <rob@clocal.co.uk>
  */
-final class UpdateVariationCompletion extends AbstractCommandHandler implements TransactionedInterface
+final class UpdateVariationCompletion extends AbstractCommandHandler implements
+    TransactionedInterface,
+    AuthAwareInterface
 {
+    use AuthAwareTrait;
+
     const STATUS_UNCHANGED = Application::VARIATION_STATUS_UNCHANGED;
     const STATUS_UPDATED = Application::VARIATION_STATUS_UPDATED;
     const STATUS_REQUIRES_ATTENTION = Application::VARIATION_STATUS_REQUIRES_ATTENTION;
@@ -494,7 +500,9 @@ final class UpdateVariationCompletion extends AbstractCommandHandler implements 
     {
         $this->application->setDeclarationConfirmation('N');
 
-        $this->markSectionRequired('undertakings');
+        if (!$this->isInternalUser()) {
+            $this->markSectionRequired('undertakings');
+        }
     }
 
     /**
