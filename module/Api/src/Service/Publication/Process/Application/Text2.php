09da6c08ad961e7dc256583a8f2a5f471de18838
Mat Evans <mat.evans@valtech.co.uk>
2015-10-16 11:40:15 +0100

    OLCS-10740 Build up the publication text for new applications and licences

--- module/Api/src/Service/Publication/Process/Application/Text2.php
+++ module/Api/src/Service/Publication/Process/Application/Text2.php
@@ -4,119 +4,28 @@ namespace Dvsa\Olcs\Api\Service\Publication\Process\Application;
 
 use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
 use Dvsa\Olcs\Api\Service\Publication\ImmutableArrayObject;
-use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
-use Dvsa\Olcs\Api\Entity\Publication\PublicationSection as PublicationSectionEntity;
 use Dvsa\Olcs\Api\Service\Publication\Process\ProcessInterface;
-use Dvsa\Olcs\Api\Service\Publication\Process\Text1 as AbstractText1;
+use Dvsa\Olcs\Api\Service\Publication\Formatter;
 
 /**
  * Class Text2
- * @package Dvsa\Olcs\Api\Service\Publication\Process\Application
+ *
  * @author Ian Lindsay <ian@hemera-business-services.co.uk>
  */
-final class Text2 extends AbstractText1 implements ProcessInterface
+final class Text2 implements ProcessInterface
 {
     /**
-     * @param PublicationLink $publication
+     * @param PublicationLink $publicationLink
      * @param ImmutableArrayObject $context
-     * @return PublicationLink
      */
-    public function process(PublicationLink $publication, ImmutableArrayObject $context)
+    public function process(PublicationLink $publicationLink, ImmutableArrayObject $context)
     {
-        $licType = $publication->getApplication()->getGoodsOrPsv()->getId();
-        $publicationSection = $publication->getPublicationSection()->getId();
+        $organisation = $publicationLink->getApplication()->getLicence()->getOrganisation();
 
         $text = [];
+        $text[] = Formatter\OrganisationName::format($organisation);
+        $text[] = Formatter\People::format($organisation, $context->offsetGet('applicationPeople'));
 
-        //licence cancellation
-        if ($context->offsetExists('licenceCancelled')) {
-            //PSV licence cancellation
-            if ($licType == LicenceEntity::LICENCE_CATEGORY_PSV) {
-                $text = $this->getPsvCancelled($publication, $context, $text);
-            } else {
-                $text = $this->getGvCancelled($publication, $context, $text);
-            }
-        } elseif ($licType == LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE) {
-            //non cancellation GV
-            switch ($publicationSection) {
-                case PublicationSectionEntity::APP_GRANTED_SECTION:
-                    $text = $this->getAllData($publication, $text);
-                    break;
-                case PublicationSectionEntity::APP_WITHDRAWN_SECTION:
-                    $text[] = $this->getLicenceInfo($publication->getLicence());
-                    break;
-                case PublicationSectionEntity::APP_REFUSED_SECTION:
-                default:
-                    $text = $this->getAllData($publication, $text);
-                    break;
-            }
-        } else {
-            //non cancellation PSV
-            $text = $this->getAllData($publication, $text);
-        }
-
-        $publication->setText2(implode("\n", $text));
-
-        return $publication;
-    }
-
-    /**
-     * @param PublicationLink $publication
-     * @param array $text
-     * @return array
-     */
-    public function getAllData(PublicationLink $publication, $text)
-    {
-        $text[] = $this->getLicenceInfo($publication->getLicence());
-        $text[] = $this->getPersonInfo($publication->getLicence()->getOrganisation());
-
-        return $text;
-    }
-
-    /**
-     * @param PublicationLink $publication
-     * @param ImmutableArrayObject $context
-     * @param array $text
-     * @return array
-     */
-    public function getGvCancelled(PublicationLink $publication, ImmutableArrayObject $context, $text)
-    {
-        $text[] = $context->offsetGet('licenceCancelled');
-        $text[] = $this->getLicenceInfo($publication->getLicence()) . "\n";
-
-        return $text;
-    }
-
-    /**
-     * @param PublicationLink $publication
-     * @param ImmutableArrayObject $context
-     * @param array $text
-     * @return array
-     */
-    public function getPsvCancelled(PublicationLink $publication, ImmutableArrayObject $context, $text)
-    {
-        $text = $this->getGvCancelled($publication, $context, $text);
-        $text[] = $this->getPersonInfo($publication->getLicence()->getOrganisation());
-
-        return $text;
-    }
-
-    /**
-     * @param LicenceEntity $licence
-     * @return string
-     */
-    public function getLicenceInfo(LicenceEntity $licence)
-    {
-        $organisation = $licence->getOrganisation();
-        $tradingNames = $organisation->getTradingNames();
-
-        $text = $organisation->getName();
-
-        if (!$tradingNames->isEmpty()) {
-            $latestTradingName = $tradingNames->last();
-            $text .= " " . sprintf($this->tradingAs, $latestTradingName->getName());
-        }
-
-        return strtoupper($text);
+        $publicationLink->setText2(implode("\n", $text));
     }
 }
