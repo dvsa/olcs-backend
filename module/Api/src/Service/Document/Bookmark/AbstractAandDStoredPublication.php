b98b629bf8977c8ce6d73abef5f6836bc66709b4
Alex P <alex.peshkov@valtech.co.uk>
2015-10-16 15:55:06 +0100

    OLCS-8025 A&D and N&P Bookmarks

---
+++ module/Api/src/Service/Document/Bookmark/AbstractAandDStoredPublication.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Abstract AandDStoredPublication class
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\Base\DynamicBookmark;
+use Dvsa\Olcs\Api\Domain\Query\Bookmark\ApplicationBundle as Qry;
+use  Dvsa\Olcs\Api\Entity\Publication\PublicationSection;
+
+/**
+ * Abstract AandDStoredPublication class
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+abstract class AbstractAandDStoredPublication extends DynamicBookmark
+{
+    protected $allowedSections = [
+        PublicationSection::APP_NEW_SECTION,
+        PublicationSection::VAR_NEW_SECTION,
+        PublicationSection::SCHEDULE_4_NEW,
+        PublicationSection::SCHEDULE_1_NI_NEW,
+        PublicationSection::SCHEDULE_1_NI_UNTRUE
+    ];
+
+    public function getQuery(array $data)
+    {
+        $bundle = [
+            'publicationLinks' => [
+                'publication',
+                'publicationSection'
+            ]
+        ];
+        return Qry::create(['id' => $data['application'], 'bundle' => $bundle]);
+    }
+
+    protected function sortPublicationLinks($publicationLinks)
+    {
+        usort(
+            $publicationLinks,
+            function ($a, $b) {
+                if ($a['publication']['pubDate'] == $b['publication']['pubDate']) {
+                    if ($a['publication']['id'] > $b['publication']['id']) {
+                        return -1;
+                    } elseif ($a['publication']['id'] < $b['publication']['id']) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                } elseif ($a['publication']['pubDate'] > $b['publication']['pubDate']) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        );
+        return $publicationLinks;
+    }
+
+    protected function filterPublicationLinks($publicationLinks)
+    {
+        $filteredPublicationLinks = [];
+
+        foreach($publicationLinks as $publicationLink) {
+            if (in_array($publicationLink['publicationSection']['id'], $this->allowedSections)) {
+                $filteredPublicationLinks[] = $publicationLink;
+            }
+        }
+        return $filteredPublicationLinks;
+    }
+}
---
+++ test/module/Api/src/Service/Document/Bookmark/AandDStoredPublicationDateTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace Dvsa\OlcsTest\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\AandDStoredPublicationDate;
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * AandDStoredPublicationDate bookmark test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class AandDStoredPublicationDateTest extends MockeryTestCase
+{
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new AandDStoredPublicationDate();
+    }
+
+    public function testGetQuery()
+    {
+        $query = $this->sut->getQuery(['application' => 1]);
+        $this->assertInstanceOf(\Dvsa\Olcs\Transfer\Query\QueryInterface::class, $query);
+    }
+
+    /**
+     * @dataProvider publicationsProvider
+     */
+    public function testRender($data, $result)
+    {
+        $this->sut->setData($data);
+        $this->assertEquals($result, $this->sut->render());
+    }
+
+    public function publicationsProvider()
+    {
+        return [
+            [
+                [
+                    'publicationLinks' => [
+                        [
+                            'publication' => [
+                                'pubDate' => '2015-10-31',
+                                'id' => 2,
+                                'publicationNo' => '4'
+                            ],
+                            'publicationSection' => [
+                                'id' => 1
+                            ]
+                        ],
+                    ],
+                ],
+                '31/10/2015'
+            ],
+            [
+                ['publicationLinks' => []],
+                ''
+            ]
+
+        ];
+    }
+
+}
