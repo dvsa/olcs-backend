0f3694542c9a04a2641c3e2c3d2b65ba71b26dce
Mat Evans <mat.evans@valtech.co.uk>
2015-10-22 12:00:50 +0100

    OLCS-6206 Email to Operator after TM fills in digital TM Form

--- app/api/module/Api/src/Domain/CommandHandler/TransportManagerApplication/Submit.php
+++ app/api/module/Api/src/Domain/CommandHandler/TransportManagerApplication/Submit.php
@@ -7,14 +7,18 @@ use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Doctrine\ORM\Query;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication;
+use Dvsa\Olcs\Api\Domain\EmailAwareInterface;
+use Dvsa\Olcs\Api\Domain\EmailAwareTrait;
 
 /**
  * Submit
  *
  * @author Mat Evans <mat.evans@valtech.co.uk>
  */
-final class Submit extends AbstractCommandHandler implements TransactionedInterface
+final class Submit extends AbstractCommandHandler implements TransactionedInterface, EmailAwareInterface
 {
+    use EmailAwareTrait;
+
     protected $repoServiceName = 'TransportManagerApplication';
 
     public function handleCommand(CommandInterface $command)
@@ -32,8 +36,51 @@ final class Submit extends AbstractCommandHandler implements TransactionedInterf
         $tma->setTmApplicationStatus($this->getRepo()->getRefdataReference($nextStatus));
         $this->getRepo()->save($tma);
 
+        if ($nextStatus === TransportManagerApplication::STATUS_TM_SIGNED) {
+            $this->sendSubmittedEmail($tma);
+        }
+
         $this->result->addMessage("Transport Manager Application ID {$tma->getId()} submitted");
 
         return $this->result;
+    }
+
+    /**
+     * Send an email to the Organisations admins and TMA creator
+     *
+     * @param TransportManagerApplication $tma
+     */
+    private function sendSubmittedEmail(TransportManagerApplication $tma)
+    {
+        // get all org admin email addresss
+        $toEmailAddresses = $tma->getApplication()->getLicence()->getOrganisation()->getAdminEmailAddresses();
+
+        // Created by, might not have a value
+        if ($tma->getCreatedBy()) {
+            // check if the TMA creator email address is alreay in the "to list", if not then add it
+            $tmaCreatorEmailAddress = $tma->getCreatedBy()->getContactDetails()->getEmailAddress();
+            if (array_search($tmaCreatorEmailAddress, $toEmailAddresses) === false) {
+                $toEmailAddresses[] = $tmaCreatorEmailAddress;
+            }
+        }
+
+        foreach ($toEmailAddresses as $to) {
+            $message = new \Dvsa\Olcs\Email\Data\Message($to, 'email.transport-manager-submitted-form.subject');
+            $message->setTranslateToWelsh($tma->getApplication()->getNiFlag());
+            $this->sendEmailTemplate(
+                $message,
+                'transport-manager-submitted-form',
+                [
+                    'tmFullName' => $tma->getTransportManager()->getHomeCd()->getPerson()->getFullName(),
+                    'licNo' => $tma->getApplication()->getLicence()->getLicNo(),
+                    'applicationId' => $tma->getApplication()->getId(),
+                    'tmaUrl' => sprintf(
+                        'http://selfserve/application/%d/transport-managers/details/%d/',
+                        $tma->getApplication()->getId(),
+                        $tma->getId()
+                    ),
+                ]
+            );
+        }
     }
 }
--- app/api/test/module/Api/src/Domain/CommandHandler/TransportManagerApplication/SubmitTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/TransportManagerApplication/SubmitTest.php
@@ -6,6 +6,9 @@ use Dvsa\Olcs\Api\Domain\CommandHandler\TransportManagerApplication\Submit as Co
 use Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication;
 use Dvsa\Olcs\Transfer\Command\TransportManagerApplication\UpdateStatus as Command;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Email\Service\Client;
+use Dvsa\Olcs\Email\Service\TemplateRenderer;
 
 /**
  * SubmitTest
@@ -24,6 +27,11 @@ class SubmitTest extends CommandHandlerTestCase
             \Dvsa\Olcs\Api\Domain\Repository\TransportManagerApplication::class
         );
 
+        $this->mockedSmServices = [
+            Client::class => m::mock(Client::class),
+            TemplateRenderer::class => m::mock(TemplateRenderer::class),
+        ];
+
         parent::setUp();
     }
 
@@ -41,8 +49,10 @@ class SubmitTest extends CommandHandlerTestCase
     {
         $command = Command::create(['id' => 863, 'version' => 234]);
 
-        $tma = new TransportManagerApplication();
+        $tma = m::mock(TransportManagerApplication::class)->makePartial();
         $tma->setIsOwner('N');
+        $tma->shouldReceive('getApplication->getLicence->getOrganisation->getAdminEmailAddresses')->with()->once()
+            ->andReturn([]);
 
         $this->repoMap['TransportManagerApplication']->shouldReceive('fetchUsingId')->once()
             ->with($command, \Doctrine\ORM\Query::HYDRATE_OBJECT, 234)->andReturn($tma);
@@ -75,6 +85,108 @@ class SubmitTest extends CommandHandlerTestCase
                 );
             }
         );
+
+        $this->sut->handleCommand($command);
+    }
+
+    public function testHandleCommandSendEmailToAdminsRemoveDuplicate()
+    {
+        $command = Command::create(['id' => 863]);
+
+        $creatorContactDetails = m::mock();
+        $creatorContactDetails->shouldReceive('getContactDetails->getEmailAddress')->with()->once()
+            ->andReturn('email1');
+
+        $tma = m::mock(TransportManagerApplication::class)->makePartial();
+        $tma->setId(12);
+        $tma->shouldReceive('getCreatedBy')->with()->twice()->andReturn($creatorContactDetails);
+        $tma->shouldReceive('getApplication->getLicence->getOrganisation->getAdminEmailAddresses')->with()->once()
+            ->andReturn(['email1']);
+
+        $tma->shouldReceive('getApplication->getNiFlag')->with()->once()->andReturn('Y');
+        $tma->shouldReceive('getTransportManager->getHomeCd->getPerson->getFullName')->with()->once()
+            ->andReturn('Bob Smith');
+        $tma->shouldReceive('getApplication->getLicence->getLicNo')->with()->once()->andReturn('LIC01');
+        $tma->shouldReceive('getApplication->getId')->with()->twice()->andReturn(76);
+
+        $this->repoMap['TransportManagerApplication']->shouldReceive('fetchUsingId')->once()
+            ->with($command)->andReturn($tma);
+        $this->repoMap['TransportManagerApplication']->shouldReceive('save')->once();
+
+        $this->mockedSmServices[TemplateRenderer::class]->shouldReceive('renderBody')->once()->andReturnUsing(
+            function (\Dvsa\Olcs\Email\Data\Message $message, $template, $vars, $layout) {
+
+                $this->assertSame('email.transport-manager-submitted-form.subject', $message->getSubject());
+                $this->assertSame('email1', $message->getTo());
+                $this->assertSame('cy_GB', $message->getLocale());
+
+                $this->assertSame('transport-manager-submitted-form', $template);
+                $this->assertSame(
+                    [
+                        'tmFullName' => 'Bob Smith',
+                        'licNo' => 'LIC01',
+                        'applicationId' => 76,
+                        'tmaUrl' => 'http://selfserve/application/76/transport-managers/details/12/'
+                    ],
+                    $vars
+                );
+                $this->assertNull($layout);
+
+            }
+        );
+        $this->mockedSmServices[Client::class]->shouldReceive('sendEmail')
+            ->with(m::type(\Dvsa\Olcs\Email\Data\Message::class))->once();
+
+        $this->sut->handleCommand($command);
+    }
+
+    public function testHandleCommandSendEmailToCreator()
+    {
+        $command = Command::create(['id' => 863]);
+
+        $creatorContactDetails = m::mock();
+        $creatorContactDetails->shouldReceive('getContactDetails->getEmailAddress')->with()->once()
+            ->andReturn('email1');
+
+        $tma = m::mock(TransportManagerApplication::class)->makePartial();
+        $tma->setId(12);
+        $tma->shouldReceive('getCreatedBy')->with()->twice()->andReturn($creatorContactDetails);
+        $tma->shouldReceive('getApplication->getLicence->getOrganisation->getAdminEmailAddresses')->with()->once()
+            ->andReturn([]);
+
+        $tma->shouldReceive('getApplication->getNiFlag')->with()->once()->andReturn('Y');
+        $tma->shouldReceive('getTransportManager->getHomeCd->getPerson->getFullName')->with()->once()
+            ->andReturn('Bob Smith');
+        $tma->shouldReceive('getApplication->getLicence->getLicNo')->with()->once()->andReturn('LIC01');
+        $tma->shouldReceive('getApplication->getId')->with()->twice()->andReturn(76);
+
+        $this->repoMap['TransportManagerApplication']->shouldReceive('fetchUsingId')->once()
+            ->with($command)->andReturn($tma);
+        $this->repoMap['TransportManagerApplication']->shouldReceive('save')->once();
+
+        $this->mockedSmServices[TemplateRenderer::class]->shouldReceive('renderBody')->once()->andReturnUsing(
+            function (\Dvsa\Olcs\Email\Data\Message $message, $template, $vars, $layout) {
+
+                $this->assertSame('email.transport-manager-submitted-form.subject', $message->getSubject());
+                $this->assertSame('email1', $message->getTo());
+                $this->assertSame('cy_GB', $message->getLocale());
+
+                $this->assertSame('transport-manager-submitted-form', $template);
+                $this->assertSame(
+                    [
+                        'tmFullName' => 'Bob Smith',
+                        'licNo' => 'LIC01',
+                        'applicationId' => 76,
+                        'tmaUrl' => 'http://selfserve/application/76/transport-managers/details/12/'
+                    ],
+                    $vars
+                );
+                $this->assertNull($layout);
+
+            }
+        );
+        $this->mockedSmServices[Client::class]->shouldReceive('sendEmail')
+            ->with(m::type(\Dvsa\Olcs\Email\Data\Message::class))->once();
 
         $this->sut->handleCommand($command);
     }
