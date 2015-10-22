5697ab8220eeccee4edc82ce14f6ba5f81eec8b5
Piotr Filak <piotr.filak@valtech.co.uk>
2015-10-22 15:31:05 +0100

    OLCS-10509 - Ext forgotten username

---
+++ app/api/module/Api/src/Domain/CommandHandler/Email/SendUsernameMultiple.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Send Username Multiple Email
+ */
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Email;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+
+/**
+ * Send Username Multiple Email
+ */
+final class SendUsernameMultiple extends AbstractCommandHandler implements \Dvsa\Olcs\Api\Domain\EmailAwareInterface
+{
+    use \Dvsa\Olcs\Api\Domain\EmailAwareTrait;
+
+    protected $repoServiceName = 'Licence';
+
+    /**
+     * @param CommandInterface $command
+     * @return Result
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        /* @var $licence \Dvsa\Olcs\Api\Entity\Licence\Licence */
+        $licence = $this->getRepo()->fetchByLicNo($command->getLicenceNumber());
+
+        $result = new Result();
+
+        foreach ($licence->getOrganisation()->getAdminOrganisationUsers() as $orgUser) {
+            // send email to each and every admin of this organisation
+            // Note: the email contains ADMIN's username (not a username of the person who forgotten it!)
+            $user = $orgUser->getUser();
+
+            $message = new \Dvsa\Olcs\Email\Data\Message(
+                $user->getContactDetails()->getEmailAddress(),
+                'email.user-forgot-username-multiple.subject'
+            );
+            $message->setTranslateToWelsh($licence->getTranslateToWelsh());
+
+            $this->sendEmailTemplate(
+                $message,
+                'user-forgot-username-multiple',
+                [
+                    'loginId' => $user->getLoginId(),
+                    // @NOTE the http://selfserve part gets replaced
+                    'url' => 'http://selfserve/'
+                ]
+            );
+
+            $result->addId('user', $user->getId(), true);
+        }
+
+        $result->addMessage('Username reminder email sent');
+        return $result;
+    }
+}
---
+++ app/api/test/module/Api/src/Domain/CommandHandler/User/RemindUsernameSelfserveTest.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Remind Username Selfserve Test
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\User;
+
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Command\Email\SendUsernameSingle as SendUsernameSingleCmd;
+use Dvsa\Olcs\Api\Domain\Command\Email\SendUsernameMultiple as SendUsernameMultipleCmd;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\User\RemindUsernameSelfserve as Sut;
+use Dvsa\Olcs\Api\Domain\Repository\User as UserRepo;
+use Dvsa\Olcs\Api\Entity\User\User;
+use Dvsa\Olcs\Transfer\Command\User\RemindUsernameSelfserve as Cmd;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+
+/**
+ * Remind Username Selfserve Test
+ */
+class RemindUsernameSelfserveTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new Sut();
+        $this->mockRepo('User', UserRepo::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleCommandSingle()
+    {
+        $data = [
+            'licenceNumber' => 'AB12345678',
+            'emailAddress' => 'test@test.me'
+        ];
+
+        /** @var User $user1 */
+        $user1 = m::mock(User::class)->makePartial();
+        $user1->setId(1);
+
+        $users = [$user1];
+
+        $this->repoMap['User']
+            ->shouldReceive('fetchForRemindUsername')
+            ->once()
+            ->with($data['licenceNumber'], $data['emailAddress'])
+            ->andReturn($users);
+
+        $command = Cmd::create($data);
+
+        $this->expectedSideEffect(
+            SendUsernameSingleCmd::class,
+            [
+                'user' => $user1,
+                'licenceNumber' => $data['licenceNumber']
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'USERNAME_REMINDER_SENT_SINGLE'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    public function testHandleCommandMultiple()
+    {
+        $data = [
+            'licenceNumber' => 'AB12345678',
+            'emailAddress' => 'test@test.me'
+        ];
+
+        /** @var User $user1 */
+        $user1 = m::mock(User::class)->makePartial();
+        $user1->setId(1);
+
+        /** @var User $user2 */
+        $user2 = m::mock(User::class)->makePartial();
+        $user2->setId(2);
+
+        $users = [$user1, $user2];
+
+        $this->repoMap['User']
+            ->shouldReceive('fetchForRemindUsername')
+            ->once()
+            ->with($data['licenceNumber'], $data['emailAddress'])
+            ->andReturn($users);
+
+        $command = Cmd::create($data);
+
+        $this->expectedSideEffect(
+            SendUsernameMultipleCmd::class,
+            [
+                'licenceNumber' => $data['licenceNumber']
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'USERNAME_REMINDER_SENT_MULTIPLE'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    /**
+     * @expectedException \Dvsa\Olcs\Api\Domain\Exception\BadRequestException
+     */
+    public function testHandleCommandThrowsNotFoundException()
+    {
+        $data = [
+            'licenceNumber' => 'AB12345678',
+            'emailAddress' => 'test@test.me'
+        ];
+
+        $users = [];
+
+        $command = Cmd::create($data);
+
+        $this->repoMap['User']
+            ->shouldReceive('fetchForRemindUsername')
+            ->once()
+            ->with($data['licenceNumber'], $data['emailAddress'])
+            ->andReturn($users);
+
+        $this->sut->handleCommand($command);
+    }
+}
