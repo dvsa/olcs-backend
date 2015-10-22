7bf454d1243e4ca2b0a9a5e2177d44faff871fa6
Henry Hayes <henryhayesuk@gmail.com>
2015-10-22 15:34:52 +0100

    Note now gives us app

---
+++ app/api/module/Api/src/Domain/QueryPartial/WithApplication.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * With Application
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryPartial;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * With Application
+ */
+final class WithApplication implements QueryPartialInterface
+{
+    /**
+     * @var With
+     */
+    private $with;
+
+    public function __construct(With $with)
+    {
+        $this->with = $with;
+    }
+
+    /**
+     * Joins on Case relationship
+     *
+     * @param QueryBuilder $qb
+     * @param array $arguments
+     */
+    public function modifyQuery(QueryBuilder $qb, array $arguments = [])
+    {
+        $alias = ((isset($arguments[0]) && isset($arguments[1])) ? $arguments[1] : $qb->getRootAliases()[0]);
+
+        $this->with->modifyQuery($qb, [$alias . '.application', 'app']);
+    }
+}
