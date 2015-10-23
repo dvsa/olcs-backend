6d18dd32c8080203b24005d79f2cf9ede45fafc8
Dan Eggleston <dan@stolenegg.com>
2015-10-23 11:30:16 +0100

    OLCS-10980 align entities following sync with master etl schema

--- module/Api/src/Entity/Cases/CasesReadAudit.php
+++ module/Api/src/Entity/Cases/CasesReadAudit.php
@@ -11,13 +11,13 @@ use Dvsa\Olcs\Api\Entity\User\User;
  * @ORM\Entity
  * @ORM\Table(name="cases_read_audit",
  *    indexes={
- *        @ORM\Index(name="ix_audit_read_cases_cases_id", columns={"cases_id"}),
+ *        @ORM\Index(name="ix_audit_read_cases_cases_id", columns={"case_id"}),
  *        @ORM\Index(name="ix_audit_read_cases_user_id", columns={"user_id"}),
  *        @ORM\Index(name="ix_audit_read_cases_created_on", columns={"created_on"})
  *    },
  *    uniqueConstraints={
- *        @ORM\UniqueConstraint(name="uk_audit_read_cases_cases_id_user_id_created_on",
- *        columns={"cases_id","user_id","created_on"})
+ *        @ORM\UniqueConstraint(name="uk_cases_read_audit_case_id_user_id_created_on",
+ *        columns={"case_id","user_id","created_on"})
  *    }
  * )
  */
@@ -26,6 +26,6 @@ class CasesReadAudit extends AbstractCasesReadAudit
     public function __construct(User $user, Cases $case)
     {
         $this->setUser($user);
-        $this->setCases($case);
+        $this->setCase($case);
     }
 }
