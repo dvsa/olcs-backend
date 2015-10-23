9639b4e17a7b7fcabae80902d4d11f40bffbd1bf
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-10-23 17:59:38 +0100

    Imported the bus reg input filters from ebsr that could be modified / reused

---
+++ module/Api/src/Service/Ebsr/Filter/InjectIsTxcApp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Ebsr\Filter;
+
+use Zend\Filter\AbstractFilter;
+use Zend\Filter\Exception;
+
+/**
+ * Class InjectIsTxcApp
+ * @package Dvsa\Olcs\Api\Service\Ebsr\Filter
+ */
+class InjectIsTxcApp extends AbstractFilter
+{
+    /**
+     * Returns the result of filtering $value
+     *
+     * @param  mixed $value
+     * @throws Exception\RuntimeException If filtering $value is impossible
+     * @return mixed
+     */
+    public function filter($value)
+    {
+        $value['isTxcApp'] = 'Y';
+        $value['ebsrRefresh'] = 0;
+
+        if (isset($value['txcAppType']) && $value['txcAppType'] == 'nonChargeableChange') {
+            $value['ebsrRefresh'] = 1;
+        }
+
+        return $value;
+    }
+}
