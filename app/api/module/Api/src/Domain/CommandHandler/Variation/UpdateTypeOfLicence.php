aba659d394854ef436fbec79699227f09f8bef3a
Dan Eggleston <dan@stolenegg.com>
2015-10-26 16:08:30 +0000

    OLCS-10762 OLCS-11027 update variation fee logic, move some logic from helper service into application entity

--- app/api/module/Api/src/Domain/CommandHandler/Variation/UpdateTypeOfLicence.php
+++ app/api/module/Api/src/Domain/CommandHandler/Variation/UpdateTypeOfLicence.php
@@ -7,19 +7,26 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Variation;
 
+use Doctrine\ORM\Query;
 use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
+use Dvsa\Olcs\Api\Domain\Command\Application\CreateFee as CreateApplicationFeeCmd;
 use Dvsa\Olcs\Api\Domain\Command\Application\UpdateApplicationCompletion;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelFee as CancelFeeCmd;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelFee;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
-use Dvsa\Olcs\Api\Entity\User\Permission;
-use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Doctrine\ORM\Query;
-use Dvsa\Olcs\Api\Entity\Application\Application;
 use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
 use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
+use Dvsa\Olcs\Api\Entity\Application\Application;
+use Dvsa\Olcs\Api\Entity\Fee\Fee;
+use Dvsa\Olcs\Api\Entity\Fee\FeeType;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Dvsa\Olcs\Api\Entity\User\Permission;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+
 
 /**
  * Update Type Of Licence
@@ -70,13 +77,49 @@ final class UpdateTypeOfLicence extends AbstractCommandHandler implements AuthAw
 
         $result->merge($this->updateApplicationCompletion($command->getId()));
 
+        $result->merge($this->handleVariationFees($application));
+
         return $result;
     }
 
     private function updateApplicationCompletion($id)
     {
-        return $this->getCommandHandler()->handleCommand(
+        return $this->handleSideEffect(
             UpdateApplicationCompletion::create(['id' => $id, 'section' => 'typeOfLicence'])
+        );
+    }
+
+    private function handleVariationFees(Application $application)
+    {
+        $result = new Result();
+
+        foreach ($application->getFees() as $fee) {
+            if ($fee->getFeeType()->getFeeType()->getId() === FeeType::FEE_TYPE_VAR) {
+                if ($fee->isFullyOutstanding()) {
+                    // only cancel and recreate variation fee if it is fully outstanding
+                    $result->merge($this->cancelAndRecreateVariationFee($fee, $application));
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Cancel existing fee and create a new one
+     */
+    private function cancelAndRecreateVariationFee(Fee $fee, Application $application)
+    {
+        return $this->handleSideEffects(
+            [
+                CancelFeeCmd::create(['id' => $fee->getId()]),
+                CreateApplicationFeeCmd::create(
+                    [
+                        'id' => $application->getId(),
+                        'feeTypeFeeType' => FeeType::FEE_TYPE_VAR
+                    ]
+                )
+            ]
         );
     }
 }
