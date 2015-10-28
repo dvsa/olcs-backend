1f597ee96f8ad2262c377b66f097cedd86819a1b
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-10-28 16:25:34 +0000

    Test for Via formatter

---
+++ app/api/test/module/Api/src/Service/Ebsr/Filter/Format/ViaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format;
+
+use Dvsa\Olcs\Api\Service\Ebsr\Filter\Format\Via;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class ViaTest
+ * @package Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format
+ */
+class ViaTest extends TestCase
+{
+    /**
+     * @dataProvider provideFilter
+     * @param $expected
+     * @param $value
+     */
+    public function testFilter($expected, $value)
+    {
+        $sut = new Via();
+
+        $result = $sut->filter(['via' => $value]);
+        $this->assertEquals($expected, $result['via']);
+    }
+
+    public function provideFilter()
+    {
+        return [
+            ['via1, via2', ['via1', 'via2']],
+            ['via1', ['via1']],
+            ['via1', 'via1'],
+            [null, null]
+        ];
+    }
+}
