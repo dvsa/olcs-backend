cb62ac1d9dd97573a69a8525dd21edd90bb315c8
Dan Eggleston <dan@stolenegg.com>
2015-10-29 17:38:17 +0000

    OLCS-4735 implement refund command handler, remove unique constraint on fee_transaction

--- app/api/module/Api/src/Domain/CommandHandler/Fee/RefundFee.php
+++ app/api/module/Api/src/Domain/CommandHandler/Fee/RefundFee.php
@@ -10,11 +10,16 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\Fee;
 use Doctrine\ORM\Query;
 use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelFee as CancelFeeCmd;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Api\Domain\CpmsAwareInterface;
 use Dvsa\Olcs\Api\Domain\CpmsAwareTrait;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
+use Dvsa\Olcs\Api\Entity\Fee\Fee as FeeEntity;
+use Dvsa\Olcs\Api\Entity\Fee\FeeTransaction as FeeTransactionEntity;
+use Dvsa\Olcs\Api\Entity\Fee\Transaction as TransactionEntity;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 
 /**
@@ -29,19 +34,51 @@ final class RefundFee extends AbstractCommandHandler implements
 {
     use AuthAwareTrait, CpmsAwareTrait;
 
+    const REFUND_COMMENT = 'Non over payment refund';
+
     protected $repoServiceName = 'Fee';
 
     public function handleCommand(CommandInterface $command)
     {
+        $now = new DateTime();
+
         /** @var FeeEntity $fee */
         $fee = $this->getRepo()->fetchUsingId($command);
 
         $response = $this->getCpmsService()->batchRefund($fee);
+        $reference = 'ref from cpms';
 
         // var_dump($response); exit;
+        $transaction = new TransactionEntity();
+        $transaction
+            ->setType($this->getRepo()->getRefdataReference(TransactionEntity::TYPE_REFUND))
+            ->setStatus($this->getRepo()->getRefdataReference(TransactionEntity::STATUS_COMPLETE))
+            ->setPaymentMethod($this->getRepo()->getRefdataReference(FeeEntity::METHOD_REFUND))
+            ->setComment(self::REFUND_COMMENT)
+            ->setReference($reference)
+            ->setCompletedDate($now)
+            ->setProcessedByUser($this->getCurrentUser());
+
+        foreach ($fee->getFeeTransactionsForRefund() as $originalFt) {
+            $feeTransaction = new FeeTransactionEntity();
+            $feeTransaction
+                ->setFee($fee)
+                ->setTransaction($transaction)
+                ->setAmount($originalFt->getAmount() * -1)
+                ->setReversedFeeTransaction($originalFt);
+            $fee->getFeeTransactions()->add($feeTransaction);
+        }
+
+        // save fee will cascade persist
+        $this->getRepo()->save($fee);
 
         $this->result
-            ->addMessage('Fee refunded');
+            ->addId('transaction', $transaction->getId())
+            ->addMessage('Refund transaction created');
+
+        $this->result->merge(
+            $this->handleSideEffect(CancelFeeCmd::create(['id' => $fee->getId()]))
+        );
 
         return $this->result;
     }
