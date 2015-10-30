7263cad7b798771ac641812c985ef9910497ea73
Dan Eggleston <dan@stolenegg.com>
2015-10-30 11:50:54 +0000

    OLCS-4735 unit test the feeTransaction grouping

--- test/module/Api/src/Domain/QueryHandler/Fee/FeeTest.php
+++ test/module/Api/src/Domain/QueryHandler/Fee/FeeTest.php
@@ -7,10 +7,14 @@
  */
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Fee;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Fee\Fee as QueryHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Api\Domain\Repository\Fee as FeeRepo;
 use Dvsa\Olcs\Api\Entity\Fee\Fee as FeeEntity;
+use Dvsa\Olcs\Api\Entity\Fee\FeeTransaction as FeeTransactionEntity;
+use Dvsa\Olcs\Api\Entity\Fee\Transaction as TransactionEntity;
+use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\Olcs\Transfer\Query\Fee\Fee as Qry;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Mockery as m;
@@ -71,7 +75,20 @@ class FeeTest extends QueryHandlerTestCase
             ->once()
             ->andReturn('TestOutstanding')
             ->shouldReceive('getOutstandingWaiveTransaction')
-            ->andReturn(null);
+            ->andReturn(null)
+            ->shouldReceive('canRefund')
+            ->once()
+            ->andReturn(true);
+
+        $completed = new \DateTime('2015-10-30');
+        $status = new RefData(TransactionEntity::STATUS_COMPLETE);
+        $ft1 = $this->getMockFeeTransaction(5, $completed, $status, '10.00', 'Payment', 'Cash');
+        $ft2 = $this->getMockFeeTransaction(6, $completed, $status, '20.00', 'Payment', 'Cash');
+        $ft3 = $this->getMockFeeTransaction(7, $completed, $status, '-10.00', 'Refund', 'Refund');
+        $ft4 = $this->getMockFeeTransaction(8, $completed, $status, '-20.00', null, null, $ft3->getTransaction());
+        $mockFee
+            ->shouldReceive('getFeeTransactions')
+            ->andReturn(new ArrayCollection([$ft1, $ft2, $ft3, $ft4]));
 
         $result = $this->sut->handleQuery($query);
 
@@ -79,11 +96,7 @@ class FeeTest extends QueryHandlerTestCase
 
         $mockFee
             ->shouldReceive('serialize')
-            ->once()->andReturn(
-                [
-                    'id' => 69,
-                ]
-            );
+            ->once()->andReturn(['id' => 69]);
 
         $expected = [
             'id' => '69',
@@ -97,8 +110,88 @@ class FeeTest extends QueryHandlerTestCase
             'waiveReason' => 'TestWaiveReason',
             'outstanding' => 'TestOutstanding',
             'hasOutstandingWaiveTransaction' => false,
+            'canRefund' => true,
+            'displayTransactions' => [
+                5 => [
+                    'transactionId' => 5,
+                    'type' => 'Payment',
+                    'completedDate' => $completed,
+                    'method' => 'Cash',
+                    'processedBy' => 'someuser',
+                    'amount' => '10.00',
+                    'status' => $status,
+                ],
+                6 => [
+                    'transactionId' => 6,
+                    'type' => 'Payment',
+                    'completedDate' => $completed,
+                    'method' => 'Cash',
+                    'processedBy' => 'someuser',
+                    'amount' => '20.00',
+                    'status' => $status,
+                ],
+                // multiple feeTransaction records with the same transaction id
+                // should group for display, summing the amounts
+                7 => [
+                    'transactionId' => 7,
+                    'type' => 'Refund',
+                    'completedDate' => $completed,
+                    'method' => 'Refund',
+                    'processedBy' => 'someuser',
+                    'amount' => '-30.00',
+                    'status' => $status,
+                ],
+            ],
         ];
 
         $this->assertEquals($expected, $result->serialize());
+    }
+
+    /**
+     * Helper method to get a mock FeeTransaction record
+     *
+     * @param int $id
+     * @param DateTime $completed
+     * @param RefData $status
+     * @param string $amount
+     * @param string $type (description)
+     * @param string $method (description)
+     * @param TransactionEntity $transaction optional transaction reference
+     *
+     * @return Mockery\Mock (FeeTransactionEntity)
+     */
+    private function getMockFeeTransaction($id, $completed, $status, $amount, $type, $method, $transaction = null)
+    {
+        if (is_null($transaction)) {
+            $transaction = m::mock(TransactionEntity::class);
+            $transaction->shouldReceive('getId')
+                ->andReturn($id)
+                ->shouldReceive('isOutstanding')
+                ->andReturn(false)
+                ->shouldReceive('isWaive')
+                ->andReturn(false)
+                ->shouldReceive('getCompletedDate')
+                ->andReturn($completed)
+                ->shouldReceive('getStatus')
+                ->andReturn($status);
+            $transaction
+                ->shouldReceive('getType->getDescription')
+                ->andReturn($type);
+            $transaction
+                ->shouldReceive('getPaymentMethod->getDescription')
+                ->andReturn($method);
+            $transaction
+                ->shouldReceive('getProcessedByUser->getLoginId')
+                ->andReturn('someuser');
+        }
+
+        $feeTransaction = m::mock(FeeTransactionEntity::class)
+            ->shouldReceive('getTransaction')
+            ->andReturn($transaction)
+            ->shouldReceive('getAmount')
+            ->andReturn($amount)
+            ->getMock();
+
+        return $feeTransaction;
     }
 }
