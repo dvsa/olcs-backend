f74e6b3adf091ba14880df237e1311597bacc429
Mat Evans <mat.evans@valtech.co.uk>
2015-11-06 14:46:50 +0000

    OLCS-11118 Fix issue with licence decision popups missing content OLCS-10396 Fix incorrext listing of active bus routes

--- module/Api/src/Domain/QueryHandler/Licence/LicenceDecisions.php
+++ module/Api/src/Domain/QueryHandler/Licence/LicenceDecisions.php
@@ -19,8 +19,28 @@ class LicenceDecisions extends AbstractQueryHandler
 {
     protected $repoServiceName = 'Licence';
 
+    protected $extraRepos = ['BusRegSearchView'];
+
     public function handleQuery(QueryInterface $query)
     {
-        return $this->result($this->getRepo()->fetchUsingId($query));
+        $licence = $this->getRepo()->fetchUsingId($query);
+        /* @var $licence \Dvsa\Olcs\Api\Entity\Licence\Licence */
+
+        $activeBusRoutes = $this->getRepo('BusRegSearchView')->fetchActiveByLicence($licence);
+
+        $decisionCriteria['activeComLics'] = !$licence->getActiveCommunityLicences()->isEmpty();
+        $decisionCriteria['activeBusRoutes'] = count($activeBusRoutes) > 0;
+        $decisionCriteria['activeVariations'] = !$licence->getActiveVariations()->isEmpty();
+
+        $suitableForDecisions = true;
+        if (in_array(true, $decisionCriteria)) {
+            $suitableForDecisions = $decisionCriteria;
+        }
+
+        return $this->result(
+            $this->getRepo()->fetchUsingId($query),
+            [],
+            ['suitableForDecisions' => $suitableForDecisions]
+        );
     }
 }
--- test/module/Api/src/Domain/QueryHandler/Licence/LicenceDecisionsTest.php
+++ test/module/Api/src/Domain/QueryHandler/Licence/LicenceDecisionsTest.php
@@ -12,6 +12,7 @@ use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Dvsa\Olcs\Api\Domain\Repository\Licence as LicenceRepo;
 use Dvsa\Olcs\Transfer\Query\Licence\LicenceDecisions as Qry;
 use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
+use Doctrine\Common\Collections\ArrayCollection;
 use Mockery as m;
 
 /**
@@ -25,23 +26,64 @@ class LicenceDecisionsTest extends QueryHandlerTestCase
     {
         $this->sut = new LicenceDecisions();
         $this->mockRepo('Licence', LicenceRepo::class);
+        $this->mockRepo('BusRegSearchView', \Dvsa\Olcs\Api\Domain\Repository\BusRegSearchView::class);
 
         parent::setUp();
     }
 
-    public function testHandleQuery()
+    public function testHandleQueryDecisionsTrue()
     {
         $query = Qry::create(['licence' => 1]);
 
         $licence = m::mock(LicenceEntity::class)->makePartial();
-        $licence->shouldReceive('serialize')
-            ->andReturn(['foo' => 'bar']);
+        $licence->shouldReceive('getActiveCommunityLicences')->andReturn(new ArrayCollection(['one']));
+        $licence->shouldReceive('getActiveVariations')->andReturn(new ArrayCollection(['one']));
+        $licence->shouldReceive('serialize')->andReturn(['foo' => 'bar']);
 
         $this->repoMap['Licence']->shouldReceive('fetchUsingId')
             ->with($query)
             ->andReturn($licence);
 
+        $this->repoMap['BusRegSearchView']->shouldReceive('fetchActiveByLicence')->with($licence)->once()
+            ->andReturn(new ArrayCollection(['one']));
+
         $result = $this->sut->handleQuery($query);
-        $this->assertEquals(['foo' => 'bar'], $result->serialize());
+        $this->assertEquals(
+            [
+                'foo' => 'bar',
+                'suitableForDecisions' => [
+                    'activeComLics' => true,
+                    'activeBusRoutes' => true,
+                    'activeVariations' => true,
+                ]
+            ],
+            $result->serialize()
+        );
+    }
+
+    public function testHandleQueryDecisionsFalse()
+    {
+        $query = Qry::create(['licence' => 1]);
+
+        $licence = m::mock(LicenceEntity::class)->makePartial();
+        $licence->shouldReceive('getActiveCommunityLicences')->andReturn(new ArrayCollection([]));
+        $licence->shouldReceive('getActiveVariations')->andReturn(new ArrayCollection([]));
+        $licence->shouldReceive('serialize')->andReturn(['foo' => 'bar']);
+
+        $this->repoMap['Licence']->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn($licence);
+
+        $this->repoMap['BusRegSearchView']->shouldReceive('fetchActiveByLicence')->with($licence)->once()
+            ->andReturn(new ArrayCollection([]));
+
+        $result = $this->sut->handleQuery($query);
+        $this->assertEquals(
+            [
+                'foo' => 'bar',
+                'suitableForDecisions' => true
+            ],
+            $result->serialize()
+        );
     }
 }
