c8412d293f30de7f18aa336cfc72714277e3f5f1
Alex P <alex.peshkov@valtech.co.uk>
2015-11-09 14:15:23 +0000

    OLCS-11097 Transport manager status

--- app/api/module/Api/src/Domain/CommandHandler/Tm/Create.php
+++ app/api/module/Api/src/Domain/CommandHandler/Tm/Create.php
@@ -125,7 +125,7 @@ final class Create extends AbstractCommandHandler implements TransactionedInterf
     {
         $transportManager = new TransportManagerEntity();
         $status = $command->getStatus() ? $command->getStatus() :
-            TransportManagerEntity::TRANSPORT_MANAGER_STATUS_ACTIVE;
+            TransportManagerEntity::TRANSPORT_MANAGER_STATUS_CURRENT;
         $transportManager->updateTransportManager(
             $this->getRepo()->getRefdataReference($command->getType()),
             $this->getRepo()->getRefdataReference($status),
