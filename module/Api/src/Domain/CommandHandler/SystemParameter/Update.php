fc00315e466cb574763432a78c334e34159d3c30
Mat Evans <mat.evans@valtech.co.uk>
2015-11-11 09:44:16 +0000

    CLI to update SystemParameter

---
+++ module/Api/src/Domain/CommandHandler/SystemParameter/Update.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\SystemParameter;
+
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+
+/**
+ * Update a SystemParameter
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+final class Update extends AbstractCommandHandler implements TransactionedInterface
+{
+    protected $repoServiceName = 'SystemParameter';
+
+    /**
+     * @return Result
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        /* @var $systemParameter \Dvsa\Olcs\Api\Entity\System\SystemParameter */
+        $systemParameter = $this->getRepo()->fetchUsingId($command);
+        $systemParameter->setParamValue($command->getValue());
+        $this->getRepo()->save($systemParameter);
+
+        $this->result->addMessage("SystemParameter '{$command->getId()}' updated");
+        return $this->result;
+    }
+}
