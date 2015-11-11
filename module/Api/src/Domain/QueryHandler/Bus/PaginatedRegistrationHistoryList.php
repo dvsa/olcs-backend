7507cff6aa800945a2a7c4dccdb7e9351e9cd540
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-11 14:34:31 +0000

    Code review changes

--- module/Api/src/Domain/QueryHandler/Bus/PaginatedRegistrationHistoryList.php
+++ module/Api/src/Domain/QueryHandler/Bus/PaginatedRegistrationHistoryList.php
@@ -7,7 +7,6 @@ namespace Dvsa\Olcs\Api\Domain\QueryHandler\Bus;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\Bus\RegistrationHistoryList as RegistrationHistoryListQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Dvsa\Olcs\Api\Domain\Repository\Bus as BusRepo;
 use Dvsa\Olcs\Transfer\Query\Bus\RegistrationHistoryList as QueryCmd;
 use Dvsa\Olcs\Api\Domain\Query\Bus\ByLicenceRoute as ByLicenceRouteQry;
 use Doctrine\ORM\Query;
@@ -19,12 +18,13 @@ class PaginatedRegistrationHistoryList extends RegistrationHistoryListQueryHandl
 {
     protected $repoServiceName = 'Bus';
 
+    /**
+     * @param QueryInterface $query
+     * @return mixed
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
     public function handleQuery(QueryInterface $query)
     {
-        /**
-         * @var BusRepo $repo
-         * @var QueryCmd $query
-         */
         $busReg = $this->getRepo()->fetchUsingId($query);
 
         $routeNoQuery = [
@@ -36,8 +36,6 @@ class PaginatedRegistrationHistoryList extends RegistrationHistoryListQueryHandl
             'licenceId' => $busReg->getLicence()->getId(),
         ];
 
-        $result = $this->getQueryHandler()->handleQuery(ByLicenceRouteQry::create($routeNoQuery));
-
-        return $result;
+        return $this->getQueryHandler()->handleQuery(ByLicenceRouteQry::create($routeNoQuery));
     }
 }
