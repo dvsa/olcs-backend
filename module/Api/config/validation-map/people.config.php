41145789590b393e61b92120133e0cd5f49f422d
Rob Caiger <rob@clocal.co.uk>
2015-11-11 16:40:36 +0000

    Commented out changed until next story

--- module/Api/config/validation-map/people.config.php
+++ module/Api/config/validation-map/people.config.php
@@ -7,13 +7,29 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers as Handler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\People\Application as AppHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\People\Licence as LicHandler;
 
+// @todo next story
+//return [
+//    AppCommandHandler\CreatePeople::class => AppHandler\Create::class,
+//    AppCommandHandler\DeletePeople::class => Handler\Standard::class, // @todo
+//    AppCommandHandler\RestorePeople::class => Handler\Standard::class, // @todo
+//    AppCommandHandler\UpdatePeople::class => Handler\Standard::class, // @todo
+//
+//    LicCommandHandler\CreatePeople::class => LicHandler\Create::class,
+//    LicCommandHandler\DeletePeople::class => Handler\Standard::class, // @todo
+//    LicCommandHandler\UpdatePeople::class => Handler\Standard::class, // @todo
+//
+//    QueryHandler\Licence\People::class => Handler\Standard::class, // @todo
+//    QueryHandler\Application\People::class => Handler\Standard::class, // @todo
+//    QueryHandler\Organisation\People::class => Handler\Standard::class, // @todo
+//];
+
 return [
-    AppCommandHandler\CreatePeople::class => AppHandler\Create::class,
+    AppCommandHandler\CreatePeople::class => Handler\Standard::class, // @todo
     AppCommandHandler\DeletePeople::class => Handler\Standard::class, // @todo
     AppCommandHandler\RestorePeople::class => Handler\Standard::class, // @todo
     AppCommandHandler\UpdatePeople::class => Handler\Standard::class, // @todo
 
-    LicCommandHandler\CreatePeople::class => LicHandler\Create::class,
+    LicCommandHandler\CreatePeople::class => Handler\Standard::class, // @todo
     LicCommandHandler\DeletePeople::class => Handler\Standard::class, // @todo
     LicCommandHandler\UpdatePeople::class => Handler\Standard::class, // @todo
 
