4a69d65dfacb7b6ee8c5e320c4471ed1a6a048f1
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-17 10:18:57 +0000

    Added UT

---
+++ app/api/test/module/Api/src/Domain/CommandHandler/Submission/CloseSubmissionTest.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Close Submission Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Cases\Submission;
+
+use Doctrine\ORM\Query;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\CommandHandler\Submission\CloseSubmission;
+use Dvsa\Olcs\Api\Domain\Repository\Submission as SubmissionRepo;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Dvsa\Olcs\Transfer\Command\Submission\CloseSubmission as Cmd;
+use Dvsa\Olcs\Api\Entity\Submission\Submission as SubmissionEntity;
+use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
+
+/**
+ * Close Submission Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+class CloseSubmissionTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new CloseSubmission();
+        $this->mockRepo('Submission', SubmissionRepo::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        $this->refData = [];
+
+        $this->references = [];
+
+        parent::initReferences();
+    }
+
+    public function testHandleCommandCloseableSubmission()
+    {
+        $command = Cmd::create(
+            [
+                'id' => 2
+            ]
+        );
+
+        /** @var SubmissionEntity $savedSubmission */
+        $submission = m::mock(SubmissionEntity::class)->makePartial();
+        $submission->setId(2);
+
+        $this->repoMap['Submission']->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($submission);
+
+        $this->repoMap['Submission']->shouldReceive('save')
+            ->once()
+            ->with(m::type(SubmissionEntity::class))
+            ->andReturnUsing(
+                function (SubmissionEntity $submission) use (&$savedSubmission) {
+                    $savedSubmission = $submission;
+                }
+            );
+
+        $result = $this->sut->handleCommand($command);
+
+        $this->assertInstanceOf('Dvsa\Olcs\Api\Domain\Command\Result', $result);
+        $this->assertObjectHasAttribute('ids', $result);
+        $this->assertObjectHasAttribute('messages', $result);
+        $this->assertContains('Submission closed', $result->getMessages());
+    }
+
+    public function testHandleCommandNotCloseableSubmission()
+    {
+        $command = Cmd::create(
+            [
+                'id' => 2
+            ]
+        );
+
+        /** @var SubmissionEntity $savedSubmission */
+        $submission = m::mock(SubmissionEntity::class)->makePartial();
+        $submission->setId(2);
+        $submission->setClosedDate(new \DateTime('now'));
+
+        $this->repoMap['Submission']->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($submission);
+
+        $this->setExpectedException(ForbiddenException::class);
+
+        $this->sut->handleCommand($command);
+    }
+}
