04d4b94c26b311f93907626d92a12c2c905aeddc
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-17 10:18:57 +0000

    Added UT

---
+++ test/module/Api/src/Domain/CommandHandler/Submission/ReopenSubmissionTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Reopen Submission Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Cases\Submission;
+
+use Doctrine\ORM\Query;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\CommandHandler\Submission\ReopenSubmission;
+use Dvsa\Olcs\Api\Domain\Repository\Submission as SubmissionRepo;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Dvsa\Olcs\Transfer\Command\Submission\ReopenSubmission as Cmd;
+use Dvsa\Olcs\Api\Entity\Submission\Submission as SubmissionEntity;
+use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
+
+/**
+ * Reopen Submission Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+class ReopenSubmissionTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new ReopenSubmission();
+        $this->mockRepo('Submission', SubmissionRepo::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        $this->refData = [];
+
+        $this->references = [];
+
+        parent::initReferences();
+    }
+
+    public function testHandleCommandForClosedSubmission()
+    {
+        $command = Cmd::create(
+            [
+                'id' => 2
+            ]
+        );
+
+        /** @var SubmissionEntity $savedSubmission */
+        $submission = m::mock(SubmissionEntity::class)->makePartial();
+        $submission->setId(2);
+        $submission->setClosedDate(new \DateTime('now'));
+
+        $this->repoMap['Submission']->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($submission);
+
+        $this->repoMap['Submission']->shouldReceive('save')
+            ->once()
+            ->with(m::type(SubmissionEntity::class))
+            ->andReturnUsing(
+                function (SubmissionEntity $submission) use (&$savedSubmission) {
+                    $savedSubmission = $submission;
+                }
+            );
+
+        $result = $this->sut->handleCommand($command);
+
+        $this->assertInstanceOf('Dvsa\Olcs\Api\Domain\Command\Result', $result);
+        $this->assertObjectHasAttribute('ids', $result);
+        $this->assertObjectHasAttribute('messages', $result);
+        $this->assertContains('Submission reopened', $result->getMessages());
+    }
+
+    /**
+     * An open submission should not be re-openable, hence test for exception
+     */
+    public function testHandleCommandForOpenSubmission()
+    {
+        $command = Cmd::create(
+            [
+                'id' => 2
+            ]
+        );
+
+        /** @var SubmissionEntity $savedSubmission */
+        $submission = m::mock(SubmissionEntity::class)->makePartial();
+        $submission->setId(2);
+
+
+        $this->repoMap['Submission']->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($submission);
+
+        $this->setExpectedException(ForbiddenException::class);
+
+        $result = $this->sut->handleCommand($command);
+    }
+}
