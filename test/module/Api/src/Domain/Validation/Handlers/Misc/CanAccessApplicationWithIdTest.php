2b3fc27d2e06b106696d8e745443491a22ae59e2
Rob Caiger <rob@clocal.co.uk>
2015-11-17 14:56:46 +0000

    Updated unit tests

---
+++ test/module/Api/src/Domain/Validation/Handlers/Misc/CanAccessApplicationWithIdTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Can Access Application With Id Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Validation\Handlers\CompanySubsidiary\Application;
+
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\OlcsTest\Api\Domain\Validation\Handlers\AbstractHandlerTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanAccessApplicationWithId;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Can Access Application With Id Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class CanAccessApplicationWithIdTest extends AbstractHandlerTestCase
+{
+    /**
+     * @var CanAccessApplicationWithId
+     */
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new CanAccessApplicationWithId();
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testIsValid($canAccess, $expected)
+    {
+        /** @var CommandInterface $dto */
+        $dto = m::mock(CommandInterface::class);
+        $dto->shouldReceive('getId')->andReturn(111);
+
+        $this->setIsValid('canAccessApplication', [111], $canAccess);
+
+        $this->assertSame($expected, $this->sut->isValid($dto));
+    }
+
+    public function provider()
+    {
+        return [
+            [true, true],
+            [false, false],
+        ];
+    }
+}
