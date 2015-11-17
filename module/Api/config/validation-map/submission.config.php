2ec3d35aaec1ceba49140f1795badbf15183c5dd
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-17 16:53:48 +0000

    Moved Submission DTOs that havent been implemented back to main list

--- module/Api/config/validation-map/submission.config.php
+++ module/Api/config/validation-map/submission.config.php
@@ -7,33 +7,7 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers as Handler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Submission as SubmissionHandler;
 
 return [
-    // Queries
-    SubmissionQueryHandler\Submission::class => Handler\Standard::class, // @todo
-    SubmissionQueryHandler\SubmissionAction::class => Handler\Standard::class, // @todo
-    SubmissionQueryHandler\SubmissionList::class => Handler\Standard::class, // @todo
-    SubmissionQueryHandler\SubmissionSectionComment::class => Handler\Standard::class, // @todo
-
-    // Commands
-
     // Submission
     AppCommandHandler\CloseSubmission::class => SubmissionHandler\Close::class,
     AppCommandHandler\ReopenSubmission::class => SubmissionHandler\Reopen::class,
-    AppCommandHandler\UpdateSubmission::class => Handler\Standard::class, // @todo
-    AppCommandHandler\DeleteSubmission::class => Handler\Standard::class, // @todo
-    AppCommandHandler\AssignSubmission::class => Handler\Standard::class, // @todo
-    AppCommandHandler\CreateSubmission::class => Handler\Standard::class, // @todo
-
-    // Submission Action
-    AppCommandHandler\CreateSubmissionAction::class => Handler\Standard::class, // @todo
-    AppCommandHandler\UpdateSubmissionAction::class => Handler\Standard::class, // @todo
-
-    // SubmissionSectionComment
-    AppCommandHandler\CreateSubmissionSectionComment::class => Handler\Standard::class, // @todo
-    AppCommandHandler\DeleteSubmissionSectionComment::class => Handler\Standard::class, // @todo
-    AppCommandHandler\UpdateSubmissionSectionComment::class => Handler\Standard::class, // @todo
-
-    // Others
-    AppCommandHandler\FilterSubmissionSections::class => Handler\Standard::class, // @todo
-    AppCommandHandler\RefreshSubmissionSections::class => Handler\Standard::class, // @todo
-
 ];
