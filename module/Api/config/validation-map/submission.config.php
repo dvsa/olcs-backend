164958159c8fef77761bd17a813597a5e48ac95b
Shaun Lizzio <shaun@lizzio.co.uk>
2015-11-18 12:41:25 +0000

    Removed case and close/reopen validation and assoc. tests

--- module/Api/config/validation-map/submission.config.php
+++ module/Api/config/validation-map/submission.config.php
@@ -2,12 +2,28 @@
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Submission as SubmissionQueryHandler;
-use Dvsa\Olcs\Api\Domain\CommandHandler\Submission as AppCommandHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers as Handler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Submission as SubmissionHandler;
 
 return [
-    // Submission
-    AppCommandHandler\CloseSubmission::class => SubmissionHandler\Close::class,
-    AppCommandHandler\ReopenSubmission::class => SubmissionHandler\Reopen::class,
+    // Queries
+    QueryHandler\Submission\Submission::class               => Handler\Misc\IsInternalUser::class,
+    QueryHandler\Submission\SubmissionAction::class         => Handler\Misc\IsInternalUser::class,
+    QueryHandler\Submission\SubmissionList::class           => Handler\Misc\IsInternalUser::class,
+    QueryHandler\Submission\SubmissionSectionComment::class => Handler\Misc\IsInternalUser::class,
+
+    // Commands
+    CommandHandler\Submission\CloseSubmission::class                => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\ReopenSubmission::class               => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\AssignSubmission::class               => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\CreateSubmission::class               => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\CreateSubmissionAction::class         => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\CreateSubmissionSectionComment::class => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\DeleteSubmissionSectionComment::class => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\FilterSubmissionSections::class       => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\RefreshSubmissionSections::class      => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\UpdateSubmission::class               => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\UpdateSubmissionAction::class         => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\UpdateSubmissionSectionComment::class => Handler\Misc\IsInternalUser::class,
+    CommandHandler\Submission\DeleteSubmission::class               => Handler\Misc\IsInternalUser::class,
 ];
--- module/Api/config/validators.config.php
+++ module/Api/config/validators.config.php
@@ -17,12 +17,5 @@ return [
         'canAccessCompanySubsidiary' => Validators\CanAccessCompanySubsidiary::class,
         'canAccessOrganisation' => Validators\CanAccessOrganisation::class,
         'canAccessOrganisationPerson' => Validators\CanAccessOrganisationPerson::class,
-
-        'canClose' => Validators\CanClose::class,
-        'canCloseSubmission' => Validators\CanCloseSubmission::class,
-        'canReopen' => Validators\CanReopen::class,
-        'canReopenSubmission' => Validators\CanReopenSubmission::class,
-        'submissionBelongsToCase' => Validators\SubmissionBelongsToCase::class,
-        'belongsToCase' => Validators\BelongsToCase::class,
     ]
 ];
