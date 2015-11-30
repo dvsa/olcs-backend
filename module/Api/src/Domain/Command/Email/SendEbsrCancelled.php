0264b5ba661f5e645741e61bae458a7e25f81b93
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-11-30 16:58:59 +0000

    Command to send Ebsr cancelled email

---
+++ module/Api/src/Domain/Command/Email/SendEbsrCancelled.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Send email to notify that EBSR submission has been cancelled
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Email;
+
+use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
+
+/**
+ * Send email to notify that EBSR submission has been cancelled
+ */
+final class SendEbsrCancelled extends AbstractIdOnlyCommand
+{
+
+}
