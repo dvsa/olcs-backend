d5e438ec013dbe1658655d2e7a4b7797fb6c2445
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-12-07 16:37:14 +0000

    Rewrite to add missing features, cc La's on emails, publication info, extra info in subject line etc.

--- test/module/Api/src/Domain/CommandHandler/Email/SendEbsrRefusedTest.php
+++ test/module/Api/src/Domain/CommandHandler/Email/SendEbsrRefusedTest.php
@@ -9,9 +9,11 @@ namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Email;
 
 use Mockery as m;
 use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Command\Email\SendEbsrRefused;
 
 /**
  * Send Ebsr Refused Email Test
+ * @group ebsrEmails
  *
  * @author Craig R <uk@valtech.co.uk>
  */
@@ -19,4 +21,5 @@ class SendEbsrRefusedTest extends SendEbsrEmailTestAbstract
 {
     protected $template = 'ebsr-refused';
     protected $sutClass = '\Dvsa\Olcs\Api\Domain\CommandHandler\Email\SendEbsrRefused';
+    protected $cmdClass = SendEbsrRefused::class;
 }
