978a178290ea719b4a2f36d893b58c9110b4dd09
Dan Eggleston <dan@stolenegg.com>
2015-12-07 13:36:29 +0000

    OLCS-6355 refactor to enable reusing the overpayment balancing fee logic

--- app/api/module/Api/config/validation-map/fee.config.php
+++ app/api/module/Api/config/validation-map/fee.config.php
@@ -5,6 +5,5 @@ use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
 
 return [
-    CommandHandler\Transaction\ReverseTransaction::class => IsInternalUser::class,
-    CommandHandler\Transaction\AdjustTransaction::class => IsInternalUser::class,
+    CommandHandler\Fee\CreateOverpaymentFee::class => IsInternalUser::class,
 ];
