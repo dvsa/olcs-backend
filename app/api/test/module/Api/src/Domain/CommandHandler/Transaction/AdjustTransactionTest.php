f31da70f2c3ad02e09ec9665fde9f15806c7fcb1
Dan Eggleston <dan@stolenegg.com>
2015-12-09 15:14:11 +0000

    OLCS-6355 unit test for adjustment command handler

--- app/api/test/module/Api/src/Domain/CommandHandler/Transaction/AdjustTransactionTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Transaction/AdjustTransactionTest.php
@@ -7,8 +7,9 @@
  */
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Transaction;
 
+use Doctrine\ORM\Query;
 use Doctrine\Common\Collections\ArrayCollection;
-use Dvsa\Olcs\Api\Domain\Command\Fee\ResetFees as ResetFeesCmd;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CreateOverpaymentFee as CreateOverpaymentFeeCmd;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Transaction\AdjustTransaction;
 use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
@@ -47,7 +48,6 @@ class AdjustTransactionTest extends CommandHandlerTestCase
         ];
 
         $this->sut = new AdjustTransaction();
-        $this->mockRepo('Fee', Repository\Fee::class);
         $this->mockRepo('Transaction', Repository\Transaction::class);
 
         /** @var UserEntity $mockUser */
@@ -67,32 +67,135 @@ class AdjustTransactionTest extends CommandHandlerTestCase
     {
         $this->refData = [
             TransactionEntity::STATUS_COMPLETE,
-            FeeEntity::STATUS_OUTSTANDING => m::mock(RefData::class)
+            TransactionEntity::TYPE_ADJUSTMENT,
+            FeeEntity::STATUS_PAID => m::mock(RefData::class)
                 ->makePartial()
                 ->shouldReceive('getDescription')
-                ->andReturn('Outstanding')
+                ->andReturn('Paid')
                 ->getMock(),
-            FeeEntity::METHOD_REVERSAL,
             FeeEntity::METHOD_CHEQUE,
-            FeeEntity::METHOD_CASH,
-            FeeEntity::METHOD_POSTAL_ORDER,
-            FeeEntity::METHOD_CARD_ONLINE,
-            FeeEntity::METHOD_CARD_OFFLINE,
         ];
 
         $this->references = [
             TransactionEntity::class => [
-                123 => m::mock(TransactionEntity::class),
+                69 => m::mock(TransactionEntity::class),
             ],
             FeeEntity::class => [
-                69 => m::mock(FeeEntity::class),
+                100 => m::mock(FeeEntity::class),
             ],
             FeeTransactionEntity::class => [
-                101 => m::mock(FeeTransactionEntity::class),
-            ],
+                200 => m::mock(FeeTransactionEntity::class),
+            ]
         ];
 
         parent::initReferences();
+    }
+
+    public function testHandleCommand()
+    {
+        $transactionId = 69;
+
+        // set up 'new' data
+        //////////////////////////////////////
+        $data = [
+            'id' => $transactionId,
+            'version' => 1,
+            'received' => '10.00',
+            'payer' => 'Dan',
+            'slipNo' => '1234',
+            'chequeNo' => '2345',
+            'chequeDate' => '2015-12-01',
+            'reason' => 'Keying error'
+        ];
+
+        $command = Cmd::create($data);
+        ////////////////////////////////////////
+
+        // set up 'existing' data
+        ////////////////////////////////////////
+        $fee = $this->mapReference(FeeEntity::class, 100);
+
+        $transaction = $this->mapReference(TransactionEntity::class, $transactionId);
+
+        $feeTransaction = $this->mapReference(FeeTransactionEntity::class, 200);
+        $feeTransaction
+            ->setFee($fee)
+            ->setTransaction($transaction)
+            ->shouldReceive('getAmount')->andReturn('100.00');
+
+        $fee->setFeeTransactions(new ArrayCollection([$feeTransaction]));
+        $fee->shouldReceive('isBalancingFee')->andReturn(false);
+
+        $transaction
+            ->shouldReceive('getReference')->andReturn('OLCS-CHEQUE-REF-1')
+            ->shouldReceive('getId')->andReturn($transactionId)
+            ->shouldReceive('getFees')->andReturn([$fee])
+            ->shouldReceive('getTotalAmount')->andReturn('100.00')
+            ->shouldReceive('getPayerName')->andReturn('Dan')
+            ->shouldReceive('getPayingInSlipNumber')->andReturn('1234')
+            ->shouldReceive('getChequePoNumber')->andReturn('2345')
+            ->shouldReceive('getChequePoDate')->andReturn('2015-12-01')
+            ->shouldReceive('getFeeTransactionsForAdjustment')->andReturn([$feeTransaction]);
+        ////////////////////////////////////////
+
+        $this->repoMap['Transaction']
+            ->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command, Query::HYDRATE_OBJECT, 1)
+            ->andReturn($transaction);
+
+        $this->mockCpmsService
+            ->shouldReceive('adjustTransaction')
+            ->once()
+            ->with('OLCS-CHEQUE-REF-1', 69, [$fee], '10.00', 'Dan', '1234', '2345', '2015-12-01', null)
+            ->andReturn(
+                [
+                    'code' => CpmsHelper::RESPONSE_SUCCESS,
+                    'receipt_reference' => 'OLCS-ADJ-REF-1',
+                ]
+            );
+
+        $this->expectedSideEffect(
+            CreateOverpaymentFeeCmd::class,
+            [
+                'receivedAmount' => '10.00',
+                'fees' => [100 => $fee],
+            ],
+            new Result()
+        );
+
+        $this->mockFeesHelperService
+            ->shouldReceive('allocatePayments')
+            ->once()
+            ->with(
+                '10.00',
+                [100 => $fee]
+            )
+            ->andReturn(
+                [
+                    // 100 => '10.00',
+                ]
+            );
+
+        $savedTransaction = null;
+        $newTransactionId = 70;
+        $this->repoMap['Transaction']
+            ->shouldReceive('save')
+            ->andReturnUsing(
+                function ($transaction) use (&$savedTransaction, $newTransactionId) {
+                    $savedTransaction = $transaction;
+                    $savedTransaction->setId($newTransactionId);
+                    // $feeTransactionId = 200;
+                    // $savedTransaction->getFeeTransactions()->forAll(
+                    //     function ($key, $ft) use (&$feeTransactionId) {
+                    //         $ft->setId($feeTransactionId + $key);
+                    //         return true; // closure *must* return true to continue
+                    //     }
+                    // );
+                }
+            );
+
+        $this->sut->handleCommand($command);
     }
 
     /**
