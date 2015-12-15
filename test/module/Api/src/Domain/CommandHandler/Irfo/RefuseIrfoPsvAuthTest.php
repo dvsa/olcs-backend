7ef5cd00a5a7d3d3ce53a40251881d3b3f7f6476
Shaun Lizzio <shaun@lizzio.co.uk>
2015-12-15 11:50:30 +0000

    More UT

---
+++ test/module/Api/src/Domain/CommandHandler/Irfo/RefuseIrfoPsvAuthTest.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Refuse Irfo Psv Auth Test
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Irfo;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelIrfoPsvAuthFees as CancelFeesDto;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\Irfo\RefuseIrfoPsvAuth as Sut;
+use Dvsa\Olcs\Transfer\Command\Irfo\UpdateIrfoPsvAuth as UpdateIrfoPsvAuthCmd;
+use Dvsa\Olcs\Api\Domain\Repository\IrfoPsvAuth as IrfoPsvAuthRepo;
+use Dvsa\Olcs\Api\Entity\Irfo\IrfoPsvAuth as IrfoPsvAuthEntity;
+use Dvsa\Olcs\Transfer\Command\Irfo\RefuseIrfoPsvAuth as Cmd;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Mockery as m;
+
+/**
+ * Refuse Irfo Psv Auth Test
+ */
+class RefuseIrfoPsvAuthTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new Sut();
+        $this->mockRepo('IrfoPsvAuth', IrfoPsvAuthRepo::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        $this->refData = [
+            IrfoPsvAuthEntity::STATUS_REFUSED,
+        ];
+
+        parent::initReferences();
+    }
+
+    /**
+     * testHandleCommand
+     */
+    public function testHandleCommand()
+    {
+        $id = 99;
+
+        $command = Cmd::Create(
+            [
+                'id' => $id,
+            ]
+        );
+
+        $data = [
+            'id' => $id,
+            'version' => 2,
+            'organisation' => 11,
+            'irfoPsvAuthType' => 22,
+            'status' => IrfoPsvAuthEntity::STATUS_PENDING,
+            'validityPeriod' => 1,
+            'inForceDate' => '2015-01-01',
+            'expiryDate' => '2016-01-01',
+            'applicationSentDate' => '2014-01-01',
+            'serviceRouteFrom' => 'From',
+            'serviceRouteTo' => 'To',
+            'journeyFrequency' => IrfoPsvAuthEntity::JOURNEY_FREQ_DAILY,
+            'isFeeExemptApplication' => 'Y',
+            'isFeeExemptAnnual' => 'Y',
+            'exemptionDetails' => 'testing',
+            'copiesRequired' => 1,
+            'copiesRequiredTotal' => 1,
+            'countrys' => ['GB'],
+            'irfoPsvAuthNumbers' => [
+                ['name' => 'test 1'],
+                ['name' => ''],
+            ],
+        ];
+
+        $command = Cmd::create($data);
+
+        // handle update
+        $this->expectedSideEffect(
+            UpdateIrfoPsvAuthCmd::class, $command->getArrayCopy(),
+            (new Result())->addMessage('IRFO PSV Auth updated successfully')
+                ->addId('irfoPsvAuth', $data['id'])
+        );
+
+        /** @var IrfoPsvAuthEntity $irfoPsvAuth */
+        $irfoPsvAuth = m::mock(IrfoPsvAuthEntity::class);
+        $irfoPsvAuth->shouldReceive('refuse')
+            ->once()
+            ->with($this->refData[IrfoPsvAuthEntity::STATUS_REFUSED])
+            ->shouldReceive('getId')
+            ->andReturn($id);
+
+        $this->repoMap['IrfoPsvAuth']->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT)
+            ->andReturn($irfoPsvAuth)
+            ->shouldReceive('save')
+            ->with(m::type(IrfoPsvAuthEntity::class))
+            ->once();
+
+        $this->expectedSideEffect(CancelFeesDto::class, ['id' => $id], new Result());
+
+        $result = $this->sut->handleCommand($command);
+
+        $this->assertInstanceOf(Result::class, $result);
+    }
+}
