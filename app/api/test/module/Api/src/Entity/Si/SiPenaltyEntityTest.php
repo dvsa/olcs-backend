cee11146bc710cf2bc2ae7537008da5df6dab65a
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-05 16:43:56 +0000

    Create and update applied penalties

--- app/api/test/module/Api/src/Entity/Si/SiPenaltyEntityTest.php
+++ app/api/test/module/Api/src/Entity/Si/SiPenaltyEntityTest.php
@@ -4,6 +4,9 @@ namespace Dvsa\OlcsTest\Api\Entity\Si;
 
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Si\SiPenalty as Entity;
+use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement;
+use Dvsa\Olcs\Api\Entity\Si\SiPenaltyType;
+use Mockery as m;
 
 /**
  * SiPenalty Entity Unit Tests
@@ -18,4 +21,26 @@ class SiPenaltyEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /**
+     * Tests create and update
+     */
+    public function testCreateAndUpdate()
+    {
+        $si = m::mock(SeriousInfringement::class);
+        $penaltyType = m::mock(SiPenaltyType::class);
+        $startDate = new \DateTime('2015-12-25');
+        $endDate = new \DateTime('2015-12-26');
+        $imposed = 'Y';
+        $reasonNotImposed = 'reason not imposed';
+
+        $entity = new Entity($si, $penaltyType, $startDate, $endDate, $imposed, $reasonNotImposed);
+
+        $this->assertEquals($si, $entity->getSeriousInfringement());
+        $this->assertEquals($penaltyType, $entity->getSiPenaltyType());
+        $this->assertEquals($startDate, $entity->getStartDate());
+        $this->assertEquals($endDate, $entity->getEndDate());
+        $this->assertEquals($imposed, $entity->getImposed());
+        $this->assertEquals($reasonNotImposed, $entity->getReasonNotImposed());
+    }
 }
