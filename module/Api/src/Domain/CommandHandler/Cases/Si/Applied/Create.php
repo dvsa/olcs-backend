a5bf5592076c6a0194ff43febac85a0a39a69f76
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-05 16:43:19 +0000

    Command handler to create si applied penalties

---
+++ module/Api/src/Domain/CommandHandler/Cases/Si/Applied/Create.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Create Erru applied penalty
+ */
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Si\Applied;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Api\Entity\Cases\Cases as CaseEntity;
+use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement as SiEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiPenalty as SiPenaltyEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiPenaltyType as SiPenaltyTypeEntity;
+use Dvsa\Olcs\Transfer\Command\Cases\Si\Applied\Create as CreatePenaltyCmd;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+
+/**
+ * Create Erru applied penalty
+ */
+final class Create extends AbstractCommandHandler implements TransactionedInterface
+{
+    const DATE_FORMAT = 'Y-m-d';
+
+    protected $repoServiceName = 'SiPenalty';
+    protected $extraRepos = ['Cases'];
+
+    /**
+     * Create Erru applied penalty
+     *
+     * @param CommandInterface $command
+     * @return Result
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        /**
+         * @var CaseEntity $case
+         * @var SiEntity $si
+         * @var SiPenaltyTypeEntity $siPenaltyType
+         * @var CreatePenaltyCmd $command
+         */
+        $case = $this->getRepo('Cases')->fetchById($command->getCase());
+        $si = $case->getSeriousInfringements()->first(); //there will only ever be one si
+        $siPenaltyType = $this->getRepo()->getReference(SiPenaltyTypeEntity::class, $command->getSiPenaltyType());
+
+        $penalty = new SiPenaltyEntity(
+            $si,
+            $siPenaltyType,
+            \DateTime::createFromFormat(self::DATE_FORMAT, $command->getStartDate()),
+            \DateTime::createFromFormat(self::DATE_FORMAT, $command->getEndDate()),
+            $command->getImposed(),
+            $command->getReasonNotImposed()
+        );
+
+        $this->getRepo()->save($penalty);
+
+        $result = new Result();
+        $result->addMessage('Applied penalty created');
+        $result->addId('case', $case->getId());
+        $result->addId('si', $si->getId());
+        $result->addId('penalty', $penalty->getId());
+
+        return $result;
+    }
+}
