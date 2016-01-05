e84a9f6eb18be15c0f7984b306e28d5e144ad788
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-05 11:52:01 +0000

    Added si penalty repo

---
+++ module/Api/src/Domain/Repository/SiPenalty.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Repository;
+
+use Dvsa\Olcs\Api\Entity\Si\SiPenalty as Entity;
+
+/**
+ * SiPenalty
+ */
+class SiPenalty extends AbstractRepository
+{
+    protected $entity = Entity::class;
+}
