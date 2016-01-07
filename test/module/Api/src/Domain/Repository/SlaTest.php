851844c865b0ed83011a357cad2cfdd46a640f22
Shaun Lizzio <shaun@lizzio.co.uk>
2016-01-07 16:06:41 +0000

    Added Repo test

---
+++ test/module/Api/src/Domain/Repository/SlaTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Sla Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Repository;
+
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Repository;
+
+/**
+ * Sla Test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+class SlaTest extends RepositoryTestCase
+{
+    public function setUp()
+    {
+        $this->setUpSut(Repository\Sla::class);
+    }
+
+    public function testFetchByCategories()
+    {
+        $categories = ['foo', 'bar'];
+
+        $qb = $this->createMockQb('QUERY');
+
+        $this->mockCreateQueryBuilder($qb);
+
+        $qb->shouldReceive('getQuery->getResult')->once()->andReturn('foobar');
+
+        $result = $this->sut->fetchByCategories($categories);
+
+        $this->assertEquals('QUERY AND m.category IN [[["foo","bar"]]]', $this->query);
+
+        $this->assertEquals('foobar', $result);
+    }
+}
