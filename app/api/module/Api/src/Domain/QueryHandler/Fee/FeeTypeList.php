4786610847a5043d2ba9fdcdb83958079e46e179
Mat Evans <mat.evans@valtech.co.uk>
2016-01-11 15:46:41 +0000

    OLCS-11542 Updated fee type lists

--- app/api/module/Api/src/Domain/QueryHandler/Fee/FeeTypeList.php
+++ app/api/module/Api/src/Domain/QueryHandler/Fee/FeeTypeList.php
@@ -11,6 +11,8 @@ use Dvsa\Olcs\Api\Domain\Repository\FeeType as FeeTypeRepo;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation as OrganisationEntity;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Application\Application;
 
 /**
  * Fee Type List
@@ -33,10 +35,15 @@ class FeeTypeList extends AbstractQueryHandler
 
         $feeTypes = $repo->fetchList($query, DoctrineQuery::HYDRATE_OBJECT);
 
-        $feeTypes = $this->filterDuplicates($feeTypes);
+        // get fee types where traffic area *is not* specified
+        $genericFeetypes = $this->filterDuplicates($feeTypes, null);
+        // get fee types where traffic area *is* specified
+        $specificFeeTypes = $this->filterDuplicates($feeTypes, $this->getTrafficArea($query));
+        // merge fee types together so that fee types with specified traffic area will overwrite non specified
+        $filteredFeeTypes = $specificFeeTypes + $genericFeetypes;
 
         $valueOptions = [];
-        foreach ($feeTypes as $ft) {
+        foreach ($filteredFeeTypes as $ft) {
             $valueOptions['feeType'][$ft->getId()] = $ft->getDescription();
         }
 
@@ -44,23 +51,55 @@ class FeeTypeList extends AbstractQueryHandler
         $valueOptions['irfoPsvAuth'] = $this->getIrfoPsvAuthValueOptions($query);
 
         return [
-            'result' => $this->resultList($feeTypes),
-            'count' => count($feeTypes),
+            'result' => $this->resultList($filteredFeeTypes),
+            'count' => count($filteredFeeTypes),
             'valueOptions' => $valueOptions,
         ];
+    }
+
+    /**
+     * Get a Traffic area from the query params
+     *
+     * @param QueryInterface $query
+     *
+     * @return TrafficArea|null
+     */
+    private function getTrafficArea(QueryInterface $query)
+    {
+        /* @var $licence Licence */
+        $licence = $this->getRepo()->getReference(Licence::class, $query->getLicence());
+        if ($licence) {
+            return $licence->getTrafficArea();
+        }
+
+        /* @var $application Application */
+        $application = $this->getRepo()->getReference(Application::class, $query->getApplication());
+        if ($application) {
+            return $application->getLicence()->getTrafficArea();
+        }
+
+        return null;
     }
 
     /**
      * This is in lieu of being able to do proper groupwise max in the
      * repository method using Doctrine
      *
-     * @param array
+     * @param array $feeTypes Array of fee types
+     * @param TrafficArea $trafficArea TrafficArea to get fee types for, default to null
+     *
      * @return array
      */
-    public function filterDuplicates($feeTypes)
+    private function filterDuplicates($feeTypes, \Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea $trafficArea = null)
     {
         $filtered = [];
         foreach ($feeTypes as $ft) {
+            /* @var $ft \Dvsa\Olcs\Api\Entity\Fee\FeeType */
+
+            if ($ft->getTrafficArea() !== $trafficArea) {
+                continue;
+            }
+
             // if IRFO, we group by irfoFeeType id rather than feeType id
             $feeTypeId = $ft->getIrfoFeeType() ? $ft->getIrfoFeeType()->getId() : $ft->getFeeType()->getId();
             if (!isset($filtered[$feeTypeId]) || $ft->getEffectiveFrom() > $filtered[$feeTypeId]->getEffectiveFrom()) {
