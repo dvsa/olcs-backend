9194fde251ee0746054f5a71ecab892427813182
Rob Caiger <rob@clocal.co.uk>
2016-01-11 10:25:52 +0000

    Implemented query handler for Pid

---
+++ module/Api/src/Domain/QueryHandler/User/Pid.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Pid
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\User;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * Pid
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class Pid extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'User';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        /** @var UserEntity $user */
+        $user = $this->getRepo()->fetchUsingId($query);
+
+        return [
+            'pid' => $user->getPid()
+        ];
+    }
+}
