bd9f4bade76cce31ce86806f1f26c792a382b6fe
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-12 16:43:37 +0000

    Serious infringement entity update with erru response

--- app/api/test/module/Api/src/Entity/Si/SeriousInfringementEntityTest.php
+++ app/api/test/module/Api/src/Entity/Si/SeriousInfringementEntityTest.php
@@ -2,8 +2,11 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Si;
 
+use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement as Entity;
+use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
+use Mockery as m;
 
 /**
  * SeriousInfringement Entity Unit Tests
@@ -18,4 +21,21 @@ class SeriousInfringementEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /**
+     * tests updateErruResponse
+     */
+    public function testUpdateErruResponse()
+    {
+        $user = m::mock(UserEntity::class);
+        $date = new \DateTime();
+
+        $entity = new SeriousInfringement();
+
+        $entity->updateErruResponse($user, $date);
+
+        $this->assertEquals($user, $entity->getErruResponseUser());
+        $this->assertEquals($date, $entity->getErruResponseTime());
+        $this->assertEquals('Y', $entity->getErruResponseSent());
+    }
 }
