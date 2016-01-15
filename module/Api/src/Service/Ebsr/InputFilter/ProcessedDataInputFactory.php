911c72878db6e123693ada1698bc6cca2234d99e
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-15 17:56:17 +0000

    Validation done after the EBSR data has been through doctrine

---
+++ module/Api/src/Service/Ebsr/InputFilter/ProcessedDataInputFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Ebsr\InputFilter;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class ProcessedDataInputFactory
+ * @package Dvsa\Olcs\Api\Service\Ebsr\InputFilter
+ */
+class ProcessedDataInputFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $service = new Input('processed_data');
+
+        $filterChain = $service->getFilterChain();
+
+        $validatorChain = $service->getValidatorChain();
+        $validatorChain->attach($serviceLocator->get('ValidatorManager')->get('Rules\LocalAuthorityNotRequired'));
+        $validatorChain->attach($serviceLocator->get('ValidatorManager')->get('Rules\LocalAuthorityMissing'));
+
+        return $service;
+    }
+}
