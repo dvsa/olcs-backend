47436ec248fcd2b0e33823bec8e287e92ddbf725
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-15 17:34:54 +0000

    Check for local authorities missing in the ebsr file

---
+++ app/api/module/Api/src/Service/Ebsr/RulesValidator/LocalAuthorityMissing.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Ebsr\RulesValidator;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Validator\Exception;
+use Doctrine\Common\Collections\ArrayCollection;
+use Dvsa\Olcs\Api\Entity\Bus\LocalAuthority as LaEntity;
+
+/**
+ * Class LocalAuthorityMissing
+ * @package Dvsa\Olcs\Api\Service\Ebsr\RulesValidator
+ */
+class LocalAuthorityMissing extends AbstractValidator
+{
+    const LA_MISSING_ERROR = 'la-ta-missing-error';
+
+    /**
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::LA_MISSING_ERROR => 'According to the stops, these Local Authorities are missing from the data: %value%'
+    ];
+
+    /**
+     * Returns true if and only if $value meets the validation requirements
+     *
+     * If $value fails validation, then this method returns false, and
+     * getMessages() will return an array of messages that explain why the
+     * validation failed.
+     *
+     * @param  mixed $value
+     * @return bool
+     */
+    public function isValid($value, $context = [])
+    {
+        /**
+         * @var LaEntity $naptan
+         * @var ArrayCollection $localAuthorities
+         * @var ArrayCollection $naptanLocalAuthorities
+         */
+        $localAuthorities = $value['localAuthoritys']; //grammar mismatch result of entity array collection name
+        $naptanLocalAuthorities = $value['naptanAuthorities'];
+
+        $missing = [];
+
+        foreach ($naptanLocalAuthorities as $naptan) {
+            if (!$localAuthorities->contains($naptan)) {
+                $missing[] = $naptan->getDescription();
+            }
+        }
+
+        if (!empty($missing)) {
+            $this->error(self::LA_MISSING_ERROR, implode(', ', $missing));
+            return false;
+        }
+
+        return true;
+    }
+}
