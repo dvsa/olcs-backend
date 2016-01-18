4e3911b15e239980cf84f598d3176755432288d4
Alex P <alex.peshkov@valtech.co.uk>
2016-01-18 16:47:21 +0000

    OLCS-11614 BusServiceType data service migration

--- app/api/module/Api/src/Domain/Repository/BusServiceType.php
+++ app/api/module/Api/src/Domain/Repository/BusServiceType.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Dvsa\Olcs\Api\Domain\Exception;
 use Dvsa\Olcs\Api\Entity\Bus\BusServiceType as Entity;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 /**
  * Bus Service Type
@@ -25,5 +26,14 @@ class BusServiceType extends AbstractRepository
         $qb->andWhere($qb->expr()->in($this->alias . '.txcName', $txcNames));
 
         return $qb->getQuery()->execute();
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     * @param QueryInterface $query
+     */
+    protected function applyListFilters(QueryBuilder $qb, QueryInterface $query)
+    {
+        $qb->orderBy($this->alias . '.description', 'ASC');
     }
 }
