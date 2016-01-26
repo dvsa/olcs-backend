24955aa76c0c67304b0cb07bbe96f6be18cf6bdb
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-26 13:28:15 +0000

    Format the ebsr regNo

---
+++ test/module/Api/src/Service/Ebsr/Filter/Format/ExistingRegNoTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format;
+
+use Dvsa\Olcs\Api\Service\Ebsr\Filter\Format\ExistingRegNo;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class ExistingRegNoTest
+ * @package Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format
+ */
+class ExistingRegNoTest extends TestCase
+{
+    /**
+     * Tests filter
+     */
+    public function testFilter()
+    {
+        $sut = new ExistingRegNo();
+
+        $licNo = 123;
+        $routeNo = 45;
+        $expected = $licNo . '/' . $routeNo;
+
+        $value = [
+            'licNo' => $licNo,
+            'routeNo' => $routeNo
+        ];
+
+        $result = $sut->filter($value);
+        $this->assertEquals($expected, $result['existingRegNo']);
+    }
+}
