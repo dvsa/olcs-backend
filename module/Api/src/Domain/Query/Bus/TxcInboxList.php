000a742d3ca05f44f95c9a55deed00f9ce3d3b9a
Shaun Lizzio <shaun@lizzio.co.uk>
2016-01-26 10:10:34 +0000

    Added new queries injected with Organisation OR LA

---
+++ module/Api/src/Domain/Query/Bus/TxcInboxList.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * TxcInboxList
+ */
+namespace Dvsa\Olcs\Api\Domain\Query\Bus;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\OrderedTrait;
+use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
+use Dvsa\Olcs\Transfer\Query\PagedTrait;
+
+/**
+ * TxcInboxList
+ */
+final class TxcInboxList extends AbstractQuery implements PagedQueryInterface, OrderedQueryInterface
+{
+    use PagedTrait;
+    use OrderedTrait;
+
+    /**
+     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
+     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     * @Transfer\Optional
+     */
+    protected $localAuthority = null;
+
+    /**
+     * @var string
+     * @Transfer\Optional
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({
+     *      "name":"Zend\Validator\InArray",
+     *      "options": {
+     *          "haystack": {
+     *              "ebsrt_new", "ebsrt_refresh"
+     *          }
+     *      }
+     * })
+     */
+    protected $subType;
+
+    /**
+     * @var string
+     * @Transfer\Optional
+     * @Transfer\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Transfer\Validator({
+     *      "name":"Zend\Validator\InArray",
+     *      "options": {
+     *          "haystack": {
+     *              "ebsrs_expired", "ebsrs_expiring", "ebsrs_processed", "ebsrs_published", "ebsrs_expiring",
+     * "ebsrs_validated", "ebsrs_processing", "ebsrs_publishing", "ebsrs_submitted", "ebsrs_submitting",
+     * "ebsrs_validating",  "ebsrs_distributed", "ebsrs_distributing"
+     *          }
+     *      }
+     * })
+     */
+    protected $status;
+
+    /**
+     * @return string
+     */
+    public function getSubType()
+    {
+        return $this->subType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLocalAuthority()
+    {
+        return $this->localAuthority;
+    }
+}
