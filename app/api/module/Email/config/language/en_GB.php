200a577ab87a32833382942e3cbfa9fd4d1fd909
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-05 17:21:31 +0000

    Subject entry for ebsr failure email

--- app/api/module/Email/config/language/en_GB.php
+++ app/api/module/Email/config/language/en_GB.php
@@ -8,6 +8,7 @@ return array(
     'email.ebsr-refused.subject' => 'EBSR Application Refused - %s - EBSR ID: %s',
     'email.ebsr-registered.subject' => 'EBSR Application Registered - %s - EBSR ID: %s',
     'email.ebsr-withdrawn.subject' => 'EBSR Application Withdrawn - %s - EBSR ID: %s',
+    'email.ebsr-failed.subject' => 'EBSR Application Failed - %s - EBSR ID: %s',
     'email.inbox-reminder.continuation.subject' => 'Operator licence continuation/expiry - Reminder',
     'email.inbox-reminder.standard.subject' => 'Operator licensing information - Reminder',
     'email.licensing-information.continuation.subject' => 'Operator licence continuation/expiry',
