ed45406f84a0b0eb6faaaabc29eda1f636f38383
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-05 16:31:27 +0000

    Added command to send ebsr error emails

---
+++ module/Api/src/Domain/Command/Email/SendEbsrErrors.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Send email to notify of EBSR errors
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Email;
+
+use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
+
+/**
+ * Send email to notify of EBSR errors
+ */
+final class SendEbsrErrors extends AbstractIdOnlyCommand
+{
+
+}
