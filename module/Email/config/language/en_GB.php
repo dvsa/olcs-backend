94fa1e2ca2c33b6122e002536eb127543b0e454c
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-09 10:05:41 +0000

    Added ebsr subject when there is no bus reg id available

--- module/Email/config/language/en_GB.php
+++ module/Email/config/language/en_GB.php
@@ -9,6 +9,7 @@ return array(
     'email.ebsr-registered.subject' => 'EBSR Application Registered - %s - EBSR ID: %s',
     'email.ebsr-withdrawn.subject' => 'EBSR Application Withdrawn - %s - EBSR ID: %s',
     'email.ebsr-failed.subject' => 'EBSR Application Failed - %s - EBSR ID: %s',
+    'email.ebsr-failed-no-bus-reg.subject' => 'EBSR Application Failed - EBSR ID: %s',
     'email.inbox-reminder.continuation.subject' => 'Operator licence continuation/expiry - Reminder',
     'email.inbox-reminder.standard.subject' => 'Operator licensing information - Reminder',
     'email.licensing-information.continuation.subject' => 'Operator licence continuation/expiry',
