9debc8b374503fe408a66aace35de6e3ff7d4863
Alex P <alex.peshkov@valtech.co.uk>
2016-02-11 12:10:14 +0000

    OLCS-4005 new validator added

---
+++ app/api/test/module/Api/src/Domain/Validation/Handlers/Misc/CanManageUserInternalTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Can Manage User Internal Test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Validation\Handlers\Misc;
+
+use Dvsa\Olcs\Api\Entity\User\Permission;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\OlcsTest\Api\Domain\Validation\Handlers\AbstractHandlerTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanManageUserInternal;
+
+/**
+ * Can Manage User Internal Test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class CanManageUserInternalTest extends AbstractHandlerTestCase
+{
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new CanManageUserInternal();
+
+        parent::setUp();
+    }
+
+    public function testIsValid()
+    {
+        /** @var CommandInterface $dto */
+        $dto = m::mock(CommandInterface::class);
+
+        $this->setIsGranted(Permission::INTERNAL_USER, true);
+        $this->setIsGranted(Permission::CAN_MANAGE_USER_INTERNAL, true);
+
+        $this->assertTrue($this->sut->isValid($dto));
+    }
+
+    public function testIsNotValid()
+    {
+        /** @var CommandInterface $dto */
+        $dto = m::mock(CommandInterface::class);
+
+        $this->setIsGranted(Permission::INTERNAL_USER, true);
+        $this->setIsGranted(Permission::CAN_MANAGE_USER_INTERNAL, false);
+
+        $this->assertFalse($this->sut->isValid($dto));
+    }
+}
