741aec3d25e84ef96c2ed2d016c3f1db6d554dd5
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-15 10:54:44 +0000

    creation of a serious infringement

--- app/api/test/module/Api/src/Entity/Si/SeriousInfringementEntityTest.php
+++ app/api/test/module/Api/src/Entity/Si/SeriousInfringementEntityTest.php
@@ -2,10 +2,15 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Si;
 
+use Dvsa\Olcs\Api\Entity\Cases\Cases as CaseEntity;
+use Dvsa\Olcs\Api\Entity\ContactDetails\Country as CountryEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiCategory as SiCategoryEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiCategoryType as SiCategoryTypeEntity;
 use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Si\SeriousInfringement as Entity;
 use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
+use Dvsa\Olcs\Api\Entity\ContactDetails\Country;
 use Mockery as m;
 
 /**
@@ -23,6 +28,41 @@ class SeriousInfringementEntityTest extends EntityTester
     protected $entityClass = Entity::class;
 
     /**
+     * Tests creation of a serious infringement
+     */
+    public function testCreate()
+    {
+        $case = m::mock(CaseEntity::class);
+        $checkDate = new \DateTime('2015-12-25');
+        $infringementDate = new \DateTime('2015-12-26');
+        $memberStateCode = m::mock(CountryEntity::class);
+        $siCategory = m::mock(SiCategoryEntity::class);
+        $siCategoryType = m::mock(SiCategoryTypeEntity::class);
+        $notificationNumber = '0ffefb6b-6344-4a60-9a53-4381c32f98d9';
+        $workflowId = '20776dc3-5fe7-42d5-b554-09ad12fa25c4';
+
+        $entity = new SeriousInfringement(
+            $case,
+            $checkDate,
+            $infringementDate,
+            $memberStateCode,
+            $siCategory,
+            $siCategoryType,
+            $notificationNumber,
+            $workflowId
+        );
+
+        $this->assertEquals($case, $entity->getCase());
+        $this->assertEquals($checkDate, $entity->getCheckDate());
+        $this->assertEquals($infringementDate, $entity->getInfringementDate());
+        $this->assertEquals($memberStateCode, $entity->getMemberStateCode());
+        $this->assertEquals($siCategory, $entity->getSiCategory());
+        $this->assertEquals($siCategoryType, $entity->getSiCategoryType());
+        $this->assertEquals($notificationNumber, $entity->getNotificationNumber());
+        $this->assertEquals($workflowId, $entity->getWorkflowId());
+    }
+
+    /**
      * tests updateErruResponse
      */
     public function testUpdateErruResponse()
@@ -30,7 +70,7 @@ class SeriousInfringementEntityTest extends EntityTester
         $user = m::mock(UserEntity::class);
         $date = new \DateTime();
 
-        $entity = new SeriousInfringement();
+        $entity = m::mock(SeriousInfringement::class)->makePartial();
 
         $entity->updateErruResponse($user, $date);
 
