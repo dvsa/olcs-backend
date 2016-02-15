6d4c037af50474e4ec207af5ee2a0eeabfe5b519
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-15 14:58:44 +0000

    Added siCategory and siCategoryType repos

---
+++ module/Api/src/Domain/Repository/SiCategoryType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Repository;
+
+use Dvsa\Olcs\Api\Entity\Si\SiCategoryType as Entity;
+
+/**
+ * SiCategoryType
+ */
+class SiCategoryType extends AbstractRepository
+{
+    protected $entity = Entity::class;
+}
