8053b6ca9f518b028fc7312b24b6ada86e32ecd0
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-16 14:47:08 +0000

    Input factory for national register (pre doctrine)

---
+++ app/api/module/Api/src/Service/Nr/InputFilter/ComplianceEpisodeInputFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Nr\InputFilter;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Service\InputFilter\Input;
+use Olcs\XmlTools\Filter\MapXmlFile;
+use Dvsa\Olcs\Api\Service\Nr\Filter\Vrm as VrmFilter;
+use Dvsa\Olcs\Api\Service\Nr\Validator\Vrm as VrmValidator;
+use Dvsa\Olcs\Api\Service\Nr\Filter\Format\IsExecuted;
+
+/**
+ * Class ComplianceEpisodeInputFactory
+ * @package Dvsa\Olcs\Api\Service\Nr\InputFilter
+ */
+class ComplianceEpisodeInputFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $service = new Input('compliance_episode');
+
+        /** @var \Olcs\XmlTools\Filter\MapXmlFile $mapXmlFile */
+        $mapXmlFile = $serviceLocator->get('FilterManager')->get(MapXmlFile::class);
+        $mapXmlFile->setMapping($serviceLocator->get('ComplianceEpisodeXmlMapping'));
+
+        $filterChain = $service->getFilterChain();
+        $filterChain->attach($mapXmlFile);
+        $filterChain->attach($serviceLocator->get('FilterManager')->get(IsExecuted::class));
+        $filterChain->attach($serviceLocator->get('FilterManager')->get(VrmFilter::class));
+
+        $validatorChain = $service->getValidatorChain();
+        $validatorChain->attach($serviceLocator->get('ValidatorManager')->get(VrmValidator::class));
+
+        return $service;
+    }
+}
