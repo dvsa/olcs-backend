3330861cf69a4d52c55da2b118b9ed1e68fe7a41
Alex P <alex.peshkov@valtech.co.uk>
2016-02-17 11:29:07 +0000

    OLCS-11304 Revisit TA_Address bookmark

--- app/api/module/Api/src/Service/Document/Bookmark/TaAddress.php
+++ app/api/module/Api/src/Service/Document/Bookmark/TaAddress.php
@@ -3,37 +3,61 @@
 namespace Dvsa\Olcs\Api\Service\Document\Bookmark;
 
 use Dvsa\Olcs\Api\Service\Document\Bookmark\Base\DynamicBookmark;
-use Dvsa\Olcs\Api\Domain\Query\Bookmark\LicenceBundle as Qry;
+use Dvsa\Olcs\Api\Domain\Query\Bookmark\LicenceBundle as LicenceQry;
+use Dvsa\Olcs\Api\Domain\Query\Bookmark\UserBundle as UserQry;
 
 /**
  * Traffic Area Address bookmark
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
 class TaAddress extends DynamicBookmark
 {
     public function getQuery(array $data)
     {
-        $bundle = [
+        $userBundle = [
+            'contactDetails' => [
+                'address'
+            ],
+            'team' => [
+                'trafficArea'
+            ]
+        ];
+        $licenceBundle = [
             'trafficArea' => [
                 'contactDetails' => [
                     'address'
                 ]
             ]
         ];
-        return Qry::create(['id' => $data['licence'], 'bundle' => $bundle]);
+        $userQry = UserQry::create(['id' => $data['user'], 'bundle' => $userBundle]);
+        $licenceQry = LicenceQry::create(['id' => $data['licence'], 'bundle' => $licenceBundle]);
+
+        return [$userQry, $licenceQry];
     }
 
     public function render()
     {
-        $trafficArea = $this->data['trafficArea'];
-        $address = isset($trafficArea['contactDetails']['address']) ? $trafficArea['contactDetails']['address'] : [];
+        $user = $this->data[0];
+        $licence = $this->data[1];
+
+        if (isset($user['team']['trafficArea']) && isset($user['contactDetails']['address'])) {
+            $trafficArea = $user['team']['trafficArea']['name'];
+            $address = $user['contactDetails']['address'];
+        } elseif (isset($licence['trafficArea']['contactDetails']['address'])) {
+            $trafficArea = $licence['trafficArea']['name'];
+            $address = $licence['trafficArea']['contactDetails']['address'];
+        } else {
+            $trafficArea = isset($licence['trafficArea']['name']) ? $licence['trafficArea']['name'] : '';
+            $address = [];
+        }
 
         return implode(
             "\n",
             array_filter(
                 [
-                    $trafficArea['name'],
+                    $trafficArea,
                     Formatter\Address::format($address)
                 ]
             )
