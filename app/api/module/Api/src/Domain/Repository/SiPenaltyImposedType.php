3a02591f5c26dad7838131215cbadce8e2af7d15
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-17 12:27:11 +0000

    Repositories for siPenaltyImposedType and siPenaltyRequestedType

---
+++ app/api/module/Api/src/Domain/Repository/SiPenaltyImposedType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Repository;
+
+use Dvsa\Olcs\Api\Entity\Si\SiPenaltyImposedType as Entity;
+
+/**
+ * SiPenaltyImposedType
+ */
+class SiPenaltyImposedType extends AbstractRepository
+{
+    protected $entity = Entity::class;
+}
