a13c5588b3004e5a5f5a94d86b14d88014f92508
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-22 18:14:20 +0000

    Fix docblock

--- module/Api/src/Service/Ebsr/InputFilter/XmlStructureInputFactory.php
+++ module/Api/src/Service/Ebsr/InputFilter/XmlStructureInputFactory.php
@@ -9,7 +9,7 @@ use Olcs\XmlTools\Filter\ParseXml;
 use Dvsa\Olcs\Api\Service\InputFilter\Input;
 
 /**
- * Class FileStructureInputFactory
+ * Class XmlStructureInputFactory
  * @package Dvsa\Olcs\Api\Service\Ebsr\InputFilter
  */
 class XmlStructureInputFactory implements FactoryInterface
