8a6aa4730be82ef8c1f4fc01e1e49da78400e27c
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-22 18:16:58 +0000

    Test erru compliance episode xml conforms to the schema

---
+++ app/api/module/Api/src/Service/Nr/InputFilter/XmlStructureInputFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Nr\InputFilter;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Olcs\XmlTools\Validator\Xsd;
+use Olcs\XmlTools\Filter\ParseXmlString;
+use Dvsa\Olcs\Api\Service\InputFilter\Input;
+
+/**
+ * Class XmlStructureInputFactory
+ * @package Dvsa\Olcs\Api\Service\Nr\InputFilter
+ */
+class XmlStructureInputFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $service = new Input('xml_structure');
+
+        $filterChain = $service->getFilterChain();
+        $filterChain->attach($serviceLocator->get('FilterManager')->get(ParseXmlString::class));
+
+        $validatorChain = $service->getValidatorChain();
+
+        $xsdValidator = $serviceLocator->get('ValidatorManager')->get(Xsd::class);
+        $xsdValidator->setXsd('ERRU2MS_Infringement_Req.xsd');
+        $validatorChain->attach($xsdValidator);
+
+        return $service;
+    }
+}
