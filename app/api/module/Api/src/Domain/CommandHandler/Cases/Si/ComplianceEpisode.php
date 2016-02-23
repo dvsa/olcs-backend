80e5d9cb8b2ee49112140b858018466ad716c30a
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-23 10:30:08 +0000

    Add a todo when creating task commands, so we know to change to the team id once we have live data

--- app/api/module/Api/src/Domain/CommandHandler/Cases/Si/ComplianceEpisode.php
+++ app/api/module/Api/src/Domain/CommandHandler/Cases/Si/ComplianceEpisode.php
@@ -378,7 +378,7 @@ final class ComplianceEpisode extends AbstractCommandHandler implements Transact
             'description' => 'ERRU case has been automatically created',
             'actionDate' => date('Y-m-d', strtotime('+7 days')),
             'urgent' => 'Y',
-            'assignedToTeam' => 2,
+            'assignedToTeam' => 2, // @todo this will need changing once we have the real life team data
             'case' => $case->getId(),
             'licence' => $case->getLicence()->getId(),
         ];
