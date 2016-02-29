e1deb701fc184aa71e2fcc5476114e2b5b8c9626
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-29 14:19:17 +0000

    NAtional register validation handlers

--- module/Api/config/validation-map/national-register.config.php
+++ module/Api/config/validation-map/national-register.config.php
@@ -7,5 +7,7 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc;
 
 return [
     // Queries
-    QueryHandler\Nr\ReputeUrl::class => Misc\IsInternalUser::class
+    QueryHandler\Nr\ReputeUrl::class => Misc\IsInternalUser::class,
+    // Commands
+    CommandHandler\Cases\Si\ComplianceEpisode::class => Misc\NoValidationRequired::class //incoming xml from ATOS
 ];
