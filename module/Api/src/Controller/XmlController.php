b0dce3acd630afe08fd8d8deea0e512fb447c78a
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-29 15:17:59 +0000

    New XML controller to handle requests arriving in XML format

---
+++ module/Api/src/Controller/XmlController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Controller;
+
+use Zend\Log\Logger;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Dvsa\Olcs\Api\Domain\Exception;
+use Dvsa\Olcs\Api\Mvc\Controller\Plugin\Response as ResponsePlugin;
+
+/**
+ * Xml Controller
+ * @method ResponsePlugin response()
+ */
+class XmlController extends AbstractRestfulController
+{
+    /**
+     * @inheritdoc
+     */
+    public function create($data)
+    {
+        $dto = $this->params('dto');
+
+        try {
+            $this->handleCommand($dto);
+            return $this->response()->xmlAccepted();
+        } catch (Exception\Exception $ex) {
+            return $this->response()->xmlBadRequest();
+        }
+    }
+
+    /**
+     * @param $dto
+     * @return mixed
+     */
+    protected function handleCommand($dto)
+    {
+        return $this->getServiceLocator()->get('CommandHandlerManager')->handleCommand($dto);
+    }
+}
---
+++ test/module/Api/src/Controller/XmlControllerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace Dvsa\OlcsTest\Api\Controller;
+
+use Dvsa\Olcs\Api\Controller\XmlController;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\CommandHandlerInterface;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Zend\Http\Response as HttpResponse;
+use Dvsa\Olcs\Api\Mvc\Controller\Plugin\Response;
+use Mockery\Adapter\Phpunit\MockeryTestCase as TestCase;
+use Mockery as m;
+use Zend\Mvc\Controller\Plugin\Params;
+use Zend\Mvc\Controller\PluginManager;
+use Dvsa\Olcs\Api\Domain\Exception;
+
+/**
+ * Class XmlControllerTest
+ */
+class XmlControllerTest extends TestCase
+{
+    /**
+     * @param $mockSl
+     * @return XmlController
+     */
+    protected function setupSut($mockSl)
+    {
+        $sut = new XmlController();
+        $sut->setPluginManager($mockSl);
+        $sut->setServiceLocator($mockSl);
+        return $sut;
+    }
+
+    /**
+     * Tests create when a valid response is returned
+     */
+    public function testCreate()
+    {
+        $validResponse = new HttpResponse();
+        $validResponse->setStatusCode(HttpResponse::STATUS_CODE_202);
+        $command = m::mock(CommandInterface::class);
+        $result = new Result();
+
+        $mockResponse = m::mock(Response::class);
+        $mockResponse->shouldReceive('xmlAccepted')->andReturn($validResponse);
+
+        $mockParams = m::mock(Params::class);
+        $mockParams->shouldReceive('__invoke')->with('dto')->andReturn($command);
+
+        $mockCommandHandler = m::mock(CommandHandlerInterface::class);
+        $mockCommandHandler->shouldReceive('handleCommand')->with($command)->andReturn($result);
+
+        $mockSl = $this->getMockSl($mockResponse, $mockParams, $mockCommandHandler);
+
+        $sut = $this->setupSut($mockSl);
+
+        $controllerResponse = $sut->create([]);
+
+        $this->assertSame($validResponse, $controllerResponse);
+    }
+
+    /**
+     * Tests create when an exception is thrown
+     */
+    public function testCreateWithException()
+    {
+        $badRequestResponse = new HttpResponse();
+        $badRequestResponse->setStatusCode(HttpResponse::STATUS_CODE_400);
+        $command = m::mock(CommandInterface::class);
+
+        $mockResponse = m::mock(Response::class);
+        $mockResponse->shouldReceive('xmlBadRequest')->andReturn($badRequestResponse);
+
+        $mockParams = m::mock(Params::class);
+        $mockParams->shouldReceive('__invoke')->with('dto')->andReturn($command);
+
+        $mockCommandHandler = m::mock(CommandHandlerInterface::class);
+        $mockCommandHandler->shouldReceive('handleCommand')
+            ->with($command)
+            ->andThrow('\Dvsa\Olcs\Api\Domain\Exception\Exception');
+
+        $mockSl = $this->getMockSl($mockResponse, $mockParams, $mockCommandHandler);
+
+        $sut = $this->setupSut($mockSl);
+
+        $controllerResponse = $sut->create([]);
+
+        $this->assertSame($badRequestResponse, $controllerResponse);
+    }
+
+
+    /**
+     * @param $mockResponse
+     * @param $mockParams
+     * @param $mockCommandHandler
+     * @return m\MockInterface
+     */
+    protected function getMockSl($mockResponse, $mockParams, $mockCommandHandler)
+    {
+        $mockSl = m::mock(PluginManager::class);
+        $mockSl->shouldReceive('get')->with('response', null)->andReturn($mockResponse);
+        $mockSl->shouldReceive('get')->with('params', null)->andReturn($mockParams);
+        $mockSl->shouldReceive('get')->with('CommandHandlerManager')->andReturn($mockCommandHandler);
+        $mockSl->shouldReceive('setController');
+
+        return $mockSl;
+    }
+}
