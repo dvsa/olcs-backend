cb4d5e6402cd776ffe5d8876e3deee009bca5b4b
Mat Evans <mat.evans@valtech.co.uk>
2016-03-01 10:40:24 +0000

    OLCS-12111 Migrate scanning web service into backend

---
+++ module/Scanning/src/Controller/AbstractController.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Abstract controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Dvsa\Olcs\Scanning\Controller;
+
+use Olcs\Logging\Log\Logger;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+/**
+ * Abstract controller
+ *
+ * @NOTE This abstract extends zends abstract restful controller to standardise response
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+abstract class AbstractController extends AbstractRestfulController
+{
+    public function create($data)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function delete($id)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function deleteList()
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function get($id)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function getList()
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function head($id = null)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function options()
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function patch($id, $data)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function replaceList($data)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function patchList($data)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function update($id, $data)
+    {
+        return $this->respondError(405, 'Method Not Allowed');
+    }
+
+    public function notFoundAction()
+    {
+        return $this->respondError(404, 'Page Not Found');
+    }
+
+    protected function respondError($code, $message)
+    {
+        $this->getResponse()
+            ->setStatusCode($code)
+            ->getHeaders()
+            ->addHeaderLine('Content-Type', 'application/problem+json');
+
+        $data = ['status' => $code, 'title' => $message];
+
+        $this->debug('Responding with ' . $code . ': ' . $message, $data);
+
+        return $this->respond($data);
+    }
+
+    protected function respond($data)
+    {
+        return new JsonModel($data);
+    }
+
+    protected function debug($message, $data = [])
+    {
+        return Logger::debug(
+            $message,
+            [
+                'data' => $data
+            ]
+        );
+    }
+}
---
+++ module/Scanning/src/Module.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dvsa\Olcs\Scanning;
+
+/**
+ * Scanning Module
+ *
+ * @author Mat Evans <mat.evans@valtech.co.uk>
+ */
+class Module
+{
+    public function getConfig()
+    {
+        $base = include __DIR__ . '/../config/module.config.php';
+        return $base;
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ ,
+                ),
+            ),
+        );
+    }
+}
