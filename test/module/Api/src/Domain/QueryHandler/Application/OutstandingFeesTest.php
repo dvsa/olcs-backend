f6b5140f41614baa26267a7d33cc794793f16c34
Alex P <alex.peshkov@valtech.co.uk>
2016-03-02 11:23:06 +0000

    OLCS-11471 Use stored card to submit selfserve application or variation

---
+++ test/module/Api/src/Domain/QueryHandler/Application/OutstandingFeesTest.php
@@ -0,0 +1,84 @@
+<?php
+
+
+/**
+ * Outstanding Fees Test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Application;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\Application\OutstandingFees;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
+use Dvsa\Olcs\Api\Domain\Repository\Application as ApplicationRepo;
+use Dvsa\Olcs\Transfer\Query\Application\OutstandingFees as Qry;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Dvsa\Olcs\Api\Service\FeesHelperService;
+use Mockery as m;
+
+/**
+ * Outstanding Fees Test
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class OutstandingFeesTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new OutstandingFees();
+        $this->mockRepo('Application', ApplicationRepo::class);
+        $this->mockRepo('SystemParameter', \Dvsa\Olcs\Api\Domain\Repository\SystemParameter::class);
+        $this->mockedSmServices['FeesHelperService'] = m::mock(FeesHelperService::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $organisationId = 69;
+
+        $query = Qry::create(['id' => $organisationId, 'hideExpired' => false]);
+
+        $mockApplication = m::mock('Dvsa\Olcs\Api\Domain\QueryHandler\BundleSerializableInterface')
+            ->shouldReceive('getId')
+            ->andReturn($organisationId)
+            ->shouldReceive('serialize')
+            ->getMock();
+
+        $fees = [
+            $this->getMockFee(98, 198),
+            $this->getMockFee(99, 199),
+        ];
+
+        $this->mockedSmServices['FeesHelperService']
+            ->shouldReceive('getOutstandingFeesForApplication')
+            ->with(69)
+            ->andReturn($fees)
+            ->once();
+
+        $this->repoMap['Application']
+            ->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->once()
+            ->andReturn($mockApplication);
+
+        $this->repoMap['SystemParameter']
+            ->shouldReceive('getDisableSelfServeCardPayments')
+            ->once()
+            ->with()
+            ->andReturn(true);
+
+        $result = $this->sut->handleQuery($query);
+
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    private function getMockFee($feeId)
+    {
+        return m::mock()
+            ->shouldReceive('serialize')
+            ->shouldReceive('getId')
+            ->andReturn($feeId)
+            ->getMock();
+    }
+}
