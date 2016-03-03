078dbff888f085c53fa33a2d83337b0f3eb07c66
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-03 17:08:55 +0000

    Implement licence number exists check to be called by ATOS INR service

--- module/Api/config/validation-map/national-register.config.php
+++ module/Api/config/validation-map/national-register.config.php
@@ -10,6 +10,11 @@ return [
     QueryHandler\Nr\ReputeUrl::class => Misc\IsInternalUser::class,
 
     /**
+     * This is for ATOS to call when they verify whether a licence exists prior to sending erru requests
+     */
+    QueryHandler\Licence\Exists::class => Misc\NoValidationRequired::class,
+
+    /**
      * This is incoming xml from ATOS. The xml will have been validated on the transfer side using ZendXml/Security.
      * The schema and other data is validated by national register itself \Dvsa\Olcs\Api\Service\Nr\InputFilter
      */
