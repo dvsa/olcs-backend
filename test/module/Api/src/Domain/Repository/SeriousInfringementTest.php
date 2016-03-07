4535c7fc9d72515107f3c406abf41256157aa021
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-07 12:13:44 +0000

    Can no longer fetch serious infringement by notification number, this field has moved to erru request table

--- test/module/Api/src/Domain/Repository/SeriousInfringementTest.php
+++ test/module/Api/src/Domain/Repository/SeriousInfringementTest.php
@@ -40,48 +40,4 @@ class SeriousInfringementTest extends RepositoryTestCase
 
         $this->assertEquals($expectedQuery, $this->query);
     }
-
-    /**
-     * @dataProvider fetchByNotificationNumberProvider
-     *
-     * @param $results
-     * @param $expectedReturnValue
-     */
-    public function testFetchByNotificationNo($results, $expectedReturnValue)
-    {
-        $notificationNumber = 9435839546;
-        $qb = m::mock(QueryBuilder::class);
-        $repo = m::mock(EntityRepository::class);
-        $comparison = m::mock(Comparison::class);
-
-        $this->em->shouldReceive('getRepository')->with(SiEntity::class)->andReturn($repo);
-
-        $repo->shouldReceive('createQueryBuilder')->with('m')->once()->andReturn($qb);
-
-        $this->queryBuilder->shouldReceive('modifyQuery')->with($qb)->once()->andReturnSelf();
-
-        $qb->shouldReceive('expr->eq')
-            ->with('m.notificationNumber', ':notificationNumber')
-            ->once()
-            ->andReturn($comparison);
-        $qb->shouldReceive('where')->with($comparison)->once()->andReturnSelf();
-        $qb->shouldReceive('setParameter')->with('notificationNumber', $notificationNumber)->once()->andReturnSelf();
-        $qb->shouldReceive('setMaxResults')->with(1)->once()->andReturnSelf();
-        $qb->shouldReceive('getQuery->getResult')->with()->once()->andReturn($results);
-
-        $this->assertSame($expectedReturnValue, $this->sut->fetchByNotificationNumber($notificationNumber));
-    }
-
-    /**
-     * data provider for testFetchByNotificationNumber()
-     *
-     * @return array
-     */
-    public function fetchByNotificationNumberProvider()
-    {
-        return [
-            [[], null],
-            [[0 => 'result'], 'result']
-        ];
-    }
 }
