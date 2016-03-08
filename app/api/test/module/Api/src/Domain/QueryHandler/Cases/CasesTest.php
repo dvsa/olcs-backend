41993f9707462506e97ab6c572498d1c660ce98f
Shaun Lizzio <shaun@lizzio.co.uk>
2016-03-08 15:19:45 +0000

    Fix UT

--- app/api/test/module/Api/src/Domain/QueryHandler/Cases/CasesTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/Cases/CasesTest.php
@@ -5,6 +5,7 @@ namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Cases;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Cases\Cases;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Dvsa\Olcs\Api\Domain\Repository\Cases as CasesRepo;
+use Dvsa\Olcs\Api\Domain\Repository\Note as NoteRepo;
 use Dvsa\Olcs\Transfer\Query\Cases\Cases as Qry;
 use Mockery as m;
 
@@ -19,6 +20,7 @@ class CasesTest extends QueryHandlerTestCase
     {
         $this->sut = new Cases();
         $this->mockRepo('Cases', CasesRepo::class);
+        $this->mockRepo('Note', NoteRepo::class);
 
         parent::setUp();
     }
@@ -26,15 +28,31 @@ class CasesTest extends QueryHandlerTestCase
     public function testHandleQuery()
     {
         $query = Qry::create(['id' => 24]);
+        $noteType = 'note type';
+        $licenceId = 77;
+        $latestNote = 'test note';
+
+        $mockLicence = m::mock(\Dvsa\Olcs\Api\Entity\Licence\Licence::class);
+        $mockLicence->shouldReceive('getId')->andReturn($licenceId);
 
         $mockCase = m::mock(\Dvsa\Olcs\Api\Entity\Cases\Cases::class);
         $mockCase->shouldReceive('serialize')->andReturn(['SERIALIZED']);
+        $mockCase->shouldReceive('getNoteType')->andReturn($noteType);
+        $mockCase->shouldReceive('getLicence')->andReturn($mockLicence);
 
         $this->repoMap['Cases']->shouldReceive('fetchUsingId')->with($query)->andReturn($mockCase);
+        $this->repoMap['Note']->shouldReceive('fetchForOverview')
+            ->with($licenceId, null, $noteType)->andReturn($latestNote);
 
         $result = $this->sut->handleQuery($query);
 
-        $this->assertSame(['SERIALIZED'], $result->serialize());
+        $this->assertSame(
+            [
+                'SERIALIZED',
+                'latestNote' => 'test note'
+            ],
+            $result->serialize()
+        );
         $this->assertInstanceOf('Dvsa\Olcs\Api\Domain\QueryHandler\Result', $result);
     }
 }
