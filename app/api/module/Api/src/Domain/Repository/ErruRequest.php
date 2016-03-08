5947f34546f235936c3a8b9c487737e3a42ba3d9
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-08 13:21:03 +0000

    ErruRequest repository

---
+++ app/api/module/Api/src/Domain/Repository/ErruRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Erru Request
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Dvsa\Olcs\Api\Domain\Repository;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Exception;
+use Dvsa\Olcs\Api\Entity\Si\ErruRequest as Entity;
+
+/**
+ * Erru Request
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class ErruRequest extends AbstractRepository
+{
+    protected $entity = Entity::class;
+
+    /**
+     * Returns whether erru requests exists in the database for this workflow id
+     *
+     * @param string $workflowId
+     * @return bool
+     */
+    public function existsByWorkflowId($workflowId)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->where($qb->expr()->eq($this->alias .'.workflowId', ':workflowId'));
+        $qb->setParameter('workflowId', $workflowId);
+        $qb->setMaxResults(1);
+
+        return count($qb->getQuery()->getResult()) === 1;
+    }
+}
---
+++ app/api/test/module/Api/src/Domain/Repository/ErruRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * ErruRequest test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Repository;
+
+use Doctrine\ORM\Query;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Repository\ErruRequest as ErruRequestRepo;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityRepository;
+use Dvsa\Olcs\Api\Entity\Si\ErruRequest;
+use Doctrine\ORM\Query\Expr\Comparison;
+
+/**
+ * ErruRequest test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class ErruRequestTest extends RepositoryTestCase
+{
+    public function setUp()
+    {
+        $this->setUpSut(ErruRequestRepo::class, true);
+    }
+
+    /**
+     * Test existsByWorkflowId method
+     *
+     * @param array $result
+     * @param bool $recordFound
+     *
+     * @dataProvider existsByWorkflowIdProvider
+     */
+    public function testExistsByWorkflowId($result, $recordFound)
+    {
+        $workflowId = '123456';
+        $qb = m::mock(QueryBuilder::class);
+        $repo = m::mock(EntityRepository::class);
+        $doctrineComparison = m::mock(Comparison::class);
+
+        $this->em->shouldReceive('getRepository')->with(ErruRequest::class)->andReturn($repo);
+
+        $repo->shouldReceive('createQueryBuilder')->with('m')->once()->andReturn($qb);
+
+        $qb->shouldReceive('expr->eq')->with('m.workflowId', ':workflowId')->once()->andReturn($doctrineComparison);
+        $qb->shouldReceive('where')->with($doctrineComparison)->once()->andReturnSelf();
+        $qb->shouldReceive('setParameter')->with('workflowId', $workflowId)->once()->andReturnSelf();
+        $qb->shouldReceive('setMaxResults')->with(1)->once()->andReturnSelf();
+        $qb->shouldReceive('getQuery->getResult')->with()->once()->andReturn($result);
+
+        $this->assertSame($recordFound, $this->sut->existsByWorkflowId($workflowId));
+    }
+
+    /**
+     * Data provider for testExistsByLicNo
+     *
+     * @return array
+     */
+    public function existsByWorkflowIdProvider()
+    {
+        return [
+            [[0 => 'Result'], true],
+            [[], false]
+        ];
+    }
+}
