de0ae97ea0fd4092e6fb95f94baa4b592b671708
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-10 18:01:32 +0000

    Improvement to composer-update.sh to also work with xmltools repo and give more accurate messages about symlink creation

--- composer-update.sh
+++ composer-update.sh
@@ -6,23 +6,30 @@ hadUtilsSymlink=false
 
 if [ -L "vendor/olcs/OlcsCommon" ]
 then
-    echo "Removing symlink"
+    echo "Removing olcs-common symlink"
     hadCommonSymlink=true
     rm vendor/olcs/OlcsCommon
 fi
 
 if [ -L "vendor/olcs/olcs-transfer" ]
 then
-    echo "Removing symlink"
+    echo "Removing olcs-transfer symlink"
     hadTransferSymlink=true
     rm vendor/olcs/olcs-transfer
 fi
 
 if [ -L "vendor/olcs/olcs-utils" ]
 then
-    echo "Removing symlink"
+    echo "Removing olcs-utils symlink"
     hadUtilsSymlink=true
     rm vendor/olcs/olcs-utils
+fi
+
+if [ -L "vendor/olcs/olcs-xmltools" ]
+then
+    echo "Removing olcs-xmltools symlink"
+    hadXmlSymlink=true
+    rm vendor/olcs/olcs-xmltools
 fi
 
 if [ -f composer.phar ] ;
@@ -36,7 +43,7 @@ if [ "$hadCommonSymlink" = true ] || [ "$1" = "--force" ];
 then
     if [ -d "vendor/olcs/OlcsCommon" ];
     then
-        echo "Recreating symlink"
+        echo "Recreating olcs-common symlink"
         rm -rf vendor/olcs/OlcsCommon
         (cd vendor/olcs && ln -s ../../../olcs-common/ OlcsCommon)
     fi
@@ -46,7 +53,7 @@ if [ "$hadTransferSymlink" = true ] || [ "$1" = "--force" ];
 then
     if [ -d "vendor/olcs/olcs-transfer" ] ;
     then
-        echo "Recreating symlink"
+        echo "Recreating olcs-transfer symlink"
         rm -rf vendor/olcs/olcs-transfer
         (cd vendor/olcs && ln -s ../../../olcs-transfer/ olcs-transfer)
     fi
@@ -56,8 +63,18 @@ if [ "$hadUtilsSymlink" = true ] || [ "$1" = "--force" ];
 then
     if [ -d "vendor/olcs/olcs-utils" ] ;
     then
-        echo "Recreating symlink"
+        echo "Recreating olcs-utils symlink"
         rm -rf vendor/olcs/olcs-utils
         (cd vendor/olcs && ln -s ../../../olcs-utils/ olcs-utils)
+    fi
+fi
+
+if [ "$hadXmlSymlink" = true ] || [ "$1" = "--force" ];
+then
+    if [ -d "vendor/olcs/olcs-xmltools" ] ;
+    then
+        echo "Recreating olcs-xmltools symlink"
+        rm -rf vendor/olcs/olcs-xmltools
+        (cd vendor/olcs && ln -s ../../../olcs-xmltools/ olcs-xmltools)
     fi
 fi
