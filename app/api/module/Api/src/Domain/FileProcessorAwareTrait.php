20f59c9adc6b75102618850fee47c36d762ce43d
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-11 18:20:55 +0000

    Make file processor usable in other command handlers

---
+++ app/api/module/Api/src/Domain/FileProcessorAwareTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain;
+
+use Dvsa\Olcs\Api\Service\Ebsr\FileProcessorInterface as FileProcessor;
+
+/**
+ * FileProcessor Aware Trait
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+trait FileProcessorAwareTrait
+{
+    /**
+     * @var FileProcessor
+     */
+    protected $fileProcessor;
+
+    /**
+     * @param FileProcessor $fileProcessor
+     */
+    public function setFileProcessor(FileProcessor $fileProcessor)
+    {
+        $this->fileProcessor = $fileProcessor;
+    }
+
+    /**
+     * @return FileProcessor
+     */
+    public function getFileProcessor()
+    {
+        return $this->fileProcessor;
+    }
+}
