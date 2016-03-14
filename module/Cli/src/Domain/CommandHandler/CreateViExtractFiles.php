3f71fdaeb7bba864a8d08d8d2ba963bad7587dee
Alex P <alex.peshkov@valtech.co.uk>
2016-03-14 17:41:46 +0000

    OLCS-12134 Create Mobile Compliance VI Extract Files - Part 2

--- module/Cli/src/Domain/CommandHandler/CreateViExtractFiles.php
+++ module/Cli/src/Domain/CommandHandler/CreateViExtractFiles.php
@@ -19,6 +19,8 @@ use Dvsa\Olcs\Transfer\Query\QueryInterface;
  */
 final class CreateViExtractFiles extends AbstractCommandHandler
 {
+    const PHP_EOL_WIN = "\r\n";
+
     protected $extraRepos = [
         'ViOcView',
         'ViOpView',
@@ -26,7 +28,7 @@ final class CreateViExtractFiles extends AbstractCommandHandler
         'ViVhlView'
     ];
 
-    protected $exportPath = null;//'/tmp';
+    protected $exportPath = null;
 
     protected $paramMap = [
         'oc' => [
@@ -57,8 +59,8 @@ final class CreateViExtractFiles extends AbstractCommandHandler
 
         $config = $mainServiceLocator->get('Config');
 
-        if (isset($config['batch_config']['vi-extract-files']['export-path'])) {
-            $this->exportPath = $config['batch_config']['vi-extract-files']['export-path'];
+        if (isset($config['vi_extract_files']['export_path'])) {
+            $this->exportPath = $config['vi_extract_files']['export_path'];
         }
 
         return parent::createService($serviceLocator);
@@ -79,13 +81,13 @@ final class CreateViExtractFiles extends AbstractCommandHandler
                 if (!$total) {
                     continue;
                 }
-                $content = implode(PHP_EOL, array_column($results, 'line'));
+                $content = implode(self::PHP_EOL_WIN, array_column($results, 'line'));
                 $success = file_put_contents(
                     $this->exportPath . '/' . $type['prefix'] . ((new \DateTime())->format('Ymd')) . '.dat',
                     $content
                 );
                 if ($success === false) {
-                    $this->result->addMessage(
+                    throw new \Exception(
                         'Error writing record(s) for ' . $type['name'] . ', please check the target path'
                     );
                 } else {
