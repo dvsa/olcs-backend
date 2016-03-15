4ee17bad15deb71e42ccf1cb8cc400d764a58c95
Piotr Filak <piotr.filak@valtech.co.uk>
2016-03-15 15:15:24 +0000

    OLCS-10400 - Irfo continuations - Renew state

--- app/api/module/Api/src/Domain/CommandHandler/Irfo/CreateIrfoPsvAuth.php
+++ app/api/module/Api/src/Domain/CommandHandler/Irfo/CreateIrfoPsvAuth.php
@@ -15,15 +15,14 @@ use Dvsa\Olcs\Api\Entity\Irfo\IrfoPsvAuthNumber;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
 use Dvsa\Olcs\Api\Entity\ContactDetails\Country;
 use Dvsa\Olcs\Transfer\Command\Irfo\CreateIrfoPsvAuth as Cmd;
-use Dvsa\Olcs\Api\Domain\Command\Fee\CreateFee as FeeCreateFee;
-use Dvsa\Olcs\Api\Entity\Fee\Fee;
-use Dvsa\Olcs\Api\Entity\Fee\FeeType as FeeTypeEntity;
 
 /**
  * Create IrfoPsvAuth
  */
 final class CreateIrfoPsvAuth extends AbstractCommandHandler implements TransactionedInterface
 {
+    use IrfoPsvAuthFeeTrait;
+
     protected $repoServiceName = 'IrfoPsvAuth';
 
     protected $extraRepos = ['IrfoPsvAuthNumber', 'FeeType'];
@@ -57,17 +56,8 @@ final class CreateIrfoPsvAuth extends AbstractCommandHandler implements Transact
         $result->addId('irfoPsvAuth', $irfoPsvAuth->getId());
         $result->addMessage('IRFO PSV Auth created successfully');
 
-        // Check if is *not* fee exempt.
-        if ($irfoPsvAuth->getIsFeeExemptApplication() !== 'Y' &&
-            in_array(
-                $irfoPsvAuth->getStatus()->getId(),
-                [IrfoPsvAuth::STATUS_PENDING, IrfoPsvAuth::STATUS_RENEW]
-            )
-        ) {
-            $result->merge($this->createApplicationFee($irfoPsvAuth));
-        } else {
-            $result->merge($this->createExemptFee($irfoPsvAuth));
-        }
+        // generate application fee
+        $result->merge($this->generateApplicationFee($irfoPsvAuth));
 
         return $result;
     }
@@ -128,61 +118,5 @@ final class CreateIrfoPsvAuth extends AbstractCommandHandler implements Transact
         }
 
         return $irfoPsvAuth;
-    }
-
-    /**
-     * Creates outstanding application fee
-     *
-     * @param IrfoPsvAuth $irfoPsvAuth
-     * @return Result
-     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
-     */
-    public function createApplicationFee(IrfoPsvAuth $irfoPsvAuth)
-    {
-        $irfoFeeType = $this->getRepo('FeeType')->getLatestIrfoFeeType(
-            $irfoPsvAuth,
-            $this->getRepo()->getRefdataReference(FeeTypeEntity::FEE_TYPE_IRFOPSVAPP)
-        );
-
-        $feeAmount = (float) $irfoFeeType->getFixedValue();
-
-        $data = [
-            'irfoPsvAuth' => $irfoPsvAuth->getId(),
-            'invoicedDate' => date('Y-m-d'),
-            'description' => $irfoFeeType->getDescription() . ' for Auth ' . $irfoPsvAuth->getId(),
-            'feeType' => $irfoFeeType->getId(),
-            'amount' => $feeAmount,
-            'feeStatus' => Fee::STATUS_OUTSTANDING,
-        ];
-
-        return $this->handleSideEffect(FeeCreateFee::create($data));
-    }
-
-    /**
-     * Creates exempt fee for 0 amount. Marked as PAID
-     *
-     * @param IrfoPsvAuth $irfoPsvAuth
-     * @return Result
-     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
-     */
-    public function createExemptFee(IrfoPsvAuth $irfoPsvAuth)
-    {
-        $irfoFeeType = $this->getRepo('FeeType')->getLatestIrfoFeeType(
-            $irfoPsvAuth,
-            $this->getRepo()->getRefdataReference(FeeTypeEntity::FEE_TYPE_IRFOPSVAPP)
-        );
-
-        $data = [
-            'irfoPsvAuth' => $irfoPsvAuth->getId(),
-            'invoicedDate' => date('Y-m-d'),
-            'description' => $irfoFeeType->getDescription() . ' for Auth ' . $irfoPsvAuth->getId(),
-            'feeType' => $irfoFeeType->getId(),
-            'amount' => 0,
-            'feeStatus' => Fee::STATUS_PAID,
-            'irfoFeeExempt' => 'Y',
-            'waiveReason' => $irfoPsvAuth->getExemptionDetails(),
-        ];
-
-        return $this->handleSideEffect(FeeCreateFee::create($data));
     }
 }
