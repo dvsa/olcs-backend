d7bc43cf89d250ecdc67cb9076f0880ad184ed48
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-16 15:34:17 +0000

    Allows transport manager info to be added to the publication police table

---
+++ app/api/test/module/Api/src/Service/Publication/Context/TransportManager/PersonTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Publication\Context\TransportManager;
+
+use Dvsa\Olcs\Api\Service\Publication\Context\TransportManager\Person as Sut;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Entity\Publication\PublicationLink;
+use Dvsa\Olcs\Api\Entity\Person\Person as PersonEntity;
+use Dvsa\Olcs\Api\Entity\Tm\TransportManager as TmEntity;
+use Dvsa\Olcs\Api\Domain\QueryHandler\QueryHandlerInterface;
+
+/**
+ * Class PersonTest
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class PersonTest extends MockeryTestCase
+{
+    /**
+     * @group publicationFilter
+     *
+     * Test the transport manager name filter
+     */
+    public function testProvideWithTitle()
+    {
+        $id = 66;
+
+        $mockPerson = m::mock(PersonEntity::class);
+        $mockPerson->shouldReceive('getId')->once()->andReturn($id);
+
+        $output = [
+            'tmPeople' => [
+                $id => $mockPerson
+            ]
+        ];
+
+        $expectedOutput = new \ArrayObject($output);
+
+        $mockTm = m::mock(TmEntity::class);
+        $mockTm->shouldReceive('getHomeCd->getPerson')->once()->andReturn($mockPerson);
+
+        $publication = m::mock(PublicationLink::class);
+        $publication->shouldReceive('getTransportManager')->once()->andReturn($mockTm);
+
+        $sut = new Sut(m::mock(QueryHandlerInterface::class));
+        $this->assertEquals($expectedOutput, $sut->provide($publication, new \ArrayObject()));
+    }
+}
