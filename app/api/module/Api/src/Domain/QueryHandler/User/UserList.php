259343dfb2fddbe43f57e8e3e1e46e2c7e253679
Shaun Lizzio <shaun@lizzio.co.uk>
2016-03-21 12:39:04 +0000

    Restored UserList to original code, added where isClosed = 0 to fix bug

--- app/api/module/Api/src/Domain/QueryHandler/User/UserList.php
+++ app/api/module/Api/src/Domain/QueryHandler/User/UserList.php
@@ -10,29 +10,21 @@ namespace Dvsa\Olcs\Api\Domain\QueryHandler\User;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Doctrine\ORM\Query;
-use Dvsa\Olcs\Api\Entity\User\Permission;
-use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
-use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
-use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 
 /**
  * Get a list of Users
  *
  * @author Mat Evans <mat.evans@valtech.co.uk>
- * @author Shaun Lizzio <shaun@lizzio.co.uk>
  */
-class UserList extends AbstractQueryHandler implements AuthAwareInterface
+class UserList extends AbstractQueryHandler
 {
-    use AuthAwareTrait;
-
     protected $repoServiceName = 'User';
 
     public function handleQuery(QueryInterface $query)
     {
-        if (!$this->isGranted(Permission::INTERNAL_USER)) {
-            throw new ForbiddenException('You do not have permission access this data');
-        }
+        // TODO - restrict access to this query
 
+        /** @var LegacyOffence $repo */
         $repo = $this->getRepo();
 
         return [
