b2897bd10a8fd6446d2b486b61babf6c6cffc235
Mat Evans <mat.evans@valtech.co.uk>
2016-03-21 13:55:58 +0000

    OLCS-12289 Align entities due to viAction column being removed from operating centre table

--- module/Api/src/Entity/OperatingCentre/AbstractOperatingCentre.php
+++ module/Api/src/Entity/OperatingCentre/AbstractOperatingCentre.php
@@ -162,15 +162,6 @@ abstract class AbstractOperatingCentre implements BundleSerializableInterface, J
     protected $version = 1;
 
     /**
-     * Vi action
-     *
-     * @var string
-     *
-     * @ORM\Column(type="string", name="vi_action", length=1, nullable=true)
-     */
-    protected $viAction;
-
-    /**
      * Application
      *
      * @var \Doctrine\Common\Collections\ArrayCollection
@@ -644,29 +635,6 @@ abstract class AbstractOperatingCentre implements BundleSerializableInterface, J
     public function getVersion()
     {
         return $this->version;
-    }
-
-    /**
-     * Set the vi action
-     *
-     * @param string $viAction
-     * @return OperatingCentre
-     */
-    public function setViAction($viAction)
-    {
-        $this->viAction = $viAction;
-
-        return $this;
-    }
-
-    /**
-     * Get the vi action
-     *
-     * @return string
-     */
-    public function getViAction()
-    {
-        return $this->viAction;
     }
 
     /**
