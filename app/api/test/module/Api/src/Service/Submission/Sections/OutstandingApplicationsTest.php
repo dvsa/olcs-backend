0ca91d975a8951b09eae54ce697c1f8e287c77ca
Shaun Lizzio <shaun@lizzio.co.uk>
2016-03-24 14:38:28 +0000

    Fixed UT

--- app/api/test/module/Api/src/Service/Submission/Sections/OutstandingApplicationsTest.php
+++ app/api/test/module/Api/src/Service/Submission/Sections/OutstandingApplicationsTest.php
@@ -20,78 +20,77 @@ class OutstandingApplicationsTest extends SubmissionSectionTest
             'tables' => [
                 'outstanding-applications' => [
                     0 => [
-
                         'id' => 1,
                         'version' => 2,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     1 => [
-
                         'id' => 101,
                         'version' => 202,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     2 => [
-
                         'id' => 2,
                         'version' => 4,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     3 => [
-
                         'id' => 102,
                         'version' => 204,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     4 => [
-
                         'id' => 1,
                         'version' => 2,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     5 => [
-
                         'id' => 101,
                         'version' => 202,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     6 => [
-
                         'id' => 2,
                         'version' => 4,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ],
                     7 => [
-
                         'id' => 102,
                         'version' => 204,
-                        'applicationType' => 'TBC',
+                        'applicationType' => 'New',
                         'receivedDate' => '05/05/2014',
                         'oor' => 'Unknown',
-                        'ooo' => 'Unknown'
+                        'ooo' => 'Unknown',
+                        'licNo' => 'OB12345'
                     ]
-
                 ]
             ]
         ]
