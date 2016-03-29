1c02a79aff05f13f8c1280cdd6a95231fc0f1681
Piotr Filak <piotr.filak@valtech.co.uk>
2016-03-29 12:38:55 +0100

    OLCS-10414 - irfo_psv_auth_type.irfo_fee_type made unique

--- module/Api/src/Entity/Irfo/IrfoPsvAuthType.php
+++ module/Api/src/Entity/Irfo/IrfoPsvAuthType.php
@@ -12,6 +12,9 @@ use Doctrine\ORM\Mapping as ORM;
  *    indexes={
  *        @ORM\Index(name="ix_irfo_psv_auth_type_created_by", columns={"created_by"}),
  *        @ORM\Index(name="ix_irfo_psv_auth_type_last_modified_by", columns={"last_modified_by"})
+ *    },
+ *    uniqueConstraints={
+ *        @ORM\UniqueConstraint(name="uk_irfo_psv_auth_type_irfo_fee_type", columns={"irfo_fee_type"})
  *    }
  * )
  */
