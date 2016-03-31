09145d6a718e336f51307f2831a6f957289fbee8
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-03-31 13:44:06 +0100

    Support for bcc, attaching from the document store, and a much more thorough unit test. Waiting for confirmation on what to do about multipart messages

--- module/Email/src/Service/Email.php
+++ module/Email/src/Service/Email.php
@@ -5,6 +5,9 @@ namespace Dvsa\Olcs\Email\Service;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Mail as ZendMail;
+use Zend\Mime\Message as MimeMessage;
+use Zend\Mime\Part as ZendMimePart;
+use Zend\Mime\Mime as ZendMime;
 use Zend\Mail\Transport\Smtp as SmtpTransport;
 use Zend\Mail\Transport\SmtpOptions;
 use Zend\Mail\Transport\Exception\RuntimeException as ZendMailTransportRuntimeException;
@@ -65,6 +68,7 @@ class Email implements FactoryInterface
     }
 
     /**
+     * @todo we should support plain text alternatives to our HTML emails, have raised with Sam
      *
      * @param string $fromEmail From email address
      * @param string $fromName  From name
@@ -73,24 +77,50 @@ class Email implements FactoryInterface
      * @param string $body      Email body
      * @param bool   $isHtml    whether to send as a HTML email
      * @param array  $cc        cc'd addresses
+     * @param array  $bcc       bcc'd addresses
+     * @param array  $docs      attached documents
      */
-    public function send($fromEmail, $fromName, $to, $subject, $body, $isHtml = false, array $cc = [])
-    {
-        if ((bool) $isHtml) {
-            $htmlPart = new \Zend\Mime\Part($body);
-            $htmlPart->type = \Zend\Mime\Mime::TYPE_HTML;
+    public function send(
+        $fromEmail,
+        $fromName,
+        $to,
+        $subject,
+        $body,
+        $isHtml = false,
+        array $cc = [],
+        array $bcc = [],
+        array $docs = []
+    ) {
+        $messageType = (bool)$isHtml ? ZendMime::TYPE_HTML : ZendMime::TYPE_TEXT;
 
-            $body = new \Zend\Mime\Message();
-            $body->addPart($htmlPart);
+        $messagePart = new ZendMimePart($body);
+        $messagePart->encoding = ZendMime::ENCODING_QUOTEDPRINTABLE;
+        $messagePart->type = $messageType;
+
+        $emailBody = new MimeMessage();
+        $emailBody->addPart($messagePart);
+
+        if (!empty($docs)) {
+            $messageType = ZendMime::MULTIPART_MIXED;
+
+            foreach ($docs as $doc) {
+                $attachment = new ZendMimePart($doc['content']);
+                $attachment->filename    = $doc['fileName'];
+                $attachment->type        = ZendMime::TYPE_OCTETSTREAM;
+                $attachment->encoding    = ZendMime::ENCODING_BASE64;
+                $attachment->disposition = ZendMime::DISPOSITION_ATTACHMENT;
+                $emailBody->addPart($attachment);
+            }
         }
 
         $mail = new ZendMail\Message();
         $mail->setFrom($fromEmail, $fromName);
         $mail->addTo($to);
         $mail->addCc($cc);
-
+        $mail->addBcc($bcc);
         $mail->setSubject($subject);
-        $mail->setBody($body);
+        $mail->setBody($emailBody);
+        $mail->getHeaders()->get('content-type')->setType($messageType);
 
         $trans = $this->getMailTransport();
         $trans->send($mail);
