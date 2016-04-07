36d0b425458763e0ce13842d0ac89329803d624b
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-04-07 15:31:11 +0100

    Commands for creation and emailing of police documents

---
+++ app/api/module/Api/src/Domain/Command/Publication/CreatePoliceDocument.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Create a police document for the publication
+ */
+namespace Dvsa\Olcs\Api\Domain\Command\Publication;
+
+use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
+
+/**
+ * Create a police document for the publication
+ */
+final class CreatePoliceDocument extends AbstractIdOnlyCommand
+{
+    //
+}
