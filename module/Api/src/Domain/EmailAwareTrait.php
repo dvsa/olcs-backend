b169a006d8aea8629f2b341047727f0ec3a7c674
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-04-07 14:50:27 +0100

    New queue aware trait, moved the creation of email queueing from the emailawaretrait

--- module/Api/src/Domain/EmailAwareTrait.php
+++ module/Api/src/Domain/EmailAwareTrait.php
@@ -4,9 +4,6 @@ namespace Dvsa\Olcs\Api\Domain;
 
 use Dvsa\Olcs\Email\Service\TemplateRenderer;
 use Dvsa\Olcs\Email\Data\Message;
-use Dvsa\Olcs\Api\Domain\Command\Queue\Create as CreateQueue;
-use Dvsa\Olcs\Api\Entity\Queue\Queue;
-use Zend\Json\Json as ZendJson;
 
 /**
  * Email Aware
@@ -62,32 +59,5 @@ trait EmailAwareTrait
     {
         $this->getTemplateRendererService()->renderBody($message, $template, $variables, $layout);
         return $this->sendEmail($message);
-    }
-
-    /**
-     * Adds an email to the queue
-     *
-     * @param string $cmdClass
-     * @param array $cmdData
-     * @param int $entityId
-     * @param string|null $processAfterDate
-     * @return CreateQueue
-     */
-    public function emailQueue($cmdClass, array $cmdData, $entityId, $processAfterDate = null)
-    {
-        $options =                     [
-            'commandClass' => $cmdClass,
-            'commandData' => $cmdData,
-        ];
-
-        return CreateQueue::create(
-            [
-                'entityId' => $entityId,
-                'type' => Queue::TYPE_EMAIL,
-                'status' => Queue::STATUS_QUEUED,
-                'options' => ZendJson::encode($options),
-                'processAfterDate' => $processAfterDate
-            ]
-        );
     }
 }
