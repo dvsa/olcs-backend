d529f846506beec8ce15027627da866531d8ea64
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-04-28 12:03:57 +0100

    Emails now work properly and send as multipart text/html

--- app/api/test/module/Email/src/Service/EmailTest.php
+++ app/api/test/module/Email/src/Service/EmailTest.php
@@ -69,57 +69,6 @@ class EmailTest extends MockeryTestCase
     }
 
     /**
-     * Tests sending html email
-     */
-    public function testSendHtml()
-    {
-        $transport = m::mock(TransportInterface::class);
-
-        $this->sut->setMailTransport($transport);
-
-        $transport->shouldReceive('send')
-            ->once()
-            ->with(m::type(Message::class))
-            ->andReturnUsing(
-                function (Message $message) {
-
-                    $content = $message->toString();
-
-                    $parts = explode("\r\n", $content);
-
-                    array_shift($parts);
-
-                    $expected = [
-                        'From: foo <foo@bar.com>',
-                        'To: bar@foo.com',
-                        'Cc: cc@foo.com',
-                        'Bcc: bcc@foo.com',
-                        'Subject: Subject',
-                        'MIME-Version: 1.0',
-                        'Content-Type: ' . ZendMime::TYPE_HTML,
-                        'Content-Transfer-Encoding: ' . ZendMime::ENCODING_QUOTEDPRINTABLE,
-                        '',
-                        'This is the content'
-                    ];
-
-                    $this->assertEquals($expected, $parts);
-                }
-            );
-
-        $this->sut->send(
-            'foo@bar.com',
-            'foo',
-            'bar@foo.com',
-            'Subject',
-            'This is the content',
-            true,
-            ['cc@foo.com'],
-            ['bcc@foo.com'],
-            []
-        );
-    }
-
-    /**
      * Tests sending plain text email
      */
     public function testSendText()
@@ -163,7 +112,7 @@ class EmailTest extends MockeryTestCase
             'bar@foo.com',
             'Subject',
             'This is the content',
-            false,
+            null,
             ['cc@foo.com'],
             ['bcc@foo.com']
         );
@@ -183,29 +132,46 @@ class EmailTest extends MockeryTestCase
             ->with(m::type(Message::class))
             ->andReturnUsing(
                 function (Message $message) {
+                    //expecting 3 parts, multipart text/html followed by two attachments
                     $parts = $message->getBody()->getParts();
-                    $this->assertCount(2, $parts);
+                    $this->assertCount(3, $parts);
 
                     /**
                      * @var ZendMimePart $messagePart
                      * @var ZendMimePart $attachmentPart
                      */
                     $messagePart = $parts[0];
-                    $attachmentPart = $parts[1];
+                    $attachmentPart1 = $parts[1];
+                    $attachmentPart2 = $parts[2];
 
-                    //test part one (the message text)
-                    $this->assertEquals(ZendMime::TYPE_HTML, $messagePart->type);
-                    $this->assertEquals(ZendMime::ENCODING_QUOTEDPRINTABLE, $messagePart->encoding);
-                    $this->assertEquals('msg content', $messagePart->getRawContent());
+                    $expectedPlainText = "Content-Type: " . ZendMime::TYPE_TEXT . "\n" .
+                    "Content-Transfer-Encoding: " . ZendMime::ENCODING_QUOTEDPRINTABLE . "\n\n" .
+                    "plain content";
+
+                    $expectedHtml = "Content-Type: " . ZendMime::TYPE_HTML . "\n" .
+                        "Content-Transfer-Encoding: " . ZendMime::ENCODING_QUOTEDPRINTABLE . "\n\n" .
+                        "html content";
+
+                    //test part one (this is a generated multipart message body, so we check both parts are included)
+                    $this->assertContains($expectedPlainText, $messagePart->getRawContent());
+                    $this->assertContains($expectedHtml, $messagePart->getRawContent());
                     $this->assertInstanceOf(ZendMimePart::class, $messagePart);
 
-                    //test part two (the attachment)
-                    $this->assertEquals(ZendMime::TYPE_OCTETSTREAM, $attachmentPart->type);
-                    $this->assertEquals(ZendMime::ENCODING_BASE64, $attachmentPart->encoding);
-                    $this->assertEquals(ZendMime::DISPOSITION_ATTACHMENT, $attachmentPart->disposition);
-                    $this->assertEquals('docFilename', $attachmentPart->filename);
-                    $this->assertEquals('docContent', $attachmentPart->getRawContent());
-                    $this->assertInstanceOf(ZendMimePart::class, $attachmentPart);
+                    //test part two (the first attachment)
+                    $this->assertEquals(ZendMime::TYPE_OCTETSTREAM, $attachmentPart1->type);
+                    $this->assertEquals(ZendMime::ENCODING_BASE64, $attachmentPart1->encoding);
+                    $this->assertEquals(ZendMime::DISPOSITION_ATTACHMENT, $attachmentPart1->disposition);
+                    $this->assertEquals('docFilename', $attachmentPart1->filename);
+                    $this->assertEquals('docContent', $attachmentPart1->getRawContent());
+                    $this->assertInstanceOf(ZendMimePart::class, $attachmentPart1);
+
+                    //test part three (the second attachment)
+                    $this->assertEquals(ZendMime::TYPE_OCTETSTREAM, $attachmentPart2->type);
+                    $this->assertEquals(ZendMime::ENCODING_BASE64, $attachmentPart2->encoding);
+                    $this->assertEquals(ZendMime::DISPOSITION_ATTACHMENT, $attachmentPart2->disposition);
+                    $this->assertEquals('docFilename2', $attachmentPart2->filename);
+                    $this->assertEquals('docContent2', $attachmentPart2->getRawContent());
+                    $this->assertInstanceOf(ZendMimePart::class, $attachmentPart2);
 
                     /**
                      * @var AddressList $from
@@ -240,12 +206,108 @@ class EmailTest extends MockeryTestCase
             0 => [
                 'content' => 'docContent',
                 'fileName' => 'docFilename'
+            ],
+            1 => [
+                'content' => 'docContent2',
+                'fileName' => 'docFilename2'
             ]
         ];
 
         $cc = ['cc1@foo.com', 'cc2@foo.com'];
         $bcc = ['bcc1@foo.com', 'bcc2@foo.com', 'bcc3@foo.com'];
 
-        $this->sut->send('foo@bar.com', 'foo', 'bar@foo.com', 'msg subject', 'msg content', true, $cc, $bcc, $docs);
+        $this->sut->send(
+            'foo@bar.com',
+            'foo',
+            'bar@foo.com',
+            'msg subject',
+            'plain content',
+            'html content',
+            $cc,
+            $bcc,
+            $docs
+        );
+    }
+
+    /**
+     * Tests sending an email without attachments
+     */
+    public function testSendWithoutAttachments()
+    {
+        $transport = m::mock(TransportInterface::class);
+
+        $this->sut->setMailTransport($transport);
+
+        $transport->shouldReceive('send')
+            ->once()
+            ->with(m::type(Message::class))
+            ->andReturnUsing(
+                function (Message $message) {
+                    //expecting 2 parts, text and html
+                    $parts = $message->getBody()->getParts();
+                    $this->assertCount(2, $parts);
+
+                    /**
+                     * @var ZendMimePart $plainPart
+                     * @var ZendMimePart $htmlPart
+                     */
+                    $plainPart = $parts[0];
+                    $htmlPart = $parts[1];
+
+                    //test part one (plain text)
+                    $this->assertEquals('plain content', $plainPart->getRawContent());
+                    $this->assertEquals(ZendMime::TYPE_TEXT, $plainPart->type);
+                    $this->assertEquals(ZendMime::ENCODING_QUOTEDPRINTABLE, $plainPart->encoding);
+                    $this->assertInstanceOf(ZendMimePart::class, $plainPart);
+
+                    //test part two (html)
+                    $this->assertEquals('html content', $htmlPart->getRawContent());
+                    $this->assertEquals(ZendMime::TYPE_HTML, $htmlPart->type);
+                    $this->assertEquals(ZendMime::ENCODING_QUOTEDPRINTABLE, $htmlPart->encoding);
+                    $this->assertInstanceOf(ZendMimePart::class, $htmlPart);
+
+                    /**
+                     * @var AddressList $from
+                     * @var AddressList $toList
+                     * @var AddressList $ccList
+                     * @var AddressList $bccList
+                     */
+                    $headers = $message->getHeaders();
+                    $from = $headers->get('from')->getAddressList();
+                    $toList = $headers->get('to')->getAddressList();
+                    $ccList = $headers->get('cc')->getAddressList();
+                    $bccList = $headers->get('bcc')->getAddressList();
+
+                    //test mail headers
+                    $this->assertEquals(ZendMime::MULTIPART_ALTERNATIVE, $headers->get('content-type')->getType());
+                    $this->assertEquals('msg subject', $headers->get('subject')->getFieldValue());
+                    $this->assertEquals(true, $from->has('foo@bar.com'));
+                    $this->assertEquals(1, $from->count());
+                    $this->assertEquals(true, $toList->has('bar@foo.com'));
+                    $this->assertEquals(1, $toList->count());
+                    $this->assertEquals(true, $ccList->has('cc1@foo.com'));
+                    $this->assertEquals(true, $ccList->has('cc2@foo.com'));
+                    $this->assertEquals(2, $ccList->count());
+                    $this->assertEquals(true, $bccList->has('bcc1@foo.com'));
+                    $this->assertEquals(true, $bccList->has('bcc2@foo.com'));
+                    $this->assertEquals(true, $bccList->has('bcc3@foo.com'));
+                    $this->assertEquals(3, $bccList->count());
+                }
+            );
+
+        $cc = ['cc1@foo.com', 'cc2@foo.com'];
+        $bcc = ['bcc1@foo.com', 'bcc2@foo.com', 'bcc3@foo.com'];
+
+        $this->sut->send(
+            'foo@bar.com',
+            'foo',
+            'bar@foo.com',
+            'msg subject',
+            'plain content',
+            'html content',
+            $cc,
+            $bcc,
+            []
+        );
     }
 }
