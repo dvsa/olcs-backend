ed0d00eda3131027306120f378d875dd11794e17
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-09 15:37:29 +0100

    UT coverage

--- app/api/module/Api/src/Domain/QueryHandler/Cases/Pi/PiDefinitionList.php
+++ app/api/module/Api/src/Domain/QueryHandler/Cases/Pi/PiDefinitionList.php
@@ -3,25 +3,13 @@
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Cases\Pi;
 
 use Doctrine\ORM\Query;
-use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Dvsa\Olcs\Api\Domain\Repository\PiDefinition as PiDefinitionRepo;
 
 /**
  * Pi Definition List QueryHandler
  */
-final class PiDefinitionList extends AbstractQueryHandler
+final class PiDefinitionList extends AbstractListQueryHandler
 {
     protected $repoServiceName = 'PiDefinition';
-
-    public function handleQuery(QueryInterface $query)
-    {
-        /** @var PiDefinitionRepo $repo */
-        $repo = $this->getRepo();
-
-        return [
-            'result' => $this->resultList($repo->fetchList($query, Query::HYDRATE_OBJECT)),
-            'count' => $repo->fetchCount($query)
-        ];
-    }
 }
