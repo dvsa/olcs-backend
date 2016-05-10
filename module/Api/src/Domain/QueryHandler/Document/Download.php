b79e6fc7f1befb74d4712b5448c9129f6c1f3516
Mat Evans <mat.evans@valtech.co.uk>
2016-05-10 16:22:48 +0100

    OLCS-12744 Prevent download document from looking up record in DB as not required

--- module/Api/src/Domain/QueryHandler/Document/Download.php
+++ module/Api/src/Domain/QueryHandler/Document/Download.php
@@ -10,8 +10,6 @@ namespace Dvsa\Olcs\Api\Domain\QueryHandler\Document;
 use Dvsa\Olcs\Api\Domain\Exception\NotFoundException;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Dvsa\Olcs\Transfer\Query\Document\Download as Qry;
-use Dvsa\Olcs\Api\Entity\Doc\Document as DocumentEntity;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Dvsa\Olcs\Api\Domain\UploaderAwareInterface;
 use Dvsa\Olcs\Api\Domain\UploaderAwareTrait;
@@ -28,36 +26,21 @@ class Download extends AbstractQueryHandler implements UploaderAwareInterface
     protected $repoServiceName = 'Document';
 
     /**
-     * @param Qry $query
-     * @return \Dvsa\Olcs\Api\Domain\QueryHandler\Result
+     * @param QueryInterface $query
+     * @return array
+     * @throws NotFoundException
      */
     public function handleQuery(QueryInterface $query)
     {
-        /** @var DocumentEntity[] $documents */
-        $documents = $this->getRepo()->fetchByIdentifier($query->getIdentifier());
-
-        if (empty($documents)) {
-            throw new NotFoundException();
-        }
-
-        $document = $documents[0];
-
-        $fullFileName = $document->getFilename();
-        $fileName = basename($fullFileName);
-
-        $file = $this->getUploader()->download($document->getIdentifier());
+        $file = $this->getUploader()->download($query->getIdentifier());
 
         if ($file === null) {
             throw new NotFoundException();
         }
 
-        return $this->result(
-            $document,
-            [],
-            [
-                'fileName' => $fileName,
-                'content' => base64_encode($file->getContent())
-            ]
-        );
+        return [
+            'fileName' => basename($query->getIdentifier()),
+            'content' => base64_encode($file->getContent()),
+        ];
     }
 }
