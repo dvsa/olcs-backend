f97cbc6613fc228c91bba8f4b08d025ba5999306
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-05-13 18:25:58 +0100

    Added logging and extra validation to transxchange client

--- test/module/Api/src/Service/Ebsr/TransExchangeClientTest.php
+++ test/module/Api/src/Service/Ebsr/TransExchangeClientTest.php
@@ -6,6 +6,8 @@ use Dvsa\Olcs\Api\Service\Ebsr\TransExchangeClient;
 use Mockery\Adapter\Phpunit\MockeryTestCase as TestCase;
 use Mockery as m;
 use Olcs\XmlTools\Filter\MapXmlFile;
+use Olcs\XmlTools\Filter\ParseXmlString;
+use Olcs\XmlTools\Validator\Xsd;
 use Zend\Http\Client as RestClient;
 use Zend\Http\Request;
 use Zend\Http\Response;
@@ -17,28 +19,77 @@ use Zend\ServiceManager\ServiceLocatorInterface;
  */
 class TransExchangeClientTest extends TestCase
 {
+    /**
+     * Tests making a request
+     */
     public function testMakeRequest()
     {
         $requestBody = 'body';
-
+        $responseContent = '<success></success>';
         $result = [
             'success' => true
         ];
+
+        $domDocument = new \DOMDocument();
 
         $mockRequest = m::mock(Request::class);
         $mockRequest->shouldReceive('setContent')->with($requestBody);
 
         $mockResponse = m::mock(Response::class);
-        $mockResponse->shouldReceive('getContent')->andReturn('<success></success>');
+        $mockResponse->shouldReceive('getContent')->andReturn($responseContent);
 
         $mockClient = m::mock(RestClient::class);
         $mockClient->shouldReceive('getRequest')->andReturn($mockRequest);
         $mockClient->shouldReceive('send')->andReturn($mockResponse);
 
-        $mockFilter = m::mock(MapXmlFile::class);
-        $mockFilter->shouldReceive('filter')->with(m::type('DOMDocument'))->andReturn($result);
+        $mockParser = m::mock(ParseXmlString::class);
+        $mockParser->shouldReceive('filter')->once()->with($responseContent)->andReturn($domDocument);
 
-        $sut = new TransExchangeClient($mockClient, $mockFilter);
+        $mockXsd = m::mock(Xsd::class);
+        $mockXsd->shouldReceive('isValid')->once()->with($domDocument)->andReturn(true);
+
+        $mockFilter = m::mock(MapXmlFile::class);
+        $mockFilter->shouldReceive('filter')->once()->with($domDocument)->andReturn($result);
+
+        $sut = new TransExchangeClient($mockClient, $mockFilter, $mockParser, $mockXsd);
+
+        $this->assertEquals($result, $sut->makeRequest($requestBody));
+    }
+
+    /**
+     * Tests exception thrown when request not valid
+     *
+     * @expectedException \Dvsa\Olcs\Api\Domain\Exception\RestResponseException
+     */
+    public function testMakeRequestThrowsException()
+    {
+        $requestBody = 'body';
+        $responseContent = '<success></success>';
+        $result = [
+            'success' => true
+        ];
+
+        $domDocument = new \DOMDocument();
+
+        $mockRequest = m::mock(Request::class);
+        $mockRequest->shouldReceive('setContent')->with($requestBody);
+
+        $mockResponse = m::mock(Response::class);
+        $mockResponse->shouldReceive('getContent')->andReturn($responseContent);
+
+        $mockClient = m::mock(RestClient::class);
+        $mockClient->shouldReceive('getRequest')->andReturn($mockRequest);
+        $mockClient->shouldReceive('send')->andReturn($mockResponse);
+
+        $mockParser = m::mock(ParseXmlString::class);
+        $mockParser->shouldReceive('filter')->once()->with($responseContent)->andReturn($domDocument);
+
+        $mockXsd = m::mock(Xsd::class);
+        $mockXsd->shouldReceive('isValid')->once()->with($domDocument)->andReturn(false);
+
+        $mockFilter = m::mock(MapXmlFile::class);
+
+        $sut = new TransExchangeClient($mockClient, $mockFilter, $mockParser, $mockXsd);
 
         $this->assertEquals($result, $sut->makeRequest($requestBody));
     }
