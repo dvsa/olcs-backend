aa69159baf62cf45c896811d24e3ffce42047f52
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-05-13 17:52:07 +0100

    Added schema xsd for transxchange publisher

--- module/Api/config/module.config.php
+++ module/Api/config/module.config.php
@@ -636,6 +636,8 @@ return [
         'http://www.w3.org/2001/xml.xsd' => __DIR__ . '/../data/ebsr/xsd/xml.xsd',
         'http://www.transxchange.org.uk/schema/2.1/TransXChange_registration.xsd' =>
             __DIR__ . '/../data/ebsr/xsd/TransXChange_schema_2.1/TransXChange_registration.xsd',
+        'http://www.transxchange.org.uk/schema/2.1/publisher/3.1.2/TransXChangePublisherService.xsd' =>
+            __DIR__ . '/../data/ebsr/xsd/TransXChange_schema_2.1/TransXChangePublisherService.xsd',
         'https://webgate.ec.testa.eu/erru/1.0' => __DIR__ . '/../data/nr/xsd/ERRU2MS_Infringement_Req.xsd'
     ],
     'validators' => [
