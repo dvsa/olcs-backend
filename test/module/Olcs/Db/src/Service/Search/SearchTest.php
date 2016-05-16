22f26be04e4e217c50a58925b72c9401a50916fa
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-16 13:35:01 +0100

    Fixed UT

--- test/module/Olcs/Db/src/Service/Search/SearchTest.php
+++ test/module/Olcs/Db/src/Service/Search/SearchTest.php
@@ -261,7 +261,7 @@ class SearchTest extends TestCase
                         'should' => [
                             $this->generateMatch('_all', 'FOO BAR'),
                             $this->generateMatch('correspondence_postcode', 'FOO BAR'),
-                            $this->generateMatch('application_id', 'FOO BAR'),
+                            $this->generateMatch('app_id', 'FOO BAR'),
                             $this->generateWildcard('org_name_wildcard', 'foo bar*', 2.0)
                         ]
                     ]
