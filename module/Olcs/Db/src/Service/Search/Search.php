2a1bed440f8479f269b01441213638ad40091332
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-16 08:54:09 +0100

    Separated application and case queries

--- module/Olcs/Db/src/Service/Search/Search.php
+++ module/Olcs/Db/src/Service/Search/Search.php
@@ -152,6 +152,16 @@ class Search
 
                 break;
             case 'application':
+                $correspondencePostcodeQuery = new Query\Match();
+                $correspondencePostcodeQuery->setField('correspondence_postcode', $search);
+                $queryBool->addShould($correspondencePostcodeQuery);
+
+                $applicationIdQuery = new Query\Match();
+                $applicationIdQuery->setField('application_id', $search);
+                $queryBool->addShould($applicationIdQuery);
+
+                $queryBool->addShould($this->generateOrgNameWildcardQuery($search));
+                break;
             case 'case':
                 $correspondencePostcodeQuery = new Query\Match();
                 $correspondencePostcodeQuery->setField('correspondence_postcode', $search);
@@ -220,6 +230,7 @@ class Search
 
                 break;
             case 'busreg':
+
                 $queryMatch = new Query\Match();
                 $queryMatch->setFieldQuery('reg_no', $search);
                 $queryMatch->setFieldBoost('reg_no', 2);
