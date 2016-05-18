eb7792d035984cddaf371a1252f22c05a221476f
Piotr Filak <piotr.filak@valtech.co.uk>
2016-05-18 15:34:23 +0100

    OLCS-12795 - Add an SI

---
+++ app/api/module/Api/src/Domain/CommandHandler/Cases/Si/UpdateSi.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Si;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Domain\Exception;
+use Dvsa\Olcs\Api\Entity\ContactDetails\Country as CountryEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiCategory as SiCategoryEntity;
+use Dvsa\Olcs\Api\Entity\Si\SiCategoryType as SiCategoryTypeEntity;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+
+/**
+ * UpdateSi
+ */
+final class UpdateSi extends AbstractCommandHandler
+{
+    protected $repoServiceName = 'SeriousInfringement';
+
+    /**
+     * Update Erru applied penalty
+     *
+     * @param CommandInterface $command
+     * @return Result
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        $si = $this->getRepo()->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
+
+        if ($si->getCase()->isErru()) {
+            throw new Exception\ValidationException(['This is an ERRU case']);
+        }
+
+        $si->update(
+            new \DateTime($command->getCheckDate()),
+            new \DateTime($command->getInfringementDate()),
+            $this->getRepo()->getReference(SiCategoryEntity::class, SiCategoryEntity::ERRU_DEFAULT_CATEGORY),
+            $this->getRepo()->getReference(SiCategoryTypeEntity::class, $command->getSiCategoryType())
+        );
+
+        $si->setMemberStateCode(
+            $this->getRepo()->getReference(CountryEntity::class, $command->getMemberStateCode())
+        );
+
+        if ($command->getNotificationNumber() !== null) {
+            $si->setNotificationNumber($command->getNotificationNumber());
+        }
+
+        if ($command->getReason() !== null) {
+            $si->setReason($command->getReason());
+        }
+
+        $this->getRepo()->save($si);
+
+        $result = new Result();
+        $result->addId('si', $si->getId());
+        $result->addMessage('Serious Infringement updated');
+
+        return $result;
+    }
+}
