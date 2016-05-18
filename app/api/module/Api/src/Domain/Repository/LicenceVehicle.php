705ad4a2e089a02ad6025b56b9226dc99b86a369
Alex P <alex.peshkov@valtech.co.uk>
2016-05-18 15:02:19 +0100

    OLCS-12806 Vehicle details not displayed on PSV Variations

--- app/api/module/Api/src/Domain/Repository/LicenceVehicle.php
+++ app/api/module/Api/src/Domain/Repository/LicenceVehicle.php
@@ -82,13 +82,14 @@ class LicenceVehicle extends AbstractRepository
      * Create paginated query - vehicles data for PSV application
      *
      * @param QueryInterface $query
-     * @param $applicationId
+     * @param int $applicationId
+     * @param int $licenceId
      * @return QueryBuilder
      */
-    public function createPaginatedVehiclesDataForApplicationQueryPsv(QueryInterface $query, $applicationId)
+    public function createPaginatedVehiclesDataForApplicationQueryPsv(QueryInterface $query, $applicationId, $licenceId)
     {
         $qb = $this->createFilteredQueryForLvaPsv($query);
-        $qb->andWhere($qb->expr()->eq('m.application', ':application'));
+        $qb->andWhere($qb->expr()->eq('m.licence', ':licence'));
 
         $qb->andWhere(
             $qb->expr()->orX(
@@ -98,6 +99,7 @@ class LicenceVehicle extends AbstractRepository
         );
 
         $qb->setParameter('application', $applicationId);
+        $qb->setParameter('licence', $licenceId);
 
         return $qb;
     }
