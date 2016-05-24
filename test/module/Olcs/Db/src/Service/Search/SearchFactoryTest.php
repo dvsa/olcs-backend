922536be63fb90e95f4c883ba1c92a97b581b4ae
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-24 13:57:03 +0100

    Added filter for SS searches

--- test/module/Olcs/Db/src/Service/Search/SearchFactoryTest.php
+++ test/module/Olcs/Db/src/Service/Search/SearchFactoryTest.php
@@ -4,6 +4,7 @@ namespace OlcsTest\Db\Service\Search;
 
 use Olcs\Db\Service\Search\SearchFactory;
 use Mockery as m;
+use ZfcRbac\Service\AuthorizationService;
 
 /**
  * Class SearchFactoryTest
@@ -14,14 +15,17 @@ class SearchFactoryTest extends \PHPUnit_Framework_TestCase
     public function testCreateService()
     {
         $mockClient = m::mock('Elastica\Client');
+        $mockAuthService = m::mock(AuthorizationService::class);
 
         $mockSl = m::mock('Zend\ServiceManager\ServiceLocatorInterface');
         $mockSl->shouldReceive('get')->with('ElasticSearch\Client')->andReturn($mockClient);
+        $mockSl->shouldReceive('get')->with(AuthorizationService::class)->andReturn($mockAuthService);
 
         $sut = new SearchFactory();
         $service = $sut->createService($mockSl);
 
         $this->assertInstanceOf('Olcs\Db\Service\Search\Search', $service);
         $this->assertSame($mockClient, $service->getClient());
+        $this->assertSame($mockAuthService, $service->getAuthService());
     }
 }
