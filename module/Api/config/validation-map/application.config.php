58db420579115b75493107290058ee842532a105
Alex P <alex.peshkov@valtech.co.uk>
2016-05-31 12:16:15 +0100

    OLCS-12887 validation hanlders fixed

--- module/Api/config/validation-map/application.config.php
+++ module/Api/config/validation-map/application.config.php
@@ -3,12 +3,15 @@
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsSystemUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalOrSystemUser;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanAccessApplicationWithId;
 
 return [
     CommandHandler\Application\Schedule41Approve::class => IsInternalUser::class,
     CommandHandler\Application\Schedule41Refuse::class  => IsInternalUser::class,
-    CommandHandler\Application\Schedule41Cancel::class  => IsInternalUser::class,
+    CommandHandler\Application\Schedule41Cancel::class  => IsInternalOrSystemUser::class,
     CommandHandler\Application\Schedule41Reset::class   => IsInternalUser::class,
+    CommandHandler\Application\NotTakenUpApplication::class => IsSystemUser::class,
     QueryHandler\Application\OutstandingFees::class     => CanAccessApplicationWithId::class,
 ];
