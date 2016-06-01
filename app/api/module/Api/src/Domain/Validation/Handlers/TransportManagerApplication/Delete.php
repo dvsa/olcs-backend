488fbe0e9b750575b225999cae466306d2a18416
Alex P <alex.peshkov@valtech.co.uk>
2016-06-01 14:23:52 +0100

    OLCS-12887 validator fixed

--- app/api/module/Api/src/Domain/Validation/Handlers/TransportManagerApplication/Delete.php
+++ app/api/module/Api/src/Domain/Validation/Handlers/TransportManagerApplication/Delete.php
@@ -3,19 +3,28 @@
 namespace Dvsa\Olcs\Api\Domain\Validation\Handlers\TransportManagerApplication;
 
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\AbstractHandler;
+use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
+use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
+use Dvsa\Olcs\Api\Rbac\PidIdentityProvider as PidIdentityProviderEntity;
 
 /**
  * Delete TMA
  *
  * @author Mat Evans <mat.evans@valtech.co.uk>
  */
-class Delete extends AbstractHandler
+class Delete extends AbstractHandler implements AuthAwareInterface
 {
+    use AuthAwareTrait;
+
     /**
      * @inheritdoc
      */
     public function isValid($dto)
     {
+        $userId = $this->getUser() ? $this->getUser()->getId() : null;
+        if ($userId === PidIdentityProviderEntity::SYSTEM_USER) {
+            return true;
+        }
         foreach ($dto->getIds() as $id) {
             if ($this->canAccessTransportManagerApplication($id) !== true) {
                 return false;
