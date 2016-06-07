ace9764ca7c79e4dbcbad550c0f6ba3da226c704
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-06-07 15:59:54 +0100

    Transxchange client now throws specific transxchange exception

---
+++ app/api/module/Api/src/Domain/Exception/TransxchangeException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Exception;
+
+/**
+ * Exception thrown when we get a transxchange failure
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class TransxchangeException extends \Exception
+{
+
+}
