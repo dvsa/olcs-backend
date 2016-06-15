6024bd323795912e6834ee1357524ae56e92016b
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-06-15 16:29:05 +0100

    Added exception for Inr client

---
+++ module/Api/src/Domain/Exception/InrClientException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Exception;
+
+/**
+ * Exception thrown when we get a failure related to the INR client
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class InrClientException extends \Exception
+{
+
+}
