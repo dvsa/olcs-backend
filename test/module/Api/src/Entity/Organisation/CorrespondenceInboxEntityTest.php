a90f8b18b92abfaabd1326e7d73be4ab24917ff0
Mat Evans <mat.evans@valtech.co.uk>
2016-06-20 11:34:54 +0100

    OMT-612 Validate access to correspondence inbox

--- test/module/Api/src/Entity/Organisation/CorrespondenceInboxEntityTest.php
+++ test/module/Api/src/Entity/Organisation/CorrespondenceInboxEntityTest.php
@@ -5,6 +5,7 @@ namespace Dvsa\OlcsTest\Api\Entity\Organisation;
 use Mockery as m;
 use Dvsa\Olcs\Api\Entity\Doc\Document;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
 use Dvsa\Olcs\Api\Entity\Organisation\CorrespondenceInbox;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Organisation\CorrespondenceInbox as Entity;
@@ -32,5 +33,17 @@ class CorrespondenceInboxEntityTest extends EntityTester
 
         $this->assertSame($licence, $record->getLicence());
         $this->assertSame($document, $record->getDocument());
+    }
+
+    public function testGetRelatedOrganisation()
+    {
+        $organisation = m::mock(Organisation::class);
+        $licence = m::mock(Licence::class);
+        $licence->shouldReceive('getOrganisation')->with()->once()->andReturn($organisation);
+        $document = m::mock(Document::class);
+
+        $sut = new CorrespondenceInbox($licence, $document);
+
+        $this->assertSame($organisation, $sut->getRelatedOrganisation());
     }
 }
