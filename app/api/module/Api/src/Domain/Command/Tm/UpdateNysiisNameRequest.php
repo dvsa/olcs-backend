0c9a4a6eb49093eecb6e5f33716ee136c1f5bab2
Shaun Lizzio <shaun@lizzio.co.uk>
2016-06-21 08:58:34 +0100

    Working backend url

--- app/api/module/Api/src/Domain/Command/Tm/UpdateNysiisNameRequest.php
+++ app/api/module/Api/src/Domain/Command/Tm/UpdateNysiisNameRequest.php
@@ -3,7 +3,6 @@
 namespace Dvsa\Olcs\Api\Domain\Command\Tm;
 
 use Dvsa\Olcs\Api\Domain\Command\AbstractIdOnlyCommand;
-use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
 
 /**
  * Process Nysiis TM Name request
