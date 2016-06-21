c9428e00a41d331b1997253d062ca6da595ca049
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-06-21 13:07:13 +0100

    If the licence number isn't a community licence number, try using the initial value for the licence instead

--- module/Api/src/Service/Nr/Filter/LicenceNumber.php
+++ module/Api/src/Service/Nr/Filter/LicenceNumber.php
@@ -16,20 +16,26 @@ class LicenceNumber extends ZendAbstractFilter
 {
     /**
      * Returns the result of filtering $value
+     * We expect either a community licence e.g. UKGB/OB1234567/00000 or a standard licence e.g. OB1234567
      *
-     * @param  array $value
+     * @param array $value input value
+     *
      * @throws Exception
      * @return array
      */
     public function filter($value)
     {
+        //licence number defaults to the the initial value
+        $value['licenceNumber'] = $value['communityLicenceNumber'];
+
+        //see if we can split into three parts, as per a community licence number
         $explodedCommunityLicence = explode('/', $value['communityLicenceNumber']);
 
+        //if we have three parts, assume a community licence and extract the middle part
         if (count($explodedCommunityLicence) === 3) {
             $value['licenceNumber'] = $explodedCommunityLicence[1];
-            return $value;
         }
 
-        throw new Exception('Could not extract the licence number from community licence');
+        return $value;
     }
 }
