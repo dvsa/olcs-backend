de7654623a1343abc9b1b94fc95788b7251e24f6
Shaun Lizzio <shaun@lizzio.co.uk>
2016-06-21 08:58:02 +0100

    Added config for Nysiis transfer command

--- app/api/module/Api/config/command-map.config.php
+++ app/api/module/Api/config/command-map.config.php
@@ -803,7 +803,7 @@ return [
     TransferCommand\Tm\Merge::class => CommandHandler\Tm\Merge::class,
     TransferCommand\Tm\Unmerge::class => CommandHandler\Tm\Unmerge::class,
     TransferCommand\Tm\UndoDisqualification::class => CommandHandler\Tm\UndoDisqualification::class,
-    Command\Tm\UpdateNysiisNameRequest::class => CommandHandler\Tm\UpdateNysiisName::class,
+    TransferCommand\Tm\UpdateNysiisName::class => CommandHandler\Tm\UpdateNysiisName::class,
 
     // Task
     TransferCommand\Task\CloseTasks::class => CommandHandler\Task\CloseTasks::class,
