fa243acc61e5a4c26ffc4a88df819d0e107319eb
Mat Evans <mat.evans@valtech.co.uk>
2016-06-27 10:53:43 +0100

    OLCS-13136 Document create/upload API validators

---
+++ app/api/module/Api/src/Domain/Validation/Validators/CanAccessStatement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\Validation\Validators;
+
+/**
+ * Can Access Statement
+ */
+class CanAccessStatement extends AbstractCanAccessEntity
+{
+    protected $repo = 'Statement';
+}
