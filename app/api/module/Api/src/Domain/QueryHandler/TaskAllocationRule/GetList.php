3de6a305118e9a765d05142f82f6335a420ec747
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-06-30 10:38:12 +0100

    OLCS-13105: remove soft-deleted user from result record set

--- app/api/module/Api/src/Domain/QueryHandler/TaskAllocationRule/GetList.php
+++ app/api/module/Api/src/Domain/QueryHandler/TaskAllocationRule/GetList.php
@@ -2,7 +2,9 @@
 
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\TaskAllocationRule;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Entity;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 /**
@@ -14,11 +16,34 @@ class GetList extends AbstractQueryHandler
 {
     protected $repoServiceName = 'TaskAllocationRule';
 
+    /**
+     * @param \Dvsa\Olcs\Transfer\Query\TaskAllocationRule\GetList $query
+     * @return array
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
     public function handleQuery(QueryInterface $query)
     {
+        /** @var  \Dvsa\Olcs\Api\Domain\Repository\TaskAllocationRule $repo */
+        $repo = $this->getRepo();
+
+        $repo->disableSoftDeleteable();
+
+        $result = $repo->fetchList($query, \Doctrine\ORM\Query::HYDRATE_OBJECT);
+
+        //  remove deleted users
+        /** @var  Entity\Task\TaskAllocationRule $rule */
+        foreach ($result as $rule) {
+            $this->cleanDeletedUser($rule);
+
+            /** @var Entity\Task\TaskAlphaSplit $split */
+            foreach ($rule->getTaskAlphaSplits() as $split) {
+                $this->cleanDeletedUser($split);
+            }
+        }
+
         return [
             'result' => $this->resultList(
-                $this->getRepo()->fetchList($query, \Doctrine\ORM\Query::HYDRATE_OBJECT),
+                $result,
                 [
                     'category',
                     'team',
@@ -27,8 +52,22 @@ class GetList extends AbstractQueryHandler
                     'taskAlphaSplits' => ['user' => ['contactDetails' => ['person']]],
                 ]
             ),
-            'count' => $this->getRepo()->fetchCount($query),
-            'count-unfiltered' => $this->getRepo()->hasRows($query),
+            'count' => $repo->fetchCount($query),
+            'count-unfiltered' => $repo->hasRows($query),
         ];
+    }
+
+    /**
+     * Remove user entity from entity if user has DeletedDate not null (soft-deleted)
+     *
+     * @param Entity\Task\TaskAllocationRule|Entity\Task\TaskAlphaSplit $entity
+     */
+    private function cleanDeletedUser($entity)
+    {
+        $user = $entity->getUser();
+
+        if ($user !== null && $user->getDeletedDate() !== null) {
+            $entity->setUser(null);
+        }
     }
 }
