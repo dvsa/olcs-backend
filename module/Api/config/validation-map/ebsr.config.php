f49fb09f865cb5ee32604f3579d8aa2d84624e76
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-07-04 18:04:04 +0100

    Query to bring back an EBSR submission

--- module/Api/config/validation-map/ebsr.config.php
+++ module/Api/config/validation-map/ebsr.config.php
@@ -8,6 +8,7 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsOperatorUser;
 return [
     QueryHandler\Bus\Ebsr\TxcInboxList::class                                       => IsLocalAuthorityUser::class,
     QueryHandler\Bus\Ebsr\EbsrSubmissionList::class                                 => IsOperatorUser::class,
+    QueryHandler\Bus\Ebsr\EbsrSubmission::class                                     => IsOperatorUser::class,
     QueryHandler\Bus\Ebsr\OrganisationUnprocessedList::class                        => IsOperatorUser::class,
     CommandHandler\Bus\Ebsr\QueuePacks::class                                       => IsOperatorUser::class
 ];
