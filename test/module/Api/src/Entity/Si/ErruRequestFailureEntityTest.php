1c25452ba60216774256167598fd5ab15346d72c
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-07-08 16:23:38 +0100

    Code to create an erru request failure

--- test/module/Api/src/Entity/Si/ErruRequestFailureEntityTest.php
+++ test/module/Api/src/Entity/Si/ErruRequestFailureEntityTest.php
@@ -4,6 +4,9 @@ namespace Dvsa\OlcsTest\Api\Entity\Si;
 
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Si\ErruRequestFailure as Entity;
+use Dvsa\Olcs\Api\Entity\Doc\Document;
+use Zend\Serializer\Adapter\Json;
+use Mockery as m;
 
 /**
  * ErruRequestFailure Entity Unit Tests
@@ -18,4 +21,47 @@ class ErruRequestFailureEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /**
+     * Tests creation of erru request failures
+     */
+    public function testCreate()
+    {
+        $document = m::mock(Document::class);
+
+        $json = new Json();
+
+        $errors = ['foo' => 'bar'];
+        $errorsJson = $json->serialize($errors);
+
+        $input = ['bar' => 'foo'];
+        $inputJson = $json->serialize($input);
+
+        $entity = new Entity($document, $errors, $input);
+
+        $this->assertEquals($document, $entity->getDocument());
+        $this->assertEquals($errorsJson, $entity->getErrors());
+        $this->assertEquals($inputJson, $entity->getInput());
+    }
+
+    /**
+     * Makes sure if the input field is a string then the data is ignored
+     */
+    public function testCreateWithStringInput()
+    {
+        $document = m::mock(Document::class);
+
+        $json = new Json();
+
+        $errors = ['foo' => 'bar'];
+        $errorsJson = $json->serialize($errors);
+
+        $input = 'some string';
+
+        $entity = new Entity($document, $errors, $input);
+
+        $this->assertEquals($document, $entity->getDocument());
+        $this->assertEquals($errorsJson, $entity->getErrors());
+        $this->assertNull($entity->getInput());
+    }
 }
