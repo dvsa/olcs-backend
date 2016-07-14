8895b428f0a774f2f9361a17bc30a9c501a2c1e9
Mat Evans <mat.evans@valtech.co.uk>
2016-07-14 14:08:34 +0100

    OLCS-13252 Fix permission issue, by allowing System user full access

--- test/module/Api/src/Domain/Validation/Validators/CanAccessOrganisationTest.php
+++ test/module/Api/src/Domain/Validation/Validators/CanAccessOrganisationTest.php
@@ -37,6 +37,7 @@ class CanAccessOrganisationTest extends AbstractValidatorsTestCase
     public function testIsValid($isOwner, $expected)
     {
         $this->setIsGranted(Permission::INTERNAL_USER, false);
+        $this->auth->shouldReceive('getIdentity')->andReturn(null);
         $entity = m::mock(Organisation::class);
 
         $repo = $this->mockRepo('Organisation');
