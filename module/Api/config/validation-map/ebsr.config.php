c3def4bb4eddbb64619406fe76bd647d08023b63
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-07-18 12:20:29 +0100

    olcs-13214 validator added to make sure ebsr submission can only be viewed by the correct people

--- module/Api/config/validation-map/ebsr.config.php
+++ module/Api/config/validation-map/ebsr.config.php
@@ -4,11 +4,12 @@ use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsLocalAuthorityUser;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsOperatorUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Bus\Ebsr\CanAccessEbsrSubmissionWithId;
 
 return [
-    QueryHandler\Bus\Ebsr\TxcInboxList::class                                       => IsLocalAuthorityUser::class,
-    QueryHandler\Bus\Ebsr\EbsrSubmissionList::class                                 => IsOperatorUser::class,
-    QueryHandler\Bus\Ebsr\EbsrSubmission::class                                     => IsOperatorUser::class,
-    QueryHandler\Bus\Ebsr\OrganisationUnprocessedList::class                        => IsOperatorUser::class,
-    CommandHandler\Bus\Ebsr\QueuePacks::class                                       => IsOperatorUser::class
+    QueryHandler\Bus\Ebsr\TxcInboxList::class                                  => IsLocalAuthorityUser::class,
+    QueryHandler\Bus\Ebsr\EbsrSubmissionList::class                            => IsOperatorUser::class,
+    QueryHandler\Bus\Ebsr\EbsrSubmission::class                                => CanAccessEbsrSubmissionWithId::class,
+    QueryHandler\Bus\Ebsr\OrganisationUnprocessedList::class                   => IsOperatorUser::class,
+    CommandHandler\Bus\Ebsr\QueuePacks::class                                  => IsOperatorUser::class
 ];
