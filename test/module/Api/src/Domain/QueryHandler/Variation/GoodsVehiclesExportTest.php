9d4a03f8dc0702263af5133a7f24f398a81debec
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-07-19 10:27:17 +0100

    OLCS-13228: add Goods Vehicle Export Handlers; add unit test; fix annotations;

---
+++ test/module/Api/src/Domain/QueryHandler/Variation/GoodsVehiclesExportTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Variation;
+
+use Doctrine\ORM\QueryBuilder;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Variation\GoodsVehiclesExport;
+use Dvsa\Olcs\Api\Domain\Repository;
+use Dvsa\Olcs\Api\Entity;
+use Dvsa\Olcs\Transfer\Query;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Mockery as m;
+
+/**
+ * @covers \Dvsa\Olcs\Api\Domain\QueryHandler\Variation\GoodsVehiclesExport
+ */
+class GoodsVehiclesExportTest extends QueryHandlerTestCase
+{
+    const ID = 1111;
+    const APP_ID = 8888;
+    const LICENCE_ID = 777;
+
+    /** @var GoodsVehiclesExport|m\MockInterface */
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = m::mock(GoodsVehiclesExport::class . '[getData]')
+            ->shouldAllowMockingProtectedMethods();
+
+        $this->mockRepo('Application', Repository\Application::class);
+        $this->mockRepo('LicenceVehicle', Repository\LicenceVehicle::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = Query\Application\GoodsVehiclesExport::create(['id' => self::ID]);
+
+        /** @var QueryBuilder $mockListQb */
+        $mockListQb = m::mock(QueryBuilder::class);
+
+        $mockLicEntity = m::mock(Entity\Application\Application::class)
+            ->shouldReceive('getId')->once()->andReturn(self::LICENCE_ID)
+            ->getMock();
+
+        $mockAppEntity = m::mock(Entity\Application\Application::class)
+            ->shouldReceive('getId')->once()->andReturn(self::APP_ID)
+            ->shouldReceive('getLicence')->once()->andReturn($mockLicEntity)
+            ->getMock();
+
+        $this->repoMap['Application']
+            ->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn($mockAppEntity);
+
+        $this->repoMap['LicenceVehicle']
+            ->shouldReceive('createPaginatedVehiclesDataForVariationQuery')
+            ->with($query, self::APP_ID, self::LICENCE_ID)
+            ->andReturn($mockListQb);
+
+        $this->sut
+            ->shouldReceive('getData')
+            ->once()
+            ->with($mockListQb)
+            ->andReturn('EXPECT_RESULT');
+
+        //  call & check
+        $actual = $this->sut->handleQuery($query);
+
+        static::assertEquals('EXPECT_RESULT', $actual);
+    }
+}
