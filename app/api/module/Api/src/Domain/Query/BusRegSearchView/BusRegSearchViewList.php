580993cbc21de34bcf89e59d006dd65b4d662271
Shaun Lizzio <shaun@lizzio.co.uk>
2016-07-19 12:07:01 +0100

    Extended query from transfer object

--- app/api/module/Api/src/Domain/Query/BusRegSearchView/BusRegSearchViewList.php
+++ app/api/module/Api/src/Domain/Query/BusRegSearchView/BusRegSearchViewList.php
@@ -7,25 +7,15 @@ use Dvsa\Olcs\Transfer\Query\OrderedQueryInterface;
 use Dvsa\Olcs\Transfer\Query\OrderedTrait;
 use Dvsa\Olcs\Transfer\Query\PagedQueryInterface;
 use Dvsa\Olcs\Transfer\Query\PagedTrait;
+use \Dvsa\Olcs\Transfer\Query\BusRegSearchView\BusRegSearchViewList as BusRegSearchViewTransfer;
 
 /**
  * Class Bus Reg Search View List for Organisation or LA, optionally filtered by licence and bus reg status
  *
  * @Transfer\RouteName("backend/bus-reg-search-view-list")
  */
-class BusRegSearchViewList extends AbstractQuery implements PagedQueryInterface, OrderedQueryInterface
+class BusRegSearchViewList extends BusRegSearchViewTransfer
 {
-    use PagedTrait;
-    use OrderedTrait;
-
-    /**
-     * @var int
-     * @Transfer\Optional
-     * @Transfer\Filter({"name":"Zend\Filter\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\Digits"})
-     * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
-     */
-    protected $licId;
 
     /**
      * @var int
@@ -44,30 +34,6 @@ class BusRegSearchViewList extends AbstractQuery implements PagedQueryInterface,
      * @Transfer\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
      */
     protected $localAuthorityId;
-
-    /**
-     * @var String
-     * @Transfer\Optional
-     * @Transfer\Validator({
-     *     "name":"Zend\Validator\InArray",
-     *     "options": {
-     *          "haystack": {
-     *              "breg_s_admin","breg_s_cancellation","breg_s_cancelled","breg_s_cns","breg_s_curt","breg_s_expired",
-     *              "breg_s_new","breg_s_refused","breg_s_registered","breg_s_revoked","breg_s_surr","breg_s_var",
-     *              "breg_s_withdrawn"
-     *          }
-     *     }
-     * })
-     */
-    protected $busRegStatus;
-
-    /**
-     * @return int
-     */
-    public function getLicId()
-    {
-        return $this->licId;
-    }
 
     /**
      * @return int
@@ -101,13 +67,5 @@ class BusRegSearchViewList extends AbstractQuery implements PagedQueryInterface,
     public function setLocalAuthorityId($localAuthorityId)
     {
         $this->localAuthorityId = $localAuthorityId;
-    }
-
-    /**
-     * @return String
-     */
-    public function getBusRegStatus()
-    {
-        return $this->busRegStatus;
     }
 }
