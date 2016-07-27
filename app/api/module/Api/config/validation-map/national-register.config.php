9e65260a34cefb0c50dd798de913feccf1e07825
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-07-27 16:18:53 +0100

    Compliance episodes and compliance licence checks, may only be invoked by anonymous users

--- app/api/module/Api/config/validation-map/national-register.config.php
+++ app/api/module/Api/config/validation-map/national-register.config.php
@@ -12,13 +12,13 @@ return [
     /**
      * This is for ATOS to call when they verify whether a licence exists prior to sending erru requests
      */
-    QueryHandler\Licence\Exists::class => Misc\NoValidationRequired::class,
+    QueryHandler\Licence\Exists::class => Misc\IsAnonymousUser::class,
 
     /**
      * This is incoming xml from ATOS. The xml will have been validated on the transfer side using ZendXml/Security.
      * The schema and other data is validated by national register itself \Dvsa\Olcs\Api\Service\Nr\InputFilter
      */
-    CommandHandler\Cases\Si\ComplianceEpisodeDocument::class => Misc\NoValidationRequired::class,
+    CommandHandler\Cases\Si\ComplianceEpisodeDocument::class => Misc\IsAnonymousUser::class,
 
     /**
      * Create response xml and send to ATOS
