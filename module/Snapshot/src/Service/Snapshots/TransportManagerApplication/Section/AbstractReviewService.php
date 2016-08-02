112b36829fd06a3fa1c47481665e903e86300a2a
Dmitrij Golubev <dgolubev@DGolubev-pc.home>
2016-08-02 15:37:49 +0100

    OLCS-13385: add method to get file name; show only file name; fix annotation and unit tests;

--- module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Section/AbstractReviewService.php
+++ module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Section/AbstractReviewService.php
@@ -1,17 +1,13 @@
 <?php
 
-/**
- * Abstract Review Service
- *
- * @author Rob Caiger <rob@clocal.co.uk>
- */
 namespace Dvsa\Olcs\Snapshot\Service\Snapshots\TransportManagerApplication\Section;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Dvsa\Olcs\Api\Entity\Person\Person;
+use Dvsa\Olcs\Snapshot\Service\Formatter\Address;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
-use Dvsa\Olcs\Snapshot\Service\Formatter\Address;
 
 /**
  * Abstract Review Service
@@ -22,6 +18,13 @@ abstract class AbstractReviewService implements ReviewServiceInterface, ServiceL
 {
     use ServiceLocatorAwareTrait;
 
+    /**
+     * Format Full name
+     *
+     * @param Person $person Person Entity
+     *
+     * @return string
+     */
     protected function formatPersonFullName(Person $person)
     {
         $parts = [];
@@ -36,6 +39,14 @@ abstract class AbstractReviewService implements ReviewServiceInterface, ServiceL
         return implode(' ', $parts);
     }
 
+    /**
+     * Format date
+     *
+     * @param \DateTime|string $date   Date
+     * @param string           $format Date Format
+     *
+     * @return bool|string
+     */
     protected function formatDate($date, $format = 'd M Y')
     {
         if ($date instanceof \DateTime) {
@@ -45,16 +56,39 @@ abstract class AbstractReviewService implements ReviewServiceInterface, ServiceL
         return date($format, strtotime($date));
     }
 
+    /**
+     * Format address
+     *
+     * @param array|\Dvsa\Olcs\Api\Entity\ContactDetails\Address $address Address data or entity
+     *
+     * @return string
+     */
     protected function formatFullAddress($address)
     {
         return Address::format($address, ['addressFields' => 'FULL']);
     }
 
+    /**
+     * Format address short
+     *
+     * @param array|\Dvsa\Olcs\Api\Entity\ContactDetails\Address $address Address data or entity
+     *
+     * @return string
+     */
     protected function formatShortAddress($address)
     {
         return Address::format($address);
     }
 
+    /**
+     * Filter documents
+     *
+     * @param ArrayCollection $files       List of documents
+     * @param string          $category    Category
+     * @param string          $subCategory Sub category
+     *
+     * @return mixed
+     */
     protected function findFiles($files, $category, $subCategory)
     {
         $criteria = Criteria::create();
@@ -64,18 +98,54 @@ abstract class AbstractReviewService implements ReviewServiceInterface, ServiceL
         return $files->matching($criteria);
     }
 
+    /**
+     * Translate
+     *
+     * @param string $string Text or Tranlation key
+     *
+     * @return string
+     */
     protected function translate($string)
     {
         return $this->getServiceLocator()->get('translator')->translate($string, 'snapshot');
     }
 
+    /**
+     * Translate and replace
+     *
+     * @param string $translationKey Tranlation key
+     * @param array  $arguments      Values for replacement
+     *
+     * @return string
+     */
     protected function translateReplace($translationKey, array $arguments)
     {
         return vsprintf($this->translate($translationKey), $arguments);
     }
 
+    /**
+     * Format Yes/No
+     *
+     * @param string $value Y or N
+     *
+     * @return string
+     */
     protected function formatYesNo($value)
     {
         return $value === 'Y' ? 'Yes' : 'No';
+    }
+
+    /**
+     * Take file name only form file path
+     *
+     * @param string $filePath File path
+     *
+     * @return string
+     */
+    protected function formatFileNameOnly($filePath)
+    {
+        $parts = preg_split('~[\\\\/]~', $filePath);
+
+        return end($parts) ?: '';
     }
 }
