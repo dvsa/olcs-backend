352acac504864e97a899e8a16f5659977f2ed38b
Shaun Lizzio <shaun@lizzio.co.uk>
2016-08-05 10:02:58 +0100

    Added Additional unit tests for Nysiis

--- test/module/Api/src/Service/Data/NysiisFactoryTest.php
+++ test/module/Api/src/Service/Data/NysiisFactoryTest.php
@@ -11,7 +11,6 @@ use Dvsa\Olcs\Api\Service\Data\NysiisFactory;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery as m;
 use Zend\Soap\Client as ZendSoapClient;
-use Dvsa\Olcs\Api\Domain\Exception\NysiisException;
 
 /**
  * NysiisFactory Test
@@ -25,6 +24,32 @@ class NysiisFactoryTest extends MockeryTestCase
         $this->sm = m::mock('\Zend\ServiceManager\ServiceLocatorInterface');
 
         parent::setUp();
+    }
+
+    public function testCreateServiceValidSoapClient()
+    {
+        $config = [
+            'nysiis' => [
+                'wsdl' => [
+                    'uri' => 'wsdlFile'
+                ]
+            ]
+        ];
+        $this->sm->shouldReceive('get')
+            ->with('Config')
+            ->andReturn($config);
+
+        $mockSoapClient = m::mock(\SoapClient::class);
+
+        $nysiisFactory = m::mock('Dvsa\Olcs\Api\Service\Data\NysiisFactory')
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $nysiisFactory->shouldReceive('generateSoapClient')->with($config)->andReturn($mockSoapClient);
+
+        $service = $nysiisFactory->createService($this->sm);
+
+        $this->assertEquals($mockSoapClient, $service->getSoapClient());
+        $this->assertEquals($config, $service->getNysiisConfig());
     }
 
     public function testCreateServiceExceptionLogged()
@@ -43,6 +68,27 @@ class NysiisFactoryTest extends MockeryTestCase
         $sut = new NysiisFactory();
 
         $this->setExpectedException('SoapFault');
+
+        $service = $sut->createService($this->sm);
+
+        $this->assertFalse($service->getSoapClient());
+        $this->assertEquals($config, $service->getNysiisConfig());
+    }
+
+    public function testCreateServiceWithNoWsdlExceptionLogged()
+    {
+        $config = [
+            'nysiis' => [
+                'wsdl' => []
+            ]
+        ];
+        $this->sm->shouldReceive('get')
+            ->with('Config')
+            ->andReturn($config);
+
+        $sut = new NysiisFactory();
+
+        $this->setExpectedException('\Dvsa\Olcs\Api\Domain\Exception\NysiisException');
 
         $service = $sut->createService($this->sm);
 
