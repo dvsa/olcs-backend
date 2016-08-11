0b21d08593cc89e2ca1266f9a3c2652ecc995c20
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-08-11 10:19:06 +0100

    Added a new filter to create data for the ebsr_submission_result column

---
+++ app/api/test/module/Api/src/Service/Ebsr/Filter/Format/SubmissionResultTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format;
+
+use Dvsa\Olcs\Api\Service\Ebsr\Filter\Format\SubmissionResult;
+use Dvsa\Olcs\Api\Entity\Ebsr\EbsrSubmission as EbsrSubmissionEntity;
+use Mockery as m;
+
+/**
+ * Class SubmissionResultTest
+ * @package Dvsa\OlcsTest\Api\Service\Ebsr\Filter\Format
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class SubmissionResultTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests filter
+     *
+     * @dataProvider provideFilter
+     *
+     * @param $submissionDate
+     * @param $rawData
+     * @param $expectedData
+     */
+    public function testFilter($submissionDate, $rawData, $expectedData)
+    {
+        $sut = new SubmissionResult();
+
+        $ebsrSub = m::mock(EbsrSubmissionEntity::class);
+        $ebsrSub->shouldReceive('getSubmittedDate')->once()->andReturn($submissionDate);
+
+        $errors = ['errors'];
+
+        $value = [
+            'errorMessages' => $errors,
+            'ebsrSub' => $ebsrSub,
+            'rawData' => $rawData
+        ];
+
+        $extraBusData = array_merge($expectedData, ['submissionErrors' => $errors]);
+
+        $expectedOutput = [
+            'errors' => $errors,
+            'extra_bus_data' => $extraBusData
+        ];
+
+        $this->assertEquals($expectedOutput, $sut->filter($value));
+    }
+
+    /**
+     * Data provider for testFilter
+     *
+     * @return array
+     */
+    public function provideFilter()
+    {
+        $submissionDate = '2015-12-25 00:00:00';
+        $submissionDateTime = new \DateTime($submissionDate);
+        $formattedSubmissionDate = $submissionDateTime->format(SubmissionResult::DATE_FORMAT);
+
+        $blankExpectedData = [
+            'submissionDate' => $formattedSubmissionDate,
+            'registrationNumber' => SubmissionResult::UNKNOWN_REG_NO,
+            'origin' => SubmissionResult::UNKNOWN_START_POINT,
+            'destination' => SubmissionResult::UNKNOWN_FINISH_POINT,
+            'lineName' => SubmissionResult::UNKNOWN_SERVICE_NO,
+            'startDate' => SubmissionResult::UNKNOWN_START_DATE,
+            'hasBusData' => false
+        ];
+
+        $licNo = 'OB1234567';
+        $routeNo = '8910';
+        $regNo = $licNo . '/' . $routeNo;
+        $startPoint = 'start point';
+        $finishPoint = 'end point';
+
+        $serviceNo = 555;
+        $otherServiceNos = [666, 777];
+
+        $joinedServiceNo = '555 (666, 777)';
+
+        $effectiveDate = '2016-12-26 00:00:00';
+        $effectiveDateTime = new \DateTime($effectiveDate);
+        $formattedEffectiveDateTime = $effectiveDateTime->format(SubmissionResult::DATE_FORMAT);
+
+        $populatedInputData = [
+            'licNo' => $licNo,
+            'routeNo' => $routeNo,
+            'serviceNo' => $serviceNo,
+            'otherServiceNumbers' => $otherServiceNos,
+            'startPoint' => $startPoint,
+            'finishPoint' => $finishPoint,
+            'effectiveDate' => $effectiveDate
+        ];
+
+        $populatedExpectedData = [
+            'submissionDate' => $formattedSubmissionDate,
+            'registrationNumber' => $regNo,
+            'origin' => $startPoint,
+            'destination' => $finishPoint,
+            'lineName' => $joinedServiceNo,
+            'startDate' => $formattedEffectiveDateTime,
+            'hasBusData' => true
+        ];
+
+        return [
+            [$submissionDate, [], $blankExpectedData],
+            [$submissionDateTime, $populatedInputData, $populatedExpectedData]
+        ];
+    }
+}
