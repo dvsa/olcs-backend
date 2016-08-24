0e48515368765608e27db0bfb496eb1510c28f48
Mat Evans <mat.evans@valtech.co.uk>
2016-08-24 10:58:09 +0100

    OLCS-13598 Fix permission denied when downloading migrated TxCInbox document

--- app/api/module/Api/src/Domain/Validation/Validators/CanAccessDocument.php
+++ app/api/module/Api/src/Domain/Validation/Validators/CanAccessDocument.php
@@ -8,4 +8,29 @@ namespace Dvsa\Olcs\Api\Domain\Validation\Validators;
 class CanAccessDocument extends AbstractCanAccessEntity
 {
     protected $repo = 'Document';
+
+    /**
+     * Is Valid
+     *
+     * @param int $entityId Document ID
+     *
+     * @return bool
+     */
+    public function isValid($entityId)
+    {
+        // check if can validate through parent
+        $valid = parent::isValid($entityId);
+        if ($valid) {
+            // if passed validation then return
+            return true;
+        }
+
+        // attempt to find if the document is linked through a txc_inbox
+        $entities = $this->getRepo('TxcInbox')->fetchLinkedToDocument($entityId);
+        if (!empty($entities)) {
+            return $this->isOwner($entities[0]);
+        }
+
+        return false;
+    }
 }
