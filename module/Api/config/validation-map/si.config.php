cdc65db70ed2c270d78e00503f88c76679abe006
Piotr Filak <piotr.filak@valtech.co.uk>
2016-08-30 15:14:59 +0100

    OLCS-11623 - Olcs\Service\Data\SiPenaltyType

--- module/Api/config/validation-map/si.config.php
+++ module/Api/config/validation-map/si.config.php
@@ -5,4 +5,5 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
 
 return [
     QueryHandler\Si\SiCategoryTypeListData::class                           => IsInternalUser::class,
+    QueryHandler\Si\SiPenaltyTypeListData::class                            => IsInternalUser::class,
 ];
