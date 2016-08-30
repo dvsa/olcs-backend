e35ad9479b007b408b2aa86ef61785e80d2b938a
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-08-30 11:50:14 +0100

    OLCS-13511/OLCS-13641: remove unused methods from Document; fix PHP 5.6 issue in AbstractBookmark; add unit tests coverage;

---
+++ app/api/test/module/Api/src/Service/Document/Stub/NamingServiceAwareTraitStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Document\Stub;
+
+use Dvsa\Olcs\Api\Service\Document\NamingServiceAwareTrait;
+
+class NamingServiceAwareTraitStub
+{
+    use NamingServiceAwareTrait;
+}
