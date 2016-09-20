dc66cd1b2cb1236025efd1a9f4b61b863dd6653f
Mat Evans <mat.evans@valtech.co.uk>
2016-09-20 12:32:56 +0100

    OLCS-13830 Use new licence index and query template

---
+++ module/Olcs/Db/src/Service/Search/QueryTemplate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Olcs\Db\Service\Search;
+
+use Elastica\Query;
+
+class QueryTemplate extends Query
+{
+    /**
+     * QueryTemplate constructor.
+     *
+     * @param $filename
+     */
+    public function __construct($filename, $searchTerm)
+    {
+        if (!file_exists($filename)) {
+            throw new \RuntimeException("Query template file '". $filename ."' is missing");
+        }
+
+        $template = str_replace('%SEARCH_TERM%', $searchTerm, file_get_contents($filename));
+
+        $this->_params = json_decode($template, true);
+    }
+}
