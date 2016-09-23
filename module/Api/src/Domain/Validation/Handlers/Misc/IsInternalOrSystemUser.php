d42371118d7c0282bd689c997454fd2a123a6d4e
Piotr Filak <piotr.filak@valtech.co.uk>
2016-09-23 09:34:09 +0100

    OLCS-13892 - IsSystemUser validation

--- module/Api/src/Domain/Validation/Handlers/Misc/IsInternalOrSystemUser.php
+++ module/Api/src/Domain/Validation/Handlers/Misc/IsInternalOrSystemUser.php
@@ -10,7 +10,6 @@ namespace Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc;
 use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\AbstractHandler;
-use Dvsa\Olcs\Api\Rbac\PidIdentityProvider as PidIdentityProviderEntity;
 
 /**
  * Is Internal or System User
@@ -26,7 +25,6 @@ class IsInternalOrSystemUser extends AbstractHandler implements AuthAwareInterfa
      */
     public function isValid($dto)
     {
-        $team = ($this->getUser() && $this->getUser()->getTeam()) ? $this->getUser()->getTeam()->getId() : null;
-        return $this->isInternalUser() || $team === PidIdentityProviderEntity::SYSTEM_TEAM;
+        return $this->isInternalUser() || $this->isSystemUser();
     }
 }
