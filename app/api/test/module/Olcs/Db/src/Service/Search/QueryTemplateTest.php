e4773a2e1ef151f0ccb5b02ea0bc1534127a4afd
Piotr Filak <piotr.filak@valtech.co.uk>
2016-09-26 11:38:40 +0100

    OLCS-13904 - apply aggregations to the new query templates

--- app/api/test/module/Olcs/Db/src/Service/Search/QueryTemplateTest.php
+++ app/api/test/module/Olcs/Db/src/Service/Search/QueryTemplateTest.php
@@ -4,7 +4,6 @@ namespace OlcsTest\Db\Service\Search;
 
 use Olcs\Db\Service\Search\QueryTemplate;
 use Mockery as m;
-use ZfcRbac\Service\AuthorizationService;
 
 /**
  * Class QueryTemplateTest
@@ -19,15 +18,285 @@ class QueryTemplateTest extends m\Adapter\Phpunit\MockeryTestCase
         unset($sut);
     }
 
-    public function testQueryTemplate()
+    /**
+     * @dataProvider queryTemplateDataProvider
+     */
+    public function testQueryTemplate($query, $filters, $dates, $expected)
     {
-        $sut = new QueryTemplate(__DIR__ .'/mock-query-template.json', 'SMITH');
-        $this->assertEquals(['query' => 'SMITH'], $sut->getParam('query'));
+        $sut = new QueryTemplate(__DIR__ .'/mock-query-template.json', $query, $filters, $dates);
+        $this->assertEquals($expected, $sut->getParam('query'));
     }
 
-    public function testQueryTemplateExtendedChars()
+    public function queryTemplateDataProvider()
     {
-        $sut = new QueryTemplate(__DIR__ .'/mock-query-template.json', 'SM"\das\'[]{}ITH');
-        $this->assertEquals(['query' => 'SM"\das\'[]{}ITH'], $sut->getParam('query'));
+        return [
+            // simple query
+            [
+                'SMITH',
+                null,
+                null,
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with special chars
+            [
+                'SM"\das\'[]{}ITH',
+                null,
+                null,
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SM"\das\'[]{}ITH'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with one filter
+            [
+                'SMITH',
+                [
+                    'field_1' => 'value1'
+                ],
+                null,
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'value1'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with multiple filters
+            [
+                'SMITH',
+                [
+                    'field_1' => 'value1',
+                    'field_2' => 'value2',
+                    'field_3' => 'value3',
+                ],
+                null,
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'value1'
+                                        ]
+                                    ],
+                                    [
+                                        'match' => [
+                                            'field_2' => 'value2'
+                                        ]
+                                    ],
+                                    [
+                                        'match' => [
+                                            'field_3' => 'value3'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with from_and_to date range
+            [
+                'SMITH',
+                null,
+                [
+                    'field_1_from_and_to' => '2010-09-30'
+                ],
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => '2010-09-30'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with from only date range
+            [
+                'SMITH',
+                null,
+                [
+                    'field_1_from' => '2010-09-30'
+                ],
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'range' => [
+                                            'field_1' => ['from' => '2010-09-30']
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with from and to date range
+            [
+                'SMITH',
+                null,
+                [
+                    'field_1_from' => '2010-09-30',
+                    'field_1_to' => '2010-10-30'
+                ],
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'range' => [
+                                            'field_1' => ['from' => '2010-09-30', 'to' => '2010-10-30']
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            // query with filters and date ranges
+            [
+                'SMITH',
+                [
+                    'field_1' => 'value1',
+                    'field_2' => 'value2',
+                    'field_3' => 'value3',
+                ],
+                [
+                    'field_1_from_and_to' => '2010-09-30',
+                    'field_2_from' => '2010-09-30',
+                    'field_3_from' => '2010-09-30',
+                    'field_3_to' => '2010-10-30'
+                ],
+                [
+                    'indices' => [
+                        'query' => [
+                            'bool' => [
+                                'should' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'SMITH'
+                                        ]
+                                    ]
+                                ],
+                                'must' => [
+                                    [
+                                        'match' => [
+                                            'field_1' => 'value1'
+                                        ]
+                                    ],
+                                    [
+                                        'match' => [
+                                            'field_2' => 'value2'
+                                        ]
+                                    ],
+                                    [
+                                        'match' => [
+                                            'field_3' => 'value3'
+                                        ]
+                                    ],
+                                    [
+                                        'match' => [
+                                            'field_1' => '2010-09-30'
+                                        ]
+                                    ],
+                                    [
+                                        'range' => [
+                                            'field_2' => ['from' => '2010-09-30']
+                                        ]
+                                    ],
+                                    [
+                                        'range' => [
+                                            'field_3' => ['from' => '2010-09-30', 'to' => '2010-10-30']
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+        ];
     }
 }
