2630cb4d48e518ca31fa3669adca1b1247f80a00
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-10-04 17:21:36 +0100

    Improve transxchange client logging

--- module/Api/src/Service/Ebsr/TransExchangeClient.php
+++ module/Api/src/Service/Ebsr/TransExchangeClient.php
@@ -18,7 +18,7 @@ class TransExchangeClient implements TransExchangeClientInterface
     const REQUEST_MAP_TEMPLATE = 'RequestMap';
     const TIMETABLE_TEMPLATE = 'Timetable';
     const DVSA_RECORD_TEMPLATE = 'DvsaRecord';
-    const TRANSXCHANGE_INVALID_XML = 'TransXchange response did not validate against the schema';
+    const TRANSXCHANGE_INVALID_XML = 'TransXchange response did not validate against the schema: ';
 
     /**
      * @var RestClient
@@ -76,15 +76,16 @@ class TransExchangeClient implements TransExchangeClientInterface
         $response = $this->restClient->send();
         $body = $response->getContent();
 
-        Logger::info('TransXchange response', ['data' => $body]);
+        Logger::info('TransXchange response', ['data' => $response->toString()]);
 
         //security check, and parse into dom document
         $dom = $this->xmlParser->filter($body);
 
         //validate against schema
         if (!($this->xsdValidator->isValid($dom))) {
-            Logger::info('TransXchange error', ['data' => self::TRANSXCHANGE_INVALID_XML]);
-            throw new TransxchangeException(self::TRANSXCHANGE_INVALID_XML);
+            $message = self::TRANSXCHANGE_INVALID_XML . implode(', ', $this->xsdValidator->getMessages());
+            Logger::info('TransXchange error', ['data' => $message]);
+            throw new TransxchangeException($message);
         }
 
         return $this->xmlFilter->filter($dom);
