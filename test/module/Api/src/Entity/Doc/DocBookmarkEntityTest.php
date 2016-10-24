d43ccac1a8f2c2484113d34c19dc7b47453b1f8f
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-10-24 15:28:35 +0100

    OLCS-13703: add unit tests

--- test/module/Api/src/Entity/Doc/DocBookmarkEntityTest.php
+++ test/module/Api/src/Entity/Doc/DocBookmarkEntityTest.php
@@ -2,13 +2,13 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Doc;
 
-use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Entity\Doc\DocBookmark as Entity;
+use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 
 /**
- * DocBookmark Entity Unit Tests
- *
- * Initially auto-generated but won't be overridden
+ * @covers Dvsa\Olcs\Api\Entity\Doc\DocBookmark
+ * @covers Dvsa\Olcs\Api\Entity\Doc\AbstractDocBookmark
  */
 class DocBookmarkEntityTest extends EntityTester
 {
@@ -18,4 +18,13 @@ class DocBookmarkEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    public function testConstructor()
+    {
+        $sut = new Entity();
+
+        $actual = $sut->getDocParagraphBookmarks();
+        static::assertInstanceOf(ArrayCollection::class, $actual);
+        static::assertEmpty($actual);
+    }
 }
--- test/module/Api/src/Entity/Irfo/IrfoPsvAuthNumberEntityTest.php
+++ test/module/Api/src/Entity/Irfo/IrfoPsvAuthNumberEntityTest.php
@@ -2,13 +2,14 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Irfo;
 
+use Dvsa\Olcs\Api\Entity\Irfo\IrfoPsvAuth;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Irfo\IrfoPsvAuthNumber as Entity;
+use Mockery as m;
 
 /**
- * IrfoPsvAuthNumber Entity Unit Tests
- *
- * Initially auto-generated but won't be overridden
+ * @covers Dvsa\Olcs\Api\Entity\Irfo\IrfoPsvAuthNumber
+ * @covers Dvsa\Olcs\Api\Entity\Irfo\AbstractIrfoPsvAuthNumber
  */
 class IrfoPsvAuthNumberEntityTest extends EntityTester
 {
@@ -18,4 +19,30 @@ class IrfoPsvAuthNumberEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    /** @var  IrfoPsvAuth */
+    private $mockIrfoPsvAuth;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mockIrfoPsvAuth = m::mock(IrfoPsvAuth::class);
+    }
+
+    public function testConstructor()
+    {
+        $name = 'unit_Name';
+
+        $sut = new Entity($this->mockIrfoPsvAuth, $name);
+
+        static::assertEquals($this->mockIrfoPsvAuth, $sut->getIrfoPsvAuth());
+        static::assertEquals($name, $sut->getName());
+    }
+
+    public function testGetCalculatedValues()
+    {
+        $actual = (new Entity($this->mockIrfoPsvAuth, ''))->jsonSerialize();
+        static::assertNull($actual['irfoPsvAuth']);
+    }
 }
