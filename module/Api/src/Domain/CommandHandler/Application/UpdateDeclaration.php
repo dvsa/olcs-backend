82eecd69b0f777c736b7344c1579cffaca9a31a2
Alex P <alex.peshkov@valtech.co.uk>
2016-11-02 12:33:09 +0000

    OLCS-14248 Interim fee charged incorrectly on upgrade applications selfserve part done

--- module/Api/src/Domain/CommandHandler/Application/UpdateDeclaration.php
+++ module/Api/src/Domain/CommandHandler/Application/UpdateDeclaration.php
@@ -18,6 +18,7 @@ use Dvsa\Olcs\Api\Domain\Command\Application\CreateFee as CreateFeeCommand;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Dvsa\Olcs\Transfer\Command\Application\UpdateDeclaration as UpdateDeclarationCommand;
 use Dvsa\Olcs\Api\Domain\Command\Application\UpdateApplicationCompletion as UpdateApplicationCompletionCommand;
+use Dvsa\Olcs\Api\Entity\Fee\FeeType as FeeTypeEntity;
 
 /**
  * UpdateDeclaration
@@ -105,15 +106,12 @@ final class UpdateDeclaration extends AbstractCommandHandler implements Transact
     {
         // if interim is requested
         if ($command->getInterimRequested() === 'Y') {
-            // get existing interim fees
-            $outstandingInterimFees = $this->feeRepo->fetchInterimFeesByApplicationId($command->getId(), true);
-            // if there are no existing interim fees then create interim fee
-            if (empty($outstandingInterimFees)) {
+            if ($this->shouldCreateInterimFee($command, $isVariation)) {
                 return $this->handleSideEffect(
                     CreateFeeCommand::create(
                         [
                             'id' => $command->getId(),
-                            'feeTypeFeeType' => \Dvsa\Olcs\Api\Entity\Fee\FeeType::FEE_TYPE_GRANTINT
+                            'feeTypeFeeType' => FeeTypeEntity::FEE_TYPE_GRANTINT
                         ]
                     )
                 );
@@ -122,6 +120,35 @@ final class UpdateDeclaration extends AbstractCommandHandler implements Transact
             return $this->handleSideEffect(
                 CancelAllInterimFeesCommand::create(['id' => $command->getId()])
             );
+        }
+
+        return false;
+    }
+
+    /**
+     * Should be create interim fee
+     *
+     * @param UpdateDeclarationCommand $command     update declaration command
+     * @param bool                     $isVariation is variation
+     *
+     * @return bool
+     */
+    private function shouldCreateInterimFee(UpdateDeclarationCommand $command, $isVariation)
+    {
+        $id = $command->getId();
+
+        $outstandingInterimFees = $this->feeRepo->fetchInterimFeesByApplicationId($id, true);
+        if (!$isVariation && empty($outstandingInterimFees)) {
+            return true;
+        }
+
+        $variationFees = $this->feeRepo->fetchFeeByTypeAndApplicationId(
+            FeeTypeEntity::FEE_TYPE_VAR,
+            $id
+        );
+
+        if ($isVariation && empty($outstandingInterimFees) && !empty($variationFees)) {
+            return true;
         }
 
         return false;
