f80313286fd7961e94f6ea348b9f70235f71c875
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-11-07 18:36:43 +0000

    Factory for nysiis rest client

---
+++ app/api/module/Api/src/Service/Nysiis/NysiisRestClientFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Nysiis;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Http\Client as RestClient;
+use Dvsa\Olcs\Utils\Client\ClientAdapterLoggingWrapper;
+
+/**
+ * Class NysiisRestClientFactory
+ * @package Dvsa\Olcs\Api\Service\Nysiis
+ */
+class NysiisRestClientFactory implements FactoryInterface
+{
+    /**
+     * Create service.
+     *
+     * @param ServiceLocatorInterface $serviceLocator service locator
+     *
+     * @return NysiisRestClient
+     * @throws \RuntimeException
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Config');
+
+        if (!isset($config['nysiis']['rest']['uri'])) {
+            throw new \RuntimeException('Missing nysiis rest client uri');
+        }
+
+        if (!isset($config['nysiis']['rest']['options'])) {
+            throw new \RuntimeException('Missing nysiis rest client options');
+        }
+
+        $httpClient = new RestClient($config['nysiis']['rest']['uri'], $config['nysiis']['rest']['options']);
+
+        $wrapper = new ClientAdapterLoggingWrapper();
+        $wrapper->wrapAdapter($httpClient);
+
+        return new NysiisRestClient($httpClient);
+    }
+}
---
+++ app/api/test/module/Api/src/Service/Nysiis/NysiisRestClientFactoryTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Data;
+
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Service\Nysiis\NysiisRestClientFactory;
+use Dvsa\Olcs\Api\Service\Nysiis\NysiisRestClient;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+
+/**
+ * NysiisRestClientFactory Test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class NysiisRestClientFactoryTest extends MockeryTestCase
+{
+    /**
+     * Tests client created properly
+     */
+    public function testCreateServiceValid()
+    {
+        $config = [
+            'nysiis' => [
+                'rest' => [
+                    'uri' => 'address',
+                    'options' => []
+                ]
+            ]
+        ];
+
+        $mockSl = m::mock(ServiceLocatorInterface::class);
+        $mockSl->shouldReceive('get')->with('Config')->andReturn($config);
+        $sut = new NysiisRestClientFactory();
+        $this->assertInstanceOf(NysiisRestClient::class, $sut->createService($mockSl));
+    }
+
+    /**
+     * Tests exception thrown for missing config
+     *
+     * @param $config
+     * @param $errorMsg
+     *
+     * @dataProvider createServiceFailProvider
+     */
+    public function testCreateServiceMissingConfig($config, $errorMsg)
+    {
+        $this->setExpectedException(\RuntimeException::class, $errorMsg);
+
+        $config = [
+            'nysiis' => [
+                'rest' => $config
+            ]
+        ];
+
+        $mockSl = m::mock(ServiceLocatorInterface::class);
+        $mockSl->shouldReceive('get')->with('Config')->andReturn($config);
+        $sut = new NysiisRestClientFactory();
+        $sut->createService($mockSl);
+    }
+
+    /**
+     * data provider for testing service creation failures
+     */
+    public function createServiceFailProvider()
+    {
+        return [
+            [['uri' => 'address'], 'Missing nysiis rest client options'],
+            [['options' => []], 'Missing nysiis rest client uri']
+        ];
+    }
+
+}
