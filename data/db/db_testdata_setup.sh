72d1c76f9f112b0593aebb13f3bd4dd2b8c82aa3
Mat Evans <mat.evans@valtech.co.uk>
2016-11-16 15:44:30 +0000

    Update build script of dev database to include patches

--- data/db/db_testdata_setup.sh
+++ data/db/db_testdata_setup.sh
@@ -3,35 +3,56 @@
 # causes the shell to exit if any subcommand or pipeline returns a non-zero status.
 set -e
 
+# Check can connect to DB
+if ! mysql -e "exit"
+then
+   echo
+   echo "ERROR Cannot connect to database. Make sure you have a ~/.my.cnf file"
+   echo
+   exit
+fi
+
 # drop/create db
-echo "DatabaseSetup.sql"
-mysql -uroot -ppassword < DatabaseSetup.sql
+echo "Create olcs_be database"
+mysql < DatabaseSetup.sql
 
 # schema
-echo "olcs_schema.sql"
-mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs_schema.sql
+echo "Apply OLCS schema"
+mysql olcs_be < ../../../olcs-etl/olcs_schema.sql
 
 # schema
 # Add manual Views and Stored Procs
+echo "View, procs and functions"
 for sqlFile in ../../../olcs-etl/views_procs/*.sql
 do
-  echo "Executing $sqlFile"
-  mysql -uroot -ppassword -e "use olcs_be;\. $sqlFile"
+  mysql olcs_be < $sqlFile
 done
 
 # refdata
 echo "ref_data.sql"
-mysql -uroot -ppassword olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql
+mysql olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql
 echo "other_ref_data.sql"
-mysql -uroot -ppassword olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/other_ref_data.sql
+mysql olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/other_ref_data.sql
 
 # stub data
 echo "olcs_stub_data.sql"
-mysql -uroot -ppassword olcs_be < ../../../olcs-etl/olcs_stub_data.sql
+mysql olcs_be < ../../../olcs-etl/olcs_stub_data.sql
 
 # test data
 echo "testdata.sql"
-mysql -uroot -ppassword olcs_be < testdata.sql
+mysql olcs_be < testdata.sql
+
+# ETL schema is required by pacthes
+echo "Create ETL schema (used for logging in patches)"
+mysql olcs_be < ../../../olcs-etl/etl-schema.sql
+
+# Apply post-live patches
+echo "Apply post-live patches"
+cd ../../../olcs-etl
+./post-live-patch.sh -d olcs_be -v 4.0.6
+./post-live-patch.sh -d olcs_be -v 4.0.7
+./post-live-patch.sh -d olcs_be -v 4.0.8
+./post-live-patch.sh -d olcs_be -v post-live
 
 sudo service httpd restart
 
