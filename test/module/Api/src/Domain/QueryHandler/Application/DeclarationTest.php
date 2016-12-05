216943af57d8d2a761e3f5ce889cbf950611e784
Alex P <alex.peshkov@valtech.co.uk>
2016-12-05 10:23:58 +0000

    OLCS-14141 unit tests fixed

--- test/module/Api/src/Domain/QueryHandler/Application/DeclarationTest.php
+++ test/module/Api/src/Domain/QueryHandler/Application/DeclarationTest.php
@@ -1,15 +1,12 @@
 <?php
 
-/**
- * DeclarationTest
- *
- * @author Mat Evans <mat.evans@valtech.co.uk>
- */
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Application;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\Application\Declaration;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Dvsa\Olcs\Api\Domain\Repository\Application as ApplicationRepo;
+use Dvsa\Olcs\Api\Domain\Repository\SystemParameter as SystemParameterRepo;
+use Dvsa\Olcs\Api\Entity\System\SystemParameter as SystemParameterEntity;
 use Dvsa\Olcs\Transfer\Query\Application\Declaration as Qry;
 use Dvsa\Olcs\Api\Service\FeesHelperService;
 use Dvsa\Olcs\Api\Service\Lva\SectionAccessService;
@@ -26,9 +23,11 @@ class DeclarationTest extends QueryHandlerTestCase
     {
         $this->sut = new Declaration();
         $this->mockRepo('Application', ApplicationRepo::class);
+        $this->mockRepo('SystemParameter', SystemParameterRepo::class);
         $this->mockedSmServices = [
             'FeesHelperService' => m::mock(FeesHelperService::class),
             'SectionAccessService' => m::mock(SectionAccessService::class),
+            'Review\ApplicationUndertakings' => m::mock(),
         ];
 
         parent::setUp();
@@ -51,9 +50,15 @@ class DeclarationTest extends QueryHandlerTestCase
             ->with($query)
             ->andReturn($mockApplication);
 
-        $mockApplication->shouldReceive('serialize')->once()->andReturn(['foo' => 'bar']);
+        $this->repoMap['SystemParameter']->shouldReceive('fetchValue')
+            ->with(SystemParameterEntity::DISABLE_GDS_VERIFY_SIGNATURES)
+            ->andReturn(true)
+            ->once();
+
+        $mockApplication->shouldReceive('serialize')->twice()->andReturn(['foo' => 'bar']);
         $mockApplication->shouldReceive('canHaveInterimLicence')->with()->once()->andReturn('xxx');
         $mockApplication->shouldReceive('isLicenceUpgrade')->with()->once()->andReturn('yyy');
+        $mockApplication->shouldReceive('isGoods')->with()->once()->andReturn(true);
 
         $this->mockedSmServices['FeesHelperService']
             ->shouldReceive('getTotalOutstandingFeeAmountForApplication')
@@ -69,6 +74,13 @@ class DeclarationTest extends QueryHandlerTestCase
             ->once()
             ->getMock();
 
+        $this->mockedSmServices['Review\ApplicationUndertakings']
+            ->shouldReceive('getMarkup')
+            ->with(['foo' => 'bar', 'isGoods' => true, 'isInternal' => false])
+            ->once()
+            ->andReturn('markup')
+            ->getMock();
+
         $expected = [
             'foo' => 'bar',
             'canHaveInterimLicence' => 'xxx',
@@ -76,6 +88,8 @@ class DeclarationTest extends QueryHandlerTestCase
             'outstandingFeeTotal' => 123.45,
             'sections' => 'bar',
             'variationCompletion' => 'foo',
+            'disableSignatures' => true,
+            'declarations' => 'markup',
         ];
 
         $this->assertEquals($expected, $this->sut->handleQuery($query)->serialize());
