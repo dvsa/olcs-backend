19151f25d1c37f31c75f39c693c1fb12fa685ffe
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-12-13 16:15:05 +0000

    OLCS-14849: custom CURL adapter; ability to use stream for Query; download as stream from DS and Api; add output of stream instead fpassthu; if status code 206 then accept empty response;add annotation; unit tests;

--- app/api/test/module/DocumentShare/src/Object/FileTest.php
+++ app/api/test/module/DocumentShare/src/Object/FileTest.php
@@ -6,10 +6,18 @@ use Dvsa\Olcs\DocumentShare\Data\Object\File;
 use org\bovigo\vfs\vfsStream;
 
 /**
- * @covers Dvsa\Olcs\DocumentShare\Data\Object\File
+ * @covers \Dvsa\Olcs\DocumentShare\Data\Object\File
  */
 class FileTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  \org\bovigo\vfs\vfsStreamDirectory */
+    private $vfs;
+
+    public function setUp()
+    {
+        $this->vfs = vfsStream::setup('temp');
+    }
+
     public function testSetGet()
     {
         $sut = new File();
@@ -29,10 +37,8 @@ class FileTest extends \PHPUnit_Framework_TestCase
 
     public function testSetResource()
     {
-        $vfs = vfsStream::setup('tmp');
-
-        $fsFilePath1 = vfsStream::newFile('unitTemp1')->at($vfs)->url();
-        $fsFilePath2 = vfsStream::newFile('unitTemp2')->at($vfs)->url();
+        $fsFilePath1 = vfsStream::newFile('unitTemp1')->at($this->vfs)->url();
+        $fsFilePath2 = vfsStream::newFile('unitTemp2')->at($this->vfs)->url();
 
         $sut = new File();
 
@@ -52,5 +58,55 @@ class FileTest extends \PHPUnit_Framework_TestCase
         //  check file removed when destroy object
         unset($sut);
         static::assertFalse(is_file($fsFilePath2));
+    }
+
+    public function testSetContentFromDsStream()
+    {
+        $res = vfsStream::newFile('res')
+            ->at($this->vfs)
+            ->url();
+
+        $streamContent = 'UNIT_expect_content' . str_repeat('E', File::CHUNK_SIZE);
+        $streamBody =
+            str_repeat('x', File::CHUNK_SIZE - strlen('content') / 2) .
+            '"content":"' . base64_encode($streamContent) . '"' .
+            str_repeat('aftercontent', 3);
+
+        $stream = vfsStream::newFile('stream')
+            ->withContent($streamBody)
+            ->at($this->vfs)
+            ->url();
+
+        $sut = new File();
+        $sut->setResource($res);
+        $sut->setContentFromDsStream($stream);
+
+        static::assertEquals($streamContent, $sut->getContent());
+    }
+
+    public function testSetContentFromDsStreamExcStream404()
+    {
+        $this->setExpectedException(\Exception::class, File::ERR_CANT_OPEN_STREAM);
+
+        $res = vfsStream::newFile('res')
+            ->at($this->vfs)
+            ->url();
+
+        (new File())
+            ->setResource($res)
+            ->setContentFromDsStream(null);
+    }
+
+    public function testSetContentFromDsStreamExcRes404()
+    {
+        $this->setExpectedException(\Exception::class, File::ERR_CANT_OPEN_RES);
+
+        $stream = vfsStream::newFile('stream')
+            ->at($this->vfs)
+            ->url();
+
+        (new File())
+            ->setResource(null)
+            ->setContentFromDsStream($stream);
     }
 }
