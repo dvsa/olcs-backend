368c17d200001ebb21846aa5677ace94cd273da7
Mat Evans <mat.evans@valtech.co.uk>
2016-12-20 09:45:28 +0000

    Update db_testdata_setup script to work with Liquibase

--- data/db/db_testdata_setup.sh
+++ data/db/db_testdata_setup.sh
@@ -3,56 +3,24 @@
 # causes the shell to exit if any subcommand or pipeline returns a non-zero status.
 set -e
 
-# Check can connect to DB
-if ! mysql -e "exit"
-then
-   echo
-   echo "ERROR Cannot connect to database. Make sure you have a ~/.my.cnf file"
-   echo
-   exit
+ETL_PATH="../../../olcs-etl/"
+
+if [ ! -d $ETL_PATH ]; then
+  echo
+  echo "Cannot find you ETL directory, You'll have to run it manually!!!"
+  echo
+  echo "Go to your ERL directory and then run"
+  echo "./create-properties-from-mycnf.sh"
+  echo "./create-base.sh"
+  echo "liquibase/liquibase update -Ddataset=testdata"
+  echo
+  exit
 fi
 
-# drop/create db
-echo "Create olcs_be database"
-mysql < DatabaseSetup.sql
-
-# schema
-echo "Apply OLCS schema"
-mysql olcs_be < ../../../olcs-etl/olcs_schema.sql
-
-# schema
-# Add manual Views and Stored Procs
-echo "View, procs and functions"
-for sqlFile in ../../../olcs-etl/views_procs/*.sql
-do
-  mysql olcs_be < $sqlFile
-done
-
-# refdata
-echo "ref_data.sql"
-mysql olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/ref_data.sql
-echo "other_ref_data.sql"
-mysql olcs_be < ../../../olcs-etl/script_generator/src/main/resources/vosa/olcs/etl/scripts/other_ref_data.sql
-
-# stub data
-echo "olcs_stub_data.sql"
-mysql olcs_be < ../../../olcs-etl/olcs_stub_data.sql
-
-# test data
-echo "testdata.sql"
-mysql olcs_be < testdata.sql
-
-# ETL schema is required by pacthes
-echo "Create ETL schema (used for logging in patches)"
-mysql olcs_be < ../../../olcs-etl/etl-schema.sql
-
-# Apply post-live patches
-echo "Apply post-live patches"
-cd ../../../olcs-etl
-./post-live-patch.sh -d olcs_be -v 4.0.6
-./post-live-patch.sh -d olcs_be -v 4.0.7
-./post-live-patch.sh -d olcs_be -v 4.0.8
-./post-live-patch.sh -d olcs_be -v post-live
+cd $ETL_PATH
+./create-properties-from-mycnf.sh
+./create-base.sh
+liquibase/liquibase update -Ddataset=testdata
 
 sudo service httpd restart
 
