0151a905afd1a5d68df435945a39a2abf429e882
Ian Lindsay <ian@hemera-business-services.co.uk>
2017-01-31 14:24:12 +0000

    Change how EBSR validates variation numbers, rewrite unit test to more specifically test messages and individual paths

--- app/api/module/Api/src/Service/Ebsr/RulesValidator/ProcessedData/VariationNumber.php
+++ app/api/module/Api/src/Service/Ebsr/RulesValidator/ProcessedData/VariationNumber.php
@@ -14,15 +14,16 @@ class VariationNumber extends AbstractValidator
     const CANCELLATION_VARIATION_NUMBER_ERROR = 'cancellation-variation-number-error';
     const VARIATION_VARIATION_NUMBER_ERROR = 'variation-variation-number-error';
     const NEW_VARIATION_NUMBER_ERROR = 'new-variation-number-error';
+    const EXPECTED_VARIATION_MSG = 'The expected variation number was %d';
 
     /**
      * @var array
      */
     protected $messageTemplates = [
         self::CANCELLATION_VARIATION_NUMBER_ERROR =>
-            'For cancellations, the variation number must be equal to previous variation number',
+            'For cancellations, the variation number must be equal to previous variation number. %value%',
         self::VARIATION_VARIATION_NUMBER_ERROR =>
-            'For variations, the variation number should be 1 greater than the previous variation number',
+            'For variations, the variation number should be one greater than the previous variation number. %value%',
         self::NEW_VARIATION_NUMBER_ERROR => 'For new applications, the variation number must be zero'
     ];
 
@@ -44,8 +45,12 @@ class VariationNumber extends AbstractValidator
         $busReg = $context['busReg'];
 
         if ($value['txcAppType'] === BusRegEntity::TXC_APP_CANCEL) {
-            if ($value['variationNo'] != $busReg->getVariationNo()) {
-                $this->error(self::CANCELLATION_VARIATION_NUMBER_ERROR);
+            $expectedVariationNumber = $busReg->getVariationNo();
+            if ($value['variationNo'] != $expectedVariationNumber) {
+                $this->error(
+                    self::CANCELLATION_VARIATION_NUMBER_ERROR,
+                    sprintf(self::EXPECTED_VARIATION_MSG, $expectedVariationNumber)
+                );
                 return false;
             }
         } elseif ($value['txcAppType'] === BusRegEntity::TXC_APP_NEW) {
@@ -54,8 +59,19 @@ class VariationNumber extends AbstractValidator
                 return false;
             }
         } else {
-            if ($value['variationNo'] != $busReg->getVariationNo() + 1) {
-                $this->error(self::VARIATION_VARIATION_NUMBER_ERROR);
+            /**
+             * When we fetch the previous bus reg record, we exclude certain statuses, and get the first record with a
+             * valid status. Here we are getting the latest variation without excluding these statuses
+             * (expired, withdrawn, refused)
+             */
+            $latestVariation = $busReg->getLicence()->getLatestBusVariation($busReg->getRegNo(), []);
+            $expectedVariationNumber = $latestVariation->getVariationNo() + 1;
+
+            if ($value['variationNo'] != $expectedVariationNumber) {
+                $this->error(
+                    self::VARIATION_VARIATION_NUMBER_ERROR,
+                    sprintf(self::EXPECTED_VARIATION_MSG, $expectedVariationNumber)
+                );
                 return false;
             }
         }
--- app/api/test/module/Api/src/Service/Ebsr/RulesValidator/ProcessedData/VariationNumberTest.php
+++ app/api/test/module/Api/src/Service/Ebsr/RulesValidator/ProcessedData/VariationNumberTest.php
@@ -4,78 +4,128 @@ namespace Dvsa\OlcsTest\Api\Service\Ebsr\RulesValidator\ProcessedData;
 
 use Dvsa\Olcs\Api\Service\Ebsr\RulesValidator\ProcessedData\VariationNumber;
 use Dvsa\Olcs\Api\Entity\Bus\BusReg as BusRegEntity;
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
 /**
  * Class VariationNumberTest
  * @package Dvsa\OlcsTest\Api\Service\Ebsr\RulesValidator\ProcessedData
  */
-class VariationNumberTest extends \PHPUnit_Framework_TestCase
+class VariationNumberTest extends MockeryTestCase
 {
     /**
-     * tests isValid returns true for the correct data
-     *
-     * @dataProvider isValidWhenValidProvider
-     *
-     * @param string $txcAppType
-     * @param int $variationNoValue
-     * @param int $variationNoBusReg
+     * tests isValid returns true for a valid new app
      */
-    public function testIsValidWhenValid($txcAppType, $variationNoValue, $variationNoBusReg)
+    public function testValidNewApp()
     {
         $sut = new VariationNumber();
-        $busReg = new BusRegEntity();
-        $busReg->setVariationNo($variationNoBusReg);
 
-        $value = [
-            'txcAppType' => $txcAppType,
-            'variationNo' => $variationNoValue
-        ];
+        $value = $this->getValue(BusRegEntity::TXC_APP_NEW, 0);
+        $isValid = $sut->isValid($value, ['busReg' => null]);
 
-        $context = ['busReg' => $busReg];
-
-        $this->assertEquals(true, $sut->isValid($value, $context));
-        $this->assertEmpty($sut->getMessages());
+        $this->assertEquals(true, $isValid);
+        $this->assertCount(0, $sut->getMessages());
     }
 
     /**
-     * tests isValid returns false for the correct data, as well as the correct error message
-     *
-     * @dataProvider isValidWhenNotValidProvider
-     *
-     * @param string $txcAppType
-     * @param int $variationNoValue
-     * @param int $variationNoBusReg
-     * @param string $errorKey
+     * tests isValid returns false for invalid new app, checks correct error message
      */
-    public function testIsValidWhenNotValid($txcAppType, $variationNoValue, $variationNoBusReg, $errorKey)
+    public function testInvalidNewApp()
     {
         $sut = new VariationNumber();
-        $busReg = new BusRegEntity();
-        $busReg->setVariationNo($variationNoBusReg);
 
-        $value = [
-            'txcAppType' => $txcAppType,
-            'variationNo' => $variationNoValue
-        ];
-
-        $context = ['busReg' => $busReg];
-
-        $this->assertEquals(false, $sut->isValid($value, $context));
+        $value = $this->getValue(BusRegEntity::TXC_APP_NEW, 1);
+        $isValid = $sut->isValid($value, ['busReg' => null]);
         $messages = $sut->getMessages();
-        $this->assertArrayHasKey($errorKey, $messages);
+
+        $this->assertEquals(false, $isValid);
+        $this->assertEquals(
+            $sut->getMessageTemplates()[VariationNumber::NEW_VARIATION_NUMBER_ERROR],
+            $messages[VariationNumber::NEW_VARIATION_NUMBER_ERROR]
+        );
         $this->assertCount(1, $messages);
     }
 
     /**
-     * Provider for testIsValidWhenValid
+     * tests isValid returns true for a valid cancellation
+     */
+    public function testValidCancellation()
+    {
+        $sut = new VariationNumber();
+        $variationNo = 1;
+
+        $busReg = m::mock(BusRegEntity::class);
+        $busReg->shouldReceive('getVariationNo')->once()->withNoArgs()->andReturn($variationNo);
+
+        $value = $this->getValue(BusRegEntity::TXC_APP_CANCEL, $variationNo);
+        $isValid = $sut->isValid($value, ['busReg' => $busReg]);
+
+        $this->assertEquals(true, $isValid);
+        $this->assertCount(0, $sut->getMessages());
+    }
+
+    /**
+     * tests isValid returns false for invalid cancellation, and checks message
+     */
+    public function testInvalidCancellation()
+    {
+        $sut = new VariationNumber();
+        $expectedVariationNo = 1;
+        $error = 'For cancellations, the variation number must be equal to previous variation number. '
+            . 'The expected variation number was ' . $expectedVariationNo;
+
+        $busReg = m::mock(BusRegEntity::class);
+        $busReg->shouldReceive('getVariationNo')->once()->withNoArgs()->andReturn($expectedVariationNo);
+
+        $value = $this->getValue(BusRegEntity::TXC_APP_CANCEL, 2);
+        $isValid = $sut->isValid($value, ['busReg' => $busReg]);
+        $messages = $sut->getMessages();
+
+        $this->assertEquals(false, $isValid);
+        $this->assertEquals(
+            $messages[VariationNumber::CANCELLATION_VARIATION_NUMBER_ERROR],
+            $error
+        );
+        $this->assertCount(1, $messages);
+    }
+
+    /**
+     * tests isValid returns true for a valid variation
+     *
+     * @dataProvider validVariationProvider
+     *
+     * @param string $txcAppType
+     * @param int $variationNoValue
+     * @param int $variationNoBusReg
+     */
+    public function testValidVariation($txcAppType, $variationNoValue, $variationNoBusReg)
+    {
+        $sut = new VariationNumber();
+
+        $variationBusReg = m::mock(BusRegEntity::class);
+        $variationBusReg->shouldReceive('getVariationNo')->once()->withNoArgs()->andReturn($variationNoBusReg);
+
+        $regNo = 'OB1234567/8';
+        $busReg = m::mock(BusRegEntity::class);
+        $busReg->shouldReceive('getRegNo')->once()->withNoArgs()->andReturn($regNo);
+        $busReg->shouldReceive('getLicence->getLatestBusVariation')
+            ->once()
+            ->with($regNo, [])
+            ->andReturn($variationBusReg);
+
+        $value = $this->getValue($txcAppType, $variationNoValue);
+        $this->assertEquals(true, $sut->isValid($value, ['busReg' => $busReg]));
+        $this->assertCount(0, $sut->getMessages());
+    }
+
+    /**
+     * Provider for testValidVariation
      *
      * @return array
      */
-    public function isValidWhenValidProvider()
+    public function validVariationProvider()
     {
         return [
-            [BusRegEntity::TXC_APP_NEW, 0, null],
-            [BusRegEntity::TXC_APP_CANCEL, 0, 0],
             [BusRegEntity::TXC_APP_NON_CHARGEABLE, 1, 0],
             [BusRegEntity::TXC_APP_NON_CHARGEABLE, 2, 1],
             [BusRegEntity::TXC_APP_CHARGEABLE, 1, 0],
@@ -84,24 +134,70 @@ class VariationNumberTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Provider for testIsValidWhenNotValid
+     * tests isValid returns false for the correct data, as well as the correct error message
+     *
+     * @dataProvider invalidVariationProvider
+     *
+     * @param string $txcAppType
+     * @param int $variationNoValue
+     * @param int $variationNoBusReg
+     */
+    public function testInvalidVariation($txcAppType, $variationNoValue, $variationNoBusReg)
+    {
+        $sut = new VariationNumber();
+
+        $expectedVariationNo = $variationNoBusReg + 1;
+        $error = 'For variations, the variation number should be one greater than the previous variation number. '
+            . 'The expected variation number was ' . $expectedVariationNo;
+
+        $variationBusReg = m::mock(BusRegEntity::class);
+        $variationBusReg->shouldReceive('getVariationNo')->once()->withNoArgs()->andReturn($variationNoBusReg);
+
+        $regNo = 'OB1234567/8';
+        $busReg = m::mock(BusRegEntity::class);
+        $busReg->shouldReceive('getRegNo')->once()->withNoArgs()->andReturn($regNo);
+        $busReg->shouldReceive('getLicence->getLatestBusVariation')
+            ->once()
+            ->with($regNo, [])
+            ->andReturn($variationBusReg);
+
+        $value = $this->getValue($txcAppType, $variationNoValue);
+        $this->assertEquals(false, $sut->isValid($value, ['busReg' => $busReg]));
+        $messages = $sut->getMessages();
+
+        $this->assertEquals($error, $messages[VariationNumber::VARIATION_VARIATION_NUMBER_ERROR]);
+        $this->assertCount(1, $messages);
+    }
+
+    /**
+     * Provider for testInvalidVariation
      *
      * @return array
      */
-    public function isValidWhenNotValidProvider()
+    public function invalidVariationProvider()
     {
         return [
-            [BusRegEntity::TXC_APP_NEW, 1, null, VariationNumber::NEW_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CANCEL, 0, 1, VariationNumber::CANCELLATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CANCEL, 1, 0, VariationNumber::CANCELLATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 2, 0, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 0, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 1, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 2, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CHARGEABLE, 2, 0, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 0, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 1, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR],
-            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 2, VariationNumber::VARIATION_VARIATION_NUMBER_ERROR]
+            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 2, 0],
+            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 0],
+            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 1],
+            [BusRegEntity::TXC_APP_NON_CHARGEABLE, 0, 2],
+            [BusRegEntity::TXC_APP_CHARGEABLE, 2, 0],
+            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 0],
+            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 1],
+            [BusRegEntity::TXC_APP_CHARGEABLE, 0, 2]
+        ];
+    }
+
+    /**
+     * @param $txcAppType
+     * @param $variationNo
+     * @return array
+     */
+    private function getValue($txcAppType, $variationNo)
+    {
+        return [
+            'txcAppType' => $txcAppType,
+            'variationNo' => $variationNo
         ];
     }
 }
