bfcf410c43fcbe534d8efcd48bc732a8ac0ba357
Ian Lindsay <ian@hemera-business-services.co.uk>
2017-02-06 18:06:32 +0000

    CLI Command handler to expire bus registrations that have passed their end date

---
+++ test/module/Cli/src/Domain/CommandHandler/Bus/ExpireTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dvsa\OlcsTest\Cli\Domain\CommandHandler\Bus;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\Repository\Bus as BusRepo;
+use Dvsa\Olcs\Cli\Domain\Command\Bus\Expire as ExpireBusCmd;
+use Dvsa\Olcs\Cli\Domain\CommandHandler\Bus\Expire as ExpireHandler;
+use Mockery as m;
+
+/**
+ * Bus reg expiry command handler test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class ExpireTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new ExpireHandler();
+        $this->mockRepo('Bus', BusRepo::class);
+
+        parent::setUp();
+    }
+
+    /**
+     * tests handleCommand
+     */
+    public function testHandleCommand()
+    {
+        $this->repoMap['Bus']->shouldReceive('expireRegistrations')->withNoArgs();
+        $result = $this->sut->handleCommand(ExpireBusCmd::create([]));
+
+        $messages = [0 => 'registrations have been expired'];
+        $this->assertEquals($messages, $result->getMessages());
+    }
+}
