5aec86af6b15764fbfd197053eb8ede549ff5099
Alex P <alex.peshkov@valtech.co.uk>
2017-02-28 16:35:38 +0000

    OLCS-15329 Additional bookmark 'comments' required for Community licences

---
+++ app/api/module/Api/src/Service/Document/Bookmark/Alias/Comments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Alias;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\EuropeanLicenceNumber;
+
+/**
+ * Alias of EuropeanLicenceNumber
+ */
+class Comments extends EuropeanLicenceNumber
+{
+    //
+}
