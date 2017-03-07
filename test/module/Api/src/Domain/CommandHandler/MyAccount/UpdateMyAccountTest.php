ffea81488651949a095a3b58ee5ad92cbb19bb12
Alex P <alex.peshkov@valtech.co.uk>
2017-03-07 12:52:14 +0000

    OLCS-15275 RO users update my account details

--- test/module/Api/src/Domain/CommandHandler/MyAccount/UpdateMyAccountTest.php
+++ test/module/Api/src/Domain/CommandHandler/MyAccount/UpdateMyAccountTest.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * Update MyAccount Test
- */
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\MyAccount;
 
 use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
@@ -13,9 +10,15 @@ use Dvsa\Olcs\Api\Domain\Command\MyAccount\UpdateMyAccount as Cmd;
 use Dvsa\Olcs\Api\Domain\CommandHandler\MyAccount\UpdateMyAccount;
 use Dvsa\Olcs\Api\Domain\Repository\ContactDetails;
 use Dvsa\Olcs\Api\Domain\Repository\User;
+use Dvsa\Olcs\Api\Domain\Repository\Person;
+use Dvsa\Olcs\Api\Domain\Repository\PhoneContact;
+use Dvsa\Olcs\Api\Domain\Repository\Address;
 use Dvsa\Olcs\Api\Entity\ContactDetails\Country;
 use Dvsa\Olcs\Api\Entity\ContactDetails\ContactDetails as ContactDetailsEntity;
 use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
+use Dvsa\Olcs\Api\Entity\ContactDetails\PhoneContact as PhoneContactEntity;
+use Dvsa\Olcs\Api\Entity\ContactDetails\Address as AddressEntity;
+use Dvsa\Olcs\Api\Entity\Person\Person as PersonEntity;
 use Dvsa\Olcs\Api\Entity\User\Team;
 use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
@@ -31,6 +34,9 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
         $this->sut = new UpdateMyAccount();
         $this->mockRepo('User', User::class);
         $this->mockRepo('ContactDetails', ContactDetails::class);
+        $this->mockRepo('PhoneContact', PhoneContact::class);
+        $this->mockRepo('Address', Address::class);
+        $this->mockRepo('Person', Person::class);
 
         $this->mockedSmServices = [
             AuthorizationService::class => m::mock(AuthorizationService::class),
@@ -43,7 +49,9 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
     protected function initReferences()
     {
         $this->refData = [
-            ContactDetailsEntity::CONTACT_TYPE_USER
+            ContactDetailsEntity::CONTACT_TYPE_USER,
+            PhoneContactEntity::TYPE_FAX,
+            'title_mr',
         ];
 
         $this->references = [
@@ -172,7 +180,7 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
             'contactDetails' => [
                 'emailAddress' => 'test1@test.me',
                 'person' => [
-                    'title' => m::mock(RefData::class),
+                    'title' => 'title_mr',
                     'forename' => 'updated forename',
                     'familyName' => 'updated familyName',
                     'birthDate' => '1975-12-12',
@@ -184,18 +192,13 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
                     'addressLine4' => 'a45',
                     'town' => 'town',
                     'postcode' => 'LS1 2AB',
-                    'countryCode' => m::mock(Country::class),
+                    'countryCode' => 'GB',
                 ],
                 'phoneContacts' => [
                     [
-                        'phoneContactType' => m::mock(RefData::class),
+                        'phoneContactType' => PhoneContactEntity::TYPE_FAX,
                         'phoneNumber' => '111',
                     ],
-                    [
-                        'id' => 999,
-                        'phoneContactType' => m::mock(RefData::class),
-                        'phoneNumber' => '222',
-                    ]
                 ],
             ],
         ];
@@ -212,12 +215,89 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
 
         $command = Cmd::create($data);
 
+        $savedPerson = null;
+        $this->repoMap['Person']
+            ->shouldReceive('save')
+            ->with(m::type(PersonEntity::class))
+            ->andReturnUsing(
+                function (PersonEntity $person) use ($data, &$savedPerson) {
+                    $savedPerson = $person;
+                    $dataPerson = $data['contactDetails']['person'];
+                    $this->assertSame($dataPerson['forename'], $savedPerson->getForename());
+                    $this->assertSame($dataPerson['familyName'], $savedPerson->getFamilyName());
+                    $this->assertSame($this->refData[$dataPerson['title']], $savedPerson->getTitle());
+                    $this->assertEquals(new \DateTime($dataPerson['birthDate']), $savedPerson->getBirthDate());
+                    $savedPerson->setId(753);
+                    return $savedPerson;
+                }
+            )
+            ->once();
+
+        $savedAddress = null;
+        $this->repoMap['Address']
+            ->shouldReceive('save')
+            ->with(m::type(AddressEntity::class))
+            ->andReturnUsing(
+                function (AddressEntity $address) use ($data, &$savedAddress) {
+                    $savedAddress = $address;
+                    $dataAddress = $data['contactDetails']['address'];
+                    $this->assertSame($dataAddress['addressLine1'], $savedAddress->getAddressLine1());
+                    $this->assertSame($dataAddress['addressLine2'], $savedAddress->getAddressLine2());
+                    $this->assertSame($dataAddress['addressLine3'], $savedAddress->getAddressLine3());
+                    $this->assertSame($dataAddress['addressLine4'], $savedAddress->getAddressLine4());
+                    $this->assertSame($dataAddress['town'], $savedAddress->getTown());
+                    $this->assertSame($dataAddress['postcode'], $savedAddress->getPostcode());
+                    $this->assertSame(
+                        $this->references[Country::class][$dataAddress['countryCode']],
+                        $savedAddress->getCountryCode()
+                    );
+                    $savedAddress->setId(754);
+                }
+            )
+            ->once();
+
+        $existingPhoneContact = m::mock(PhoneContactEntity::class);
+
+        $this->repoMap['PhoneContact']
+            ->shouldReceive('delete')
+            ->with($existingPhoneContact)
+            ->once()
+            ->shouldReceive('save')
+            ->with(m::type(PhoneContactEntity::class))
+            ->andReturnUsing(
+                function (PhoneContactEntity $phoneContact) use ($data) {
+                    $dataPhoneContacts = $data['contactDetails']['phoneContacts'][0];
+                    $this->assertSame($dataPhoneContacts['phoneNumber'], $phoneContact->getPhoneNumber());
+                    $this->assertSame(
+                        $this->refData[PhoneContactEntity::TYPE_FAX],
+                        $phoneContact->getPhoneContactType()
+                    );
+                    $phoneContact->setId(755);
+                }
+            )
+            ->once();
+
         /** @var ContactDetailsEntity $contactDetails */
         $contactDetails = m::mock(ContactDetailsEntity::class)->makePartial();
-        $contactDetails->shouldReceive('update')
+        $contactDetails->shouldReceive('setPerson')
+            ->with(m::type(PersonEntity::class))
             ->once()
-            ->with($data['contactDetails'])
-            ->andReturnSelf();
+            ->shouldReceive('setAddress')
+            ->with(m::type(AddressEntity::class))
+            ->once()
+            ->shouldReceive('setEmailAddress')
+            ->with('test1@test.me')
+            ->once()
+            ->shouldReceive('getPhoneContacts')
+            ->andReturn([$existingPhoneContact])
+            ->once()
+            ->shouldReceive('getPerson')
+            ->andReturnNull()
+            ->once()
+            ->shouldReceive('getAddress')
+            ->andReturnNull()
+            ->once()
+            ->getMock();
 
         /** @var TeamEntity $user */
         $team = m::mock(Team::class)->makePartial();
@@ -237,11 +317,6 @@ class UpdateMyAccountTest extends CommandHandlerTestCase
             ->shouldReceive('populateRefDataReference')
             ->once()
             ->andReturn($data);
-
-        $this->repoMap['ContactDetails']->shouldReceive('populateRefDataReference')
-            ->once()
-            ->with($data['contactDetails'])
-            ->andReturn($data['contactDetails']);
 
         /** @var UserEntity $savedUser */
         $savedUser = null;
