9bfc2b267c5523624b2271a332a407e7343ace2d
Ian Lindsay <ian@hemera-business-services.co.uk>
2017-03-17 14:19:47 +0000

    olcs-15838 when a bus reg is withdrawn, and outstanding fees are cancelled

--- app/api/test/module/Api/src/Domain/CommandHandler/Bus/WithdrawBusRegTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Bus/WithdrawBusRegTest.php
@@ -5,6 +5,7 @@
  */
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Bus;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Query;
 use Mockery as m;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Bus\WithdrawBusReg;
@@ -14,6 +15,8 @@ use Dvsa\Olcs\Transfer\Command\Bus\WithdrawBusReg as Cmd;
 use Dvsa\Olcs\Api\Entity\Bus\BusReg as BusRegEntity;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\Command\Email\SendEbsrWithdrawn;
+use Dvsa\Olcs\Api\Entity\Fee\Fee as FeeEntity;
+use Dvsa\Olcs\Api\Domain\Command\Fee\CancelFee as CancelFeeCmd;
 
 /**
  * Withdraw BusReg Test
@@ -39,9 +42,13 @@ class WithdrawBusRegTest extends CommandHandlerTestCase
     }
 
     /**
-     * test handleCommand when Ebsr
+     * @dataProvider handleCommandProvider
+     *
+     * @param $isEbsr
+     * @param $hasOutstandingFee
+     * @param $fees
      */
-    public function testHandleCommandEbsr()
+    public function testHandleCommand($isEbsr, $hasOutstandingFee, $fees)
     {
         $id = 99;
         $ebsrId = 55;
@@ -53,56 +60,75 @@ class WithdrawBusRegTest extends CommandHandlerTestCase
             ]
         );
 
-        /** @var BusRegEntity $busReg */
         $busReg = m::mock(BusRegEntity::class);
-        $busReg->shouldReceive('withdraw')->once();
-        $busReg->shouldReceive('getId')->andReturn($id);
-        $busReg->shouldReceive('isFromEbsr')->once()->andReturn(true);
-        $busReg->shouldReceive('getEbsrSubmissions->first->getId')->once()->andReturn($ebsrId);
-        $this->expectedEmailQueueSideEffect(SendEbsrWithdrawn::class, ['id' => $ebsrId], $ebsrId, new Result());
+        $busReg->shouldReceive('withdraw')
+               ->once()
+               ->with(
+                   $this->refData[BusRegEntity::STATUS_WITHDRAWN],
+                   $this->refData['reg_in_error']
+               )
+               ->andReturnSelf();
+        $busReg->shouldReceive('getId')->once()->withNoArgs()->andReturn($id);
+        $busReg->shouldReceive('isFromEbsr')->once()->withNoArgs()->andReturn($isEbsr);
+        $busReg->shouldReceive('getEbsrSubmissions->first->getId')
+            ->times($isEbsr ? 1 : 0)
+            ->withNoArgs()
+            ->andReturn($ebsrId);
+        $busReg->shouldReceive('getFees')->once()->withNoArgs()->andReturn($fees);
+
+        if ($isEbsr) {
+            $this->expectedEmailQueueSideEffect(SendEbsrWithdrawn::class, ['id' => $ebsrId], $ebsrId, new Result());
+        }
+
+        if ($hasOutstandingFee) {
+            $this->expectedSideEffect(CancelFeeCmd::class, ['id' => 77], new Result());
+            $this->expectedSideEffect(CancelFeeCmd::class, ['id' => 78], new Result());
+            $this->expectedSideEffect(CancelFeeCmd::class, ['id' => 79], new Result());
+        }
 
         $this->repoMap['Bus']->shouldReceive('fetchUsingId')
+            ->once()
             ->with($command, Query::HYDRATE_OBJECT)
             ->andReturn($busReg)
             ->shouldReceive('save')
             ->with(m::type(BusRegEntity::class))
             ->once();
 
-        $result = $this->sut->handleCommand($command);
-
-        $this->assertInstanceOf(Result::class, $result);
+        $this->sut->handleCommand($command);
     }
 
     /**
-     * test handleCommand when not Ebsr
+     * @return array
      */
-    public function testHandleCommandNotEbsr()
+    public function handleCommandProvider()
     {
-        $id = 99;
+        $fee1 = m::mock(FeeEntity::class);
+        $fee1->shouldReceive('isOutstanding')->once()->withNoArgs()->andReturn(false);
+        $fee1->shouldReceive('getId')->never();
 
-        $command = Cmd::Create(
-            [
-                'id' => $id,
-                'reason' => 'reg_in_error'
-            ]
-        );
+        $fee2 = m::mock(FeeEntity::class);
+        $fee2->shouldReceive('isOutstanding')->once()->withNoArgs()->andReturn(true);
+        $fee2->shouldReceive('getId')->once()->withNoArgs()->andReturn(77);
 
-        /** @var BusRegEntity $busReg */
-        $busReg = m::mock(BusRegEntity::class);
-        $busReg->shouldReceive('withdraw')->once();
-        $busReg->shouldReceive('getId')->andReturn($id);
-        $busReg->shouldReceive('isFromEbsr')->once()->andReturn(false);
-        $busReg->shouldReceive('getEbsrSubmissions->first->getId')->never();
+        $fee3 = m::mock(FeeEntity::class);
+        $fee3->shouldReceive('isOutstanding')->once()->withNoArgs()->andReturn(true);
+        $fee3->shouldReceive('getId')->once()->withNoArgs()->andReturn(78);
 
-        $this->repoMap['Bus']->shouldReceive('fetchUsingId')
-            ->with($command, Query::HYDRATE_OBJECT)
-            ->andReturn($busReg)
-            ->shouldReceive('save')
-            ->with(m::type(BusRegEntity::class))
-            ->once();
+        $fee4 = m::mock(FeeEntity::class);
+        $fee4->shouldReceive('isOutstanding')->once()->withNoArgs()->andReturn(false);
+        $fee4->shouldReceive('getId')->never();
 
-        $result = $this->sut->handleCommand($command);
+        $fee5 = m::mock(FeeEntity::class);
+        $fee5->shouldReceive('isOutstanding')->once()->withNoArgs()->andReturn(true);
+        $fee5->shouldReceive('getId')->once()->withNoArgs()->andReturn(79);
 
-        $this->assertInstanceOf(Result::class, $result);
+        $feeCollection = new ArrayCollection([$fee1, $fee2, $fee3, $fee4, $fee5]);
+
+        return [
+            [true, true, $feeCollection],
+            [false, true, $feeCollection],
+            [true, false, new ArrayCollection()],
+            [false, false, new ArrayCollection()]
+        ];
     }
 }
