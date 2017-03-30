3858d30d1a2c85c02ca30db339380d2b54e88120
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-03-30 13:01:52 +0100

    OLCS-16121: move operator and org qry and cmd to related config file; change permission from IsInternalUser to IsInternalEdit;

--- module/Api/config/validation-map/organisation.config.php
+++ module/Api/config/validation-map/organisation.config.php
@@ -1,8 +1,7 @@
 <?php
 
-use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers as Handler;
+use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc;
 
 return [
@@ -10,7 +9,11 @@ return [
     QueryHandler\Organisation\Dashboard::class            => Misc\CanAccessOrganisationWithId::class,
     QueryHandler\Organisation\Organisation::class         => Misc\CanAccessOrganisationWithId::class,
     QueryHandler\Organisation\OutstandingFees::class      => Misc\CanAccessOrganisationWithId::class,
+    QueryHandler\Organisation\BusinessDetails::class => Misc\IsInternalUser::class,
+    QueryHandler\Organisation\CpidOrganisation::class => Misc\IsInternalUser::class,
 
     // Commands
     CommandHandler\Organisation\UpdateBusinessType::class => Misc\CanAccessOrganisationWithId::class,
+    CommandHandler\Organisation\CpidOrganisationExport::class => Misc\IsInternalUser::class,
+    CommandHandler\Organisation\TransferTo::class => Misc\IsInternalEdit::class,
 ];
