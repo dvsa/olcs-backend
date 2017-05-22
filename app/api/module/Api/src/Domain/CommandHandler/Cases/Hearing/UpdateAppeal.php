545cf7ded4122a6e875de8b7b0f14b18b54a02d3
lindsayi <ian.lindsay@valtech.co.uk>
2017-05-22 15:45:39 +0100

    olcs-16833 fix updateAppeal command handler, and remove most of the logic which wil go into the Alleal Entity. Add unit test.

--- app/api/module/Api/src/Domain/CommandHandler/Cases/Hearing/UpdateAppeal.php
+++ app/api/module/Api/src/Domain/CommandHandler/Cases/Hearing/UpdateAppeal.php
@@ -8,12 +8,12 @@
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Hearing;
 
 use Doctrine\ORM\Query;
-use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Cases\Appeal;
-use Dvsa\Olcs\Transfer\Command\Cases\Hearing\UpdateAppeal as Cmd;
+use Dvsa\Olcs\Api\Entity\Cases\Appeal as AppealEntity;
+use Dvsa\Olcs\Api\Domain\Repository\Appeal as AppealRepo;
+use Dvsa\Olcs\Transfer\Command\Cases\Hearing\UpdateAppeal as UpdateAppealCmd;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 
 /**
@@ -28,87 +28,46 @@ final class UpdateAppeal extends AbstractCommandHandler implements Transactioned
     /**
      * Updates Appeal and associated entities
      *
-     * @param CommandInterface $command
+     * @param CommandInterface|UpdateAppealCmd $command command to update the appeal
+     *
      * @return Result
      */
     public function handleCommand(CommandInterface $command)
     {
         $result = new Result();
 
-        $appeal = $this->createAppealObject($command);
+        /**
+         * @var AppealRepo   $repo
+         * @var AppealEntity $appeal
+         */
+        $repo = $this->getRepo();
+        $appeal = $repo->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
+
+        $outcome = $command->getOutcome() !== null ? $repo->getRefdataReference($command->getOutcome()) : null;
+
+        $appeal->update(
+            $repo->getRefdataReference($command->getReason()),
+            $command->getAppealDate(),
+            $command->getAppealNo(),
+            $command->getDeadlineDate(),
+            $command->getOutlineGround(),
+            $command->getHearingDate(),
+            $command->getDecisionDate(),
+            $command->getPapersDueDate(),
+            $command->getPapersDueTcDate(),
+            $command->getPapersSentDate(),
+            $command->getPapersSentTcDate(),
+            $command->getComment(),
+            $outcome,
+            $command->getIsWithdrawn(),
+            $command->getWithdrawnDate(),
+            $command->getDvsaNotified()
+        );
 
         $this->getRepo()->save($appeal);
         $result->addMessage('Appeal updated');
         $result->addId('appeal', $appeal->getId());
 
         return $result;
-    }
-
-    /**
-     * Update the appeal object
-     *
-     * @param Cmd $command
-     * @return Appeal
-     */
-    private function createAppealObject(Cmd $command)
-    {
-        $appeal = $this->getRepo()->fetchUsingId($command, Query::HYDRATE_OBJECT, $command->getVersion());
-
-        $appeal->setAppealNo($command->getAppealNo());
-
-        if ($command->getDeadlineDate() !== null) {
-            $appeal->setDeadlineDate(new \DateTime($command->getDeadlineDate()));
-        }
-
-        $appeal->setAppealDate(new \DateTime($command->getAppealDate()));
-        $appeal->setReason($this->getRepo()->getRefdataReference($command->getReason()));
-
-        if ($command->getOutlineGround() !== null) {
-            $appeal->setOutlineGround($command->getOutlineGround());
-        }
-
-        if ($command->getHearingDate() !== null) {
-            $appeal->setHearingDate(new \DateTime($command->getHearingDate()));
-        }
-
-        if ($command->getDecisionDate() !== null) {
-            $appeal->setDecisionDate(new \DateTime($command->getDecisionDate()));
-        }
-
-        if ($command->getPapersDueDate() !== null) {
-            $appeal->setPapersDueDate(new \DateTime($command->getPapersDueDate()));
-        }
-
-        if ($command->getPapersDueTcDate() !== null) {
-            $appeal->setPapersDueTcDate(new \DateTime($command->getPapersDueTcDate()));
-        }
-
-        if ($command->getPapersSentDate() !== null) {
-            $appeal->setPapersSentDate(new \DateTime($command->getPapersSentDate()));
-        }
-
-        if ($command->getPapersSentTcDate() !== null) {
-            $appeal->setPapersSentTcDate(new \DateTime($command->getPapersSentTcDate()));
-        }
-
-        if ($command->getComment() !== null) {
-            $appeal->setComment($command->getComment());
-        }
-
-        if ($command->getOutcome() !== null) {
-            $appeal->setOutcome($this->getRepo()->getRefdataReference($command->getOutcome()));
-        }
-
-        if ($command->getIsWithdrawn() === 'Y') {
-            if ($command->getWithdrawnDate() !== null) {
-                $appeal->setWithdrawnDate(new \DateTime($command->getWithdrawnDate()));
-            }
-        } else {
-            $appeal->setWithdrawnDate(null);
-        }
-
-        $appeal->setDvsaNotified($command->getDvsaNotified());
-
-        return $appeal;
     }
 }
