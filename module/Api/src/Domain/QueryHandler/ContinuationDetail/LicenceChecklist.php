1c1ea0aa92757361d54205e8cb41245821e74ad3
Alex P <alex.peshkov@valtech.co.uk>
2017-05-30 11:15:35 +0100

    OLCS-16811 (Both) Review digital continuation - type of licence

---
+++ module/Api/src/Domain/QueryHandler/ContinuationDetail/LicenceChecklist.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\ContinuationDetail;
+
+use Doctrine\Common\Collections\Criteria;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail as ContinuationDetailEntity;
+
+/**
+ * Licence Checklist for continuation
+ *
+ * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ */
+class LicenceChecklist extends AbstractQueryHandler
+{
+    protected $repoServiceName = 'ContinuationDetail';
+
+    protected $extraRepos = ['Licence'];
+
+    public function handleQuery(QueryInterface $query)
+    {
+        /** @var ContinuationDetailEntity $licence */
+        $continuationDetail = $this->getRepo()->fetchUsingId($query);
+
+        return $this->result(
+            $continuationDetail,
+            [
+                'licence' => [
+                    'licenceType',
+                    'status',
+                    'goodsOrPsv',
+                    'trafficArea'
+                ]
+            ]
+        );
+    }
+}
