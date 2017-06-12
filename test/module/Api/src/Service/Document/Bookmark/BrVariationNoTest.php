5f03c42a4fc5c6d308a0696f949fc048f529571a
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-12 18:18:22 +0100

    olcs-15252 bus reg variation no bookmark

---
+++ test/module/Api/src/Service/Document/Bookmark/BrVariationNoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\BrVariationNo;
+
+/**
+ * Class BrVariationNoTest
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrVariationNoTest extends BusRegFlatTestAbstract
+{
+    const SUT_CLASS_NAME = BrVariationNo::class;
+}
