8c6cf6634db85f2b3af415f2f245990c59f249a9
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-12 18:17:15 +0100

    olcs-15252 - bookmark for bus reg via

---
+++ test/module/Api/src/Service/Document/Bookmark/BrViaTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\BrVia;
+
+/**
+ * Class BrViaTest
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrViaTest extends BusRegFlatTestAbstract
+{
+    const SUT_CLASS_NAME = BrVia::class;
+}
