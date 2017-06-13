51b193542a11fbdf327a11bd099db3315ac82c39
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-13 10:35:23 +0100

    olcs-15252 BrEndDate bookmark created

---
+++ module/Api/src/Service/Document/Bookmark/BrEndDate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Domain\Query\Bookmark\BusRegBundle as Qry;
+
+/**
+ * Class BrEndDate
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrEndDate extends BusRegFlatAbstract
+{
+    const FORMATTER = 'Date';
+    const FIELD  = 'endDate';
+    const SRCH_FLD_KEY = 'id';
+    const SRCH_VAL_KEY = 'busRegId';
+    const QUERY_CLASS = Qry::class;
+}
