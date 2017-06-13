5dad9bc01837f498a60112663b20acf403622b97
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-13 17:10:33 +0100

    olcs-15252 - bus reg service number bookmark

---
+++ app/api/test/module/Api/src/Service/Document/Bookmark/BrServiceNoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\BrServiceNo;
+
+/**
+ * Class BrServiceNoTest
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrServiceNoTest extends BusRegFlatTestAbstract
+{
+    const SUT_CLASS_NAME = BrServiceNo::class;
+}
