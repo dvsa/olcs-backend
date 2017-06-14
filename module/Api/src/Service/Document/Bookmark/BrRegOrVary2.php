b7b1a4e8cd47e9baab2b2e4c4f33f8fd63a5f6c3
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-14 11:49:33 +0100

    olcs-15252 bus register or vary bookmarks

---
+++ module/Api/src/Service/Document/Bookmark/BrRegOrVary2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark;
+
+/**
+ * BrRegOrVary2 Bookmark
+ */
+class BrRegOrVary2 extends AbstractBrRegOrVary
+{
+    protected $renderReg = 'new service';
+    protected $renderVary = 'variation';
+}
