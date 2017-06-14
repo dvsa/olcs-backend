b0387eb18083913f5285143819db56c5a8aed51c
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-14 18:30:08 +0100

    olcs-15252 - BrTasNotified bookmark

---
+++ test/module/Api/src/Service/Document/Bookmark/BrTasNotified.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Document\Bookmark;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\BrTasNotified;
+
+/**
+ * Class BrTasNotifiedTest
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrTasNotifiedTest extends BusRegFlatTestAbstract
+{
+    const SUT_CLASS_NAME = BrTasNotified::class;
+}
