5daba24a883fb390941fe2cbb7b6814680f94a8a
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-14 18:19:05 +0100

    olcs-15252 - removed logic now abstracted

--- app/api/module/Api/src/Service/Document/Bookmark/Formatter/BrOtherServiceNos.php
+++ app/api/module/Api/src/Service/Document/Bookmark/Formatter/BrOtherServiceNos.php
@@ -7,30 +7,9 @@ namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Formatter;
  *
  * @author Ian Lindsay <ian@hemera-business-services.co.uk>
  */
-class BrOtherServiceNos implements FormatterInterface
+class BrOtherServiceNos extends AbstractArrayList
 {
     const FORMAT = '(%s)';
     const SEPARATOR = ', ';
-
-    /**
-     * format bus reg service numbers
-     *
-     * @param array $serviceNos service numbers
-     *
-     * @return string
-     */
-    public static function format(array $serviceNos)
-    {
-        if (!empty($serviceNos)) {
-            return sprintf(
-                self::FORMAT,
-                implode(
-                    self::SEPARATOR,
-                    array_column($serviceNos, 'serviceNo')
-                )
-            );
-        }
-
-        return '';
-    }
+    const COLUMN = 'serviceNo';
 }
