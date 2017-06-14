846639841c3134c867f774b0c1f36c29e59f28fb
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-14 18:12:05 +0100

    olcs-15252 - moved logic from BrTasNotified into reusable formatter

---
+++ app/api/module/Api/src/Service/Document/Bookmark/Formatter/BrTasNotified.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Formatter;
+
+/**
+ * BrTasNotified formatter
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class BrTasNotified extends AbstractArrayList
+{
+    const FORMAT = '(%s)';
+    const SEPARATOR = ', ';
+    const COLUMN = 'name';
+}
