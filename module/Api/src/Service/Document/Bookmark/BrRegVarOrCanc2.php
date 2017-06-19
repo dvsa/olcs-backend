1a1556ae5b3bc64723d7beb6b22c8c388d081b59
lindsayi <ian.lindsay@valtech.co.uk>
2017-06-19 16:05:56 +0100

    BrRegVaryOrCanc2 now correctly extends the abstract

--- module/Api/src/Service/Document/Bookmark/BrRegVarOrCanc2.php
+++ module/Api/src/Service/Document/Bookmark/BrRegVarOrCanc2.php
@@ -5,7 +5,7 @@ namespace Dvsa\Olcs\Api\Service\Document\Bookmark;
 /**
  * BrRegVarOrCanc2 Bookmark
  */
-class BrRegVarOrCanc2 extends BrRegVarOrCanc
+class BrRegVarOrCanc2 extends AbstractBrRegVarOrCanc
 {
     protected $renderNew = 'REGISTER';
     protected $renderVar = 'VARY';
