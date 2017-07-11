ca73ce8ab1036a2e9cb1bcbc9bead9dc8c1d5e09
Alex P <alex.peshkov@valtech.co.uk>
2017-07-11 12:53:03 +0100

    OLCS-16813 (Both) Review digital continuation - checkbox page generic behaviour

--- app/api/module/Api/src/Domain/QueryHandler/ContinuationDetail/LicenceChecklist.php
+++ app/api/module/Api/src/Domain/QueryHandler/ContinuationDetail/LicenceChecklist.php
@@ -6,6 +6,9 @@ use Doctrine\Common\Collections\Criteria;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail as ContinuationDetailEntity;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Zend\Filter\Word\UnderscoreToCamelCase;
 
 /**
  * Licence Checklist for continuation
@@ -16,13 +19,39 @@ class LicenceChecklist extends AbstractQueryHandler
 {
     protected $repoServiceName = 'ContinuationDetail';
 
+    const CONDITIONS_UNDERTAKINGS_SECTION = 'conditions_undertakings';
+
+    /**
+     * @var \Dvsa\Olcs\Api\Service\Lva\SectionAccessService
+     */
+    protected $sectionAccessService;
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $mainServiceLocator = $serviceLocator->getServiceLocator();
+
+        $this->sectionAccessService = $mainServiceLocator->get('SectionAccessService');
+
+        return parent::createService($serviceLocator);
+    }
+
+    /**
+     * Handle query
+     *
+     * @param QueryInterface $query query
+     *
+     * @return \Dvsa\Olcs\Api\Domain\QueryHandler\Result
+     */
     public function handleQuery(QueryInterface $query)
     {
         /** @var ContinuationDetailEntity $continuationDetail */
-        $continuationDetail = $this->getRepo()->fetchUsingId($query);
-
+        $continuationDetail = $this->getRepo()->fetchWithLicence($query->getId());
+        $licence = $continuationDetail->getLicence();
         $notRemoved = Criteria::create();
         $notRemoved->andWhere($notRemoved->expr()->isNull('removalDate'));
+
+        $sections = $this->sectionAccessService->getAccessibleSectionsForLicence($licence);
+        $sections = $this->alterSections(array_keys($sections), $licence);
 
         return $this->result(
             $continuationDetail,
@@ -46,7 +75,36 @@ class LicenceChecklist extends AbstractQueryHandler
                         'criteria' => $notRemoved
                     ]
                 ]
+            ],
+            [
+                'sections' => $sections
             ]
         );
+    }
+
+    /**
+     * Alter sections
+     *
+     * @param array   $sections sections
+     * @param Licence $licence licence
+     *
+     * @return array
+     */
+    protected function alterSections($sections, Licence $licence)
+    {
+        if (
+            count($licence->getConditionUndertakings()) === 0
+            && in_array(self::CONDITIONS_UNDERTAKINGS_SECTION, $sections)
+        ) {
+            unset($sections[array_search(self::CONDITIONS_UNDERTAKINGS_SECTION, $sections)]);
+        }
+        $filter = new UnderscoreToCamelCase();
+        array_walk(
+            $sections,
+            function(&$item) use ($filter) {
+                $item = lcfirst($filter->filter($item));
+            }
+        );
+        return $sections;
     }
 }
