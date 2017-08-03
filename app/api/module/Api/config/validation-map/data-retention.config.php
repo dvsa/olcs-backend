c540e0d5b4cad1f3d0730ba77da50aea51ad6c8c
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-08-03 11:28:28 +0100

    OLCS-17144 data retention rule list

--- app/api/module/Api/config/validation-map/data-retention.config.php
+++ app/api/module/Api/config/validation-map/data-retention.config.php
@@ -1,9 +1,8 @@
 <?php
 
-use Dvsa\Olcs\Api\Domain\CommandHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsSystemUser;
+use Dvsa\Olcs\Api\Domain\QueryHandler;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
 
 return [
-    CommandHandler\DataRetention\Populate::class => IsSystemUser::class,
-    CommandHandler\DataRetention\DeleteEntities::class => IsSystemUser::class,
+    QueryHandler\DataRetention\RuleList::class => IsInternalUser::class,
 ];
