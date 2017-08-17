f65b88000f63707e4597b3785ae83051800f3223
Alex Peshkov <alex.peshkov@valtech.co.uk>
2017-08-17 11:42:17 +0100

    Unit tests fixed

--- app/api/test/module/Cli/src/Domain/CommandHandler/DataGovUkExportTest.php
+++ app/api/test/module/Cli/src/Domain/CommandHandler/DataGovUkExportTest.php
@@ -86,6 +86,19 @@ class DataGovUkExportTest extends CommandHandlerTestCase
         $this->tmpPath = $this->vfsStream->url() . '/unit';
     }
 
+    protected function initReferences()
+    {
+        $this->categoryReferences = [
+            Category::CATEGORY_REPORT => m::mock(Category::class),
+        ];
+
+        $this->subCategoryReferences = [
+            SubCategory::REPORT_SUB_CATEGORY_PSV  => m::mock(SubCategory::class),
+        ];
+
+        parent::initReferences();
+    }
+
     public function testInvalidReportException()
     {
         $cmd = Cmd::create(
@@ -102,6 +115,9 @@ class DataGovUkExportTest extends CommandHandlerTestCase
         $this->sut->handleCommand($cmd);
     }
 
+    /**
+     * @group test123
+     */
     public function testPsvOperatorListOk()
     {
         $fileName = 'psv-operator-list.csv';
