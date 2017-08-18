b3176539b07d2ac80d699134e022154e10f33aff
Ian Lindsay <ian@hemera-business-services.co.uk>
2017-08-18 10:33:10 +0100

    olcs-17206 added new aliases for pi hearing bookmarks

---
+++ app/api/module/Api/src/Service/Document/Bookmark/Alias/PiHearingTime2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Alias;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\PiHearingTime;
+
+/**
+ * Alias of PiHearingTime
+ */
+class PiHearingTime2 extends PiHearingTime
+{
+    //
+}
