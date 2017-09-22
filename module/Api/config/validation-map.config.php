e9d4d76b6f47dce702f5c628e03c2e8639fdd823
Mat Evans <mat.evans@valtech.co.uk>
2017-09-22 16:30:29 +0100

    OLCS-17988 Set validation handler for QueryHandler\Search\Licence

--- module/Api/config/validation-map.config.php
+++ module/Api/config/validation-map.config.php
@@ -3,6 +3,7 @@
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Standard;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 /**
  * @NOTE When you implement one of the following rules, please move it to the (or create a) relevant
@@ -16,7 +17,6 @@ $map = [
     CommandHandler\Variation\UpdateTypeOfLicence::class => Standard::class, // @todo
     QueryHandler\ApplicationOperatingCentre\ApplicationOperatingCentre::class => Standard::class, // @todo
     QueryHandler\CompaniesHouse\GetList::class => Standard::class, // @todo
-    QueryHandler\Search\Licence::class => Standard::class, // @todo
     QueryHandler\Variation\TypeOfLicence::class => Standard::class, // @todo
 ];
 // @codingStandardsIgnoreEnd
