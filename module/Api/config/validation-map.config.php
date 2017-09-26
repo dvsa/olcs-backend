7ea7236afb4b6ad41c59aee47992792690069182
Mat Evans <mat.evans@valtech.co.uk>
2017-09-26 10:38:23 +0100

    OLCS-17988 Set validation handlers for handlers that had been missed

--- module/Api/config/validation-map.config.php
+++ module/Api/config/validation-map.config.php
@@ -2,22 +2,11 @@
 
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Standard;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NotIsAnonymousUser;
 
-/**
- * @NOTE When you implement one of the following rules, please move it to the (or create a) relevant
- * validation-map/*.config.php. Eventually this file should be empty
- */
 // @codingStandardsIgnoreStart
 $map = [
-    CommandHandler\Variation\DeleteListConditionUndertaking::class => Standard::class, // @todo
-    CommandHandler\Variation\UpdateAddresses::class => Standard::class, // @todo
-    CommandHandler\Variation\UpdateConditionUndertaking::class => Standard::class, // @todo
-    CommandHandler\Variation\UpdateTypeOfLicence::class => Standard::class, // @todo
-    QueryHandler\ApplicationOperatingCentre\ApplicationOperatingCentre::class => Standard::class, // @todo
-    QueryHandler\CompaniesHouse\GetList::class => Standard::class, // @todo
-    QueryHandler\Variation\TypeOfLicence::class => Standard::class, // @todo
+    QueryHandler\CompaniesHouse\GetList::class => NotIsAnonymousUser::class,
 ];
 // @codingStandardsIgnoreEnd
 
