0fd63ccd1866b5378c71a21b5081485be140cf5f
Mat Evans <mat.evans@valtech.co.uk>
2017-10-04 17:04:22 +0100

    OLCS-13652 Increase unit test coverage on Api/Domain namespace

---
+++ app/api/test/module/Api/src/Domain/QueryPartial/WithContactDetailsTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryPartial;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Dvsa\Olcs\Api\Domain\QueryPartial\WithContactDetails;
+use Dvsa\Olcs\Api\Domain\QueryPartial\With;
+use Dvsa\Olcs\Api\Domain\QueryPartial\WithRefdata;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+use Dvsa\Olcs\Api\Entity\ContactDetails;
+use Mockery as m;
+
+/**
+ * WithContactDetailsTest
+ */
+class WithContactDetailsTest extends QueryPartialTestCase
+{
+    /** @var m\Mock */
+    private $em;
+
+    /** @var m\Mock */
+    private $with;
+
+    public function setUp()
+    {
+        $this->em = m::mock(EntityManagerInterface::class);
+        // Cannot mock With as it is Final
+        $this->with = new With();
+        // Cannot mock WithRefdata as it is Final
+        $this->withRefData = new WithRefdata($this->em, $this->with);
+        $this->sut = new WithContactDetails($this->em, $this->with, $this->withRefData);
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testModifyQuery($expectedDql, $arguments)
+    {
+        $entityMetadata = m::mock();
+        $entityMetadata->associationMappings = [
+            'property1' => ['targetEntity' => 'Foo'],
+        ];
+        $this->em->shouldReceive('getClassMetadata')->with(ContactDetails\ContactDetails::class)->once()
+            ->andReturn($entityMetadata);
+        $this->em->shouldReceive('getClassMetadata')->with(ContactDetails\Address::class)->once()
+            ->andReturn($entityMetadata);
+        $this->em->shouldReceive('getClassMetadata')->with(ContactDetails\PhoneContact::class)->once()
+            ->andReturn($entityMetadata);
+        $this->sut->modifyQuery($this->qb, $arguments);
+        $this->assertSame(
+            $expectedDql,
+            $this->qb->getDQL()
+        );
+    }
+
+    public function dataProvider()
+    {
+        return [
+            [
+                'SELECT a, a_cd, a_cd_a, a_cd_a_cc, a_cd_pc FROM foo a LEFT JOIN a.contactDetails a_cd '.
+                    'LEFT JOIN a_cd.address a_cd_a LEFT JOIN a_cd_a.countryCode a_cd_a_cc '.
+                    'LEFT JOIN a_cd.phoneContacts a_cd_pc',
+                []
+            ],
+            [
+                'SELECT a, a_cd, a_cd_a, a_cd_a_cc, a_cd_pc FROM foo a LEFT JOIN a.PROP a_cd '.
+                    'LEFT JOIN a_cd.address a_cd_a LEFT JOIN a_cd_a.countryCode a_cd_a_cc '.
+                    'LEFT JOIN a_cd.phoneContacts a_cd_pc',
+                ['PROP'],
+            ],
+            [
+                'SELECT a, ALIAS, ALIAS_a, ALIAS_a_cc, ALIAS_pc FROM foo a LEFT JOIN a.PROP ALIAS '.
+                    'LEFT JOIN ALIAS.address ALIAS_a LEFT JOIN ALIAS_a.countryCode ALIAS_a_cc '.
+                    'LEFT JOIN ALIAS.phoneContacts ALIAS_pc',
+                ['PROP', 'ALIAS']
+            ],
+        ];
+    }
+}
---
+++ app/api/test/module/Api/src/Domain/QueryPartial/WithCreatedTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryPartial;
+
+use Dvsa\Olcs\Api\Domain\QueryPartial\WithCreatedBy;
+use Dvsa\Olcs\Api\Domain\QueryPartial\With;
+use Mockery as m;
+
+/**
+ * WithCreatedTest
+ */
+class WithCreatedTest extends QueryPartialTestCase
+{
+    /** @var m\Mock */
+    private $with;
+
+    public function setUp()
+    {
+        // Cannot mock With as it is Final
+        $this->with = new With();
+        $this->sut = new WithCreatedBy($this->with);
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testModifyQuery($expectedDql, $arguments)
+    {
+        $this->sut->modifyQuery($this->qb, $arguments);
+        $this->assertSame(
+            $expectedDql,
+            $this->qb->getDQL()
+        );
+    }
+
+    public function dataProvider()
+    {
+        return [
+            [
+                'SELECT a, u, cd, p FROM foo a LEFT JOIN a.createdBy u LEFT JOIN u.contactDetails cd '.
+                    'LEFT JOIN cd.person p',
+                []
+            ],
+            [
+                'SELECT a, u, cd, p FROM foo a LEFT JOIN a.createdBy u LEFT JOIN u.contactDetails cd '.
+                    'LEFT JOIN cd.person p',
+                ['ENTITY']
+            ],
+            [
+                'SELECT a, u, cd, p FROM foo a LEFT JOIN ALIAS.createdBy u LEFT JOIN u.contactDetails cd '.
+                    'LEFT JOIN cd.person p',
+                ['ENTITY', 'ALIAS']
+            ],
+        ];
+    }
+}
