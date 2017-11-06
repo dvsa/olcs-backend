22c163b3bf3983a25ac9fe8577771d2204fb41e8
Richard Ward <richard.ward@bjss.com>
2017-11-06 17:54:45 +0000

    Create an admin task when the last organisation person is removed
    OLCS-18200

--- app/api/module/Api/src/Domain/CommandHandler/Application/Grant/GrantPeople.php
+++ app/api/module/Api/src/Domain/CommandHandler/Application/Grant/GrantPeople.php
@@ -5,14 +5,17 @@
  *
  * @author Rob Caiger <rob@clocal.co.uk>
  */
+
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Application\Grant;
 
+use Dvsa\Olcs\Api\Entity\System\Category;
+use Dvsa\Olcs\Api\Domain\Command\Task\CreateTask;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Domain\Repository\OrganisationPerson as OrganisationPersonRepository;
 use Dvsa\Olcs\Api\Domain\Util\EntityCloner;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
 use Dvsa\Olcs\Api\Entity\Person\Person;
-use Dvsa\Olcs\Transfer\Command\Application\CreateSnapshot;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Dvsa\Olcs\Api\Entity\Application\Application as ApplicationEntity;
 use Dvsa\Olcs\Api\Entity\Application\ApplicationOrganisationPerson;
@@ -57,6 +60,24 @@ final class GrantPeople extends AbstractCommandHandler implements TransactionedI
 
         $this->result->addMessage('Organisation person records have been copied');
 
+        $personCount = $this->getOrganisationRepository()->fetchCountForOrganisation(
+            $application->getLicence()->getOrganisation()->getId()
+        );
+
+        if ($personCount == 0) {
+            $this->handleSideEffect(
+                CreateTask::create(
+                    [
+                        'category' => Category::CATEGORY_APPLICATION,
+                        'subCategory' => Category::TASK_SUB_CATEGORY_DIRECTOR_CHANGE_DIGITAL,
+                        'description' => 'Last person removed',
+                        'licence' => $application->getLicence()->getId(),
+                    ]
+                )
+            );
+            $this->result->addMessage('Task created as there are no people in the organisation');
+        }
+
         return $this->result;
     }
 
@@ -80,7 +101,7 @@ final class GrantPeople extends AbstractCommandHandler implements TransactionedI
         /** Application */
         $targetOp->setPerson($targetPerson);
 
-        $this->getRepo('OrganisationPerson')->save($targetOp);
+        $this->getOrganisationRepository()->save($targetOp);
     }
 
     /**
@@ -111,14 +132,26 @@ final class GrantPeople extends AbstractCommandHandler implements TransactionedI
      * links to
      *
      * @param Organisation $org
-     * @param Person $person
+     * @param Person       $person
      */
     private function deleteByOrgAndPerson(Organisation $org, Person $person)
     {
-        $orgPersonRecords = $this->getRepo('OrganisationPerson')->fetchByOrgAndPerson($org, $person);
+        $orgPersonRecords = $this->getOrganisationRepository()->fetchByOrgAndPerson($org, $person);
 
         foreach ($orgPersonRecords as $orgPersonRecord) {
-            $this->getRepo('OrganisationPerson')->delete($orgPersonRecord);
+            $this->getOrganisationRepository()->delete($orgPersonRecord);
         }
+    }
+
+    /**
+     * Get the OrganisationPerson repository
+     *
+     * @return OrganisationPersonRepository
+     */
+    private function getOrganisationRepository()
+    {
+        /** @var OrganisationPersonRepository $repository */
+        $repository = $this->getRepo('OrganisationPerson');
+        return $repository;
     }
 }
--- app/api/test/module/Api/src/Domain/CommandHandler/Application/Grant/GrantPeopleTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Application/Grant/GrantPeopleTest.php
@@ -5,20 +5,23 @@
  *
  * @author Rob Caiger <rob@clocal.co.uk>
  */
+
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Application\Grant;
 
+use Dvsa\Olcs\Api\Domain\Command\Task\CreateTask;
+use Dvsa\Olcs\Api\Entity\System\Category;
 use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Application\Grant\GrantPeople;
 use Dvsa\Olcs\Api\Entity\Application\Application as ApplicationEntity;
 use Dvsa\Olcs\Api\Entity\Application\ApplicationOrganisationPerson;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
 use Dvsa\Olcs\Api\Entity\Organisation\OrganisationPerson;
 use Dvsa\Olcs\Api\Entity\Person\Person;
 use Mockery as m;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
 use Dvsa\Olcs\Api\Domain\Command\Application\Grant\GrantPeople as GrantPeopleCmd;
-use ZfcRbac\Service\AuthorizationService;
 
 /**
  * Grant People Test
@@ -87,12 +90,12 @@ class GrantPeopleTest extends CommandHandlerTestCase
         $aop1->setAction('A');
         $aop1->setPerson($person1);
 
+        $aop1Saved = false;
+
         $aops = new ArrayCollection();
         $aops->add($aop1);
 
-        /** @var ApplicationEntity $application */
-        $application = m::mock(ApplicationEntity::class)->makePartial();
-        $application->setApplicationOrganisationPersons($aops);
+        $application = $this->createMockApplication($aops);
 
         $this->repoMap['Application']->shouldReceive('fetchUsingId')
             ->with($command)
@@ -112,8 +115,19 @@ class GrantPeopleTest extends CommandHandlerTestCase
         $this->repoMap['OrganisationPerson']->shouldReceive('save')
             ->once()
             ->andReturnUsing(
-                function (OrganisationPerson $orgPerson) use ($aop1) {
+                function (OrganisationPerson $orgPerson) use ($aop1, &$aop1Saved) {
                     $this->assertNotSame($aop1, $orgPerson);
+                    $aop1Saved = true;
+                }
+            );
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchCountForOrganisation')
+            ->once()
+            ->with('TEST_ORGANISATION_ID')
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $this->assertTrue($aop1Saved);
+                    return 1;
                 }
             );
 
@@ -149,12 +163,12 @@ class GrantPeopleTest extends CommandHandlerTestCase
         $aop1->setOriginalPerson($person2);
         $aop1->setOrganisation($org1);
 
+        $aop1Saved = false;
+
         $aops = new ArrayCollection();
         $aops->add($aop1);
 
-        /** @var ApplicationEntity $application */
-        $application = m::mock(ApplicationEntity::class)->makePartial();
-        $application->setApplicationOrganisationPersons($aops);
+        $application = $this->createMockApplication($aops);
 
         $this->repoMap['Application']->shouldReceive('fetchUsingId')
             ->with($command)
@@ -183,13 +197,24 @@ class GrantPeopleTest extends CommandHandlerTestCase
             ->shouldReceive('save')
             ->once()
             ->andReturnUsing(
-                function (OrganisationPerson $orgPerson) use ($aop1) {
+                function (OrganisationPerson $orgPerson) use ($aop1, &$aop1Saved) {
                     $this->assertNotSame($aop1, $orgPerson);
+                    $aop1Saved = true;
                 }
             )
             ->shouldReceive('delete')
             ->once()
             ->with($orgPersonRecord);
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchCountForOrganisation')
+            ->once()
+            ->with('TEST_ORGANISATION_ID')
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $this->assertTrue($aop1Saved);
+                    return 1;
+                }
+            );
 
         $result = $this->sut->handleCommand($command);
 
@@ -221,12 +246,12 @@ class GrantPeopleTest extends CommandHandlerTestCase
         $aop1->setPerson($person2);
         $aop1->setOrganisation($org1);
 
+        $aop1Saved = false;
+
         $aops = new ArrayCollection();
         $aops->add($aop1);
+        $application = $this->createMockApplication($aops);
 
-        /** @var ApplicationEntity $application */
-        $application = m::mock(ApplicationEntity::class)->makePartial();
-        $application->setApplicationOrganisationPersons($aops);
 
         $this->repoMap['Application']->shouldReceive('fetchUsingId')
             ->with($command)
@@ -243,7 +268,22 @@ class GrantPeopleTest extends CommandHandlerTestCase
             ->andReturn($orgPersonRecords)
             ->shouldReceive('delete')
             ->once()
-            ->with($orgPersonRecord);
+            ->with($orgPersonRecord)
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $aop1Saved = true;
+                }
+            );
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchCountForOrganisation')
+            ->once()
+            ->with('TEST_ORGANISATION_ID')
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $this->assertTrue($aop1Saved);
+                    return 1;
+                }
+            );
 
         $result = $this->sut->handleCommand($command);
 
@@ -255,5 +295,105 @@ class GrantPeopleTest extends CommandHandlerTestCase
         ];
 
         $this->assertEquals($expected, $result->toArray());
+    }
+
+    public function testGrantWhenNoMorePeople()
+    {
+        $data = [
+            'id' => 111
+        ];
+
+        $command = GrantPeopleCmd::create($data);
+
+        $person2 = new Person();
+
+        /** @var Organisation|m\Mock $org1 */
+        $org1 = m::mock(Organisation::class)->makePartial();
+
+        /** @var ApplicationOrganisationPerson $aop1 */
+        $aop1 = m::mock(ApplicationOrganisationPerson::class)->makePartial();
+        $aop1->setAction('D');
+        $aop1->setPerson($person2);
+        $aop1->setOrganisation($org1);
+
+        $aop1Saved = false;
+
+        $aops = new ArrayCollection();
+        $aops->add($aop1);
+        $application = $this->createMockApplication($aops);
+
+
+        $this->repoMap['Application']->shouldReceive('fetchUsingId')
+            ->with($command)
+            ->andReturn($application);
+
+        $orgPersonRecord = m::mock(OrganisationPerson::class)->makePartial();
+
+        $orgPersonRecords = [
+            $orgPersonRecord
+        ];
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchByOrgAndPerson')
+            ->with($org1, $person2)
+            ->andReturn($orgPersonRecords)
+            ->shouldReceive('delete')
+            ->once()
+            ->with($orgPersonRecord)
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $aop1Saved = true;
+                }
+            );
+
+        $this->repoMap['OrganisationPerson']->shouldReceive('fetchCountForOrganisation')
+            ->once()
+            ->with('TEST_ORGANISATION_ID')
+            ->andReturnUsing(
+                function () use (&$aop1Saved) {
+                    $this->assertTrue($aop1Saved);
+                    return 0;
+                }
+            );
+
+        $this->expectedSideEffect(
+            CreateTask::class,
+            [
+                'category' => Category::CATEGORY_APPLICATION,
+                'subCategory' => Category::TASK_SUB_CATEGORY_DIRECTOR_CHANGE_DIGITAL,
+                'description' => 'Last person removed',
+                'licence' => 'TEST_LICENCE_ID',
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'Organisation person records have been copied',
+                'Task created as there are no people in the organisation',
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    private function createMockApplication($applicationOrganisationPersons)
+    {
+        /** @var Organisation|m\Mock $organisation */
+        $organisation = m::mock(Organisation::class)->makePartial();
+        $organisation->setId('TEST_ORGANISATION_ID');
+
+        /** @var Licence|m\Mock $licence */
+        $licence = m::mock(Licence::class)->makePartial();
+        $licence->setOrganisation($organisation);
+        $licence->setId('TEST_LICENCE_ID');
+
+        /** @var ApplicationEntity $application */
+        $application = m::mock(ApplicationEntity::class)->makePartial();
+        $application->setApplicationOrganisationPersons($applicationOrganisationPersons);
+        $application->setLicence($licence);
+        return $application;
     }
 }
