e885c7479dd51d4061f09826143f0133618ff9da
Igor <igorstimoli@gmail.com>
2017-11-15 11:46:35 +0000

    OLCS-17960: add batch-clean-variations to Cli

---
+++ app/api/module/Cli/src/Domain/CommandHandler/CleanUpAbandonedVariations.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Clean up abandoned variations
+ */
+namespace Dvsa\Olcs\Cli\Domain\CommandHandler;
+
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Domain\Repository\Application;
+use Dvsa\Olcs\Api\Entity\Application\Application as ApplicationEntity;
+
+/**
+ * Remove abandoned variations
+ */
+
+final class CleanUpAbandonedVariations extends AbstractCommandHandler
+{
+    protected $repoServiceName = 'Application';
+
+    protected $olderThan = '4 hours';
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $mainServiceLocator = $serviceLocator->getServiceLocator();
+
+        $config = $mainServiceLocator->get('Config');
+
+        if (isset($config['batch_config']['clean-abandoned-variations']['older-than'])) {
+            $this->olderThan = $config['batch_config']['clean-abandoned-variations']['older-than'];
+        }
+
+        return parent::createService($serviceLocator);
+    }
+
+    public function handleCommand(CommandInterface $command)
+    {
+        $olderThanDate = date('Y-m-d H:i:s', strtotime('-' . $this->olderThan));
+
+        /** @var Application $repo */
+        $repo = $this->getRepo();
+
+        $abandonedVariations = $repo->fetchAbandonedVariations($olderThanDate);
+
+        /* @var $variation ApplicationEntity */
+        foreach ($abandonedVariations as $variation) {
+
+            $params = ['id' => $variation->getId()];
+            //TODO: call delete command for each variation
+
+        }
+
+        $this->result->addMessage(count($abandonedVariations) . ' abandoned variation records deleted');
+
+        return $this->result;
+    }
+}
