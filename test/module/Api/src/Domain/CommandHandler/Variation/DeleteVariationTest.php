f7e976686e8820eccddfa407d56968dacc0b34ea
Richard Ward <richard.ward@bjss.com>
2017-11-15 14:30:00 +0000

    neaten up test/sut code
    OLCS-18189

--- test/module/Api/src/Domain/CommandHandler/Variation/DeleteVariationTest.php
+++ test/module/Api/src/Domain/CommandHandler/Variation/DeleteVariationTest.php
@@ -37,24 +37,21 @@ class DeleteVariationTest extends CommandHandlerTestCase
     public function testThatApplicationsAreRejected()
     {
         $this->createMockApplication(false, null);
-        $this->expectException(BadVariationTypeException::class);
-        $this->expectExceptionMessage("Applications can not be deleted");
+        $this->expectExceptionWithMessage("Applications can not be deleted");
         $this->sut->handleCommand(DeleteVariation::create(['id' => 'DUMMY_APPLICATION_ID']));
     }
 
     public function testThatStandardVariationsAreRejected()
     {
         $this->createMockApplication(true, null);
-        $this->expectException(BadVariationTypeException::class);
-        $this->expectExceptionMessage("Standard variations can not be deleted");
+        $this->expectExceptionWithMessage("Standard variations can not be deleted");
         $this->sut->handleCommand(DeleteVariation::create(['id' => 'DUMMY_APPLICATION_ID']));
     }
 
     public function testThatOtherVariationTypesAreRejected()
     {
         $this->createMockApplication(true, 'vtyp_foo');
-        $this->expectException(BadVariationTypeException::class);
-        $this->expectExceptionMessage("Variations of type 'vtyp_foo' can not be deleted");
+        $this->expectExceptionWithMessage("Variations of type 'vtyp_foo' can not be deleted");
         $this->sut->handleCommand(DeleteVariation::create(['id' => 'DUMMY_APPLICATION_ID']));
     }
 
@@ -62,27 +59,72 @@ class DeleteVariationTest extends CommandHandlerTestCase
     {
         $application = $this->createMockApplication(true, Application::VARIATION_TYPE_DIRECTOR_CHANGE);
 
+        $this->expectDeletePersons($application);
+        $this->expectDeleteDocuments($application);
+        $this->expectDeletePreviousConvictions($application);
+        $this->expectDeleteApplication($application);
+
+        $this->assertResult(
+            $this->sut->handleCommand(DeleteVariation::create(['id' => 'DUMMY_APPLICATION_ID'])),
+            [
+                'id' => ['application DUMMY_APPLICATION_ID' => 'DUMMY_APPLICATION_ID'],
+                'messages' => ['Application with id DUMMY_APPLICATION_ID was deleted']
+            ]
+        );
+    }
+
+    private function createMockApplication($isVariation, $variationType)
+    {
+        /** @var Application|m\Mock $application */
+        $application = m::mock(Application::class)->makePartial();
+        $application->setId('DUMMY_APPLICATION_ID');
+        $application->setIsVariation($isVariation);
+        if ($variationType !== null) {
+            $application->setVariationType(new RefData($variationType));
+        }
+        $this->repoMap['Application']->shouldReceive('fetchById')->with($application->getId())->andReturn(
+            $application
+        );
+        return $application;
+    }
+
+    private function createMockApplicationOrganisationPerson($personId)
+    {
+        return m::mock(ApplicationOrganisationPerson::class)->shouldReceive('getPerson')->with()->andReturn(
+            m::mock(Person::class)->shouldReceive('getId')->with()->andReturn($personId)->getMock()
+        )->getMock();
+    }
+
+    private function createMockDocument($documentId)
+    {
+        return m::mock(Document::class)->shouldReceive('getId')->with()->andReturn($documentId)->getMock();
+    }
+
+    private function createMockConviction($convictionId)
+    {
+        return m::mock(PreviousConviction::class)->shouldReceive('getId')->with()->andReturn($convictionId)->getMock();
+    }
+
+    private function expectDeletePersons(m\MockInterface $application)
+    {
         $application->shouldReceive('getApplicationOrganisationPersons')->with()->andReturn(
             [
-                $this->createApplicationOrganisationPersonMock('DUMMY_PERSON_ID_1'),
-                $this->createApplicationOrganisationPersonMock('DUMMY_PERSON_ID_2'),
+                $this->createMockApplicationOrganisationPerson('DUMMY_PERSON_ID_1'),
+                $this->createMockApplicationOrganisationPerson('DUMMY_PERSON_ID_2'),
             ]
         );
 
         $this->expectedSideEffect(
             DeletePeople::class,
-            [
-                'id' => 'DUMMY_APPLICATION_ID',
-                'personIds' => [
-                    'DUMMY_PERSON_ID_1',
-                    'DUMMY_PERSON_ID_2',
-                ]
-            ],
+            ['id' => 'DUMMY_APPLICATION_ID', 'personIds' => ['DUMMY_PERSON_ID_1', 'DUMMY_PERSON_ID_2',]],
             new Result()
         );
+    }
 
+    private function expectDeleteDocuments(m\MockInterface $application)
+    {
         $application->shouldReceive('getDocuments')->with()->andReturn(
-            [$this->createDocumentMock('DUMMY_DOCUMENT_ID_1'), $this->createDocumentMock('DUMMY_DOCUMENT_ID_2')]
+            [$this->createMockDocument('DUMMY_DOCUMENT_ID_1'), $this->createMockDocument('DUMMY_DOCUMENT_ID_2')]
         );
 
         $this->expectedSideEffect(
@@ -90,9 +132,12 @@ class DeleteVariationTest extends CommandHandlerTestCase
             ['ids' => ['DUMMY_DOCUMENT_ID_1', 'DUMMY_DOCUMENT_ID_2',]],
             new Result()
         );
+    }
 
+    private function expectDeletePreviousConvictions(m\MockInterface $application)
+    {
         $application->shouldReceive('getPreviousConvictions')->with()->andReturn(
-            [$this->createConvictionMock('DUMMY_CONVICTION_ID_1'), $this->createConvictionMock('DUMMY_CONVICTION_ID_2')]
+            [$this->createMockConviction('DUMMY_CONVICTION_ID_1'), $this->createMockConviction('DUMMY_CONVICTION_ID_2')]
         );
 
         $this->expectedSideEffect(
@@ -100,67 +145,24 @@ class DeleteVariationTest extends CommandHandlerTestCase
             ['ids' => ['DUMMY_CONVICTION_ID_1', 'DUMMY_CONVICTION_ID_2',]],
             new Result()
         );
+    }
 
+    private function expectDeleteApplication(Application $application)
+    {
         $this->repoMap['Application']->shouldReceive('delete')->once()->with($application);
+    }
 
-        $result = $this->sut->handleCommand(DeleteVariation::create(['id' => 'DUMMY_APPLICATION_ID']));
-
-        self::assertInstanceOf(Result::class, $result);
-        $this->assertSame(
-            [
-                'id' => ['application DUMMY_APPLICATION_ID' => 'DUMMY_APPLICATION_ID'],
-                'messages' => [
-                    'Application with id DUMMY_APPLICATION_ID was deleted'
-                ]
-            ],
-            $result->toArray()
-        );
+    private function assertResult(Result $result, $data)
+    {
+        $this->assertSame($data, $result->toArray());
     }
 
     /**
-     * @param $isVariation
-     * @param $variationType
-     *
-     * @return Application|m\Mock
+     * @param string $message
      */
-    private function createMockApplication($isVariation, $variationType)
+    private function expectExceptionWithMessage($message)
     {
-        /** @var Application|m\Mock $application */
-        $application = m::mock(Application::class)->makePartial();
-        $application->setIsVariation($isVariation);
-        if ($variationType !== null) {
-            $application->setVariationType(new RefData($variationType));
-        }
-        $this->repoMap['Application']->shouldReceive('fetchById')->with('DUMMY_APPLICATION_ID')->andReturn(
-            $application
-        );
-        return $application;
-    }
-
-    /**
-     * @param $personId
-     *
-     * @return m\MockInterface
-     */
-    protected function createApplicationOrganisationPersonMock($personId)
-    {
-        return m::mock(ApplicationOrganisationPerson::class)->shouldReceive('getPerson')->with()->andReturn(
-            m::mock(Person::class)->shouldReceive('getId')->with()->andReturn($personId)->getMock()
-        )->getMock();
-    }
-
-    /**
-     * @param $documentId
-     *
-     * @return m\MockInterface
-     */
-    protected function createDocumentMock($documentId)
-    {
-        return m::mock(Document::class)->shouldReceive('getId')->with()->andReturn($documentId)->getMock();
-    }
-
-    protected function createConvictionMock($convictionId)
-    {
-        return m::mock(PreviousConviction::class)->shouldReceive('getId')->with()->andReturn($convictionId)->getMock();
+        $this->expectException(BadVariationTypeException::class);
+        $this->expectExceptionMessage($message);
     }
 }
