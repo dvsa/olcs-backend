0b8aa763879bddcf537a08377fff22939549552c
Richard Ward <richard.ward@bjss.com>
2017-11-15 11:42:15 +0000

    Implement a DeleteVariation command/handler
    It can only be used for director change variations
    
    it doesn't yet delete child entities
    
    OLCS-18189

---
+++ app/api/module/Api/src/Domain/CommandHandler/Variation/DeleteVariation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Variation;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Domain\Exception\BadVariationTypeException;
+use Dvsa\Olcs\Api\Entity\Application\Application;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Transfer\Command\Variation\DeleteVariation as DeleteVariationCommand;
+
+/**
+ * Class GrantDirectorChange
+ */
+class DeleteVariation extends AbstractCommandHandler implements TransactionedInterface
+{
+    protected $repoServiceName = 'Application';
+
+    /**
+     * @param DeleteVariationCommand|CommandInterface $command GrantDirectorChange Command
+     *
+     * @return Result
+     * @throws BadVariationTypeException
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        /** @var Application $application */
+        $application = $this->getRepo()->fetchById($command->getId());
+
+        if (!$application->getIsVariation()) {
+            throw new BadVariationTypeException("Applications can not be deleted");
+        }
+
+        if ($application->getVariationType() === null) {
+            throw new BadVariationTypeException("Standard variations can not be deleted");
+        }
+
+        $variationType = $application->getVariationType()->getId();
+        if ($variationType !== Application::VARIATION_TYPE_DIRECTOR_CHANGE) {
+            throw new BadVariationTypeException("Variations of type '{$variationType}' can not be deleted");
+        }
+
+        $this->getRepo()->delete($application);
+        $this->result->addId('application ' . $command->getId(), $command->getId());
+        $this->result->addMessage('Application with id ' . $command->getId() . ' was deleted');
+
+        return $this->result;
+    }
+}
