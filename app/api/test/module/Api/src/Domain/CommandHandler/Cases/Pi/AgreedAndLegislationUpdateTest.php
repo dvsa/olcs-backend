3ef24070400299ce859f328d0b05d53f3fd5a317
Igor <igorstimoli@gmail.com>
2017-11-29 10:14:16 +0000

    OLCS-18171: test updates and CS fixes

--- app/api/test/module/Api/src/Domain/CommandHandler/Cases/Pi/AgreedAndLegislationUpdateTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Cases/Pi/AgreedAndLegislationUpdateTest.php
@@ -6,6 +6,7 @@
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Cases\Pi;
 
 use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Mockery as m;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Cases\Pi\AgreedAndLegislationUpdate;
@@ -17,6 +18,7 @@ use Dvsa\Olcs\Api\Entity\Pi\Pi as PiEntity;
 use Dvsa\Olcs\Api\Entity\Pi\PresidingTc as PresidingTcEntity;
 use Dvsa\Olcs\Api\Entity\Cases\Cases as CasesEntity;
 use Dvsa\Olcs\Api\Entity\Pi\Reason as ReasonEntity;
+use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
 
 /**
  * Agreed and Legislation Update Test
@@ -52,6 +54,9 @@ class AgreedAndLegislationUpdateTest extends CommandHandlerTestCase
             ],
             ReasonEntity::class => [
                 7 => m::mock(ReasonEntity::class)
+            ],
+            UserEntity::class => [
+                20 => m::mock(UserEntity::class)
             ]
         ];
 
@@ -107,6 +112,249 @@ class AgreedAndLegislationUpdateTest extends CommandHandlerTestCase
 
         $result = $this->sut->handleCommand($command);
 
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    public function testHandleCommandEcmsCaseFalseNoAssignedCaseworkerNoEcmsDate()
+    {
+        $agreedByTc = 44;
+        $agreedByTcRole = 'tc_r_dhtru';
+        $agreedDate = '2015-06-12';
+        $comment = 'comment';
+        $piTypes = ['pi_t_imp', 'pi_t_other'];
+        $reasons = [5,7];
+        $id = 22;
+        $version = 33;
+        $isEcmsCase = 'N';
+        $assignedCaseworker = null;
+        $ecmsFirstReceivedDate = null;
+        $commandData = [
+            'id' => $id,
+            'version' => $version,
+            'comment' => $comment,
+            'agreedByTc' => $agreedByTc,
+            'agreedByTcRole' => $agreedByTcRole,
+            'agreedDate' => $agreedDate,
+            'piTypes' => $piTypes,
+            'reasons' => $reasons,
+            'isEcmsCase' => $isEcmsCase,
+            'assignedCaseworker' => $assignedCaseworker,
+            'ecmsFirstReceivedDate' => $ecmsFirstReceivedDate
+        ];
+
+        $command = Cmd::Create($commandData);
+
+        /**@var PiEntity $pi **/
+        $pi = m::mock(PiEntity::class)->makePartial();
+
+        $this->repoMap['Pi']
+            ->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT, $version)
+            ->andReturn($pi)
+            ->shouldReceive('save')
+            ->with(m::type(PiEntity::class))
+            ->once()
+            ->andReturnUsing(
+                function (PiEntity $pi) use ($id) {
+                    $pi->setId($id);
+                }
+            );
+
+        $this->expectedSideEffect(
+            GenerateSlaTargetDateCmd::class,
+            [
+                'pi' => $id
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+        $this->assertEquals(0, $pi->getIsEcmsCase());
+        $this->assertNull($pi->getAssignedCaseworker());
+        $this->assertNull($pi->getEcmsFirstReceivedDate());
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    public function testHandleCommandEcmsCaseTrueNoAssignedCaseworkerNoEcmsDate()
+    {
+        $agreedByTc = 44;
+        $agreedByTcRole = 'tc_r_dhtru';
+        $agreedDate = '2015-06-12';
+        $comment = 'comment';
+        $piTypes = ['pi_t_imp', 'pi_t_other'];
+        $reasons = [5,7];
+        $id = 22;
+        $version = 33;
+        $isEcmsCase = 'Y';
+        $assignedCaseworker = null;
+        $ecmsFirstReceivedDate = null;
+        $commandData = [
+            'id' => $id,
+            'version' => $version,
+            'comment' => $comment,
+            'agreedByTc' => $agreedByTc,
+            'agreedByTcRole' => $agreedByTcRole,
+            'agreedDate' => $agreedDate,
+            'piTypes' => $piTypes,
+            'reasons' => $reasons,
+            'isEcmsCase' => $isEcmsCase,
+            'assignedCaseworker' => $assignedCaseworker,
+            'ecmsFirstReceivedDate' => $ecmsFirstReceivedDate
+        ];
+
+        $command = Cmd::Create($commandData);
+
+        /**@var PiEntity $pi **/
+        $pi = m::mock(PiEntity::class)->makePartial();
+
+        $this->repoMap['Pi']
+            ->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT, $version)
+            ->andReturn($pi)
+            ->shouldReceive('save')
+            ->with(m::type(PiEntity::class))
+            ->once()
+            ->andReturnUsing(
+                function (PiEntity $pi) use ($id) {
+                    $pi->setId($id);
+                }
+            );
+
+        $this->expectedSideEffect(
+            GenerateSlaTargetDateCmd::class,
+            [
+                'pi' => $id
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+        $this->assertEquals(1, $pi->getIsEcmsCase());
+        $this->assertNull($pi->getAssignedCaseworker());
+        $this->assertNull($pi->getEcmsFirstReceivedDate());
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    public function testHandleCommandEcmsCaseTrueWithAssignedCaseworkerWithDate()
+    {
+        $agreedByTc = 44;
+        $agreedByTcRole = 'tc_r_dhtru';
+        $agreedDate = '2015-06-12';
+        $comment = 'comment';
+        $piTypes = ['pi_t_imp', 'pi_t_other'];
+        $reasons = [5,7];
+        $id = 22;
+        $version = 33;
+        $isEcmsCase = 'Y';
+        $assignedCaseworker = 20;
+        $ecmsFirstReceivedDate = '2015-12-12';
+        $commandData = [
+            'id' => $id,
+            'version' => $version,
+            'comment' => $comment,
+            'agreedByTc' => $agreedByTc,
+            'agreedByTcRole' => $agreedByTcRole,
+            'agreedDate' => $agreedDate,
+            'piTypes' => $piTypes,
+            'reasons' => $reasons,
+            'isEcmsCase' => $isEcmsCase,
+            'assignedCaseworker' => $assignedCaseworker,
+            'ecmsFirstReceivedDate' => $ecmsFirstReceivedDate
+        ];
+
+        $command = Cmd::Create($commandData);
+
+        /**@var PiEntity $pi **/
+        $pi = m::mock(PiEntity::class)->makePartial();
+
+        $this->repoMap['Pi']
+            ->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT, $version)
+            ->andReturn($pi)
+            ->shouldReceive('save')
+            ->with(m::type(PiEntity::class))
+            ->once()
+            ->andReturnUsing(
+                function (PiEntity $pi) use ($id) {
+                    $pi->setId($id);
+                }
+            );
+
+        $this->expectedSideEffect(
+            GenerateSlaTargetDateCmd::class,
+            [
+                'pi' => $id
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+        $this->assertEquals(1, $pi->getIsEcmsCase());
+        $this->assertEquals(
+            (new DateTime('2015-12-12', new \DateTimeZone('UTC')))->format('Y-m-d'),
+            $pi->getEcmsFirstReceivedDate()->format('Y-m-d')
+        );
+        $this->assertEquals($assignedCaseworker, $pi->getAssignedCaseworker()->getId());
+        $this->assertInstanceOf(Result::class, $result);
+    }
+
+    public function testHandleCommandEcmsCaseFalseWithAssignedCaseworkerWithDate()
+    {
+        $agreedByTc = 44;
+        $agreedByTcRole = 'tc_r_dhtru';
+        $agreedDate = '2015-06-12';
+        $comment = 'comment';
+        $piTypes = ['pi_t_imp', 'pi_t_other'];
+        $reasons = [5,7];
+        $id = 22;
+        $version = 33;
+        $isEcmsCase = 'F';
+        $assignedCaseworker = 20;
+        $ecmsFirstReceivedDate = '2015-12-12';
+        $commandData = [
+            'id' => $id,
+            'version' => $version,
+            'comment' => $comment,
+            'agreedByTc' => $agreedByTc,
+            'agreedByTcRole' => $agreedByTcRole,
+            'agreedDate' => $agreedDate,
+            'piTypes' => $piTypes,
+            'reasons' => $reasons,
+            'isEcmsCase' => $isEcmsCase,
+            'assignedCaseworker' => $assignedCaseworker,
+            'ecmsFirstReceivedDate' => $ecmsFirstReceivedDate
+        ];
+
+        $command = Cmd::Create($commandData);
+
+        /**@var PiEntity $pi **/
+        $pi = m::mock(PiEntity::class)->makePartial();
+
+        $this->repoMap['Pi']
+            ->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT, $version)
+            ->andReturn($pi)
+            ->shouldReceive('save')
+            ->with(m::type(PiEntity::class))
+            ->once()
+            ->andReturnUsing(
+                function (PiEntity $pi) use ($id) {
+                    $pi->setId($id);
+                }
+            );
+
+        $this->expectedSideEffect(
+            GenerateSlaTargetDateCmd::class,
+            [
+                'pi' => $id
+            ],
+            new Result()
+        );
+
+        $result = $this->sut->handleCommand($command);
+        $this->assertEquals(0, $pi->getIsEcmsCase());
+        $this->assertNull($pi->getEcmsFirstReceivedDate());
+        $this->assertEquals($assignedCaseworker, $pi->getAssignedCaseworker()->getId());
         $this->assertInstanceOf(Result::class, $result);
     }
 }
