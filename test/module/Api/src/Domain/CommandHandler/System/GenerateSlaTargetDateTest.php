22964fd22487869d18795729dc646038e2fb9c5a
Igor <igorstimoli@gmail.com>
2018-01-12 11:56:54 +0000

    OLCS=-18145: Update GenerateSlaTargetDateTest

--- test/module/Api/src/Domain/CommandHandler/System/GenerateSlaTargetDateTest.php
+++ test/module/Api/src/Domain/CommandHandler/System/GenerateSlaTargetDateTest.php
@@ -11,8 +11,10 @@ use Dvsa\Olcs\Api\Domain\Repository\Sla as SlaRepo;
 use Dvsa\Olcs\Api\Domain\Repository\SlaTargetDate as Repo;
 use Dvsa\Olcs\Api\Domain\Repository\Submission as SubmissionRepo;
 use Dvsa\Olcs\Api\Domain\Util\SlaCalculatorInterface;
+use Dvsa\Olcs\Api\Entity\Cases\ProposeToRevoke;
 use Dvsa\Olcs\Api\Entity\Pi\Pi as PiEntity;
 use Dvsa\Olcs\Api\Entity\Submission\Submission as SubmissionEntity;
+use Dvsa\Olcs\Api\Entity\Cases\ProposeToRevoke as ProposeToRevokeEntity;
 use Dvsa\Olcs\Api\Entity\System\Sla as SlaEntity;
 use Dvsa\Olcs\Api\Entity\System\SlaTargetDate as SlaTargetDateEntity;
 use Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea as TrafficAreaEntity;
@@ -31,6 +33,7 @@ class GenerateSlaTargetDateTest extends CommandHandlerTestCase
         $this->mockRepo('Sla', SlaRepo::class);
         $this->mockRepo('SlaTargetDate', Repo::class);
         $this->mockRepo('Submission', SubmissionRepo::class);
+        $this->mockRepo('ProposeToRevoke', ProposeToRevoke::class);
 
         $this->mockedSmServices = [
             SlaCalculatorInterface::class => m::mock(SlaCalculatorInterface::class)
@@ -50,89 +53,14 @@ class GenerateSlaTargetDateTest extends CommandHandlerTestCase
         parent::initReferences();
     }
 
-    public function testHandleCommandForPi()
+    public function testHandleCommandForPublicInquiry()
     {
         $params = [
             'pi' => 111,
         ];
         $command = Command::create($params);
 
-        $v1 = new \DateTime('2015-01-01');
-        $v2 = new \DateTime('2015-02-02');
-
-        /** @var PiEntity $piEntity */
-        $piEntity = m::mock(PiEntity::class)->makePartial();
-        $piEntity->setId($params['pi']);
-        $piEntity->setSlaTargetDates(new ArrayCollection());
-        $piEntity->shouldReceive('getCase->isTm')->once()->andReturn(true);
-        $piEntity->shouldReceive('getField1')->andReturn($v1);
-        $piEntity->shouldReceive('getField2')->andReturn($v2);
-        $piEntity->shouldReceive('getField3')->andReturn(null);
-
-        $this->repoMap['Pi']->shouldReceive('fetchById')
-            ->once()
-            ->with($params['pi'])
-            ->andReturn($piEntity)
-            ->shouldReceive('save')
-            ->once();
-
-        $sla1 = m::mock(SlaEntity::class)->makePartial();
-        $sla1->setId(1);
-        $sla1->setCompareTo('field1');
-        $sla1->shouldReceive('appliesTo')->with($v1)->once()->andReturn(true);
-
-        $sla2 = m::mock(SlaEntity::class)->makePartial();
-        $sla2->setId(2);
-        $sla2->setCompareTo('field2');
-        $sla2->shouldReceive('appliesTo')->with($v2)->once()->andReturn(true);
-
-        $sla3 = m::mock(SlaEntity::class)->makePartial();
-        $sla3->setId(3);
-        $sla3->setCompareTo('field3');
-        $sla3->shouldReceive('appliesTo')->never();
-
-        $slaTargetDate2 = m::mock(SlaTargetDateEntity::class)->makePartial();
-        $slaTargetDate2->setSla($sla2);
-        $slaTargetDate2->setAgreedDate(new \DateTime('2014-02-02'));
-        $piEntity->getSlaTargetDates()->set($sla2->getId(), $slaTargetDate2);
-
-        $slaTargetDate3 = m::mock(SlaTargetDateEntity::class)->makePartial();
-        $slaTargetDate3->setSla($sla3);
-        $slaTargetDate3->setAgreedDate(new \DateTime('2014-03-03'));
-        $piEntity->getSlaTargetDates()->set($sla3->getId(), $slaTargetDate3);
-
-        $v1Sla = new \DateTime('2016-01-01');
-        $v2Sla = new \DateTime('2016-02-02');
-        $this->mockedSmServices[SlaCalculatorInterface::class]
-            ->shouldReceive('applySla')
-            ->once()
-            ->with($v1, $sla1, $this->references[TrafficAreaEntity::class][TrafficAreaEntity::SE_MET_TRAFFIC_AREA_CODE])
-            ->andReturn($v1Sla)
-            ->shouldReceive('applySla')
-            ->once()
-            ->with($v2, $sla2, $this->references[TrafficAreaEntity::class][TrafficAreaEntity::SE_MET_TRAFFIC_AREA_CODE])
-            ->andReturn($v2Sla);
-
-        $this->repoMap['Sla']->shouldReceive('fetchByCategories')
-            ->once()
-            ->with(['pi', 'pi_hearing'], Query::HYDRATE_OBJECT)
-            ->andReturn([$sla1, $sla2, $sla3]);
-
-        $result = $this->sut->handleCommand($command);
-
-        $this->assertEquals(['SLA Target Dates successfully saved'], $result->getMessages());
-
-        $this->assertEquals(2, $piEntity->getSlaTargetDates()->count());
-
-        $std1 = $piEntity->getSlaTargetDates()->get($sla1->getId());
-        $this->assertEquals($sla1, $std1->getSla());
-        $this->assertEquals($v1, $std1->getAgreedDate());
-        $this->assertEquals($v1Sla, $std1->getTargetDate());
-
-        $std2 = $piEntity->getSlaTargetDates()->get($sla2->getId());
-        $this->assertEquals($sla2, $std2->getSla());
-        $this->assertEquals($v2, $std2->getAgreedDate());
-        $this->assertEquals($v2Sla, $std2->getTargetDate());
+        $this->doTest($command, $params, 'pi', PiEntity::class, 'Pi', ['pi', 'pi_hearing']);
     }
 
     public function testHandleCommandForSubmission()
@@ -142,24 +70,39 @@ class GenerateSlaTargetDateTest extends CommandHandlerTestCase
         ];
         $command = Command::create($params);
 
+        $this->doTest($command, $params, 'submission', SubmissionEntity::class ,'Submission', ['submission']);
+    }
+
+    public function testHandleCommandForProposeToRevoke()
+    {
+        $params = [
+            'proposeToRevoke' => 111,
+        ];
+        $command = Command::create($params);
+
+        $this->doTest($command, $params, 'proposeToRevoke', ProposeToRevokeEntity::class, 'ProposeToRevoke', ['ptr']);
+    }
+
+    public function doTest(Command $command, $params, $entityParam, $entityClass, $repoName, $categories)
+    {
+
         $v1 = new \DateTime('2015-01-01');
         $v2 = new \DateTime('2015-02-02');
 
-        /** @var SubmissionEntity $submissionEntity */
-        $submissionEntity = m::mock(SubmissionEntity::class)->makePartial();
-        $submissionEntity->setId($params['submission']);
-        $submissionEntity->setSlaTargetDates(new ArrayCollection());
-        $submissionEntity->shouldReceive('getCase->isTm')->once()->andReturn(false);
-        $submissionEntity->shouldReceive('getCase->getLicence->getTrafficArea')->once()
+        $entity = m::mock($entityClass)->makePartial();
+        $entity->setId($params[$entityParam]);
+        $entity->setSlaTargetDates(new ArrayCollection());
+        $entity->shouldReceive('getCase->isTm')->once()->andReturn(false);
+        $entity->shouldReceive('getCase->getLicence->getTrafficArea')->once()
             ->andReturn($this->references[TrafficAreaEntity::class][TrafficAreaEntity::SE_MET_TRAFFIC_AREA_CODE]);
-        $submissionEntity->shouldReceive('getField1')->andReturn($v1);
-        $submissionEntity->shouldReceive('getField2')->andReturn($v2);
-        $submissionEntity->shouldReceive('getField3')->andReturn(null);
+        $entity->shouldReceive('getField1')->andReturn($v1);
+        $entity->shouldReceive('getField2')->andReturn($v2);
+        $entity->shouldReceive('getField3')->andReturn(null);
 
-        $this->repoMap['Submission']->shouldReceive('fetchById')
+        $this->repoMap[$repoName]->shouldReceive('fetchById')
             ->once()
-            ->with($params['submission'])
-            ->andReturn($submissionEntity)
+            ->with($params[$entityParam])
+            ->andReturn($entity)
             ->shouldReceive('save')
             ->once();
 
@@ -181,12 +124,12 @@ class GenerateSlaTargetDateTest extends CommandHandlerTestCase
         $slaTargetDate2 = m::mock(SlaTargetDateEntity::class)->makePartial();
         $slaTargetDate2->setSla($sla2);
         $slaTargetDate2->setAgreedDate(new \DateTime('2014-02-02'));
-        $submissionEntity->getSlaTargetDates()->set($sla2->getId(), $slaTargetDate2);
+        $entity->getSlaTargetDates()->set($sla2->getId(), $slaTargetDate2);
 
         $slaTargetDate3 = m::mock(SlaTargetDateEntity::class)->makePartial();
         $slaTargetDate3->setSla($sla3);
         $slaTargetDate3->setAgreedDate(new \DateTime('2014-03-03'));
-        $submissionEntity->getSlaTargetDates()->set($sla3->getId(), $slaTargetDate3);
+        $entity->getSlaTargetDates()->set($sla3->getId(), $slaTargetDate3);
 
         $v1Sla = new \DateTime('2016-01-01');
         $v2Sla = new \DateTime('2016-02-02');
@@ -202,21 +145,21 @@ class GenerateSlaTargetDateTest extends CommandHandlerTestCase
 
         $this->repoMap['Sla']->shouldReceive('fetchByCategories')
             ->once()
-            ->with(['submission'], Query::HYDRATE_OBJECT)
+            ->with($categories, Query::HYDRATE_OBJECT)
             ->andReturn([$sla1, $sla2, $sla3]);
 
         $result = $this->sut->handleCommand($command);
 
         $this->assertEquals(['SLA Target Dates successfully saved'], $result->getMessages());
 
-        $this->assertEquals(2, $submissionEntity->getSlaTargetDates()->count());
+        $this->assertEquals(2, $entity->getSlaTargetDates()->count());
 
-        $std1 = $submissionEntity->getSlaTargetDates()->get($sla1->getId());
+        $std1 = $entity->getSlaTargetDates()->get($sla1->getId());
         $this->assertEquals($sla1, $std1->getSla());
         $this->assertEquals($v1, $std1->getAgreedDate());
         $this->assertEquals($v1Sla, $std1->getTargetDate());
 
-        $std2 = $submissionEntity->getSlaTargetDates()->get($sla2->getId());
+        $std2 = $entity->getSlaTargetDates()->get($sla2->getId());
         $this->assertEquals($sla2, $std2->getSla());
         $this->assertEquals($v2, $std2->getAgreedDate());
         $this->assertEquals($v2Sla, $std2->getTargetDate());
