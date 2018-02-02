38ec4e37cb8fe3cd7837842032b9f8954f86270a
Igor <igorstimoli@gmail.com>
2018-02-02 17:33:44 +0000

    OLCS-18803: fix document_to_delete processing

--- app/api/module/Api/src/Entity/Doc/DocumentToDelete.php
+++ app/api/module/Api/src/Entity/Doc/DocumentToDelete.php
@@ -3,6 +3,7 @@
 namespace Dvsa\Olcs\Api\Entity\Doc;
 
 use Doctrine\ORM\Mapping as ORM;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 
 /**
  * DocumentToDelete Entity
@@ -13,4 +14,31 @@ use Doctrine\ORM\Mapping as ORM;
 class DocumentToDelete extends AbstractDocumentToDelete
 {
 
+    const MAX_ATTEMPTS = 3;
+
+    const PROCESS_AFTER_MINUTES = 2;
+
+    const INCREMENT_PROCESS_AFTER_EXPONENTIALLY = true;
+
+    public function markAsFailed()
+    {
+        $this->setAttempts($this->getAttempts() + 1);
+        $this->setProcessAfterDate($this->estimateProcessAfterDate());
+    }
+
+    /**
+     * @return DateTime
+     */
+    private function estimateProcessAfterDate()
+    {
+        $now = new DateTime();
+        $minutesToAdd = self::PROCESS_AFTER_MINUTES;
+        if (self::INCREMENT_PROCESS_AFTER_EXPONENTIALLY) {
+            $minutesToAdd = pow(self::PROCESS_AFTER_MINUTES, $this->getAttempts());
+        }
+
+        $now->add(new \DateInterval('PT'.$minutesToAdd.'M'));
+
+        return $now;
+    }
 }
