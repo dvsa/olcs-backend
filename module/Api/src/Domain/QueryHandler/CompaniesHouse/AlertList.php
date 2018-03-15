0f8bec13d34619dc37d26d9e869deb04189f4905
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-03-15 17:31:22 +0000

    WIP - gettting licence info

--- module/Api/src/Domain/QueryHandler/CompaniesHouse/AlertList.php
+++ module/Api/src/Domain/QueryHandler/CompaniesHouse/AlertList.php
@@ -28,16 +28,42 @@ class AlertList extends AbstractQueryHandler
         $repo = $this->getRepo();
 
         $results = [];
-        //$companiesHouseAlerts
-            $results = $repo->fetchCaListWithLicences($query);
-/*
+
+        $companiesHouseAlerts = $repo->fetchCaListWithLicences($query);
+
+        $results = $this->resultList(
+            $repo->fetchList($query, \Doctrine\ORM\Query::HYDRATE_OBJECT),
+            [
+                'organisation',
+                'reasons' => [
+                    'reasonType',
+                ],
+            ]
+        );
+        $licences = [];
         foreach ($companiesHouseAlerts as $companiesHouseAlert) {
-            $result = $this->resultList([$companiesHouseAlert], ['organisation' => ['licences'], 'reasons' => ['reasonType']]);
-            foreach ($companiesHouseAlert[0]->getOrganisation()->getLicences() as $licence) {
-                $result['licence'] = $this->resultList([$licence])[0];
+            $companiesHouseAlert->getOrganisation()->getActiveLicences();
+            $licences [] = $this->resultList(
+                [$companiesHouseAlert],
+                ['organisation' => ['licences'], 'reasons' => ['reasonType']]
+            );
+            foreach ($licences[0] as $licence) {
+                $data [] = $licence;
             }
-            $results[] = $result;
-        }*/
+
+        }
+
+
+        /**
+         * foreach ($companiesHouseAlerts as $companiesHouseAlert) {
+         * $result = $this->resultList([$companiesHouseAlert],
+         * ['organisation' => ['licences'], 'reasons' => ['reasonType']]);
+         * foreach ($companiesHouseAlert[0]->getOrganisation()->getLicences() as $licence) {
+         * $result['licence'] = $this->resultList([$licence])[0];
+         * }
+         * $results[] = $result;
+         * }
+         **/
         return [
             'result' => $results,
             'count' => $repo->fetchCount($query),
