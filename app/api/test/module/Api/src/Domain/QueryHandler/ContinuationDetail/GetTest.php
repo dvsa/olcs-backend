fdb66c006ebf28aa4d16115ab5c09d49301b0ed6
Igor <igorstimoli@gmail.com>
2018-03-23 13:43:51 +0000

    OLCS-19229: handle old payments with invalid transactions

--- app/api/test/module/Api/src/Domain/QueryHandler/ContinuationDetail/GetTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/ContinuationDetail/GetTest.php
@@ -33,9 +33,12 @@ class GetTest extends QueryHandlerTestCase
         parent::setUp();
     }
 
-    public function testHandleQuery()
+    private function mockContinuationDetail($data = [])
     {
-        $query = Qry::create(['id'=> 123]);
+        $defaultData = [
+            'getOtherFinancesAmount' => 0
+        ];
+        $data = array_merge($defaultData, $data);
 
         $continuationDetail = m::mock(BundleSerializableInterface::class);
         $continuationDetail
@@ -49,8 +52,8 @@ class GetTest extends QueryHandlerTestCase
                             ->shouldReceive('getType')->with()->andReturn(
                                 m::mock()->shouldReceive('getId')->andReturn('ORG_TYPE_ID')->getMock()
                             )->once()
-                        ->shouldReceive('getId')->with()->andReturn(99)->once()
-                        ->getMock()
+                            ->shouldReceive('getId')->with()->andReturn(99)->once()
+                            ->getMock()
                     )
                     ->twice()
                     ->shouldReceive('getId')
@@ -78,18 +81,35 @@ class GetTest extends QueryHandlerTestCase
             ->andReturn(0)
             ->once()
             ->shouldReceive('getOtherFinancesAmount')
-            ->andReturn(0)
+            ->andReturn($data['getOtherFinancesAmount'])
             ->once()
             ->shouldReceive('getSignatureType')
             ->andReturn(
                 m::mock()
-                ->shouldReceive('getId')
-                ->andReturn(RefData::SIG_PHYSICAL_SIGNATURE)
-                ->once()
-                ->getMock()
+                    ->shouldReceive('getId')
+                    ->andReturn(RefData::SIG_PHYSICAL_SIGNATURE)
+                    ->once()
+                    ->getMock()
             )
             ->twice()
             ->getMock();
+
+        return $continuationDetail;
+    }
+
+    private function mockDigitalSignature()
+    {
+        return m::mock()->shouldReceive('getSignatureName')->with()->once()->andReturn('NAME')
+            ->shouldReceive('getCreatedOn')->with()->once()->andReturn('DATE')
+            ->shouldReceive('getDateOfBirth')->with()->once()->andReturn('DOB')
+            ->getMock();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = Qry::create(['id'=> 123]);
+
+        $continuationDetail = $this->mockContinuationDetail();
 
         $continuationDetail->shouldReceive('getDigitalSignature')->with()->once()->andReturn(null);
 
@@ -172,68 +192,94 @@ class GetTest extends QueryHandlerTestCase
         );
     }
 
+    public function testHandleQueryNoTransaction()
+    {
+        $query = Qry::create(['id'=> 123]);
+
+        $continuationDetail = $this->mockContinuationDetail();
+
+        $continuationDetail->shouldReceive('getDigitalSignature')->with()->once()->andReturn(null);
+
+        $this->repoMap['ContinuationDetail']
+            ->shouldReceive('fetchUsingId')->with($query)->once()->andReturn($continuationDetail);
+
+        $this->repoMap['Document']
+            ->shouldReceive('fetchListForContinuationDetail')->with(123, Query::HYDRATE_ARRAY)->once()
+            ->andReturn(['document1', 'document2']);
+
+        $this->repoMap['SystemParameter']
+            ->shouldReceive('getDisableSelfServeCardPayments')
+            ->andReturn(false)
+            ->once();
+        $this->repoMap['SystemParameter']
+            ->shouldReceive('getDisableGdsVerifySignatures')
+            ->andReturn('DISABLE_SIGNATURES')
+            ->once();
+
+        $mockFee = m::mock(BundleSerializableInterface::class)
+            ->shouldReceive('serialize')
+            ->with(['feeType' => ['feeType'], 'licence'])
+            ->andReturn(['fee_entity'])
+            ->once()
+            ->getMock();
+
+        $mockContinuationFee = m::mock()
+            ->shouldReceive('getLatestTransaction')
+            ->andReturn(null)
+            ->getMock();
+
+        $this->repoMap['Fee']
+            ->shouldReceive('fetchOutstandingContinuationFeesByLicenceId')
+            ->with(1)
+            ->andReturn([$mockFee])
+            ->once()
+            ->shouldReceive('fetchLatestPaidContinuationFee')
+            ->with(1)
+            ->andReturn($mockContinuationFee)
+            ->once();
+
+        $this->mockedSmServices['FinancialStandingHelperService']
+            ->shouldReceive('getFinanceCalculationForOrganisation')->with(99)->once()->andReturn('123.99');
+
+        $this->mockedSmServices['ContinuationReview\Declaration']
+            ->shouldReceive('getDeclarationMarkup')->with($continuationDetail)->once()
+            ->andReturn('DECLARATIONS');
+
+        $this->assertEquals(
+            [
+                'licence_entity',
+                'financeRequired' => '123.99',
+                'disableCardPayments' => false,
+                'fees' => [
+                    ['fee_entity']
+                ],
+                'documents' => ['document1', 'document2'],
+                'organisationTypeId' => 'ORG_TYPE_ID',
+                'declarations' => 'DECLARATIONS',
+                'disableSignatures' => 'DISABLE_SIGNATURES',
+                'hasOutstandingContinuationFee' => true,
+                'signature' => [],
+                'reference' => null,
+                'isFinancialEvidenceRequired' => true,
+                'conditionsUndertakings' => ['foo'],
+                'isPhysicalSignature' => true,
+            ],
+            $this->sut->handleQuery($query)->serialize()
+        );
+    }
+
     public function testHandleQueryWithSignature()
     {
         $query = Qry::create(['id'=> 123]);
 
-        $continuationDetail = m::mock(BundleSerializableInterface::class);
-        $continuationDetail->shouldReceive('getLicence')
-            ->times(1)
-            ->andReturn(
-                m::mock()
-                    ->shouldReceive('getOrganisation')
-                    ->andReturn(
-                        m::mock()
-                            ->shouldReceive('getType')->with()->andReturn(
-                                m::mock()->shouldReceive('getId')->andReturn('ORG_TYPE_ID')->getMock()
-                            )->once()
-                        ->shouldReceive('getId')->with()->andReturn(99)->once()
-                        ->getMock()
-                    )
-                    ->twice()
-                    ->shouldReceive('getId')
-                    ->andReturn(1)
-                    ->twice()
-                    ->shouldReceive('getGroupedConditionsUndertakings')
-                    ->andReturn(['foo'])
-                    ->once()
-                    ->getMock()
-            )
-            ->shouldReceive('serialize')
-            ->with(['licence' => ['organisation', 'trafficArea', 'licenceType', 'goodsOrPsv']])
-            ->andReturn(['licence_entity'])
-            ->once()
-            ->shouldReceive('getId')
-            ->andReturn(123)
-            ->once()
-            ->shouldReceive('getAverageBalanceAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getFactoringAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getOverdraftAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getOtherFinancesAmount')
-            ->andReturn(10000)
-            ->once()
-            ->shouldReceive('getSignatureType')
-            ->andReturn(
-                m::mock()
-                    ->shouldReceive('getId')
-                    ->andReturn(RefData::SIG_PHYSICAL_SIGNATURE)
-                    ->once()
-                    ->getMock()
-            )
-            ->twice()
-            ->getMock();
+        $continuationDetail = $this->mockContinuationDetail(
+            [
+                'getOtherFinancesAmount' => 10000
+            ]
+        );
 
         $continuationDetail->shouldReceive('getDigitalSignature')->with()->times(4)->andReturn(
-            m::mock()->shouldReceive('getSignatureName')->with()->once()->andReturn('NAME')
-                ->shouldReceive('getCreatedOn')->with()->once()->andReturn('DATE')
-                ->shouldReceive('getDateOfBirth')->with()->once()->andReturn('DOB')
-                ->getMock()
+            $this->mockDigitalSignature()
         );
 
         $this->repoMap['ContinuationDetail']
@@ -320,64 +366,14 @@ class GetTest extends QueryHandlerTestCase
     {
         $query = Qry::create(['id'=> 123]);
 
-        $continuationDetail = m::mock(BundleSerializableInterface::class);
-        $continuationDetail->shouldReceive('getLicence')
-            ->times(1)
-            ->andReturn(
-                m::mock()
-                    ->shouldReceive('getOrganisation')
-                    ->andReturn(
-                        m::mock()
-                            ->shouldReceive('getType')->with()->andReturn(
-                                m::mock()->shouldReceive('getId')->andReturn('ORG_TYPE_ID')->getMock()
-                            )->once()
-                            ->shouldReceive('getId')->with()->andReturn(99)->once()
-                            ->getMock()
-                    )
-                    ->twice()
-                    ->shouldReceive('getId')
-                    ->andReturn(1)
-                    ->twice()
-                    ->shouldReceive('getGroupedConditionsUndertakings')
-                    ->andReturn(['foo'])
-                    ->once()
-                    ->getMock()
-            )
-            ->shouldReceive('serialize')
-            ->with(['licence' => ['organisation', 'trafficArea', 'licenceType', 'goodsOrPsv']])
-            ->andReturn(['licence_entity'])
-            ->once()
-            ->shouldReceive('getId')
-            ->andReturn(123)
-            ->once()
-            ->shouldReceive('getAverageBalanceAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getFactoringAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getOverdraftAmount')
-            ->andReturn(0)
-            ->once()
-            ->shouldReceive('getOtherFinancesAmount')
-            ->andReturn(10000)
-            ->once()
-            ->shouldReceive('getSignatureType')
-            ->andReturn(
-                m::mock()
-                    ->shouldReceive('getId')
-                    ->andReturn(RefData::SIG_PHYSICAL_SIGNATURE)
-                    ->once()
-                    ->getMock()
-            )
-            ->twice()
-            ->getMock();
+        $continuationDetail = $this->mockContinuationDetail(
+            [
+                'getOtherFinancesAmount' => 10000
+            ]
+        );
 
         $continuationDetail->shouldReceive('getDigitalSignature')->with()->times(4)->andReturn(
-            m::mock()->shouldReceive('getSignatureName')->with()->once()->andReturn('NAME')
-                ->shouldReceive('getCreatedOn')->with()->once()->andReturn('DATE')
-                ->shouldReceive('getDateOfBirth')->with()->once()->andReturn('DOB')
-                ->getMock()
+            $this->mockDigitalSignature()
         );
 
         $this->repoMap['ContinuationDetail']
