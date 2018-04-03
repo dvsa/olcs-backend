1a2dbab34cee5631de5a69656a939516bebae91f
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-04-03 17:31:41 +0100

    WIP ReadO only user retrieval

--- app/api/module/Api/src/Domain/QueryHandler/Tm/Documents.php
+++ app/api/module/Api/src/Domain/QueryHandler/Tm/Documents.php
@@ -5,6 +5,7 @@
  *
  * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
+
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Tm;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
@@ -22,23 +23,18 @@ class Documents extends AbstractQueryHandler
 
     public function handleQuery(QueryInterface $query)
     {
-        $currentUser = $this->getCurrentUser();
-        $roles = $currentUser->getRoles()->getValues();
 
-        /** @var \Dvsa\Olcs\Api\Entity\User\Role $role */
-        foreach ($roles as $role) {
-            if ($role->getRole() == \Dvsa\Olcs\Api\Entity\User\Role::ROLE_INTERNAL_LIMITED_READ_ONLY) {
-                return [
-                    'result' => null,
-                    'count' => 0
-                ];
-            }
+        if ($this->isReadOnlyInternalUser()) {
+            return [
+                'result' => null,
+                'count' => 0
+            ];
         }
 
         $documents = $this->getRepo()->fetchListForTm($query->getId());
         return [
             'result' => $this->resultList($documents),
-            'count'  => count($documents)
+            'count' => count($documents)
         ];
     }
 }
