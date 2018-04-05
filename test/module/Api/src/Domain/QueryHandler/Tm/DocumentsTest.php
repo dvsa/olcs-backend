681cd0d07395b9b88df87c440d595d8430a18c0d
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-04-05 11:48:50 +0100

    running review script and fixing tests

--- test/module/Api/src/Domain/QueryHandler/Tm/DocumentsTest.php
+++ test/module/Api/src/Domain/QueryHandler/Tm/DocumentsTest.php
@@ -7,12 +7,15 @@
  */
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Tm;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Tm\Documents as QueryHandler;
+use Dvsa\Olcs\Api\Entity\User\Role;
 use Dvsa\Olcs\Transfer\Query\Tm\Documents as Query;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Dvsa\Olcs\Api\Domain\Repository\Document as DocumentRepo;
 use Dvsa\Olcs\Api\Domain\QueryHandler\BundleSerializableInterface;
 use Mockery as m;
+use ZfcRbac\Service\AuthorizationService;
 
 /**
  * Documents Test
@@ -25,6 +28,8 @@ class DocumentsTest extends QueryHandlerTestCase
     {
         $this->sut = new QueryHandler();
         $this->mockRepo('Document', DocumentRepo::class);
+        $mockedAuth = m::mock(AuthorizationService::class)->makePartial();
+        $this->mockedSmServices[AuthorizationService::class] = $mockedAuth;
 
         parent::setUp();
     }
@@ -46,10 +51,38 @@ class DocumentsTest extends QueryHandlerTestCase
             ->andReturn([$mockDocument])
             ->getMock();
 
+        $mockedId = m::mock(IdentityInterface::class)->shouldReceive('getUser')->andReturn(
+            m::mock(User::class)->shouldReceive('getRoles')->andReturn(new ArrayCollection([]))->getMock()
+        )->getMock();
+
+        $this->mockedSmServices[AuthorizationService::class]->shouldReceive('getIdentity')->andReturn($mockedId);
+
         $this->assertSame(
             [
                 'result'    => ['foo'],
                 'count'     => 1,
+            ],
+            $this->sut->handleQuery($query)
+        );
+    }
+
+    public function testHandleQueryReadOnlyUser()
+    {
+        $query = Query::create(['id' => 1]);
+
+
+        $mockedRole = m::mock(Role::class);
+        $mockedRole->shouldReceive('getRole')->andReturn(Role::ROLE_INTERNAL_LIMITED_READ_ONLY);
+        $mockedId = m::mock(IdentityInterface::class)->shouldReceive('getUser')->andReturn(
+            m::mock(User::class)->shouldReceive('getRoles')->andReturn(new ArrayCollection([$mockedRole]))->getMock()
+        )->getMock();
+
+        $this->mockedSmServices[AuthorizationService::class]->shouldReceive('getIdentity')->andReturn($mockedId);
+
+        $this->assertSame(
+            [
+                'result'    => null,
+                'count'     => 0,
             ],
             $this->sut->handleQuery($query)
         );
