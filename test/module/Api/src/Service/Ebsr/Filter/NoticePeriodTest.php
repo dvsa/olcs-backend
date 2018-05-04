1da66b076f32244eab26c488188789a1e3262c8b
Parth <parth.vyas@dvsa.gov.uk>
2018-05-04 14:32:54 +0100

    Add NoticePeriod filter tests. OLCS-19509

---
+++ test/module/Api/src/Service/Ebsr/Filter/NoticePeriodTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Service\Ebsr\Filter;
+
+use Dvsa\Olcs\Api\Service\Ebsr\Filter\NoticePeriod;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class NoticePeriodTest
+ * @package Dvsa\OlcsTest\Api\Service\Ebsr\Filter
+ */
+class NoticePeriodTest extends TestCase
+{
+    /**
+     * @dataProvider provideFilter
+     * @param $data
+     * @param $expected
+     */
+    public function testFilter($data, $expected)
+    {
+        $sut = new NoticePeriod();
+        $result = $sut->filter($data);
+
+        $this->assertEquals($expected, $result['busNoticePeriod']);
+    }
+
+    public function provideFilter()
+    {
+        return [
+            [['trafficAreas' => ['English']], 2],
+            [['trafficAreas' => ['Scottish']], 1],
+            [['trafficAreas' => ['Welsh']], 3],
+            [['trafficAreas' => ['English', 'Welsh']], 3],
+            [['trafficAreas' => ['Welsh', 'English']], 3],
+            [['trafficAreas' => ['English', 'Scottish']], 1],
+            [['trafficAreas' => ['Scottish', 'English']], 1],
+            [['trafficAreas' => ['Welsh', 'Scottish']], 1],
+            [['trafficAreas' => ['Scottish', 'Welsh']], 1],
+        ];
+    }
+}
