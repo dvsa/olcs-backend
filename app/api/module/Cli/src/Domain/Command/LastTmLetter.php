4d1409f6d7c67af04f490348a2eda4a76f53a0e8
Igor Stimoli <igorstimoli@gmail.com>
2018-05-11 14:18:31 +0100

    OLCS-19481

---
+++ app/api/module/Cli/src/Domain/Command/LastTmLetter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dvsa\Olcs\Cli\Domain\Command;
+
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ * Export data to csv for data.gov.uk
+ *
+ * @author Dmitry Golubev <dmitrij.golubev@valtech.co.uk>
+ */
+final class LastTmLetter extends AbstractCommand
+{
+
+}
