27b9330b70f6835f0e2c16b9523df068f7e05fde
Igor Stimoli <igorstimoli@gmail.com>
2018-05-29 10:35:29 +0100

    more blank line removal

--- app/api/module/Api/src/Entity/Bus/AbstractBusReg.php
+++ app/api/module/Api/src/Entity/Bus/AbstractBusReg.php
@@ -3090,7 +3090,6 @@ abstract class AbstractBusReg implements BundleSerializableInterface, JsonSerial
     public function clearProperties($properties = array())
     {
         foreach ($properties as $property) {
-
             if (property_exists($this, $property)) {
                 if ($this->$property instanceof Collection) {
                     $this->$property = new ArrayCollection(array());
