d0c1eb9440628e72693ee22da3d3b3bcc0982fe0
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-05-30 15:55:29 +0100

    new query for sectors and api endpoint created

--- module/Api/config/module.config.php
+++ module/Api/config/module.config.php
@@ -332,6 +332,7 @@ return [
             'DataService' => RepositoryFactory::class,
             'DocumentToDelete' => RepositoryFactory::class,
             'Hearing' => RepositoryFactory::class,
+            'Sectors' => RepositoryFactory::class,
         ]
     ],
     'entity_namespaces' => include(__DIR__ . '/namespace.config.php'),
