f7e5f8fcdc2e4d106374a2e95f375147d0b00471
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-05-31 11:18:15 +0100

    Changing access permissions for Sectors query

--- module/Api/config/validation-map/permits.config.php
+++ module/Api/config/validation-map/permits.config.php
@@ -1,8 +1,8 @@
 <?php
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
-  QueryHandler\Permits\SectorsList::class => IsInternalUser::class,
+  QueryHandler\Permits\SectorsList::class => NoValidationRequired::class,
 ];
