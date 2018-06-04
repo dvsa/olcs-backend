995ab2a7f6755fe379c63b0d4214616d5fce30c0
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-06-04 16:26:57 +0100

    dashboard data read queries done

--- module/Api/src/Entity/AbstractPaymentStatus.php
+++ module/Api/src/Entity/AbstractPaymentStatus.php
@@ -19,7 +19,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @ORM\Table(name="payment_status",
  *    indexes={
  *        @ORM\Index(name="payment_status_created_by", columns={"created_by"}),
- *        @ORM\Index(name="ecmt_ecmt_payment_status_last_modified_by", columns={"last_modified_by"})
+ *        @ORM\Index(name="ecmt_payment_status_last_modified_by", columns={"last_modified_by"})
  *    }
  * )
  */
