70e1e9f644f99f2b5db7a15fb078993f1d64617d
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-06-05 11:27:14 +0100

    new commands added

--- module/Api/config/validation-map/permits.config.php
+++ module/Api/config/validation-map/permits.config.php
@@ -1,10 +1,13 @@
 <?php
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
   QueryHandler\Permits\SectorsList::class => NoValidationRequired::class,
   QueryHandler\Permits\ConstrainedCountries::class => NoValidationRequired::class,
   QueryHandler\Permits\EcmtPermits::class => NoValidationRequired::class,
+  CommandHandler\Permits\EcmtPermits::class => NoValidationRequired::class,
+  CommandHandler\Permits\EcmtPermitCountryLink::class => NoValidationRequired::class,
 ];
