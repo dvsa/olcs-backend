8edac7f7c5cc8418d32faa9d72792d972e1a9aa2
Igor Stimoli <igorstimoli@gmail.com>
2018-06-12 11:18:15 +0100

    OLCS-19389: Safety Inspector (workshop) contact details and address are now (soft)deleted on S.I. remove

---
+++ app/api/module/Api/src/Domain/CommandHandler/Traits/DeleteContactDetailsAndAddressTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\Traits;
+
+use Dvsa\Olcs\Api\Entity\ContactDetails\ContactDetails;
+
+trait DeleteContactDetailsAndAddressTrait
+{
+    private function injectRepos()
+    {
+        $requiredRepos = ['ContactDetails', 'Address'];
+        foreach ($requiredRepos as $requiredRepo) {
+            if (!in_array($requiredRepo, $this->extraRepos)) {
+                $this->extraRepos[] = $requiredRepo;
+            }
+        }
+    }
+
+    /** @var ContactDetails $contactDetails */
+    private function maybeDeleteContactDetailsAndAddress($contactDetails)
+    {
+        if ($contactDetails === null) {
+            return;
+        }
+        $this->injectRepos();
+        if ($contactDetails->getAddress() !== null) {
+            $addressRepo = $this->getRepo('Address');
+            $addressRepo->delete($contactDetails->getAddress());
+        }
+        $this->getRepo('ContactDetails')->delete($contactDetails);
+    }
+}
