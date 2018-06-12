50e2f906d1e179f841da867b3e09f4e62511f656
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-12 11:21:56 +0100

    olcs-20199 rename feature toggle validator config that wasn't being picked up

---
+++ module/Api/config/validation-map/feature-toggle.config.php
@@ -0,0 +1,13 @@
+<?php
+
+use Dvsa\Olcs\Api\Domain\QueryHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
+
+return [
+    CommandHandler\FeatureToggle\Create::class => IsInternalUser::class,
+    CommandHandler\FeatureToggle\Update::class => IsInternalUser::class,
+    CommandHandler\FeatureToggle\Delete::class => IsInternalUser::class,
+    QueryHandler\FeatureToggle\ById::class => IsInternalUser::class,
+    QueryHandler\FeatureToggle\GetList::class => IsInternalUser::class,
+];
