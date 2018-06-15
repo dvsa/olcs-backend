094674f3f1f85bfc0424d8807edfae80b63c1989
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-15 13:30:49 +0100

    olcs-20199 added query handler to check whether a toggle (or group of toggles) is enabled

--- module/Api/config/validation-map/feature-toggle.config.php
+++ module/Api/config/validation-map/feature-toggle.config.php
@@ -3,6 +3,7 @@
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
     CommandHandler\FeatureToggle\Create::class => IsInternalUser::class,
@@ -10,4 +11,5 @@ return [
     CommandHandler\FeatureToggle\Delete::class => IsInternalUser::class,
     QueryHandler\FeatureToggle\ById::class => IsInternalUser::class,
     QueryHandler\FeatureToggle\GetList::class => IsInternalUser::class,
+    QueryHandler\FeatureToggle\IsEnabled::class => NoValidationRequired::class,
 ];
