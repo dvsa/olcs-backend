691972783682db5f4d601133be6b3ce964fc2f3b
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-22 17:25:28 +0100

    olcs-20199 update togle service factory + test to use config from the DB

--- test/module/Api/src/Service/Toggle/ToggleServiceFactoryTest.php
+++ test/module/Api/src/Service/Toggle/ToggleServiceFactoryTest.php
@@ -2,6 +2,10 @@
 
 namespace Dvsa\OlcsTest\Api\Service\Toggle;
 
+use Dvsa\Olcs\Api\Domain\Query\FeatureToggle\FetchList;
+use Dvsa\Olcs\Api\Domain\RepositoryServiceManager;
+use Dvsa\Olcs\Api\Domain\Repository\FeatureToggle as FeatureToggleRepo;
+use Dvsa\Olcs\Api\Entity\System\FeatureToggle as FeatureToggleEntity;
 use Dvsa\Olcs\Api\Service\Toggle\ToggleService;
 use Mockery as m;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -15,20 +19,52 @@ class ToggleServiceFactoryTest extends m\Adapter\Phpunit\MockeryTestCase
 {
     public function testCreateService()
     {
-        $config = [
-            'feature_toggle' => [
-                'Test Config' => [
-                    'name' => 'test name',
-                    'conditions' => [],
-                    'status' => 'inactive'
-                ],
+        $configName1 = 'config name 1';
+        $configName2 = 'config name 2';
+
+        $dbConfig = [
+            0 => [
+                'friendlyName' => 'friendly name 1',
+                'configName' => $configName1,
+                'status' => [
+                    'id' => FeatureToggleEntity::ACTIVE_STATUS
+                ]
             ],
+            1 => [
+                'friendlyName' => 'friendly name 2',
+                'configName' => $configName2,
+                'status' => [
+                    'id' => FeatureToggleEntity::INACTIVE_STATUS
+                ]
+            ]
         ];
 
+        $featureToggleRepo = m::mock(FeatureToggleRepo::class);
+        $featureToggleRepo->shouldReceive('fetchList')
+            ->once()
+            ->with(m::type(FetchList::class))
+            ->andReturn($dbConfig);
+
+        $repoServiceManager = m::mock(RepositoryServiceManager::class);
+        $repoServiceManager->shouldReceive('get')
+            ->once()
+            ->with('FeatureToggle')
+            ->andReturn($featureToggleRepo);
+
         $mockSl = m::mock(ServiceLocatorInterface::class);
-        $mockSl->shouldReceive('get')->once()->with('Config')->andReturn($config);
+        $mockSl->shouldReceive('get')
+            ->once()
+            ->with('RepositoryServiceManager')
+            ->andReturn($repoServiceManager);
 
         $sut = new ToggleServiceFactory();
-        $this->assertInstanceOf(ToggleService::class, $sut->createService($mockSl));
+        $toggleService = $sut->createService($mockSl);
+
+        $this->assertInstanceOf(ToggleService::class, $toggleService);
+
+        //shouldn't do this, but it's the best way to check the config is transferred accurately
+        //from DB to toggle manager
+        $this->assertEquals(true, $toggleService->isEnabled($configName1));
+        $this->assertEquals(false, $toggleService->isEnabled($configName2));
     }
 }
