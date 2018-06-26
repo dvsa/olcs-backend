f427f8af5410f5e8b09e3f6f64963334c1fcaa55
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-06-26 16:47:28 +0100

    olcs-20199 feature toggle administration restricted to internal admins

--- module/Api/config/validation-map/feature-toggle.config.php
+++ module/Api/config/validation-map/feature-toggle.config.php
@@ -2,14 +2,14 @@
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalAdmin;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
-    CommandHandler\FeatureToggle\Create::class => IsInternalUser::class,
-    CommandHandler\FeatureToggle\Update::class => IsInternalUser::class,
-    CommandHandler\FeatureToggle\Delete::class => IsInternalUser::class,
-    QueryHandler\FeatureToggle\ById::class => IsInternalUser::class,
-    QueryHandler\FeatureToggle\GetList::class => IsInternalUser::class,
+    CommandHandler\FeatureToggle\Create::class => IsInternalAdmin::class,
+    CommandHandler\FeatureToggle\Update::class => IsInternalAdmin::class,
+    CommandHandler\FeatureToggle\Delete::class => IsInternalAdmin::class,
+    QueryHandler\FeatureToggle\ById::class => IsInternalAdmin::class,
+    QueryHandler\FeatureToggle\GetList::class => IsInternalAdmin::class,
     QueryHandler\FeatureToggle\IsEnabled::class => NoValidationRequired::class,
 ];
