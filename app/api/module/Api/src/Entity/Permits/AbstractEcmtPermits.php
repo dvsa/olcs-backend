00692048fbfdfdedbee579fd3135cc6836f7d7fe
vagrant <vagrant@olcs-v2.(none)>
2018-06-28 10:01:55 +0000

    modifications after etl changes

--- app/api/module/Api/src/Entity/Permits/AbstractEcmtPermits.php
+++ app/api/module/Api/src/Entity/Permits/AbstractEcmtPermits.php
@@ -778,7 +778,6 @@ abstract class AbstractEcmtPermits implements BundleSerializableInterface, JsonS
     public function clearProperties($properties = array())
     {
         foreach ($properties as $property) {
-
             if (property_exists($this, $property)) {
                 if ($this->$property instanceof Collection) {
                     $this->$property = new ArrayCollection(array());
