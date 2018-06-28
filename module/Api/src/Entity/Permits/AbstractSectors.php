6e997d4677f6c2fc982fa8e6ef913f5353d6d64b
vagrant <vagrant@olcs-v2.(none)>
2018-06-28 10:01:55 +0000

    modifications after etl changes

--- module/Api/src/Entity/Permits/AbstractSectors.php
+++ module/Api/src/Entity/Permits/AbstractSectors.php
@@ -524,7 +524,6 @@ abstract class AbstractSectors implements BundleSerializableInterface, JsonSeria
     public function clearProperties($properties = array())
     {
         foreach ($properties as $property) {
-
             if (property_exists($this, $property)) {
                 if ($this->$property instanceof Collection) {
                     $this->$property = new ArrayCollection(array());
