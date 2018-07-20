926070181e16e2cfa311b4de07ac675e90ba5c69
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-07-20 19:00:18 +0100

    WIP: UpdateEcmtPermitApplication command added

--- module/Api/src/Domain/CommandHandler/Permits/UpdateEcmtPermitApplication.php
+++ module/Api/src/Domain/CommandHandler/Permits/UpdateEcmtPermitApplication.php
@@ -5,7 +5,7 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\Permits;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
 
 /**
  * Update ECMT Permit Application
@@ -15,70 +15,79 @@ use Dvsa\Olcs\Api\Entity\Licence\Licence;
 final class UpdateEcmtPermitApplication extends AbstractCommandHandler implements TransactionedInterface
 {
     protected $repoServiceName = 'EcmtPermitApplication';
-    protected $extraRepos = ['Licence'];
+    //protected $extraRepos = ['Licence'];
 
 
     public function handleCommand(CommandInterface $command)
     {
         $result = new Result();
+
+        /* @var $ecmtPermitApplication EcmtPermitApplication */
         $ecmtPermitApplication = $this->getRepo()->fetchUsingId($command, Query::HYDRATE_OBJECT);
 
-        if ($command->getLicence())
+        if ($command->getLicence() !== null)
         {
-            $ecmtPermitApplication->setLicence($this->getRepo()->getReference(Licence::class, $command->getLicence()));
+            $ecmtPermitApplication->setLicence($this->getRepo('Licence')->fetchById($command->getLicence()));
         }
 
-        if ($command->getStatus())
+        if ($command->getStatus() !== null)
         {
             $ecmtPermitApplication->setStatus($this->getRepo()->getRefdataReference($command->getStatus()));
         }
 
-        if ($command->getPermitType())
+        if ($command->getPermitType() !== null)
         {
             $ecmtPermitApplication->setPermitType($this->getRepo()->getRefdataReference($command->getPermitType()));
         }
 
-        if ($command->getCabotage())
+        if ($command->getPaymentStatus() !== null)
         {
-            $ecmtPermitApplication->setCabotage($command->getCabotage());
+            $ecmtPermitApplication->setPaymentStatus($this->getRepo()->getRefdataReference($command->getPaymentStatus()));
         }
 
-        if ($command->getDeclaration())
-        {
-            $ecmtPermitApplication->setDeclaration($command->getDeclaration());
-        }
 
-        if ($command->getEmissions())
+        if ($command->getEmissions() !== null)
         {
             $ecmtPermitApplication->setEmissions($command->getEmissions());
         }
 
-        if ($command->getInternationalJourneys())
+        if ($command->getCabotage() !== null)
+        {
+            $ecmtPermitApplication->setCabotage($command->getCabotage());
+        }
+
+        if ($command->getDeclaration() !== null)
+        {
+            $ecmtPermitApplication->setDeclaration($command->getDeclaration());
+        }
+
+        if ($command->getInternationalJourneys() !== null)
         {
             $ecmtPermitApplication->setInternationalJourneys($command->getInternationalJourneys());
         }
 
-        if ($command->getNoOfPermits())
+        if ($command->getNoOfPermits() !== null)
         {
             $ecmtPermitApplication->setNoOfPermits($command->getNoOfPermits());
         }
 
-        if ($command->getPermitsRequired())
+        if ($command->getPermitsRequired() !== null)
         {
             $ecmtPermitApplication->setPermitsRequired($command->getPermitsRequired());
         }
 
-        if ($command->getSectors())
+        if ($command->getSectors() !== null)
         {
             $ecmtPermitApplication->setSectors($command->getSectors());
         }
 
-        if ($command->getTrips())
+        if ($command->getTrips() !== null)
         {
             $ecmtPermitApplication->setTrips($command->getTrips());
         }
 
         $this->getRepo()->save($ecmtPermitApplication);
+        $result->addId('ecmtPermitApplication', $ecmtPermitApplication->getId());
         $result->addMessage('ECMT Permit Application updated');
         return $result;
     }
