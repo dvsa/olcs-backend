d3e3e190fa09fd1ba1bbb4653b316d5487135d32
Andy Newton <andrew.newton@capgemini.com>
2018-07-27 17:57:20 +0100

    Command handler and test for Permits Required ECMT save

--- module/Api/config/command-map.config.php
+++ module/Api/config/command-map.config.php
@@ -1028,5 +1028,6 @@ return [
     TransferCommand\Permits\UpdateEcmtEmissions::class => CommandHandler\Permits\UpdateEcmtEmissions::class,
     TransferCommand\Permits\CancelEcmtPermitApplication::class => CommandHandler\Permits\CancelEcmtPermitApplication::class,
     TransferCommand\Permits\UpdateDeclaration::class => CommandHandler\Permits\UpdateDeclaration::class,
+    TransferCommand\Permits\UpdateEcmtPermitsRequired::class => CommandHandler\Permits\UpdateEcmtPermitsRequired::class,
 
 ];
