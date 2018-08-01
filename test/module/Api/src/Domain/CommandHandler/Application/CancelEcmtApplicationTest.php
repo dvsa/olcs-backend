66d6e29b7525bfc714b3c4f2dd4efe80d4312058
Scott Callaway <github@scottcallaway.co.uk>
2018-08-01 17:51:24 +0100

    Add Test for CancelEcmtApplication Command Handler

---
+++ test/module/Api/src/Domain/CommandHandler/Application/CancelEcmtApplicationTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 26/07/2018
+ * Time: 12:02
+ */
+
+/**
+ * Update ECMT EURO6 Emissions Test
+ *
+ * @author ONE
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Permits;
+
+use Dvsa\Olcs\Api\Domain\CommandHandler\Permits\CancelEcmtPermitApplication;
+
+use Dvsa\Olcs\Api\Domain\Repository;
+
+use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
+use Dvsa\Olcs\Transfer\Command\Permits\CancelEcmtPermitApplication as Cmd;
+
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Doctrine\ORM\Query;
+use Mockery as m;
+
+class CancelEcmtApplicationTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new CancelEcmtPermitApplication();
+        $this->mockRepo('EcmtPermitApplication', Repository\EcmtPermitApplication::class);
+
+        parent::setUp();
+    }
+
+    protected function initReferences()
+    {
+        $this->refData = [
+          EcmtPermitApplication::STATUS_CANCELLED
+        ];
+
+        parent::initReferences();
+    }
+
+    public function testHandleCommand()
+    {
+        $applicationId = 1;
+        $command = Cmd::create(['id' => $applicationId]);
+
+        $application = m::mock(EcmtPermitApplication::class);
+        $application->shouldReceive('getId')->withNoArgs()->once()->andReturn(1);
+
+        $application->shouldReceive('setStatus')->with($this->refData[EcmtPermitApplication::STATUS_CANCELLED])->once();
+
+        $this->repoMap['EcmtPermitApplication']->shouldReceive('fetchById')
+            ->with($applicationId)
+            ->andReturn($application)
+            ->shouldReceive('save')
+            ->once()
+            ->with($application);
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [
+              'ecmtPermitApplication' => $applicationId
+            ],
+            'messages' => []
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+}
