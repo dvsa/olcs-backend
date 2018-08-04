71e5b0feab247b7b3ed49d47fb9b90e48b1d9d8b
Andy Newton <andrew.newton@capgemini.com>
2018-08-04 23:11:42 +0100

    OLCS-21073 - Fix to issue of licence change creating new ECMT Application rows in the DB

---
+++ test/module/Api/src/Domain/CommandHandler/Application/UpdateEcmtLicenceTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 26/07/2018
+ * Time: 12:02
+ */
+
+/**
+ * Update ECMT Licence Test
+ *
+ * @author ONE
+ */
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Permits;
+
+use Dvsa\Olcs\Api\Domain\CommandHandler\Permits\UpdateEcmtLicence;
+use Dvsa\Olcs\Api\Domain\Repository;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
+use Dvsa\Olcs\Transfer\Command\Permits\UpdateEcmtLicence as Cmd;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Doctrine\ORM\Query;
+use Mockery as m;
+
+class UpdateEcmtLicenceTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new UpdateEcmtLicence();
+        $this->mockRepo('EcmtPermitApplication', Repository\EcmtPermitApplication::class);
+        $this->mockRepo('Licence', Repository\Licence::class);
+        parent::setUp();
+    }
+
+    public function testHandleCommand()
+    {
+        $data = [
+            'id' => 5,
+            'licence' => 7
+        ];
+
+        $command = Cmd::create($data);
+        $licence = m::mock(Licence::class);
+        $application = m::mock(EcmtPermitApplication::class);
+        $application->shouldReceive('setLicence')->with($licence)->once();
+        $application->shouldReceive('getId')->withNoArgs()->once()->andReturn(5);
+
+
+        $this->repoMap['EcmtPermitApplication']->shouldReceive('fetchById')
+            ->with(5)
+            ->andReturn($application);
+
+        $this->repoMap['Licence']->shouldReceive('fetchById')
+            ->with(7)
+            ->andReturn($licence);
+
+
+        $this->repoMap['EcmtPermitApplication']->shouldReceive('save')
+            ->once()
+            ->with($application);
+
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => ['ecmtPermitApplication'=> 5],
+            'messages' => [
+                'EcmtPermitApplication Licence Updated successfully'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+}
