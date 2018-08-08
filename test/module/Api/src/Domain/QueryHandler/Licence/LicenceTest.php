26fabda608058cb33d17813571ea779666ff39ba
Igor Stimoli <igor.stimoli@bjss.com>
2018-08-08 14:23:24 +0100

    OLCS-20650: Licence is now not accessible by external user if status != valid, suspended, curtailed

--- test/module/Api/src/Domain/QueryHandler/Licence/LicenceTest.php
+++ test/module/Api/src/Domain/QueryHandler/Licence/LicenceTest.php
@@ -2,6 +2,7 @@
 
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Licence;
 
+use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
 use Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail;
 use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
@@ -66,7 +67,9 @@ class LicenceTest extends QueryHandlerTestCase
             )
             ->shouldReceive('isSpecialRestricted')
             ->andReturn(false)
-            ->once();
+            ->once()
+            ->shouldReceive('getStatus->getId')
+            ->andReturn(LicenceEntity::LICENCE_STATUS_VALID);
 
         $mockContinuationDetail = m::mock(\Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail::class)
             ->shouldReceive('serialize')->with(['continuation', 'licence'])->once()->andReturn(['CD'])
@@ -127,7 +130,9 @@ class LicenceTest extends QueryHandlerTestCase
             )
             ->shouldReceive('isSpecialRestricted')
             ->andReturn(true)
-            ->once();
+            ->once()
+            ->shouldReceive('getStatus->getId')
+            ->andReturn(LicenceEntity::LICENCE_STATUS_VALID);
 
         $this->repoMap['ContinuationDetail']->shouldReceive('fetchForLicence')->with(111)
             ->andReturn([]);
@@ -190,7 +195,9 @@ class LicenceTest extends QueryHandlerTestCase
                     ->getMock()
             )
             ->shouldReceive('isSpecialRestricted')->andReturn(true)->once()
-            ->shouldReceive('isExpiring')->with()->once()->andReturn($isExpiring);
+            ->shouldReceive('isExpiring')->with()->once()->andReturn($isExpiring)
+            ->shouldReceive('getStatus->getId')
+            ->andReturn(LicenceEntity::LICENCE_STATUS_VALID);
 
         $this->repoMap['ContinuationDetail']->shouldReceive('fetchForLicence')->with(111)
             ->andReturn([$continuationDetail]);
@@ -241,7 +248,9 @@ class LicenceTest extends QueryHandlerTestCase
                     ->andReturn(true)
                     ->getMock()
             )
-            ->shouldReceive('isSpecialRestricted')->andReturn(true)->once();
+            ->shouldReceive('isSpecialRestricted')->andReturn(true)->once()
+            ->shouldReceive('getStatus->getId')
+            ->andReturn(LicenceEntity::LICENCE_STATUS_VALID);
 
         $this->repoMap['ContinuationDetail']->shouldReceive('fetchForLicence')->with(111)
             ->andReturn([]);
@@ -259,5 +268,121 @@ class LicenceTest extends QueryHandlerTestCase
         $expected = ['showExpiryWarning' => false];
 
         $this->assertArraySubset($expected, $result->serialize());
+    }
+
+    private function switchToExternalUser()
+    {
+        $this->sut = new Licence();
+        $this->mockRepo('Licence', Repository\Licence::class);
+        $this->mockRepo('ContinuationDetail', Repository\ContinuationDetail::class);
+        $this->mockRepo('Note', Repository\Note::class);
+        $this->mockRepo('SystemParameter', Repository\SystemParameter::class);
+
+        /** @var UserEntity $currentUser */
+        $currentUser = m::mock(UserEntity::class)->makePartial();
+        $currentUser->shouldReceive('isAnonymous')->andReturn(false);
+
+        $this->mockedSmServices = [
+            'SectionAccessService' => m::mock(),
+            AuthorizationService::class => m::mock(AuthorizationService::class)
+                ->shouldReceive('isGranted')
+                ->with(\Dvsa\Olcs\Api\Entity\User\Permission::SELFSERVE_USER, null)
+                ->andReturn(true)
+                ->shouldReceive('isGranted')
+                ->with(\Dvsa\Olcs\Api\Entity\User\Permission::INTERNAL_USER, null)
+                ->andReturn(false)
+                ->getMock(),
+        ];
+
+        $this->mockedSmServices[AuthorizationService::class]->shouldReceive('getIdentity->getUser')
+            ->andReturn($currentUser);
+
+        parent::setUp();
+    }
+
+    public function testHandleQueryExternalUser()
+    {
+        $this->switchToExternalUser();
+        $query = Qry::create(['id' => 111]);
+
+        /** @var LicenceEntity $licence */
+        $licence = m::mock(LicenceEntity::class)->makePartial();
+        $licence->setId(111);
+
+        $licence->shouldReceive('serialize')
+            ->andReturn(['foo' => 'bar'])
+            ->shouldReceive('getNiFlag')
+            ->andReturn('N')
+            ->once()
+            ->getMock()
+            ->shouldReceive('getOrganisation')->andReturn(
+                m::mock(Organisation::class)->shouldReceive('isMlh')->once()
+                    ->andReturn(true)
+                    ->getMock()
+            )
+            ->shouldReceive('isSpecialRestricted')
+            ->andReturn(false)
+            ->once()
+            ->shouldReceive('getStatus->getId')
+            ->andReturn(LicenceEntity::LICENCE_STATUS_VALID);
+
+        $mockContinuationDetail = m::mock(\Dvsa\Olcs\Api\Entity\Licence\ContinuationDetail::class)
+            ->shouldReceive('serialize')->with(['continuation', 'licence'])->once()->andReturn(['CD'])
+            ->getMock();
+        $this->repoMap['ContinuationDetail']->shouldReceive('fetchForLicence')->with(111)
+            ->andReturn([$mockContinuationDetail]);
+        $this->repoMap['Note']
+            ->shouldReceive('fetchForOverview')
+            ->with(111)
+            ->once()
+            ->andReturn('latest note');
+
+        $this->repoMap['Licence']->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn($licence);
+
+        $sections = ['bar', 'cake'];
+
+        $this->mockedSmServices['SectionAccessService']->shouldReceive('getAccessibleSectionsForLicence')
+            ->once()
+            ->with($licence)
+            ->andReturn($sections);
+
+        $result = $this->sut->handleQuery($query);
+
+        $expected = [
+            'foo' => 'bar',
+            'sections' => ['bar', 'cake'],
+            'niFlag' => 'N',
+            'isMlh' => true,
+            'continuationMarker' => ['CD'],
+            'latestNote' => 'latest note',
+            'canHaveInspectionRequest' => true,
+            'showExpiryWarning' => false,
+        ];
+
+        $this->assertEquals($expected, $result->serialize());
+    }
+
+    public function testHandleQueryExternalUserForbidden()
+    {
+        $this->switchToExternalUser();
+        $this->expectException(ForbiddenException::class);
+        $query = Qry::create(['id' => 111]);
+
+        /** @var LicenceEntity $licence */
+        $licence = m::mock(LicenceEntity::class)->makePartial();
+        $licence->setId(111);
+
+        $licence
+            ->shouldReceive('getStatus->getId')
+            ->once()
+            ->andReturn(LicenceEntity::LICENCE_STATUS_REVOKED);
+
+        $this->repoMap['Licence']->shouldReceive('fetchUsingId')
+            ->with($query)
+            ->andReturn($licence);
+
+        $this->sut->handleQuery($query);
     }
 }
