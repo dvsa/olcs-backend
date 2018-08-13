85da87ef0e7559b14d688a9203595c063eb899e1
Igor Stimoli <igor.stimoli@bjss.com>
2018-08-13 10:46:14 +0100

    OLCS-20882: CS fixes

--- app/api/module/Cli/src/Service/Queue/Consumer/Ebsr/EbsrSubmissionEntity.php
+++ app/api/module/Cli/src/Service/Queue/Consumer/Ebsr/EbsrSubmissionEntity.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: igor.stimoli
- * Date: 09/08/2018
- * Time: 13:55
- */
 
 namespace Dvsa\Olcs\Cli\Service\Queue\Consumer\Ebsr;
-
 
 class EbsrSubmissionEntity
 {
