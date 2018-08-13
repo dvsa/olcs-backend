5e739dc351b09725c6aa59f66ff1018b85d15a53
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-08-13 11:12:15 +0100

    correcting case

---
+++ app/api/module/Cli/src/Domain/Query/Util/GetDbValue.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Dvsa\Olcs\Cli\Domain\Query\Util;
+
+use Dvsa\Olcs\Transfer\Query\AbstractQuery;
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+
+/**
+ * Class GetDbValue
+ *
+ * @package Dvsa\Olcs\Cli\Domain\Query\Util
+ */
+class GetDbValue extends AbstractQuery
+{
+    /**
+     * @var string
+     */
+    protected $columnName;
+
+    /**
+     * @var string
+     */
+    protected $tableName;
+
+    /**
+     * @var string
+     */
+    protected $filterName;
+
+    /**
+     * @var mixed
+     */
+    protected $filterValue;
+
+    /**
+     * @return string
+     */
+    public function getColumnName(): string
+    {
+        return $this->columnName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilterName(): string
+    {
+        return $this->filterName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFilterValue()
+    {
+        return $this->filterValue;
+    }
+
+}
