2306ec42ecf671353d3aebe13877e639cd077cf0
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-08-13 11:06:38 +0100

    simple entity validation - unit tests to follow

--- module/Cli/src/Domain/QueryHandler/Util/getDbValue.php
+++ module/Cli/src/Domain/QueryHandler/Util/getDbValue.php
@@ -3,42 +3,44 @@
 namespace Dvsa\Olcs\Cli\Domain\QueryHandler\Util;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 class getDbValue extends AbstractQueryHandler
 {
+
+    private $fqdn = 'Dvsa\\Olcs\\Api\Entity\\';
+
+    protected $repoServiceName;
+
     /**
      * Handle query
      *
      * @param \Dvsa\Olcs\Cli\Domain\Query\Util\getDbValue $query query
      *
-     * @return array
+     * @return Result
      * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
      */
-    private $fqdn = 'Dvsa\\Olcs\\Api\Entity\\';
-
     public function handleQuery(QueryInterface $query)
     {
+        $result = new Result();
 
-        protected $repoServiceName = $query->getTableName();
-        if($this->isValidEntity() && $this->isValidProperty($query->))
+        $this->repoServiceName = $query->getTableName();
+        if ($this->isValidEntity() && $this->isValidProperty($query->getColumnName())) {
+            return $result;
+        }
     }
 
 
-    private function isValidEntity() : bool
+    private function isValidEntity(): bool
     {
-        return class_exists($this->fqdn .$this->repoServiceName);
+        return class_exists($this->fqdn . $this->repoServiceName);
     }
 
-    private function isValidProperty(string $property) : bool
+    private function isValidProperty(string $property): bool
     {
-        $className = $this->fqdn.filter_var($this->repoServiceName,FILTER_SANITIZE_STRING);
-
+        $className = $this->fqdn . filter_var($this->repoServiceName, FILTER_SANITIZE_STRING);
         $entity = new $className();
         return property_exists($entity, $property);
     }
-
-
-
-
 }
