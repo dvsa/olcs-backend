dd03d639f8ee7d5161875b7a1bc7c1c8d2db933e
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-08-13 14:56:26 +0100

    working creating entity - refining for table name and fixing up a test

--- module/Cli/src/Domain/QueryHandler/Util/GetDbValue.php
+++ module/Cli/src/Domain/QueryHandler/Util/GetDbValue.php
@@ -2,14 +2,19 @@
 
 namespace Dvsa\Olcs\Cli\Domain\QueryHandler\Util;
 
+use Dvsa\Olcs\Api\Domain\Exception\RuntimeException;
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use RegexIterator;
 
 class getDbValue extends AbstractQueryHandler
 {
 
-    private $fqdn = 'Dvsa\\Olcs\\Api\Entity\\';
+    private $entity = null;
 
     protected $repoServiceName;
 
@@ -26,7 +31,7 @@ class getDbValue extends AbstractQueryHandler
         $result = new Result();
 
         $this->repoServiceName = $query->getTableName();
-        if ($this->isValidEntity() && $this->isValidProperty($query->getColumnName())) {
+        if ($this->isValidEntity() && $this->isValidProperty($query, $query->getColumnName())) {
             return $result;
         }
     }
@@ -34,13 +39,63 @@ class getDbValue extends AbstractQueryHandler
 
     private function isValidEntity(): bool
     {
-        return class_exists($this->fqdn . $this->repoServiceName);
+        $this->entity = $this->getEntityFromName($this->repoServiceName);
+        return !empty($this->entity);
     }
 
-    private function isValidProperty(string $property): bool
+    private function isValidProperty(QueryInterface $query, $property): bool
     {
-        $className = $this->fqdn . filter_var($this->repoServiceName, FILTER_SANITIZE_STRING);
-        $entity = new $className();
-        return property_exists($entity, $property);
+
+        try {
+            $entity = $this->getRepo($this->repoServiceName)->fetchList()[0];
+            return property_exists($entity, $property);
+        } catch (RuntimeException $runtimeException) {
+            throw $runtimeException;
+        }
+    }
+
+    protected function getEntityFromName(string $tableName)
+    {
+        $fqdn = [];
+        $Directory = new RecursiveDirectoryIterator(__DIR__ . '/../../../../../Api/src/Entity');
+        $Iterator = new RecursiveIteratorIterator($Directory);
+        $regex = '/^(.+\/' . preg_quote($this->repoServiceName) . '\.php)$/m';
+        $matches = new RegexIterator($Iterator, $regex);
+
+        foreach ($matches as $file) {
+            $fileContents = file_get_contents($file);
+            $fqdn [] = $this->getFqdn($fileContents) . '\\' . filter_var(
+                    $this->repoServiceName,
+                    FILTER_SANITIZE_STRING
+                );
+        }
+
+        if (count($fqdn) <= 1 && class_exists($fqdn[0])) {
+            return new $fqdn[0];
+        }
+    }
+
+    protected function getFqdn(string $contents)
+    {
+        $namespace = '';
+        $isNamespace = false;
+        //Go through each token and evaluate it as necessary
+        foreach (token_get_all($contents) as $token) {
+            if (is_array($token) && $token[0] == T_NAMESPACE) {
+                $isNamespace = true;
+            }
+            if ($isNamespace) {
+                if (is_array($token) && in_array($token[0], [T_STRING, T_NS_SEPARATOR])) {
+                    $namespace .= $token[1];
+                } else {
+                    if ($token === ';') {
+                        break;
+                    }
+                }
+            }
+        }
+        return $namespace;
     }
 }
+
+
