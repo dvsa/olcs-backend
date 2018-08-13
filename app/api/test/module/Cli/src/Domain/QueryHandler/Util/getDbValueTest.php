a0e85b9a620a9ad32f171582787f2938a7395a5d
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-08-13 11:11:44 +0100

    correcting case

---
+++ app/api/test/module/Cli/src/Domain/QueryHandler/Util/getDbValueTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\OlcsTest\Cli\Domain\QueryHandler\
+use Dvsa\Olcs\Cli\Domain\QueryHandler\Util\getDbValue;
+
+class GetDbValueTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testHandleQuery()
+    {
+
+    }
+}
