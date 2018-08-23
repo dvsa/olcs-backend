99711509b242c804b0106e94ad11899481d802e1
Jonathan Thomas <jonathan@opalise.co.uk>
2018-08-23 16:17:27 +0100

    OLCS-21288 review query handlers and refactor

--- module/Api/src/Domain/QueryHandler/Permits/EcmtCountriesList.php
+++ module/Api/src/Domain/QueryHandler/Permits/EcmtCountriesList.php
@@ -2,53 +2,21 @@
 
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Permits;
 
-use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
-use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Doctrine\ORM\Query;
-use Dvsa\Olcs\Transfer\Query\Permits\EcmtCountriesList as ListDto;
 
 /**
  * Get a list of ECMT Countries
  *
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
-class EcmtCountriesList extends AbstractQueryHandler implements ToggleRequiredInterface
+class EcmtCountriesList extends AbstractListQueryHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
     protected $toggleConfig = [FeatureToggle::BACKEND_ECMT];
+
     protected $repoServiceName = 'Country';
-
-    /**
-     * @var ListDto
-     */
-    private $listDto;
-
-    public function handleQuery(QueryInterface $query)
-    {
-        $repo = $this->getRepo();
-        $data = $query->getArrayCopy();
-        $this->listDto = ListDto::create($data);
-        $results = $repo->fetchList($this->listDto, Query::HYDRATE_OBJECT);
-
-        return [
-          'results' => $this->resultList(
-              $results
-          ),
-          'count' => $repo->fetchCount($this->listDto)
-        ];
-    }
-
-    /**
-     * Gets the list Dto that was used (gets round problem with UT comparing objects)
-     *
-     * @return ListDto
-     */
-    public function getListDto()
-    {
-        return $this->listDto;
-    }
 }
--- module/Api/src/Domain/QueryHandler/Permits/EcmtPermitApplication.php
+++ module/Api/src/Domain/QueryHandler/Permits/EcmtPermitApplication.php
@@ -2,37 +2,23 @@
 
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Permits;
 
-use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
-use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Doctrine\ORM\Query;
 
 /**
  * Get a list of ECMT Permit Applications
  *
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
-class EcmtPermitApplication extends AbstractQueryHandler implements ToggleRequiredInterface
+class EcmtPermitApplication extends AbstractListQueryHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
     protected $toggleConfig = [FeatureToggle::BACKEND_ECMT];
+
     protected $repoServiceName = 'EcmtPermitApplication';
 
-    public function handleQuery(QueryInterface $query)
-    {
-
-        $repo = $this->getRepo();
-        $count = $repo->fetchCount($query);
-
-        return [
-          'result' => $this->resultList(
-              $repo->fetchList($query, Query::HYDRATE_OBJECT),
-              ['licence']
-          ),
-          'count' => $count,
-        ];
-    }
+    protected $bundle = ['licence'];
 }
