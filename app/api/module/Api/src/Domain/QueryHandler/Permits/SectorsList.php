2d18822494436db4c4b289696ced42afc136e3d2
Jonathan Thomas <jonathan@opalise.co.uk>
2018-08-23 16:17:27 +0100

    OLCS-21288 review query handlers and refactor

--- app/api/module/Api/src/Domain/QueryHandler/Permits/SectorsList.php
+++ app/api/module/Api/src/Domain/QueryHandler/Permits/SectorsList.php
@@ -2,35 +2,20 @@
 
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Permits;
 
-use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
-use Dvsa\Olcs\Transfer\Query\QueryInterface;
-use Doctrine\ORM\Query;
 
 /**
  * Get a list of Sectors
  *
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
-class SectorsList extends AbstractQueryHandler implements ToggleRequiredInterface
+class SectorsList extends AbstractListQueryHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
     protected $toggleConfig = [FeatureToggle::BACKEND_ECMT];
     protected $repoServiceName = 'Sectors';
-
-    public function handleQuery(QueryInterface $query)
-    {
-
-        $repo = $this->getRepo();
-        $count = $repo->fetchCount($query);
-        return [
-          'result' => $this->resultList(
-              $repo->fetchList($query, Query::HYDRATE_OBJECT)
-          ),
-          'count' => $count,
-        ];
-    }
 }
