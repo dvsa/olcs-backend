8ed567ae41dd755c199c03836cf077bf53e3cfa5
Igor Stimoli <igor.stimoli@bjss.com>
2018-08-24 14:54:49 +0200

    OLCS-21201: GetDbValue unit tests

--- test/module/Cli/src/Domain/QueryHandler/Util/GetDbValueTest.php
+++ test/module/Cli/src/Domain/QueryHandler/Util/GetDbValueTest.php
@@ -5,6 +5,7 @@ namespace Dvsa\OlcsTest\Cli\Domain\QueryHandler\GetDbValue;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
 use Dvsa\Olcs\Api\Entity\System\RefData;
+use Dvsa\Olcs\Cli\Domain\Exception;
 use Dvsa\Olcs\Cli\Domain\QueryHandler\Util\GetDbValue;
 use Dvsa\Olcs\Cli\Domain\Query\Util\GetDbValue as Qry;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
@@ -61,5 +62,55 @@ class GetDbValueTest extends QueryHandlerTestCase
             ->andReturn($application);
 
         $this->assertInstanceOf(Result::class, $this->sut->handleQuery($query));
+    }
+
+    public function testHandleQueryMissingParameters()
+    {
+        $parameters = [];
+        $query = Qry::create($parameters);
+        $this->expectException(Exception\MissingParameterException::class);
+        $this->sut->handleQuery($query);
+    }
+
+    public function testHandleQueryInvalidEntity()
+    {
+        $parameters = [
+            'entityName' => 'SomethingThatDoesNotExist',
+            'propertyName' => 'status',
+            'filterProperty' => 'id',
+            'filterValue' => '1'
+        ];
+
+        $query = Qry::create($parameters);
+        $this->expectException(Exception\InvalidEntityException::class);
+        $this->sut->handleQuery($query);
+    }
+
+    public function testHandleQueryInvalidPropertyName()
+    {
+        $parameters = [
+            'entityName' => 'Application\Application',
+            'propertyName' => 'SomethingThatDoesNotExist',
+            'filterProperty' => 'id',
+            'filterValue' => '1'
+        ];
+
+        $query = Qry::create($parameters);
+        $this->expectException(Exception\InvalidPropertyException::class);
+        $this->sut->handleQuery($query);
+    }
+
+    public function testHandleQueryInvalidFilterProperty()
+    {
+        $parameters = [
+            'entityName' => 'Application\Application',
+            'propertyName' => 'status',
+            'filterProperty' => 'SomethingThatDoesNotExist',
+            'filterValue' => '1'
+        ];
+
+        $query = Qry::create($parameters);
+        $this->expectException(Exception\InvalidPropertyException::class);
+        $this->sut->handleQuery($query);
     }
 }
