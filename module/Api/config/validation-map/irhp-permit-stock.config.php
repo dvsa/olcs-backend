0c0a895d4c09e7dceccca10ab563c2d9a582d418
Scott Callaway <github@scottcallaway.co.uk>
2018-09-05 10:35:29 +0100

    OLCS-21035 - Applying correct validation

--- module/Api/config/validation-map/irhp-permit-stock.config.php
+++ module/Api/config/validation-map/irhp-permit-stock.config.php
@@ -2,8 +2,7 @@
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalAdmin;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
-    QueryHandler\IrhpPermitStock\GetList::class => NoValidationRequired::class,
+    QueryHandler\IrhpPermitStock\GetList::class => IsInternalAdmin::class,
 ];
