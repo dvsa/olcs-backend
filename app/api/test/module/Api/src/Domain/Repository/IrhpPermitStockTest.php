bb0a900a7f0d95b8d3bd8cacf442ca1e9593e92f
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-09-19 13:20:04 +0100

    review comment refactor /OLCS-19274

---
+++ app/api/test/module/Api/src/Domain/Repository/IrhpPermitStockTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * LegacyOffence Repo test
+ *
+ * @author Shaun Lizzio <shaun@lizzio.co.uk>
+ */
+namespace Dvsa\OlcsTest\Api\Domain\Repository;
+
+use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+use Dvsa\Olcs\Api\Entity\Legacy\LegacyOffence;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpPermitStock as Repo;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * IrhpPermitStock Repo test
+ *
+ */
+class IrhpPermitStockTest extends RepositoryTestCase
+{
+    public function setUp()
+    {
+        $this->setUpSut(Repo::class);
+    }
+
+    public function getNextIrhpPermitStockByPermitType()
+    {
+
+    }
+}
