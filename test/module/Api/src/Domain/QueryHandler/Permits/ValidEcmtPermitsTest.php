93abd05ff531c3dc563bf4fb829b466aa345b2e5
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-09-21 12:31:06 +0100

    olcs-21385 switch to reusable unit test for query handler

--- test/module/Api/src/Domain/QueryHandler/Permits/ValidEcmtPermitsTest.php
+++ test/module/Api/src/Domain/QueryHandler/Permits/ValidEcmtPermitsTest.php
@@ -2,50 +2,18 @@
 
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Permits;
 
-use Dvsa\Olcs\Api\Domain\QueryHandler\Permits\ValidEcmtPermits as QueryHandler;
-use Dvsa\Olcs\Api\Entity\Permits\IrhpPermit;
-use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Permits\ValidEcmtPermits as Handler;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpPermit as Repo;
 use Dvsa\Olcs\Transfer\Query\Permits\ValidEcmtPermits as Query;
-use Mockery as m;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\AbstractListQueryHandlerTest;
 
 /**
  * ValidEcmtPermits Test
- *
- * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
-class ValidEcmtPermitsTest extends QueryHandlerTestCase
+class ValidEcmtPermitsTest extends AbstractListQueryHandlerTest
 {
-    public function setUp()
-    {
-        $this->sut = new QueryHandler();
-        $this->mockRepo('IrhpPermit', Repo::class);
-
-        parent::setUp();
-    }
-
-    public function testHandleQuery()
-    {
-        $data = ['id' => 1, 'page' => 1, 'limit' => 10];
-        $query = Query::create($data);
-
-        $irhpPermit = m::mock(IrhpPermit::class)->makePartial();
-        $irhpPermit->shouldReceive('serialize')
-            ->andReturn([]);
-
-        $this->repoMap['IrhpPermit']->shouldReceive('fetchByEcmtApplicationPaginated')
-            ->with($query)
-            ->andReturn($irhpPermit)
-            ->shouldReceive('fetchCount')
-            ->with($query)
-            ->andReturn('COUNT');
-
-        $result = $this->sut->handleQuery($query);
-
-        $expected = [
-            'results' => [],
-            'count' => 'COUNT'
-        ];
-
-        $this->assertEquals($expected, $result);
-    }
+    protected $sutClass = Handler::class;
+    protected $sutRepo = 'IrhpPermit';
+    protected $qryClass = Query::class;
+    protected $repoClass = Repo::class;
 }
