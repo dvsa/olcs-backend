88f27cec7b9edf706bcbbe3aaecd7f2b0ce37b91
wilkinsonc <chris.wilkinson@capgemini.com>
2018-09-22 13:53:50 +0100

    OLCS-19273 Changes to address MR feedback

--- module/Api/src/Domain/Repository/IrhpPermitStock.php
+++ module/Api/src/Domain/Repository/IrhpPermitStock.php
@@ -17,6 +17,11 @@ class IrhpPermitStock extends AbstractRepository
      * (after the stock for the given date has expired)
      *
      * Filtered for a given permit type
+     *
+     * @param string $permitType
+     * @param DateTime $date
+     *
+     * @return array
      */
     public function getNextIrhpPermitStockByPermitType($permitType, $date)
     {
@@ -24,7 +29,6 @@ class IrhpPermitStock extends AbstractRepository
                 ->select('ips')
                 ->from(Entity::class, 'ips')
                 ->innerJoin('ips.irhpPermitType', 'ipt')
-                ->where('ips.validTo >= ?1') //stock is valid
                 ->where('ips.validFrom >= ?1') //stock starts in future
                 ->andWhere('ipt.name = ?2') //Permit Type ECMT
                 ->orderBy('ips.validTo', 'ASC')
