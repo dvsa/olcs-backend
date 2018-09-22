68faf2b64edf067ecc2fcfd1a1e9d4b664fba0f1
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-09-22 16:15:29 +0100

    rebase and used abs function to prevent negative randomised score values /OLCS-19275

--- module/Api/src/Domain/Repository/IrhpCandidatePermit.php
+++ module/Api/src/Domain/Repository/IrhpCandidatePermit.php
@@ -6,7 +6,6 @@ use Dvsa\Olcs\Api\Entity\Permits\IrhpCandidatePermit as Entity;
 use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
 use Doctrine\ORM\Query;
 use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitApplication as IrhpPermitApplicationEntity;
-use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
 use Doctrine\ORM\QueryBuilder;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
