5b61884c4bd940c250ca1852da38f5ab187c025c
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-09-24 11:30:24 +0100

    moved command and command handler after review OLCS-19275

---
+++ module/Cli/src/Domain/Command/Permits/CalculateRandomApplicationScore.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Calculate randomised app score
+ *
+ */
+namespace Dvsa\Olcs\Cli\Domain\Command\Permits;
+
+use Dvsa\Olcs\Transfer\Util\Annotation as Transfer;
+use Dvsa\Olcs\Transfer\Command\AbstractCommand;
+
+/**
+ */
+final class CalculateRandomApplicationScore extends AbstractStockIdCommand
+{
+}
