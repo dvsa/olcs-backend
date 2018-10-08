36af7e086c1864b57709b0ebc34a61c9e0002b8d
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-10-08 17:09:30 +0100

    CanVerify test

--- app/api/module/Api/src/Domain/Validation/Handlers/Misc/CanVerify.php
+++ app/api/module/Api/src/Domain/Validation/Handlers/Misc/CanVerify.php
@@ -7,6 +7,7 @@ use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\AbstractHandler;
 use Dvsa\Olcs\Api\Entity\User\Permission;
+use Dvsa\Olcs\Transfer\Command\GdsVerify\ProcessSignatureResponse;
 
 class CanVerify extends AbstractHandler implements AuthAwareInterface
 {
@@ -14,13 +15,24 @@ class CanVerify extends AbstractHandler implements AuthAwareInterface
 
 
     /**
-     * @inheritdoc
+     * @param ProcessSignatureResponse $dto
+     *
+     * @return bool
      */
     public function isValid($dto)
     {
-        return $this->isOperator() || $this->isTransportManager();
+        $allowed = $this->isOperator();
+        if (!empty($dto->getTransportManagerApplication())) {
+            $allowed = $allowed || $this->isTransportManager();
+        }
+        return $allowed;
     }
 
+    /**
+     * isTransportManager
+     *
+     * @return bool
+     */
     private function isTransportManager()
     {
         return $this->isGranted(Permission::TRANSPORT_MANAGER);
