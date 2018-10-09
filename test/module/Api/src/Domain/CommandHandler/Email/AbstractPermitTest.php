ff5feca87a368e725e9f13d5c34d4ba2eb39a219
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-10-09 15:51:29 +0100

    OLCS-22014 test is awaiting fee

--- test/module/Api/src/Domain/CommandHandler/Email/AbstractPermitTest.php
+++ test/module/Api/src/Domain/CommandHandler/Email/AbstractPermitTest.php
@@ -7,9 +7,12 @@ use Dvsa\Olcs\Api\Domain\CommandHandler\CommandHandlerInterface;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Email\SendEcmtAppSubmitted;
 use Dvsa\Olcs\Api\Domain\Exception\MissingEmailException;
 use Dvsa\Olcs\Api\Domain\Repository\EcmtPermitApplication as PermitApplicationRepo;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Dvsa\Olcs\Api\Entity\ContactDetails\ContactDetails;
+use Dvsa\Olcs\Api\Entity\Fee\Fee;
 use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
 use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitApplication;
 use Dvsa\Olcs\Api\Entity\User\User;
 use Dvsa\Olcs\Email\Data\Message;
 use Dvsa\Olcs\Email\Domain\Command\SendEmail;
@@ -133,6 +136,128 @@ abstract class AbstractPermitTest extends CommandHandlerTestCase
         $this->assertSame($orgEmails, $message->getCc());
         $this->assertSame($this->subject, $message->getSubject());
     }
+
+
+
+
+    /**
+     * test handle command awaiting fee
+     */
+    public function testHandleCommandAwaitingFee()
+    {
+        $permitAppId = 1234;
+        $applicationRef = 'OC1234567/1234';
+        $command = $this->command::create(['id' => $permitAppId]);
+
+        $permitsRequired = 2;
+        $permitsAwarded = 2;
+        $issueFeeAmount = 123;
+        $permitsGrantedissueFeeTotal = $permitsRequired * $issueFeeAmount;
+        $paymentDeadlineNumDays = 10;
+        $issueFeeDeadlineDate = new DateTime('11 December 2018');
+        $awaitingFeeUrl = 'http://selfserve/permits/' . $permitAppId . '/ecmt-awaiting-fee/';
+
+        $userEmail = 'email1@test.com';
+        $orgEmails = [
+            'orgEmail1@test.com',
+            'orgEmail2@test.com'
+        ];
+
+        $templateVars = [
+            'appUrl' => 'http://selfserve/',
+            'permitsUrl' => 'http://selfserve/permits',
+            'guidanceUrl' => 'https://www.gov.uk/guidance/international-authorisations-and-permits-for-road-haulage',
+            'applicationRef' => $applicationRef,
+            'permitsRequired' => $permitsRequired,
+            'permitsGrantedissueFeeTotal' => $permitsGrantedissueFeeTotal,
+            'issueFeeAmount' => $issueFeeAmount,
+            'paymentDeadlineNumDays' => $paymentDeadlineNumDays,
+            'issueFeeDeadlineDate' => $issueFeeDeadlineDate,
+            'awaitingFeeUrl' => $awaitingFeeUrl
+        ];
+
+
+
+
+
+        $contactDetails = m::mock(ContactDetails::class);
+        $contactDetails->shouldReceive('getEmailAddress')->once()->withNoArgs()->andReturn($userEmail);
+
+        $userEntity = m::mock(User::class);
+        $userEntity->shouldReceive('getContactDetails')->once()->withNoArgs()->andReturn($contactDetails);
+
+        $organisation = m::mock(Organisation::class);
+        $organisation->shouldReceive('getAdminEmailAddresses')->once()->andReturn($orgEmails);
+
+        $irhpPermitApplication = m::mock(IrhpPermitApplication::class);
+        $irhpPermitApplication->shouldReceive('first')->andReturn($irhpPermitApplication);
+        $irhpPermitApplication->shouldReceive('getPermitsRequired')->andReturn($permitsRequired);
+        $irhpPermitApplication->shouldReceive('countPermitsAwarded')->andReturn($permitsAwarded);
+
+
+        $fee = m::mock(Fee::class);
+        $fee->shouldReceive('isEcmtIssuingFee')->andReturn(true);
+        $fee->shouldReceive('getFeeTypeAmount')->andReturn($issueFeeAmount);
+        $fee->shouldReceive('getOutstandingAmount')->andReturn($permitsGrantedissueFeeTotal);
+        $fee->shouldReceive('getInvoicedDateTime')->andReturn(
+            new DateTime('1 December 2018')
+        );
+
+
+        $fees = [$fee];
+
+
+
+
+
+        $applicationEntity = m::mock(EcmtPermitApplication::class);
+        $applicationEntity->shouldReceive('getIrhpPermitApplications')->andReturn($irhpPermitApplication);
+        $applicationEntity->shouldReceive('getCreatedBy')->once()->withNoArgs()->andReturn($userEntity);
+        $applicationEntity->shouldReceive('getApplicationRef')->twice()->withNoArgs()->andReturn($applicationRef);
+        $applicationEntity->shouldReceive('getId')->twice()->withNoArgs()->andReturn($permitAppId);
+        $applicationEntity->shouldReceive('getPermitsRequired')->andReturn($permitsRequired);
+        $applicationEntity->shouldReceive('getFees->matching')->andReturn($fees);
+
+        $applicationEntity->shouldReceive('getLicence->getOrganisation')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($organisation);
+        $applicationEntity->shouldReceive('isAwaitingFee')->once()->withNoArgs()->andReturn(true);
+
+        $this->repoMap['EcmtPermitApplication']
+            ->shouldReceive('fetchUsingId')
+            ->with($command)
+            ->once()
+            ->andReturn($applicationEntity);
+
+        $this->mockedSmServices[TemplateRenderer::class]->shouldReceive('renderBody')->once();
+
+        $data = [
+            'to' => $userEmail,
+            'locale' => 'en_GB',
+            'subject' => $this->subject,
+        ];
+
+        $this->expectedSideEffect(SendEmail::class, $data, new Result());
+
+        $result = $this->sut->handleCommand($command);
+
+        $this->assertSame(['EcmtPermitApplication' => $permitAppId], $result->getIds());
+        $this->assertSame(['Email sent'], $result->getMessages());
+
+        /** @var Message $message */
+        $message = $this->sut->getMessage();
+        $this->assertSame($userEmail, $message->getTo());
+        $this->assertSame($orgEmails, $message->getCc());
+        $this->assertSame($this->subject, $message->getSubject());
+    }
+
+
+
+
+
+
+
 
     /**
      * test the exception is dealt with when there are no email addresses
