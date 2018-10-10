3e35ad62bc02fcce65da41c0d791a42465166b1c
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-10-10 13:27:10 +0100

    OLCS-21662 changing fee status to cancelled when an ECMT application is withdrawn or declined

--- module/Api/src/Domain/CommandHandler/Permits/WithdrawEcmtPermitApplication.php
+++ module/Api/src/Domain/CommandHandler/Permits/WithdrawEcmtPermitApplication.php
@@ -4,11 +4,11 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\Permits;
 
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
-
 use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication;
-
+use Dvsa\Olcs\Api\Entity\Fee\Fee;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Dvsa\Olcs\Transfer\Command\Permits\WithdrawEcmtPermitApplication as WithdrawEcmtPermitApplicationCmd;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * Withdraw an ECMT Permit application
@@ -18,6 +18,7 @@ use Dvsa\Olcs\Transfer\Command\Permits\WithdrawEcmtPermitApplication as Withdraw
 final class WithdrawEcmtPermitApplication extends AbstractCommandHandler
 {
     protected $repoServiceName = 'EcmtPermitApplication';
+    protected $extraRepos = ['Fee'];
 
     /**
      * Handle command
@@ -38,6 +39,14 @@ final class WithdrawEcmtPermitApplication extends AbstractCommandHandler
         $application->withdraw($newStatus);
 
         $this->getRepo()->save($application);
+
+        $criteria = Criteria::create();
+        $criteria->andWhere($criteria->expr()->eq('feeStatus', $this->refData(Fee::STATUS_OUTSTANDING)));
+        $criteria->andWhere($criteria->expr()->eq('ecmtPermitApplication', $application));
+        foreach ($application->getFees()->matching($criteria) as $fee) {
+            $fee->setFeeStatus($this->refData(Fee::STATUS_CANCELLED));
+            $this->getRepo('Fee')->save($fee);
+        }
 
         $result = new Result();
         $result->addId('ecmtPermitApplication', $id);
