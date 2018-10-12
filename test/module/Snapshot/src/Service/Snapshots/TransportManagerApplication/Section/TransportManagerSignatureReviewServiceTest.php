111c14e1b2b60101275f8eddbb383b3ad3ecb25e
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-10-12 16:20:03 +0100

    Revert "Snapshot work"
    This reverts commit a3f3bc16dcc4720256e5b751e5305c5b7301b3f2.

--- test/module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Section/TransportManagerSignatureReviewServiceTest.php
+++ test/module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Section/TransportManagerSignatureReviewServiceTest.php
@@ -57,18 +57,15 @@ class TransportManagerSignatureReviewServiceTest extends MockeryTestCase
             ->andReturn('ADDRESS');
 
         $mockTranslator->shouldReceive('translate')->with($expected['label'], 'snapshot')->once()
-            ->andReturn($expected['label'] . 'translated');
+            ->andReturn($expected['label'] .'translated');
 
         $tma = m::mock(TransportManagerApplication::class);
         $tma->shouldReceive('getTmDigitalSignature')->andReturn($data['digitalSignature']);
 
-        $tma->shouldReceive('getOpDigitalSignature')->once()->andReturn(null);
-        $tma->shouldReceive('getIsOwner')->once()->andReturn('N');
-
         $tma->shouldReceive('getApplication->getLicence->getOrganisation->getType->getId')->with()->once()
             ->andReturn($data['organisationType']);
 
-        $expectedMarkup = $expected['label'] . 'translated_ADDRESS';
+        $expectedMarkup = $expected['label'] .'translated_ADDRESS';
 
         if ($data['digitalSignature']) {
             $digitalSignatureDate = (new DateTime('2018-01-01'))->format('d-m-Y');
@@ -160,61 +157,6 @@ class TransportManagerSignatureReviewServiceTest extends MockeryTestCase
                     'label' => 'responsible-person-signature',
                 ]
             ],
-        ];
-    }
-
-    /**
-     * testAppropriateTemplateUsedForDigitalSignatures
-     *
-     * @dataProvider digitalSignatureDataProvider
-     *
-     * @param $conditions
-     * @param $expected
-     */
-    public function testAppropriateTemplateUsedForDigitalSignatures($conditions, $expected)
-    {
-
-        $mockTranslator = m::mock();
-        $this->sm->setService('translator', $mockTranslator);
-
-        $mockTranslator
-            ->shouldReceive('translate')
-            ->with(TransportManagerSignatureReviewService::ADDRESS, 'snapshot')
-            ->once()
-            ->andReturn('ADDRESS');
-
-        $mockTranslator->shouldReceive('translate')->with($expected['label'], 'snapshot')->once()
-            ->andReturn($expected['label'] . 'translated');
-
-        $tma = m::mock(TransportManagerApplication::class);
-
-        $tma->shouldReceive('getOpDigitalSignature')->once()->andReturn($conditions['OpSignature']);
-        $tma->shouldReceive('getIsOwner')->once()->andReturn($conditions['isOwner']);
-        $tma->shouldReceive('getTmDigitalSignature')->once()->andReturn($conditions['tmSignature']);
-        $tma->shouldReceive('getApplication->getLicence->getOrganisation->getType->getId')->with()->once()
-            ->andReturn(Organisation::ORG_TYPE_SOLE_TRADER);
-
-        $mockTranslator
-            ->shouldReceive('translate')
-            ->with($conditions['markup'], 'snapshot')
-            ->once()
-            ->andReturn('%s_%s_%s_%s_%s');
-
-        $actual = $this->sut->getConfig($tma);
-        $this->assertEquals([
-            'markup' => $expected['markup']
-        ], $actual);
-    }
-
-    public function digitalSignatureDataProvider()
-    {
-        return [
-
-                "as operator" => [
-                    ['OpSignature' => true, 'isOwner' => true, 'tmSignature' => false, "markup" =>TransportManagerSignatureReviewService::SIGNATURE_DIGITAL],
-                    ['label' => 'owners-signature', 'markup' => 'test']
-                ]
-
         ];
     }
 }
