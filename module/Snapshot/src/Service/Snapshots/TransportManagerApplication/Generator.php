b29efacbbb7f93781ffc68bcadc7b55f3c935a2f
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-10-14 19:22:47 +0100

    updating for variations

--- module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Generator.php
+++ module/Snapshot/src/Service/Snapshots/TransportManagerApplication/Generator.php
@@ -5,6 +5,7 @@
  *
  * @author Rob Caiger <rob@clocal.co.uk>
  */
+
 namespace Dvsa\Olcs\Snapshot\Service\Snapshots\TransportManagerApplication;
 
 use Doctrine\Common\Collections\Criteria;
@@ -49,7 +50,9 @@ class Generator extends AbstractGenerator
             $tma->getTmApplicationStatus()->getId() === TransportManagerApplication::STATUS_OPERATOR_SIGNED
         ) {
             $sections[] = $this->getDeclarationReviewSection($tma);
-            $sections[] = $this->getSignatureReviewSection($tma);
+            if (!$tma->getApplication()->isVariation()) {
+                $sections[] = $this->getSignatureReviewSection($tma);
+            }
         }
 
         return $this->generateReadonly(
