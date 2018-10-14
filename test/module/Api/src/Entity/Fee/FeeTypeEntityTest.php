b17a1fe49f18e8d44d18f16dfe392fb04ef54a38
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-10-14 22:20:30 +0100

    payment fixes - add checks in the fee type entity for whether a fee is ecmt related

--- test/module/Api/src/Entity/Fee/FeeTypeEntityTest.php
+++ test/module/Api/src/Entity/Fee/FeeTypeEntityTest.php
@@ -20,6 +20,9 @@ class FeeTypeEntityTest extends EntityTester
      */
     protected $entityClass = Entity::class;
 
+    /**
+     * @var Entity
+     */
     protected $sut;
 
     public function setUp()
@@ -107,5 +110,82 @@ class FeeTypeEntityTest extends EntityTester
         $feeType->setId(Entity::FEE_TYPE_IRFOPSVAPP);
         $this->sut->setFeeType($feeType);
         $this->assertFalse($this->sut->isShowQuantity());
+    }
+
+    /**
+     * @dataProvider dpIsEcmtApplication
+     */
+    public function testIsEcmtApplication(string $feeType, bool $expectedResult)
+    {
+        $feeTypeRefData = new RefData($feeType);
+        $this->sut->setFeeType($feeTypeRefData);
+        $this->assertEquals($expectedResult, $this->sut->isEcmtApplication());
+    }
+
+    public function dpIsEcmtApplication(): array
+    {
+        return $this->listTypes([Entity::FEE_TYPE_ECMT_APP]);
+    }
+
+    /**
+     * @dataProvider dpIsEcmtIssue
+     */
+    public function testIsEcmtIssue(string $feeType, bool $expectedResult)
+    {
+        $feeTypeRefData = new RefData($feeType);
+        $this->sut->setFeeType($feeTypeRefData);
+        $this->assertEquals($expectedResult, $this->sut->isEcmtIssue());
+    }
+
+    public function dpIsEcmtIssue(): array
+    {
+        return $this->listTypes([Entity::FEE_TYPE_ECMT_ISSUE]);
+    }
+
+    /**
+     * Build a list of valid fee types, that can be fed into a method and return a true/false
+     * Avoids duplication in data providers
+     *
+     * @param array $matchingTypes array of the types that will return true
+     *
+     * @return array
+     */
+    private function listTypes(array $matchingTypes): array
+    {
+        $returnTypes = [];
+
+        $types = [
+            Entity::FEE_TYPE_APP,
+            Entity::FEE_TYPE_GRANT,
+            Entity::FEE_TYPE_CONT,
+            Entity::FEE_TYPE_VEH,
+            Entity::FEE_TYPE_GRANTINT,
+            Entity::FEE_TYPE_INTVEH,
+            Entity::FEE_TYPE_DUP,
+            Entity::FEE_TYPE_APP,
+            Entity::FEE_TYPE_ANN,
+            Entity::FEE_TYPE_GRANTVAR,
+            Entity::FEE_TYPE_BUSAPP,
+            Entity::FEE_TYPE_BUSVAR,
+            Entity::FEE_TYPE_GVANNVEH,
+            Entity::FEE_TYPE_INTUPGRADEVEH,
+            Entity::FEE_TYPE_INTAMENDED,
+            Entity::FEE_TYPE_IRFOPSVAPP,
+            Entity::FEE_TYPE_IRFOPSVANN,
+            Entity::FEE_TYPE_IRFOPSVCOPY,
+            Entity::FEE_TYPE_IRFOGVPERMIT,
+            Entity::FEE_TYPE_ADJUSTMENT,
+            Entity::FEE_TYPE_ECMT_APP,
+            Entity::FEE_TYPE_ECMT_ISSUE,
+        ];
+
+        foreach ($types as $type) {
+            $returnTypes[] = [
+                $type,
+                in_array($type, $matchingTypes)
+            ];
+        }
+
+        return $returnTypes;
     }
 }
