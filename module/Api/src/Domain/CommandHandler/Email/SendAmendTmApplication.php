0e85dbe856cfb3574d9023dae442dd1fb1c53017
Parth <parth.vyas@dvsa.gov.uk>
2018-10-15 14:28:30 +0100

    OLCS-21569: Incorporate user submitted email into TMA email command handlers

--- module/Api/src/Domain/CommandHandler/Email/SendAmendTmApplication.php
+++ module/Api/src/Domain/CommandHandler/Email/SendAmendTmApplication.php
@@ -2,62 +2,13 @@
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Email;
 
-class SendAmendTmApplication extends AbstractEmailHandler
+use Dvsa\Olcs\Api\Domain\CommandHandler\Email\AbstractSendTmEmail;
+
+/**
+ * Send Amend Transport Manager Application Email
+ */
+class SendAmendTmApplication extends AbstractSendTmEmail
 {
-    protected $repoServiceName = 'TransportManagerApplication';
-
-    protected $template = 'transport-manager-complete-digital-form';
-
-    protected $subject = 'email.transport-manager-complete-digital-form.subject';
-
-    protected $resultMessage = 'Transport Manager Application email sent';
-
-    /**
-     * @param object $tma
-     *
-     * @return array
-     */
-    protected function getRecipients($tma): array
-    {
-        /* @var $tma \Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication */
-        return [
-            'to' => $tma->getTransportManager()->getUsers()->first()->getContactDetails()->getEmailAddress(),
-            'cc' => [],
-            'bcc' => []
-        ];
-    }
-
-    /**
-     * Returns variables for templates
-     *
-     * @param object $tma
-     *
-     * @return array
-     */
-    protected function getTemplateVariables($tma)
-    {
-        /* @var $tma \Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication */
-        return [
-            'organisation' => $tma->getApplication()->getLicence()->getOrganisation()->getName(),
-            'reference' => $tma->getApplication()->getLicence()->getLicNo() . '/' . $tma->getApplication()->getId(),
-            'username' => $username = $tma->getTransportManager()->getUsers()->isEmpty() ? 'not registered' :
-                $tma->getTransportManager()->getUsers()->first()->getLoginId(),
-            'isNi' => $tma->getApplication()->getNiFlag() === 'Y',
-            'signInLink' => sprintf(
-                'http://selfserve/%s/%d/transport-managers/details/%d/edit-details/',
-                ($tma->getApplication()->getIsVariation()) ? 'variation' : 'application',
-                $tma->getApplication()->getId(),
-                $tma->getId()
-            )
-        ];
-    }
-
-    protected function getTranslateToWelsh($tma): string
-    {
-        /* @var $tma \Dvsa\Olcs\Api\Entity\Tm\TransportManagerApplication */
-        return $tma->getApplication()->getLicence()->getTranslateToWelsh();
-    }
-
-
-
+    protected $template = 'transport-manager-amend-digital-form';
+    protected $subject = 'email.transport-manager-amend-digital-form.subject';
 }
