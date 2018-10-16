cbc4a8f5a608b4a50f494a981c91df9435eff470
Henry White <henry.white@capgemini.com>
2018-10-16 13:39:00 +0100

    OLCS-22048: Review comments, added logic for StartDate bookmark (+1 squashed commit) Squashed commits: [a286f1b3a] OLCS-22047: Created GeneratePermit Command, GeneratePermit CommandHandler, IrhpStartDate Bookmark, IrhpExpiryDate Bookmark, OpCorrespondenceAddress Bookmark, IrhpPermitBundle Query and IrhpPermitBundle QueryHandler to support the generation of an IRHP ECMT RTF

---
+++ module/Api/src/Domain/QueryHandler/Bookmark/IrhpPermitStockBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Bookmark;
+
+/**
+ * IrhpPermitStockBundle
+ *
+ * @author Henry White <henry.white@capgemini.com>
+ */
+class IrhpPermitStockBundle extends AbstractBundle
+{
+    protected $repoServiceName = 'IrhpPermitStock';
+}
