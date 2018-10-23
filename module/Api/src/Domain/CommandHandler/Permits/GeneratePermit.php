4e1918a52e3d8f8d80731ddf40aa094b64ec7b13
Henry White <henry.white@capgemini.com>
2018-10-23 18:02:07 +0100

    OLCS-22048: Updated unit tests for Review comments (+3 squashed commits) Squashed commits: [b6bdec5a8] OLCS-22048: Review comments [d824fcde5] OLCS-22048: Review comments [cf533f29a] OLCS-22048: Use existing FieldType Trait (+3 squashed commits) Squashed commits: [d9b559eb0] OLCS-22048: Tidy up [c15837cd1] OLCS-22048: Added unit tests [db32edb80] OLCS-22048: Updated GeneratePermits to be more generic and to generate a covering letter for each permit

--- module/Api/src/Domain/CommandHandler/Permits/GeneratePermit.php
+++ module/Api/src/Domain/CommandHandler/Permits/GeneratePermit.php
@@ -3,16 +3,17 @@
 /**
  * Generate Permit
  */
+
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Permits;
 
 use Dvsa\Olcs\Api\Domain\Command\Document\GenerateAndStore;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Entity\Permits\EcmtPermitApplication as EcmtPermitApplicationEntity;
 use Dvsa\Olcs\Api\Entity\System\Category as CategoryEntity;
 use Dvsa\Olcs\Api\Entity\System\SubCategory as SubCategoryEntity;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Doctrine\ORM\Query;
-use Dvsa\Olcs\Api\Domain\UploaderAwareTrait;
 
 /**
  * Generate a Permit in RTF form
@@ -23,6 +24,34 @@ final class GeneratePermit extends AbstractCommandHandler
 {
     protected $repoServiceName = 'IrhpPermit';
 
+    private $templates = [
+        EcmtPermitApplicationEntity::PERMIT_TYPE => [
+            'permit' => [
+                'templateName' => EcmtPermitApplicationEntity::PERMIT_TEMPLATE_NAME,
+                'query' => [
+                    'licence' => '',
+                    'irhpPermit' => '',
+                    'irhpPermitStock' => '',
+                    'organisation' => ''
+                ],
+                'subCategory' => SubCategoryEntity::DOC_SUB_CATEGORY_PERMIT
+            ],
+            'coveringLetter' => [
+                'templateName' => EcmtPermitApplicationEntity::PERMIT_COVERING_LETTER_TEMPLATE_NAME,
+                'query' => [
+                    'licence' => '',
+                    'irhpPermit' => ''
+                ],
+                'subCategory' => SubCategoryEntity::DOC_SUB_CATEGORY_PERMIT_COVERING_LETTER
+            ]
+        ]
+    ];
+
+    /**
+     * @param CommandInterface $command
+     * @return Result
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
     public function handleCommand(CommandInterface $command)
     {
         $result = new Result();
@@ -31,43 +60,75 @@ final class GeneratePermit extends AbstractCommandHandler
             $irhpPermit = $this->getRepo()->fetchById($id, Query::HYDRATE_OBJECT);
 
             $irhpPermitApplication = $irhpPermit->getIrhpPermitApplication();
-            $irhpPermitStock = $irhpPermitApplication
-                ->getIrhpPermitWindow()
-                ->getIrhpPermitStock();
+            $irhpPermitStock = $irhpPermitApplication->getIrhpPermitWindow()->getIrhpPermitStock();
+            $irhpPermitType = $irhpPermitStock->getIrhpPermitType()->getName();
 
-            $irhpPermitType = $irhpPermitStock
-                ->getIrhpPermitType();
+            $template = $this->getTemplates()[trim($irhpPermitType)];
 
-            $description = sprintf(
-                'IRHP %s %d',
-                strtoupper(str_replace('_', ' ', $irhpPermitType->getName())),
-                $irhpPermit->getId()
-            );
+            foreach ($template as $documentType => $document) {
+                $documentQuery = [];
 
-            $document = $this->handleSideEffect(
-                GenerateAndStore::create(
-                    [
-                        'template' => 'IRHP_'. strtoupper($irhpPermitType->getName()),
-                        'query' => [
-                            'licence' => $irhpPermitApplication->getLicence()->getId(),
-                            'irhpPermit' => $irhpPermit->getId(),
-                            'irhpPermitStock' => $irhpPermitStock->getId(),
-                            'organisation' => $irhpPermitApplication->getLicence()->getOrganisation()->getId()
-                        ],
-                        'knownValues' => [],
-                        'description' => $description,
-                        'category' => CategoryEntity::CATEGORY_PERMITS,
-                        'subCategory' => SubCategoryEntity::DOC_SUB_CATEGORY_PERMIT,
-                        'isExternal' => false,
-                        'isScan' => false
-                    ]
-                )
-            );
+                foreach ($document['query'] as $key => $queryParam) {
+                    $queryParam = '';
+                    switch ($key) {
+                        case 'licence':
+                            $queryParam = $irhpPermitApplication
+                                ->getEcmtPermitApplication()
+                                ->getLicence()
+                                ->getId();
+                            break;
+                        case 'irhpPermit':
+                            $queryParam = $irhpPermit->getId();
+                            break;
+                        case 'irhpPermitStock':
+                            $queryParam = $irhpPermitStock->getId();
+                            break;
+                        case 'organisation':
+                            $queryParam = $irhpPermitApplication
+                                ->getEcmtPermitApplication()
+                                ->getLicence()
+                                ->getOrganisation()
+                                ->getId();
+                            break;
+                    }
 
-            $result->addId('identifier', $document->getId('identifier'), true);
-            $result->addMessage($description . ' RTF created and stored');
+                    $documentQuery[$key] = $queryParam;
+                }
+
+                $documentDescription = sprintf(
+                    '%s %d',
+                    strtoupper(str_replace('_', ' ', $document['templateName'])),
+                    $irhpPermit->getId()
+                );
+
+                $documentGenerated = $this->handleSideEffect(
+                    GenerateAndStore::create(
+                        [
+                            'template' => $document['templateName'],
+                            'query' => $documentQuery,
+                            'knownValues' => [],
+                            'description' => $documentDescription,
+                            'category' => CategoryEntity::CATEGORY_PERMITS,
+                            'subCategory' => $document['subCategory'],
+                            'isExternal' => false,
+                            'isScan' => false
+                        ]
+                    )
+                );
+
+                $result->addId($documentType, $documentGenerated->getId('identifier'), true);
+                $result->addMessage($documentDescription . ' RTF created and stored');
+            }
         }
 
         return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemplates()
+    {
+        return $this->templates;
     }
 }
