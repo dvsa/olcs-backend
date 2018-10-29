61bd631e9a3768972083a215b258afe9d7383011
Igor Stimoli <igor.stimoli@bjss.com>
2018-10-29 15:09:29 +0000

    OLCS-22270: add feature toggle to Surrender Create CommandHandler

--- app/api/module/Api/src/Domain/CommandHandler/Surrender/Create.php
+++ app/api/module/Api/src/Domain/CommandHandler/Surrender/Create.php
@@ -6,14 +6,20 @@ use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
+use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
+use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Dvsa\Olcs\Transfer\Command\Surrender\Create as Cmd;
 use Dvsa\Olcs\Api\Entity\Surrender;
 
-final class Create extends AbstractCommandHandler implements AuthAwareInterface, TransactionedInterface
+final class Create extends AbstractCommandHandler implements AuthAwareInterface, TransactionedInterface, ToggleRequiredInterface
 {
     use \Dvsa\Olcs\Api\Domain\AuthAwareTrait;
+    use ToggleAwareTrait;
+
+    protected $toggleConfig = [FeatureToggle::BACKEND_SURRENDER];
 
     protected $repoServiceName = 'Surrender';
 
