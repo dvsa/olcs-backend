019d13da8cfcae059f64373ee7d3042cf7ea95f6
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-10-29 17:21:22 +0000

    WIP OLCS-22273 start of test

---
+++ test/module/Api/src/Domain/QueryHandler/Surrender/StatusTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Surrender;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\Surrender\Status;
+use Dvsa\Olcs\Api\Domain\Repository\Surrender;
+use Dvsa\Olcs\Transfer\Query\Surrender\GetStatus;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+
+class StatusTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new Status();
+        $this->mockRepo('Surrender', Surrender::class);
+        parent::setUp();
+    }
+
+    public function testHandleQuery()
+    {
+        $query = GetStatus::create(['id' => 1]);
+        $mockSurrender = \Mockery::mock(\Dvsa\Olcs\Api\Entity\Surrender::class);
+        $mockSurrender->shouldReceive('getStatus')->once()->andReturn(null);
+        $this->repoMap['Surrender']->
+        shouldReceive('fetchById')->once()->andReturn(
+            $mockSurrender
+        );
+        $result = $this->sut->handleQuery($query);
+        $this->assertEquals([], $result->serialize());
+    }
+}
