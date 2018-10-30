8980a94c7f4d9b4e2a91d71f5ed4065e22ef68d2
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-10-30 17:17:40 +0000

    olcs-22378 add feature toggle configs to backend permits command handler

--- app/api/module/Api/src/Domain/QueryHandler/IrhpPermitApplication/GetList.php
+++ app/api/module/Api/src/Domain/QueryHandler/IrhpPermitApplication/GetList.php
@@ -3,14 +3,20 @@
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\IrhpPermitApplication;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
+use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
+use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
+use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 /**
  * IRHP Application
  *
  */
-class GetList extends AbstractListQueryHandler
+class GetList extends AbstractListQueryHandler implements ToggleRequiredInterface
 {
+    use ToggleAwareTrait;
+
+    protected $toggleConfig = [FeatureToggle::ADMIN_PERMITS];
     protected $repoServiceName = 'EcmtPermitApplication';
     protected $bundle = ['licence' => ['organisation']];
 }
