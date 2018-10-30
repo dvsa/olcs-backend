4d0a50dd0d4885fbdc754d796fea0cb99a8ba7b7
Parth <parth.vyas@dvsa.gov.uk>
2018-10-30 10:21:05 +0000

    OLCS-21985: Modify Licence query handler & Application repo for surrender link

--- module/Api/src/Domain/Repository/Application.php
+++ module/Api/src/Domain/Repository/Application.php
@@ -305,4 +305,28 @@ class Application extends AbstractRepository
 
         return $qb->getQuery()->getResult();
     }
+
+    public function fetchOpenApplicationsForLicence($licenceId)
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->leftJoin($this->alias . '.licence', 'l');
+
+        $qb->andWhere($qb->expr()->eq($this->alias . '.licence', ':licenceId'));
+        $qb->andWhere($qb->expr()->eq($this->alias . '.status', ':applicationStatus'));
+//        $qb->andWhere($qb->expr()->notIn(
+//            'l' . '.status',
+//            [
+//                LicenceE::LICENCE_STATUS_VALID,
+//                LicenceE::LICENCE_STATUS_CURTAILED,
+//                LicenceE::LICENCE_STATUS_SUSPENDED
+//            ]
+//        ));
+
+        $qb->setParameter('licenceId', $licenceId);
+        $qb->setParameter('applicationStatus', Entity::APPLICATION_STATUS_UNDER_CONSIDERATION);
+
+        return $qb->getQuery()->getResult();
+        //return count($qb->getQuery()->getResult()) === 0;
+    }
 }
