5e0cbd579bbb6476ae1b9185b747bde4ba643761
Igor Stimoli <igor.stimoli@bjss.com>
2018-11-06 12:09:33 +0000

    OLCS-22271: Delete Surrender CommandHandler

--- app/api/module/Api/config/validation-map/surrender.config.php
+++ app/api/module/Api/config/validation-map/surrender.config.php
@@ -6,4 +6,5 @@ use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsExternalUser;
 return [
     CommandHandler\Surrender\Create::class                              => IsExternalUser::class,
     CommandHandler\Surrender\Update::class                              => IsExternalUser::class,
+    CommandHandler\Surrender\Delete::class                              => IsExternalUser::class,
 ];
