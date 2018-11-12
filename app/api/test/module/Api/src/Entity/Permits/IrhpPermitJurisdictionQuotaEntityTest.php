727d6c33fc6011908329ff6097cf85b8199973f3
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-11-12 01:57:02 +0000

    olcs-22291 create and update sector quotas

--- app/api/test/module/Api/src/Entity/Permits/IrhpPermitJurisdictionQuotaEntityTest.php
+++ app/api/test/module/Api/src/Entity/Permits/IrhpPermitJurisdictionQuotaEntityTest.php
@@ -2,8 +2,11 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Permits;
 
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitStock;
+use Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitJurisdictionQuota as Entity;
+use Mockery as m;
 
 /**
  * IrhpPermitJurisdictionQuota Entity Unit Tests
@@ -18,4 +21,42 @@ class IrhpPermitJurisdictionQuotaEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    public function testCreate()
+    {
+        $trafficArea = m::mock(TrafficArea::class);
+        $stock = m::mock(IrhpPermitStock::class);
+
+        $entity = $this->createEntity($trafficArea, $stock);
+
+        self::assertInstanceOf(Entity::class, $entity);
+        self::assertEquals($trafficArea, $entity->getTrafficArea());
+        self::assertEquals($stock, $entity->getIrhpPermitStock());
+    }
+
+    public function testUpdate()
+    {
+        $quotaNumber = 999;
+
+        $entity = $this->createEntity();
+        $entity->update($quotaNumber);
+
+        $this->assertEquals($quotaNumber, $entity->getQuotaNumber());
+    }
+
+    /**
+     * Create an entity, optionally passing in customised traffic area and stock
+     */
+    private function createEntity($trafficArea = null, $stock = null)
+    {
+        if ($trafficArea === null) {
+            $trafficArea = m::mock(TrafficArea::class);
+        }
+
+        if ($stock === null) {
+            $stock = m::mock(IrhpPermitStock::class);
+        }
+
+        return Entity::create($trafficArea, $stock);
+    }
 }
