0b13400f846002cde0889b9debd821d10bb30036
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-11-12 01:57:02 +0000

    olcs-22291 create and update sector quotas

---
+++ test/module/Api/src/Domain/CommandHandler/IrhpPermitJurisdiction/CreateTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\IrhpPermitJurisdiction;
+
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\CommandHandler\IrhpPermitJurisdiction\Create as CreateHandler;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpPermitJurisdictionQuota as PermitJurisdictionQuotaRepo;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpPermitStock as IrhpPermitStockRepo;
+use Dvsa\Olcs\Api\Domain\Repository\TrafficArea as TrafficAreaRepo;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitJurisdictionQuota;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitStock;
+use Dvsa\Olcs\Api\Entity\TrafficArea\TrafficArea;
+use Dvsa\Olcs\Transfer\Query\TrafficArea\TrafficAreaList;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Dvsa\Olcs\Api\Domain\Command\IrhpPermitJurisdiction\Create as CreateCmd;
+use Mockery as m;
+
+/**
+ * Create IRHP Permit Jurisdiction Test
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class CreateTest extends CommandHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new CreateHandler();
+        $this->mockRepo('IrhpPermitJurisdictionQuota', PermitJurisdictionQuotaRepo::class);
+        $this->mockRepo('IrhpPermitStock', IrhpPermitStockRepo::class);
+        $this->mockRepo('TrafficArea', TrafficAreaRepo::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleCommand()
+    {
+        $stockId = 999;
+        $stockEntity = m::mock(IrhpPermitStock::class);
+        $stockEntity->shouldReceive('getId')->once()->withNoArgs()->andReturn($stockId);
+
+        $trafficArea1 = m::mock(TrafficArea::class);
+        $trafficArea2 = m::mock(TrafficArea::class);
+        $trafficAreaResults = [$trafficArea1, $trafficArea2];
+
+        $command = CreateCmd::create(['id' => $stockId]);
+
+        $this->repoMap['IrhpPermitStock']
+            ->shouldReceive('fetchUsingId')
+            ->once()
+            ->with($command)
+            ->andReturn($stockEntity);
+
+        $this->repoMap['TrafficArea']
+            ->shouldReceive('fetchList')
+            ->once()
+            ->with(m::type(TrafficAreaList::class), Query::HYDRATE_OBJECT)
+            ->andReturn($trafficAreaResults);
+
+        $this->repoMap['IrhpPermitJurisdictionQuota']
+            ->shouldReceive('save')
+            ->times(count($trafficAreaResults))
+            ->with(m::type(IrhpPermitJurisdictionQuota::class));
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [
+                'Irhp Permit Stock' => $stockId
+            ],
+            'messages' => [
+                'Irhp jurisdiction quotas created for stock ' . $stockId
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+}
--- test/module/Api/src/Entity/Permits/IrhpPermitSectorQuotaEntityTest.php
+++ test/module/Api/src/Entity/Permits/IrhpPermitSectorQuotaEntityTest.php
@@ -2,8 +2,11 @@
 
 namespace Dvsa\OlcsTest\Api\Entity\Permits;
 
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitStock;
+use Dvsa\Olcs\Api\Entity\Permits\Sectors;
 use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
 use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitSectorQuota as Entity;
+use Mockery as m;
 
 /**
  * IrhpPermitSectorQuota Entity Unit Tests
@@ -18,4 +21,41 @@ class IrhpPermitSectorQuotaEntityTest extends EntityTester
      * @var string
      */
     protected $entityClass = Entity::class;
+
+    public function testCreate()
+    {
+        $sector = m::mock(Sectors::class);
+        $stock = m::mock(IrhpPermitStock::class);
+        $entity = $this->createEntity($sector, $stock);
+
+        self::assertInstanceOf(Entity::class, $entity);
+        self::assertEquals($sector, $entity->getSector());
+        self::assertEquals($stock, $entity->getIrhpPermitStock());
+    }
+
+    public function testUpdate()
+    {
+        $quotaNumber = 999;
+
+        $entity = $this->createEntity();
+        $entity->update($quotaNumber);
+
+        $this->assertEquals($quotaNumber, $entity->getQuotaNumber());
+    }
+
+    /**
+     * Create an entity, optionally passing in customised sector and stock
+     */
+    private function createEntity($sector = null, $stock = null)
+    {
+        if ($sector === null) {
+            $sector = m::mock(Sectors::class);
+        }
+
+        if ($stock === null) {
+            $stock = m::mock(IrhpPermitStock::class);
+        }
+
+        return Entity::create($sector, $stock);
+    }
 }
