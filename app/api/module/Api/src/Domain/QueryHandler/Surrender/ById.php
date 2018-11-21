c657ad615634f453c3097d1a8ed173d8b6d6b938
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-11-21 12:58:20 +0000

    updated id query

--- app/api/module/Api/src/Domain/QueryHandler/Surrender/ById.php
+++ app/api/module/Api/src/Domain/QueryHandler/Surrender/ById.php
@@ -4,14 +4,23 @@
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Surrender;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryByIdHandler;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
-final class ById extends AbstractQueryByIdHandler implements ToggleRequiredInterface
+final class ById extends AbstractQueryHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
     protected $toggleConfig = [FeatureToggle::BACKEND_SURRENDER];
     protected $repoServiceName = 'Surrender';
+
+    public function handleQuery(QueryInterface $query)
+    {
+        $surrender = $this->getRepo('Surrender')->fetchById($query->getId());
+        $disableSignatures = $this->getRepo('SystemParameter')->getDisableGdsVerifySignatures();
+        return $this->result($surrender, ['disableSignatures'=>$disableSignatures]);
+    }
 }
