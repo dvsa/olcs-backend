1e6da2e5e495f6ba56b1e1d3b39d6798ecb8f10f
Parth <parth.vyas@dvsa.gov.uk>
2018-11-22 14:19:04 +0000

    OLCS-20880: Modify UpdateServiceDetails commandHandler and corrosponding tests.

--- test/module/Api/src/Domain/CommandHandler/Bus/UpdateServiceDetailsTest.php
+++ test/module/Api/src/Domain/CommandHandler/Bus/UpdateServiceDetailsTest.php
@@ -182,4 +182,162 @@ class UpdateServiceDetailsTest extends CommandHandlerTestCase
             [false]
         ];
     }
+
+    /**
+     * @dataProvider dpTestHandleCommandWithResultIds
+     */
+    public function testHandleCommandWithResultIds($serviceId, $serviceNo, $createFee, $expectedResult)
+    {
+        $busRegId = 99;
+        $serviceNumber = 12345;
+        $startPoint = 'start point';
+        $finishPoint = 'finish point';
+        $via = 'via';
+        $otherDetails = 'other details';
+        $effectiveDate = 'effective_date';
+        $receivedDate = 'received_date';
+        $endDate = '1';
+        $busNoticePeriod = 2;
+        $otherServices = [
+            0 => [
+                'id' => $serviceId,
+                'version' => 1,
+                'serviceNo' => $serviceNo
+            ],
+        ];
+        $busServiceTypes = [
+            0 => 5
+        ];
+
+        $command = Cmd::Create(
+            [
+                'id' => $busRegId,
+                'serviceNumber' => $serviceNumber,
+                'startPoint' => $startPoint,
+                'finishPoint' => $finishPoint,
+                'via' => $via,
+                'otherDetails' => $otherDetails,
+                'receivedDate' => $receivedDate,
+                'effectiveDate' => $effectiveDate,
+                'endDate' => $endDate,
+                'busNoticePeriod' => $busNoticePeriod,
+                'otherServices' => $otherServices,
+                'busServiceTypes' => $busServiceTypes
+            ]
+        );
+
+        /** @var BusRegOtherServiceEntity $busReg */
+        $mockBusRegOtherServiceEntity = m::mock(BusRegOtherServiceEntity::class);
+        $mockBusRegOtherServiceEntity->shouldReceive('setServiceNo');
+        $mockBusRegOtherServiceEntity->shouldReceive('getId')->andReturn($serviceId);
+
+        /** @var BusRegOtherServiceEntity $busReg */
+        $mockBusRegObjectOtherServiceEntity = m::mock(BusRegOtherServiceEntity::class);
+        $mockBusRegObjectOtherServiceEntity->shouldReceive('getId')->andReturn(123);
+
+        /** @var RefDataEntity $mockStatus */
+        $mockStatus = m::mock(RefDataEntity::class);
+        $mockStatus->shouldReceive('getId')->andReturn(BusRegEntity::STATUS_NEW);
+
+        /** @var BusRegEntity $busReg */
+        $busReg = m::mock(BusRegEntity::class)->makePartial();
+        $busReg->shouldReceive('updateServiceDetails')
+            ->once()
+            ->shouldReceive('getId')
+            ->andReturn($busRegId)
+            ->shouldReceive('getStatus')
+            ->andReturn($mockStatus)
+            ->shouldReceive('setBusServiceTypes')
+            ->with(m::type(ArrayCollection::class))
+            ->once()
+            ->shouldReceive('getOtherServices')
+            ->andReturn([0 => $mockBusRegObjectOtherServiceEntity])
+            ->shouldReceive('shouldCreateFee')
+            ->once()
+            ->andReturn($createFee);
+
+        $this->repoMap['Bus']->shouldReceive('fetchUsingId')
+            ->with($command, Query::HYDRATE_OBJECT, $command->getVersion())
+            ->andReturn($busReg)
+            ->shouldReceive('save')
+            ->with(m::type(BusRegEntity::class))
+            ->once();
+
+        $this->repoMap['BusRegOtherService']->shouldReceive('fetchById')
+            ->andReturn($mockBusRegOtherServiceEntity)
+            ->shouldReceive('save')
+            ->with(m::type(BusRegOtherServiceEntity::class))
+            ->shouldReceive('delete')
+            ->with(m::type(BusRegOtherServiceEntity::class));
+
+        $mockBusNoticePeriodEntity = m::mock(BusNoticePeriodEntity::class);
+
+        $this->repoMap['BusNoticePeriod']->shouldReceive('fetchById')
+            ->andReturn($mockBusNoticePeriodEntity);
+
+        if ($createFee) {
+            $createFeeResult = new Result();
+            $createFeeResult
+                ->addId('fee', 99)
+                ->addMessage('bus reg fee created');
+            $this->expectedSideEffect(CmdCreateBusFee::class, ['id' => $busRegId], $createFeeResult);
+        }
+
+        $result = $this->sut->handleCommand($command);
+
+
+        $this->assertEquals($expectedResult, $result->getIds());
+    }
+
+    public function dpTestHandleCommandWithResultIds(): array
+    {
+        return [
+            [
+                'serviceId' => 19,
+                'otherServiceNo' => 0,
+                'createFee' => true,
+                'expectedResult' => [
+                    'BusRegOtherService' => 19,
+                    'fee' => 99,
+                    'BusReg' => 99
+                ]
+            ],
+            [
+                'serviceId' => null,
+                'otherServiceNo' => null,
+                'createFee' => false,
+                'expectedResult' => [
+                    'BusReg' => 99
+                ]
+            ],
+            [
+                'serviceId' => 11,
+                'otherServiceNo' => null,
+                'createFee' => true,
+                'expectedResult' => [
+                    'fee' => 99,
+                    'BusReg' => 99
+                ]
+            ],
+            [
+                'serviceId' => null,
+                'otherServiceNo' => 0,
+                'createFee' => false,
+                'expectedResult' => [
+                    'BusRegOtherService' => null,
+                    'BusReg' => 99
+                ]
+            ],
+            [
+                'serviceId' => 1,
+                'otherServiceNo' => '1b',
+                'createFee' => true,
+                'expectedResult' => [
+                    'BusRegOtherService' => 1,
+                    'fee' => 99,
+                    'BusReg' => 99
+                ]
+            ],
+        ];
+    }
 }
