a3a6ff158db5827a9b997f455ff39049f28341cc
Igor Stimoli <igor.stimoli@bjss.com>
2018-12-03 10:41:16 +0000

    OLCS-22756: unit tests

--- test/module/Api/src/Domain/Validation/Validators/CanAccessLicenceTest.php
+++ test/module/Api/src/Domain/Validation/Validators/CanAccessLicenceTest.php
@@ -9,11 +9,8 @@ namespace Dvsa\OlcsTest\Api\Domain\Validation\Validators;
 
 use Dvsa\Olcs\Api\Domain\Validation\Validators\CanAccessLicence;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
-use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\Olcs\Api\Entity\User\Permission;
-use Dvsa\Olcs\Api\Entity\User\User;
 use Mockery as m;
-use ZfcRbac\Identity\IdentityInterface;
 
 /**
  * Can Access Licence Test
@@ -37,13 +34,12 @@ class CanAccessLicenceTest extends AbstractValidatorsTestCase
     /**
      * @dataProvider provider
      */
-    public function testIsValid($isOwner, $status, $expected)
+    public function testIsValid($isOwner, $expected)
     {
         $this->setIsGranted(Permission::INTERNAL_USER, false);
         $this->auth->shouldReceive('getIdentity')->andReturn(null);
         $entity = m::mock(Licence::class);
-        $status = new RefData($status);
-        $entity->shouldReceive('getStatus')->andReturn($status);
+
         $repo = $this->mockRepo('Licence');
         $repo->shouldReceive('fetchById')->with(111)->andReturn($entity);
 
@@ -55,13 +51,12 @@ class CanAccessLicenceTest extends AbstractValidatorsTestCase
     /**
      * @dataProvider provider
      */
-    public function testIsValidLicNo($isOwner, $status, $expected)
+    public function testIsValidLicNo($isOwner, $expected)
     {
         $this->setIsGranted(Permission::INTERNAL_USER, false);
         $this->auth->shouldReceive('getIdentity')->andReturn(null);
         $entity = m::mock(Licence::class);
-        $status = new RefData($status);
-        $entity->shouldReceive('getStatus')->andReturn($status);
+
         $repo = $this->mockRepo('Licence');
         $repo->shouldReceive('fetchByLicNo')->with('XY12345')->andReturn($entity);
 
@@ -70,83 +65,27 @@ class CanAccessLicenceTest extends AbstractValidatorsTestCase
         $this->assertEquals($expected, $this->sut->isValid('XY12345'));
     }
 
-    public function testIsValidInternal()
+    /**
+     * @dataProvider provider
+     */
+    public function testIsValidInternal($isOwner, $expected)
     {
         $this->setIsGranted(Permission::INTERNAL_USER, true);
-        $this->assertEquals(true, $this->sut->isValid(111));
-    }
+        $entity = m::mock(Licence::class);
 
-    public function testIsValidSystem()
-    {
-        $this->setIsGranted(Permission::INTERNAL_USER, false);
-        $mockUser = m::mock(User::class);
-        $mockUser->shouldReceive('isSystemUser')->andReturn(true);
-        $mockIdentity = m::mock(IdentityInterface::class);
-        $mockIdentity->shouldReceive('getUser')->andReturn($mockUser);
-        $this->auth->shouldReceive('getIdentity')->andReturn($mockIdentity);
+        $repo = $this->mockRepo('Licence');
+        $repo->shouldReceive('fetchById')->with(111)->andReturn($entity);
+
+        $this->setIsValid('isOwner', [$entity], $isOwner);
+
         $this->assertEquals(true, $this->sut->isValid(111));
     }
 
     public function provider()
     {
-
-        $cases = [];
-
-        $acceptedStatuses = [
-            Licence::LICENCE_STATUS_VALID,
-            Licence::LICENCE_STATUS_SUSPENDED,
-            Licence::LICENCE_STATUS_CURTAILED,
+        return [
+            [true, true],
+            [false, false]
         ];
-
-        $notAcceptedStatuses = [
-            Licence::LICENCE_STATUS_UNDER_CONSIDERATION,
-            Licence::LICENCE_STATUS_GRANTED,
-            Licence::LICENCE_STATUS_SURRENDER_UNDER_CONSIDERATION,
-            Licence::LICENCE_STATUS_SURRENDERED,
-            Licence::LICENCE_STATUS_WITHDRAWN,
-            Licence::LICENCE_STATUS_REFUSED,
-            Licence::LICENCE_STATUS_REVOKED,
-            Licence::LICENCE_STATUS_NOT_TAKEN_UP,
-            Licence::LICENCE_STATUS_TERMINATED,
-            Licence::LICENCE_STATUS_CONTINUATION_NOT_SOUGHT,
-            Licence::LICENCE_STATUS_UNLICENSED,
-            Licence::LICENCE_STATUS_CANCELLED,
-        ];
-
-        foreach ($acceptedStatuses as $acceptedStatus) {
-            $caseIsOwner = [
-                'isOwner' => true,
-                'status' => $acceptedStatus,
-                'expected' => true,
-            ];
-
-            $caseIsNotOwner = [
-                'isOwner' => false,
-                'status' => $acceptedStatus,
-                'expected' => false,
-            ];
-
-            array_push($cases, $caseIsOwner);
-            array_push($cases, $caseIsNotOwner);
-        }
-
-        foreach ($notAcceptedStatuses as $notAcceptedStatus) {
-            $caseIsOwner = [
-                'isOwner' => true,
-                'status' => $notAcceptedStatus,
-                'expected' => false,
-            ];
-
-            $caseIsNotOwner = [
-                'isOwner' => false,
-                'status' => $notAcceptedStatus,
-                'expected' => false,
-            ];
-
-            array_push($cases, $caseIsOwner);
-            array_push($cases, $caseIsNotOwner);
-        }
-
-        return $cases;
     }
 }
