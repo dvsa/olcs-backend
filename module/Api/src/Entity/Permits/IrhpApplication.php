bf5dc7da876ebebb45ffadb49edc4b353dd1364b
Piotr Filak <piotr.filak@capgemini.com>
2019-01-08 15:50:12 +0000

    OLCS-22908 - SS - Bilateral Permits application overview page

--- module/Api/src/Entity/Permits/IrhpApplication.php
+++ module/Api/src/Entity/Permits/IrhpApplication.php
@@ -23,6 +23,124 @@ use Dvsa\Olcs\Api\Entity\OrganisationProviderInterface;
 class IrhpApplication extends AbstractIrhpApplication implements OrganisationProviderInterface
 {
     /**
+     * @todo this needs to be much more robust, not least because how we store certain data is going to change
+     */
+    const SECTIONS = [
+        'licence' => 'fieldIsNotNull',
+        'countries' => 'countriesPopulated',
+        'permitsRequired' => 'fieldIsInt',
+    ];
+
+    const SECTION_COMPLETION_CANNOT_START = 'section_sts_csy';
+    const SECTION_COMPLETION_NOT_STARTED = 'section_sts_nys';
+    const SECTION_COMPLETION_COMPLETED = 'section_sts_com';
+
+    /**
+     * @param string $field field being checked
+     *
+     * @return bool
+     */
+    private function fieldIsNotNull($field)
+    {
+        return $this->$field !== null;
+    }
+
+    /**
+     * @param string $field field being checked
+     *
+     * @return bool
+     */
+    private function fieldIsInt($field)
+    {
+        return is_int($this->$field);
+    }
+
+    /**
+     * Checks an array collection has records
+     *
+     * @param string $field field being checked
+     *
+     * @return bool
+     */
+    private function collectionHasRecord($field)
+    {
+        return (bool)$this->$field->count();
+    }
+
+    /**
+     * This is a custom validator for the countries field
+     * It isn't completely dynamic because it's assumed that
+     * this won't be needed in the futuree
+     *
+     * @param string $field field being checked
+     *
+     * @return bool
+     */
+    private function countriesPopulated($field)
+    {
+        return $this->collectionHasRecord('irhpPermitApplications');
+    }
+
+    /**
+     * Gets calculated values
+     *
+     * @return array
+     */
+    public function getCalculatedBundleValues()
+    {
+        return [
+            'applicationRef' => $this->getApplicationRef(),
+            'canBeCancelled' => false, //$this->canBeCancelled(),
+            'canBeSubmitted' => false, //$this->canBeSubmitted(),
+            'hasOutstandingFees' => false, //$this->hasOutstandingFees(),
+            'sectionCompletion' => $this->getSectionCompletion(self::SECTIONS),
+            'hasCheckedAnswers' => false, //$this->hasCheckedAnswers(),
+            'hasMadeDeclaration' => false, //$this->hasMadeDeclaration(),
+            'isNotYetSubmitted' => true, //$this->isNotYetSubmitted(),
+        ];
+    }
+
+    /**
+     * @todo this needs to be much more robust, not least because how we store certain data is going to change
+     */
+    protected function getSectionCompletion($sections)
+    {
+        $sectionCompletion = [];
+        $totalCompleted = 0;
+        $totalSections = count($sections);
+
+        foreach ($sections as $field => $validator) {
+            //default field to not started
+            $status = self::SECTION_COMPLETION_NOT_STARTED;
+            $fieldCompleted = $this->$validator($field);
+
+            //if field completed, increment the completed number, and set the status
+            if ($fieldCompleted) {
+                $totalCompleted++;
+                $status = self::SECTION_COMPLETION_COMPLETED;
+            }
+
+            $sectionCompletion[$field] = $status;
+        }
+
+        $sectionCompletion['totalSections'] = $totalSections;
+        $sectionCompletion['totalCompleted'] = $totalCompleted;
+        $sectionCompletion['allCompleted'] = ($totalSections === $totalCompleted);
+
+        return $sectionCompletion;
+    }
+
+    /**
+     * Get the application reference
+     *
+     * @return string
+     */
+    public function getApplicationRef()
+    {
+        return sprintf('%s / %d', $this->licence->getLicNo(), $this->id);
+    }
+
+    /**
      * Get the organisation
      *
      * @return OrganisationEntity
@@ -31,6 +149,4 @@ class IrhpApplication extends AbstractIrhpApplication implements OrganisationPro
     {
         return $this->getLicence()->getOrganisation();
     }
-
-
 }
