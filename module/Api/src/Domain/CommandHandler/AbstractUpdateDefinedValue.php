5a9c51dcaa1c4343bba0d3ea9c120a1f0ab97e6d
Ian Lindsay <ian@hemera-business-services.co.uk>
2019-01-16 12:37:26 +0000

    olcs-22191 implement generic handlers for one question per page, test with existing ECMT

---
+++ module/Api/src/Domain/CommandHandler/AbstractUpdateDefinedValue.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler;
+
+use Doctrine\ORM\Query;
+
+use Dvsa\Olcs\Api\Domain\Repository\RepositoryInterface;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+
+/**
+ * Abstract update command handler
+ * Designed for "one question per page"
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class AbstractUpdateDefinedValue extends AbstractCommandHandler
+{
+    protected $repoServiceName = 'changeMe';
+    protected $recordName = 'Record';
+    protected $entityMethodName = 'changeMe';
+    protected $definedValue = 'changeMe';
+    protected $isRefData = false;
+
+    /**
+     * Handle command
+     *
+     * @param CommandInterface $command command
+     *
+     * @return \Dvsa\Olcs\Api\Domain\Command\Result
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        $entityMethod = $this->entityMethodName;
+
+        /** @var RepositoryInterface $repo */
+        $repo = $this->getRepo($this->repoServiceName);
+
+        //fetch the record from the DB
+        $recordObject = $repo->fetchUsingId($command, Query::HYDRATE_OBJECT);
+
+        $value = $this->definedValue;
+
+        //convert to ref data if necessary
+        if ($this->isRefData) {
+            $value = $this->refDataOrNull($value);
+        }
+
+        //call update method on the entity
+        $recordObject->$entityMethod($value);
+
+        //save the record
+        $repo->save($recordObject);
+
+        $this->result->addId($this->recordName, $recordObject->getId());
+        $this->result->addMessage($this->recordName . ' updated');
+
+        return $this->result;
+    }
+}
