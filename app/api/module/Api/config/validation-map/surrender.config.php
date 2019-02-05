f1878ae571f38c10cddfb1de1509b0cc6173b344
James Hodgson <james@tdchodgy.co.uk>
2019-02-05 11:34:36 +0000

    OLCS-22275: Improve surrender delete validation

--- app/api/module/Api/config/validation-map/surrender.config.php
+++ app/api/module/Api/config/validation-map/surrender.config.php
@@ -1,16 +1,15 @@
 <?php
 
 use Dvsa\Olcs\Api\Domain\CommandHandler;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanAccessLicenceWithId;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanAccessLicenceWithLicence;
-use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanDeleteSurrender;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanAccessLicenceWithId;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\CanSurrenderLicence;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Surrender\Delete as CanDeleteSurrender;
 
 return [
     CommandHandler\Surrender\Create::class                              => CanSurrenderLicence::class,
     CommandHandler\Surrender\Update::class                              => CanAccessLicenceWithId::class,
-    CommandHandler\Surrender\Delete::class                              => CanAccessLicenceWithId::class,
+    CommandHandler\Surrender\Delete::class                              => CanDeleteSurrender::class,
     QueryHandler\Surrender\GetStatus::class                             => CanAccessLicenceWithId::class,
     QueryHandler\Surrender\ByLicence::class                             => CanAccessLicenceWithId::class,
     CommandHandler\Surrender\SubmitForm::class                          => CanAccessLicenceWithId::class,
