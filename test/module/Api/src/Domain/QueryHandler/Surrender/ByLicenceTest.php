c86f692830c909eed9f6a9ed1680fcaee10c0a7b
Parth <parth.vyas@dvsa.gov.uk>
2019-02-05 11:18:23 +0000

    OLCS-22257: Modify ByLicenceTest to include additional result method values.

--- test/module/Api/src/Domain/QueryHandler/Surrender/ByLicenceTest.php
+++ test/module/Api/src/Domain/QueryHandler/Surrender/ByLicenceTest.php
@@ -4,9 +4,13 @@ namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\Surrender;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Api\Domain\QueryHandler\Surrender\ByLicence as QryHandler;
+use Dvsa\Olcs\Api\Domain\Repository\GoodsDisc;
+use Dvsa\Olcs\Api\Domain\Repository\PsvDisc;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Dvsa\Olcs\Api\Entity\Surrender;
 use Dvsa\Olcs\Transfer\Query\Surrender\ByLicence;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Mockery as m;
 
 class ByLicenceTest extends QueryHandlerTestCase
 {
@@ -15,20 +19,33 @@ class ByLicenceTest extends QueryHandlerTestCase
         $this->sut = new QryHandler();
         $this->mockRepo('Surrender', Surrender::class);
         $this->mockRepo('SystemParameter', SystemParameter::class);
+        $this->mockRepo('GoodsDisc', GoodsDisc::class);
+        $this->mockRepo('PsvDisc', PsvDisc::class);
         parent::setUp();
     }
 
     public function testQueryHandle()
     {
         $query = ByLicence::create(['id' => 1]);
-        $surrender = new Surrender();
-        $surrender->setId(1);
+
+        $surrender = m::mock(Surrender::class);
+        $surrender->shouldReceive('getLicence->getCorrespondenceCd->getAddress->getLastModifiedOn')->andReturn(new DateTime());
+
         $this->repoMap['Surrender']->shouldReceive(
             'fetchOneByLicence'
         )->andReturn($surrender);
+
         $this->repoMap['SystemParameter']->shouldReceive(
             'getDisableGdsVerifySignatures'
         )->andReturn(true);
+
+        $this->repoMap['GoodsDisc']->shouldReceive('countForLicence')->with($query->getId())
+            ->andReturn(9);
+
+        $this->repoMap['PsvDisc']->shouldReceive('countForLicence')->with($query->getId())
+            ->andReturn(7);
+
+
         $expected = new Result(
             $surrender,
             [
@@ -40,7 +57,11 @@ class ByLicenceTest extends QueryHandlerTestCase
                 'signatureType'
             ],
             [
-                'disableSignatures' => true
+                'disableSignatures' => true,
+                'goodsDiscsOnLicence' => 9,
+                'psvDiscsOnLicence' => 7,
+                'addressLastModified' => new DateTime()
+
             ]
         );
         $this->assertEquals($expected, $this->sut->handleQuery($query));
