7475a46d03cab146f42a970b32d70336bb0a55bb
James Hodgson <james@tdchodgy.co.uk>
2019-02-13 15:29:45 +0000

    Add a test for approve

--- app/api/module/Api/src/Domain/CommandHandler/Surrender/Approve.php
+++ app/api/module/Api/src/Domain/CommandHandler/Surrender/Approve.php
@@ -16,23 +16,22 @@ class Approve extends AbstractSurrenderCommandHandler
      * @param CommandInterface $command
      * @return Result
      * @throws RuntimeException
+     * @throws \Exception
      */
     public function handleCommand(CommandInterface $command)
     {
         $result = $this->handleSideEffect(UpdateSurrender::create(
             [
                 'id' => $command->getId(),
-                'status' => Surrender::SURRENDER_STATUS_SIGNED,
-                'signatureType' => $this->getRepo()->getRefdataReference(RefData::SIG_PHYSICAL_SIGNATURE)
+                'status' => Surrender::SURRENDER_STATUS_APPROVED
             ]
         ));
         $this->result->addMessage($result);
 
-        $surrenderDate = new \DateTime();
         $result = $this->handleSideEffect(SurrenderLicence::create(
             [
                 'id' => $command->getId(),
-                'surrenderDate' => $surrenderDate->format('Y-m-d'),
+                'surrenderDate' => $command->getSurrenderDate(),
                 'terminated' => false
             ]
         ));
