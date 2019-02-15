034964fba0aba1b5bc2577d9836fecadec2fa1e9
Parth <parth.vyas@dvsa.gov.uk>
2019-02-15 16:56:40 +0000

    OLCS-22990: Add feature toggle to surrender queries.

--- app/api/module/Api/src/Domain/QueryHandler/Surrender/OpenBusReg.php
+++ app/api/module/Api/src/Domain/QueryHandler/Surrender/OpenBusReg.php
@@ -4,12 +4,18 @@ namespace Dvsa\Olcs\Api\Domain\QueryHandler\Surrender;
 
 use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Api\Domain\Repository\BusRegSearchView as Repository;
+use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
+use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
+use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Dvsa\Olcs\Transfer\Query\Bus\SearchViewList;
 use Doctrine\ORM\Query as DoctrineQuery;
 
-class OpenBusReg extends AbstractQueryHandler
+class OpenBusReg extends AbstractQueryHandler implements ToggleRequiredInterface
 {
+    use ToggleAwareTrait;
+
+    protected $toggleConfig = [FeatureToggle::BACKEND_SURRENDER];
     protected $repoServiceName = 'BusRegSearchView';
 
     /**
