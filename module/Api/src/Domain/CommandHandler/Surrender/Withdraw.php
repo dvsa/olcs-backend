31a19f312dfdef2499780201a8d4d5d886d67e0f
James Hodgson <james@tdchodgy.co.uk>
2019-02-15 13:51:07 +0000

    Withdraw command now does something

--- module/Api/src/Domain/CommandHandler/Surrender/Withdraw.php
+++ module/Api/src/Domain/CommandHandler/Surrender/Withdraw.php
@@ -4,10 +4,15 @@
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Surrender;
 
 
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Surrender;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Dvsa\Olcs\Transfer\Command\Surrender\Update as UpdateSurrender;
 
 class Withdraw extends AbstractSurrenderCommandHandler
 {
+
+    protected $extraRepos = ['Licence'];
 
     /**
      * @param CommandInterface $command
@@ -15,6 +20,23 @@ class Withdraw extends AbstractSurrenderCommandHandler
      */
     public function handleCommand(CommandInterface $command)
     {
+        $result = $this->handleSideEffect(UpdateSurrender::create(
+            [
+                'id' => $command->getId(),
+                'status' => Surrender::SURRENDER_STATUS_WITHDRAWN
+            ]
+        ));
+        $this->result->addMessage($result);
 
+        /** @var Licence $licence */
+        $licence = $this->getRepo('Licence')->fetchById($command->getId());
+        $status = $this->getRepo()->getRefdataReference($command->getStatus());
+        $licence->setStatus($status);
+
+        $this->getRepo('Licence')->save($licence);
+
+        $this->result->addMessage('Licence ' . $licence->getId() . ' surrender withdrawn');
+
+        return $this->result;
     }
 }
