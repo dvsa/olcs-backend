6befaf93af3fe1612c8dd0f6f2f5db073fc69b8d
Piotr Filak <piotr.filak@capgemini.com>
2019-02-27 11:40:05 +0000

    OLCS-23630 - Unable to create ECMT application for Euro 5 for internal or self service

--- app/api/test/module/Api/src/Domain/Repository/IrhpPermitWindowTest.php
+++ app/api/test/module/Api/src/Domain/Repository/IrhpPermitWindowTest.php
@@ -295,4 +295,64 @@ class IrhpPermitWindowTest extends RepositoryTestCase
 
         $this->assertEquals($expectedQuery, $this->query);
     }
+
+    public function testFetchLastOpenWindowByIrhpPermitType()
+    {
+        $now = new \DateTime('2018-10-25 13:21:10');
+
+        $qb = $this->createMockQb('BLAH');
+
+        $this->mockCreateQueryBuilder($qb);
+
+        $qb->shouldReceive('getQuery')->andReturn(
+            m::mock()->shouldReceive('execute')
+                ->shouldReceive('getResult')
+                ->andReturn(['RESULTS'])
+                ->getMock()
+        );
+        $this->assertEquals(
+            'RESULTS',
+            $this->sut->fetchLastOpenWindowByIrhpPermitType(
+                IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT,
+                $now
+            )
+        );
+
+        $expectedQuery = 'BLAH '
+            . 'SELECT ipw '
+            . 'INNER JOIN ipw.irhpPermitStock ips '
+            . 'INNER JOIN ips.irhpPermitType ipt '
+            . 'AND ipt.id = [['.IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT.']] '
+            . 'AND ipw.startDate <= [[2018-10-25T13:21:10+0000]] '
+            . 'AND ipw.endDate > [[2018-10-25T13:21:10+0000]] '
+            . 'ORDER BY ipw.endDate DESC '
+            . 'LIMIT 1';
+
+        $this->assertEquals($expectedQuery, $this->query);
+    }
+
+    /**
+     * @expectedException \Dvsa\Olcs\Api\Domain\Exception\NotFoundException
+     * @expectedExceptionMessage No window available.
+     */
+    public function testFetchLastOpenWindowByIrhpPermitTypeWhenNoWindowOpen()
+    {
+        $now = new \DateTime('2018-10-25 13:21:10');
+
+        $qb = $this->createMockQb('BLAH');
+
+        $this->mockCreateQueryBuilder($qb);
+
+        $qb->shouldReceive('getQuery')->andReturn(
+            m::mock()->shouldReceive('execute')
+                ->shouldReceive('getResult')
+                ->andReturn([])
+                ->getMock()
+        );
+
+        $this->sut->fetchLastOpenWindowByIrhpPermitType(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT,
+            $now
+        );
+    }
 }
