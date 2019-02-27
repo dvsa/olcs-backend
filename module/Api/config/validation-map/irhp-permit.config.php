c1a59f2aaeb0030aa32556d23279fc59e04a34d0
Piotr Filak <piotr.filak@capgemini.com>
2019-02-27 16:19:56 +0000

    OLCS-23295 - Administrator prints Annual Bilateral permits

--- module/Api/config/validation-map/irhp-permit.config.php
+++ module/Api/config/validation-map/irhp-permit.config.php
@@ -11,6 +11,8 @@ return [
     QueryHandler\IrhpPermit\GetList::class => IsInternalUser::class,
     QueryHandler\IrhpPermit\GetListByEcmtId::class => IsInternalUser::class,
     QueryHandler\IrhpPermit\GetListByIrhpId::class => IsInternalUser::class,
+    CommandHandler\IrhpPermit\GenerateCoverLetterDocument::class => IsSideEffect::class,
+    CommandHandler\IrhpPermit\GeneratePermitDocument::class => IsSideEffect::class,
     CommandHandler\IrhpPermit\Replace::class => IsInternalUser::class,
     CommandHandler\IrhpPermit\CreateReplacement::class => IsSideEffect::class,
     QueryHandler\IrhpPermit\ByPermitNumber::class => NotIsAnonymousUser::class,
