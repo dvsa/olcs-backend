a08fded22549f5a1996975d543d04f14e4498112
Ian Lindsay <ian@hemera-business-services.co.uk>
2019-03-04 17:29:32 +0000

    olcs-21964 abstract delete command handler improvement

--- app/api/module/Api/src/Domain/CommandHandler/IrhpPermitRange/Delete.php
+++ app/api/module/Api/src/Domain/CommandHandler/IrhpPermitRange/Delete.php
@@ -2,11 +2,8 @@
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\IrhpPermitRange;
 
-use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
-use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Domain\Exception\NotFoundException;
-use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 
@@ -15,37 +12,11 @@ use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
  *
  * @author Scott Callaway <scott.callaway@capgemini.com>
  */
-final class Delete extends AbstractCommandHandler implements ToggleRequiredInterface
+final class Delete extends AbstractDeleteCommandHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
     protected $toggleConfig = [FeatureToggle::ADMIN_PERMITS];
     protected $repoServiceName = 'IrhpPermitRange';
-
-    /**
-     * Delete Command Handler
-     *
-     * @param CommandInterface $command
-     * @return \Dvsa\Olcs\Api\Domain\Command\Result
-     * @throws ValidationException
-     */
-    public function handleCommand(CommandInterface $command)
-    {
-        $id = $command->getId();
-        $range = $this->getRepo()->fetchById($id);
-
-        if (!$range->canDelete()) {
-            throw new ValidationException(['irhp-permit-range-cannot-delete-active-dependencies']);
-        }
-
-        try {
-            $this->getRepo()->delete($range);
-            $this->result->addId('id', $id);
-            $this->result->addMessage(sprintf('Permit Range Deleted', $id));
-        } catch (NotFoundException $e) {
-            $this->result->addMessage(sprintf('Id %d not found', $id));
-        }
-
-        return $this->result;
-    }
+    protected $extraError = 'irhp-permit-range-cannot-delete-active-dependencies';
 }
