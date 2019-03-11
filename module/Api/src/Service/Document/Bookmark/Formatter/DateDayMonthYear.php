bdca40e316129b014286b81d482fcc72ce25bdb2
Henry White <henry.white@capgemini.com>
2019-03-11 12:13:09 +0000

    OLCS-23665: Review comments

--- module/Api/src/Service/Document/Bookmark/Formatter/DateDayMonthYear.php
+++ module/Api/src/Service/Document/Bookmark/Formatter/DateDayMonthYear.php
@@ -3,7 +3,7 @@
 namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Formatter;
 
 /**
- * Date2 formatter
+ * DateDayMonthYear formatter
  *
  * Example: 01 January 1970
  */
@@ -11,6 +11,6 @@ class DateDayMonthYear implements FormatterInterface
 {
     public static function format(array $data)
     {
-        return date("d F Y", strtotime(reset($data)));
+        return date('d F Y', strtotime(reset($data)));
     }
 }
