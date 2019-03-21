bca6e8c42b3de51bc3d735be2e56cb4806f43eaf
Parth <parth.vyas@dvsa.gov.uk>
2019-03-21 16:04:06 +0000

    OLCS-23724: Add interim fees to refund queue when interim refused.

--- app/api/module/Api/src/Domain/CommandHandler/Application/RefuseInterim.php
+++ app/api/module/Api/src/Domain/CommandHandler/Application/RefuseInterim.php
@@ -5,6 +5,10 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\Application;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
+use Dvsa\Olcs\Api\Entity\Fee\Fee;
+use Dvsa\Olcs\Api\Entity\Fee\FeeType;
+use Dvsa\Olcs\Api\Domain\Command\Queue\Create;
+use Dvsa\Olcs\Api\Entity\Queue\Queue;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Dvsa\Olcs\Api\Entity\Application\Application as ApplicationEntity;
 use Dvsa\Olcs\Transfer\Command\Application\RefuseInterim as Cmd;
@@ -30,7 +34,7 @@ final class RefuseInterim extends AbstractCommandHandler implements Transactione
     {
         /** @var ApplicationEntity $application */
         $application = $this->getRepo()->fetchUsingId($command);
-
+        
         $status = $this->getRepo()->getRefdataReference(ApplicationEntity::INTERIM_STATUS_REFUSED);
         $application->setInterimStatus($status);
         $application->setInterimEnd(new DateTime());
@@ -39,6 +43,25 @@ final class RefuseInterim extends AbstractCommandHandler implements Transactione
 
         $this->getRepo()->save($application);
 
+        $this->maybeRefundInterimFee($application);
+
         return $this->result;
+    }
+
+    private function maybeRefundInterimFee($application)
+    {
+        /** @var Fee $fee */
+        foreach ($application->getFees() as $fee) {
+            if ($fee->canRefund() && $fee->getFeeType()->getFeeType()->getId() === FeeType::FEE_TYPE_GRANTINT) {
+                $createCommand = Create::create(
+                    [
+                        'entityId' => $fee->getId(),
+                        'type' => Queue::TYPE_REFUND_INTERIM_FEES,
+                        'status' => Queue::STATUS_QUEUED,
+                    ]
+                );
+                $this->result->merge($this->handleSideEffect($createCommand));
+            }
+        }
     }
 }
