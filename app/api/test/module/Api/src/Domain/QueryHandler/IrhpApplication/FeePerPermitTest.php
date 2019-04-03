6e729576f6e02b8d5db4cd4dbf121248c1e35290
Jonathan Thomas <jonathan@opalise.co.uk>
2019-04-03 08:38:49 +0100

    OLCS-23014 ss annual multilateral permits number of permits page

--- app/api/test/module/Api/src/Domain/QueryHandler/IrhpApplication/FeePerPermitTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/IrhpApplication/FeePerPermitTest.php
@@ -24,38 +24,53 @@ class FeePerPermitTest extends QueryHandlerTestCase
         parent::setUp();
     }
 
-    public function testHandleQuery()
+    public function testHandleQueryBilateral()
     {
         $irhpApplicationId = 52;
-        $applicationFeeTypeProductRef = 'applicationFeeTypeProductRef';
-        $issueFeeTypeProductRef = 'issueFeeTypeProductRef';
-        $feePerPermit = 50;
 
         $applicationFeeType = m::mock(FeeType::class);
+        $applicationFeeType->shouldReceive('getFixedValue')
+            ->andReturn(45);
+
         $issueFeeType = m::mock(FeeType::class);
+        $issueFeeType->shouldReceive('getFixedValue')
+            ->andReturn(27);
 
         $irhpApplication = m::mock(IrhpApplication::class);
-        $irhpApplication->shouldReceive('getApplicationFeeTypeProductReference')
-            ->andReturn($applicationFeeTypeProductRef);
-        $irhpApplication->shouldReceive('getIssueFeeTypeProductReference')
-            ->andReturn($issueFeeTypeProductRef);
-        $irhpApplication->shouldReceive('getFeePerPermit')
-            ->with($applicationFeeType, $issueFeeType)
-            ->andReturn($feePerPermit);
+        $irhpApplication->shouldReceive('getIrhpPermitType->getId')
+            ->andReturn(4);
 
         $this->repoMap['IrhpApplication']->shouldReceive('fetchById')
             ->with($irhpApplicationId)
             ->andReturn($irhpApplication);
 
         $this->repoMap['FeeType']->shouldReceive('getLatestByProductReference')
-            ->with($applicationFeeTypeProductRef)
+            ->with(FeeType::FEE_TYPE_IRHP_APP_BILATERAL_PRODUCT_REF)
             ->andReturn($applicationFeeType);
 
         $this->repoMap['FeeType']->shouldReceive('getLatestByProductReference')
-            ->with($issueFeeTypeProductRef)
+            ->with(FeeType::FEE_TYPE_IRHP_ISSUE_BILATERAL_PRODUCT_REF)
             ->andReturn($issueFeeType);
 
-        $expectedResult = ['feePerPermit' => $feePerPermit];
+        $expectedResult = ['feePerPermit' => 72];
+        $result = $this->sut->handleQuery(FeePerPermitQry::create(['id' => $irhpApplicationId]));
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testHandleQueryNotBilateral()
+    {
+        $irhpApplicationId = 52;
+
+        $irhpApplication = m::mock(IrhpApplication::class);
+        $irhpApplication->shouldReceive('getIrhpPermitType->getId')
+            ->andReturn(5);
+
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchById')
+            ->with($irhpApplicationId)
+            ->andReturn($irhpApplication);
+
+        $expectedResult = ['feePerPermit' => 'Not applicable'];
         $result = $this->sut->handleQuery(FeePerPermitQry::create(['id' => $irhpApplicationId]));
 
         $this->assertEquals($expectedResult, $result);
