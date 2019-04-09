9cbb15f00d4abff4e4f18175c54ae46b3f5f90e3
Andy Newton <andy@vitri.ltd>
2019-04-09 15:44:39 +0100

    OLCS 23008 - icw saves multilateral

---
+++ app/api/test/module/Api/src/Domain/QueryHandler/IrhpPermitWindow/OpenByTypeTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\IrhpPermitWindow;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Mockery as m;
+use Dvsa\Olcs\Api\Domain\QueryHandler\IrhpPermitWindow\OpenByType as QueryHandler;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpPermitWindow as PermitWindowRepo;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitWindow as PermitWindowEntity;
+use Dvsa\Olcs\Transfer\Query\IrhpPermitWindow\OpenByType as ListQuery;
+
+/**
+ * Open By Type Test
+ * @author Andy Newton <andy@vitri.ltd>
+ */
+class OpenByTypeTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new QueryHandler();
+        $this->mockRepo('IrhpPermitWindow', PermitWindowRepo::class);
+
+        parent::setUp();
+    }
+
+    public function testHandleCommand()
+    {
+        $query = ListQuery::create(['irhpPermitType' => 5, 'currentDateTime' => '2020-05-01 10:10:20']);
+
+        $item1 = m::mock(PermitWindowEntity::class)->makePartial();
+        $item2 = m::mock(PermitWindowEntity::class)->makePartial();
+        $permitWindows = new ArrayCollection();
+        $permitWindows->add($item1);
+        $permitWindows->add($item2);
+
+        $this->repoMap['IrhpPermitWindow']
+            ->shouldReceive('fetchOpenWindowsByType')
+            ->once()
+            ->andReturn($permitWindows);
+
+        $result = $this->sut->handleQuery($query);
+
+        $expected = [
+            'result' => [
+                0 => [],
+                1 => []
+            ]
+        ];
+
+        $this->assertArraySubset($expected, $result);
+    }
+}
