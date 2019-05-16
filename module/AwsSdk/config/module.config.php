38387f49a0ba5209eee3b6be5b6418b51de6673c
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-05-16 13:51:05 +0100

    WIP s3 changes

--- module/AwsSdk/config/module.config.php
+++ module/AwsSdk/config/module.config.php
@@ -8,7 +8,5 @@ return [
             'S3Client' =>\Dvsa\Olcs\AwsSdk\S3ClientFactory::class,
             'AwsCredentialsProvider' => \Dvsa\Olcs\AwsSdk\AwsCredentailsProviderFactory::class
             ],
-        'aliases' => [
-        ],
     ]
 ];
