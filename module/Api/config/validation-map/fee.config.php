b180fac18b6a3d2fbf4a42da3b5ec8c4a57d4111
James Hodgson <james@tdchodgy.co.uk>
2019-05-31 15:02:00 +0100

    Hook up query handler

--- module/Api/config/validation-map/fee.config.php
+++ module/Api/config/validation-map/fee.config.php
@@ -9,4 +9,5 @@ return [
     CommandHandler\Fee\CreateOverpaymentFee::class => IsInternalUser::class,
     CommandHandler\Fee\ResetFees::class => IsInternalUser::class,
     QueryHandler\Fee\Fee::class => CanAccessFeeWithId::class,
+    QueryHandler\Fee\InterimRefunds::class => IsInternalUser::class,
 ];
