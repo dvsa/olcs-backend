8f8fdc55bdad0c303ad39d1b71213d1969c853cf
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-06-28 07:21:19 +0100

    remove role

--- module/AwsSdk/src/Factories/AwsCredentialsProviderFactory.php
+++ module/AwsSdk/src/Factories/AwsCredentialsProviderFactory.php
@@ -26,28 +26,8 @@ class AwsCredentialsProviderFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $config = $serviceLocator->get('Config');
-
-
         $credentialsProvidor = CredentialProvider::instanceProfile();
         $provider = CredentialProvider::memoize($credentialsProvidor);
         return $provider;
-    }
-
-    private function getSpecificRole( array $config, $role)
-    {
-
-        $profile = new InstanceProfileProvider();
-        return new AssumeRoleCredentialProvider([
-            'client' => new StsClient([
-                'region' => $config['awsOptions']['region'],
-                'version' => $config['awsOptions']['version'],
-                'credentials' => $profile
-            ]),
-            'assume_role_params' => [
-                'RoleArn' => $config['awsOptions'][$role]['roleArn'],
-                'RoleSessionName' => $config['awsOptions'][$role]['roleSessionName'],
-            ]
-        ]);
     }
 }
