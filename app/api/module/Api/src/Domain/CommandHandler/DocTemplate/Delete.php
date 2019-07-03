857fd23506fb09a5538c85594a1aed6f94c937f3
Andy Newton <andy@vitri.ltd>
2019-07-03 09:36:14 +0100

    OLCS-24778 - Delete doc template bug

--- app/api/module/Api/src/Domain/CommandHandler/DocTemplate/Delete.php
+++ app/api/module/Api/src/Domain/CommandHandler/DocTemplate/Delete.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Delete a Document Template
  *
@@ -7,9 +6,67 @@
  */
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\DocTemplate;
 
-use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractDeleteCommandHandler;
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactioningCommandHandler;
+use Dvsa\Olcs\Api\Entity\Doc\DocTemplate;
+use Dvsa\Olcs\Api\Service\File\ContentStoreFileUploader;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
-final class Delete extends AbstractDeleteCommandHandler
+final class Delete extends AbstractCommandHandler implements TransactionedInterface
 {
     protected $repoServiceName = 'DocTemplate';
+
+    protected $extraRepos = ['Document'];
+
+    /**
+     * @var ContentStoreFileUploader
+     */
+    private $fileUploader;
+
+    /**
+     * Creates service (needs instance of file uploader)
+     *
+     * @param ServiceLocatorInterface $serviceLocator service locator
+     *
+     * @return TransactioningCommandHandler
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        /** @var ServiceLocatorInterface $mainServiceLocator  */
+        $mainServiceLocator = $serviceLocator->getServiceLocator();
+
+        $this->fileUploader = $mainServiceLocator->get('FileUploader');
+
+        return parent::createService($serviceLocator);
+    }
+
+    /**
+     * Deletes a document template and document record and file
+     *
+     * @param CommandInterface $command command
+     *
+     * @return Result
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        /** @var DocTemplate $docTemplate */
+        $docTemplate = $this->getRepo()->fetchUsingId($command);
+        $identifier = $docTemplate->getDocument()->getIdentifier();
+
+        if (!empty($identifier)) {
+            $this->fileUploader->remove($identifier);
+            $this->result->addMessage('File removed');
+        }
+
+        $this->getRepo('Document')->delete($docTemplate->getDocument());
+        $this->result->addMessage('Document record deleted');
+
+        $this->getRepo()->delete($docTemplate);
+        $this->result->addMessage('DocTemplate record deleted');
+
+        return $this->result;
+    }
 }
--- app/api/test/module/Api/src/Domain/CommandHandler/DocTemplate/DeleteTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/DocTemplate/DeleteTest.php
@@ -2,22 +2,117 @@
 
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\DocTemplate;
 
-use Dvsa\Olcs\Api\Domain\CommandHandler\DocTemplate\Delete as DeleteHandler;
-use Dvsa\Olcs\Api\Domain\Repository\DocTemplate as DocTemplateRepo;
-use Dvsa\Olcs\Transfer\Command\DocTemplate\Delete as DeleteCmd;
+use Dvsa\Olcs\Api\Domain\CommandHandler\DocTemplate\Delete;
+use Dvsa\Olcs\Api\Domain\Repository\DocTemplate;
+use Dvsa\Olcs\Api\Domain\Repository\Document;
 use Dvsa\Olcs\Api\Entity\Doc\DocTemplate as DocTemplateEntity;
-use Dvsa\OlcsTest\Api\Domain\CommandHandler\AbstractDeleteCommandHandlerTest;
+use Dvsa\Olcs\Api\Entity\Doc\Document as DocumentEntity;
+use Dvsa\Olcs\Transfer\Command\Document\DeleteDocument as Cmd;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Mockery as m;
 
 /**
- * Delete DocTemplate test
+ * Delete DocumentTemplate Test
  *
  * @author Andy Newton <andy@vitri.ltd>
  */
-class DeleteTest extends AbstractDeleteCommandHandlerTest
+class DeleteTest extends CommandHandlerTestCase
 {
-    protected $cmdClass = DeleteCmd::class;
-    protected $sutClass = DeleteHandler::class;
-    protected $repoServiceName = 'DocTemplate';
-    protected $repoClass = DocTemplateRepo::class;
-    protected $entityClass = DocTemplateEntity::class;
+    public function setUp()
+    {
+        $this->sut = new Delete();
+        $this->mockRepo('DocTemplate', DocTemplate::class);
+        $this->mockRepo('Document', Document::class);
+
+        $this->mockedSmServices = [
+            'FileUploader' => m::mock()
+        ];
+
+        parent::setUp();
+    }
+
+    public function testHandleCommand()
+    {
+        $docTemplateId = 123;
+        $command = Cmd::create(['id' => $docTemplateId]);
+
+        /** @var DocumentEntity $document */
+        $document = m::mock(DocumentEntity::class)->makePartial();
+        $document->setIdentifier('DocIdentifier');
+
+
+        /** @var DocTemplateEntity $docTemplate */
+        $docTemplate = m::mock(DocTemplateEntity::class)->makePartial();
+        $docTemplate->setDocument($document);
+        $docTemplate->setId($docTemplateId);
+
+        $this->mockedSmServices['FileUploader']->shouldReceive('remove')
+            ->once()
+            ->with('DocIdentifier');
+
+        $this->repoMap['DocTemplate']->shouldReceive('fetchUsingId')
+            ->with($command)
+            ->andReturn($docTemplate)
+            ->shouldReceive('delete')
+            ->once()
+            ->with($docTemplate);
+
+        $this->repoMap['Document']
+            ->shouldReceive('delete')
+            ->once()
+            ->with($document);
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'File removed',
+                'Document record deleted',
+                'DocTemplate record deleted'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    public function testHandleCommandEmptyIdentifier()
+    {
+        $docTemplateId = 123;
+        $command = Cmd::create(['id' => $docTemplateId]);
+
+        /** @var DocumentEntity $document */
+        $document = m::mock(DocumentEntity::class)->makePartial();
+        $document->setIdentifier(null);
+
+
+        /** @var DocTemplateEntity $docTemplate */
+        $docTemplate = m::mock(DocTemplateEntity::class)->makePartial();
+        $docTemplate->setDocument($document);
+        $docTemplate->setId($docTemplateId);
+
+        $this->repoMap['DocTemplate']->shouldReceive('fetchUsingId')
+            ->with($command)
+            ->andReturn($docTemplate)
+            ->shouldReceive('delete')
+            ->once()
+            ->with($docTemplate);
+
+        $this->repoMap['Document']
+            ->shouldReceive('delete')
+            ->once()
+            ->with($document);
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'Document record deleted',
+                'DocTemplate record deleted'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
 }
