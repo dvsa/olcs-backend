dd02857e20df4a45b854ee5a063c4865e1816479
Jonathan Thomas <jonathan@opalise.co.uk>
2019-08-28 13:56:53 +0100

    OLCS-25243 ss short term ecmt - allow concurrent applications for different stocks

--- app/api/test/module/Api/src/Domain/QueryHandler/IrhpApplication/ActiveApplicationTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/IrhpApplication/ActiveApplicationTest.php
@@ -6,7 +6,8 @@ use Dvsa\Olcs\Api\Domain\QueryHandler\BundleSerializableInterface;
 use Dvsa\Olcs\Api\Domain\QueryHandler\IrhpApplication\ActiveApplication as ActiveApplicationHandler;
 use Dvsa\Olcs\Api\Domain\Repository\IrhpApplication as IrhpApplicationRepo;
 use Dvsa\Olcs\Transfer\Query\IrhpApplication\ActiveApplication as QryClass;
-use Dvsa\Olcs\Api\Entity\Permits\IrhpApplication as IrhpApplicationEntity;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpApplication as IrhpApplication;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitType as IrhpPermitType;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Mockery as m;
 
@@ -15,106 +16,258 @@ use Mockery as m;
  */
 class ActiveApplicationTest extends QueryHandlerTestCase
 {
-    protected $sutClass = ActiveApplicationHandler::class;
-    protected $sutRepo = 'IrhpApplication';
     protected $bundle = [
         'licence',
         'irhpPermitType',
     ];
-    protected $qryClass = QryClass::class;
-    protected $repoClass = IrhpApplicationRepo::class;
-    protected $entityClass = IrhpApplicationEntity::class;
 
     /**
      * Set up test
      */
     public function setUp()
     {
-        $this->sut = new $this->sutClass();
-        $this->mockRepo($this->sutRepo, $this->repoClass);
+        $this->sut = new ActiveApplicationHandler();
+
+        $this->mockRepo('IrhpApplication', IrhpApplicationRepo::class);
+
         parent::setUp();
     }
 
-    /**
-     * tests handle query
-     */
-    public function testHandleQuery()
+    public function testHandleQueryWithYear()
     {
-        $licence = 1;
-        $irhpPermitType = 2;
+        $licenceId = 7;
+        $irhpPermitTypeId = IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM;
+        $year = 2019;
 
-        $query = $this->qryClass::create(['licence' => $licence, 'irhpPermitType' => $irhpPermitType]);
+        $irhpApplication1 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_REMOVAL,
+            false,
+            2021
+        );
 
-        $resultArray = [
-            0 => [
-                'id' => 3,
-                'licence' => ['id' => $licence],
-                'irhpPermitType' => ['id' => $irhpPermitType]
-            ],
+        $irhpApplication2 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL,
+            true,
+            2020
+        );
+
+        $irhpApplication3 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM,
+            false,
+            2022
+        );
+
+        $irhpApplication4 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM,
+            true,
+            2020
+        );
+
+        $irhpApplication5 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM,
+            true,
+            2019
+        );
+
+        $irhpApplication6 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_MULTILATERAL,
+            false,
+            2021
+        );
+
+        $serializedIrhpApplication5 = [
+            'property1' => 'value1',
+            'property2' => 'value2',
+            'property3' => 'value3',
         ];
 
-        $mockEntity = m::mock($this->entityClass)
-            ->shouldReceive('serialize')
+        $irhpApplication5->shouldReceive('serialize')
             ->once()
             ->with($this->bundle)
-            ->andReturn($resultArray[0])->getMock();
+            ->andReturn($serializedIrhpApplication5);
 
-        $mockEntity->shouldReceive('getIrhpPermitType->getId')->andReturn(2);
+        $irhpApplications = [
+            $irhpApplication1,
+            $irhpApplication2,
+            $irhpApplication3,
+            $irhpApplication4,
+            $irhpApplication5,
+            $irhpApplication6,
+        ];
 
-        $mockEntity->shouldReceive('isActive')->andReturn(true);
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchByLicence')
+            ->with($licenceId)
+            ->andReturn($irhpApplications);
 
-        $this->repoMap['IrhpApplication']
-            ->shouldReceive('fetchByLicence')
-            ->with($licence)
-            ->once()
-            ->andReturn([$mockEntity]);
+        $query = QryClass::create(
+            [
+                'licence' => $licenceId,
+                'irhpPermitType' => $irhpPermitTypeId,
+                'year' => $year,
+            ]
+        );
 
-        $result = $this->sut->handleQuery($query)->serialize();
-
-        $this->assertEquals($irhpPermitType, $result['irhpPermitType']['id']);
-        $this->assertEquals($licence, $result['licence']['id']);
+        $serializedResult = $this->sut->handleQuery($query)->serialize();
+        $this->assertEquals($serializedIrhpApplication5, $serializedResult);
     }
 
-    public function testNull() {
-        $licence = 1;
-        $irhpPermitType = 2;
-
-        $query = $this->qryClass::create(['licence' => $licence, 'irhpPermitType' => $irhpPermitType]);
-
-        $resultArray = [];
-
-        $this->repoMap['IrhpApplication']
-            ->shouldReceive('fetchByLicence')
-            ->with($licence)
-            ->once()
-            ->andReturn($resultArray);
-
-        $result = $this->sut->handleQuery($query);
-
-        $this->assertEquals(null, $result);
-    }
-
-    public function testDifferentType()
+    public function testHandleQueryWithYearNull()
     {
-        $licence = 1;
-        $irhpPermitType = 2;
+        $licenceId = 7;
+        $irhpPermitTypeId = IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM;
+        $year = 2019;
 
-        $query = $this->qryClass::create(['licence' => $licence, 'irhpPermitType' => $irhpPermitType]);
+        $irhpApplication1 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_REMOVAL,
+            false,
+            2021
+        );
 
-        $mockEntity = m::mock($this->entityClass);
+        $irhpApplication2 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL,
+            true,
+            2020
+        );
 
-        $mockEntity->shouldReceive('getIrhpPermitType->getId')->andReturn(3);
+        $irhpApplication3 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM,
+            false,
+            2022
+        );
 
-        $mockEntity->shouldReceive('isActive')->andReturn(true);
+        $irhpApplication4 = $this->createMockApplicationWithYear(
+            IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM,
+            true,
+            2020
+        );
 
-        $this->repoMap['IrhpApplication']
-            ->shouldReceive('fetchByLicence')
-            ->with($licence)
+        $irhpApplication5 = $this->createMockApplicationWithYear(IrhpPermitType::IRHP_PERMIT_TYPE_ID_MULTILATERAL, false, 2021);
+
+        $irhpApplications = [
+            $irhpApplication1,
+            $irhpApplication2,
+            $irhpApplication3,
+            $irhpApplication4,
+            $irhpApplication5,
+        ];
+
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchByLicence')
+            ->with($licenceId)
+            ->andReturn($irhpApplications);
+
+        $query = QryClass::create(
+            [
+                'licence' => $licenceId,
+                'irhpPermitType' => $irhpPermitTypeId,
+                'year' => $year,
+            ]
+        );
+
+        $this->assertNull(
+            $this->sut->handleQuery($query)
+        );
+    }
+
+    public function testHandleQueryWithoutYear()
+    {
+        $licenceId = 7;
+        $irhpPermitTypeId = IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM;
+        $year = 2019;
+
+        $irhpApplication1 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_REMOVAL, false);
+        $irhpApplication2 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL, true);
+        $irhpApplication3 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM, false);
+        $irhpApplication4 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM, true);
+        $irhpApplication5 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL, true);
+
+        $serializedIrhpApplication4 = [
+            'property1' => 'value1',
+            'property2' => 'value2',
+            'property3' => 'value3',
+        ];
+
+        $irhpApplication4->shouldReceive('serialize')
             ->once()
-            ->andReturn([$mockEntity]);
+            ->with($this->bundle)
+            ->andReturn($serializedIrhpApplication4);
 
-        $result = $this->sut->handleQuery($query);
+        $irhpApplications = [
+            $irhpApplication1,
+            $irhpApplication2,
+            $irhpApplication3,
+            $irhpApplication4,
+            $irhpApplication5,
+        ];
 
-        $this->assertEquals(null, $result);
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchByLicence')
+            ->with($licenceId)
+            ->andReturn($irhpApplications);
+
+        $query = QryClass::create(
+            [
+                'licence' => $licenceId,
+                'irhpPermitType' => $irhpPermitTypeId,
+            ]
+        );
+
+        $serializedResult = $this->sut->handleQuery($query)->serialize();
+        $this->assertEquals($serializedIrhpApplication4, $serializedResult);
+    }
+
+    public function testHandleQueryWithoutYearNull()
+    {
+        $licenceId = 7;
+        $irhpPermitTypeId = IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM;
+        $year = 2019;
+
+        $irhpApplication1 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_REMOVAL, false);
+        $irhpApplication2 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL, true);
+        $irhpApplication3 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_ECMT_SHORT_TERM, false);
+        $irhpApplication4 = $this->createMockApplication(IrhpPermitType::IRHP_PERMIT_TYPE_ID_BILATERAL, true);
+
+        $irhpApplications = [
+            $irhpApplication1,
+            $irhpApplication2,
+            $irhpApplication3,
+            $irhpApplication4,
+        ];
+
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchByLicence')
+            ->with($licenceId)
+            ->andReturn($irhpApplications);
+
+        $query = QryClass::create(
+            [
+                'licence' => $licenceId,
+                'irhpPermitType' => $irhpPermitTypeId,
+            ]
+        );
+
+        $this->assertNull(
+            $this->sut->handleQuery($query)
+        );
+    }
+
+    private function createMockApplicationWithYear($irhpPermitType, $isActive, $validityYear)
+    {
+        $irhpApplication = $this->createMockApplication($irhpPermitType, $isActive);
+
+        $irhpApplication->shouldReceive(
+            'getFirstIrhpPermitApplication->getIrhpPermitWindow->getIrhpPermitStock->getValidityYear'
+        )->andReturn($validityYear);
+
+        return $irhpApplication;
+    }
+
+    private function createMockApplication($irhpPermitTypeId, $isActive)
+    {
+        $irhpApplication = m::mock(IrhpApplication::class);
+
+        $irhpApplication->shouldReceive('getIrhpPermitType->getId')
+            ->andReturn($irhpPermitTypeId);
+        $irhpApplication->shouldReceive('isActive')
+            ->andReturn($isActive);
+
+        return $irhpApplication;
     }
 }
