49da4195fbe4cc9ee88358b88ec5b9e72c590d2f
Piotr Filak <piotr.filak@capgemini.com>
2019-10-19 12:37:39 +0100

    OLCS-25736 - SS-Annual ECMT - Pagination on view permit restrictions on Awaiting fee application is not working properly

--- app/api/module/Api/src/Domain/QueryHandler/Permits/UnpaidEcmtPermits.php
+++ app/api/module/Api/src/Domain/QueryHandler/Permits/UnpaidEcmtPermits.php
@@ -2,17 +2,19 @@
 
 namespace Dvsa\Olcs\Api\Domain\QueryHandler\Permits;
 
-use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractListQueryHandler;
+use Doctrine\ORM\Query;
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
 use Dvsa\Olcs\Api\Domain\ToggleAwareTrait;
 use Dvsa\Olcs\Api\Domain\ToggleRequiredInterface;
 use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 /**
  * Get all unpaid permits by application and status
  *
  * @author Tonci Vidovic <tonci.vidovic@capgemini.com>
  */
-final class UnpaidEcmtPermits extends AbstractListQueryHandler implements ToggleRequiredInterface
+class UnpaidEcmtPermits extends AbstractQueryHandler implements ToggleRequiredInterface
 {
     use ToggleAwareTrait;
 
@@ -26,4 +28,36 @@ final class UnpaidEcmtPermits extends AbstractListQueryHandler implements Toggle
             'emissionsCategory',
         ],
     ];
+
+    /**
+     * Handle list query
+     *
+     * @param QueryInterface $query query
+     *
+     * @return array
+     */
+    public function handleQuery(QueryInterface $query)
+    {
+        $repo = $this->getRepo();
+
+        // fetch list of candidate permits
+        $irhpCandidatePermits = $this->resultList(
+            $repo->fetchList($query, Query::HYDRATE_OBJECT),
+            $this->bundle
+        );
+
+        foreach ($irhpCandidatePermits as $i => $irhpCandidatePermit) {
+            // set value of permit number
+            $irhpCandidatePermit['permitNumber'] = ($query->getPage() - 1) * $query->getLimit() + $i + 1;
+
+            // TODO - OLCS-25959 set value of constrained countries
+
+            $irhpCandidatePermits[$i] = $irhpCandidatePermit;
+        }
+
+        return [
+            'result' => $irhpCandidatePermits,
+            'count' => $repo->fetchCount($query)
+        ];
+    }
 }
