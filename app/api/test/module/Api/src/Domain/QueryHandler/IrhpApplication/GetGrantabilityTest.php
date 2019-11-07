2b0a4f3aeb13bba3b8dbf423d0feb9e2d8d23147
Andy Newton <andy@vitri.ltd>
2019-11-07 11:21:17 +0000

    OLCS-25963 - internal pre-grant candidate permit edit actions

---
+++ app/api/test/module/Api/src/Domain/QueryHandler/IrhpApplication/GetGrantabilityTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\IrhpApplication;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\IrhpApplication\GetGrantability as GetGrantabilityHandler;
+use Dvsa\Olcs\Api\Domain\Repository\IrhpApplication as IrhpApplicationRepo;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpApplication as IrhpApplicationEntity;
+use Dvsa\Olcs\Api\Service\Permits\GrantabilityChecker;
+use Dvsa\Olcs\Transfer\Query\IrhpApplication\GetGrantability as QryClass;
+use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
+use Mockery as m;
+
+/**
+ * Get Grantability Test
+ *
+ * @author Andy Newton <andy@vitri.ltd>
+ */
+class GetGrantabilityTest extends QueryHandlerTestCase
+{
+    public function setUp()
+    {
+        $this->sut = new GetGrantabilityHandler();
+        $this->mockRepo('IrhpApplication', IrhpApplicationRepo::class);
+
+        $this->mockedSmServices = [
+            'PermitsGrantabilityChecker' => m::mock(GrantabilityChecker::class),
+        ];
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider dpTestHandleQuery
+     */
+    public function testHandleQuery($isGrantable, $canBeGranted, $expected)
+    {
+        $query = QryClass::create(['id' => 100011]);
+
+        $irhpApplication = m::mock(IrhpApplicationEntity::class);
+
+        $this->repoMap['IrhpApplication']->shouldReceive('fetchById')
+            ->with($query->getId())
+            ->andReturn($irhpApplication);
+
+        $this->mockedSmServices['PermitsGrantabilityChecker']->shouldReceive('isGrantable')
+            ->with($irhpApplication)
+            ->andReturn($isGrantable);
+
+        $irhpApplication->shouldReceive('canBeGranted')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($canBeGranted);
+
+        $this->assertEquals(
+            $expected,
+            $this->sut->handleQuery($query)
+        );
+    }
+
+    public function dpTestHandleQuery()
+    {
+        return [
+            [
+                true,
+                true,
+                [
+                    'grantable' => 1,
+                    'message' => '',
+                ]
+            ],
+            [
+                true,
+                false,
+                [
+                    'grantable' => 0,
+                    'message' => 'IRHP Application can not be granted',
+                ]
+            ],
+            [
+                false,
+                true,
+                [
+                    'grantable' => 0,
+                    'message' => 'Application requests too many permits from a range',
+                ]
+            ]
+        ];
+    }
+}
