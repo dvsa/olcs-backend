4d52546ad76b7107d2654a5e5663c9aae6ec0c64
Andy Newton <andy@vitri.ltd>
2019-12-02 10:14:07 +0000

    OLCS-26259 - Validator too restrictive on some query handlers for internal users

--- module/Api/config/validation-map/irhp-permit-stock.config.php
+++ module/Api/config/validation-map/irhp-permit-stock.config.php
@@ -3,10 +3,11 @@
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalAdmin;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsSystemAdmin;
 
 return [
-    QueryHandler\IrhpPermitStock\ById::class => IsInternalAdmin::class,
+    QueryHandler\IrhpPermitStock\ById::class => IsInternalUser::class,
     QueryHandler\IrhpPermitStock\GetList::class => IsInternalAdmin::class,
     QueryHandler\IrhpPermitStock\GetFormList::class => IsInternalAdmin::class,
     CommandHandler\IrhpPermitStock\Create::class => IsSystemAdmin::class,
