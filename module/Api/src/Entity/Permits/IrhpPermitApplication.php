84970f5a2006eeda7ccbccebbdf03860da1b2ada
Piotr Filak <piotr.filak@capgemini.com>
2020-01-15 14:21:02 +0000

    OLCS-25357 - Drop ecmt_permit_application table

--- module/Api/src/Entity/Permits/IrhpPermitApplication.php
+++ module/Api/src/Entity/Permits/IrhpPermitApplication.php
@@ -27,8 +27,6 @@ use RuntimeException;
  *        @ORM\Index(name="fk_irhp_permit_applications_irhp_permit_windows1_idx",
      *     columns={"irhp_permit_window_id"}),
  *        @ORM\Index(name="fk_irhp_permit_applications_licence1_idx", columns={"licence_id"}),
- *        @ORM\Index(name="fk_irhp_permit_application_ecmt_permit_application1_idx",
-     *     columns={"ecmt_permit_application_id"}),
  *        @ORM\Index(name="fk_irhp_permit_application_sectors_id1_idx", columns={"sectors_id"}),
  *        @ORM\Index(name="irhp_permit_type_ref_data_status_id_fk", columns={"status"}),
  *        @ORM\Index(name="fk_irhp_permit_application_created_by_user_id", columns={"created_by"}),
