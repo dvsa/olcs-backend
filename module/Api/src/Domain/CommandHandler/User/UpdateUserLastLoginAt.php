976145ad98833212540e779f42444021dfb55715
James Wragg <james.wragg@dvsa.gov.uk>
2020-05-27 10:15:12 +0100

    Cleaned up Imports

--- module/Api/src/Domain/CommandHandler/User/UpdateUserLastLoginAt.php
+++ module/Api/src/Domain/CommandHandler/User/UpdateUserLastLoginAt.php
@@ -3,32 +3,13 @@
 /**
  * Update User
  */
+
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\User;
 
-use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
-use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 use Dvsa\Olcs\Api\Domain\Command\Result;
-use Dvsa\Olcs\Api\Domain\Command\Document\GenerateAndStore;
-use Dvsa\Olcs\Api\Domain\Command\Email\SendUserTemporaryPassword as SendUserTemporaryPasswordDto;
-use Dvsa\Olcs\Api\Domain\Command\PrintScheduler\Enqueue as EnqueueFileCommand;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractUserCommandHandler;
-use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
-use Dvsa\Olcs\Api\Domain\Exception\ForbiddenException;
-use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
-use Dvsa\Olcs\Api\Domain\OpenAmUserAwareInterface;
-use Dvsa\Olcs\Api\Domain\OpenAmUserAwareTrait;
-use Dvsa\Olcs\Api\Entity\ContactDetails\ContactDetails;
-use Dvsa\Olcs\Api\Entity\EventHistory\EventHistory;
-use Dvsa\Olcs\Api\Entity\EventHistory\EventHistoryType;
-use Dvsa\Olcs\Api\Entity\Licence\Licence;
-use Dvsa\Olcs\Api\Entity\Organisation\Organisation;
-use Dvsa\Olcs\Api\Entity\User\Permission;
 use Dvsa\Olcs\Api\Entity\User\User;
-use Dvsa\Olcs\Api\Entity\System\Category as CategoryEntity;
-use Dvsa\Olcs\Api\Entity\System\SubCategory as SubCategoryEntity;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use \Dvsa\Olcs\Transfer\Command\User\UpdateUserLastLoginAt as UpdateUserLastLoginAtCommand;
-use Doctrine\ORM\Query;
 
 /**
  * Update User Last Login At
