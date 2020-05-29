6c9c0336f11a7e7695f231d44533140b1075117e
James Wragg <james.wragg@dvsa.gov.uk>
2020-05-29 13:49:09 +0100

    Added Unit Test

--- app/api/test/module/Api/src/Domain/CommandHandler/User/UpdateUserLastLoginAtTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/User/UpdateUserLastLoginAtTest.php
@@ -4,18 +4,78 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\User;
 
 use Dvsa\Olcs\Api\Domain\Repository\User;
 use Dvsa\Olcs\Api\Domain\CommandHandler\User\UpdateUserLastLoginAt as Sut;
+use Dvsa\Olcs\Api\Entity\User\User as UserEntity;
+use Dvsa\Olcs\Api\Rbac\Identity;
+use Dvsa\Olcs\Transfer\Command\User\UpdateUserLastLoginAt as Cmd;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
+use Mockery as m;
+use ZfcRbac\Service\AuthorizationService;
 
 class UpdateUserLastLoginAtTest extends CommandHandlerTestCase
 {
+    const USER_ID = 123456;
+
+    /** @var UpdateUserLastLoginAt|m\Mock sut */
+    protected $sut;
+
     public function setUp()
     {
         $this->sut = new Sut();
         $this->mockRepo('User', User::class);
+
+        $this->mockedSmServices = [
+            AuthorizationService::class => m::mock(AuthorizationService::class)
+        ];
+
+        $this->mockedSmServices[AuthorizationService::class]
+            ->shouldReceive('getIdentity')
+            ->once()
+            ->andReturn($this->getMockIdentity());
+
+        parent::setUp();
     }
 
     public function testUserLastLoginAtIsUpdatedToCurrentTimestamp()
     {
+        $this->repoMap['User']->shouldReceive('save')
+            ->once()
+            ->with(m::on(function ($user) {
+                if ($user->getId() !== self::USER_ID) {
+                    return false;
+                }
+                if (!$user->getLastLoginAt() instanceof \DateTime) {
+                    return false;
+                }
+                return true;
+            }));
 
+        $command = Cmd::create([
+            'secureToken' => 'test'
+        ]);
+        $result = $this->sut->handleCommand($command);
+
+        $expectedResult = [
+            'id' => [
+                'user' => self::USER_ID,
+            ],
+            'messages' => [
+                'User last login at updated successfully'
+            ]
+        ];
+
+        $this->assertEquals($expectedResult, $result->toArray());
+    }
+
+    private function getMockIdentity()
+    {
+        /** @var UserEntity $mockUser */
+        $mockUser = m::mock(UserEntity::class)->makePartial();
+        $mockUser->setId(self::USER_ID);
+        $mockUser->setLastLoginAt(null);
+
+        return m::mock(Identity::class)
+            ->shouldReceive('getUser')
+            ->andReturn($mockUser)
+            ->getMock();
     }
 }
