2648733313742d9aff34a8f64dd54170e5e1b636
Ian Lindsay <ian@hemera-business-services.co.uk>
2020-06-04 14:24:27 +0100

    olcs-27375 add a total column combining application and grant fees

--- module/Api/src/Domain/QueryHandler/Fee/IrhpBilateralByCountry.php
+++ module/Api/src/Domain/QueryHandler/Fee/IrhpBilateralByCountry.php
@@ -19,56 +19,68 @@ class IrhpBilateralByCountry extends AbstractQueryHandler
     private $map = [
         'NO' => [
             'single' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
             'multi' => [
+                'total' => 50,
                 'application' => 5,
                 'grant' => 45
             ],
             'cabotage' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
         ],
         'SE' => [
             'single' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
             'multi' => [
+                'total' => 50,
                 'application' => 5,
                 'grant' => 45
             ],
             'cabotage' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
         ],
         'CH' => [
             'single' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
             'multi' => [
+                'total' => 50,
                 'application' => 5,
                 'grant' => 45
             ],
             'cabotage' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
         ],
         'FR' => [
             'single' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
             'multi' => [
+                'total' => 50,
                 'application' => 5,
                 'grant' => 45
             ],
             'cabotage' => [
+                'total' => 8,
                 'application' => 2,
                 'grant' => 6
             ],
