15be428e3ff383bc6688b603e7f7590cc1a5056e
Jonathan Thomas <jonathan@opalise.co.uk>
2020-06-26 11:12:08 +0100

    OLCS-27393 db design work rework for non eu bilateral permits and mixed stocks

---
+++ app/api/test/module/Api/src/Entity/Permits/IrhpPermitRequestEntityTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dvsa\OlcsTest\Api\Entity\Permits;
+
+use Dvsa\OlcsTest\Api\Entity\Abstracts\EntityTester;
+use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitRequest as Entity;
+
+/**
+ * IrhpPermitRequest Entity Unit Tests
+ *
+ * Initially auto-generated but won't be overridden
+ */
+class IrhpPermitRequestEntityTest extends EntityTester
+{
+    /**
+     * Define the entity to test
+     *
+     * @var string
+     */
+    protected $entityClass = Entity::class;
+}
