7ba5199e2f14a33053305040362ac7b947a510eb
James Wragg <james.wragg@dvsa.gov.uk>
2020-06-29 11:32:48 +0100

    VOL-864: Added exception logging with stack trace around Queue Processing

--- module/Cli/src/Service/Queue/Consumer/AbstractCommandConsumer.php
+++ module/Cli/src/Service/Queue/Consumer/AbstractCommandConsumer.php
@@ -7,6 +7,7 @@ use Dvsa\Olcs\Api\Domain\Exception\Exception as DomainException;
 use Dvsa\Olcs\Api\Domain\Exception\NotReadyException;
 use Dvsa\Olcs\Api\Domain\Exception\NysiisException;
 use Dvsa\Olcs\Api\Domain\Exception\TransxchangeException;
+use Olcs\Logging\Log\Logger;
 use Dvsa\Olcs\Api\Entity\Queue\Queue as QueueEntity;
 use Dvsa\Olcs\Email\Exception\EmailNotSentException;
 use Zend\ServiceManager\Exception\ExceptionInterface as ZendServiceException;
@@ -74,21 +75,29 @@ abstract class AbstractCommandConsumer extends AbstractConsumer
         try {
             $result = $this->handleCommand($command);
         } catch (NotReadyException $e) {
+            Logger::logException($e, \Zend\Log\Logger::WARN);
             return $this->retry($item, $e->getRetryAfter(), $e->getMessage());
         } catch (EmailNotSentException $e) {
+            Logger::logException($e, \Zend\Log\Logger::WARN);
             return $this->retry($item, $this->retryAfter, $e->getMessage());
         } catch (NysiisException $e) {
+            Logger::logException($e, \Zend\Log\Logger::WARN);
             return $this->retry($item, $e->getRetryAfter(), $e->getMessage());
         } catch (DomainException $e) {
+            Logger::logException($e, \Zend\Log\Logger::ERR);
             $message = !empty($e->getMessages()) ? implode(', ', $e->getMessages()) : $e->getMessage();
             return $this->failed($item, $message);
         } catch (ZendServiceException $e) {
+            Logger::logException($e, \Zend\Log\Logger::ERR);
             return $this->handleZendServiceException($item, $e);
         } catch (\Doctrine\ORM\ORMException $e) {
+            Logger::logException($e, \Zend\Log\Logger::ERR);
             return $this->failed($item, $e->getMessage());
         } catch (DBALException $e) {
+            Logger::logException($e, \Zend\Log\Logger::ERR);
             return $this->failed($item, $e->getMessage());
         } catch (\Exception $e) {
+            Logger::logException($e, \Zend\Log\Logger::ERR);
             return $this->failed($item, $e->getMessage());
         }
 
