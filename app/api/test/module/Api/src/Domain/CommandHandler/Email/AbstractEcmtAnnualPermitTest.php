e5ccbad56bbef03477d5546ec9e137b1dda90494
Jonathan Thomas <jonathan@opalise.co.uk>
2020-11-19 17:18:44 +0000

    OLCS-27882 update successful and part successful ecmt emails to handle both euro 5 and euro 6

--- app/api/test/module/Api/src/Domain/CommandHandler/Email/AbstractEcmtAnnualPermitTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Email/AbstractEcmtAnnualPermitTest.php
@@ -8,6 +8,7 @@ use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Dvsa\Olcs\Api\Entity\Fee\Fee;
 use Dvsa\Olcs\Api\Entity\Fee\FeeType;
 use Dvsa\Olcs\Api\Entity\Permits\IrhpPermitApplication;
+use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\Olcs\Email\Data\Message;
 use Dvsa\Olcs\Email\Domain\Command\SendEmail;
 use Dvsa\Olcs\Email\Service\TemplateRenderer;
@@ -152,8 +153,12 @@ abstract class AbstractEcmtAnnualPermitTest extends AbstractPermitTest
      */
     public function testHandleCommandAwaitingFee($licenceTranslateToWelsh, $expectedLocale)
     {
-        $permitsRequired = 2;
-        $permitsAwarded = 2;
+        $permitsRequired = 8;
+        $permitsAwarded = 6;
+        $euro5PermitsRequired = 3;
+        $euro5PermitsAwarded = 2;
+        $euro6PermitsRequired = 5;
+        $euro6PermitsAwarded = 4;
         $issueFeeAmount = 123;
         $permitsGrantedissueFeeTotal = $permitsRequired * $issueFeeAmount;
         $paymentDeadlineNumDays = '10';
@@ -169,7 +174,11 @@ abstract class AbstractEcmtAnnualPermitTest extends AbstractPermitTest
             'applicationRef' => $this->applicationRef,
             'awaitingFeeUrl' => $awaitingFeeUrl,
             'permitsRequired' => $permitsRequired,
-            'permitsGranted' => $permitsRequired,
+            'permitsGranted' => $permitsAwarded,
+            'euro5PermitsRequired' => $euro5PermitsRequired,
+            'euro6PermitsRequired' => $euro6PermitsRequired,
+            'euro5PermitsGranted' => $euro5PermitsAwarded,
+            'euro6PermitsGranted' => $euro6PermitsAwarded,
             'paymentDeadlineNumDays' => $paymentDeadlineNumDays,
             'issueFeeDeadlineDate' => $issueFeeDeadlineDate,
             'issueFeeAmount' => $issueFeeAmount,
@@ -179,29 +188,53 @@ abstract class AbstractEcmtAnnualPermitTest extends AbstractPermitTest
         ];
 
         $irhpPermitApplication = m::mock(IrhpPermitApplication::class);
-        $irhpPermitApplication->shouldReceive('first')->andReturn($irhpPermitApplication);
-        $irhpPermitApplication->shouldReceive('calculateTotalPermitsRequired')->andReturn($permitsRequired);
-        $irhpPermitApplication->shouldReceive('countPermitsAwarded')->andReturn($permitsAwarded);
+        $irhpPermitApplication->shouldReceive('calculateTotalPermitsRequired')
+            ->withNoArgs()
+            ->andReturn($permitsRequired);
+        $irhpPermitApplication->shouldReceive('countPermitsAwarded')
+            ->withNoArgs()
+            ->andReturn($permitsAwarded);
+        $irhpPermitApplication->shouldReceive('getRequiredEuro5')
+            ->withNoArgs()
+            ->andReturn($euro5PermitsRequired);
+        $irhpPermitApplication->shouldReceive('countPermitsAwarded')
+            ->with(RefData::EMISSIONS_CATEGORY_EURO5_REF)
+            ->andReturn($euro5PermitsAwarded);
+        $irhpPermitApplication->shouldReceive('getRequiredEuro6')
+            ->withNoArgs()
+            ->andReturn($euro6PermitsRequired);
+        $irhpPermitApplication->shouldReceive('countPermitsAwarded')
+            ->with(RefData::EMISSIONS_CATEGORY_EURO6_REF)
+            ->andReturn($euro6PermitsAwarded);
         $irhpPermitApplication->shouldReceive('getIrhpPermitWindow->getIrhpPermitStock->getValidityYear')
+            ->withNoArgs()
             ->andReturn($validityYear);
 
         $fee = m::mock(Fee::class);
-        $fee->shouldReceive('isEcmtIssuingFee')->andReturn(true);
-        $fee->shouldReceive('getFeeTypeAmount')->andReturn($issueFeeAmount);
-        $fee->shouldReceive('getOutstandingAmount')->andReturn($permitsGrantedissueFeeTotal);
-        $fee->shouldReceive('getInvoicedDateTime')->andReturn(
+        $fee->shouldReceive('isEcmtIssuingFee')->withNoArgs()->andReturn(true);
+        $fee->shouldReceive('getFeeTypeAmount')->withNoArgs()->andReturn($issueFeeAmount);
+        $fee->shouldReceive('getOutstandingAmount')->withNoArgs()->andReturn($permitsGrantedissueFeeTotal);
+        $fee->shouldReceive('getInvoicedDateTime')->withNoArgs()->andReturn(
             new DateTime('8 March 2019')
         );
         $fees = [$fee];
 
-        $this->applicationEntity->shouldReceive('getIrhpPermitApplications')->andReturn($irhpPermitApplication);
+        $this->applicationEntity->shouldReceive('getFirstIrhpPermitApplication')
+            ->withNoArgs()
+            ->andReturn($irhpPermitApplication);
+
         $this->applicationEntity->shouldReceive('calculateTotalPermitsRequired')->andReturn($permitsRequired);
         $this->applicationEntity->shouldReceive('getFees->matching')->andReturn($fees);
         $this->applicationEntity->shouldReceive('isAwaitingFee')->once()->withNoArgs()->andReturn(true);
         $this->applicationEntity->shouldReceive('getCreatedBy')->once()->withNoArgs()->andReturn($this->userEntity);
-        $this->applicationEntity->shouldReceive('getLicence->getTranslateToWelsh')->andReturn($licenceTranslateToWelsh);
+        $this->applicationEntity->shouldReceive('getLicence->getTranslateToWelsh')
+            ->withNoArgs()
+            ->andReturn($licenceTranslateToWelsh);
 
-        $this->organisation->shouldReceive('getAdminEmailAddresses')->once()->andReturn($this->orgEmails);
+        $this->organisation->shouldReceive('getAdminEmailAddresses')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($this->orgEmails);
 
         $this->mockedSmServices[TemplateRenderer::class]->shouldReceive('renderBody')->once()->with(
             m::type(Message::class),
