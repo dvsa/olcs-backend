e717a0e0b3627a92b5260893bfcfe826688f3a5a
James Hodgson <james.hodgson@dvsa.gov.uk>
2020-11-25 15:59:31 +0000

    VOL-1180: Auto generate task for first and final letters

--- app/api/module/Api/src/Domain/CommandHandler/Document/CreateLetter.php
+++ app/api/module/Api/src/Domain/CommandHandler/Document/CreateLetter.php
@@ -2,48 +2,76 @@
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Document;
 
+use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\Command\Document\GenerateAndStore as GenerateAndStoreCmd;
+use Dvsa\Olcs\Api\Domain\Command\Task\CreateTask;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler\TransactionedInterface;
 use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Dvsa\Olcs\Api\Entity\Doc\DocTemplate;
+use Dvsa\Olcs\Api\Entity\System\Category;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Dvsa\Olcs\Api\Entity\Doc\DocTemplate as Entity;
-use Dvsa\Olcs\Transfer\Command\Document\CreateLetter as Cmd;
+use Dvsa\Olcs\Transfer\Command\Document\CreateLetter as CreateLetterCommand;
+use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 
 /**
  * Create Application
  *
  * @author Rob Caiger <rob@clocal.co.uk>
  */
-final class CreateLetter extends AbstractCommandHandler implements TransactionedInterface
+final class CreateLetter extends AbstractCommandHandler implements TransactionedInterface, AuthAwareInterface
 {
+    use AuthAwareTrait;
+
+    const DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FIRST = [
+        '/templates/PUB_APPS_SUPP_DOCS_1ST.rtf',
+        '/templates/GB/ENV_PUB_APPS_SUPP_DOCS_1ST.rtf',
+        '/templates/GB/PSV_NEW_APP_SUPP_DOCS_1ST.rtf',
+        '/templates/GB/PSV_VAR_APP_SUPP_DOCS_1ST.rtf',
+        '/templates/NI/PUB_APPS_SUPP_DOCS_1ST.rtf',
+        'templates/GB/Incompletenon-digitalNewapp1strequestforsupportingdocs.rtf',
+        'templates/GB/Incompletenon-digitalVarapp1strequestforsupportingdocs.rtf',
+    ];
+
+    const DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FINAL = [
+        '/templates/GB/PUB_APPS_SUPP_DOCS_FINAL.rtf',
+        '/templates/GB/ENV_PUB_APPS_SUPP_DOCS_FINAL.rtf',
+        '/templates/GB/PSV_NEW_APP_SUPP_DOCS_FINAL.rtf',
+        '/templates/GB/PSV_VAR_APP_SUPP_DOCS_FINAL.rtf',
+        '/templates/NI/PUB_APPS_SUPP_DOCS_FINAL.rtf',
+    ];
+
     protected $repoServiceName = 'DocTemplate';
 
     /**
      * Handle command
      *
-     * @param Cmd $command command
+     * @param CreateLetterCommand $command command
      *
      * @return \Dvsa\Olcs\Api\Domain\Command\Result
      */
     public function handleCommand(CommandInterface $command)
     {
-        /** @var Entity $template */
         $template = $this->getRepo()->fetchById($command->getTemplate());
+        assert($template instanceof DocTemplate, 'Expected instance of DocTemplate');
 
-        return $this->generateDocument($template, $command);
+        $this->result->merge($this->generateDocument($template, $command));
+
+        $this->handleFollowUpTasks($template, $command);
+
+        return $this->result;
     }
 
     /**
      * Generate document
      *
-     * @param Entity $template template
-     * @param Cmd    $command  command
+     * @param DocTemplate $template template
+     * @param CreateLetterCommand $command  command
      *
      * @return \Dvsa\Olcs\Api\Domain\Command\Result
      */
-    protected function generateDocument(DocTemplate $template, Cmd $command)
+    protected function generateDocument(DocTemplate $template, CreateLetterCommand $command)
     {
         $queryData = $command->getData();
 
@@ -64,5 +92,112 @@ final class CreateLetter extends AbstractCommandHandler implements Transactioned
         } catch (\Exception $e) {
             throw new ValidationException([$e->getMessage()]);
         }
+    }
+
+    /**
+     * @param CreateLetterCommand $command
+     * @param DocTemplate $template
+     * @throws \Exception
+     */
+    protected function handleFollowUpTasks(DocTemplate $template, CreateLetterCommand $command)
+    {
+        $this->handleFollowUpTaskForApplicationOrVariationFirstLetter($template, $command);
+        $this->handleFollowUpTaskForApplicationFinalLetter($template, $command);
+    }
+
+    /**
+     * Generates a task for an application or variation when a 1ST letter is created.
+     *
+     * @param CreateLetterCommand $command
+     * @param DocTemplate $template
+     * @throws \Exception
+     */
+    protected function handleFollowUpTaskForApplicationOrVariationFirstLetter(DocTemplate $template, CreateLetterCommand $command)
+    {
+        if (! in_array($template->getDocument()->getIdentifier(), static::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FIRST)) {
+            return;
+        }
+
+        $applicationId = $command->getData()['application'] ?? null;
+        if (is_null($applicationId)) {
+            throw new \Exception('Expected `applicationId` when creating a task for first letter.');
+        }
+
+        $licenceId = $command->getData()['licence'] ?? null;
+        if (is_null($licenceId)) {
+            throw new \Exception('Expected `licenceId` when creating a task for first letter.');
+        }
+
+        $actionDate = new DateTime();
+        $actionDate->add(new \DateInterval('P14D'));
+
+        $currentUser = $this->getCurrentUser()->getId();
+
+        $this->createTask([
+            'category' => Category::CATEGORY_APPLICATION,
+            'subCategory' => Category::TASK_SUB_CATEGORY_APPLICATION_RESPONSE_TO_FIRST_REQUEST,
+            'description' => 'Check response to first letters',
+            'application' => $applicationId,
+            'licence' => $licenceId,
+            'assignedToUser' => $currentUser,
+            'assignedByUser' => $currentUser,
+            'urgent' => 'Y',
+            'actionDate' => $actionDate->format('Y-m-d')
+        ]);
+    }
+
+    /**
+     * Generated a task for an application or variation when a FINAL letter is created.
+     *
+     * @param CreateLetterCommand $command
+     * @param DocTemplate $template
+     * @throws \Exception
+     */
+    protected function handleFollowUpTaskForApplicationFinalLetter(DocTemplate $template, CreateLetterCommand $command)
+    {
+        if (! in_array(
+            $template->getDocument()->getIdentifier(),
+            static::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FINAL
+        )) {
+            return;
+        }
+
+        $applicationId = $command->getData()['application'] ?? null;
+        if (is_null($applicationId)) {
+            throw new \Exception('Expected `applicationId` when creating a task for final letter.');
+        }
+
+        $licenceId = $command->getData()['licence'] ?? null;
+        if (is_null($licenceId)) {
+            throw new \Exception('Expected `licenceId` when creating a task for final letter.');
+        }
+
+        $actionDate = new DateTime();
+        $actionDate->add(new \DateInterval('P14D'));
+
+        $currentUser = $this->getCurrentUser()->getId();
+
+        $this->createTask([
+            'category' => Category::CATEGORY_APPLICATION,
+            'subCategory' => Category::TASK_SUB_CATEGORY_APPLICATION_RESPONSE_TO_FINAL_REQUEST,
+            'description' => 'Check response to final letters',
+            'application' => $applicationId,
+            'licence' => $licenceId,
+            'assignedToUser' => $currentUser,
+            'assignedByUser' => $currentUser,
+            'urgent' => 'Y',
+            'actionDate' => $actionDate->format('Y-m-d')
+        ]);
+    }
+
+    /**
+     * Creates a task.
+     *
+     * @param array $arrayDto
+     */
+    protected function createTask(array $arrayDto)
+    {
+        $taskDto = CreateTask::create($arrayDto);
+        $this->result->merge($this->handleSideEffect($taskDto));
     }
 }
--- app/api/test/module/Api/src/Domain/CommandHandler/Document/CreateLetterTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Document/CreateLetterTest.php
@@ -4,13 +4,17 @@ namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Document;
 
 use Dvsa\Olcs\Api\Domain\Command\Document\GenerateAndStore;
 use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\Command\Task\CreateTask;
 use Dvsa\Olcs\Api\Domain\Repository\DocTemplate;
+use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
+use Dvsa\Olcs\Api\Entity\System\Category;
 use Mockery as m;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Document\CreateLetter;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
 use Dvsa\Olcs\Transfer\Command\Document\CreateLetter as Cmd;
 use Dvsa\Olcs\Api\Entity;
 use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
+use ZfcRbac\Service\AuthorizationService;
 
 /**
  * Create Letter Test
@@ -19,10 +23,16 @@ use Dvsa\Olcs\Api\Domain\Exception\ValidationException;
  */
 class CreateLetterTest extends CommandHandlerTestCase
 {
+    protected const LICENCE_ID = 2;
+    protected const APPLICATION_ID = 4;
+    protected const USER_ID = 123;
+
     public function setUp(): void
     {
         $this->sut = new CreateLetter();
         $this->mockRepo('DocTemplate', DocTemplate::class);
+
+        $this->mockedSmServices[AuthorizationService::class] = m::mock(AuthorizationService::class);
 
         parent::setUp();
     }
@@ -38,32 +48,12 @@ class CreateLetterTest extends CommandHandlerTestCase
             'meta' => 'foo',
             'disableBookmarks' => true
         ];
-
         $command = Cmd::create($data);
-
-        /** @var Entity\Doc\DocTemplate $template */
-        $template = m::mock(Entity\Doc\DocTemplate::class)->makePartial();
-        $template->setDescription('Foo-:Bar_Cake Cheese');
-        $template->shouldReceive('getDocument->getIdentifier')
-            ->andReturn('Foo-Bar_Cake Cheese.rtf');
-
-        $this->repoMap['DocTemplate']->shouldReceive('fetchById')
-            ->with(111)
-            ->andReturn($template);
+        $this->setupDocTemplateRepo('Foo-Bar_Cake Cheese.rtf');
 
         $result = new Result();
         $result->addMessage('GenerateAndStore');
-        $data = [
-            'template' => 'Foo-Bar_Cake Cheese.rtf',
-            'query' => $expectedQueryData,
-            'description' => 'Foo-:Bar_Cake Cheese',
-            'category' => '123',
-            'subCategory' => '321',
-            'isExternal' => false,
-            'isScan' => false,
-            'metadata' => 'foo',
-            'disableBookmarks' => true
-        ];
+        $data = $this->getGenerateAndStoreData('Foo-Bar_Cake Cheese.rtf', $expectedQueryData);
         $this->expectedSideEffect(GenerateAndStore::class, $data, $result);
 
         $result = $this->sut->handleCommand($command);
@@ -78,10 +68,202 @@ class CreateLetterTest extends CommandHandlerTestCase
         $this->assertEquals($expected, $result->toArray());
     }
 
+    /**
+     * @dataProvider followUpTaskForApplicationOrVariationFirstLetterProvider
+     */
+    public function testHandleCommandWithFollowUpTaskForApplicationOrVariationFirstLetter($templateIdentifier)
+    {
+        $this->setupDocTemplateRepo($templateIdentifier);
+        $this->setupAuthService();
+
+        $cmdData = [
+            'template' => 111,
+            'data' => [
+                'details' => [
+                    'category' => '123',
+                    'documentSubCategory' => '321'
+                ],
+                'application' => static::APPLICATION_ID,
+                'licence' => static::LICENCE_ID
+            ],
+            'meta' => 'foo',
+            'disableBookmarks' => true
+        ];
+        $command = Cmd::create($cmdData);
+
+        $expectedQueryData = [
+            'details' => [
+                'category' => '123',
+                'documentSubCategory' => '321'
+            ],
+            'application' => static::APPLICATION_ID,
+            'licence' => static::LICENCE_ID
+        ];
+        $generateAndStoreResult = new Result();
+        $generateAndStoreResult->addMessage('GenerateAndStore');
+        $generateAndStoreData = $this->getGenerateAndStoreData($templateIdentifier, $expectedQueryData);
+        $this->expectedSideEffect(GenerateAndStore::class, $generateAndStoreData, $generateAndStoreResult);
+
+        $createTaskResult = new Result();
+        $createTaskResult->addMessage('CreateTask');
+        $actionDate = new DateTime();
+        $actionDate->add(new \DateInterval('P14D'));
+        $createTaskData = [
+            'category' => Category::CATEGORY_APPLICATION,
+            'subCategory' => Category::TASK_SUB_CATEGORY_APPLICATION_RESPONSE_TO_FIRST_REQUEST,
+            'description' => 'Check response to first letters',
+            'application' => static::APPLICATION_ID,
+            'licence' => static::LICENCE_ID,
+            'assignedToUser' => static::USER_ID,
+            'assignedByUser' => static::USER_ID,
+            'urgent' => 'Y',
+            'actionDate' => $actionDate->format('Y-m-d')
+        ];
+        $this->expectedSideEffect(CreateTask::class, $createTaskData, $createTaskResult);
+
+        $result = $this->sut->handleCommand($command);
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'GenerateAndStore',
+                'CreateTask'
+            ]
+        ];
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    /**
+     * @dataProvider followUpTaskForApplicationOrVariationFirstLetterExceptionProvider
+     */
+    public function testHandleCommandWithFollowUpTaskForApplicationOrVariationFirstLetterException(
+        array $commandData,
+        string $templateIdentifier,
+        string $expectedExceptionMessage
+    ) {
+        $this->setupDocTemplateRepo($templateIdentifier);
+
+        $command = Cmd::create($commandData);
+
+        $generateAndStoreResult = new Result();
+        $generateAndStoreResult->addMessage('GenerateAndStore');
+        $generateAndStoreData = $this->getGenerateAndStoreData($templateIdentifier, $commandData['data']);
+        $this->expectedSideEffect(GenerateAndStore::class, $generateAndStoreData, $generateAndStoreResult);
+
+        $this->expectExceptionMessage($expectedExceptionMessage);
+        $this->expectException(\Exception::class);
+
+        $result = $this->sut->handleCommand($command);
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'GenerateAndStore',
+                'CreateTask'
+            ]
+        ];
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    /**
+     * @dataProvider followUpTaskForApplicationOrVariationFinalLetterProvider
+     */
+    public function testHandleCommandWithFollowUpTaskForApplicationOrVariationFinalLetter($templateIdentifier)
+    {
+        $this->setupDocTemplateRepo($templateIdentifier);
+        $this->setupAuthService();
+
+        $cmdData = [
+            'template' => 111,
+            'data' => [
+                'details' => [
+                    'category' => '123',
+                    'documentSubCategory' => '321'
+                ],
+                'application' => static::APPLICATION_ID,
+                'licence' => static::LICENCE_ID
+            ],
+            'meta' => 'foo',
+            'disableBookmarks' => true
+        ];
+        $command = Cmd::create($cmdData);
+
+        $expectedQueryData = [
+            'details' => [
+                'category' => '123',
+                'documentSubCategory' => '321'
+            ],
+            'application' => static::APPLICATION_ID,
+            'licence' => static::LICENCE_ID
+        ];
+        $generateAndStoreResult = new Result();
+        $generateAndStoreResult->addMessage('GenerateAndStore');
+        $generateAndStoreData = $this->getGenerateAndStoreData($templateIdentifier, $expectedQueryData);
+        $this->expectedSideEffect(GenerateAndStore::class, $generateAndStoreData, $generateAndStoreResult);
+
+        $createTaskResult = new Result();
+        $createTaskResult->addMessage('CreateTask');
+        $actionDate = new DateTime();
+        $actionDate->add(new \DateInterval('P14D'));
+        $createTaskData = [
+            'category' => Category::CATEGORY_APPLICATION,
+            'subCategory' => Category::TASK_SUB_CATEGORY_APPLICATION_RESPONSE_TO_FINAL_REQUEST,
+            'description' => 'Check response to final letters',
+            'application' => static::APPLICATION_ID,
+            'licence' => static::LICENCE_ID,
+            'assignedToUser' => static::USER_ID,
+            'assignedByUser' => static::USER_ID,
+            'urgent' => 'Y',
+            'actionDate' => $actionDate->format('Y-m-d')
+        ];
+        $this->expectedSideEffect(CreateTask::class, $createTaskData, $createTaskResult);
+
+        $result = $this->sut->handleCommand($command);
+
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'GenerateAndStore',
+                'CreateTask'
+            ]
+        ];
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
+    /**
+     * @dataProvider followUpTaskForApplicationOrVariationFirstLetterExceptionProvider
+     */
+    public function testHandleCommandWithFollowUpTaskForApplicationOrVariationFinalLetterException(
+        array $commandData,
+        string $templateIdentifier,
+        string $expectedExceptionMessage
+    ) {
+        $this->setupDocTemplateRepo($templateIdentifier);
+
+        $command = Cmd::create($commandData);
+
+        $generateAndStoreResult = new Result();
+        $generateAndStoreResult->addMessage('GenerateAndStore');
+        $generateAndStoreData = $this->getGenerateAndStoreData($templateIdentifier, $commandData['data']);
+        $this->expectedSideEffect(GenerateAndStore::class, $generateAndStoreData, $generateAndStoreResult);
+
+        $this->expectExceptionMessage($expectedExceptionMessage);
+        $this->expectException(\Exception::class);
+
+        $result = $this->sut->handleCommand($command);
+        $expected = [
+            'id' => [],
+            'messages' => [
+                'GenerateAndStore',
+                'CreateTask'
+            ]
+        ];
+        $this->assertEquals($expected, $result->toArray());
+    }
+
     public function testHandleCommandWithException()
     {
-        //$this->expectException(\Exception::class, 'Error generating document');
-        $this->expectException(ValidationException::class, 'Error generating document');
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('Error generating document');
         $queryData = ['details' => ['category' => '123', 'documentSubCategory' => '321']];
         $expectedQueryData = ['details' => ['category' => '123', 'documentSubCategory' => '321']];
 
@@ -123,5 +305,138 @@ class CreateLetterTest extends CommandHandlerTestCase
             new \Exception('Error generating document')
         );
         $this->sut->handleCommand($command);
+    }
+
+    protected function getGenerateAndStoreData(string $templateIdentifier, array $expectedQueryData): array
+    {
+        $generateAndStoreData = [
+            'template' => $templateIdentifier,
+            'query' => $expectedQueryData,
+            'description' => 'Foo-:Bar_Cake Cheese',
+            'category' => '123',
+            'subCategory' => '321',
+            'isExternal' => false,
+            'isScan' => false,
+            'metadata' => 'foo',
+            'disableBookmarks' => true
+        ];
+        return $generateAndStoreData;
+    }
+
+    protected function setupDocTemplateRepo(string $templateIdentifier): void
+    {
+        /** @var Entity\Doc\DocTemplate $template */
+        $template = m::mock(Entity\Doc\DocTemplate::class)->makePartial();
+        $template->setDescription('Foo-:Bar_Cake Cheese');
+        $template->shouldReceive('getDocument->getIdentifier')
+            ->andReturn($templateIdentifier);
+
+        $this->repoMap['DocTemplate']->shouldReceive('fetchById')
+            ->with(111)
+            ->andReturn($template);
+    }
+
+    protected function setupAuthService(): void
+    {
+        $mockUser = m::mock()
+            ->shouldReceive('getId')
+            ->andReturn(static::USER_ID)
+            ->once()
+            ->getMock();
+        $mockIdentity = m::mock()
+            ->shouldReceive('getUser')
+            ->andReturn($mockUser)
+            ->once()
+            ->getMock();
+        $this->mockedSmServices[AuthorizationService::class]
+            ->shouldReceive('getIdentity')
+            ->andReturn($mockIdentity)
+            ->once();
+    }
+
+    public function followUpTaskForApplicationOrVariationFinalLetterProvider()
+    {
+        return [CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FINAL];
+    }
+
+    public function followUpTaskForApplicationOrVariationFirstLetterProvider()
+    {
+        return [CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FIRST];
+    }
+
+    public function followUpTaskForApplicationOrVariationFirstLetterExceptionProvider()
+    {
+        return [
+            'missing_application' => [
+                'commandData' => [
+                    'template' => 111,
+                    'data' => [
+                        'details' => [
+                            'category' => '123',
+                            'documentSubCategory' => '321'
+                        ],
+                        'licence' => static::LICENCE_ID
+                    ],
+                    'meta' => 'foo',
+                    'disableBookmarks' => true
+                ],
+                'templateIdentifier' => CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FIRST[0],
+                'expectedExceptionMessage' => 'Expected `applicationId` when creating a task for first letter.'
+            ],
+            'missing_licence' => [
+                'commandData' => [
+                    'template' => 111,
+                    'data' => [
+                        'details' => [
+                            'category' => '123',
+                            'documentSubCategory' => '321'
+                        ],
+                        'application' => static::APPLICATION_ID,
+                    ],
+                    'meta' => 'foo',
+                    'disableBookmarks' => true
+                ],
+                'templateIdentifier' => CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FIRST[0],
+                'expectedExceptionMessage' => 'Expected `licenceId` when creating a task for first letter.'
+            ]
+        ];
+    }
+
+    public function followUpTaskForApplicationOrVariationFinalLetterExceptionProvider()
+    {
+        return [
+            'missing_application' => [
+                'commandData' => [
+                    'template' => 111,
+                    'data' => [
+                        'details' => [
+                            'category' => '123',
+                            'documentSubCategory' => '321'
+                        ],
+                        'licence' => static::LICENCE_ID
+                    ],
+                    'meta' => 'foo',
+                    'disableBookmarks' => true
+                ],
+                'templateIdentifier' => CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FINAL[0],
+                'expectedExceptionMessage' => 'Expected `applicationId` when creating a task for final letter.'
+            ],
+            'missing_licence' => [
+                'commandData' => [
+                    'template' => 111,
+                    'data' => [
+                        'details' => [
+                            'category' => '123',
+                            'documentSubCategory' => '321'
+                        ],
+                        'application' => static::APPLICATION_ID,
+                    ],
+                    'meta' => 'foo',
+                    'disableBookmarks' => true
+                ],
+                'templateIdentifier' => CreateLetter::DOCUMENT_TEMPLATE_IDENTIFIERS_FOLLOW_UP_FINAL[0],
+                'expectedExceptionMessage' => 'Expected `licenceId` when creating a task for final letter.'
+            ]
+        ];
     }
 }
