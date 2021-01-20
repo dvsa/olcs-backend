4f8d334ce8604be8f23818b6d399a831a9b48ad6
Jonathan Thomas <jonathan@opalise.co.uk>
2021-01-20 10:57:55 +0000

    OLCS-28192 permits admin hide application paths marked as hidden from the application path drop down

--- app/api/module/Api/src/Domain/Repository/ApplicationPathGroup.php
+++ app/api/module/Api/src/Domain/Repository/ApplicationPathGroup.php
@@ -2,7 +2,10 @@
 
 namespace Dvsa\Olcs\Api\Domain\Repository;
 
+use Doctrine\ORM\QueryBuilder;
 use Dvsa\Olcs\Api\Entity\Generic\ApplicationPathGroup as Entity;
+use Dvsa\Olcs\Transfer\Query\IrhpApplication\ApplicationPathGroupList;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 
 /**
  * Application Path Group
@@ -12,4 +15,20 @@ use Dvsa\Olcs\Api\Entity\Generic\ApplicationPathGroup as Entity;
 class ApplicationPathGroup extends AbstractRepository
 {
     protected $entity = Entity::class;
+
+    /**
+     * Apply List Filters
+     *
+     * @param QueryBuilder   $qb    Doctrine Query Builder
+     * @param QueryInterface $query Http Query
+     *
+     * @return void
+     */
+    protected function applyListFilters(QueryBuilder $qb, QueryInterface $query)
+    {
+        if ($query instanceof ApplicationPathGroupList) {
+            $qb->andWhere($qb->expr()->eq($this->alias . '.isVisibleInInternal', ':isVisibleInInternal'))
+                ->setParameter('isVisibleInInternal', true);
+        }
+    }
 }
