f187c05e6fab6421aad5eec764f01fb7fa8ae525
James Wragg <james.wragg@dvsa.gov.uk>
2021-05-18 12:36:50 +0000

    VOL-1870: Introduced Permits Report endpoint

---
+++ app/api/module/Api/src/Domain/QueryHandler/Permits/Report/ReportList.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Dvsa\Olcs\Api\Domain\QueryHandler\Permits\Report;
+
+use Dvsa\Olcs\Api\Domain\QueryHandler\AbstractQueryHandler;
+use Dvsa\Olcs\Api\Domain\Service\PermitsReportService;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
+
+/**
+ * Permits Report List
+ */
+class ReportList extends AbstractQueryHandler
+{
+    /**
+     * @inheritdoc
+     */
+    public function handleQuery(QueryInterface $query)
+    {
+        return [
+            'result' => $types = PermitsReportService::REPORT_TYPES,
+            'count' => count($types),
+        ];
+    }
+}
