b014860294e94b2556473e45f99b489f677faa52
Marcus Turner <marcus.turner@dvsa.gov.uk>
2021-07-27 11:06:48 +0000

    VOL-2208: Update database and backend query to support total authorised HGVs and HGVs

--- test/module/Api/src/Domain/CommandHandler/Licence/ContinueLicenceTest.php
+++ test/module/Api/src/Domain/CommandHandler/Licence/ContinueLicenceTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Licence;
 
 use Doctrine\ORM\Query;
@@ -19,70 +21,43 @@ use Dvsa\Olcs\Api\Entity\System\Category;
 use Dvsa\Olcs\Api\Entity\Task\Task;
 use Dvsa\Olcs\Api\Domain\Util\DateTime\DateTime;
 use Mockery as m;
-use Laminas\ServiceManager\ServiceLocatorInterface;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Service\FinancialStandingHelperService;
+use Dvsa\Olcs\Api\Domain\Repository\ContinuationDetail as ContinuationDetailRepo;
+use Dvsa\Olcs\Transfer\Command\Licence\ContinueLicence;
+use Olcs\TestHelpers\Service\MocksServicesTrait;
+use Dvsa\OlcsTest\Api\Domain\CommandHandler\MocksAbstractCommandHandlerServicesTrait;
 
 /**
- * ContinueLicenceTest
- *
- * @author Mat Evans <mat.evans@valtech.co.uk>
- *
+ * @see \Dvsa\Olcs\Api\Domain\CommandHandler\Licence\ContinueLicence
  */
 class ContinueLicenceTest extends CommandHandlerTestCase
 {
-    public function setUp(): void
+    use MocksServicesTrait;
+    use MocksAbstractCommandHandlerServicesTrait;
+
+    protected const LICENCE_ID = 1;
+    protected const LICENCE_ID_COMMAND_PROPERTY = 'id';
+    protected const A_NUMBER_OF_VEHICLES = 2;
+
+    /**
+     * @test
+     */
+    public function handleCommand_IsCallable()
     {
-        $this->sut = new CommandHandler();
-        $this->mockRepo('Licence', \Dvsa\Olcs\Api\Domain\Repository\Licence::class);
-        $this->mockRepo('ContinuationDetail', \Dvsa\Olcs\Api\Domain\Repository\ContinuationDetail::class);
-        $this->mockRepo('GoodsDisc', \Dvsa\Olcs\Api\Domain\Repository\GoodsDisc::class);
+        // Setup
+        $this->setUpSut();
 
-        $this->mockedSmServices['FinancialStandingHelperService'] = m::mock();
-        $this->mockedSmServices[CacheEncryption::class] = m::mock(CacheEncryption::class);
-
-        parent::setUp();
+        // Assert
+        $this->assertIsCallable([$this->sut, 'handleCommand']);
     }
 
-    protected function initReferences()
-    {
-        $this->refData = [
-            'con_det_sts_complete',
-            LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL,
-            LicenceEntity::LICENCE_TYPE_STANDARD_INTERNATIONAL,
-            LicenceEntity::LICENCE_TYPE_SPECIAL_RESTRICTED,
-            LicenceEntity::LICENCE_TYPE_RESTRICTED,
-            LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE,
-            LicenceEntity::LICENCE_CATEGORY_PSV,
-        ];
-
-        parent::initReferences();
-    }
-
-    private function getGoodsLicence($licenceId, $type = LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL)
-    {
-        $organisation = m::mock(Organisation::class);
-        $organisation->shouldReceive('getId')->withNoArgs()->andReturn(9999);
-        $licence = new LicenceEntity($organisation, new RefData());
-        $licence->setId($licenceId);
-        $licence->setLicenceType($this->refData[$type]);
-        $licence->setGoodsOrPsv($this->refData[LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE]);
-
-        return $licence;
-    }
-
-    private function getPsvLicence($licenceId, $type = LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL)
-    {
-        $organisation = m::mock(Organisation::class);
-        $organisation->shouldReceive('getId')->withNoArgs()->andReturn(9999);
-        $licence = new LicenceEntity($organisation, new RefData());
-        $licence->setId($licenceId);
-        $licence->setLicenceType($this->refData[$type]);
-        $licence->setGoodsOrPsv($this->refData[LicenceEntity::LICENCE_CATEGORY_PSV]);
-
-        return $licence;
-    }
-
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandNoContinuationDetail()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -98,8 +73,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->sut->handleCommand($command);
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandPsvSpecialRestricted()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -137,8 +116,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandPsvStandardNational()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -191,8 +174,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandPsvStandardInternational()
     {
+        $this->setUpLegacy();
         $licenceId = 717;
         $data = ['id' => $licenceId, 'version' => 654];
         $totCommunityLic = 34;
@@ -273,8 +260,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandGoodsNotStandardInternational()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -326,8 +317,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['1502 goods discs created', 'Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandGoodsStandardInternational()
     {
+        $this->setUpLegacy();
         $licenceId = 717;
         $totCommunityLic = 34;
         $data = ['id' => $licenceId, 'version' => 654];
@@ -406,9 +401,11 @@ class ContinueLicenceTest extends CommandHandlerTestCase
 
     /**
      * @dataProvider signatureProvider
+     * @depends handleCommand_IsCallable
      */
     public function testHandleCommandWithSnapshotAndSignature($signature, $description, $actionDate)
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -506,8 +503,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         ];
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandInsufficientFinancesTask()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -579,8 +580,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandInsufficientFinancesTaskSpecialRestricted()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -630,8 +635,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandOtherFinancesTask()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -694,8 +703,12 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
     }
 
+    /**
+     * @depends handleCommand_IsCallable
+     */
     public function testHandleCommandOtherFinancesTaskNotCreated()
     {
+        $this->setUpLegacy();
         $data = ['id' => 717, 'version' => 654];
 
         $command = Command::create($data);
@@ -744,5 +757,223 @@ class ContinueLicenceTest extends CommandHandlerTestCase
         $result = $this->sut->handleCommand($command);
 
         $this->assertSame(['Licence 717 continued'], $result->getMessages());
+    }
+
+    /**
+     * @test
+     * @depends handleCommand_IsCallable
+     */
+    public function handleCommand_SetsTotAuthVehicles_ForPsvLicences_ThatHaveContinutationDetails_AndLicenceIsNotSpecialRestricted()
+    {
+        // Setup
+        $this->setUpSut();
+        $licence = $this->licenceForPsv(static::LICENCE_ID);
+        $continuation = $this->continuationDetailForLicence($licence);
+        $continuation->setTotAuthVehicles($expectedNumber = static::A_NUMBER_OF_VEHICLES);
+        $this->injectEntities($licence, $continuation);
+        $command = ContinueLicence::create([
+            static::LICENCE_ID_COMMAND_PROPERTY => $licence->getId(),
+        ]);
+
+        // Expect
+        $this->licenceRepository()->expects('save')->withArgs(function (Licence $licence) use ($expectedNumber) {
+            $this->assertSame($expectedNumber, $licence->getTotAuthVehicles());
+            return true;
+        });
+
+        // Execute
+        $this->sut->handleCommand($command);
+    }
+
+    public function setUp(): void
+    {
+        $this->setUpServiceManager();
+    }
+
+    protected function setUpSut()
+    {
+        $this->sut = new CommandHandler();
+
+        if (null !== $this->serviceManager()) {
+            $this->sut->createService($this->commandHandlerManager());
+        }
+    }
+
+    protected function setUpDefaultServices()
+    {
+        $this->serviceManager()->setService('FinancialStandingHelperService', m::mock(FinancialStandingHelperService::class));
+        $this->setUpAbstractCommandHandlerServices();
+        $this->licenceRepository();
+        $this->continuationDetailRepository();
+    }
+
+    /**
+     * @return m\MockInterface|\Dvsa\Olcs\Api\Domain\Repository\Licence
+     */
+    protected function licenceRepository(): m\MockInterface
+    {
+        $repositoryServiceManager = $this->repositoryServiceManager();
+        if (! $repositoryServiceManager->has('Licence')) {
+            $instance = $this->setUpMockService(Licence::class);
+
+            // Inject default licence instance
+            $instance->allows('fetchUsingId')->andReturnUsing(function ($id) {
+                return $this->licenceForGoodsLicence($id);
+            })->byDefault();
+
+            $repositoryServiceManager->setService('Licence', $instance);
+        }
+        return $repositoryServiceManager->get('Licence');
+    }
+
+    /**
+     * @return ContinuationDetailRepo
+     */
+    protected function continuationDetailRepository(): ContinuationDetailRepo
+    {
+        $repositoryServiceManager = $this->repositoryServiceManager();
+        if (! $repositoryServiceManager->has('ContinuationDetail')) {
+            $instance = $this->setUpMockService(ContinuationDetailRepo::class);
+
+            // Inject default entity
+            $instance->allows('fetchForLicence')->andReturnUsing(function ($licenceId) {
+                return [];
+            })->byDefault();
+
+            $repositoryServiceManager->setService('ContinuationDetail', $instance);
+        }
+        return $repositoryServiceManager->get('ContinuationDetail');
+    }
+
+    /**
+     * @param object ...$entities
+     */
+    protected function injectEntities(object ...$entities)
+    {
+        foreach ($entities as $entity) {
+            switch (get_class($entity)) {
+                case Licence::class:
+                    assert(is_callable([$entity, 'getId']));
+                    $this->licenceRepository()
+                        ->allows('fetchById')
+                        ->withArgs(function ($licenceId) use ($entity) {
+                            return $licenceId === $entity->getId();
+                        })
+                        ->andReturn($entity)
+                        ->byDefault();
+                    break;
+                case ContinuationDetail::class:
+                    assert(is_callable([$entity, 'getLicence']));
+                    if (null !== ($licence = $entity->getLicence())) {
+                        assert(is_callable([$licence, 'getId']));
+                        $this->continuationDetailRepository()
+                            ->allows('fetchForLicence')
+                            ->withArgs(function ($licenceId) use ($licence) {
+                                return $licenceId === $licence->getId();
+                            })
+                            ->andReturn([$entity])
+                            ->byDefault();
+                        break;
+                    }
+                    // Continue to error if we have not injected anything for the entity
+                default:
+                    assert(false, 'Cannot inject the entity provided');
+            }
+        }
+    }
+
+    /**
+     * @param Licence $licence
+     * @return ContinuationDetail
+     */
+    protected function continuationDetailForLicence(Licence $licence): ContinuationDetail
+    {
+        $instance = new ContinuationDetail();
+        $instance->setLicence($licence);
+        return $instance;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Licence
+     */
+    protected function licenceForPsv($id): Licence
+    {
+        $instance = new Licence($this->organisation(), new RefData(Licence::LICENCE_STATUS_VALID));
+        $instance->setId($id);
+        $instance->setGoodsOrPsv(new RefData(Licence::LICENCE_CATEGORY_PSV));
+        return $instance;
+    }
+
+    /**
+     * @return Organisation
+     */
+    protected function organisation(): Organisation
+    {
+        return new Organisation();
+    }
+
+    /**
+     * @deprecated Use new test format
+     */
+    public function setUpLegacy(): void
+    {
+        $this->setUpSut();
+        $this->mockRepo('Licence', \Dvsa\Olcs\Api\Domain\Repository\Licence::class);
+        $this->mockRepo('ContinuationDetail', \Dvsa\Olcs\Api\Domain\Repository\ContinuationDetail::class);
+        $this->mockRepo('GoodsDisc', \Dvsa\Olcs\Api\Domain\Repository\GoodsDisc::class);
+
+        $this->mockedSmServices['FinancialStandingHelperService'] = m::mock();
+        $this->mockedSmServices[CacheEncryption::class] = m::mock(CacheEncryption::class);
+
+        parent::setUp();
+    }
+
+    /**
+     * @deprecated Use new test format
+     */
+    protected function initReferences()
+    {
+        $this->refData = [
+            'con_det_sts_complete',
+            LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL,
+            LicenceEntity::LICENCE_TYPE_STANDARD_INTERNATIONAL,
+            LicenceEntity::LICENCE_TYPE_SPECIAL_RESTRICTED,
+            LicenceEntity::LICENCE_TYPE_RESTRICTED,
+            LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE,
+            LicenceEntity::LICENCE_CATEGORY_PSV,
+        ];
+
+        parent::initReferences();
+    }
+
+    /**
+     * @deprecated Use real instances for entities instead of mocks
+     */
+    private function getGoodsLicence($licenceId, $type = LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL)
+    {
+        $organisation = m::mock(Organisation::class);
+        $organisation->shouldReceive('getId')->withNoArgs()->andReturn(9999);
+        $licence = new LicenceEntity($organisation, new RefData());
+        $licence->setId($licenceId);
+        $licence->setLicenceType($this->refData[$type]);
+        $licence->setGoodsOrPsv($this->refData[LicenceEntity::LICENCE_CATEGORY_GOODS_VEHICLE]);
+
+        return $licence;
+    }
+
+    /**
+     * @deprecated Use real instances for entities instead of mocks
+     */
+    private function getPsvLicence($licenceId, $type = LicenceEntity::LICENCE_TYPE_STANDARD_NATIONAL)
+    {
+        $organisation = m::mock(Organisation::class);
+        $organisation->shouldReceive('getId')->withNoArgs()->andReturn(9999);
+        $licence = new LicenceEntity($organisation, new RefData());
+        $licence->setId($licenceId);
+        $licence->setLicenceType($this->refData[$type]);
+        $licence->setGoodsOrPsv($this->refData[LicenceEntity::LICENCE_CATEGORY_PSV]);
+
+        return $licence;
     }
 }
