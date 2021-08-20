f14290e01e9a4ad4e2fbfd893307e5c74a1a6777
James Hodgson <james.hodgson@dvsa.gov.uk>
2021-08-20 13:47:09 +0000

    VOL-2161: Support passing realm to login

--- module/Auth/test/Service/OpenAm/Client/UriBuilderTest.php
+++ module/Auth/test/Service/OpenAm/Client/UriBuilderTest.php
@@ -11,13 +11,20 @@ class UriBuilderTest extends MockeryTestCase
 {
     public function testBuild(): void
     {
-        $sut = new UriBuilder('olcs.openam', null);
+        $sut = new UriBuilder('olcs.openam');
         $this->assertEquals('olcs.openam/foo/bar', $sut->build('foo/bar'));
     }
 
     public function testBuildWithRealm(): void
     {
         $sut = new UriBuilder('olcs.openam', 'foo');
+        $this->assertEquals('olcs.openam/foo/bar?realm=foo', $sut->build('foo/bar'));
+    }
+
+    public function testBuildWithRealmSetAfterInitialising(): void
+    {
+        $sut = new UriBuilder('olcs.openam');
+        $sut->setRealm('foo');
         $this->assertEquals('olcs.openam/foo/bar?realm=foo', $sut->build('foo/bar'));
     }
 
--- test/module/Api/src/Domain/CommandHandler/Auth/LoginTest.php
+++ test/module/Api/src/Domain/CommandHandler/Auth/LoginTest.php
@@ -8,6 +8,7 @@ use Dvsa\Olcs\Api\Domain\CommandHandler\Auth\Login;
 use Dvsa\Olcs\Api\Domain\Exception\RuntimeException;
 use Dvsa\Olcs\Api\Domain\Repository\User as UserRepo;
 use Dvsa\Olcs\Api\Entity\User\User;
+use Dvsa\Olcs\Auth\Adapter\OpenAm;
 use Dvsa\Olcs\Auth\Service\AuthenticationServiceInterface;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
 use Laminas\Authentication\Adapter\ValidatableAdapterInterface;
@@ -59,6 +60,34 @@ class LoginTest extends CommandHandlerTestCase
         $this->sut->handleCommand(\Dvsa\Olcs\Transfer\Command\Auth\Login::create([
             'username' => $testUsername,
             'password' => $testPassword,
+        ]));
+    }
+
+    /**
+     * @test
+     */
+    public function handleCommand_AdapterSetsRealm_FromCommand_WhenAdapterIsOpenAM()
+    {
+        // Setup
+        $openAMAdapter = $this->authenticationAdapter(OpenAm::class);
+        $openAMAdapter->allows('setIdentity')->withArgs([$testUsername = 'testUsername']);
+        $openAMAdapter->allows('setCredential')->withArgs([$testPassword = 'testPassword']);
+
+        $this->sut = new Login($this->authenticationService(), $openAMAdapter);
+        parent::setUp();
+
+        $this->authenticationService()->expects('authenticate')->andReturns(
+            new \Laminas\Authentication\Result(\Laminas\Authentication\Result::FAILURE, false)
+        );
+
+        // Expectations
+        $openAMAdapter->expects('setRealm')->withArgs([$testRealm ='testRealm']);
+
+        // Execute
+        $this->sut->handleCommand(\Dvsa\Olcs\Transfer\Command\Auth\Login::create([
+            'username' => $testUsername,
+            'password' => $testPassword,
+            'realm' => $testRealm
         ]));
     }
 
@@ -223,18 +252,18 @@ class LoginTest extends CommandHandlerTestCase
     /**
      * @return ValidatableAdapterInterface|m\LegacyMockInterface|m\MockInterface
      */
-    protected function authenticationAdapter(): ValidatableAdapterInterface
+    protected function authenticationAdapter(string $adapterClass = ValidatableAdapterInterface::class): ValidatableAdapterInterface
     {
-        if (!$this->serviceManager->has(ValidatableAdapterInterface::class)) {
-            $adapter = m::mock(ValidatableAdapterInterface::class);
+        if (!$this->serviceManager->has($adapterClass)) {
+            $adapter = m::mock($adapterClass);
             $adapter->shouldIgnoreMissing();
             $adapter->allows('isValid')->andReturns(true)->byDefault();
             $adapter->allows('code')->andReturns(1)->byDefault();
             $adapter->allows('identity')->andReturns('user')->byDefault();
             $adapter->allows('getMessages')->andReturns(['Authentication was successful'])->byDefault();
-            $this->serviceManager->setService(ValidatableAdapterInterface::class, $adapter);
+            $this->serviceManager->setService($adapterClass, $adapter);
         }
 
-        return $this->serviceManager->get(ValidatableAdapterInterface::class);
+        return $this->serviceManager->get($adapterClass);
     }
 }
