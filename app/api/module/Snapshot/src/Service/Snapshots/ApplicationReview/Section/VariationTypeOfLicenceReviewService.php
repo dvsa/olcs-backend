fd95074ccb16e72376ac1687ed2576c4db286453
Jonathan Thomas <jonathan.thomas@bjss.com>
2022-03-18 15:50:04 +0000

    VOL-2901 lgv to mixed fleet and vice versa type of licence section on snapshot report

--- app/api/module/Snapshot/src/Service/Snapshots/ApplicationReview/Section/VariationTypeOfLicenceReviewService.php
+++ app/api/module/Snapshot/src/Service/Snapshots/ApplicationReview/Section/VariationTypeOfLicenceReviewService.php
@@ -7,6 +7,9 @@
  */
 namespace Dvsa\Olcs\Snapshot\Service\Snapshots\ApplicationReview\Section;
 
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\System\RefData;
+
 /**
  * Variation Type Of Licence Review Service
  *
@@ -18,21 +21,74 @@ class VariationTypeOfLicenceReviewService extends AbstractReviewService
      * Format the readonly config from the given data
      *
      * @param array $data
+     *
      * @return array
      */
     public function getConfigFromData(array $data = array())
     {
-        return ['freetext' => $this->getFreeText($data)];
+        $licenceLicenceType = $data['licence']['licenceType']['id'];
+        $applicationLicenceType = $data['licenceType']['id'];
+
+        if ($licenceLicenceType == Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL &&
+            $applicationLicenceType == Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL
+        ) {
+            return $this->getStandardInternationalResponse($data);
+        }
+
+        return $this->getNonStandardInternationalResponse($data);
     }
 
-    private function getFreeText($data)
+    /**
+     * Get the readonly config applicable to variations where both existing licence and variation have a licence
+     * type of standard international
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    private function getStandardInternationalResponse(array $data)
     {
-        return $this->translateReplace(
+        $applicationVehicleType = $data['vehicleType']['id'];
+        $licenceVehicleType = $data['licence']['vehicleType']['id'];
+
+        $value = $this->translateReplace(
+            'variation-application-type-of-licence-std-int-value-template',
+            [
+                $this->translate('variation-application-type-of-licence-std-int-value-' . $licenceVehicleType),
+                $this->translate('variation-application-type-of-licence-std-int-value-' . $applicationVehicleType)
+            ]
+        );
+
+        return [
+            'multiItems' => [
+                [
+                    [
+                        'label' => 'variation-application-type-of-licence-std-int-caption',
+                        'value' => $value,
+                    ],
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Get the readonly config applicable to variations where either the existing licence or variation (or both) have
+     * a licence type other than standard international
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    private function getNonStandardInternationalResponse(array $data)
+    {
+        $freeText = $this->translateReplace(
             'variation-application-type-of-licence-freetext',
             [
                 $this->formatRefdata($data['licence']['licenceType']),
                 $this->formatRefdata($data['licenceType'])
             ]
         );
+
+        return ['freetext' => $freeText];
     }
 }
--- app/api/test/module/Snapshot/src/Service/Snapshots/ApplicationReview/Section/VariationTypeOfLicenceReviewServiceTest.php
+++ app/api/test/module/Snapshot/src/Service/Snapshots/ApplicationReview/Section/VariationTypeOfLicenceReviewServiceTest.php
@@ -10,6 +10,8 @@ namespace Dvsa\OlcsTest\Snapshot\Service\Snapshots\ApplicationReview\Section;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use OlcsTest\Bootstrap;
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\System\RefData;
 use Dvsa\Olcs\Snapshot\Service\Snapshots\ApplicationReview\Section\VariationTypeOfLicenceReviewService;
 
 /**
@@ -21,6 +23,7 @@ class VariationTypeOfLicenceReviewServiceTest extends MockeryTestCase
 {
     protected $sut;
     protected $sm;
+    protected $mockTranslator;
 
     public function setUp(): void
     {
@@ -28,28 +31,133 @@ class VariationTypeOfLicenceReviewServiceTest extends MockeryTestCase
 
         $this->sut = new VariationTypeOfLicenceReviewService();
         $this->sut->setServiceLocator($this->sm);
+
+        $this->mockTranslator = m::mock();
+        $this->sm->setService('translator', $this->mockTranslator);
     }
 
-    public function testGetConfigFromData()
+    /**
+     * @dataProvider dpGetConfigFromData
+     */
+    public function testGetConfigFromData($licenceLicenceTypeId, $applicationLicenceTypeId)
     {
         $data = [
             'licenceType' => [
+                'id' => $applicationLicenceTypeId,
                 'description' => 'foo'
             ],
             'licence' => [
                 'licenceType' => [
+                    'id' => $licenceLicenceTypeId,
                     'description' => 'bar'
                 ]
             ]
         ];
 
-        $mockTranslator = m::mock();
-        $this->sm->setService('translator', $mockTranslator);
-
-        $mockTranslator->shouldReceive('translate')
+        $this->mockTranslator->shouldReceive('translate')
             ->with('variation-application-type-of-licence-freetext')
             ->andReturn('translated-text-%s-%s');
 
         $this->assertEquals(['freetext' => 'translated-text-bar-foo'], $this->sut->getConfigFromData($data));
+    }
+
+    public function dpGetConfigFromData()
+    {
+        return [
+            [
+                Licence::LICENCE_TYPE_RESTRICTED,
+                Licence::LICENCE_TYPE_STANDARD_NATIONAL,
+            ],
+            [
+                Licence::LICENCE_TYPE_RESTRICTED,
+                Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL,
+            ],
+            [
+                Licence::LICENCE_TYPE_STANDARD_NATIONAL,
+                Licence::LICENCE_TYPE_RESTRICTED,
+            ],
+            [
+                Licence::LICENCE_TYPE_STANDARD_NATIONAL,
+                Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL,
+            ],
+            [
+                Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL,
+                Licence::LICENCE_TYPE_STANDARD_NATIONAL,
+            ],
+            [
+                Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL,
+                Licence::LICENCE_TYPE_RESTRICTED,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dpGetConfigFromDataStandardInternational
+     */
+    public function testConfigFromDataStandardInternational(
+        $licenceVehicleTypeId,
+        $applicationVehicleTypeId,
+        $expectedTranslatedValue
+    ) {
+        $data = [
+            'licenceType' => [
+                'id' => Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL
+            ],
+            'vehicleType' => [
+                'id' => $applicationVehicleTypeId
+            ],
+            'licence' => [
+                'licenceType' => [
+                    'id' => Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL
+                ],
+                'vehicleType' => [
+                    'id' => $licenceVehicleTypeId
+                ]
+            ]
+        ];
+
+        $this->mockTranslator->shouldReceive('translate')
+            ->with('variation-application-type-of-licence-std-int-value-template')
+            ->andReturn('translated-template %s %s');
+
+        $this->mockTranslator->shouldReceive('translate')
+            ->with('variation-application-type-of-licence-std-int-value-app_veh_type_mixed')
+            ->andReturn('translated-mixed');
+
+        $this->mockTranslator->shouldReceive('translate')
+            ->with('variation-application-type-of-licence-std-int-value-app_veh_type_lgv')
+            ->andReturn('translated-lgv');
+
+        $expectedConfigFromData = [
+            'multiItems' => [
+                [
+                    [
+                        'label' => 'variation-application-type-of-licence-std-int-caption',
+                        'value' => $expectedTranslatedValue,
+                    ],
+                ]
+            ]
+        ];
+
+        $this->assertEquals(
+            $expectedConfigFromData,
+            $this->sut->getConfigFromData($data)
+        );
+    }
+
+    public function dpGetConfigFromDataStandardInternational()
+    {
+        return [
+            [
+                RefData::APP_VEHICLE_TYPE_MIXED,
+                RefData::APP_VEHICLE_TYPE_LGV,
+                'translated-template translated-mixed translated-lgv'
+            ],
+            [
+                RefData::APP_VEHICLE_TYPE_LGV,
+                RefData::APP_VEHICLE_TYPE_MIXED,
+                'translated-template translated-lgv translated-mixed'
+            ],
+        ];
     }
 }
