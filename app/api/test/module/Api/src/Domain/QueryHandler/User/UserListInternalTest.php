33051c925fb5a0558a05c660f247882b07001c82
Ian Lindsay <ian@hemera-business-services.co.uk>
2022-04-04 15:49:57 +0100

    VOL-2911 test user list queries redirect correctly

--- app/api/test/module/Api/src/Domain/QueryHandler/User/UserListInternalTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/User/UserListInternalTest.php
@@ -1,20 +1,20 @@
 <?php
 
-/**
- * UserListInternalTest
- */
+declare(strict_types=1);
+
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\User;
 
+use Dvsa\Olcs\Api\Domain\Query\User\UserListInternalByTrafficArea;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Api\Domain\QueryHandler\User\UserListInternal as QueryHandler;
 use Dvsa\Olcs\Api\Domain\Repository\User as Repo;
-use Dvsa\Olcs\Api\Entity\User\Permission as PermissionEntity;
 use Dvsa\Olcs\Transfer\Query\User\UserListInternal as Query;
 use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Mockery as m;
 use ZfcRbac\Service\AuthorizationService;
 
 /**
- * UserListInternalTest
+ * @see QueryHandler
  */
 class UserListInternalTest extends QueryHandlerTestCase
 {
@@ -30,8 +30,16 @@ class UserListInternalTest extends QueryHandlerTestCase
         parent::setUp();
     }
 
-    public function testHandleQuery()
+    public function testHandleQuery(): void
     {
+        $userData = [
+            'dataAccess' => [
+                'canAccessAll' => true,
+            ]
+        ];
+
+        $this->expectedUserDataCacheCall($userData);
+
         $user = m::mock(\Dvsa\Olcs\Api\Entity\User\User::class)->makePartial();
         $user->setId(74);
 
@@ -44,5 +52,37 @@ class UserListInternalTest extends QueryHandlerTestCase
 
         $this->assertSame(74, $result['result'][0]['id']);
         $this->assertSame('COUNT', $result['count']);
+    }
+
+    public function testRedirectWhenNotFullAccess(): void
+    {
+        $trafficAreas = ['B','C'];
+
+        $initialQueryData = [
+            'sort' => 'sort-field',
+            'order' => 'ASC',
+            'excludeLimitedReadOnly' => true,
+            'team' => 999,
+        ];
+
+        $additionalData = [
+            'trafficAreas' => $trafficAreas,
+        ];
+
+        $newQueryData = array_merge($initialQueryData, $additionalData);
+
+        $query = Query::create($initialQueryData);
+
+        $userData = [
+            'dataAccess' => [
+                'canAccessAll' => false,
+                'trafficAreas' => $trafficAreas
+            ]
+        ];
+
+        $queryResult = new Result();
+        $this->expectedUserDataCacheCall($userData);
+        $this->expectedQuery(UserListInternalByTrafficArea::class, $newQueryData, $queryResult);
+        $this->assertSame($queryResult, $this->sut->handleQuery($query));
     }
 }
--- app/api/test/module/Api/src/Domain/QueryHandler/User/UserListTest.php
+++ app/api/test/module/Api/src/Domain/QueryHandler/User/UserListTest.php
@@ -1,12 +1,11 @@
 <?php
 
-/**
- * UserListTest
- *
- * @author Mat Evans <mat.evans@valtech.co.uk>
- */
+declare(strict_types=1);
+
 namespace Dvsa\OlcsTest\Api\Domain\QueryHandler\User;
 
+use Dvsa\Olcs\Api\Domain\Query\User\UserListByTrafficArea;
+use Dvsa\Olcs\Api\Domain\QueryHandler\Result;
 use Dvsa\Olcs\Api\Domain\QueryHandler\User\UserList as QueryHandler;
 use Dvsa\Olcs\Api\Domain\Repository\User as Repo;
 use Dvsa\Olcs\Transfer\Query\User\UserList as Query;
@@ -14,9 +13,7 @@ use Dvsa\OlcsTest\Api\Domain\QueryHandler\QueryHandlerTestCase;
 use Mockery as m;
 
 /**
- * UserListTest
- *
- * @author Mat Evans <mat.evans@valtech.co.uk>
+ * @see QueryHandler
  */
 class UserListTest extends QueryHandlerTestCase
 {
@@ -28,8 +25,20 @@ class UserListTest extends QueryHandlerTestCase
         parent::setUp();
     }
 
-    public function testHandleQuery()
+    /**
+     * @dataProvider handleQueryProvider
+     */
+    public function testHandleQuery($isInternal, $canAccessAll): void
     {
+        $userData = [
+            'isInternal' => $isInternal,
+            'dataAccess' => [
+                'canAccessAll' => $canAccessAll,
+            ]
+        ];
+
+        $this->expectedUserDataCacheCall($userData);
+
         $query = Query::create(['QUERY']);
 
         $user = m::mock(\Dvsa\Olcs\Api\Entity\User\User::class)->makePartial();
@@ -43,5 +52,47 @@ class UserListTest extends QueryHandlerTestCase
 
         $this->assertSame(74, $result['result'][0]['id']);
         $this->assertSame('COUNT', $result['count']);
+    }
+
+    public function handleQueryProvider(): array
+    {
+        return [
+            'not internal and does not have full access' => [false, false],
+            'is internal and does have full access' => [true, true],
+        ];
+    }
+
+    public function testRedirectWhenInternalAndNotFullAccess(): void
+    {
+        $trafficAreas = ['B','C'];
+
+        $initialQueryData = [
+            'sort' => 'sort-field',
+            'order' => 'ASC',
+            'team' => 999,
+            'organisation' => 888,
+            'isInternal' => true
+        ];
+
+        $additionalData = [
+            'trafficAreas' => $trafficAreas,
+        ];
+
+        $newQueryData = array_merge($initialQueryData, $additionalData);
+
+        $query = Query::create($initialQueryData);
+
+        $userData = [
+            'isInternal' => true,
+            'dataAccess' => [
+                'canAccessAll' => false,
+                'trafficAreas' => $trafficAreas
+            ]
+        ];
+
+        $queryResult = new Result();
+        $this->expectedUserDataCacheCall($userData);
+        $this->expectedQuery(UserListByTrafficArea::class, $newQueryData, $queryResult);
+        $this->assertSame($queryResult, $this->sut->handleQuery($query));
     }
 }
