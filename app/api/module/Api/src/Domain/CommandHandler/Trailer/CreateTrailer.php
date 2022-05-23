e47599dae437587af4acf1e54a86ae560c344c37
Jonathan Thomas <jonathan.thomas@bjss.com>
2022-05-23 14:28:14 +0100

    VOL-3107 enable a trailer to be flagged as a longer semi trailer during a self serve variation

--- app/api/module/Api/src/Domain/CommandHandler/Trailer/CreateTrailer.php
+++ app/api/module/Api/src/Domain/CommandHandler/Trailer/CreateTrailer.php
@@ -9,10 +9,9 @@ namespace Dvsa\Olcs\Api\Domain\CommandHandler\Trailer;
 
 use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
-use Dvsa\Olcs\Api\Entity\Licence\Trailer;
 use Dvsa\Olcs\Api\Entity\Licence\Licence;
+use Dvsa\Olcs\Api\Entity\Licence\Trailer;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
-use Laminas\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Create Trailer
@@ -23,8 +22,6 @@ final class CreateTrailer extends AbstractCommandHandler
 {
     protected $repoServiceName = 'Trailer';
 
-    protected $licenceRepo = null;
-
     public function handleCommand(CommandInterface $command)
     {
         $licence = $this->getRepo()
@@ -32,6 +29,7 @@ final class CreateTrailer extends AbstractCommandHandler
 
         $trailer = new Trailer();
         $trailer->setTrailerNo($command->getTrailerNo());
+        $trailer->setIsLongerSemiTrailer($command->getIsLongerSemiTrailer() == 'Y');
         $trailer->setSpecifiedDate(new \DateTime($command->getSpecifiedDate()));
         $trailer->setLicence($licence);
 
--- app/api/test/module/Api/src/Domain/CommandHandler/Trailer/CreateTrailerTest.php
+++ app/api/test/module/Api/src/Domain/CommandHandler/Trailer/CreateTrailerTest.php
@@ -5,42 +5,56 @@
  *
  * @author Josh Curtis <josh.curtis@valtech.co.uk>
  */
-namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Licence;
+namespace Dvsa\OlcsTest\Api\Domain\CommandHandler\Trailer;
 
 use Mockery as m;
-use Doctrine\ORM\Query;
 use Dvsa\OlcsTest\Api\Domain\CommandHandler\CommandHandlerTestCase;
-
 use Dvsa\Olcs\Api\Domain\Repository\Trailer as TrailerRepo;
-use Dvsa\Olcs\Api\Domain\Repository\Licence as LicenceRepo;
 use Dvsa\Olcs\Api\Domain\CommandHandler\Trailer\CreateTrailer;
-
+use Dvsa\Olcs\Api\Entity\Licence\Licence;
 use Dvsa\Olcs\Api\Entity\Licence\Trailer;
-
 use Dvsa\Olcs\Transfer\Command\Trailer\CreateTrailer as Cmd;
 
 /**
- * Class UpdateTypeOfLicenceTest
+ * Class CreateTrailerTest
  *
- * @package Dvsa\OlcsTest\Api\Domain\CommandHandler\Licence
+ * @package Dvsa\OlcsTest\Api\Domain\CommandHandler\Trailer
  *
  * @author Josh Curtis <josh.curtis@valtech.co.uk>
  */
 class CreateTrailerTest extends CommandHandlerTestCase
 {
+    private $licence;
+
     public function setUp(): void
     {
         $this->sut = new CreateTrailer();
         $this->mockRepo('Trailer', TrailerRepo::class);
-        $this->mockRepo('Licence', LicenceRepo::class);
+
+        $this->licence = m::mock(Licence::class);
 
         parent::setUp();
     }
 
-    public function testHandleCommand()
+    protected function initReferences()
+    {
+        $this->references = [
+            Licence::class => [
+                7 => $this->licence
+            ],
+        ];
+
+        parent::initReferences();
+    }
+
+    /**
+     * @dataProvider dpIsLongerSemiTrailer
+     */
+    public function testHandleCommand($isLongerSemiTrailer, $expectedIsLongerSemiTrailer)
     {
         $data = [
             'trailerNo' => 'A1000',
+            'isLongerSemiTrailer' => $isLongerSemiTrailer,
             'licence' => '7',
             'specifiedDate' => '2015-01-01'
         ];
@@ -52,9 +66,11 @@ class CreateTrailerTest extends CommandHandlerTestCase
             ->once()
             ->with(m::type(Trailer::class))
             ->andReturnUsing(
-                function (Trailer $trailer) use ($data) {
-                    $this->assertEquals($trailer->getTrailerNo(), $data['trailerNo']);
-                    $this->assertEquals($trailer->getSpecifiedDate()->format('Y-m-d'), $data['specifiedDate']);
+                function (Trailer $trailer) use ($data, $expectedIsLongerSemiTrailer) {
+                    $this->assertEquals($data['trailerNo'], $trailer->getTrailerNo());
+                    $this->assertEquals($expectedIsLongerSemiTrailer, $trailer->getIsLongerSemiTrailer());
+                    $this->assertSame($this->licence, $trailer->getLicence());
+                    $this->assertEquals($data['specifiedDate'], $trailer->getSpecifiedDate()->format('Y-m-d'));
                 }
             );
 
@@ -70,6 +86,13 @@ class CreateTrailerTest extends CommandHandlerTestCase
         ];
 
         $this->assertEquals($expected, $result->toArray());
+    }
 
+    public function dpIsLongerSemiTrailer()
+    {
+        return [
+            ['Y', true],
+            ['N', false],
+        ];
     }
 }
