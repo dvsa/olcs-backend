6dbe49b3cd47a6aa0ebea3730286b426e9ccb0cc
Andy Newton <andy.newton@dvsa.gov.uk>
2022-10-25 16:43:27 +0100

    GovUk Account redirect request cmd handler

---
+++ module/Api/src/Domain/CommandHandler/GovUkAccount/GetGovUkAccountRedirect.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Domain\CommandHandler\GovUkAccount;
+
+use Dvsa\Olcs\Api\Domain\Command\Result;
+use Dvsa\Olcs\Api\Domain\CommandHandler\AbstractCommandHandler;
+use Dvsa\Olcs\Api\Service\GovUkAccount\GovUkAccountService;
+use Dvsa\Olcs\Transfer\Command\CommandInterface;
+
+/**
+ * GetGovUkAccountnRedirect
+ */
+class GetGovUkAccountRedirect extends AbstractCommandHandler
+{
+    /** @var GovUkAccountService */
+    private GovUkAccountService $govUkAccountService;
+
+    /**
+     * Constructor.
+     *
+     * @param GovUkAccountService $govUkAccountService
+     */
+    public function __construct(GovUkAccountService $govUkAccountService)
+    {
+        $this->govUkAccountService = $govUkAccountService;
+    }
+
+    /**
+     * Handle command
+     *
+     * @param \Dvsa\Olcs\Transfer\Command\GovUkAccount\GetGovUkAccountRedirect $command Command to handle
+     *
+     * @return \Dvsa\Olcs\Api\Domain\Command\Result
+     * @throws \Dvsa\Olcs\Api\Domain\Exception\RuntimeException
+     */
+    public function handleCommand(CommandInterface $command)
+    {
+        $stateTokenStr = $this->govUkAccountService->createStateToken($command->getArrayCopy());
+        $authorisationUrl = $this->govUkAccountService->getAuthorisationUrl($stateTokenStr);
+        return $this->result->addMessage($authorisationUrl->getUrl());
+    }
+}
+
