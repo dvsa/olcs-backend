90681612c2812b3c61fed9645247f4dbf8707eee
Shaun Hare <shaun.hare@dvsa.gov.uk>
2023-10-31 13:09:58 +0000

    updated providers for config

--- config/autoload/config-parameters.global.php
+++ config/autoload/config-parameters.global.php
@@ -1,6 +1,8 @@
 <?php
 
 use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\SecretsManager;
+use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\ParameterStore;
+
 
 return [
     'config_parameters' => [
@@ -8,6 +10,9 @@ return [
             SecretsManager::class => [
                 // Todo: will need to be parameterised once all the terraform is ready.
                 'DEVAPPDA-BASE-SM-APPLICATION-SECRETS',
+            ],
+            ParameterStore::class => [
+                '/applicationparams/da/',
             ],
         ],
     ],
