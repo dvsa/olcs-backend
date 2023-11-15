e105515f90c1d494356174a9741f0c4f986ff824
Ian Lindsay <ian.lindsay@dvsa.gov.uk>
2023-11-15 14:00:19 +0000

    Revert "Merge branch 'revert-2fdc3cd9' into 'master'"
    This reverts merge request !2351

--- module/Api/src/Entity/System/AbstractCategory.php
+++ module/Api/src/Entity/System/AbstractCategory.php
@@ -76,6 +76,15 @@ abstract class AbstractCategory implements BundleSerializableInterface, JsonSeri
     protected $isDocCategory = 1;
 
     /**
+     * Is messaging
+     *
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean", name="is_messaging", nullable=false, options={"default": 0})
+     */
+    protected $isMessaging = 0;
+
+    /**
      * Is scan category
      *
      * @var boolean
@@ -218,6 +227,30 @@ abstract class AbstractCategory implements BundleSerializableInterface, JsonSeri
     public function getIsDocCategory()
     {
         return $this->isDocCategory;
+    }
+
+    /**
+     * Set the is messaging
+     *
+     * @param boolean $isMessaging new value being set
+     *
+     * @return Category
+     */
+    public function setIsMessaging($isMessaging)
+    {
+        $this->isMessaging = $isMessaging;
+
+        return $this;
+    }
+
+    /**
+     * Get the is messaging
+     *
+     * @return boolean
+     */
+    public function getIsMessaging()
+    {
+        return $this->isMessaging;
     }
 
     /**
