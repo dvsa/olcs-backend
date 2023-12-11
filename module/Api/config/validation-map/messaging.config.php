25c56b0e0e55a2022ce7e8579e83e5260d7dee70
James Wragg <james.wragg@dvsa.gov.uk>
2023-12-11 17:31:19 +0000

    fix: VOL-4687 - Redesigned Query Handlers for Conversations

--- module/Api/config/validation-map/messaging.config.php
+++ module/Api/config/validation-map/messaging.config.php
@@ -4,5 +4,6 @@ use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
-    QueryHandler\Messaging\ConversationList::class => NoValidationRequired::class,
+    QueryHandler\Messaging\Conversations\ByLicence::class => NoValidationRequired::class,
+    QueryHandler\Messaging\Conversations\ByApplicationToLicence::class => NoValidationRequired::class,
 ];
