76b123d7894599762fc3780b7eac37da35c0a246
Wade Womersley <155439365+wadedvsa@users.noreply.github.com>
2024-01-12 11:33:38 +0000

    feat: Close Handler for Conversations (dvsa/olcs-backend#27)
    * feat: Close Handler for Conversations
    * fix: CR changes (typo in closed)
    * fix: PR don't need setLastModifidBy due to blameable
    * fix: Namespace change for Close class

--- app/api/module/Api/config/validation-map/messaging.config.php
+++ app/api/module/Api/config/validation-map/messaging.config.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
@@ -7,4 +8,5 @@ return [
     QueryHandler\Messaging\Conversations\ByLicence::class => NoValidationRequired::class,
     QueryHandler\Messaging\Conversations\ByApplicationToLicence::class => NoValidationRequired::class,
     QueryHandler\Messaging\Message\ByConversation::class => NoValidationRequired::class,
+    CommandHandler\Messaging\Conversation\Close::class => NoValidationRequired::class,
 ];
