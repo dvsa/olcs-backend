898adcabfb30965745cbcfdb37b6b9c61335f2ea
James Wragg <james.wragg@dvsa.gov.uk>
2024-01-15 15:49:33 +0000

    fix: Duplicate import

--- module/Api/config/validation-map/messaging.config.php
+++ module/Api/config/validation-map/messaging.config.php
@@ -2,7 +2,6 @@
 
 use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\QueryHandler;
-use Dvsa\Olcs\Api\Domain\CommandHandler;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\NoValidationRequired;
 
 return [
