0d67cc371f3750e2831142002fe9dc0a33f5f8d2
James Wragg <james.wragg@dvsa.gov.uk>
2024-01-30 16:39:38 +0000

    fix: Messaging tables version column not null and default value of 1

--- module/Api/src/Entity/Messaging/AbstractMessagingSubject.php
+++ module/Api/src/Entity/Messaging/AbstractMessagingSubject.php
@@ -104,10 +104,10 @@ abstract class AbstractMessagingSubject implements BundleSerializableInterface, 
      *
      * @var int
      *
-     * @ORM\Column(type="smallint", name="version", nullable=true)
+     * @ORM\Column(type="smallint", name="version", nullable=false, options={"default": 1})
      * @ORM\Version
      */
-    protected $version;
+    protected $version = 1;
 
     /**
      * Set the category
