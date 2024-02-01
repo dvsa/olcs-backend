57762d04d669556cec633fbf3cb1f491d62705ae
James Wragg <james.wragg@dvsa.gov.uk>
2024-02-01 09:56:29 +0000

    fix: removed duplicate Create

--- module/Api/config/validation-map/messaging.config.php
+++ module/Api/config/validation-map/messaging.config.php
@@ -17,6 +17,5 @@ return [
     CommandHandler\Messaging\Message\Create::class                     => NoValidationRequired::class,
     QueryHandler\Messaging\Conversations\ByOrganisation::class         => NoValidationRequired::class,
     QueryHandler\Messaging\Subjects\All::class                         => NoValidationRequired::class,
-    CommandHandler\Messaging\Message\Create::class                     => NoValidationRequired::class,
     CommandHandler\Messaging\Conversation\Create::class                => NoValidationRequired::class,
 ];
