2667808f79064397710189d129cc73dc808be4fa
James Wragg <james.wragg@dvsa.gov.uk>
2024-02-01 10:10:59 +0000

    fix: reformat file and optimised

--- module/Api/src/Domain/CommandHandler/Messaging/Message/Create.php
+++ module/Api/src/Domain/CommandHandler/Messaging/Message/Create.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Messaging\Message;
 
+use DateInterval;
+use DateTime;
+use DateTimeInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
 use Dvsa\Olcs\Api\Domain\Command\Result;
@@ -19,6 +22,7 @@ use Dvsa\Olcs\Api\Entity\System\FeatureToggle;
 use Dvsa\Olcs\Api\Entity\Task\Task;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Dvsa\Olcs\Transfer\Command\Messaging\Message\Create as CreateMessageCommand;
+use RuntimeException;
 
 final class Create extends AbstractCommandHandler implements ToggleRequiredInterface, AuthAwareInterface
 {
@@ -30,10 +34,7 @@ final class Create extends AbstractCommandHandler implements ToggleRequiredInter
 
     protected $toggleConfig = [FeatureToggle::MESSAGING];
     protected $extraRepos = [
-        Repository\Conversation::class,
-        Repository\Message::class,
-        Repository\MessageContent::class,
-        Repository\Task::class,
+        Repository\Conversation::class, Repository\Message::class, Repository\MessageContent::class, Repository\Task::class,
     ];
 
     /**
@@ -47,16 +48,7 @@ final class Create extends AbstractCommandHandler implements ToggleRequiredInter
 
         $result = new Result();
 
-        $result
-            ->addId('message', $message->getId())
-            ->addMessage('Message created')
-            ->addId('messageContent', $message->getMessagingContent()->getId())
-            ->addMessage('MessageContent created')
-            ->addId('messageConversation', $message->getMessagingConversation()->getId())
-            ->addMessage('Message added to conversation')
-            ->addId('task', $updatedTask->getId())
-            ->addMessage(sprintf('Updated task action date: %s', $updatedTask->getActionDate()->format(\DateTimeInterface::ATOM)))
-            ->addMessage(sprintf('Updated task description: %s', $updatedTask->getDescription()));
+        $result->addId('message', $message->getId())->addMessage('Message created')->addId('messageContent', $message->getMessagingContent()->getId())->addMessage('MessageContent created')->addId('messageConversation', $message->getMessagingConversation()->getId())->addMessage('Message added to conversation')->addId('task', $updatedTask->getId())->addMessage(sprintf('Updated task action date: %s', $updatedTask->getActionDate()->format(DateTimeInterface::ATOM)))->addMessage(sprintf('Updated task description: %s', $updatedTask->getDescription()));
 
         return $result;
     }
@@ -91,17 +83,14 @@ final class Create extends AbstractCommandHandler implements ToggleRequiredInter
     private function createMessageContentEntity(string $text): MessagingContent
     {
         $entity = new MessagingContent();
-        $entity
-            ->setText($text);
+        $entity->setText($text);
         return $entity;
     }
 
     private function createMessageEntity(MessagingConversation $messagingConversation, MessagingContent $messagingContent): MessagingMessage
     {
         $entity = new MessagingMessage();
-        $entity
-            ->setMessagingConversation($messagingConversation)
-            ->setMessagingContent($messagingContent);
+        $entity->setMessagingConversation($messagingConversation)->setMessagingContent($messagingContent);
         return $entity;
     }
 
@@ -113,11 +102,6 @@ final class Create extends AbstractCommandHandler implements ToggleRequiredInter
     private function getMessageRepository(): Repository\Message
     {
         return $this->getRepo(Repository\Message::class);
-    }
-
-    private function getTaskRepository(): Repository\Task
-    {
-        return $this->getRepo(Repository\Task::class);
     }
 
     private function updateTaskDescriptionAndActionDate(CreateMessageCommand $command): Task
@@ -133,21 +117,26 @@ final class Create extends AbstractCommandHandler implements ToggleRequiredInter
     {
         if ($this->isInternalUser()) {
             return self::TASK_DESCRIPTION_ON_INTERNAL_REPLY;
-        } else if ($this->isExternalUser()) {
+        } elseif ($this->isExternalUser()) {
             return self::TASK_DESCRIPTION_ON_EXTERNAL_REPLY;
         }
 
-        throw new \RuntimeException("Unable to generate task description; not internal or external user.");
+        throw new RuntimeException("Unable to generate task description; not internal or external user.");
     }
 
-    private function determineActionDate(): \DateTime
+    private function determineActionDate(): DateTime
     {
         if ($this->isInternalUser()) {
-            return (new \DateTime())->add(new \DateInterval('P14D'));
-        } else if ($this->isExternalUser()) {
-            return (new \DateTime());
+            return (new DateTime())->add(new DateInterval('P14D'));
+        } elseif ($this->isExternalUser()) {
+            return (new DateTime());
         }
 
-        throw new \RuntimeException("Unable to determine task action date; not internal or external user.");
+        throw new RuntimeException("Unable to determine task action date; not internal or external user.");
+    }
+
+    private function getTaskRepository(): Repository\Task
+    {
+        return $this->getRepo(Repository\Task::class);
     }
 }
