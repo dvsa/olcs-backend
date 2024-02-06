f32f70685d96dc3aed389edfbfd2689c6a7058a4
wadedvsa <155439365+wadedvsa@users.noreply.github.com>
2024-02-06 15:17:10 +0000

    test: CreateTaskTest structure testing fix

--- test/module/Api/src/Domain/Command/Task/CreateTaskTest.php
+++ test/module/Api/src/Domain/Command/Task/CreateTaskTest.php
@@ -29,6 +29,7 @@ class CreateTaskTest extends \PHPUnit\Framework\TestCase
             'assignedToTeam' => 444,
             'isClosed' => true,
             'urgent' => true,
+            'messaging' => true,
             'application' => 555,
             'licence' => 666,
             'busReg' => 123,
