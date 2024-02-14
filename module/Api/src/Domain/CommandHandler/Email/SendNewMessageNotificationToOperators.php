757d5f4c2005d16913db87c10d1a44b7e3b7a9ab
Wade Womersley <155439365+wadedvsa@users.noreply.github.com>
2024-02-14 12:55:09 +0000

    fix: Use licence instead of application for sending email (#68)
    * fix: Use licence instead of application for sending email
    
    * fix: PHPCS

--- module/Api/src/Domain/CommandHandler/Email/SendNewMessageNotificationToOperators.php
+++ module/Api/src/Domain/CommandHandler/Email/SendNewMessageNotificationToOperators.php
@@ -9,29 +9,29 @@ use Dvsa\Olcs\Api\Domain\Command\Result;
 use Dvsa\Olcs\Api\Domain\Command\Task\CreateTask;
 use Dvsa\Olcs\Api\Domain\EmailAwareInterface;
 use Dvsa\Olcs\Api\Domain\Exception\MissingEmailException;
-use Dvsa\Olcs\Api\Domain\Repository\Application as ApplicationRepo;
-use Dvsa\Olcs\Api\Entity\Application\Application as ApplicationEntity;
+use Dvsa\Olcs\Api\Domain\Repository\Licence as LicenceRepo;
+use Dvsa\Olcs\Api\Entity\Licence\Licence as LicenceEntity;
 use Dvsa\Olcs\Api\Entity\System\Category;
 
 final class SendNewMessageNotificationToOperators extends AbstractEmailHandler implements EmailAwareInterface
 {
-    protected $repoServiceName = ApplicationRepo::class;
+    protected $repoServiceName = LicenceRepo::class;
     protected $template = 'messaging-new-message-operator';
     protected $subject = 'email.new-message-for-operator.subject';
 
-    /** @param ApplicationEntity $recordObject */
+    /** @param LicenceEntity $recordObject */
     protected function getRecipients($recordObject): array
     {
-        return $this->organisationRecipients($recordObject->getLicence()->getOrganisation());
+        return $this->organisationRecipients($recordObject->getOrganisation());
     }
 
-    /** @param ApplicationEntity $recordObject */
+    /** @param LicenceEntity $recordObject */
     protected function getTranslateToWelsh($recordObject)
     {
-        return $recordObject->getLicence()->getTranslateToWelsh();
+        return $recordObject->getTranslateToWelsh();
     }
 
-    /** @param ApplicationEntity $recordObject */
+    /** @param LicenceEntity $recordObject */
     protected function createMissingEmailTask($recordObject, Result $result, MissingEmailException $exception): Result
     {
         $taskData = [
@@ -40,11 +40,10 @@ final class SendNewMessageNotificationToOperators extends AbstractEmailHandler i
             'description'     => sprintf(
                 'Unable to send email - no organisation recipients found for Org: %s' .
                 ' - Please update the organisation admin user contacts to ensure at least one has a valid email address.',
-                $recordObject->getLicence()->getOrganisation()->getName(),
+                $recordObject->getOrganisation()->getName(),
             ),
             'actionDate'      => (new DateTimeImmutable())->format('Y-m-d'),
-            'licence'         => $recordObject->getLicence()->getId(),
-            'irhpApplication' => $recordObject->getId(),
+            'licence'         => $recordObject->getId(),
             'urgent'          => 'Y',
         ];
 
