5f6ca1e996c891c2bd9088da48832c926642a4f4
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-02-20 09:35:34 +0000

    feat: migrate config to application

---
+++ config/autoload/config-parameters.global.php
@@ -0,0 +1,25 @@
+<?php
+
+use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\SecretsManager;
+use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\ParameterStore;
+
+$environment = $_ENV['APP_ENV'] ?? null;
+
+$providers = [];
+
+if (!empty($environment)) {
+    $providers = [
+        SecretsManager::class => [
+            sprintf('DEVAPP%s-BASE-SM-APPLICATION-API', strtoupper($environment)),
+        ],
+        ParameterStore::class => [
+            sprintf('/applicationparams/%s/', strtolower($environment)),
+        ],
+    ];
+}
+
+return [
+    'config_parameters' => [
+        'providers' => $providers,
+    ],
+];
