dee379547159ada41e3f52e5dcd644f40d007421
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2024-03-28 14:46:54 +0000

    feat: Add assertions

--- test/module/Api/src/Domain/Validation/Validators/CanAccessDocumentTest.php
+++ test/module/Api/src/Domain/Validation/Validators/CanAccessDocumentTest.php
@@ -2,8 +2,14 @@
 
 namespace Dvsa\OlcsTest\Api\Domain\Validation\Validators;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Dvsa\Olcs\Api\Domain\Validation\Validators\CanAccessDocument;
 use Dvsa\Olcs\Api\Entity\Bus\LocalAuthority;
+use Dvsa\Olcs\Api\Entity\Doc\Document;
+use Dvsa\Olcs\Api\Entity\User\Permission;
+use Dvsa\Olcs\Api\Entity\User\Role;
+use Dvsa\Olcs\GdsVerify\Exception;
+use LmcRbacMvc\Identity\IdentityInterface;
 use Mockery as m;
 
 /**
@@ -16,6 +22,10 @@ class CanAccessDocumentTest extends AbstractValidatorsTestCase
      */
     protected $sut;
 
+    private const IS_INTERNAL_USER = 1;
+    private const IS_EXTERNAL_USER = 2;
+    private const IS_LOCAL_AUTHORITY_USER = 3;
+
     public function setUp(): void
     {
         $this->sut = m::mock(CanAccessDocument::class)
@@ -25,15 +35,26 @@ class CanAccessDocumentTest extends AbstractValidatorsTestCase
         parent::setUp();
     }
 
-    public function testIsValidParentTrue()
+    /**
+     * @dataProvider userCanAccessDocumentDataProvider
+     * @return void
+     * @throws Exception
+     */
+    public function testUserCanAccessDocument($userType, $document, $expectedResult)
     {
-        $txcInboxId = 52;
+        $this->getMockIdentity($userType);
 
-        $this->sut->shouldReceive('callParentIsValid')
-            ->with($txcInboxId)
-            ->andReturn(true);
+        $this->mockRepo(\Dvsa\Olcs\Api\Domain\Repository\Document::class)
+            ->shouldReceive('fetchById')
+            ->andReturn($document);
 
-        $this->assertEquals(true, $this->sut->isValid($txcInboxId));
+        $this->mockRepo(\Dvsa\Olcs\Api\Domain\Repository\TxcInbox::class)
+            ->shouldReceive('fetchLinkedToDocument')
+            ->andReturn([]);
+
+        $this->setIsValid('isOwner', [$document], true);
+
+        $this->assertEquals($expectedResult, $this->sut->isValid(123));
     }
 
     public function testIsValidNoTxcEntitiesFalse()
@@ -49,6 +70,8 @@ class CanAccessDocumentTest extends AbstractValidatorsTestCase
         $txcInboxRepo = $this->mockRepo('TxcInbox');
         $txcInboxRepo->shouldReceive('fetchLinkedToDocument')
             ->andReturn($linkedTxcInboxEntities);
+
+        $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_ADMIN, null)->once()->andReturn(false);
 
         $this->assertFalse($this->sut->isValid($txcInboxId));
     }
@@ -84,6 +107,10 @@ class CanAccessDocumentTest extends AbstractValidatorsTestCase
         $txcInboxRepo = $this->mockRepo('TxcInbox');
         $txcInboxRepo->shouldReceive('fetchLinkedToDocument')
             ->andReturn($linkedTxcInboxEntities);
+
+        $this->auth->shouldReceive('isGranted')->once()->andReturn(false);
+        $this->auth->shouldReceive('getIdentity')->once()->andReturn(false);
+
 
         $this->assertFalse($this->sut->isValid($txcInboxId));
     }
@@ -124,6 +151,75 @@ class CanAccessDocumentTest extends AbstractValidatorsTestCase
         $txcInboxRepo->shouldReceive('fetchLinkedToDocument')
             ->andReturn($linkedTxcInboxEntities);
 
+        $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_ADMIN, null)->once()->andReturn(false);
+
         $this->assertTrue($this->sut->isValid($txcInboxId));
+    }
+
+    private function getMockDocument(bool $isExternal): m\MockInterface {
+
+        $mockDoc = m::mock(Document::class);
+
+        $mockDoc->shouldReceive('getIsExternal')->andReturn($isExternal);
+        $mockDoc->allows('getId')->andReturn(123);
+
+        return $mockDoc;
+    }
+
+    private function getMockIdentity(int $userType): m\MockInterface {
+
+        $mockIdentity = m::mock(IdentityInterface::class);
+        $mockIdentity->shouldReceive('getUser->getOrganisationUsers')->andReturn(new ArrayCollection([]));
+        $mockIdentity->shouldReceive('getUser->isSystemUser')->andReturn(false);
+
+        $this->auth->shouldReceive('getIdentity')->andReturn($mockIdentity);
+
+        switch($userType) {
+            case static::IS_INTERNAL_USER:
+                $this->auth->shouldReceive('isGranted')->with(Permission::INTERNAL_USER, null)->andReturn(true);
+                $this->auth->shouldReceive('isGranted')->with(Permission::SELFSERVE_USER, null)->andReturn(false);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_USER, null)->twice()->andReturn(false);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_ADMIN, null)->andReturn(false);
+                break;
+            case static::IS_EXTERNAL_USER:
+                $this->auth->shouldReceive('isGranted')->with(Permission::INTERNAL_USER, null)->andReturn(false);
+                $this->auth->shouldReceive('isGranted')->with(Permission::SELFSERVE_USER, null)->andReturn(true);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_USER, null)->andReturn(false);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_ADMIN, null)->andReturn(false);
+                break;
+            case static::IS_LOCAL_AUTHORITY_USER:
+                $this->auth->shouldReceive('isGranted')->with(Permission::INTERNAL_USER, null)->andReturn(false);
+                $this->auth->shouldReceive('isGranted')->with(Permission::SELFSERVE_USER, null)->andReturn(true);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_USER, null)->andReturn(true);
+                $this->auth->shouldReceive('isGranted')->with(Permission::LOCAL_AUTHORITY_ADMIN, null)->andReturn(true);
+                break;
+            default:
+                throw new Exception();
+        }
+
+        return $mockIdentity;
+    }
+
+    public function userCanAccessDocumentDataProvider(): array
+    {
+        return [
+            'internalUserCanAccess' => [
+                'userType' => static::IS_INTERNAL_USER,
+                'document' => null,
+                'expected' => true
+            ],
+
+            'externalUserCanAccessExternalOwnedDocument' => [
+                'userType' => static::IS_EXTERNAL_USER,
+                'document' => $this->getMockDocument(true),
+                'expected' => true
+            ],
+
+            'externalUserCannotAccessInternalDocument' => [
+                'userType' => static::IS_EXTERNAL_USER,
+                'document' => $this->getMockDocument(false),
+                'expected' => false
+            ],
+        ];
     }
 }
