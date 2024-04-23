a24632a694d8cee0eebb63bd9e9248292d5c2d63
wadedvsa <155439365+wadedvsa@users.noreply.github.com>
2024-04-23 11:17:30 +0100

    fix: Missing extends

--- module/Olcs/Db/src/Service/Search/Indices/Person.php
+++ module/Olcs/Db/src/Service/Search/Indices/Person.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Olcs\Db\Service\Search\Indices;
 
-use InvalidArgumentException;
 use Olcs\Db\Service\Search\Indices\Terms\ComplexTermInterface;
 use Olcs\Db\Service\Search\Indices\Terms\TransportManagerLicenceStatus;
 
-class Person
+class Person extends AbstractIndex
 {
     /** @return ComplexTermInterface[] */
     public function getFilters(): array
