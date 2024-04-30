3a7d57fb6028d9133fe1dec1ae6912acff2cc21e
Andy Newton <andy.newton@dvsa.gov.uk>
2024-04-30 13:43:54 +0100

    feat: Remove custom AlignEntitiesToSchema script and supporting files.

--- Scripts/AlignEntitiesToSchema.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-chdir(__DIR__ . '/../');
-$loader = require_once(__DIR__ . '/../vendor/autoload.php');
-$loader->addPsr4('Cli\\', __DIR__ . '/Cli/');
-
-$cli = new \Cli\AlignEntitiesToSchema();
-$cli->run(include('config/application.config.php'));
