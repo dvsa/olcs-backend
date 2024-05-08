815c35855adea067f98493fea03e1195b0d07911
Ian Lindsay <ian@hemera-business-services.co.uk>
2024-05-08 12:25:27 +0100

    fix: VOL-5226 tighten permissions for irfo gv permit actions

--- module/Api/config/validation-map/irfo.config.php
+++ module/Api/config/validation-map/irfo.config.php
@@ -2,6 +2,7 @@
 
 use Dvsa\Olcs\Api\Domain\QueryHandler;
 use Dvsa\Olcs\Api\Domain\CommandHandler;
+use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalEdit;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc\IsInternalUser;
 
 return [
@@ -21,18 +22,18 @@ return [
     CommandHandler\Irfo\CreateIrfoGvPermit::class                                 => IsInternalUser::class,
     CommandHandler\Irfo\CreateIrfoPermitStock::class                              => IsInternalUser::class,
     CommandHandler\Irfo\CreateIrfoPsvAuth::class                                  => IsInternalUser::class,
-    CommandHandler\Irfo\GenerateIrfoGvPermit::class                               => IsInternalUser::class,
+    CommandHandler\Irfo\GenerateIrfoGvPermit::class                               => IsInternalEdit::class,
     CommandHandler\Irfo\GenerateIrfoPsvAuth::class                                => IsInternalUser::class,
     CommandHandler\Irfo\GrantIrfoPsvAuth::class                                   => IsInternalUser::class,
-    CommandHandler\Irfo\RefuseIrfoGvPermit::class                                 => IsInternalUser::class,
+    CommandHandler\Irfo\RefuseIrfoGvPermit::class                                 => IsInternalEdit::class,
     CommandHandler\Irfo\RefuseIrfoPsvAuth::class                                  => IsInternalUser::class,
-    CommandHandler\Irfo\ResetIrfoGvPermit::class                                  => IsInternalUser::class,
+    CommandHandler\Irfo\ResetIrfoGvPermit::class                                  => IsInternalEdit::class,
     CommandHandler\Irfo\UpdateIrfoDetails::class                                  => IsInternalUser::class,
-    CommandHandler\Irfo\UpdateIrfoGvPermit::class                                 => IsInternalUser::class,
+    CommandHandler\Irfo\UpdateIrfoGvPermit::class                                 => IsInternalEdit::class,
     CommandHandler\Irfo\UpdateIrfoPermitStock::class                              => IsInternalUser::class,
     CommandHandler\Irfo\UpdateIrfoPermitStockIssued::class                        => IsInternalUser::class,
     CommandHandler\Irfo\UpdateIrfoPsvAuth::class                                  => IsInternalUser::class,
-    CommandHandler\Irfo\WithdrawIrfoGvPermit::class                               => IsInternalUser::class,
+    CommandHandler\Irfo\WithdrawIrfoGvPermit::class                               => IsInternalEdit::class,
     CommandHandler\Irfo\WithdrawIrfoPsvAuth::class                                => IsInternalUser::class,
     CommandHandler\Irfo\RenewIrfoPsvAuth::class                                   => IsInternalUser::class,
     CommandHandler\Irfo\ResetIrfoPsvAuth::class                                   => IsInternalUser::class,
