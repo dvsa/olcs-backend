5de7707c88db487aac63b0d01b21729e3867d600
Shaun Hare <shaun.hare@dvsa.gov.uk>
2024-06-04 09:18:37 +0100

    merge conflicts

--- module/Api/src/Service/Ebsr/FileProcessor.php
+++ module/Api/src/Service/Ebsr/FileProcessor.php
@@ -4,7 +4,6 @@ namespace Dvsa\Olcs\Api\Service\Ebsr;
 
 use Dvsa\Olcs\Api\Filesystem\Filesystem;
 use Dvsa\Olcs\Api\Service\File\FileUploaderInterface;
-use Symfony\Component\Finder\Finder;
 use Laminas\Filter\Decompress;
 use Dvsa\Olcs\Api\Domain\Exception\EbsrPackException;
 use Laminas\Filter\Exception\RuntimeException as LaminasFilterRuntimeException;
@@ -13,13 +12,13 @@ use Laminas\Filter\Exception\RuntimeException as LaminasFilterRuntimeException;
  * Class FileProcessor
  * @package Dvsa\Olcs\Api\Service\Ebsr
  */
-class FileProcessor implements FileProcessorInterface
+class FileProcessor implements FileProcessorInterface, EbsrProcessingInterface
 {
+
     public const DECOMPRESS_ERROR_PREFIX = 'There was a problem with the pack file: ';
-    /**
-     * @var string
-     */
-    private $subDirPath = '';
+
+    const OUTPUT_TYPE = 'xmlFileName';
+
 
     /**
      * FileProcessor constructor.
@@ -27,10 +26,16 @@ class FileProcessor implements FileProcessorInterface
      * @param FileUploaderInterface $fileUploader     file uploader
      * @param Filesystem            $fileSystem       symphony file system component
      * @param Decompress            $decompressFilter decompression filter
-     * @param string                $tmpDir           the temporary directory
+     * @param string $tmpDir           the temporary directory
      */
-    public function __construct(private readonly FileUploaderInterface $fileUploader, private readonly Filesystem $fileSystem, private readonly Decompress $decompressFilter, private $tmpDir)
-    {
+
+    public function __construct(
+        private  FileUploaderInterface $fileUploader,
+        private readOnly Filesystem $fileSystem,
+        private  Decompress $decompressFilter,
+        private readOnly ZipProcessor $zipProcessor,
+        private readOnly string $tmpDir
+    ) {
     }
 
     /**
@@ -41,7 +46,7 @@ class FileProcessor implements FileProcessorInterface
      *
      * @return void
      */
-    public function setSubDirPath($subDirPath)
+    public function setSubDirPath($subDirPath): void
     {
         $this->subDirPath = $subDirPath;
     }
@@ -56,46 +61,43 @@ class FileProcessor implements FileProcessorInterface
      * @throws \RuntimeException
      * @throws EbsrPackException
      */
-    public function fetchXmlFileNameFromDocumentStore($identifier, $isTransXchange = false)
+    public function fetchXmlFileNameFromDocumentStore($identifier, $isTransXchange = false): string
     {
         $targetDir = $this->tmpDir . $this->subDirPath;
-
-        if (!$this->fileSystem->exists($targetDir)) {
-            throw new \RuntimeException('The specified tmp directory does not exist');
-        }
-
-        $file = $this->fileUploader->download($identifier);
-
-        $filePath = $this->fileSystem->createTmpFile($targetDir, 'ebsr');
-        $this->fileSystem->dumpFile($filePath, $file->getContent());
-
-        $tmpDir = $this->fileSystem->createTmpDir($targetDir, 'zip');
-        $this->decompressFilter->setTarget($tmpDir);
-
-        //attempt to decompress the zip file
         try {
-            $this->decompressFilter->filter($filePath);
-        } catch (LaminasFilterRuntimeException $e) {
-            throw new EbsrPackException(self::DECOMPRESS_ERROR_PREFIX . $e->getMessage());
+            $xmlFilename = $this->zipProcessor->process($identifier);
+            if(!$this->fileSystem->exists($targetDir)) {
+                throw new \RuntimeException('The specified tmp directory does not exist');
+            }
+
+
+            //transxchange runs through tomcat, therefore tomcat needs permissions on the files we've just created
+            if ($isTransXchange) {
+                $execCmd = 'setfacl -bR -m u:tomcat:rwx ' . dirname($xmlFilename);
+                exec(escapeshellcmd($execCmd));
+            }
         }
-
-        //transxchange runs through tomcat, therefore tomcat needs permissions on the files we've just created
-        if ($isTransXchange) {
-            $execCmd = 'setfacl -bR -m u:tomcat:rwx ' . $tmpDir;
-            exec(escapeshellcmd($execCmd));
+        catch (LaminasFilterRuntimeException $e) {
+            throw new EbsrPackException('Cannot unzip file : ' . $e->getMessage());
         }
+        return $xmlFilename;
+    }
 
-        $finder = new Finder();
-        $files = iterator_to_array($finder->files()->name('*.xml')->in($tmpDir));
-
-        if (count($files) > 1) {
-            throw new EbsrPackException('There is more than one XML file in the pack');
-        } elseif (!count($files)) {
-            throw new EbsrPackException('Could not find an XML file in the pack');
+    /**
+     * @param string $identifier
+     * @param array $options
+     * @throws EbsrPackException
+     */
+    public function process(string $identifier, array $options = []): string
+    {
+        if (!empty($options['isTransXchange'])) {
+            return $this->fetchXmlFileNameFromDocumentStore($identifier, true);
         }
+        return $this->fetchXmlFileNameFromDocumentStore($identifier);
+    }
 
-        $xml = key($files);
-
-        return $xml;
+    public function getOutputType(): string
+    {
+        return  self::OUTPUT_TYPE;
     }
 }
