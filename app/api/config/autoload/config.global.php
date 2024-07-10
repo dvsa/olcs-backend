e25ed5b47f926da6e994a3e9459c6d83edd79e94
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-07-10 11:57:45 +0100

    feat: change logging level to `WARN` (dvsa/olcs-backend#196)
    * feat: change logging level to `WARN`
    
    * fix: set the normal logger to `WARN`

--- app/api/config/autoload/config.global.php
+++ app/api/config/autoload/config.global.php
@@ -364,7 +364,7 @@ return [
                         'priority' => [
                             'name' => 'priority',
                             'options' => [
-                                'priority' => \Laminas\Log\Logger::DEBUG
+                                'priority' => \Laminas\Log\Logger::WARN
                             ]
                         ],
                     ]
@@ -381,7 +381,7 @@ return [
                         'priority' => [
                             'name' => 'priority',
                             'options' => [
-                                'priority' => \Laminas\Log\Logger::DEBUG
+                                'priority' => \Laminas\Log\Logger::WARN
                             ]
                         ],
                     ]
