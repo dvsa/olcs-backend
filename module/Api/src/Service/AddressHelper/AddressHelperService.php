e703613511ada7de27bf89084dbe51f3a1e0556a
James Wragg <james.wragg@dvsa.gov.uk>
2024-07-16 15:32:39 +0100

    feat: added address helper service to api module

---
+++ module/Api/src/Service/AddressHelper/AddressHelperService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\AddressHelper;
+
+use Dvsa\Olcs\Api\Domain\Exception\NotFoundException;
+use Dvsa\Olcs\Api\Domain\Repository;
+use Dvsa\Olcs\Api\Entity;
+use Dvsa\Olcs\DvsaAddressService\Service\AddressInterface;
+
+class AddressHelperService
+{
+    public function __construct(
+        protected AddressInterface $addressService,
+        protected Repository\PostcodeEnforcementArea $postcodeEnforcementAreaRepository,
+        protected Repository\AdminAreaTrafficArea $adminAreaTrafficAreaRepository)
+    {
+
+    }
+
+    public function lookupAddress(string $query): array
+    {
+        return $this->addressService->lookupAddress($query);
+    }
+
+    public function fetchTrafficAreaByPostcodeOrUprn(string $postcode): ?Entity\TrafficArea\TrafficArea
+    {
+        $addressData = $this->lookupAddress($postcode);
+        if (empty($addressData)) {
+            return null;
+        }
+
+        $addressData = $addressData[0];
+
+        $adminArea = $addressData['administrative_area'] ?? null;
+        if (empty($adminArea)) {
+            return null;
+        }
+
+        try {
+            return $this->adminAreaTrafficAreaRepository->fetchById($adminArea)->getTrafficArea();
+        } catch (NotFoundException $e) {
+            return null;
+        }
+    }
+
+    public function fetchEnforcementAreaByPostcode(string $postcode): ?Entity\EnforcementArea\EnforcementArea
+    {
+        /**
+         * This postcode pattern ensures that:
+         *  - The space between the prefix and the suffix is optional.
+         *  - It accurately captures the prefix and the first digit of the suffix.
+         *  - It matches the entire postal code structure, reducing the risk of partial or incorrect matches that
+         *    could lead to unexpected behavior or vulnerabilities.
+         */
+        preg_match('/^([A-Za-z]{1,2}\d{1,2})\s?(\d)[A-Za-z]{2}$/', $postcode, $matches);
+
+        if (empty($matches)) {
+            return null;
+        }
+
+        $prefix = $matches[1];
+        $suffixDigit = $matches[2];
+
+        $pea = $this->postcodeEnforcementAreaRepository->fetchByPostcodeId($prefix . ' ' . $suffixDigit);
+
+        if ($pea === null) {
+            // if not found, try by just the prefix
+            $pea = $this->postcodeEnforcementAreaRepository->fetchByPostcodeId($prefix);
+        }
+
+        return $pea?->getEnforcementArea();
+    }
+}
---
+++ module/Api/src/Service/AddressHelper/AddressHelperServiceFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\AddressHelper;
+
+use Dvsa\Olcs\Api\Domain\Repository;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+
+class AddressHelperServiceFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): AddressHelperService
+    {
+        return new AddressHelperService(
+            $container->get(AddressHelperService::class),
+            $container->get(Repository\PostcodeEnforcementArea::class),
+            $container->get(Repository\AdminAreaTrafficArea::class)
+        );
+    }
+}
