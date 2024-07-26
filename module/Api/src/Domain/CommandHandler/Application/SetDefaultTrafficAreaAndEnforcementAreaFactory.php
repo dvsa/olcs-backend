a6cdaa65431b8fd333e1c5127264b15de34f3805
James Wragg <5068769+jerotire@users.noreply.github.com>
2024-07-26 13:35:25 +0100

    fix: Fixes factories for command handlers and types (#206)

--- module/Api/src/Domain/CommandHandler/Application/SetDefaultTrafficAreaAndEnforcementAreaFactory.php
+++ module/Api/src/Domain/CommandHandler/Application/SetDefaultTrafficAreaAndEnforcementAreaFactory.php
@@ -2,13 +2,14 @@
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\Application;
 
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactioningCommandHandler;
 use Dvsa\Olcs\Api\Service\AddressHelper\AddressHelperService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
 class SetDefaultTrafficAreaAndEnforcementAreaFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): SetDefaultTrafficAreaAndEnforcementArea
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): TransactioningCommandHandler
     {
         $instance = new SetDefaultTrafficAreaAndEnforcementArea(
             $container->get(AddressHelperService::class)
