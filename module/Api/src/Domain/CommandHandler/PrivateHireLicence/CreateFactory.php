7b641446d61dc10a0d184f86e07444138f4e4c70
James Wragg <james.wragg@dvsa.gov.uk>
2024-07-26 13:29:29 +0100

    fix: Fixes factories for command handlers and types

--- module/Api/src/Domain/CommandHandler/PrivateHireLicence/CreateFactory.php
+++ module/Api/src/Domain/CommandHandler/PrivateHireLicence/CreateFactory.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\PrivateHireLicence;
 
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactioningCommandHandler;
 use Dvsa\Olcs\Api\Service\AddressHelper\AddressHelperService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
 class CreateFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Create
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): TransactioningCommandHandler
     {
         $instance = new Create(
             $container->get(AddressHelperService::class)
--- module/Api/src/Domain/CommandHandler/PrivateHireLicence/UpdateFactory.php
+++ module/Api/src/Domain/CommandHandler/PrivateHireLicence/UpdateFactory.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Dvsa\Olcs\Api\Domain\CommandHandler\PrivateHireLicence;
 
+use Dvsa\Olcs\Api\Domain\CommandHandler\TransactioningCommandHandler;
 use Dvsa\Olcs\Api\Service\AddressHelper\AddressHelperService;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
 class UpdateFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Update
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): TransactioningCommandHandler
     {
         $instance = new Update(
             $container->get(AddressHelperService::class)
